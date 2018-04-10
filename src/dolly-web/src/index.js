176c7293b413cd432ac36875c256fbebbd879f18
kefka <peter.flogstad@gmail.com>
2018-04-10 11:54:23 +0200

    Configs redux store in own file

--- src/dolly-web/src/index.js
+++ src/dolly-web/src/index.js
@@ -1,31 +1,19 @@
 import React from 'react';
-import {Provider} from 'react-redux';
 import ReactDOM from 'react-dom';
-import {ConnectedRouter} from 'react-router-redux';
+import AppFrame from './AppFrame';
 import createHistory from 'history/createBrowserHistory';
+import {Provider} from 'react-redux';
+import {ConnectedRouter} from 'react-router-redux';
 import Routes from './Routes';
 import registerServiceWorker from './registerServiceWorker';
-import AppFrame from './AppFrame';
 import {fetchPersons} from "./actions/personActions";
+import configureStore from './store/configureStore';
 import 'normalize.css';
-import {createStore, combineReducers, applyMiddleware} from 'redux';
-import reducerCollection from './reducers';
-import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
-import thunk from 'redux-thunk';
-import {routerReducer, routerMiddleware } from 'react-router-redux';
 
 
 const history = createHistory();
-const middleware = routerMiddleware(history);
 
-// Redux store config.
-const appReduxStore = createStore(
-    combineReducers({
-        ...reducerCollection,
-        router: routerReducer
-    }),
-    applyMiddleware(middleware, thunk, reduxImmutableStateInvariant())
-);
+const appReduxStore = configureStore(history);
 
 appReduxStore.dispatch(fetchPersons());
 
