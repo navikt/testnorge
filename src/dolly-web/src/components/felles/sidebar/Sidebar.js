64c510363d5ca70b86fdb4cfa488f3820896715d
Danishkh <dani95@hotmail.com>
2018-04-12 10:33:27 +0200

    Lagt til sidebar med submenus

--- src/dolly-web/src/components/felles/sidebar/Sidebar.js
+++ src/dolly-web/src/components/felles/sidebar/Sidebar.js
@@ -4,23 +4,64 @@ import './sidebar.css';
 
 
 export default class Sidebar extends React.Component {
+    constructor() {
+        super();
+        this.state = {showItems:[]}
+    }
+
+    onClick(index){
+        let showItems = this.state.showItems;
+        showItems[index] = !showItems[index];
+        this.setState({showItems});
+    }
 
     render() {
-
         return (
-            <div id="sidebar">
-                <h2>This is a sidebar</h2>
-                <SidebarLink path="/">
-                    This is a sidebar-link
-                </SidebarLink>
-                <div>
-                    This is element2
-                </div>
-                <div>
-                    This is element3
-                </div>
+           <div id="sidebar">
+               <ul>
+                   <li onClick={this.onClick.bind(this,0)}>
+                       Dolly link1
+                       {this.state.showItems[0] ?
+                           <div>
+                               <SidebarLink path="/" >Submenu1</SidebarLink>
+                           </div>
+                           : null
+                       }
+                   </li>
 
-            </div>
+                   <li onClick={this.onClick.bind(this,1)}>
+                       Dolly link2
+                        {this.state.showItems[1] ?
+                           <div>
+                               <SidebarLink path="/" >Submenu2</SidebarLink>
+                           </div>
+                           : null
+                        }
+                   </li>
+
+                   <li onClick={this.onClick.bind(this,2)}>
+                       Dolly link3
+                       {this.state.showItems[2] ?
+                            <div>
+                                <SidebarLink path="/">Submenu3</SidebarLink>
+                            </div>
+                            : null
+                       }
+                   </li>
+
+                   <li onClick={this.onClick.bind(this,3)}>
+                       Dolly link4
+                       {
+                           this.state.showItems[3] ?
+                               <div>
+                                   <SidebarLink path="/">Submenu4</SidebarLink>
+                               </div>
+                               : null
+                       }
+                   </li>
+               </ul>
+
+           </div>
         )
     }
 }
