724c0d5f0a0b45205671a1224092ebf8efc64052
Jarl Øystein Samseth <jarl.oystein.samseth@nav.no>
2018-04-12 18:44:48 +0200

    REG-178: Fullførte jpa for Bruker

--- dolly-app/src/main/java/no/nav/jpa/Testgruppe.java
+++ dolly-app/src/main/java/no/nav/jpa/Testgruppe.java
@@ -1,17 +1,24 @@
 package no.nav.jpa;
 
+import static no.nav.jpa.HibernateConstants.SEQUENCE_STYLE_GENERATOR;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Parameter;
 
-import javax.persistence.*;
-
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.time.LocalDateTime;
 import java.util.Set;
-
-import static no.nav.jpa.HibernateConstants.SEQUENCE_STYLE_GENERATOR;
 
 
 @Entity
