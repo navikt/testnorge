cbd42d438628ea8d187cc5da7c8aa16d371d5406
Danishkh <dani95@hotmail.com>
2018-04-13 07:28:28 +0200

    Lagt til en dropdown liste i header

--- src/dolly-web/src/components/felles/header/Header.js
+++ src/dolly-web/src/components/felles/header/Header.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './header.css';
+import {Row, Col, Glyphicon} from 'react-bootstrap';
+import DropdownMenuHeader from './user_menu/DropdownMenuHeader';
 
 export default () => {
     return (
-        <div id="dolly-header">
-            <h1>Dolly</h1>
+        <div className="dolly-header">
+            <Row className="header_row">
+                <Col md={2}>
+
+                </Col>
+
+                <Col md={8} mdOffset={1} mdPull={1}>
+                    <h1 className="header_title">Dolly</h1>
+                </Col>
+
+                <Col md={1} mdPull={1} className="header_dropdown">
+                    <DropdownMenuHeader />
+                </Col>
+            </Row>
         </div>
     );
 };
