f604534488027bfdeebfdab63be94fed1bbf1e2a
Danishkh <dani95@hotmail.com>
2018-04-13 08:32:07 +0200

    Endret Dropdown i header, fjernet warnings

--- dolly-webapp/src/dolly-web/src/components/felles/header/user_menu/DropdownMenuHeader.js
+++ dolly-webapp/src/dolly-web/src/components/felles/header/user_menu/DropdownMenuHeader.js
@@ -1,31 +1,28 @@
 import React from 'react';
 import { DropdownButton, MenuItem, Glyphicon} from 'react-bootstrap';
-import {NavLink} from 'react-router-dom';
+
 
 class DropdownMenuHeader extends React.Component {
+
     render() {
         const icon = <Glyphicon glyph="glyphicon glyphicon-user" />;
 
         return (
               <DropdownButton
                   bsSize="small"
-                  bsStyle="btn-primary"
+                  bsStyle="default"
                   title={icon}
                   key={1}
                   id="btn-id"
               >
-                  <MenuItem eventKey="1">
-                      Link 1
-                  </MenuItem>
-                  <MenuItem eventKey="2">Link 2</MenuItem>
-                  <MenuItem eventKey="3">
+                  <MenuItem eventKey="1" href="/#">Link 1</MenuItem>
+                  <MenuItem eventKey="2" href="/#">Link 2</MenuItem>
+                  <MenuItem eventKey="3" href="/#">
                       Link 3
                   </MenuItem>
                   <MenuItem divider />
-                  <MenuItem eventKey="4">
-                      <NavLink exact to="/gruppe/81">
-                          <Glyphicon glyph="	glyphicon glyphicon-log-in"/>&nbsp; Logg inn
-                      </NavLink>
+                  <MenuItem eventKey="4" href="/#">
+                      <Glyphicon glyph="glyphicon glyphicon-log-in"/>&nbsp; Logg inn
                   </MenuItem>
               </DropdownButton>
         );
