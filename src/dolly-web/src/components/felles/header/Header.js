f5a3a49ba236673a1ef179eacf1a0e1e8f17982d
Danishkh <dani95@hotmail.com>
2018-04-13 08:32:07 +0200

    Endret Dropdown i header, fjernet warnings

--- src/dolly-web/src/components/felles/header/Header.js
+++ src/dolly-web/src/components/felles/header/Header.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import './header.css';
-import {Row, Col, Glyphicon} from 'react-bootstrap';
+import {Row, Col} from 'react-bootstrap';
 import DropdownMenuHeader from './user_menu/DropdownMenuHeader';
 
 export default () => {
     return (
-        <div className="dolly-header">
-            <Row className="header_row">
+        <div id="dolly-header">
+            <Row id="header_row">
                 <Col md={2}>
 
                 </Col>
 
                 <Col md={8} mdOffset={1} mdPull={1}>
-                    <h1 className="header_title">Dolly</h1>
+                    <h1 id="header_title">Dolly</h1>
                 </Col>
 
-                <Col md={1} mdPull={1} className="header_dropdown">
+                <Col md={1} mdPull={1} id="header_dropdown">
                     <DropdownMenuHeader />
                 </Col>
             </Row>
