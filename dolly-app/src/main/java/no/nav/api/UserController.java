818129694f49edbfcd2acf6030ca4a94dd214cd3
Jarl Øystein Samseth <jarl.oystein.samseth@nav.no>
2018-04-17 14:16:41 +0200

    REG-178: Korrigert restkontroller

--- dolly-app/src/main/java/no/nav/api/UserController.java
+++ dolly-app/src/main/java/no/nav/api/UserController.java
@@ -1,6 +1,10 @@
 package no.nav.api;
 
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import no.nav.service.BrukerService;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,19 +12,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
-@RequestMapping(value = "api/v1/bruker", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value = "/api/v1/bruker", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 public class UserController {
 	
 	@Autowired
 	BrukerService brukerService;
 	
 	@PostMapping
-	public void opprettBruker(@RequestBody String brukerId) {
-		brukerService.opprettBruker(brukerId);
+	public void opprettBruker(@RequestBody() Map<String ,String> jsonNavIdent) {
+		brukerService.opprettBruker(jsonNavIdent.get("NavIdent"));
 	}
 	
-	//opprett Team på bruker (blir eier)
 	
 //	get team og grupper og testidenter for gitt medlemsskap og eierskap  (get all for gitt bruker)
 }
