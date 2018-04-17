08206307c0366f861ebd4f2bb3637667831d590a
Jarl Øystein Samseth <jarl.oystein.samseth@nav.no>
2018-04-17 17:14:10 +0200

    REG-178: Test for feilmelding fra opprettTeam

--- dolly-app/src/test/java/no/nav/service/TeamServiceTest.java
+++ dolly-app/src/test/java/no/nav/service/TeamServiceTest.java
@@ -1,17 +1,13 @@
 package no.nav.service;
 
 
-import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
-import static org.mockito.AdditionalMatchers.gt;
-import static org.mockito.AdditionalMatchers.leq;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 import no.nav.api.request.CreateTeamRequest;
+import no.nav.exceptions.DollyFunctionalException;
 import no.nav.jpa.Bruker;
 import no.nav.jpa.Team;
 import no.nav.repository.BrukerRepository;
@@ -24,6 +20,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.DuplicateKeyException;
 
 import java.time.LocalDateTime;
 
@@ -31,12 +29,12 @@ import java.time.LocalDateTime;
 public class TeamServiceTest {
 	@Mock
 	TeamRepository teamRepository;
-
+	
 	@Mock
 	BrukerRepository brukerRepository;
 	@InjectMocks
 	TeamService teamService;
-
+	
 	Bruker eier = new Bruker("eierId");
 	Team team = Team.builder().eier(eier).navn("teamnavn").datoOpprettet(LocalDateTime.now()).build();
 	CreateTeamRequest createTeamRequest = new CreateTeamRequest(team.getNavn(), team.getBeskrivelse(), team.getEier()
@@ -50,11 +48,23 @@ public class TeamServiceTest {
 	@Test
 	public void shouldMapOgOppretteTeam() {
 		teamService.opprettTeam(createTeamRequest);
+		
 		ArgumentCaptor<Team> argument = ArgumentCaptor.forClass(Team.class);
 		Mockito.verify(teamRepository).save(argument.capture());
-		assertEquals(team.getNavn(),argument.getValue().getNavn());
-		assertEquals(team.getBeskrivelse(),argument.getValue().getBeskrivelse());
-		assertEquals(team.getEier(),argument.getValue().getEier());
+		assertEquals(team.getNavn(), argument.getValue().getNavn());
+		assertEquals(team.getBeskrivelse(), argument.getValue().getBeskrivelse());
+		assertEquals(team.getEier(), argument.getValue().getEier());
 //		assertThat(argument.getValue().getDatoOpprettet(),is(leq(LocalDateTime.now()))); //FIXME
+	}
+	
+	/**
+	 * HVIS team eksisterer fra før SÅ skal funksjonell feilmelding kastes med beskrivende melding.
+	 * HVIS NonTransientDataAccessException kastes fra teamRepository.save, SÅ skal funksjonell feilmelding kastes med en beskrivende melding.
+	 */
+	@Test(expected = DollyFunctionalException.class)
+	public void shouldThrowFunctionalExceptionWhenDBConstraintIsViolated() {
+		Exception e = new Exception("root");
+		when(teamRepository.save(any())).thenThrow(new DuplicateKeyException("feilmelding",e));
+		teamService.opprettTeam(createTeamRequest);
 	}
 }
