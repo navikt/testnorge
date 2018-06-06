38d42956524b8f06aecd0000eb1e1c81ac589c5f
VSW\ludviand <mailpostkasse@gmail.com>
2018-06-06 12:14:52 +0200

    Cleaned Table html/css

--- dolly-webapp/src/dolly-web/src/components/felles/table/TableRow.js
+++ dolly-webapp/src/dolly-web/src/components/felles/table/TableRow.js
@@ -2,21 +2,10 @@ import React from 'react'
 
 const TableRow = ({ id, rowObject }) => {
 	const s = Object.keys(rowObject).map((key, idx) => {
-		return (
-			<td key={idx}>
-				<div
-					className="dolly-table-row-inner"
-					data-flex
-					data-layout="row"
-					data-layout-align="start center"
-				>
-					{rowObject[key]}
-				</div>
-			</td>
-		)
+		return <td key={idx}>{rowObject[key]}</td>
 	})
 
-	return <tr className="dolly-table-row">{s}</tr>
+	return <tr>{s}</tr>
 }
 
 export default TableRow
