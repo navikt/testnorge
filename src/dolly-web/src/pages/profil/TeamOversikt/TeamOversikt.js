628cbc1572104f4de827f880f40d650fc43a0655
Cong Nguyen <nguyencn1991@gmail.com>
2018-07-06 12:55:00 +0200

    opprettteam component

--- src/dolly-web/src/pages/profil/TeamOversikt/TeamOversikt.js
+++ src/dolly-web/src/pages/profil/TeamOversikt/TeamOversikt.js
@@ -4,20 +4,41 @@ import PropTypes from 'prop-types'
 import Overskrift from '~/components/overskrift/Overskrift'
 import Table from '~/components/table/Table'
 import Loading from '~/components/loading/Loading'
+import OpprettTeamConnector from '../OpprettTeam/OpprettTeamConnector'
 
 export default class TeamOversikt extends Component {
 	static propTypes = {
 		teams: PropTypes.object
 	}
 
+	state = {
+		opprettTeam: true
+	}
+
+	opprettToggle = () => {
+		this.setState({ opprettTeam: true })
+	}
+
+	opprettCancel = () => {
+		this.setState({ opprettTeam: false })
+	}
+
+	opprettSuccess = () => {
+		//send opprett call til API
+		this.setState({ opprettTeam: false })
+	}
+
 	render() {
 		const { teams, handleViewChange } = this.props
 		const { items, fetching, visning } = teams
 
-		console.log(items)
 		return (
 			<div className="team-tab">
-				<Overskrift type="h2" label="Teams" actions={[{ icon: 'add-circle', onClick: () => {} }]} />
+				<Overskrift
+					type="h2"
+					label="Teams"
+					actions={[{ icon: 'add-circle', onClick: this.opprettToggle }]}
+				/>
 
 				<div className="flexbox--space">
 					<ToggleGruppe onChange={handleViewChange} name="toggleGruppe">
@@ -30,21 +51,27 @@ export default class TeamOversikt extends Component {
 					</ToggleGruppe>
 				</div>
 
+				{this.state.opprettTeam && (
+					<OpprettTeamConnector onCancel={this.opprettCancel} onSuccess={this.opprettSuccess} />
+				)}
+
 				{fetching ? (
 					<Loading label="laster teams" panel />
 				) : (
 					<Table>
 						<Table.Header>
-							<Table.Column width="30" value="Navn" />
-							<Table.Column width="30" value="Eier" />
+							<Table.Column width="20" value="Navn" />
+							<Table.Column width="30" value="Beskrivelse" />
+							<Table.Column width="20" value="Eier" />
 							<Table.Column width="20" value="Personer" />
 						</Table.Header>
 
 						{items.map(team => (
 							<Table.Row key={team.id} editAction={() => {}} deleteAction={() => {}}>
-								<Table.Column width="30" value={team.navn} />
-								<Table.Column width="30" value={team.eierNavIdent} />
-								<Table.Column width="20" value={team.medlemmer.length.toString()} />
+								<Table.Column width="20" value={team.navn} />
+								<Table.Column width="30" value={team.beskrivelse} />
+								<Table.Column width="20" value={team.eierNavIdent} />
+								<Table.Column width="10" value={team.medlemmer.length.toString()} />
 							</Table.Row>
 						))}
 					</Table>
