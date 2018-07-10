73b2932a1d8da95c6f69f0b4a7d13ad6ba3300c3
Andreas Ludvigsen <andreas.ludvigsen@nav.no>
2018-07-10 13:53:52 +0200

    form -errors

--- dolly-webapp/src/dolly-web/src/pages/gruppeOversikt/Rediger/Rediger.js
+++ dolly-webapp/src/dolly-web/src/pages/gruppeOversikt/Rediger/Rediger.js
@@ -6,12 +6,15 @@ import { Formik, Form, Field } from 'formik'
 import { DollyApi } from '~/service/Api'
 import DisplayFormikState from '~/utils/DisplayFormikState'
 import Knapp from 'nav-frontend-knapper'
+import FormErrors from '~/components/formErrors/FormErrors'
+import * as yup from 'yup'
 
 import './Rediger.less'
 
 export default class Rediger extends PureComponent {
 	static propTypes = {
 		gruppe: PropTypes.shape({
+			id: PropTypes.string,
 			navn: PropTypes.string,
 			teamTilhoerlighetNavn: PropTypes.string,
 			hensikt: PropTypes.string
@@ -21,46 +24,71 @@ export default class Rediger extends PureComponent {
 		cancelRedigerOgOpprett: PropTypes.func
 	}
 
-	createGroup = async e => {
-		const { index, createGruppe, updateGruppe } = this.props
-		const res = gruppeObj.id ? await updateGruppe(gruppeObj) : await createGruppe(gruppeObj)
+	onHandleSubmit = (values, actions) => {
+		console.log('onHandleSubmit()', values, actions)
+		// const { createGruppe, updateGruppe } = this.props
+		// const res = gruppeObj.id ? await updateGruppe(gruppeObj) : await createGruppe(gruppeObj)
 	}
 
+	validation = () =>
+		yup.object().shape({
+			navn: yup.string().required('Navn er et påkrevd felt'),
+			teamId: yup
+				.number()
+				.nullable()
+				.required('Du må velge hvilket team gruppen skal knyttes til'),
+			hensikt: yup.string().required('Gi en liten beskrivelse av hensikten med gruppen')
+		})
+
 	render() {
-		const { cancelRedigerOgOpprett, currentUserId } = this.props
+		const { cancelRedigerOgOpprett, currentUserId, gruppe } = this.props
+
+		let initialValues = {
+			navn: '',
+			teamId: null,
+			hensikt: ''
+		}
+
+		if (this.props.gruppe) {
+			initialValues = Object.assign({}, initialValues, {
+				navn: gruppe.navn,
+				teamId: 3000000,
+				hensikt: gruppe.hensikt || ''
+			})
+		}
+
 		return (
 			<Formik
-				initialValues={{
-					navn: '',
-					team: null,
-					hensikt: ''
-				}}
-				onSubmit={(values, actions) => {
-					console.log(values, actions)
-				}}
+				initialValues={initialValues}
+				validationSchema={this.validation}
+				onSubmit={this.onHandleSubmit}
 				render={props => {
 					const { values, touched, errors, dirty, isSubmitting } = props
 					return (
 						<Form className="opprett-gruppe" autoComplete="off">
-							<Field name="navn" label="Navn" className="test" component={FormikInput} />
-							<Field
-								name="team"
-								label="Velg team"
-								component={FormikDollySelect}
-								loadOptions={() =>
-									DollyApi.getTeamsByUserId(currentUserId).then(
-										DollyApi.Utils.NormalizeTeamListForDropdown
-									)
-								}
-							/>
-							<Field name="hensikt" label="Hensikt" component={FormikInput} />
+							<div className="fields">
+								<Field name="navn" label="Navn" className="test" component={FormikInput} />
+								<Field
+									name="teamId"
+									label="Velg team"
+									component={FormikDollySelect}
+									loadOptions={() =>
+										DollyApi.getTeamsByUserId(currentUserId).then(
+											DollyApi.Utils.NormalizeTeamListForDropdown
+										)
+									}
+								/>
+								<Field name="hensikt" label="Hensikt" component={FormikInput} />
 
-							<Knapp type="hoved" htmlType="submit">
-								{this.props.redigering ? 'OPPDATER' : 'OPPRETT'}
-							</Knapp>
-							<Knapp type="standard" onClick={cancelRedigerOgOpprett}>
-								Avbryt
-							</Knapp>
+								<Knapp type="hoved" htmlType="submit">
+									{this.props.redigering ? 'OPPDATER' : 'OPPRETT'}
+								</Knapp>
+								<Knapp type="standard" onClick={cancelRedigerOgOpprett}>
+									Avbryt
+								</Knapp>
+							</div>
+
+							<FormErrors errors={errors} touched={touched} />
 							<DisplayFormikState {...props} />
 						</Form>
 					)
