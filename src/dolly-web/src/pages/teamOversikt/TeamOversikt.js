33a53506c27fe255b06e5efb1212ef416fe48ca6
Cong Nguyen <nguyencn1991@gmail.com>
2018-07-12 10:21:55 +0200

    Fjernet maintablecontainer. Gav ikke bedre oversikt, så reverted tilbake til at page/team og page/gruppe defineres seperat. Opprettet en felles styling fil kalt Oversikt.

--- src/dolly-web/src/pages/teamOversikt/TeamOversikt.js
+++ src/dolly-web/src/pages/teamOversikt/TeamOversikt.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { ToggleGruppe, ToggleKnapp } from 'nav-frontend-skjema'
+import Overskrift from '~/components/overskrift/Overskrift'
+import Input from '~/components/fields/Input/Input'
 import RedigerTeamConnector from './RedigerTeam/RedigerTeamConnector'
-import MainTableContainer from '~/components/mainTableContainer/MainTableContainer'
 import TeamListe from './TeamListe'
 
 export default class TeamOversikt extends Component {
@@ -22,27 +24,32 @@ export default class TeamOversikt extends Component {
 		const { teams, history, startOpprettTeam, startRedigerTeam } = this.props
 		const { items, fetching, visning, visOpprettTeam, editTeamId } = teams
 
-		const listComponent = (
-			<TeamListe
-				items={items}
-				fetching={fetching}
-				history={history}
-				startRedigerTeam={startRedigerTeam}
-				editTeamId={editTeamId}
-			/>
-		)
-		const createTeamComponent = <RedigerTeamConnector />
-
 		return (
-			<MainTableContainer
-				headerLabel="Teams"
-				startOpprettTeam={startOpprettTeam}
-				visning={visning}
-				visOpprettTeam={visOpprettTeam}
-				byttVisning={this.handleViewChange}
-				createTeamComponent={createTeamComponent}
-				listComponent={listComponent}
-			/>
+			<div className="oversikt-container">
+				<div className="flexbox--space">
+					<Overskrift label="Teams" actions={[{ icon: 'add-circle', onClick: startOpprettTeam }]} />
+					<Input name="sokefelt" className="label-offscreen" label="" placeholder="Søk" />
+				</div>
+
+				<div className="flexbox--space">
+					<ToggleGruppe onChange={this.handleViewChange} name="toggleGruppe">
+						<ToggleKnapp value="mine" checked={visning === 'mine'} key="1">
+							Mine
+						</ToggleKnapp>
+						<ToggleKnapp value="alle" checked={visning === 'alle'} key="2">
+							Alle
+						</ToggleKnapp>
+					</ToggleGruppe>
+				</div>
+				{visOpprettTeam && <RedigerTeamConnector />}
+				<TeamListe
+					items={items}
+					fetching={fetching}
+					history={history}
+					startRedigerTeam={startRedigerTeam}
+					editTeamId={editTeamId}
+				/>
+			</div>
 		)
 	}
 }
