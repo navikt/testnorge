46ac3eb7b47c9e482701d0f60646e1b5be0035a6
Cong Nguyen <nguyencn1991@gmail.com>
2018-07-18 09:00:01 +0200

    interface for å legge til brukere i team

---
+++ src/dolly-web/src/pages/team/LeggTilBruker/LeggTilBruker.js
@@ -0,0 +1,79 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+import { FormikDollySelect } from '~/components/fields/Select/Select'
+import { FormikInput } from '~/components/fields/Input/Input'
+import { Formik, Form, Field, getIn } from 'formik'
+import { DollyApi } from '~/service/Api'
+import DisplayFormikState from '~/utils/DisplayFormikState'
+import Knapp from 'nav-frontend-knapper'
+import FormErrors from '~/components/formErrors/FormErrors'
+import * as yup from 'yup'
+import Table from '~/components/table/Table'
+import Loading from '~/components/loading/Loading'
+
+export default class RedigerTeam extends PureComponent {
+	static propTypes = {}
+
+	erRedigering = Boolean(getIn(this.props.team, 'id', false))
+
+	onHandleSubmit = async (values, actions) => {
+		const { createTeam, updateTeam, team } = this.props
+		// const res = this.erRedigering ? await updateTeam(team.id, values) : await createTeam(values)
+	}
+
+	validation = () =>
+		yup.object().shape({
+			navIdent: yup.string().required('Bruker er et påkrevd felt')
+		})
+
+	render() {
+		const { closeLeggTilBruker, team, createOrUpdateFetching } = this.props
+		// if (createOrUpdateFetching) {
+		// 	return (
+		// 		<Table.Row>
+		// 			<Loading label="oppdaterer gruppe" />
+		// 		</Table.Row>
+		// 	)
+		// }
+
+		let initialValues = {
+			navIdent: ''
+		}
+
+		return (
+			<Formik
+				initialValues={initialValues}
+				validationSchema={this.validation}
+				onSubmit={this.onHandleSubmit}
+				render={props => {
+					const { values, touched, errors, dirty, isSubmitting } = props
+					return (
+						<Form className="opprett-tabellrad" autoComplete="off">
+							<h2>Legg til bruker</h2>
+							<div className="fields">
+								<Field
+									name="navIdent"
+									label="Velg en bruker"
+									component={FormikDollySelect}
+									loadOptions={() =>
+										DollyApi.getBrukere().then(DollyApi.Utils.NormalizeBrukerListForDropdown)
+									}
+								/>
+
+								<Knapp type="hoved" htmlType="submit">
+									Legg til
+								</Knapp>
+								<Knapp type="standard" htmlType="button" onClick={closeLeggTilBruker}>
+									Avbryt
+								</Knapp>
+							</div>
+
+							<FormErrors errors={errors} touched={touched} />
+							{/* <DisplayFormikState {...props} /> */}
+						</Form>
+					)
+				}}
+			/>
+		)
+	}
+}
--- src/dolly-web/src/service/services/dolly/Utils.js
+++ src/dolly-web/src/service/services/dolly/Utils.js
@@ -2,6 +2,11 @@ export const NormalizeTeamListForDropdown = ({ data }) => ({
 	options: data.map(team => ({ value: team.id, label: team.navn }))
 })
 
+export const NormalizeBrukerListForDropdown = ({ data }) => ({
+	options: data.map(bruker => ({ value: bruker.navIdent, label: bruker.navIdent }))
+})
+
 export default {
-	NormalizeTeamListForDropdown
+	NormalizeTeamListForDropdown,
+	NormalizeBrukerListForDropdown
 }
