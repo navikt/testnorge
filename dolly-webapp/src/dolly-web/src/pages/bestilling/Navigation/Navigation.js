f869c0b6b54fc2b3c12d5c7212d13832e9a07087
Andreas Ludvigsen <andreas.ludvigsen@nav.no>
2018-08-22 10:30:22 +0200

    Css i bestilling

--- dolly-webapp/src/dolly-web/src/pages/bestilling/Navigation/Navigation.js
+++ dolly-webapp/src/dolly-web/src/pages/bestilling/Navigation/Navigation.js
@@ -21,12 +21,12 @@ export default class Navigation extends PureComponent {
 		const { currentPage, isSubmitting, onClickNext, prevPage, abortBestilling } = this.props
 
 		return (
-			<div className="oppskrift-knapper">
+			<div className="step-navknapper">
 				<Knapp type="standard" onClick={abortBestilling}>
 					AVBRYT
 				</Knapp>
 
-				<div className="oppskrift-knapper_right">
+				<div className="step-navknapper--right">
 					{!isPage.first(currentPage) && (
 						<NavButton direction="backward" type="button" onClick={prevPage} />
 					)}
