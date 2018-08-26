1a1535adc0eaa71accd090c93a1f7a5fc3ab1e4f
Peter Flogstad <peter.sergio.flogstad@nav.no>
2018-08-26 10:55:05 +0200

    Øker ressursbruk.

--- dolly-app/src/main/java/no/nav/dolly/common/nais/NaisConfig.java
+++ dolly-app/src/main/java/no/nav/dolly/common/nais/NaisConfig.java
@@ -2,17 +2,19 @@ package no.nav.dolly.common.nais;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/internal")
 public class NaisConfig {
 
-    @GetMapping("/internal/isReady")
+    @GetMapping("/isReady")
     public ResponseEntity isReady() {
         return ResponseEntity.ok("Ready");
     }
 
-    @GetMapping("/internal/isAlive")
+    @GetMapping("/isAlive")
     public ResponseEntity isAlive() {
         return ResponseEntity.ok("Alive");
     }
