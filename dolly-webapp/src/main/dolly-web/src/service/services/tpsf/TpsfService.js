f2b619c123c4ec7340b38f77a84d12cb97bbaad3
Peter Flogstad <peter.sergio.flogstad@nav.no>
2018-08-27 14:21:44 +0200

    Reverserer å bruke k8s ved api kall frontend

--- dolly-webapp/src/main/dolly-web/src/service/services/tpsf/TpsfService.js
+++ dolly-webapp/src/main/dolly-web/src/service/services/tpsf/TpsfService.js
@@ -1,7 +1,6 @@
 import Request from '../Request'
 
-//const uri = 'https://tps-forvalteren-u2.nais.preprod.local/api/v1/dolly/testdata/personerdata'
-const uri = 'http://tps-forvalteren/api/v1/dolly/testdata/personerdata';
+const uri = 'https://tps-forvalteren-u2.nais.preprod.local/api/v1/dolly/testdata/personerdata'
 
 export default class TpsfService {
 	static getTestbrukere(userArray) {
