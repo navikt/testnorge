9dece63b783d379e431c92b760b4603435ce83d3
Peter Flogstad <peter.sergio.flogstad@nav.no>
2018-08-27 14:10:48 +0200

    Prøver å bruke k8s lastbalanserer

--- dolly-webapp/src/main/dolly-web/src/service/services/tpsf/TpsfService.js
+++ dolly-webapp/src/main/dolly-web/src/service/services/tpsf/TpsfService.js
@@ -1,6 +1,7 @@
 import Request from '../Request'
 
-const uri = 'https://tps-forvalteren-u2.nais.preprod.local/api/v1/dolly/testdata/personerdata'
+//const uri = 'https://tps-forvalteren-u2.nais.preprod.local/api/v1/dolly/testdata/personerdata'
+const uri = 'http://tps-forvalteren/api/v1/dolly/testdata/personerdata';
 
 export default class TpsfService {
 	static getTestbrukere(userArray) {
