94b78e813d1cf2da7d89afa2cf50e8c6004b739c
Andreas Ludvigsen <andreas.ludvigsen@nav.no>
2018-08-28 13:39:23 +0200

    debug config

--- src/main/dolly-web/src/config.js
+++ src/main/dolly-web/src/config.js
@@ -1,6 +1,16 @@
-export default {
+const isProdBuild = process.env.NODE_ENV === 'production'
+
+const config = {
 	services: {
 		dollyBackend: process.env.dollyBackend || '/external/dolly/api/v1',
 		tpsf: process.env.tpsf || '/external/tpsf/api/v1'
-	}
+	},
+	debug: true
 }
+
+// Force values in production build
+if (isProdBuild) {
+	config.debug = false
+}
+
+export default config
