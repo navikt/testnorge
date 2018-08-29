bb7bcf8be35d529b605840d128c6d40e906d4604
k153317 <Halvard.Hella.Kverna@nav.no>
2018-08-29 15:06:30 +0200

    REG-944: Oppsett av applikasjon

---
+++ Jenkinsfile
@@ -0,0 +1,5 @@
+applicationPipeline {
+    applicationName = "ident_pool"
+    javaVersion = "1.8"
+    environment = "q11"
+}
---
+++ ident-pool-core/src/main/java/no/nav/identpool/ApplicationConfig.java
@@ -0,0 +1,9 @@
+package no.nav.identpool;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties
+public class ApplicationConfig {
+}
