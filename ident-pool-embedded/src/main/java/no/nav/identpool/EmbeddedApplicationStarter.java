d06db0fdf2fcbf85046abcc4fa5aa3934627cdf1
Hansen, Even <even.hansen@nav.no>
2018-08-31 07:04:05 +0200

    REG-944 added flyway and embedded application starter

---
+++ ident-pool-embedded/src/main/java/no/nav/identpool/EmbeddedApplicationStarter.java
@@ -0,0 +1,12 @@
+package no.nav.identpool;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+public class EmbeddedApplicationStarter {
+    public static void main(String[] args) {
+       new SpringApplicationBuilder()
+               .sources(ApplicationConfig.class)
+               .profiles("local")
+               .run(args);
+    }
+}
