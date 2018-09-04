386835273d53932d10f32f7ae9c15ffb7b92f4d4
Andreas Ludvigsen <andreas.ludvigsen@nav.no>
2018-09-04 15:11:30 +0200

    REG-1155 liten bugfix. Bedre løsningn på sentrering

--- dolly-webapp/src/main/dolly-web/src/components/header/Header.js
+++ dolly-webapp/src/main/dolly-web/src/components/header/Header.js
@@ -8,24 +8,22 @@ import './Header.less'
 export default ({ brukerData }) => {
 	return (
 		<header className="app-header">
-			<NavLink to="/" className="home-nav app-header--third">
+			<NavLink to="/" className="home-nav">
 				<div className="img-logo">
 					<img alt="NAV logo" src={logo} />
 				</div>
 				<h1>Dolly</h1>
 			</NavLink>
 
-			<div className="menu-links app-header--third">
+			<div className="menu-links">
 				<NavLink to="/">Testdatagrupper</NavLink>
 				<NavLink to="/kømanager">Kømanager</NavLink>
 				<NavLink to="/swagger-ui.html">API-dok</NavLink>
 			</div>
 
-			<div className="app-header--third">
-				<NavLink to="/profil" className="header-user-name">
-					<Icon kind="user" size="20" /> {brukerData.navIdent}
-				</NavLink>
-			</div>
+			<NavLink to="/profil" className="header-user-name">
+				<Icon kind="user" size="20" /> {brukerData.navIdent}
+			</NavLink>
 		</header>
 	)
 }
