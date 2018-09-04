c2b6c942138b6b58e1417a21634b8509b13f9265
Cong Nguyen <nguyencn1991@gmail.com>
2018-09-04 08:35:38 +0200

    Flyttet history object til egen fil for å kunne gjenbrukes.

--- dolly-webapp/src/main/dolly-web/src/index.js
+++ dolly-webapp/src/main/dolly-web/src/index.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import createHistory from 'history/createBrowserHistory'
+import history from './history'
 import configureStore from './Store'
 import { ConnectedRouter } from 'connected-react-router'
 import AppConnector from './app/AppConnector'
 
 import './styles/main.less'
 
-const history = createHistory()
 const appReduxStore = configureStore(history)
 
 render(
