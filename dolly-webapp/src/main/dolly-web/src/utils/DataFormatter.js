6bf156972923a55adfac12c72516646887b10cd4
Andreas Ludvigsen <andreas.ludvigsen@nav.no>
2018-09-19 15:19:24 +0200

    props name copy paste error

--- dolly-webapp/src/main/dolly-web/src/utils/DataFormatter.js
+++ dolly-webapp/src/main/dolly-web/src/utils/DataFormatter.js
@@ -4,7 +4,7 @@ const formatters = {}
 
 // Skriv ut FNR og DNR med mellom mellom fødselsdato og personnummer
 // Ex: 010195 12345
-formatters.formatIdentNr = identNr => {
+formatters.formatIdentNr = ident => {
 	const birth = ident.substring(0, 6)
 	const personnummer = ident.substring(6, 11)
 	return `${birth} ${personnummer}`
