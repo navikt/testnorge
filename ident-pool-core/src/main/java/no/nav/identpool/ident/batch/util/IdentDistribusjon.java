944d1f5774728554e4660b9ff3ae86535dff5cba
Hansen, Even <even.hansen@nav.no>
2018-09-20 07:43:21 +0200

    REG-1029 matematikk for antall ønskede identer per fødselsdag

---
+++ ident-pool-core/src/main/java/no/nav/identpool/ident/batch/util/IdentDistribusjon.java
@@ -0,0 +1,21 @@
+package no.nav.identpool.ident.batch.util;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class IdentDistribusjon {
+
+    public Integer antallPersonerPerDag(Integer fraAar, Integer tomAar) {
+        Integer antallDager = 365*(tomAar-fraAar+1);
+        return (int) Math.ceil((double) hentKritiskAntallForTidsintervall(fraAar, tomAar)/(double) antallDager);
+    }
+
+    private Integer hentKritiskAntallForTidsintervall(Integer fraAar, Integer tomAar) {
+        return (verdiFraDistribusjon(fraAar) + verdiFraDistribusjon(tomAar+1))*(tomAar+1-fraAar)/2;
+    }
+
+    private Integer verdiFraDistribusjon(Integer aar) {
+        double aarDouble = (double) aar;
+        return (int) (Math.floor(60000.0/20.0*(Math.exp(-aarDouble/40.0) * Math.sqrt(aarDouble))/(2.0 * Math.sqrt(10.0 * 3.14159265359))));
+    }
+}
