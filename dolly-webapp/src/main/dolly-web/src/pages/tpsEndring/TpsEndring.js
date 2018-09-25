12fe71c995bd2cf9a3feb0dda3987e0d0cd5907f
Alex Nguyen <alex.nguyen@visma.com>
2018-09-25 14:30:09 +0200

    REG-1352: Placeholder til send doedsmelding

--- dolly-webapp/src/main/dolly-web/src/pages/tpsEndring/TpsEndring.js
+++ dolly-webapp/src/main/dolly-web/src/pages/tpsEndring/TpsEndring.js
@@ -1,6 +1,6 @@
 import React, { PureComponent, Fragment } from 'react'
 import Overskrift from '~/components/overskrift/Overskrift'
-import { Formik, Form, Field, setNestedObjectValues } from 'formik'
+import { Formik, Form, Field } from 'formik'
 import { FormikInput } from '~/components/fields/Input/Input'
 import ContentContainer from '~/components/contentContainer/ContentContainer'
 import './TPSEndring.less'
@@ -23,54 +23,99 @@ export default class TPSEndring extends PureComponent {
 	}
 
 	render() {
+		return (
+			<Fragment>
+				<Overskrift label={'Send TPS-endringsmelding'} />
+				{this._renderSendFoedselsmelding()}
+				{this._renderSendDoedsmelding()}
+			</Fragment>
+		)
+	}
+
+	_renderSendFoedselsmelding = () => {
 		let initialValues = {
-			morsId: 1,
-			farsID: 2,
-			IDType: 2,
+			morsId: 0,
+			farsId: 2,
+			IdType: 2,
 			foedseldato: 0,
 			kjoenn: 0,
 			miljoe: 0
 		}
+		return (
+			<ContentContainer>
+				<Formik
+					onSubmit={this.onSubmit}
+					initialValues={initialValues}
+					render={props => {
+						const { values, touched, errors, dirty, isSubmitting } = props
+						return (
+							<Form autoComplete="off">
+								<h2>Send fødselsmelding</h2>
+								<div className="tps-endring-foedselmelding-top">
+									<Field name="morsId" label="MORS IDENT" component={FormikInput} />
+									<Field name="farsId" label="FARS IDENT" component={FormikInput} />
+									<Field
+										name="BARNETS IDENTTYPE"
+										label="BARNETS IDENTTYPE"
+										component={FormikDollySelect}
+									/>
+									<Field name="IdType" label="BARNETS FØDSELSDATO" component={FormikDatepicker} />
+									<Field name="foedseldato" label="BARNETS KJØNN" component={FormikDollySelect} />
+									<Field name="miljoe" label="SEND TIL MILJØ" component={FormikDollySelect} />
+								</div>
+								<div className="tps-endring-foedselmelding-bottom">
+									<div className="flexbox">
+										<Checkbox id={'2'} label={'ARVE ADRESSE FRA MOR'} />
+										<Checkbox id={'3'} label={'NY ADRESSE'} />
+									</div>
+									<Knapp type="hoved" htmlType="submit">
+										Opprett fødselsmelding
+									</Knapp>
+								</div>
+							</Form>
+						)
+					}}
+				/>
+			</ContentContainer>
+		)
+	}
+
+	_renderSendDoedsmelding = () => {
+		let initialValues = {
+			id: 1,
+			handling: 2,
+			doedsdato: 2,
+			miljoe: 0
+		}
 
 		return (
-			<Fragment>
-				<Overskrift label={'Send TPS-endringsmelding'} />
-				<ContentContainer>
-					<Formik
-						onSubmit={this.onSubmit}
-						initialValues={initialValues}
-						render={props => {
-							const { values, touched, errors, dirty, isSubmitting } = props
-							return (
-								<Form autoComplete="off">
-									<h2>Send fødselsmelding</h2>
-									<div className="tps-endring-foedselmelding-top">
-										<Field name="morsId" label="MORS ID" component={FormikInput} />
-										<Field name="farsId" label="FARS ID" component={FormikInput} />
-										<Field
-											name="BARNETS IDENTYPE"
-											label="BARNETS IDENTYPE"
-											component={FormikDollySelect}
-										/>
-										<Field name="IDType" label="BARNETS FØDSELSDATO" component={FormikDatepicker} />
-										<Field name="foedseldato" label="BARNETS KJØNN" component={FormikDollySelect} />
-										<Field name="miljoe" label="SEND TIL MILJØ" component={FormikDollySelect} />
-									</div>
-									<div className="tps-endring-foedselmelding-bottom">
-										<div className="flexbox">
-											<Checkbox id={'2'} label={'ARV ADRESSE FRA MOR'} />
-											<Checkbox id={'3'} label={'NY ADRESSE'} />
-										</div>
-										<Knapp type="hoved" htmlType="submit">
-											Opprett fødselsmelding
-										</Knapp>
-									</div>
-								</Form>
-							)
-						}}
-					/>
-				</ContentContainer>
-			</Fragment>
+			<ContentContainer>
+				<Formik
+					onSubmit={this.onSubmit}
+					initialValues={initialValues}
+					render={props => {
+						const { values, touched, errors, dirty, isSubmitting } = props
+						return (
+							<Form autoComplete="off">
+								<h2>Send dødsmelding</h2>
+								<div className="tps-endring-foedselmelding-top">
+									<Field name="id" label="ID" component={FormikInput} />
+									<Field name="handling" label="HANDLING" component={FormikInput} />
+									<Field name="doedsdato" label="DØDSDATO" component={FormikInput} />
+									<Field name="miljoe" label="SEND TIL MILJØ" component={FormikInput} />
+									<div className="skjemaelement" />
+									<div className="skjemaelement" />
+								</div>
+								<div className="knapp-container">
+									<Knapp type="hoved" htmlType="submit">
+										Opprett fødselsmelding
+									</Knapp>
+								</div>
+							</Form>
+						)
+					}}
+				/>
+			</ContentContainer>
 		)
 	}
 }
