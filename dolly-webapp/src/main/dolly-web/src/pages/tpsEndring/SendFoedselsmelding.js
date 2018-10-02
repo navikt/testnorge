f7c10b56bcaf3b649552d661f36e3037950feccc
Alex Nguyen <alex.nguyen@visma.com>
2018-10-02 14:50:59 +0200

    REG-1352: Foedselsmelding

---
+++ dolly-webapp/src/main/dolly-web/src/pages/tpsEndring/SendFoedselsmelding.js
@@ -0,0 +1,141 @@
+import React, { PureComponent, Fragment } from 'react'
+import { Formik, Form, Field } from 'formik'
+import { FormikInput } from '~/components/fields/Input/Input'
+import ContentContainer from '~/components/contentContainer/ContentContainer'
+import { FormikDollySelect } from '~/components/fields/Select/Select'
+import { FormikDatepicker } from '~/components/fields/Datepicker/Datepicker'
+import { TpsfApi } from '~/service/Api'
+import TpsfService from '../../service/services/tpsf/TpsfService'
+import Knapp from 'nav-frontend-knapper'
+import * as yup from 'yup'
+import Loading from '~/components/loading/Loading'
+
+export default class SendFoedselsmelding extends PureComponent {
+	state = {
+		isFetching: false,
+		nyttBarn: null
+	}
+
+	render() {
+		let initialValues = {
+			identMor: '',
+			identFar: '',
+			identtype: 'FNR',
+			foedselsdato: '',
+			kjonn: '',
+			miljoe: 't0',
+			adresseFra: ''
+		}
+
+		const identOptions = [{ value: 'FNR', label: 'FNR' }, { value: 'DNR', label: 'DNR' }]
+		const kjoennOptions = [{ value: 'M', label: 'MENN' }, { value: 'F', label: 'KVINNE' }]
+		const adresseOptions = [
+			{ value: 'LAGNY', label: 'LAG NY ADRESSE' },
+			{ value: 'MOR', label: 'ARV FRA MORS' },
+			{ value: 'FAR', label: 'ARV FRA FARS' }
+		]
+
+		return (
+			<ContentContainer>
+				<Formik
+					onSubmit={this.onFoedselsMeldingSubmit}
+					validationSchema={this.validation}
+					initialValues={initialValues}
+					render={props => {
+						const { values, touched, errors, dirty, isSubmitting } = props
+						return (
+							<Form autoComplete="off">
+								<h2>Send fødselsmelding</h2>
+								<div className="tps-endring-foedselmelding-top">
+									<Field name="identMor" label="MORS IDENT" component={FormikInput} />
+									<Field name="identFar" label="FARS IDENT" component={FormikInput} />
+									<Field
+										name="identtype"
+										label="BARNETS IDENTTYPE"
+										component={FormikDollySelect}
+										options={identOptions}
+									/>
+									<Field
+										name="foedselsdato"
+										label="BARNETS FØDSELSDATO"
+										component={FormikDatepicker}
+									/>
+									<Field
+										name="kjonn"
+										label="BARNETS KJØNN"
+										component={FormikDollySelect}
+										options={kjoennOptions}
+									/>
+									<Field
+										name="miljoe"
+										label="SEND TIL MILJØ"
+										options={this.props.dropdownMiljoe}
+										component={FormikDollySelect}
+									/>
+								</div>
+								<div className="tps-endring-foedselmelding-bottom">
+									<Field
+										name="adresseFra"
+										label="ADRESSE"
+										component={FormikDollySelect}
+										options={adresseOptions}
+									/>
+									<Knapp type="hoved" htmlType="submit">
+										Opprett fødselsmelding
+									</Knapp>
+								</div>
+							</Form>
+						)
+					}}
+				/>
+
+				{this.state.isFetching && <Loading label="Sender fødselsmelding" />}
+				{this.state.nyttBarn && this._renderNyttBarn(this.state.nyttBarn)}
+			</ContentContainer>
+		)
+	}
+
+	validation = () =>
+		yup.object().shape({
+			identMor: yup
+				.string()
+				.max(11, 'Morindent må inneholde 11 sifre')
+				.required('Indent er et påkrevd felt'),
+			identFar: yup.string().max(11, 'Indent må inneholde 11 sifre'),
+			kjonn: yup.string().required('Kjønn er et påkrevd felt'),
+			miljoe: yup.string().required('Miljø er et påkrevd felt'),
+			foedselsdato: yup.date().required('Fødselsdato er et påkrevd felt'),
+			adresseFra: yup.string().required('Adresse er et påkrevd felt')
+		})
+
+	onFoedselsMeldingSubmit = values => {
+		this.setState({ isFetching: true }, () => {
+			TpsfApi.createFoedselmelding(values)
+				.then(fodselRes => {
+					console.log(fodselRes)
+					TpsfService.getKontaktInformasjon(fodselRes.data.personId, 't0')
+						.then(kontaktInfoRes => {
+							console.log(kontaktInfoRes)
+							this.setState({
+								nyttBarn: kontaktInfoRes.data.person,
+								isFetching: false
+							})
+						})
+						.catch(err => console.log(err.response))
+					this.setState({ isFetching: false })
+				})
+				.catch(err => console.log(err.response))
+		})
+	}
+
+	_renderNyttBarn = person => {
+		return (
+			<Fragment>
+				<h2>Gratulere, {person.personNavn.gjeldendePersonnavn} ble født! </h2>
+				<p className="build-version">
+					den {person.fodselsdato} med personnr {person.fodselsnummer}
+				</p>
+			</Fragment>
+		)
+	}
+}
--- dolly-webapp/src/main/dolly-web/src/pages/tpsEndring/TpsEndring.js
+++ dolly-webapp/src/main/dolly-web/src/pages/tpsEndring/TpsEndring.js
@@ -4,112 +4,25 @@ import { Formik, Form, Field } from 'formik'
 import { FormikInput } from '~/components/fields/Input/Input'
 import ContentContainer from '~/components/contentContainer/ContentContainer'
 import './TpsEndring.less'
-import Checkbox from '~/components/fields/Checkbox/Checkbox'
 import Knapp from 'nav-frontend-knapper'
-import { FormikDollySelect } from '~/components/fields/Select/Select'
-import { FormikDatepicker } from '~/components/fields/Datepicker/Datepicker'
-import { TpsfApi, DollyApi } from '~/service/Api'
-import TpsfService from '../../service/services/tpsf/TpsfService'
+import { EnvironmentManager } from '~/service/Kodeverk'
+import SendFoedselsmelding from './SendFoedselsmelding'
 
 export default class TPSEndring extends PureComponent {
-	state = {
-		isFetching: false,
-		foedselmeldingSent: false,
-		nyttBarn: null
+	constructor() {
+		super()
+		this.evns = new EnvironmentManager().getAllDropdownEnvironments()
 	}
 
 	render() {
 		return (
 			<Fragment>
 				<Overskrift label={'Send TPS-endringsmelding'} />
-				{this._renderSendFoedselsmelding()}
+				<SendFoedselsmelding dropdownMiljoe={this.evns} />
+				{/* {this._renderSendFoedselsmelding()} */}
 				{this._renderSendDoedsmelding()}
 			</Fragment>
 		)
-	}
-
-	validation = () =>
-		yup.object().shape({
-			navn: yup.string().required('Navn er et påkrevd felt'),
-			teamId: yup.number().nullable(),
-			// .required('Du må velge hvilket team gruppen skal knyttes til'),
-			hensikt: yup.string().required('Gi en liten beskrivelse av hensikten med gruppen')
-		})
-
-	onFoedselsMeldingSubmit = values => {
-		console.log(values)
-		this.setState({ isFetching: true }, () => {
-			TpsfApi.createFoedselmelding(values).then(res => {
-				TpsfService.getKontaktInformasjon(res.data.personId, 't0').then(res2 => {
-					console.log(res2)
-					this.setState({ nyttBarn: res2.data.person, foedselmeldingSent: true, isFetching: false })
-				})
-				// this.setState({ foedselmeldingSent: true, isFetching: false })
-			})
-		})
-	}
-
-	_renderSendFoedselsmelding = () => {
-		let initialValues = {
-			identMor: '01111206747',
-			identFar: '',
-			identtype: 'FNR',
-			foedselsdato: '2018-12-01T00:00:00',
-			kjonn: 'M',
-			miljoe: 't0',
-			adresseFra: 'LAGNY'
-		}
-		return (
-			<ContentContainer>
-				<Formik
-					onSubmit={this.onFoedselsMeldingSubmit}
-					initialValues={initialValues}
-					render={props => {
-						const { values, touched, errors, dirty, isSubmitting } = props
-						return (
-							<Form autoComplete="off">
-								<h2>Send fødselsmelding</h2>
-								<div className="tps-endring-foedselmelding-top">
-									<Field name="identMor" label="MORS IDENT" component={FormikInput} />
-									<Field name="identFar" label="FARS IDENT" component={FormikInput} />
-									<Field
-										name="identtype"
-										label="BARNETS IDENTTYPE"
-										component={FormikDollySelect}
-										// loadOptions={} //TODO: spoer Cong for load options her
-									/>
-									<Field name="date" label="BARNETS FØDSELSDATO" component={FormikDatepicker} />
-									<Field name="kjonn" label="BARNETS KJØNN" component={FormikDollySelect} />
-									<Field name="miljoe" label="SEND TIL MILJØ" component={FormikDollySelect} />
-								</div>
-								<div className="tps-endring-foedselmelding-bottom">
-									<Field name="adresseFra" label="ADRESSE" component={FormikDollySelect} />
-									<Knapp type="hoved" htmlType="submit">
-										Opprett fødselsmelding
-									</Knapp>
-								</div>
-							</Form>
-						)
-					}}
-				/>
-				{this.state.nyttBarn && this._renderNyttBarn(this.state.nyttBarn)}
-			</ContentContainer>
-		)
-	}
-
-	_renderNyttBarn = person => {
-		return (
-			<Fragment>
-				<h2>Gratulere, {person.personNavn.gjeldendePersonnavn} ble født! </h2>
-				<p className="build-version">
-					den {person.fodselsdato} med personnr {person.fodselsnummer}
-				</p>
-			</Fragment>
-		)
-	}
-
-	_renderNyttBarn2 = () => {
-		return <h2> ble født! </h2>
 	}
 
 	_renderSendDoedsmelding = () => {
