176f8680759a4131b1f61896b0c1f17839519406
Alex Nguyen <alex.nguyen@visma.com>
2018-10-02 16:05:58 +0200

    init doedsmelding

--- dolly-webapp/src/main/dolly-web/src/pages/tpsEndring/TpsEndring.js
+++ dolly-webapp/src/main/dolly-web/src/pages/tpsEndring/TpsEndring.js
@@ -1,12 +1,9 @@
 import React, { PureComponent, Fragment } from 'react'
 import Overskrift from '~/components/overskrift/Overskrift'
-import { Formik, Form, Field } from 'formik'
-import { FormikInput } from '~/components/fields/Input/Input'
-import ContentContainer from '~/components/contentContainer/ContentContainer'
 import './TpsEndring.less'
-import Knapp from 'nav-frontend-knapper'
 import { EnvironmentManager } from '~/service/Kodeverk'
 import SendFoedselsmelding from './SendFoedselsmelding'
+import SendDoedsmelding from './SendDoedsmelding'
 
 export default class TPSEndring extends PureComponent {
 	constructor() {
@@ -19,48 +16,8 @@ export default class TPSEndring extends PureComponent {
 			<Fragment>
 				<Overskrift label={'Send TPS-endringsmelding'} />
 				<SendFoedselsmelding dropdownMiljoe={this.evns} />
-				{/* {this._renderSendFoedselsmelding()} */}
-				{this._renderSendDoedsmelding()}
+				<SendDoedsmelding dropdownMiljoe={this.envs} />
 			</Fragment>
-		)
-	}
-
-	_renderSendDoedsmelding = () => {
-		let initialValues = {
-			id: 1,
-			handling: 2,
-			doedsdato: 2,
-			miljoe: 0
-		}
-
-		return (
-			<ContentContainer>
-				<Formik
-					onSubmit={this.onSubmit}
-					initialValues={initialValues}
-					render={props => {
-						const { values, touched, errors, dirty, isSubmitting } = props
-						return (
-							<Form autoComplete="off">
-								<h2>Send dødsmelding</h2>
-								<div className="tps-endring-foedselmelding-top">
-									<Field name="id" label="ID" component={FormikInput} />
-									<Field name="handling" label="HANDLING" component={FormikInput} />
-									<Field name="doedsdato" label="DØDSDATO" component={FormikInput} />
-									<Field name="miljoe" label="SEND TIL MILJØ" component={FormikInput} />
-									<div className="skjemaelement" />
-									<div className="skjemaelement" />
-								</div>
-								<div className="knapp-container">
-									<Knapp type="hoved" htmlType="submit">
-										Opprett dødsmelding
-									</Knapp>
-								</div>
-							</Form>
-						)
-					}}
-				/>
-			</ContentContainer>
 		)
 	}
 }
