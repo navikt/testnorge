662382eaebc0c84e5241a280d34e9d8aed3100ec
Kristoffer Mjelva <kristoffer.mjelva@nav.no>
2018-10-03 14:14:41 +0200

    REG-1191: Fikset navn på nais-modul

--- orkestratoren-nais/src/main/java/no/nav/registre/orkestratoren/NaisApplicationStarter.java
+++ orkestratoren-nais/src/main/java/no/nav/registre/orkestratoren/NaisApplicationStarter.java
@@ -1,4 +1,4 @@
-package no.nav.registre;
+package no.nav.registre.orkestratoren;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
