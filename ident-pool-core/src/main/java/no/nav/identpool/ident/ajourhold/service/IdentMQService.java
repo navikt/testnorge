5638686e1a242b309c29649b6931ba74f53ef587
k153317 <halvard.hella.kverna@nav.no>
2018-10-11 09:45:53 +0200

    REG-1028: Unused import

--- ident-pool-core/src/main/java/no/nav/identpool/ident/ajourhold/service/IdentMQService.java
+++ ident-pool-core/src/main/java/no/nav/identpool/ident/ajourhold/service/IdentMQService.java
@@ -2,8 +2,6 @@ package no.nav.identpool.ident.ajourhold.service;
 
 import java.io.StringReader;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
