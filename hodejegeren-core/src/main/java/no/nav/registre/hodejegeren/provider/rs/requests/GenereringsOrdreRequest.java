f7ce69f1dd6625d71ec39076b3dedee0db6b64a6
Jarl Øystein Samseth <jarl.oystein.samseth@nav.no>
2018-10-16 11:52:47 +0200

    REG-1711: AllArgsConstructor i request

--- hodejegeren-core/src/main/java/no/nav/registre/hodejegeren/provider/rs/requests/GenereringsOrdreRequest.java
+++ hodejegeren-core/src/main/java/no/nav/registre/hodejegeren/provider/rs/requests/GenereringsOrdreRequest.java
@@ -2,11 +2,15 @@ package no.nav.registre.hodejegeren.provider.rs.requests;
 
 import java.util.Map;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class GenereringsOrdreRequest {
 
     private Long gruppeId;
