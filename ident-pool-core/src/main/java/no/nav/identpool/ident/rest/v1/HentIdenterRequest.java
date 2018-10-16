a0540842f64dac3993f84c8efd2b1da5a8a0afd0
Bendik Stavelin <bendik.stavelin@nav.no>
2018-10-16 11:13:11 +0200

    fjerner unused import

--- ident-pool-core/src/main/java/no/nav/identpool/ident/rest/v1/HentIdenterRequest.java
+++ ident-pool-core/src/main/java/no/nav/identpool/ident/rest/v1/HentIdenterRequest.java
@@ -1,21 +1,16 @@
 package no.nav.identpool.ident.rest.v1;
 
 import java.time.LocalDate;
-
 import javax.validation.constraints.NotNull;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 import no.nav.identpool.ident.domain.Identtype;
 import no.nav.identpool.ident.domain.Kjoenn;
 
