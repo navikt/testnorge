cfb761c28fd4b90422df011b65ffe113a03f7ccc
Alex Nguyen <alex.nguyen@visma.com>
2018-10-26 13:19:11 +0200

    REG-1579: init request to dolly backend

--- dolly-webapp/src/main/dolly-web/src/service/kodeverk/AttributtManager/sigrun/Inntekt.ts
+++ dolly-webapp/src/main/dolly-web/src/service/kodeverk/AttributtManager/sigrun/Inntekt.ts
@@ -7,51 +7,42 @@ import DateValidation from '~/components/fields/Datepicker/DateValidation'
 import * as yup from 'yup'
 
 const AttributtListe: Attributt[] = [
-	{
-		// TODO: Bytt til radio buttons
-		hovedKategori: Kategorier.Inntekter,
-		subKategori: SubKategorier.Inntekt,
-		id: 'inntekter',
-		path: 'inntekter.inntekt',
-		label: 'Kun inntektsbeløp',
-		dataSource: DataSource.SIGRUN,
-		validation: yup.object(),
-		items: [
-			// {
-			// 	hovedKategori: Kategorier.Inntekt,
-			// 	subKategori: SubKategorier.Inntekt,
-			// 	id: 'typeinntekt',
-			// 	label: 'Type inntekt',
-			// 	dataSource: DataSource.SIGRUN,
-			// 	inputType: InputType.Select,
-			// 	// options: [{ label: 'test', value: 'test' }],
-			// 	validation: yup.string().required('Velg en type inntekt.')
-			// },
-			{
-				hovedKategori: Kategorier.Inntekt,
-				subKategori: SubKategorier.Inntekt,
-				id: 'beløp',
-				label: 'Beløp',
-				dataSource: DataSource.SIGRUN,
-				inputType: InputType.Number,
-				validation: yup.number().required('Oppgi en sum.')
-			},
-			{
-				hovedKategori: Kategorier.Inntekt,
-				subKategori: SubKategorier.Inntekt,
-				id: 'År',
-				label: 'År',
-				dataSource: DataSource.SIGRUN,
-				inputType: InputType.Date,
-				validation: DateValidation
-			}
-		]
-	},
+	// {
+	// 	hovedKategori: Kategorier.Inntekter,
+	// 	subKategori: SubKategorier.Inntekt,
+	// 	id: 'inntekter',
+	// 	path: 'inntekter.inntekt',
+	// 	label: 'Kun inntektsbeløp',
+	// 	dataSource: DataSource.SIGRUN,
+	// 	validation: yup.object(),
+	// 	items: [
+	// 		{
+	// 			hovedKategori: Kategorier.Inntekt,
+	// 			subKategori: SubKategorier.Inntekt,
+	// 			id: 'beløp',
+	// 			label: 'Beløp',
+	// 			path: 'inntekt.belop',
+
+	// 			dataSource: DataSource.SIGRUN,
+	// 			inputType: InputType.Number,
+	// 			validation: yup.number().required('Oppgi en sum.')
+	// 		},
+	// 		{
+	// 			hovedKategori: Kategorier.Inntekt,
+	// 			subKategori: SubKategorier.Inntekt,
+	// 			path: 'inntekt.aar',
+	// 			id: 'År',
+	// 			label: 'År',
+	// 			dataSource: DataSource.SIGRUN,
+	// 			inputType: InputType.Date,
+	// 			validation: DateValidation
+	// 		}
+	// 	]
+	// },
 	{
 		hovedKategori: Kategorier.Inntekter,
 		subKategori: SubKategorier.Inntekt,
 		id: 'beregnetskattgrunnlag',
-		path: 'inntekter.inntekt',
 		label: 'Beregnet skattegrunnlag',
 		dataSource: DataSource.SIGRUN,
 		validation: yup.object(),
@@ -61,6 +52,7 @@ const AttributtListe: Attributt[] = [
 				subKategori: SubKategorier.Inntekt,
 				id: 'typeinntekt',
 				label: 'Type inntekt',
+				path: 'grunnlag.tekniskNavn',
 				dataSource: DataSource.SIGRUN,
 				inputType: InputType.Select,
 				apiKodeverkId: 'Beregnet skatt',
@@ -70,7 +62,8 @@ const AttributtListe: Attributt[] = [
 			{
 				hovedKategori: Kategorier.Inntekt,
 				subKategori: SubKategorier.Inntekt,
-				id: 'beløp',
+				path: 'grunnlag.verdi',
+				id: 'beloep',
 				label: 'Beløp',
 				dataSource: DataSource.SIGRUN,
 				inputType: InputType.Number,
@@ -79,8 +72,9 @@ const AttributtListe: Attributt[] = [
 			{
 				hovedKategori: Kategorier.Inntekt,
 				subKategori: SubKategorier.Inntekt,
-				id: 'År',
+				id: 'aar',
 				label: 'År',
+				path: 'inntektsaar',
 				dataSource: DataSource.SIGRUN,
 				inputType: InputType.Date,
 				validation: DateValidation
