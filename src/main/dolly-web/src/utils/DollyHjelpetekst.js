71bf0ea58c933cc9c9869bf6f56bd13b204d4a2a
Cong Nguyen <nguyencn1991@gmail.com>
2018-10-29 10:10:10 +0100

    REG-1741: Test dekning for DataFormatters. Minor refactor og opprydding.

--- src/main/dolly-web/src/utils/DollyHjelpetekst.js
+++
@@ -1,7 +0,0 @@
-import React, { Fragment } from 'react'
-
-const DollyHjelpetekst = () => (
-	<Fragment>Testdatagruppen inneholder alle testpersonene dine (FNR/DNR).</Fragment>
-)
-
-export default DollyHjelpetekst
