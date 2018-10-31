2debf3528c95986278259b57892d0998d881dc75
Jarl Øystein Samseth <jarl.oystein.samseth@nav.no>
2018-10-31 15:10:19 +0100

    REG-1711: Endret fra årsakskode til endringskode, i enum med følger.

--- hodejegeren-core/src/test/java/no/nav/registre/hodejegeren/service/AarsakskodeTilFeltnavnMapperServiceTest.java
+++ hodejegeren-core/src/test/java/no/nav/registre/hodejegeren/service/AarsakskodeTilFeltnavnMapperServiceTest.java
@@ -1,5 +1,11 @@
 package no.nav.registre.hodejegeren.service;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.eq;
+
+import java.io.IOException;
+import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
@@ -8,41 +14,32 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import java.io.IOException;
-import java.util.List;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.ArgumentMatchers.eq;
-
 @RunWith(MockitoJUnitRunner.class)
 public class AarsakskodeTilFeltnavnMapperServiceTest {
-
+    
     @InjectMocks
     private AarsakskodeTilFeltnavnMapperService aarsakskodeTilFeltnavnMapperService;
-
+    
     @Mock
     private TpsStatusQuoService tpsStatusQuoService;
-
+    
     private String aksjonsKode = "A0";
     private String environment = "Q11";
     private String fnr = "12345678901";
     private String routineName = "FS03-FDNUMMER-PERSDATA-O";
-
+    
     /**
      * Testscenario: HVIS et kall gjøres til getStatusQuoFraAarsakskode med en aarsakskode, skal riktig servicerutine og feltnavn
      * bli sendt til getStatusQuo()
      */
     @Test
     public void shouldFindFeltnavnAndServiceRoutineFromAarsakskode() throws IOException {
-        AarsakskoderTrans1 aarsakskoderTrans1 = AarsakskoderTrans1.NAVNEENDRING_FOERSTE;
-
-        aarsakskodeTilFeltnavnMapperService.getStatusQuoFraAarsakskode(aarsakskoderTrans1, aksjonsKode, environment, fnr);
-
+        aarsakskodeTilFeltnavnMapperService.getStatusQuoFraAarsakskode(Endringskoder.NAVNEENDRING_FOERSTE, aksjonsKode, environment, fnr);
+        
         ArgumentCaptor<List<String>> captor = ArgumentCaptor.forClass(List.class);
         Mockito.verify(tpsStatusQuoService).getStatusQuo(eq(routineName), captor.capture(), eq(aksjonsKode), eq(environment), eq(fnr));
         List<String> actualRequestParams = captor.getValue();
-
+        
         assertEquals(2, actualRequestParams.size());
         assertTrue(actualRequestParams.contains("datoDo"));
         assertTrue(actualRequestParams.contains("statsborger"));
