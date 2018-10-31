be665689b0ccf24c7f72451e5808bf2c26c96b25
Lien, Reidar Kjøs <Reidar.Kjos.Lien@nav.no>
2018-10-31 11:41:04 +0100

    [REG-1577] catcher nullpointers

--- dolly-app/src/main/java/no/nav/dolly/appservices/tpsf/service/SigrunResponseHandler.java
+++ dolly-app/src/main/java/no/nav/dolly/appservices/tpsf/service/SigrunResponseHandler.java
@@ -9,9 +9,17 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class SigrunResponseHandler {
 
+    private boolean containsBadResponse;
+
     public String extractResponse(ResponseEntity<String> response) {
-        boolean contains_badresponse = !response.getBody().contains("400"); //pga. hacken til sigrun-stub blir hver sigrunReq lagt i en strengliste i body for responsen hver req ga på sitt endepunkt.
-        if (contains_badresponse) {
+        try {
+            containsBadResponse = !response.getBody().contains("400"); //pga. hacken til sigrun-stub blir hver sigrunReq lagt i en strengliste i body for responsen hver req ga på sitt endepunkt.
+        }catch(NullPointerException e) {
+            log.error("Body mangler i Sigrun response");
+            containsBadResponse = false;
+        }
+
+        if (containsBadResponse) {
             return "Ok";
         } else {
             log.error("{} failed", response.toString());
