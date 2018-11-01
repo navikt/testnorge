6a10e60c2ba277e7aea45bbe1220663bb8db6eea
Jarl Øystein Samseth <jarl.oystein.samseth@nav.no>
2018-11-01 16:33:07 +0100

    REG-1711: Stabiliserte identpoolconsumer

--- hodejegeren-core/src/main/java/no/nav/registre/hodejegeren/consumer/IdentPoolConsumer.java
+++ hodejegeren-core/src/main/java/no/nav/registre/hodejegeren/consumer/IdentPoolConsumer.java
@@ -1,9 +1,12 @@
 package no.nav.registre.hodejegeren.consumer;
 
-import java.util.ArrayList;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Arrays;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -16,19 +19,23 @@ import no.nav.registre.hodejegeren.consumer.requests.HentIdenterRequest;
 @Component
 public class IdentPoolConsumer {
     
-    @Value("${ident-pool.rest-api.url}")
-    private String serverUrl;
+    private static final ParameterizedTypeReference<List<String>> RESPONSE_TYPE = new ParameterizedTypeReference<List<String>>() {
+    };
+    private URI url;
     private RestTemplate restTemplate;
     
-    public IdentPoolConsumer() {
+    public IdentPoolConsumer(@Value("${ident-pool.rest-api.url}") String serverUrl) throws URISyntaxException {
         this.restTemplate = new RestTemplate();
-        ObjectMapper OBJECT_MAPPER = new ObjectMapper()
+        ObjectMapper objectMapper = new ObjectMapper()
                 .registerModule(new JavaTimeModule())
                 .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-        this.restTemplate.setMessageConverters(Arrays.asList(new MappingJackson2HttpMessageConverter(OBJECT_MAPPER)));
+        this.restTemplate.setMessageConverters(Arrays.asList(new MappingJackson2HttpMessageConverter(objectMapper)));
+        
+        this.url = new URI(serverUrl + "/v1/identifikator");
     }
     
     public List<String> hentNyeIdenter(HentIdenterRequest hentIdenterRequest) {
-        return restTemplate.postForObject(serverUrl + "/v1/identifikator", hentIdenterRequest, ArrayList.class);
+        RequestEntity postRequest = RequestEntity.post(url).body(hentIdenterRequest);
+        return restTemplate.exchange(postRequest, RESPONSE_TYPE).getBody();
     }
 }
