7748277f064fb660d5e74b543a528d57fe0de21d
Jarl Øystein Samseth <jarl.oystein.samseth@nav.no>
2018-11-01 15:41:46 +0100

    REG-1711: Fikset at restTemplate serialiserte LocalDate feil ([yyyy,M,d]). Nå lager den ISO8601 format

--- hodejegeren-core/src/main/java/no/nav/registre/hodejegeren/consumer/IdentPoolConsumer.java
+++ hodejegeren-core/src/main/java/no/nav/registre/hodejegeren/consumer/IdentPoolConsumer.java
@@ -1,10 +1,15 @@
 package no.nav.registre.hodejegeren.consumer;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import no.nav.registre.hodejegeren.consumer.requests.HentIdenterRequest;
 
@@ -13,7 +18,15 @@ public class IdentPoolConsumer {
     
     @Value("${ident-pool.rest-api.url}")
     private String serverUrl;
-    private RestTemplate restTemplate = new RestTemplate();
+    private RestTemplate restTemplate;
+    
+    public IdentPoolConsumer() {
+        this.restTemplate = new RestTemplate();
+        ObjectMapper OBJECT_MAPPER = new ObjectMapper()
+                .registerModule(new JavaTimeModule())
+                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        this.restTemplate.setMessageConverters(Arrays.asList(new MappingJackson2HttpMessageConverter(OBJECT_MAPPER)));
+    }
     
     public List<String> hentNyeIdenter(HentIdenterRequest hentIdenterRequest) {
         return restTemplate.postForObject(serverUrl + "/v1/identifikator", hentIdenterRequest, ArrayList.class);
