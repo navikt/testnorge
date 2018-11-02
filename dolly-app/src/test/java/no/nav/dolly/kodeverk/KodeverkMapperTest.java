96ee988a133b46986c08de7372810d8c15ab5496
Lien, Reidar Kjøs <Reidar.Kjos.Lien@nav.no>
2018-11-02 12:21:39 +0100

    [REG-1900] tester endret

--- dolly-app/src/test/java/no/nav/dolly/kodeverk/KodeverkMapperTest.java
+++ dolly-app/src/test/java/no/nav/dolly/kodeverk/KodeverkMapperTest.java
@@ -1,5 +1,6 @@
 package no.nav.dolly.kodeverk;
 
+import static org.hamcrest.CoreMatchers.allOf;
 import static org.hamcrest.CoreMatchers.both;
 import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.CoreMatchers.is;
@@ -7,6 +8,7 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasProperty;
 import static org.junit.Assert.assertThat;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -24,13 +26,15 @@ public class KodeverkMapperTest {
     private static final String STANDARD_KODE_2 = "5678";
     private static final String STANDARD_TERM = "term";
     private static final String STANDARD_TERM_2 = "term2";
+    private static final LocalDate STANDARD_GYLDIGFRA = LocalDate.of(1, 1, 1);
+    private static final LocalDate STANDARD_GYLDIGTIL = LocalDate.of(1,1,2);
     private static final String STANDARD_KODEVERK_NAVN = "navn";
     private static final String KODE_BOKMAAL = "nb";
     private KodeverkMapper kodeverkMapper = new KodeverkMapper();
     private Map<String, List<Betydning>> betydninger = new HashMap<>();
     private Map<String, Beskrivelse> beskrivelser = new HashMap<>();
     private Beskrivelse standardBeskrivelse = Beskrivelse.builder().tekst("tekst").term(STANDARD_TERM).build();
-    private Betydning standardBetydning = Betydning.builder().beskrivelser(beskrivelser).build();
+    private Betydning standardBetydning = Betydning.builder().beskrivelser(beskrivelser).gyldigTil(STANDARD_GYLDIGTIL).gyldigFra(STANDARD_GYLDIGFRA).build();
 
     @Before
     public void setup() {
@@ -45,19 +49,23 @@ public class KodeverkMapperTest {
         Map<String, Beskrivelse> beskrivelser2 = new HashMap<>();
         beskrivelser2.put(KODE_BOKMAAL, beskrivelse2);
 
-        Betydning betydning2 = Betydning.builder().beskrivelser(beskrivelser2).build();
+        Betydning betydning2 = Betydning.builder().beskrivelser(beskrivelser2).gyldigFra(STANDARD_GYLDIGFRA).gyldigTil(STANDARD_GYLDIGTIL).build();
         betydninger.put(STANDARD_KODE_2, Arrays.asList(betydning2));
 
         KodeverkAdjusted kodeverk = kodeverkMapper.mapBetydningToAdjustedKodeverk("navn", betydninger);
 
-        assertThat(kodeverk.getKoder(), hasItem(both(
-                hasProperty("value", equalTo(STANDARD_KODE))).and(
-                hasProperty("label", equalTo(STANDARD_KODE + " - " + STANDARD_TERM))
+        assertThat(kodeverk.getKoder(), hasItem(allOf(
+                hasProperty("value", equalTo(STANDARD_KODE)),
+                hasProperty("label", equalTo(STANDARD_TERM)),
+                hasProperty("gyldigTil", equalTo(STANDARD_GYLDIGTIL)),
+                hasProperty("gyldigFra", equalTo(STANDARD_GYLDIGFRA))
         )));
 
-        assertThat(kodeverk.getKoder(), hasItem(both(
-                hasProperty("value", equalTo(STANDARD_KODE_2))).and(
-                hasProperty("label", equalTo(STANDARD_KODE_2 + " - " + STANDARD_TERM_2))
+        assertThat(kodeverk.getKoder(), hasItem(allOf(
+                hasProperty("value", equalTo(STANDARD_KODE_2)),
+                hasProperty("label", equalTo(STANDARD_TERM_2)),
+                hasProperty("gyldigTil", equalTo(STANDARD_GYLDIGTIL)),
+                hasProperty("gyldigFra", equalTo(STANDARD_GYLDIGFRA))
         )));
 
         assertThat(kodeverk.getName(), is("navn"));
