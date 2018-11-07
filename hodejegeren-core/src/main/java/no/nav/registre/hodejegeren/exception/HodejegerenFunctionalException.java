fdcc0fee5fa26076bbf1a9a94e02f8469e7f4a9a
Kristoffer Mjelva <kristoffer.mjelva@nav.no>
2018-11-07 13:49:38 +0100

    REG-1711: Sonar issue

--- hodejegeren-core/src/main/java/no/nav/registre/hodejegeren/exception/HodejegerenFunctionalException.java
+++ hodejegeren-core/src/main/java/no/nav/registre/hodejegeren/exception/HodejegerenFunctionalException.java
@@ -2,7 +2,7 @@ package no.nav.registre.hodejegeren.exception;
 
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus()
+@ResponseStatus
 public class HodejegerenFunctionalException extends RuntimeException {
 
     public HodejegerenFunctionalException() {
