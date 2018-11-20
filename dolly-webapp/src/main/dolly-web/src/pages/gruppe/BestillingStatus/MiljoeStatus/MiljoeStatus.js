03cfba480b7a26492687271fc3bd8059f8b587c4
Alex Nguyen <alex.nguyen@visma.com>
2018-11-20 18:55:27 +0100

    REG-2084: add warning icon

--- dolly-webapp/src/main/dolly-web/src/pages/gruppe/BestillingStatus/MiljoeStatus/MiljoeStatus.js
+++ dolly-webapp/src/main/dolly-web/src/pages/gruppe/BestillingStatus/MiljoeStatus/MiljoeStatus.js
@@ -4,17 +4,32 @@ import Icon from '~/components/icon/Icon'
 import Button from '~/components/button/Button'
 
 export default class MiljoeStatus extends PureComponent {
-	_renderMiljoe = (env, key) => (
-		<div className={'miljoe'} key={key}>
-			<Icon size={'24px'} kind="feedback-check-circle" />
-			<p>{env}</p>
-		</div>
-	)
+	_renderMiljoe = (env, key, status) => {
+		const iconKind = status == 'success' ? 'feedback-check-circle' : 'report-problem-circle'
+		return (
+			<div className={'miljoe'} key={key}>
+				<Icon size={'24px'} kind={iconKind} />
+				<p>{env}</p>
+			</div>
+		)
+	}
 
 	render() {
 		const bestillingsData = this.props.bestillingsData
-		const miljoer = ['T0', 'Q1', 'U2', 'U3', 'U5']
 		console.log(this.props.bestillingsData, 'data')
+		const envs = bestillingsData.environments
+		const failedEnvs = ['t0']
+
+		// bestillingsData.personStatus.foreach(person => {
+		// 	envs.forEach(env => {
+		// 		if (!person.tpsfSuccessEnv.includes(env)) {
+		// 			failedEnvs.push(env) && envs.splice(envs.indexOf(env), 1)
+		// 		}
+		// 	})
+		// })
+
+		console.log(envs, 'all envs')
+		console.log(failedEnvs, 'failed envs')
 
 		return (
 			<div className="miljoe-status">
@@ -22,13 +37,16 @@ export default class MiljoeStatus extends PureComponent {
 					<p>Bestilling #{bestillingsData.id}</p>
 					<h3>Status i miljøene</h3>
 					<div className="remove-button-container">
-						<Button kind="remove-circle" />
+						<Button kind="remove-circle" onClick={this.props.onCloseButton} />
 					</div>
 				</div>
 				<hr />
 				<div className={'miljoe-container'}>
-					{bestillingsData.environments.map((env, i) => {
-						return this._renderMiljoe(env, i)
+					{envs.map((env, i) => {
+						return this._renderMiljoe(env, i, 'success')
+					})}
+					{failedEnvs.map((env, i) => {
+						return this._renderMiljoe(env, i, 'failed')
 					})}
 				</div>
 			</div>
