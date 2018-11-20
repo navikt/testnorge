733bba2b0a25e75df932ba6a0b3740e76af1972b
Jarl Øystein Samseth <jarl.oystein.samseth@nav.no>
2018-11-20 17:35:19 +0100

    REG-2101:  Komponenttest for arenaInntektSynt

--- orkestratoren-core/src/main/java/no/nav/registre/orkestratoren/consumer/rs/ArenaInntektSyntConsumer.java
+++ orkestratoren-core/src/main/java/no/nav/registre/orkestratoren/consumer/rs/ArenaInntektSyntConsumer.java
@@ -1,16 +1,22 @@
 package no.nav.registre.orkestratoren.consumer.rs;
 
 import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @Component
 public class ArenaInntektSyntConsumer {
 
-    private static final String url = "https://synth-arena-inntekt-{}.nais.preprod.local/api/v1/syntetisering/generer";
-    RestTemplate restTemplate = new RestTemplate();
+    RestTemplate restTemplate;
+    private String url;
 
-    public void genererEnInntektsmeldingPerFnrIInntektstub(List<String> fnr, String miljoe) {
-        restTemplate.postForObject(url, fnr, String.class, miljoe);
+    public ArenaInntektSyntConsumer(@Value("${synthdata-arena-inntekt.rest-api.url}") String baseUrl) {
+        this.url = baseUrl + "/v1/syntetisering/generer";
+        this.restTemplate = new RestTemplate();
+    }
+
+    public void genererEnInntektsmeldingPerFnrIInntektstub(List<String> fnr) {
+        restTemplate.postForObject(url, fnr, String.class);
     }
 }
--- orkestratoren-core/src/test/java/no/nav/registre/orkestratoren/StartSyntetiseringInntektCompTest.java
+++ orkestratoren-core/src/test/java/no/nav/registre/orkestratoren/StartSyntetiseringInntektCompTest.java
@@ -1,17 +1,44 @@
 package no.nav.registre.orkestratoren;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.created;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalToJson;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.okJson;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import no.nav.registre.orkestratoren.provider.rs.SyntetiseringsController;
+import no.nav.registre.orkestratoren.provider.rs.requests.SyntetiserInntektsmeldingRequest;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWireMock(port = 0)
 @ActiveProfiles("test")
 public class StartSyntetiseringInntektCompTest {
+
+    String expectedFnrMedInntektsmelding = "11111111111";
+    private long skdMeldingGruppeId = 123L;
+    private String miljoe = "t1";
+    @Autowired
+    private SyntetiseringsController syntetiseringsController;
+    @Value("${testnorges.ida.credential.tpsf.username}")
+    private String username;
+    @Value("${testnorges.ida.credential.tpsf.password}")
+    private String password;
 
     /**
      * Orkestratoren forventer å få fødselsnumrene i samme rekkefølge fra TPSF avspillergruppen hver gang.
@@ -23,7 +50,32 @@ public class StartSyntetiseringInntektCompTest {
      * Inntekt-synt "generer syntetisk inntektsmelding"-endepunktet.
      */
     @Test
-    public void shouldBestilleinntektsmeldingForFoersteHalvpartAvFnrFraTpsf() {
 
+    public void shouldBestilleinntektsmeldingForFoersteHalvpartAvFnrFraTpsf() {
+        stubTPSF();
+        stubInntektSynt();
+
+        List<String> fnr = syntetiseringsController.opprettSyntetiskInntektsmeldingIInntektstub(new SyntetiserInntektsmeldingRequest(skdMeldingGruppeId));
+        assertEquals(Arrays.asList(expectedFnrMedInntektsmelding), fnr);
+    }
+
+    private void stubInntektSynt() {
+        stubFor(post("/inntektsynt/api/v1/syntetisering/generer")
+                .withRequestBody(equalToJson("[\"" + expectedFnrMedInntektsmelding + "\"]"))
+                .willReturn(created()));
+    }
+
+    public void stubTPSF() {
+        //Hodejegeren henter fnr i avspillergruppa hos TPSF:
+        stubTpsfFiltrerIdenterPaaAarsakskode(skdMeldingGruppeId, "01,02,39", "[\"" + expectedFnrMedInntektsmelding + "\",\n\"22222222222\"\n,\n\"33333333333\"\n]");
+        stubTpsfFiltrerIdenterPaaAarsakskode(skdMeldingGruppeId, "43,32", "[\n\"33333333333\"\n]"); //død eller utvandret
+    }
+
+    private void stubTpsfFiltrerIdenterPaaAarsakskode(long gruppeId, String aarsakskode, String okJsonResponse) {
+        stubFor(get(urlPathEqualTo("/tpsf/api/v1/endringsmelding/skd/identer/" + gruppeId))
+                .withQueryParam("aarsakskode", equalTo(aarsakskode))
+                .withQueryParam("transaksjonstype", equalTo("1"))
+                .withBasicAuth(username, password)
+                .willReturn(okJson(okJsonResponse)));
     }
 }
