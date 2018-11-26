ae3c1c86ea5a7ef1c87812ee05223558e4d08b03
Kristen Hærum <kristen.haerum@nav.no>
2018-11-26 15:08:53 +0100

    REG-2231 Lagt på endepunkt for stopp av bestillingsprogressjon

--- dolly-app/src/main/java/no/nav/dolly/api/BestillingController.java
+++ dolly-app/src/main/java/no/nav/dolly/api/BestillingController.java
@@ -1,18 +1,21 @@
 package no.nav.dolly.api;
 
-import ma.glasnost.orika.MapperFacade;
-import no.nav.dolly.domain.resultset.RsBestilling;
-import no.nav.dolly.domain.resultset.RsBestillingProgress;
-import no.nav.dolly.service.BestillingProgressService;
-import no.nav.dolly.service.BestillingService;
-
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import ma.glasnost.orika.MapperFacade;
+import no.nav.dolly.domain.jpa.Bestilling;
+import no.nav.dolly.domain.resultset.RsBestilling;
+import no.nav.dolly.domain.resultset.RsBestillingProgress;
+import no.nav.dolly.service.BestillingProgressService;
+import no.nav.dolly.service.BestillingService;
 
 @RestController
 @RequestMapping(value = "/api/v1/bestilling", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -27,11 +30,20 @@ public class BestillingController {
     @Autowired
     private BestillingService bestillingService;
 
-    @GetMapping("/{bestillingsId}")
-    public RsBestilling checkBestillingsstatus(@PathVariable("bestillingsId") Long bestillingsId) {
-        List<RsBestillingProgress> progress = mapperFacade.mapAsList(progressService.fetchProgressButReturnEmptyListIfBestillingsIdIsNotFound(bestillingsId), RsBestillingProgress.class);
-        RsBestilling rsBestilling = mapperFacade.map(bestillingService.fetchBestillingById(bestillingsId), RsBestilling.class);
+    @GetMapping("/{bestillingId}")
+    public RsBestilling checkBestillingsstatus(@PathVariable("bestillingId") Long bestillingId) {
+        List<RsBestillingProgress> progress = mapperFacade.mapAsList(progressService.fetchProgressButReturnEmptyListIfBestillingsIdIsNotFound(bestillingId), RsBestillingProgress.class);
+        RsBestilling rsBestilling = mapperFacade.map(bestillingService.fetchBestillingById(bestillingId), RsBestilling.class);
         rsBestilling.setPersonStatus(progress);
         return rsBestilling;
+    }
+
+    @Transactional
+    @DeleteMapping("/stop/{bestillingId}")
+    public RsBestilling stopBestillingProgress(@PathVariable("bestillingId") Long bestillingId) {
+        Bestilling bestilling = bestillingService.fetchBestillingById(bestillingId);
+        bestilling.setFerdig(true);
+        bestillingService.saveBestillingToDB(bestilling);
+        return mapperFacade.map(bestilling, RsBestilling.class);
     }
 }
