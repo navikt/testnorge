47b64f516894f1873b85358f7827e8c233621de7
Jarl Øystein Samseth <jarl.oystein.samseth@nav.no>
2018-11-26 14:02:24 +0100

    REG-2101:  Fikset LocalApplicationStarterTest.java

--- orkestratoren-local/src/test/java/no/nav/registre/orkestratoren/LocalApplicationStarterTest.java
+++ orkestratoren-local/src/test/java/no/nav/registre/orkestratoren/LocalApplicationStarterTest.java
@@ -3,18 +3,13 @@ package no.nav.registre.orkestratoren;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@TestPropertySource(locations="classpath:application-test.properties")
-@Profile("local")
+@SpringBootTest(classes = LocalApplicationStarter.class, properties = "classpath:application-test.properties")
 public class LocalApplicationStarterTest {
 
-	@Test
-	public void contextLoads() {
-	}
-
+    @Test
+    public void contextLoads() {
+    }
 }
