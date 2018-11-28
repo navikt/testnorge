429351892ef1de926e87d6df60e40bd8293c5a7a
Jarl Øystein Samseth <jarl.oystein.samseth@nav.no>
2018-11-28 09:44:39 +0100

    REG-1022: Forbedret testen

--- ident-pool-core/src/test/java/no/nav/identpool/navnepool/NavnepoolServiceTilfeldigeNavnTest.java
+++ ident-pool-core/src/test/java/no/nav/identpool/navnepool/NavnepoolServiceTilfeldigeNavnTest.java
@@ -1,17 +1,15 @@
 package no.nav.identpool.navnepool;
 
+import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.when;
 
 import java.security.SecureRandom;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.mockito.stubbing.Answer;
 
 import no.nav.identpool.navnepool.domain.Navn;
 
@@ -20,41 +18,20 @@ public class NavnepoolServiceTilfeldigeNavnTest {
 
     private List<String> validFornavn = Arrays.asList("fornavn1", "fornavn2");
     private List<String> validEtternavn = Arrays.asList("etternavn1", "etternavn2", "etternavn3");
-    @Mock
-    private SecureRandom randomMock;
 
     private NavnepoolService navnepoolService;
 
     @Test
     public void shouldGetAListOfRandomNames() {
-        navnepoolService = new NavnepoolService(validFornavn, validEtternavn, randomMock);
-
-        when(randomMock.nextInt(2)).thenAnswer(answerIterationFornavn);
-        when(randomMock.nextInt(3)).thenAnswer(answerIterationEtternavn);
+        navnepoolService = new NavnepoolService(validFornavn, validEtternavn, new SecureRandom());
 
         int antallNavn = 2;
         List<Navn> tilfeldigeNavn = navnepoolService.hentTilfeldigeNavn(antallNavn);
 
         assertEquals(antallNavn, tilfeldigeNavn.size());
-        assertEquals(validFornavn.get(0), tilfeldigeNavn.get(0).getFornavn());
-        assertEquals(validFornavn.get(1), tilfeldigeNavn.get(1).getFornavn());
-        assertEquals(validEtternavn.get(0), tilfeldigeNavn.get(0).getEtternavn());
-        assertEquals(validEtternavn.get(1), tilfeldigeNavn.get(1).getEtternavn());
+        List<String> tilfeldigeFornavn = tilfeldigeNavn.stream().map(Navn::getFornavn).collect(Collectors.toList());
+        List<String> tilfeldigeEtternavn = tilfeldigeNavn.stream().map(Navn::getEtternavn).collect(Collectors.toList());
+        assertTrue(validFornavn.containsAll(tilfeldigeFornavn));
+        assertTrue(validFornavn.containsAll(tilfeldigeEtternavn));
     }
-
-    private Answer<Integer> answerIterationFornavn = new Answer<Integer>() {
-        private int i = 0;
-
-        @Override public Integer answer(InvocationOnMock invocation) throws Throwable {
-            return i++;
-        }
-    };
-
-    private Answer<Integer> answerIterationEtternavn = new Answer<Integer>() {
-        private int i = 0;
-
-        @Override public Integer answer(InvocationOnMock invocation) throws Throwable {
-            return i++;
-        }
-    };
 }
