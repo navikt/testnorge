99d420a52a97f7bc1e1431f6d4aa8a8bc67af669
Alex Nguyen <alex.nguyen@visma.com>
2018-11-29 08:40:52 +0100

    REG-1741: Init more tests

---
+++ dolly-webapp/src/main/dolly-web/src/components/fields/__tests__/Datepicker.test.js
@@ -0,0 +1,13 @@
+import Datepicker from '../Datepicker/Datepicker'
+import React from 'react'
+import { shallow } from 'enzyme'
+
+// TODO: Write test for datepicker
+describe('Datepicker.js', () => {
+	// const wrapper = shallow(<Datepicker />)
+	const a = 1
+	const res = 1
+	it('should always be true', () => {
+		expect(a).toBe(res)
+	})
+})
