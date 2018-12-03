a61ecfd76d309acdedafe25c47104330439193ca
Kristoffer Mjelva <kristoffer.mjelva@nav.no>
2018-12-03 14:03:12 +0100

    REG-1711: Skriv ut fnr i TPS-feilmelding

--- hodejegeren-core/src/main/java/no/nav/registre/hodejegeren/service/TpsStatusQuoService.java
+++ hodejegeren-core/src/main/java/no/nav/registre/hodejegeren/service/TpsStatusQuoService.java
@@ -58,8 +58,8 @@ public class TpsStatusQuoService {
         } else {
             JsonNode statusQuoFromTPS = root.findValue(felt);
             if (statusQuoFromTPS == null) {
-                log.error("Kunne ikke finne status quo på person med fnr {} for felt \"{}\". Utfyllende melding fra TPS: {}", fnr, felt, root.findValue("utfyllendeMelding").asText());
-                throw new ManglendeInfoITpsException("Kunne ikke finne status quo på person med fnr " + fnr + " for felt \"" + felt + "\". Utfyllende melding fra TPS: " + root.findValue("utfyllendeMelding").asText());
+                log.error("Kunne ikke finne status quo på person med fnr {} for felt '{}'. Utfyllende melding fra TPS: {}", fnr, felt, root.findValue("utfyllendeMelding").asText());
+                throw new ManglendeInfoITpsException("Kunne ikke finne status quo på person med fnr " + fnr + " for felt '" + felt + "'. Utfyllende melding fra TPS: " + root.findValue("utfyllendeMelding").asText());
             }
             return statusQuoFromTPS.asText();
         }
