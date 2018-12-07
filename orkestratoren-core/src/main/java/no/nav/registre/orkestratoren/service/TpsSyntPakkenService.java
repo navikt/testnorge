54259061dc7391c9906212eccec1db62b1454c15
Jarl Øystein Samseth <jarl.oystein.samseth@nav.no>
2018-12-07 13:04:08 +0100

    REG-2101:  autoformattering

--- orkestratoren-core/src/main/java/no/nav/registre/orkestratoren/service/TpsSyntPakkenService.java
+++ orkestratoren-core/src/main/java/no/nav/registre/orkestratoren/service/TpsSyntPakkenService.java
@@ -5,11 +5,9 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpStatusCodeException;
-
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
