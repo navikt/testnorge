634c0067e6a0753f6f0a045e065d039985c20041
Paul Magne Lunde <paul.magne.lunde@nav.no>
2018-12-08 15:28:12 +0100

    Byttet fra JpaRepo til Repo

--- ident-pool-core/src/main/java/no/nav/identpool/repository/IdentRepository.java
+++ ident-pool-core/src/main/java/no/nav/identpool/repository/IdentRepository.java
@@ -1,17 +1,25 @@
 package no.nav.identpool.repository;
 
 import java.time.LocalDate;
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import no.nav.identpool.domain.Identtype;
 import no.nav.identpool.domain.Rekvireringsstatus;
+import org.springframework.data.repository.Repository;
 
-public interface IdentRepository extends JpaRepository<IdentEntity, Long>, QuerydslPredicateExecutor<IdentEntity> {
+public interface IdentRepository extends Repository<IdentEntity, Long>, QuerydslPredicateExecutor<IdentEntity> {
 
     boolean existsByPersonidentifikator(String identifikator);
 
     long countByFoedselsdatoBetweenAndIdenttypeAndRekvireringsstatus(LocalDate from, LocalDate to, Identtype type, Rekvireringsstatus rekvireringsstatus);
 
     IdentEntity findTopByPersonidentifikator(String personidentifkator);
+
+    List<IdentEntity> saveAll(Iterable<IdentEntity> entities);
+
+    IdentEntity save(IdentEntity newIdentEntity);
+
+    void deleteAll();
 }
