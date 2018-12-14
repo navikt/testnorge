e8daee907883787d1ee8349f2cc789d70c6d6524
Cong Nguyen <nguyencn1991@gmail.com>
2018-12-14 11:01:30 +0100

    REG-1834: Tester + feilretting i logikk på paginering

--- src/main/dolly-web/src/components/pagination/Pagination.js
+++ src/main/dolly-web/src/components/pagination/Pagination.js
@@ -4,36 +4,46 @@ import Icon from '~/components/icon/Icon'
 
 import './Pagination.less'
 
-const ITEM_PER_PAGE = 10
+const ITEM_PER_PAGE = 8
 
 export default class Pagination extends Component {
 	constructor(props) {
 		super()
 		this.state = {
-			currentPage: 0,
-			pageCount: Math.ceil(props.items.length / ITEM_PER_PAGE),
-			currentItems: props.items.slice(0, ITEM_PER_PAGE)
+			currentPage: 0
 		}
 	}
 
-	_pageChangeHandler = e => {
-		const { items } = this.props
-		const { selected } = e
+	componentDidUpdate(prevProps) {
+		if (prevProps.items !== this.props.items) this.setState({ currentPage: 0 })
+	}
 
-		const startIndex = selected * ITEM_PER_PAGE
+	_pageChangeHandler = e => {
 		this.setState({
-			currentPage: e.selected,
-			currentItems: items.slice(startIndex, startIndex + ITEM_PER_PAGE)
+			currentPage: e.selected
 		})
 	}
 
-	render() {
-		const { currentItems, pageCount, currentPage } = this.state
-		const { items } = this.props
+	_calculatePageCount = () => {
+		return Math.ceil(this.props.items.length / ITEM_PER_PAGE)
+	}
 
-		const currentIndex = currentPage * ITEM_PER_PAGE + 1
-		const lastIndex = currentIndex + ITEM_PER_PAGE - 1
-		const itemCount = items.length
+	_calculateItemsToRender = () => {
+		const startIndex = this._calculateStartIndex()
+		return this.props.items.slice(startIndex, startIndex + ITEM_PER_PAGE)
+	}
+
+	_calculateStartIndex = () => {
+		return this.state.currentPage * ITEM_PER_PAGE
+	}
+
+	render() {
+		const pageCount = this._calculatePageCount()
+		const itemsToRender = this._calculateItemsToRender()
+
+		const startIndex = this._calculateStartIndex() + 1
+		const lastIndex = this._calculateStartIndex() + ITEM_PER_PAGE
+		const itemCount = this.props.items.length
 		const renderPagination = itemCount > ITEM_PER_PAGE
 
 		return (
@@ -41,11 +51,11 @@ export default class Pagination extends Component {
 				{renderPagination && (
 					<div className="pagination-wrapper">
 						<span className="pagination-label">
-							Viser {currentIndex}-{lastIndex > itemCount ? itemCount : lastIndex} av {items.length}
+							Viser {startIndex}-{lastIndex > itemCount ? itemCount : lastIndex} av {itemCount}
 						</span>
 						<ReactPaginate
 							containerClassName="pagination-container"
-							initialPage={currentPage}
+							forcePage={this.state.currentPage}
 							pageCount={pageCount}
 							pageRangeDisplayed={2}
 							marginPagesDisplayed={1}
@@ -55,7 +65,7 @@ export default class Pagination extends Component {
 						/>
 					</div>
 				)}
-				{this.props.render(currentItems)}
+				{this.props.render(itemsToRender)}
 			</Fragment>
 		)
 	}
