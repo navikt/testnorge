098bbb6ecb92e2a5f28225d19edd163bf50de14d
Jarl Øystein Samseth <jarl.oystein.samseth@nav.no>
2018-12-14 17:27:55 +0100

    REG-2101:  Laget endepunkt for at Arena meldekortproduksjon kan hente alle levende nordmenns FNR fra Tpsf-skdavspillergruppa som orkestratorens tps-batch arbeider på (og som Arena inntekt-synt baserer seg på).

---
+++ orkestratoren-core/src/main/java/no/nav/registre/orkestratoren/provider/rs/SyntDataInfoController.java
@@ -0,0 +1,29 @@
+package no.nav.registre.orkestratoren.provider.rs;
+
+import java.util.LinkedHashSet;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.ApiOperation;
+import no.nav.registre.orkestratoren.service.SyntDataInfoService;
+
+@RestController
+@RequestMapping("api/v1/info")
+public class SyntDataInfoController {
+
+    @Autowired
+    private SyntDataInfoService syntDataInfoService;
+    @Value("${orkestratoren.batch.skdMeldingGruppeId}")
+    private Long tpsBatchGruppeId;
+
+    @ApiOperation(value = "Endepunktet returnerer samtlige FNR på levende nordmenn i "
+            + "skdavspillergruppa i TPSF som orkestratorens tps-batch arbeider på. ",
+            notes = "Se fasit-ressursen orkestratorProperties for gruppeId-en.")
+    @GetMapping("fnr/levende-nordmenn")
+    public LinkedHashSet<String> hentAlleLevendeNordmennFraTpsf() {
+        return new LinkedHashSet(syntDataInfoService.opprettListenLevendeNordmenn(tpsBatchGruppeId));
+    }
+}
