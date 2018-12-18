c57fcd744c89cfdf6e3281878780ee92af692e97
Jarl Øystein Samseth <jarl.oystein.samseth@nav.no>
2018-12-18 14:46:51 +0100

    REG-2608:  Fikset bug og skrev test - endret til det riktige - ArenaInntektSyntPakkenService skal fjerne alle som er yngre enn 13 år

--- orkestratoren-core/src/main/java/no/nav/registre/orkestratoren/service/ArenaInntektSyntPakkenService.java
+++ orkestratoren-core/src/main/java/no/nav/registre/orkestratoren/service/ArenaInntektSyntPakkenService.java
@@ -3,6 +3,7 @@ package no.nav.registre.orkestratoren.service;
 import static no.nav.registre.orkestratoren.service.FnrUtility.getFoedselsdatoFraFnr;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,8 +23,9 @@ public class ArenaInntektSyntPakkenService {
 
     public List<String> genererEnInntektsmeldingPerFnrIInntektstub(SyntetiserInntektsmeldingRequest request) {
         List<String> levendeNordmennFnr = syntDataInfoService.opprettListenLevendeNordmenn(request.getSkdMeldingGruppeId());
-        List<String> inntektsmldMottakere = levendeNordmennFnr.subList(0, levendeNordmennFnr.size() / 2);
-        inntektsmldMottakere.removeIf(fnr -> getFoedselsdatoFraFnr(fnr).isBefore(LocalDate.now().minusYears(13)));//Mottakere av inntektsmeldinger må være minst 13 år
+        List<String> inntektsmldMottakere = new ArrayList<>();
+        inntektsmldMottakere.addAll(levendeNordmennFnr.subList(0, levendeNordmennFnr.size() / 2));
+        inntektsmldMottakere.removeIf(fnr -> getFoedselsdatoFraFnr(fnr).isAfter(LocalDate.now().minusYears(13)));//Mottakere av inntektsmeldinger må være minst 13 år
         if (!inntektsmldMottakere.isEmpty()) {
             arenaInntektSyntConsumer.genererEnInntektsmeldingPerFnrIInntektstub(inntektsmldMottakere);
         }
