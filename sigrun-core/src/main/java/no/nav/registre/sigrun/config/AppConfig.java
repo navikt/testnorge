a7333c537b6e17635c29307ce5090bf50f9e312a
Sandberg <S150876@ADEO.NO>
2019-01-17 14:26:19 +0100

    Added project skeleton

---
+++ sigrun-core/src/main/java/no/nav/registre/sigrun/config/AppConfig.java
@@ -0,0 +1,16 @@
+package no.nav.registre.sigrun.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public RestTemplate restTemplateTpsf() {
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate;
+    }
+}
