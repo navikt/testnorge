265c841381e8c59c7f437d7a15b3be04b229a808
Sandberg <S150876@ADEO.NO>
2019-01-18 13:41:09 +0100

    Changed to get request and now pushes to sigrun

--- sigrun-core/src/main/java/no/nav/registre/sigrun/globals/RestConnections.java
+++
@@ -1,5 +0,0 @@
-package no.nav.registre.sigrun.globals;
-
-public class RestConnections {
-    public static final String CONNECTION_POPP = "https://synthdata-synt-rest.nais.preprod.local/api/v1/generatePopp";
-}
