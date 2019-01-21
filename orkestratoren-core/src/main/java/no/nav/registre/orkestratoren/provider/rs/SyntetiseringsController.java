486722f7b507e9f32a34a5695a0720120febda21
Kristoffer Mjelva <kristoffer.mjelva@nav.no>
2019-01-21 14:15:46 +0100

    REG-2953: Sonar

--- orkestratoren-core/src/main/java/no/nav/registre/orkestratoren/provider/rs/SyntetiseringsController.java
+++ orkestratoren-core/src/main/java/no/nav/registre/orkestratoren/provider/rs/SyntetiseringsController.java
@@ -42,10 +42,15 @@ public class SyntetiseringsController {
     @PostMapping(value = "/tps/skdmeldinger/generer")
     public ResponseEntity opprettSkdMeldingerOgSendTilTps(@RequestBody SyntetiserSkdmeldingerRequest syntetiserSkdmeldingerRequest) {
         try {
-            SkdMeldingerTilTpsRespons skdMeldingerTilTpsRespons = (SkdMeldingerTilTpsRespons) tpsSyntPakkenService.produserOgSendSkdmeldingerTilTpsIMiljoer(syntetiserSkdmeldingerRequest.getAvspillergruppeId(),
-                    syntetiserSkdmeldingerRequest.getMiljoe(),
-                    syntetiserSkdmeldingerRequest.getAntallMeldingerPerEndringskode()).getBody();
-            return ResponseEntity.ok(skdMeldingerTilTpsRespons);
+            try {
+                SkdMeldingerTilTpsRespons skdMeldingerTilTpsRespons = (SkdMeldingerTilTpsRespons) tpsSyntPakkenService.produserOgSendSkdmeldingerTilTpsIMiljoer(syntetiserSkdmeldingerRequest.getAvspillergruppeId(),
+                        syntetiserSkdmeldingerRequest.getMiljoe(),
+                        syntetiserSkdmeldingerRequest.getAntallMeldingerPerEndringskode()).getBody();
+                return ResponseEntity.ok(skdMeldingerTilTpsRespons);
+            } catch (NullPointerException npe) {
+                log.error("NullPointerException: Fikk ikke riktig respons fra Testnorge-Skd. {}", npe);
+                throw npe;
+            }
         } catch (HttpStatusCodeException e) {
             return ResponseEntity.status(e.getStatusCode()).body(e.getResponseBodyAsString());
         }
