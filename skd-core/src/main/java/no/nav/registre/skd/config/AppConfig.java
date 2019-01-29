feda37d10358f3de3d0adb47d01b6e1b47f13d89
Kristoffer Mjelva <kristoffer.mjelva@nav.no>
2019-01-29 12:51:48 +0100

    REG-2937: La til syntrest i local

--- skd-core/src/main/java/no/nav/registre/skd/config/AppConfig.java
+++ skd-core/src/main/java/no/nav/registre/skd/config/AppConfig.java
@@ -2,15 +2,11 @@ package no.nav.registre.skd.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Random;
 
-import no.nav.registre.skd.consumer.TpsSyntetisererenConsumer;
-
 @Configuration
-@Import(TpsSyntetisererenConsumer.class)
 public class AppConfig {
 
     @Bean
