162558b16c28761046aa37faa11447bc9075d6d4
Eilertsen, Odin <odin.eilertsen@nav.no>
2019-01-29 15:05:46 +0100

    REG-3012 created controller, service, yaml, and added props for AAREG (needs testing later)

---
+++ src/main/java/no/nav/registre/syntrest/services/AaregService.java
@@ -0,0 +1,41 @@
+package no.nav.registre.syntrest.services;
+
+import org.json.JSONArray;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+
+@Service
+public class AaregService implements IService {
+
+    @Value("${synth-aareg-app}")
+    private String appName;
+
+    @Value("${isAlive}")
+    private String isAlive;
+
+    @Value("${synth-aareg-url}")
+    private String synthAaregUrl;
+
+    private final RestTemplate restTemplate;
+
+    public AaregService(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    @Async
+    public CompletableFuture<JSONArray> generateAaregFromNAIS(List<Map<String, String>> request) {
+        JSONArray result = restTemplate.postForObject(synthAaregUrl, request, JSONArray.class);
+        return CompletableFuture.completedFuture(result);
+    }
+
+    public String isAlive() {
+        return restTemplate.getForObject(String.format(isAlive, appName), String.class);
+    }
+}
