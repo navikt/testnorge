66e9e6b0151444ed89e25cbe2e9ec5603107b144
Traran <Betsy.Carina.Traran@nav.no>
2019-01-31 16:17:08 +0100

    REG-3125: Default feilmelding for kodeverk og tknr.

--- dolly-webapp/src/main/dolly-web/src/ducks/tknr/index.js
+++ dolly-webapp/src/main/dolly-web/src/ducks/tknr/index.js
@@ -5,7 +5,15 @@ import _isNil from 'lodash/isNil'
 import { createAction, handleActions, combineActions } from 'redux-actions'
 import success from '~/utils/SuccessAction'
 
-export const getEnhetByTknr = createAction('GET_TKNR', tknr => DollyApi.getEnhetByTknr(tknr))
+export const getEnhetByTknr = createAction('GET_TKNR', async tknr => {
+	try {
+		const res = await DollyApi.getEnhetByTknr(tknr)
+		return res
+	} catch (err) {
+		const failureData = { data: { enhetNr: tknr, navn: 'Finnes ikke i kodeverk' } }
+		return failureData
+	}
+})
 
 const initialState = {}
 
@@ -13,7 +21,6 @@ export default handleActions(
 	{
 		[success(getEnhetByTknr)](state, action) {
 			const { data } = action.payload
-
 			return { ...state, [data.enhetNr]: data.navn }
 		}
 	},
