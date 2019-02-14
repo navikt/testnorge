0eb0f48a01e3cde2e78f904ac6036faa58fb0082
Eilertsen, Odin <odin.eilertsen@nav.no>
2019-02-14 10:04:27 +0100

    Added grafana dependencies and properties

---
+++ src/main/java/no/nav/registre/syntrest/config/AutoTimingConfiguration.java
@@ -0,0 +1,18 @@
+package no.nav.registre.syntrest.config;
+
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+
+@Configuration
+@EnableAspectJAutoProxy
+public class AutoTimingConfiguration {
+
+    @Bean
+    public TimedAspect timedAspect(MeterRegistry registry) {
+        return new TimedAspect(registry);
+    }
+}
