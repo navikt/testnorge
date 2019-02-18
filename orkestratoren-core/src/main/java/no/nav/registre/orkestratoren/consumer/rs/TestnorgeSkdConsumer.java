27d03d53b4a96c579f3311ffbdd2fb8aa3d01ac5
Kristoffer Mjelva <kristoffer.mjelva@nav.no>
2019-02-18 15:01:17 +0100

    REG-2105: Fjernet unødvendig kasting av runtime-exceptions

--- orkestratoren-core/src/main/java/no/nav/registre/orkestratoren/consumer/rs/TestnorgeSkdConsumer.java
+++ orkestratoren-core/src/main/java/no/nav/registre/orkestratoren/consumer/rs/TestnorgeSkdConsumer.java
@@ -6,7 +6,6 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriTemplate;
 
@@ -33,7 +32,7 @@ public class TestnorgeSkdConsumer {
     }
 
     @Timed(value = "orkestratoren.resource.latency", extraTags = { "operation", "skd" })
-    public ResponseEntity startSyntetisering(GenereringsOrdreRequest genereringsOrdreRequest) throws HttpStatusCodeException {
+    public ResponseEntity startSyntetisering(GenereringsOrdreRequest genereringsOrdreRequest) {
         RequestEntity postRequest = RequestEntity.post(url.expand()).body(genereringsOrdreRequest);
         return restTemplate.exchange(postRequest, RESPONSE_TYPE);
     }
