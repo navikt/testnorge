251be9ca8380fa79d8768106cebb2133ae01b044
Kristen Hærum <kristen.haerum@nav.no>
2019-02-20 15:05:26 +0100

    REG-3313 Oppdatert mapping for KRR-stub

--- dolly-app/src/main/java/no/nav/dolly/mapper/strategy/DigitalKontaktMappingStrategy.java
+++ dolly-app/src/main/java/no/nav/dolly/mapper/strategy/DigitalKontaktMappingStrategy.java
@@ -6,16 +6,16 @@ import org.springframework.stereotype.Component;
 import ma.glasnost.orika.CustomMapper;
 import ma.glasnost.orika.MapperFactory;
 import ma.glasnost.orika.MappingContext;
-import no.nav.dolly.domain.resultset.RsDollyBestillingRequest;
+import no.nav.dolly.domain.resultset.RsDollyBestilling;
 import no.nav.dolly.domain.resultset.krrstub.DigitalKontaktdataRequest;
 import no.nav.dolly.mapper.MappingStrategy;
 
 @Component
 public class DigitalKontaktMappingStrategy implements MappingStrategy{
     @Override public void register(MapperFactory factory) {
-        factory.classMap(RsDollyBestillingRequest.class, DigitalKontaktdataRequest.class)
-                .customize(new CustomMapper<RsDollyBestillingRequest, DigitalKontaktdataRequest>() {
-                    @Override public void mapAtoB(RsDollyBestillingRequest bestilling, DigitalKontaktdataRequest kontaktdataRequest, MappingContext context) {
+        factory.classMap(RsDollyBestilling.class, DigitalKontaktdataRequest.class)
+                .customize(new CustomMapper<RsDollyBestilling, DigitalKontaktdataRequest>() {
+                    @Override public void mapAtoB(RsDollyBestilling bestilling, DigitalKontaktdataRequest kontaktdataRequest, MappingContext context) {
 
                         kontaktdataRequest.setGyldigFra(ZonedDateTime.now());
                         kontaktdataRequest.setEpost(bestilling.getKrrstub().getEpost());
