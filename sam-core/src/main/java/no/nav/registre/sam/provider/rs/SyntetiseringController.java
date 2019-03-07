1f5d8e249d7558075fbfcf090d5caa670e233e93
Sandberg <Eirik.Sandberg@nav.no>
2019-03-07 12:52:34 +0100

    REG-3435 Added synt controller

--- sam-core/src/main/java/no/nav/registre/sam/provider/rs/SyntetiseringController.java
+++ sam-core/src/main/java/no/nav/registre/sam/provider/rs/SyntetiseringController.java
@@ -1,4 +1,30 @@
 package no.nav.registre.sam.provider.rs;
 
+import no.nav.freg.spring.boot.starters.log.exceptions.LogExceptions;
+import no.nav.registre.sam.provider.rs.requests.SyntetiserSamRequest;
+import no.nav.registre.sam.service.SyntetiseringService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/syntetisering")
 public class SyntetiseringController {
+
+    @Autowired
+    private SyntetiseringService syntetiseringService;
+
+    @LogExceptions
+    @PostMapping(value = "/generer")
+    public ResponseEntity genererSammeldinger(@RequestBody SyntetiserSamRequest syntetiserSamRequest) {
+        System.out.println("Finner hoder");
+        List<String> identer = syntetiseringService.finnLevendeIdenter(syntetiserSamRequest);
+        System.out.println("Synter");
+        return syntetiseringService.finnSyntetiserteMeldinger(identer);
+    }
 }
