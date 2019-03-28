bd5218cb6af8180a1c47f81c650db33500697084
Kristen Hærum <kristen.haerum@nav.no>
2019-03-28 09:23:32 +0100

    REG-3494 Lagt til tester på AaregService

---
+++ dolly-app/src/test/java/no/nav/dolly/aareg/AaregServiceTest.java
@@ -0,0 +1,121 @@
+package no.nav.dolly.aareg;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static java.util.Collections.singletonList;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.ResponseEntity;
+
+import no.nav.dolly.domain.resultset.aareg.RsAaregOppdaterRequest;
+import no.nav.dolly.domain.resultset.aareg.RsAaregOpprettRequest;
+import no.nav.dolly.domain.resultset.aareg.RsAktoerPerson;
+import no.nav.dolly.domain.resultset.aareg.RsArbeidsforhold;
+import no.nav.dolly.domain.resultset.aareg.RsOrganisasjon;
+import no.nav.dolly.domain.resultset.aareg.RsPerson;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AaregServiceTest {
+
+    private static final String IDENT = "111111111111";
+    private static final String ENV = "u2";
+    private static final String ORGNUMMER = "222222222";
+    private static final Integer NAV_ARBFORHOLD_ID = 333333333;
+
+    @Mock
+    private AaregWsConsumer aaregWsConsumer;
+
+    @Mock
+    private AaregRestConsumer aaregRestConsumer;
+
+    @InjectMocks
+    private AaregService aaregService;
+
+    @Test
+    public void gjenopprettArbeidsforhold_intetTidligereArbeidsforholdFinnes_OK() {
+
+        when(aaregRestConsumer.readArbeidsforhold(IDENT, ENV)).thenReturn(ResponseEntity.ok(new Object[] {}));
+        aaregService.gjenopprettArbeidsforhold(singletonList(RsArbeidsforhold.builder().build()), singletonList("u2"), IDENT);
+        verify(aaregWsConsumer).opprettArbeidsforhold(any(RsAaregOpprettRequest.class));
+    }
+
+    @Test
+    public void gjenopprettArbeidsforhold_intetTidligereArbeidsforholdFinnes_lesKasterException() {
+
+        when(aaregRestConsumer.readArbeidsforhold(IDENT, ENV)).thenThrow(new RuntimeException());
+        aaregService.gjenopprettArbeidsforhold(singletonList(RsArbeidsforhold.builder().build()), singletonList("u2"), IDENT);
+        verify(aaregWsConsumer).opprettArbeidsforhold(any(RsAaregOpprettRequest.class));
+    }
+
+    @Test
+    public void gjenopprettArbeidsforhold_tidligereArbeidsforholdFinnes_arbeidsgiverHarOrgnummer() {
+
+        when(aaregRestConsumer.readArbeidsforhold(IDENT, ENV)).thenReturn(ResponseEntity.ok(buildArbeidsforhold(true)));
+
+        aaregService.gjenopprettArbeidsforhold(singletonList(RsArbeidsforhold.builder()
+                .arbeidsgiver(RsOrganisasjon.builder().orgnummer(ORGNUMMER).build())
+                .arbeidstaker(RsPerson.builder().ident(IDENT).build())
+                .build()), singletonList("u2"), IDENT);
+        verify(aaregWsConsumer).oppdaterArbeidsforhold(any(RsAaregOppdaterRequest.class));
+    }
+
+    @Test
+    public void gjenopprettArbeidsforhold_tidligereArbeidsforholdFinnes_arbeidsgiverHarPersonnr() {
+
+        when(aaregRestConsumer.readArbeidsforhold(IDENT, ENV)).thenReturn(ResponseEntity.ok(buildArbeidsforhold(false)));
+
+        aaregService.gjenopprettArbeidsforhold(singletonList(RsArbeidsforhold.builder()
+                .arbeidsgiver(RsAktoerPerson.builder().ident(IDENT).build())
+                .arbeidstaker(RsPerson.builder().ident(IDENT).build())
+                .build()), singletonList("u2"), IDENT);
+        verify(aaregWsConsumer).oppdaterArbeidsforhold(any(RsAaregOppdaterRequest.class));
+    }
+
+    @Test
+    public void gjenopprettArbeidsforhold_tidligereArbeidsforholdFinnes_sjekkReturStatus() {
+
+        when(aaregRestConsumer.readArbeidsforhold(IDENT, ENV)).thenReturn(ResponseEntity.ok(buildArbeidsforhold(false)));
+        Map<String, String> status = new HashMap<>();
+        status.put(ENV, "OK");
+        when(aaregWsConsumer.oppdaterArbeidsforhold(any(RsAaregOppdaterRequest.class))).thenReturn(status);
+
+        String result = aaregService.gjenopprettArbeidsforhold(singletonList(RsArbeidsforhold.builder()
+                .arbeidsgiver(RsAktoerPerson.builder().ident(IDENT).build())
+                .arbeidstaker(RsPerson.builder().ident(IDENT).build())
+                .build()), singletonList("u2"), IDENT);
+        verify(aaregWsConsumer).oppdaterArbeidsforhold(any(RsAaregOppdaterRequest.class));
+        assertThat(result, is(equalTo("u2: arbforhold=1$OK")));
+    }
+
+
+    private Object[] buildArbeidsforhold(boolean isOrgnummer) {
+        Map<String, Object> arbeidstaker = new HashMap<>();
+        arbeidstaker.put("offentligIdent", IDENT);
+        Map<String, Object> arbeidsgiver = new HashMap<>();
+        if (isOrgnummer) {
+            arbeidsgiver.put("type", "Organisasjon");
+            arbeidsgiver.put("organisasjonsnummer", ORGNUMMER);
+        } else {
+            arbeidsgiver.put("type", "Person");
+            arbeidsgiver.put("offentligIdent", IDENT);
+        }
+        Map<String, Object> arbeidsforhold = new HashMap<>();
+        arbeidsforhold.put("arbeidsgiver", arbeidsgiver);
+        arbeidsforhold.put("arbeidsforholdId", "1");
+        arbeidsforhold.put("arbeidstaker", arbeidstaker);
+        arbeidsforhold.put("navArbeidsforholdId", NAV_ARBFORHOLD_ID);
+
+        return newArrayList(arbeidsforhold).toArray();
+    }
+}
