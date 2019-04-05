2fdc24620bc5248f21e18e54aea54bcd500cee28
s152486 <ingvild.stoen@nav.no>
2019-04-05 10:36:00 +0200

    REG-1676 Ferdig exceleksport

--- dolly-webapp/src/main/dolly-web/src/components/modal/DollyModal.js
+++ dolly-webapp/src/main/dolly-web/src/components/modal/DollyModal.js
@@ -1,10 +1,7 @@
 import Modal from 'react-modal'
 import React, { PureComponent, Fragment } from 'react'
 import Lukknapp from 'nav-frontend-lukknapp'
-import Knapp from 'nav-frontend-knapper'
 import './DollyModal.less'
-import Button from '~/components/button/Button'
-
 
 const customStyles = {
 	content: {
@@ -16,7 +13,8 @@ const customStyles = {
 		transform: 'translate(-50%, -50%)',
 		width: '25%',
 		minWidth: '500px',
-		overflow: 'inherit',
+		overflow: 'auto',
+		maxHeight: '80%'
 	},
 	overlay: {
 		background: 'rgba(0,0,0,0.75)',
@@ -25,23 +23,18 @@ const customStyles = {
 
 Modal.setAppElement('#root')
 
-
 export default class DollyModal extends PureComponent {
-
 	render() {
-		const { content, isOpen, onRequestClose, closeModal, width, children } = this.props
+		const { content, isOpen, onRequestClose, closeModal, width } = this.props
 		if (width) customStyles.content.width = width
 		else customStyles.content.width = '25%'
 
 		return (
 			<Modal isOpen={isOpen} shouldCloseOnEsc onRequestClose={onRequestClose} style={customStyles}> 
-			{children 
-				? 	<div className="dollymodal">{children}</div>
-				: 	<div className="dollymodal">
-						{content ? content : <p>Du må sende inn content som props</p>}
-						<Lukknapp onClick={closeModal} />
-					</div>
-			}
+				<div className="dollymodal">
+					{content ? content : <p>Du må sende inn content som props</p>}
+					<Lukknapp onClick={closeModal} />
+				</div>
 			</Modal>
 		)
 	}
