9a5e8484afbab897773a0656a42f94425e103385
Kristoffer Mjelva <kristoffer.mjelva@nav.no>
2019-04-12 10:26:15 +0200

    REG-3308: Fikset local-kjøring

--- aaregstub-local/src/main/java/no/nav/registre/aaregstub/LocalApplicationStarter.java
+++ aaregstub-local/src/main/java/no/nav/registre/aaregstub/LocalApplicationStarter.java
@@ -9,6 +9,7 @@ public class LocalApplicationStarter {
     public static void main(String[] args) {
         new SpringApplicationBuilder()
                 .sources(LocalApplicationStarter.class)
+                .properties("spring.cloud.vault.token=123456")
                 .profiles("local")
                 .run(args);
     }
