976903f9964af317f2d29f9a85b09a399beee912
s152486 <ingvild.stoen@nav.no>
2019-04-12 07:49:36 +0200

    REG-3843 KRR-404 Forbedret variabelnavn

--- dolly-webapp/src/main/dolly-web/src/pages/gruppe/TestbrukerListe/TestbrukerListe.js
+++ dolly-webapp/src/main/dolly-web/src/pages/gruppe/TestbrukerListe/TestbrukerListe.js
@@ -82,7 +82,7 @@ export default class TestbrukerListe extends Component {
 																				personId={bruker[0]}
 																				username={username}
 																				bestillingId={bruker[5]}
-																				editAction={() => editTestbruker(bruker[0], this.dataSourceListe(testidenter, bruker[0]))}
+																				editAction={() => editTestbruker(bruker[0], this.dataSourceUrlListe(testidenter, bruker[0]))}
 																			/>
 																		}
 																	>
@@ -110,11 +110,12 @@ export default class TestbrukerListe extends Component {
 		)
 	}
 
-	dataSourceListe = (testidenter, ident) => {
+	dataSourceUrlListe = (testidenter, ident) => {
 		let temp = []
 		testidenter.map ( testident => {
 			if (testident.ident === ident) {
 				Object.keys(testident).map ( source => { 
+				// Lagrer de 4 første tegnene av strings med "Status" i. Stub er ikke en del av de 4 tegnene (Det blir f.eks. krr, ikke krrs)
 					source.includes('Status') && temp.push(source.split('stub')[0].substr(0,4))
 				})
 			}
