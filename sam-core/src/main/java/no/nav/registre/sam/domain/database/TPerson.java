e6d3bf99a213796ac8864cdc7457a77800b5db50
Kristoffer Mjelva <kristoffer.mjelva@nav.no>
2019-04-26 14:46:59 +0200

    REG-3435: Endret TPerson til å ta kopi av timestamps i setters

--- sam-core/src/main/java/no/nav/registre/sam/domain/database/TPerson.java
+++ sam-core/src/main/java/no/nav/registre/sam/domain/database/TPerson.java
@@ -1,10 +1,7 @@
 package no.nav.registre.sam.domain.database;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -12,16 +9,15 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 import java.sql.Timestamp;
 
 import no.nav.registre.sam.utils.Utils;
 
 @Getter
 @Setter
-@ToString
-@Entity(name = "t_person")
-@AllArgsConstructor
-@NoArgsConstructor
+@Entity
+@Table(name = "t_person")
 public class TPerson {
 
     @Id
@@ -29,16 +25,22 @@ public class TPerson {
     @SequenceGenerator(name = "p_seq", sequenceName = "S_PERSON", allocationSize = 1)
     @Column(name = "person_id")
     private int personId;
+
     @Column(name = "fnr_fk")
     private String fnrFK;
+
     @Column(name = "dato_opprettet")
     private Timestamp datoOpprettet;
+
     @Column(name = "opprettet_av")
     private String opprettetAv;
-    @Column(name = "datoEndret")
+
+    @Column(name = "dato_endret")
     private Timestamp datoEndret;
+
     @Column(name = "endret_av")
     private String endretAv;
+
     @Column(name = "versjon")
     private int versjon;
 
@@ -49,5 +51,13 @@ public class TPerson {
         this.datoEndret = Utils.getTodaysDate();
         this.endretAv = "synt";
         this.versjon = 1;
+    }
+
+    public void setDatoOpprettet(Timestamp datoOpprettet) {
+        this.datoOpprettet = new Timestamp(datoOpprettet.getTime());
+    }
+
+    public void setDatoEndret(Timestamp datoEndret) {
+        this.datoEndret = new Timestamp(datoEndret.getTime());
     }
 }
