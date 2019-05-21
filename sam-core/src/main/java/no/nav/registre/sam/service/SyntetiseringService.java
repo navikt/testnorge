ed27d915852aed9edbcb5ca6adc18f34f56654c8
Sandberg <Eirik.Sandberg@nav.no>
2019-05-21 09:31:44 +0200

    REG-4095 Removed unused import

--- sam-core/src/main/java/no/nav/registre/sam/service/SyntetiseringService.java
+++ sam-core/src/main/java/no/nav/registre/sam/service/SyntetiseringService.java
@@ -23,7 +23,6 @@ import org.springframework.stereotype.Service;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
