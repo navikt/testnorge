f3194915ec3f046a17d8b7a73eeaf5c523783393
Alex Nguyen <alex.nguyen@visma.com>
2019-05-28 11:46:19 +0200

    REG-4057: fix a bug ang. aareg

--- dolly-webapp/src/main/dolly-web/src/ducks/bestilling/BestillingRequestUtils.js
+++ dolly-webapp/src/main/dolly-web/src/ducks/bestilling/BestillingRequestUtils.js
@@ -124,8 +124,10 @@ const _transformAttributt = (attribute, attributes, value) => {
 						element[subKategoriId][0] !== ''
 					) {
 						let subsubArray = []
+
+						console.log('element :', element)
 						element[subKategoriId].map(rad => {
-							subsubArray.push(parseSubItemDate(item, rad, Object.assign(rad)))
+							rad && subsubArray.push(parseSubItemDate(item, rad, Object.assign(rad)))
 						})
 
 						transformed = {
