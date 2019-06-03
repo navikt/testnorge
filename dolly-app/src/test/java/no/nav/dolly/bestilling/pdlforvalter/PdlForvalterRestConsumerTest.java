942c85b2713de0430474aaec19c654e0078f4f17
Kristen Hærum <kristen.haerum@nav.no>
2019-06-03 12:38:32 +0200

    REG-4082 Oppdatert tester

--- dolly-app/src/test/java/no/nav/dolly/bestilling/pdlforvalter/PdlForvalterRestConsumerTest.java
+++ dolly-app/src/test/java/no/nav/dolly/bestilling/pdlforvalter/PdlForvalterRestConsumerTest.java
@@ -3,6 +3,7 @@ package no.nav.dolly.bestilling.pdlforvalter;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -13,6 +14,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.RequestEntity;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.web.client.RestTemplate;
 import com.fasterxml.jackson.databind.JsonNode;
 
@@ -42,6 +44,7 @@ public class PdlForvalterRestConsumerTest {
     @Before
     public void setup() {
 
+        ReflectionTestUtils.setField(pdlForvalterRestConsumer, "environment", "u2");
         when(providersProps.getPdlForvalter()).thenReturn(ProvidersProps.PdlForvalter.builder().url(PDL_URL).build());
     }
 
@@ -51,7 +54,7 @@ public class PdlForvalterRestConsumerTest {
         pdlForvalterRestConsumer.postKontaktinformasjonForDoedsbo(PdlKontaktinformasjonForDoedsbo.builder().build(), IDENT);
 
         verify(providersProps).getPdlForvalter();
-        verify(stsOidcService).getIdToken(anyString());
+        verify(stsOidcService, times(2)).getIdToken(anyString());
         verify(restTemplate).exchange(any(RequestEntity.class), eq(JsonNode.class));
     }
 
@@ -61,7 +64,7 @@ public class PdlForvalterRestConsumerTest {
         pdlForvalterRestConsumer.postUtenlandskIdentifikasjonsnummer(PdlUtenlandskIdentifikasjonsnummer.builder().build(), IDENT);
 
         verify(providersProps).getPdlForvalter();
-        verify(stsOidcService).getIdToken(anyString());
+        verify(stsOidcService, times(2)).getIdToken(anyString());
         verify(restTemplate).exchange(any(RequestEntity.class), eq(JsonNode.class));
     }
 
@@ -71,7 +74,7 @@ public class PdlForvalterRestConsumerTest {
         pdlForvalterRestConsumer.deleteIdent(IDENT);
 
         verify(providersProps).getPdlForvalter();
-        verify(stsOidcService).getIdToken(anyString());
+        verify(stsOidcService, times(2)).getIdToken(anyString());
         verify(restTemplate).exchange(any(RequestEntity.class), eq(JsonNode.class));
     }
 }
