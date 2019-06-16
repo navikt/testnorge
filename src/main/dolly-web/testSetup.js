5233aa1d917e8643753087afcd98091bd360d1f0
Paul Magne Lunde <paul.magne.lunde@nav.no>
2019-06-16 11:03:23 +0200

    Fjernet frontend-module

--- src/main/dolly-web/testSetup.js
+++
@@ -1,7 +0,0 @@
-const Enzyme = require('enzyme')
-const EnzymeAdapter = require('enzyme-adapter-react-16')
-
-require('jest-mock-console/dist/setupTestFramework.js')
-
-// Setup enzyme's react adapter
-Enzyme.configure({ adapter: new EnzymeAdapter() })
