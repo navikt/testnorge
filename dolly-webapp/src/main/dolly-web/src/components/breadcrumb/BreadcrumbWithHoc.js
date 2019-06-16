dd8594c281362e5993bb9ca743cfc2a3d830b5d1
Paul Magne Lunde <paul.magne.lunde@nav.no>
2019-06-16 11:03:23 +0200

    Fjernet frontend-module

--- dolly-webapp/src/main/dolly-web/src/components/breadcrumb/BreadcrumbWithHoc.js
+++
@@ -1,5 +0,0 @@
-import withBreadcrumbs from 'react-router-breadcrumbs-hoc'
-import routes from '~/Routes'
-import BreadCrumb from './Breadcrumb'
-
-export default withBreadcrumbs(routes, { disableDefaults: true })(BreadCrumb)
--- dolly-webapp/src/main/dolly-web/src/components/miljoVelger/MiljoVelgerConnector.js
+++
@@ -1,8 +0,0 @@
-import { connect } from 'react-redux'
-import MiljoVelger from './MiljoVelger'
-
-const mapStateToProps = state => ({
-	environments: state.environments.data
-})
-
-export default connect(mapStateToProps)(MiljoVelger)
