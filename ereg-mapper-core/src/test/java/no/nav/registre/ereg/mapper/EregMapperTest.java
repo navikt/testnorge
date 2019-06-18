6cc696c960f8f94aa79478e6962d0078ce2edbe5
t155132 <tim.teige91@gmail.com>
2019-06-18 10:49:46 +0200

    REG-3738: Lagt til equality sjekker mot produsert fil

--- ereg-mapper-core/src/test/java/no/nav/registre/ereg/mapper/EregMapperTest.java
+++ ereg-mapper-core/src/test/java/no/nav/registre/ereg/mapper/EregMapperTest.java
@@ -13,6 +13,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.junit.Assert.assertEquals;
 import no.nav.registre.ereg.provider.rs.request.Adresse;
 import no.nav.registre.ereg.provider.rs.request.EregDataRequest;
 import no.nav.registre.ereg.provider.rs.request.Kapital;
@@ -80,8 +81,8 @@ public class EregMapperTest {
                         .mobil("33333333")
                         .build())
                 .frivillighetsKode("DA")
-                .nedleggelsesDato(null)
-                .eierskapskifteDato(null)
+                .nedleggelsesDato("18062019")
+                .eierskapskifteDato("18062019")
                 .oppstartsDato("18062019")
                 .maalform(Maalform.B)
                 .utelukkendeVirksomhetINorge(true)
@@ -127,5 +128,52 @@ public class EregMapperTest {
 
         String s = eregMapper.mapEregFromRequests(Collections.singletonList(data));
         log.info(s);
+        assertEquals("HEADER " + EregMapper.getDateNowFormatted() + "00000AA A", s.substring(0, 24));
+        assertEquals("ENH 123      BEDNNY   " + EregMapper.getDateNowFormatted() + EregMapper.getDateNowFormatted() + "J           ", s.substring(25, 75));
+        assertEquals("NAVNN   Flott                              bedrift                                                                                                                                     Tull                                ",
+                s.substring(76, 295));
+        assertEquals("PADRN   0175     NOR1         OSLO                              S2                                                                                                                       ",
+                s.substring(296, 481));
+        assertEquals("FADRN   0175     NOR1         OSLO                              S2                                                                                                                       ",
+                s.substring(482, 667));
+        assertEquals("EPOSN   noreply@nav.no                                                                                                                                        ",
+                s.substring(668, 826));
+        assertEquals("IADRN   https://www.ikkenav.no                                                                                                                                ",
+                s.substring(827, 985));
+        assertEquals("MÅL N   B", s.substring(986, 995));
+        assertEquals("ARBGN   J", s.substring(996, 1005));
+        assertEquals("ISEKN   AB      ", s.substring(1006, 1022));
+        assertEquals("STIDN   18062019", s.substring(1023, 1039));
+        assertEquals("TFONN   11111111", s.substring(1040, 1056));
+        assertEquals("TFAXN   22222222", s.substring(1057, 1073));
+        assertEquals("MTLFN   33333333", s.substring(1074, 1090));
+        assertEquals("KATGN   DA   1", s.substring(1091, 1105));
+        assertEquals("NDATN   18062019", s.substring(1106, 1122));
+        assertEquals("BDATN   18062019", s.substring(1123, 1139));
+        assertEquals("EDATN   18062019", s.substring(1140, 1156));
+        assertEquals("KJRPN   J", s.substring(1157, 1166));
+        assertEquals("UVNON   J", s.substring(1167, 1176));
+        assertEquals("UENON   J", s.substring(1177, 1186));
+        assertEquals("RVFGN   J", s.substring(1187, 1196));
+        assertEquals("UREGN   098                                Utenlands navn!                                                                                          NOR OSLO                              S2                                                                                                       ",
+                s.substring(1197, 1488));
+        assertEquals("IPF N   ", s.substring(1489, 1497));
+        assertEquals("ULOVN   J  AS      Noe som ikke er mer enn 70 tegn                                       Enda mer under 70                                                     ",
+                s.substring(1498, 1657));
+        assertEquals("KAPIN   2? 000000000000000101000000000000002222503                                                                   Noe som kan gå over flere records........................... Minst 70 ",
+                s.substring(1658, 1845));
+        assertEquals("KAPIN   2? 000000000000000101000000000000002222503                                                                   tegn for å få delt opp records i flere                                ",
+                s.substring(1846, 2033));
+        assertEquals("NACEN   0?    18062019N", s.substring(2034, 2057));
+        assertEquals("FORMN   Jobb                                                                  ",
+                s.substring(2058, 2136));
+        assertEquals("FMVAN   NOE!  ", s.substring(2137, 2151));
+
+
+
+
+
+
+
     }
 }
