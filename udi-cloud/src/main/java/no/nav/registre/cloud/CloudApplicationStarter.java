07953b8aeb76eb7b60a7fd73413e83a9e46b9046
t155132 <tim.teige91@gmail.com>
2019-07-01 13:07:11 +0200

    REG-3878: Circle i full sving og vault token?

--- udi-cloud/src/main/java/no/nav/registre/cloud/CloudApplicationStarter.java
+++ udi-cloud/src/main/java/no/nav/registre/cloud/CloudApplicationStarter.java
@@ -3,10 +3,13 @@ package no.nav.registre.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import no.nav.registre.cloud.config.VaultUtil;
+
 @SpringBootApplication(scanBasePackages = {"no.nav.registre.core", "no.nav.registre.cloud"})
 public class CloudApplicationStarter {
 
     public static void main(String[] args) {
+        VaultUtil.initCloudVaultToken();
         SpringApplication.run(CloudApplicationStarter.class, args);
     }
 
---
+++ udi-cloud/src/main/java/no/nav/registre/cloud/config/VaultUtil.java
@@ -0,0 +1,40 @@
+package no.nav.registre.cloud.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+@Slf4j
+public final class VaultUtil {
+
+    private static final String VAULT_TOKEN_PROPERTY = "VAULT_TOKEN";
+
+    private static String getVaultToken() {
+
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext();
+        ConfigurableEnvironment environment = context.getEnvironment();
+
+        try {
+            if (environment.containsProperty(VAULT_TOKEN_PROPERTY) && !"".equals(environment.getProperty(VAULT_TOKEN_PROPERTY))) {
+                return environment.getProperty(VAULT_TOKEN_PROPERTY);
+            } else if (Files.exists(Paths.get("/var/run/secrets/nais.io/vault/vault_token"))) {
+                byte[] encoded = Files.readAllBytes(Paths.get("/var/run/secrets/nais.io/vault/vault_token"));
+                return new String(encoded, StandardCharsets.UTF_8).trim();
+            } else {
+                throw new RuntimeException("Neither VAULT_TOKEN or VAULT_TOKEN_PATH is set");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Could not get a vault token for authentication", e);
+        }
+    }
+
+    public static void initCloudVaultToken() {
+        System.setProperty("spring.cloud.vault.token", getVaultToken());
+        log.info(getVaultToken());
+    }
+}
