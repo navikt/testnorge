989d796e6c5c4c09f403131ba5fc223d67ae59f8
t155132 <tim.teige91@gmail.com>
2019-07-11 08:28:28 +0200

    REG-3738: Added repo

---
+++ statisk-data-forvalter-core/src/main/java/no/nav/registre/sdForvalter/database/repository/AdresseRepository.java
@@ -0,0 +1,8 @@
+package no.nav.registre.sdForvalter.database.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import no.nav.registre.sdForvalter.database.model.AdresseModel;
+
+public interface AdresseRepository extends CrudRepository<AdresseModel, Long> {
+}
