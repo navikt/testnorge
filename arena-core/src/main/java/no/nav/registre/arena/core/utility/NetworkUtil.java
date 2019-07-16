6afab338121d07ea36bf75b711ffe95f5473afb0
K156575 <baard.krohg@gmail.com>
2019-07-16 10:35:38 +0200

    REG-3121: all logging fjernet fra NetworkUtil

--- arena-core/src/main/java/no/nav/registre/arena/core/utility/NetworkUtil.java
+++ arena-core/src/main/java/no/nav/registre/arena/core/utility/NetworkUtil.java
@@ -2,9 +2,7 @@ package no.nav.registre.arena.core.utility;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 public class NetworkUtil {
     public static Boolean validResponse(ResponseEntity response) {
 
