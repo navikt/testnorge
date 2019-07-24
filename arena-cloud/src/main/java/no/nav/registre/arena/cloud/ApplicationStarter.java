51619f27ac01bdc437f00fcfbd3db30100fa5a62
K156575 <baard.krohg@gmail.com>
2019-07-24 13:49:45 +0200

    REG-3121: la til dependency på arena-core i arena-cloud

--- arena-cloud/src/main/java/no/nav/registre/arena/cloud/ApplicationStarter.java
+++ arena-cloud/src/main/java/no/nav/registre/arena/cloud/ApplicationStarter.java
@@ -3,7 +3,7 @@ package no.nav.registre.arena.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "no.nav.registre.arena")
+@SpringBootApplication(scanBasePackages = "no.nav.registre")
 public class ApplicationStarter {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationStarter.class, args);
