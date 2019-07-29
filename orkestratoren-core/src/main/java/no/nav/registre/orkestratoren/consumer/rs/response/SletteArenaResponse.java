7dd75e005698dd12b9896bef329fe49c1af36b84
Bård-Kristian Krohg <Bard-Kristian.Krohg@nav.no>
2019-07-29 12:12:37 +0200

    REG-3121: la til arenaAdapter i IdentServiceTest, og noen endringer i local-application.properties for å kjøre lokalt.

--- orkestratoren-core/src/main/java/no/nav/registre/orkestratoren/consumer/rs/response/SletteArenaResponse.java
+++ orkestratoren-core/src/main/java/no/nav/registre/orkestratoren/consumer/rs/response/SletteArenaResponse.java
@@ -2,6 +2,7 @@ package no.nav.registre.orkestratoren.consumer.rs.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,6 +13,7 @@ import java.util.List;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class SletteArenaResponse {
     @JsonProperty("slettet")
     private List<String> slettet;
