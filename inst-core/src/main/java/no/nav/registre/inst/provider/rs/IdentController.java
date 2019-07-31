1c0953352ccae92af6c0f09950a829b292821e90
Kristoffer Mjelva <kristoffer.mjelva@nav.no>
2019-07-31 13:16:28 +0200

    REG-2558: Oppdatert swagger

--- inst-core/src/main/java/no/nav/registre/inst/provider/rs/IdentController.java
+++ inst-core/src/main/java/no/nav/registre/inst/provider/rs/IdentController.java
@@ -1,5 +1,6 @@
 package no.nav.registre.inst.provider.rs;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -30,22 +31,26 @@ public class IdentController {
     private IdentService identService;
 
     @PostMapping
+    @ApiOperation(value = "Her kan man opprette ett institusjonsopphold i inst2.")
     public OppholdResponse opprettInstitusjonsopphold(@RequestHeader String navCallId, @RequestHeader String navConsumerId,
             @RequestBody Institusjonsopphold institusjonsopphold) {
         return identService.sendTilInst2(institusjonsopphold, navCallId, navConsumerId);
     }
 
     @GetMapping
+    @ApiOperation(value = "Her kan man hente alle institusjonsoppholdene tilhørende angitte identer.")
     public Map<String, List<Institusjonsopphold>> hentInstitusjonsopphold(@RequestHeader String navCallId, @RequestHeader String navConsumerId, @RequestParam List<String> fnrs) {
         return identService.hentForhold(fnrs, navCallId, navConsumerId);
     }
 
     @PutMapping("/{oppholdId}")
+    @ApiOperation(value = "Her kan man oppdatere et institusjonsopphold med angitt oppholdId.")
     public ResponseEntity oppdaterInstitusjonsopphold(@PathVariable Long oppholdId, @RequestHeader String navCallId, @RequestHeader String navConsumerId, @RequestBody Institusjonsopphold institusjonsopphold) {
         return identService.oppdaterInstitusjonsopphold(navCallId, navConsumerId, oppholdId, institusjonsopphold);
     }
 
     @DeleteMapping
+    @ApiOperation(value = "Her kan man slette alle institusjonsoppholdene med de angitte oppholdId-ene.")
     public Map<Long, ResponseEntity> slettInstitusjonsopphold(@RequestHeader String navCallId, @RequestHeader String navConsumerId, @RequestParam List<Long> oppholdIder) {
         Map<String, Object> tokenObject = identService.hentTokenTilInst2();
         Map<Long, ResponseEntity> status = new HashMap<>();
@@ -56,12 +61,14 @@ public class IdentController {
     }
 
     @PostMapping("/batch")
+    @ApiOperation(value = "Her kan man opprette flere institusjonsopphold i inst2.")
     public Map<String, List<OppholdResponse>> opprettFlereInstitusjonsopphold(@RequestHeader String navCallId, @RequestHeader String navConsumerId,
             @RequestBody List<Institusjonsopphold> institusjonsopphold) {
         return identService.opprettInstitusjonsopphold(institusjonsopphold, navCallId, navConsumerId);
     }
 
     @DeleteMapping("/batch")
+    @ApiOperation(value = "Her kan man slette alle institusjonsoppholdene til de angitte identene.")
     public SletteOppholdResponse slettIdenter(@RequestHeader String navCallId, @RequestHeader String navConsumerId, @RequestParam List<String> identer) {
         return identService.slettInstitusjonsforholdTilIdenter(identer, navCallId, navConsumerId);
     }
