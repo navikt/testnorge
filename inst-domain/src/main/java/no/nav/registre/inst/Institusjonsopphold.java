d9ab824ee52824245992d0fe479ab1fad96afb9e
Kristoffer Mjelva <kristoffer.mjelva@nav.no>
2019-08-02 10:12:23 +0200

    REG-2558: Egen serializer og deserializer

--- inst-domain/src/main/java/no/nav/registre/inst/Institusjonsopphold.java
+++ inst-domain/src/main/java/no/nav/registre/inst/Institusjonsopphold.java
@@ -1,12 +1,9 @@
 package no.nav.registre.inst;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -41,19 +38,16 @@ public class Institusjonsopphold {
     private String kategori;
 
     @JsonAlias({ "startdato", "dato_fom" })
-    @JsonFormat(shape = JsonFormat.Shape.STRING)
     @JsonDeserialize(using = LocalDateDeserializer.class)
     @JsonSerialize(using = LocalDateSerializer.class)
     private LocalDate startdato;
 
     @JsonAlias({ "faktiskSluttdato", "dato_tom" })
-    @JsonFormat(shape = JsonFormat.Shape.STRING)
     @JsonDeserialize(using = LocalDateDeserializer.class)
     @JsonSerialize(using = LocalDateSerializer.class)
     private LocalDate faktiskSluttdato;
 
     @JsonAlias({ "forventetSluttdato", "dato_tom_forventet" })
-    @JsonFormat(shape = JsonFormat.Shape.STRING)
     @JsonDeserialize(using = LocalDateDeserializer.class)
     @JsonSerialize(using = LocalDateSerializer.class)
     private LocalDate forventetSluttdato;
