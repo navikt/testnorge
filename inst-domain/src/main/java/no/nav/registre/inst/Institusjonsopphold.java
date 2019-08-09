3c44b8995680aa3d956ba8de4a943c9380470731
Kristoffer Mjelva <kristoffer.mjelva@nav.no>
2019-08-09 09:31:10 +0200

    REG-2558: La til nye felt i institusjonsopphold

--- inst-domain/src/main/java/no/nav/registre/inst/Institusjonsopphold.java
+++ inst-domain/src/main/java/no/nav/registre/inst/Institusjonsopphold.java
@@ -2,11 +2,15 @@ package no.nav.registre.inst;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,12 +18,14 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Institusjonsopphold {
 
     @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -63,4 +69,12 @@ public class Institusjonsopphold {
 
     @JsonAlias({ "overfoert", "overfort" })
     private boolean overfoert;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    private LocalDateTime registreringstidspunkt;
+
+    @JsonIgnore
+    private String registrertAv;
 }
