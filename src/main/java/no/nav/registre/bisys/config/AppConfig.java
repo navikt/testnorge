c22554cfda3a9359fe0b79e3e65b42a4079c4f96
Kristoffer Mjelva <kristoffer.mjelva@nav.no>
2019-08-12 10:58:45 +0200

    REG-4939: Endret til å bruke testnorge-consumers

--- src/main/java/no/nav/registre/bisys/config/AppConfig.java
+++ src/main/java/no/nav/registre/bisys/config/AppConfig.java
@@ -3,65 +3,68 @@ package no.nav.registre.bisys.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.web.client.RestTemplate;
+
 import no.nav.registre.bisys.consumer.rs.BisysSyntetisererenConsumer;
-import no.nav.registre.bisys.consumer.rs.HodejegerenConsumer;
 import no.nav.registre.bisys.consumer.ui.BisysUiConsumer;
 import no.nav.registre.bisys.consumer.ui.modules.BisysUiRollerConsumer;
 import no.nav.registre.bisys.consumer.ui.modules.BisysUiSakConsumer;
+import no.nav.registre.testnorge.consumers.hodejegeren.HodejegerenConsumer;
 
 @Configuration
 public class AppConfig {
 
-  @Value("${syntrest.rest.api.url}")
-  String syntrestServerUrl;
+    @Value("${syntrest.rest.api.url}")
+    String syntrestServerUrl;
 
-  @Value("${SYNTBISYS_USERNAME}")
-  String saksbehandlerUid;
+    @Value("${SYNTBISYS_USERNAME}")
+    String saksbehandlerUid;
 
-  @Value("${SYNTBISYS_PASSWORD}")
-  String saksbehandlerPwd;
+    @Value("${SYNTBISYS_PASSWORD}")
+    String saksbehandlerPwd;
 
-  @Value("${BISYS_URL}")
-  String bisysUrl;
+    @Value("${BISYS_URL}")
+    String bisysUrl;
 
-  @Value("${SAKSBEHANDLER_ROLLE}")
-  String rolleSaksbehandler;
+    @Value("${SAKSBEHANDLER_ROLLE}")
+    String rolleSaksbehandler;
 
-  @Value("${ENHET}")
-  String enhet;
+    @Value("${ENHET}")
+    String enhet;
 
-  @Value("${testnorge-hodejegeren.rest-api.url}")
-  String hodejegerenServerUrl;
+    @Value("${testnorge-hodejegeren.rest-api.url}")
+    private String hodejegerenUrl;
 
-  @Bean
-  public RestTemplate restTemplate() {
-    return new RestTemplate();
-  }
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
-  @Bean
-  public BisysSyntetisererenConsumer bisysSyntetisererenConsumer() {
-    return new BisysSyntetisererenConsumer(syntrestServerUrl);
-  }
+    @Bean
+    public BisysSyntetisererenConsumer bisysSyntetisererenConsumer() {
+        return new BisysSyntetisererenConsumer(syntrestServerUrl);
+    }
 
-  @Bean
-  public BisysUiConsumer bisysUiConsumer() {
-    return new BisysUiConsumer(
-        saksbehandlerUid, saksbehandlerPwd, bisysUrl, rolleSaksbehandler, enhet);
-  }
+    @Bean
+    public BisysUiConsumer bisysUiConsumer() {
+        return new BisysUiConsumer(
+                saksbehandlerUid, saksbehandlerPwd, bisysUrl, rolleSaksbehandler, enhet);
+    }
 
-  @Bean
-  public BisysUiSakConsumer bisysUiSakConsumer() {
-    return new BisysUiSakConsumer();
-  }
+    @Bean
+    public BisysUiSakConsumer bisysUiSakConsumer() {
+        return new BisysUiSakConsumer();
+    }
 
-  @Bean
-  public BisysUiRollerConsumer bisysUiRollerConsumer() {
-    return new BisysUiRollerConsumer();
-  }
+    @Bean
+    public BisysUiRollerConsumer bisysUiRollerConsumer() {
+        return new BisysUiRollerConsumer();
+    }
 
-  @Bean
-  public HodejegerenConsumer hodejegerenConsumer() {
-    return new HodejegerenConsumer(hodejegerenServerUrl);
-  }
+    @Bean
+    @DependsOn("restTemplate")
+    public HodejegerenConsumer hodejegerenConsumer() {
+        return new HodejegerenConsumer(hodejegerenUrl, restTemplate());
+    }
 }
