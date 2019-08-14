d38c61ff9594b58454273ccf1d6fa62a66c729a9
Kristoffer Mjelva <kristoffer.mjelva@nav.no>
2019-08-14 08:49:50 +0200

    CORS

--- inst-core/src/main/java/no/nav/registre/inst/provider/rs/IdentController.java
+++ inst-core/src/main/java/no/nav/registre/inst/provider/rs/IdentController.java
@@ -5,6 +5,7 @@ import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,6 +29,7 @@ import no.nav.registre.inst.service.Inst2FasitService;
 
 @Slf4j
 @RestController
+@CrossOrigin
 @RequestMapping("api/v1")
 public class IdentController {
 
