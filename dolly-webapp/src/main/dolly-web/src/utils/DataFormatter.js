0fe582fbda65da7163d28fbf75df1a7ccefd72c9
s152486 <ingvild.stoen@nav.no>
2019-08-19 14:34:29 +0200

    REG-5022 REG-5025 Formatert type inntekt i oppsummering

--- dolly-webapp/src/main/dolly-web/src/utils/DataFormatter.js
+++ dolly-webapp/src/main/dolly-web/src/utils/DataFormatter.js
@@ -44,6 +44,7 @@ Formatters.decamelize = (str, separator) => {
 	const res = str
 		.replace(/([a-z\d])([A-Z])/g, '$1' + separator + '$2')
 		.replace(/([A-Z]+)([A-Z][a-z\d]+)/g, '$1' + separator + '$2')
+		.toLowerCase()
 
 	return res.charAt(0).toUpperCase() + res.slice(1)
 }
