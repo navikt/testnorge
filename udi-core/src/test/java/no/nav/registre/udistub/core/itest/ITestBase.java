5de0277c076fdb5636708728fe5eee78bfef1ead
Stian Låte <stian.late@nav.no>
2019-09-02 15:52:47 +0200

    Replaced standalone wiremock with normal

--- udi-core/src/test/java/no/nav/registre/udistub/core/itest/ITestBase.java
+++ udi-core/src/test/java/no/nav/registre/udistub/core/itest/ITestBase.java
@@ -1,15 +1,14 @@
 package no.nav.registre.udistub.core.itest;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.client.WireMock.matching;
-import static com.github.tomakehurst.wiremock.client.WireMock.okJson;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
 import static no.nav.registre.udistub.core.DefaultTestData.createPersonTo;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
-import no.nav.registre.udistub.core.database.model.Person;
 import no.nav.registre.udistub.core.service.to.PersonTo;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -33,7 +32,7 @@ public class ITestBase {
         String tpsfResponseBody = getJsonContentsAsString("tpsfResponse-happy.json");
         stubFor(get(urlMatching("/tpsfstuburl/.*"))
                 .withHeader(AUTHORIZATION, matching("test"))
-                .willReturn(okJson(tpsfResponseBody)));
+                .willReturn(aResponse().withBody(tpsfResponseBody).withStatus(200)));
     }
 
     protected String getJsonContentsAsString(String filename) throws IOException {
