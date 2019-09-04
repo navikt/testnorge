8324ff184909aece0b99954a91ba1b20dad247ab
s152486 <ingvild.stoen@nav.no>
2019-09-04 11:02:22 +0200

    REG-5250 Oppdatert miljoeStatusSelector test

--- dolly-webapp/src/main/dolly-web/src/utils/__tests__/MiljoeStatusSelector.test.js
+++ dolly-webapp/src/main/dolly-web/src/utils/__tests__/MiljoeStatusSelector.test.js
@@ -1,4 +1,5 @@
 import mss, { avvikStatus, countAntallIdenterOpprettet } from '../MiljoeStatusSelector'
+import miljoeStatusSelector from '../MiljoeStatusSelector'
 
 describe('MiljoeStatusSelector.js', () => {
 	describe('avvikStatus()', () => {
@@ -127,6 +128,276 @@ describe('MiljoeStatusSelector.js', () => {
 
 		it('skal ikke telle duplikater dersom person er opprettet i flere miljøer', () => {
 			expect(countAntallIdenterOpprettet(bestillingMockMedDuplikat)).toBe(2)
+		})
+	})
+
+	describe('miljoeStatusSelector()', () => {
+		const bestillingMockMedTpsfSuccessEnvs = {
+			tpsfStatus: [
+				{
+					statusMelding: 'OK',
+					environmentIdents: {
+						t5: ['19106129722', '14028733245'],
+						t11: ['19106129722']
+					}
+				}
+			]
+		}
+
+		const bestillingMockMedSuccessRegister = {
+			tpsfStatus: [
+				{
+					statusMelding: 'OK',
+					environmentIdents: {
+						t5: ['19106129722', '14028733245']
+					}
+				}
+			],
+			krrStubStatus: [
+				{
+					statusMelding: 'OK',
+					identer: ['19106129722']
+				}
+			],
+			sigrunStubStatus: [
+				{
+					statusMelding: 'OK',
+					identer: ['19106129722']
+				}
+			],
+			arenaforvalterStatus: [
+				{
+					status: 'OK',
+					envIdent: { t5: ['19106129722'] }
+				}
+			],
+			aaregStatus: [
+				{
+					statusMelding: 'OK',
+					environmentIdentsForhold: {
+						t5: { '19106129722': ['arbforhold 1'] }
+					}
+				}
+			],
+			instdataStatus: [
+				{
+					statusMelding: 'OK',
+					environmentIdentsForhold: {
+						t5: { '19106129722': ['opphold 1'] }
+					}
+				}
+			],
+			pdlforvalterStatus: {
+				falskIdentitet: [
+					{
+						statusMelding: 'OK',
+						identer: ['19106129722']
+					}
+				]
+			}
+		}
+
+		const bestillingMockMedFailedRegister = {
+			tpsfStatus: [
+				{
+					statusMelding: 'Feilet',
+					environmentIdents: {
+						t5: ['19106129722', '14028733245']
+					}
+				}
+			],
+			krrStubStatus: [
+				{
+					statusMelding: 'Feilet',
+					identer: ['19106129722']
+				}
+			],
+			sigrunStubStatus: [
+				{
+					statusMelding: 'Feilet',
+					identer: ['19106129722']
+				}
+			],
+			arenaforvalterStatus: [
+				{
+					status: 'Feilet',
+					envIdent: { t5: ['19106129722'] }
+				}
+			],
+			aaregStatus: [
+				{
+					statusMelding: 'Feilet',
+					environmentIdentsForhold: {
+						t5: { '19106129722': ['arbforhold 1'] }
+					}
+				}
+			],
+			instdataStatus: [
+				{
+					statusMelding: 'Feilet',
+					environmentIdentsForhold: {
+						t5: { '19106129722': ['opphold 1'] }
+					}
+				}
+			],
+			pdlforvalterStatus: {
+				falskIdentitet: [
+					{
+						statusMelding: 'Feilet',
+						identer: ['19106129722']
+					}
+				]
+			}
+		}
+
+		it('skal returnere 0 hvis bestillingen er undefined', () => {
+			expect(miljoeStatusSelector(undefined)).toBe(null)
+		})
+
+		it('skal returnere successEnvs t5 og t11', () => {
+			const miljoeObj = {
+				successEnvs: ['t5', 't11']
+			}
+			expect(miljoeStatusSelector(bestillingMockMedTpsfSuccessEnvs)).toMatchObject(miljoeObj)
+		})
+
+		it('skal returnere mange successEnvs', () => {
+			const miljoeObjMangeSuccess = {
+				successEnvs: ['t5', 'Krr-stub', 'Sigrun-stub', 'Pdl-forvalter', 'Inst', 'AAREG', 'Arena']
+			}
+			expect(miljoeStatusSelector(bestillingMockMedSuccessRegister)).toMatchObject(
+				miljoeObjMangeSuccess
+			)
+		})
+
+		it('skal returnere mange failedEnvs', () => {
+			const miljoeObjMangeFail = {
+				failedEnvs: ['t5', 'Krr-stub', 'Sigrun-stub', 'Pdl-forvalter', 'Inst', 'AAREG', 'Arena']
+			}
+			expect(miljoeStatusSelector(bestillingMockMedFailedRegister)).toMatchObject(
+				miljoeObjMangeFail
+			)
+		})
+
+		it('skal returnere tpsf kombi', () => {
+			const bestillingMockMedKombiTpsf = {
+				tpsfStatus: [
+					{
+						statusMelding: 'OK',
+						environmentIdents: {
+							t5: ['19106129722', '14028733245'],
+							t12: ['14028733245']
+						}
+					},
+					{
+						statusMelding: 'Feilet',
+						environmentIdents: {
+							t11: ['19106129722', '14028733245'],
+							t12: ['19106129722']
+						}
+					}
+				]
+			}
+			const miljoeObjKombiTpsf = {
+				successEnvs: ['t5'],
+				failedEnvs: ['t11', 't12']
+			}
+			expect(miljoeStatusSelector(bestillingMockMedKombiTpsf)).toMatchObject(miljoeObjKombiTpsf)
+		})
+
+		it('skal returnere inst kombi', () => {
+			const bestillingMockMedKombiInst = {
+				instdataStatus: [
+					{
+						statusMelding: 'OK',
+						environmentIdentsForhold: {
+							q0: { '19106129722': ['opphold 1'] },
+							q2: { '19106129722': ['opphold 1'] }
+						}
+					},
+					{
+						statusMelding: 'Feilet',
+						environmentIdentsForhold: {
+							t5: { '19106129722': ['opphold 1'] }
+						}
+					}
+				]
+			}
+			const miljoeObjKombiInst = {
+				avvikEnvs: ['Inst']
+			}
+			expect(miljoeStatusSelector(bestillingMockMedKombiInst)).toMatchObject(miljoeObjKombiInst)
+		})
+
+		it('skal returnere pdlf kombi', () => {
+			const bestillingMockMedKombiPdlf = {
+				pdlforvalterStatus: {
+					falskIdentitet: [
+						{
+							statusMelding: 'Feilet',
+							identer: ['19106129722']
+						}
+					],
+					kontaktinfoDoedsbo: [
+						{
+							statusMelding: 'OK',
+							identer: ['19106129722']
+						}
+					],
+					utenlandsid: [
+						{
+							statusMelding: 'Feilet',
+							identer: ['19106129722']
+						}
+					]
+				}
+			}
+			const miljoeObjKombiPdlf = {
+				failedEnvs: ['Pdl-forvalter']
+				//Burde vært avvikEnvs: ['Pdl-forvalter']?
+			}
+			expect(miljoeStatusSelector(bestillingMockMedKombiPdlf)).toMatchObject(miljoeObjKombiPdlf)
+		})
+
+		it('skal returnere aareg kombi', () => {
+			const bestillingMockMedKombiAareg = {
+				aaregStatus: [
+					{
+						statusMelding: 'OK',
+						environmentIdentsForhold: {
+							t6: { '19106129722': ['arbforhold 1'] }
+						}
+					},
+					{
+						statusMelding: 'Feilet',
+						environmentIdentsForhold: {
+							t5: { '19106129722': ['arbforhold 1'] }
+						}
+					}
+				]
+			}
+			const miljoeObjKombiAareg = {
+				avvikEnvs: ['AAREG']
+				//Burde vært avvikEnvs: ['Pdl-forvalter']?
+			}
+			expect(miljoeStatusSelector(bestillingMockMedKombiAareg)).toMatchObject(miljoeObjKombiAareg)
+		})
+
+		it('skal returnere pdlf overall fail', () => {
+			const bestillingMockMedPdlfFeil = {
+				pdlforvalterStatus: {
+					pdlForvalter: [
+						{
+							statusMelding: 'Feilet',
+							identer: ['19106129722']
+						}
+					]
+				}
+			}
+			const miljoeObjPdlfFeil = {
+				failedEnvs: ['Pdl-forvalter']
+			}
+			expect(miljoeStatusSelector(bestillingMockMedPdlfFeil)).toMatchObject(miljoeObjPdlfFeil)
 		})
 	})
 })
