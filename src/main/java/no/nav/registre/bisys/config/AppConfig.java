2a7fba15692fba7d0a1613dc346c4d3a8706273f
Skår, Jan Kjetil <jan.kjetil.skar@nav.no>
2019-09-06 13:04:34 +0200

    BD-818: Bytte primærtyper til String for miljøvariabler som hentes fra Fasit.

--- src/main/java/no/nav/registre/bisys/config/AppConfig.java
+++ src/main/java/no/nav/registre/bisys/config/AppConfig.java
@@ -28,10 +28,10 @@ public class AppConfig {
     String bisysUrl;
 
     @Value("${ENHET}")
-    int enhet;
+    String enhet;
 
     @Value("${GEBYR_BESLAARSAK_KODE_FRITATT_IKKE_SOKT}")
-    boolean gebyrBeslAarsakKodeFritattIkkeSokt;
+    String gebyrBeslAarsakKodeFritattIkkeSokt;
 
     @Value("${testnorge-hodejegeren.rest-api.url}")
     String hodejegerenUrl;
@@ -76,13 +76,13 @@ public class AppConfig {
     @Bean
     public BisysUiSupport bisysUiNavigationSupport() {
         return new BisysUiSupport(saksbehandlerUid, saksbehandlerPwd, bisysUrl,
-                rolleSaksbehandler, enhet);
+                rolleSaksbehandler, Integer.parseInt(enhet));
     }
 
     @Bean
     public BisysRequestAugments bisysRequestAugments() {
 
-        String gebyrBeslAarsakKode = gebyrBeslAarsakKodeFritattIkkeSokt
+        String gebyrBeslAarsakKode = gebyrBeslAarsakKodeFritattIkkeSokt.equals("1")
                 ? BisysUiFatteVedtakConsumer.KODE_BESL_AARSAK_FRITATT_IKKE_SOKT
                 : BisysUiFatteVedtakConsumer.KODE_BESL_AARSAK_ILAGT_IKKE_SOKT;
 
