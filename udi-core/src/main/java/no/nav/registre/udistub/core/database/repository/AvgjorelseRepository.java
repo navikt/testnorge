3c315008f5909d42aa0a03b696a099344290ee0a
Stian Låte <stian.late@nav.no>
2019-09-06 09:51:02 +0200

    Added RestProvider cors config

--- udi-core/src/main/java/no/nav/registre/udistub/core/database/repository/AvgjorelseRepository.java
+++ udi-core/src/main/java/no/nav/registre/udistub/core/database/repository/AvgjorelseRepository.java
@@ -6,5 +6,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AvgjorelseRepository extends CrudRepository<Avgjorelse, Long> {
-    Avgjorelse findAvgjoerelseBySaksnummer(Long saksnummer);
 }
