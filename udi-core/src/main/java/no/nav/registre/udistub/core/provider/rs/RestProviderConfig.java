89ee49b1797feb3c5dd41ac765dc5346f3ccbeb3
Stian Låte <stian.late@nav.no>
2019-09-06 10:17:06 +0200

    Adjusted cors resource path

--- udi-core/src/main/java/no/nav/registre/udistub/core/provider/rs/RestProviderConfig.java
+++ udi-core/src/main/java/no/nav/registre/udistub/core/provider/rs/RestProviderConfig.java
@@ -2,13 +2,15 @@ package no.nav.registre.udistub.core.provider.rs;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Profile("!itest")
 @Configuration
 public class RestProviderConfig implements WebMvcConfigurer {
 
-    @Value("${udistub.web.api.cors.origins:''}")
+    @Value("#{'${udistub.web.api.cors.origins}'.split(', ')}")
     private String[] allowedOrigins;
 
     @Override
