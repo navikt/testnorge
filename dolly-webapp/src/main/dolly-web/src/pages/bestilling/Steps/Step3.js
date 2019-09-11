787714e92dadc0a671eac6b9d1b40fe2f2b13295
Rayfuse <Sarah.Rayfuse@nav.no>
2019-09-11 08:23:56 +0200

    REG-5153: noen små fixes

--- dolly-webapp/src/main/dolly-web/src/pages/bestilling/Steps/Step3.js
+++ dolly-webapp/src/main/dolly-web/src/pages/bestilling/Steps/Step3.js
@@ -266,8 +266,12 @@ export default class Step3 extends PureComponent {
 	renderItem = (item, stateValues, header) => {
 		if (item.items) {
 			let valueArray = _get(this.props.values, item.id)
-			// console.log('valueArray :', valueArray)
-			if (item.id === 'barn_utvandret' || item.id === 'barn_forsvunnet') {
+
+			if (
+				item.id === 'barn_utvandret' ||
+				item.id === 'bard_innvandret' ||
+				item.id === 'barn_forsvunnet'
+			) {
 				let barnIndex = 0
 				if (header) barnIndex = header - 1
 				valueArray = _get(this.props.values.barn[barnIndex], item.id)
