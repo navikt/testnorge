3b27245fa9d7c3d25ae25107ec591338cf044aff
Andreas Ludvigsen <andreas.ludvigsen@nav.no>
2019-09-12 18:45:05 +0200

    Restrukturert komponenter og delegert ansvar

--- dolly-webapp/src/main/dolly-web/src/pages/gruppe/BestillingListe/BestillingListeConnector.js
+++ dolly-webapp/src/main/dolly-web/src/pages/gruppe/BestillingListe/BestillingListeConnector.js
@@ -1,11 +1,16 @@
 import { connect } from 'react-redux'
-import BestillingListe from './BestillingListe'
 import { sokSelector } from '~/ducks/bestillingStatus/utils'
 import { getEnvironments } from '~/ducks/environments'
+import { getBestillinger } from '~/ducks/bestillingStatus'
+import { createLoadingSelector } from '~/ducks/loading'
+import BestillingListe from './BestillingListe'
+
+const loadingBestillingerSelector = createLoadingSelector(getBestillinger)
 
 const mapStateToProps = (state, ownProps) => ({
 	searchActive: Boolean(state.search),
-	bestillinger: sokSelector(ownProps.bestillingListe.data, state.search)
+	isFetchingBestillinger: loadingBestillingerSelector(state),
+	bestillinger: sokSelector(state.bestillingStatuser.data, state.search)
 })
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
