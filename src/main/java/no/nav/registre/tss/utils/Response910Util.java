6978358f9474fab516d2931c6989943cd705e41c
Kristoffer Mjelva <kristoffer.mjelva@nav.no>
2019-09-16 14:44:58 +0200

    bugfix i parsing

--- src/main/java/no/nav/registre/tss/utils/Response910Util.java
+++ src/main/java/no/nav/registre/tss/utils/Response910Util.java
@@ -5,28 +5,28 @@ public class Response910Util {
     private static int HEADER_LENGTH = 531;
 
     public static Response910 parseResponse(String response) {
-        int currentPos = 0;
+        int currentPos = HEADER_LENGTH;
 
         return Response910.builder()
-                .idKode(response.substring(currentPos += 3, currentPos))
-                .idOff(response.substring(currentPos += 11, currentPos))
-                .kodeIdenttype(response.substring(currentPos += 4, currentPos))
-                .kodeSamhType(response.substring(currentPos += 4, currentPos))
-                .BeskrSamhType(response.substring(currentPos += 30, currentPos))
-                .datoSamhType(response.substring(currentPos += 8, currentPos))
-                .datoSamhTom(response.substring(currentPos += 8, currentPos))
-                .navn(response.substring(currentPos += 40, currentPos))
-                .kodeSpraak(response.substring(currentPos += 4, currentPos))
-                .etatsmerke(response.substring(currentPos += 1, currentPos))
-                .utbetalingssperre(response.substring(currentPos += 1, currentPos))
-                .kodeKontrint(response.substring(currentPos += 4, currentPos))
-                .beskrKontrint(response.substring(currentPos += 30, currentPos))
-                .kodeStatus(response.substring(currentPos += 4, currentPos))
-                .BeskrStatus(response.substring(currentPos += 26, currentPos))
-                .oppdater(response.substring(currentPos += 1, currentPos))
-                .kilde(response.substring(currentPos += 4, currentPos))
-                .brukerId(response.substring(currentPos += 8, currentPos))
-                .tidReg(response.substring(currentPos += 12, currentPos))
+                .idKode(response.substring(currentPos, currentPos += 3))
+                .idOff(response.substring(currentPos, currentPos += 11))
+                .kodeIdenttype(response.substring(currentPos, currentPos += 4))
+                .kodeSamhType(response.substring(currentPos, currentPos += 4))
+                .BeskrSamhType(response.substring(currentPos, currentPos += 30))
+                .datoSamhType(response.substring(currentPos, currentPos += 8))
+                .datoSamhTom(response.substring(currentPos, currentPos += 8))
+                .navn(response.substring(currentPos, currentPos += 40))
+                .kodeSpraak(response.substring(currentPos, currentPos += 4))
+                .etatsmerke(response.substring(currentPos, currentPos += 1))
+                .utbetalingssperre(response.substring(currentPos, currentPos += 1))
+                .kodeKontrint(response.substring(currentPos, currentPos += 4))
+                .beskrKontrint(response.substring(currentPos, currentPos += 30))
+                .kodeStatus(response.substring(currentPos, currentPos += 4))
+                .BeskrStatus(response.substring(currentPos, currentPos += 26))
+                .oppdater(response.substring(currentPos, currentPos += 1))
+                .kilde(response.substring(currentPos, currentPos += 4))
+                .brukerId(response.substring(currentPos, currentPos += 8))
+                .tidReg(response.substring(currentPos, currentPos + 12))
                 .build();
     }
 }
