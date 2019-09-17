81babdad5199b383c37d0a9e28e706b6ff8aab1d
Andreas Ludvigsen <andreas.ludvigsen@nav.no>
2019-09-17 10:34:08 +0200

    Fjernet datakilder i url (queryparam)

--- dolly-webapp/src/main/dolly-web/src/pages/gruppe/TestbrukerListe/TestbrukerListe.js
+++ dolly-webapp/src/main/dolly-web/src/pages/gruppe/TestbrukerListe/TestbrukerListe.js
@@ -76,12 +76,7 @@ export default class TestbrukerListe extends Component {
 															personId={bruker[0]}
 															username={username}
 															bestillingId={bruker[5]}
-															editAction={() =>
-																editTestbruker(
-																	bruker[0],
-																	this.dataSourceUrlListe(testidenter, bruker[0])
-																)
-															}
+															editAction={() => editTestbruker(bruker[0])}
 														/>
 													}
 												>
@@ -102,20 +97,5 @@ export default class TestbrukerListe extends Component {
 				}}
 			/>
 		)
-	}
-
-	dataSourceUrlListe = (testidenter, ident) => {
-		let temp = []
-		testidenter.forEach(testident => {
-			if (testident.ident === ident) {
-				Object.keys(testident).forEach(source => {
-					// Lagrer de 4 første tegnene av strings med "Status" i. Stub er ikke en del av de 4 tegnene (Det blir f.eks. krr, ikke krrs)
-					source.includes('Status') && temp.push(source.split('stub')[0].substr(0, 4))
-				})
-			}
-		})
-		let urlString = '&tpsf'
-		temp.forEach(source => (urlString += '&' + source))
-		return urlString
 	}
 }
