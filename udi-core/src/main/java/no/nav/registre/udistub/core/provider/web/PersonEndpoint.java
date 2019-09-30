afe455ee21467f51ab358ac734be8e8052d0e9ad
t155132 <tim.teige91@gmail.com>
2019-09-30 09:38:21 +0200

    fixed nullpointer exception, used wrong request type

--- udi-core/src/main/java/no/nav/registre/udistub/core/provider/web/PersonEndpoint.java
+++ udi-core/src/main/java/no/nav/registre/udistub/core/provider/web/PersonEndpoint.java
@@ -2,14 +2,14 @@ package no.nav.registre.udistub.core.provider.web;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import ma.glasnost.orika.MapperFacade;
 import no.udi.common.v2.PingRequestType;
-import no.udi.mt_1067_nav_data.v1.HentPersonstatusRequestType;
 import no.udi.mt_1067_nav_data.v1.HentPersonstatusResultat;
-import org.springframework.core.convert.ConversionService;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+import v1.mt_1067_nav.no.udi.HentPersonstatusRequestType;
 
 import javax.xml.bind.JAXBElement;
 import javax.xml.namespace.QName;
@@ -26,7 +26,7 @@ public class PersonEndpoint {
     private static final String NAMESPACE_URI = "http://udi.no.MT_1067_NAV.v1";
 
     private final PersonService personService;
-    private final ConversionService conversionService;
+    private final MapperFacade mapperFacade;
 
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "HentPersonstatusRequest")
     @ResponsePayload
@@ -34,7 +34,7 @@ public class PersonEndpoint {
 
         UdiPerson foundPerson = personService.finnPerson(request.getParameter().getFodselsnummer())
                 .orElseThrow(() -> new NotFoundException(String.format("Kunne ikke finne person med fnr:%s", request.getParameter().getFodselsnummer())));
-        HentPersonstatusResultat hentPersonstatusResultat = conversionService.convert(foundPerson, HentPersonstatusResultat.class);
+        HentPersonstatusResultat hentPersonstatusResultat = mapperFacade.map(foundPerson, HentPersonstatusResultat.class);
 
         return new JAXBElement<>(new QName("http://udi.no.MT_1067_NAV.v1", "HentPersonstatusResultat"), HentPersonstatusResultat.class, hentPersonstatusResultat);
     }
