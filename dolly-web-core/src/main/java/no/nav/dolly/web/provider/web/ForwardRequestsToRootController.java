f6b4f56733edea1130cde3a697f109b900cf5949
Stian Låte <stian.late@nav.no>
2019-09-30 15:30:17 +0200

    Added ForwardRequestsToRootController with index as root path

---
+++ dolly-web-core/src/main/java/no/nav/dolly/web/provider/web/ForwardRequestsToRootController.java
@@ -0,0 +1,13 @@
+package no.nav.dolly.web.provider.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ForwardRequestsToRootController {
+
+    @RequestMapping(value = {"/profil/**", "/team/**", "/gruppe/**", "/maler/**", "/tpsendring/**"})
+    public String forwardToIndex() {
+        return "index/";
+    }
+}
