d8e2f87863cc27d8ca5a86ee6b8e82f216b1e79e
t155132 <tim.teige91@gmail.com>
2019-10-02 15:49:01 +0200

    arbeidsadgang tillatelse

--- udi-core/src/main/java/no/nav/registre/udistub/core/converter/ws/ArbeidsadgangWsConverter.java
+++ udi-core/src/main/java/no/nav/registre/udistub/core/converter/ws/ArbeidsadgangWsConverter.java
@@ -1,8 +1,12 @@
 package no.nav.registre.udistub.core.converter.ws;
 
 import no.udi.mt_1067_nav_data.v1.Arbeidsadgang;
+import no.udi.mt_1067_nav_data.v1.JaNeiUavklart;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
+
+import java.sql.Date;
+import java.time.LocalDate;
 
 import no.nav.registre.udistub.core.service.to.UdiArbeidsadgang;
 
@@ -19,7 +23,20 @@ public class ArbeidsadgangWsConverter implements Converter<UdiArbeidsadgang, Arb
 
             resultArbeidsadgang.setArbeidsadgangsPeriode(periodeWsConverter.convert(arbeidsadgang.getPeriode()));
             resultArbeidsadgang.setArbeidsOmfang(arbeidsadgang.getArbeidsOmfang());
-            resultArbeidsadgang.setHarArbeidsadgang(arbeidsadgang.getHarArbeidsadgang());
+            JaNeiUavklart harArbeidsadgang = arbeidsadgang.getHarArbeidsadgang();
+            LocalDate now = LocalDate.now();
+            if (harArbeidsadgang == null) {
+                java.util.Date til = resultArbeidsadgang.getArbeidsadgangsPeriode().getTil().toGregorianCalendar().getTime();
+                java.util.Date fra = resultArbeidsadgang.getArbeidsadgangsPeriode().getFra().toGregorianCalendar().getTime();
+                if (fra.after(Date.valueOf(now)) && til.after(Date.valueOf(now))) {
+                    harArbeidsadgang = JaNeiUavklart.JA;
+                } else if (fra.before(Date.valueOf(now)) || til.after(Date.valueOf(now))) {
+                    harArbeidsadgang = JaNeiUavklart.NEI;
+                } else {
+                    harArbeidsadgang = JaNeiUavklart.UAVKLART;
+                }
+            }
+            resultArbeidsadgang.setHarArbeidsadgang(harArbeidsadgang);
             resultArbeidsadgang.setTypeArbeidsadgang(arbeidsadgang.getTypeArbeidsadgang());
 
             return resultArbeidsadgang;
