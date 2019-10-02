cbb91ddd6d392482c5962b031735c79d34ca9dfd
t155132 <tim.teige91@gmail.com>
2019-10-02 13:49:42 +0200

    web service security for gosys

--- udi-core/src/main/java/no/nav/registre/udistub/core/config/WebServiceConfig.java
+++ udi-core/src/main/java/no/nav/registre/udistub/core/config/WebServiceConfig.java
@@ -7,10 +7,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.soap.security.wss4j2.Wss4jSecurityInterceptor;
+import org.springframework.ws.soap.security.wss4j2.callback.SimplePasswordValidationCallbackHandler;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.xml.xsd.SimpleXsdSchema;
 import org.springframework.xml.xsd.XsdSchema;
+
+import java.util.List;
 
 @EnableWs
 @Configuration
@@ -37,5 +41,23 @@ public class WebServiceConfig extends WsConfigurerAdapter {
     @Bean
     public XsdSchema personStatusesSchema() {
         return new SimpleXsdSchema(new ClassPathResource("informasjon/MT_1067_NAV_v1.xsd"));
+    }
+
+    @Bean
+    public Wss4jSecurityInterceptor securityInterceptor() {
+        Wss4jSecurityInterceptor securityInterceptor = new Wss4jSecurityInterceptor();
+        securityInterceptor.setValidationActions("Timestamp UsernameToken Encrypt Signature NoSecurity");
+        securityInterceptor.setValidationCallbackHandler(securityCallbackHandler());
+        return securityInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(List interceptors) {
+        interceptors.add(securityInterceptor());
+    }
+
+    @Bean
+    public SimplePasswordValidationCallbackHandler securityCallbackHandler() {
+        return new SimplePasswordValidationCallbackHandler();
     }
 }
