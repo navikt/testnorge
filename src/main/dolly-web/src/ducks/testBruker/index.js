3b2fbe491fbef41c95d59d68a59089c1603cea24
s152486 <ingvild.stoen@nav.no>
2019-10-03 15:49:24 +0200

    REG-5561 Bugfix instdata. Henter fra alle valgte fnr

--- src/main/dolly-web/src/ducks/testBruker/index.js
+++ src/main/dolly-web/src/ducks/testBruker/index.js
@@ -195,6 +195,10 @@ export default function testbrukerReducer(state = initialState, action) {
 					tpsf: state.items.tpsf.filter(item => item.ident !== action.meta.ident),
 					sigrunstub: { ...state.items.sigrunstub, [action.meta.ident]: null },
 					krrstub: { ...state.items.krrstub, [action.meta.ident]: null }
+					// arenaforvalteren: { ...state.items.arenaforvalteren, [action.meta.ident]: null },
+					// aareg: { ...state.items.aareg, [action.meta.ident]: null },
+					// pdlforvalter: state.items.pdlforvalter.filter(item => item.ident !== action.meta.ident),
+					// instdata: delete state.items.instdata[action.meta.ident]
 					// ! udi-stub også her?
 				}
 			}
@@ -284,7 +288,7 @@ export default function testbrukerReducer(state = initialState, action) {
 				items: {
 					...state.items,
 					instdata: {
-						...state.items.inst,
+						...state.items.instdata,
 						[action.meta.ident]: action.payload && action.payload.data
 					}
 				}
