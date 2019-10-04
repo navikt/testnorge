dbd55c1d7037432e2eeaac45fef3ac3975953164
K156575 <baard.krohg@gmail.com>
2019-10-04 10:22:42 +0200

    bugfix for datoer

--- arena-domain/src/main/java/no/nav/registre/arena/domain/utils/DateString.java
+++ arena-domain/src/main/java/no/nav/registre/arena/domain/utils/DateString.java
@@ -1,7 +1,12 @@
 package no.nav.registre.arena.domain.utils;
 
+import java.util.Objects;
+
 public class DateString {
     public static String dayMonthYearToYearMonthDay(String date) {
+        if (Objects.isNull(date) || date.trim().isEmpty()) {
+            return null;
+        }
         String[] ddmmyyyy = date.split("-");
         return (ddmmyyyy[2] + "-" + ddmmyyyy[1] + "-" + ddmmyyyy[0]);
     }
