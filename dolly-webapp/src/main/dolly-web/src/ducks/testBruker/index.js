75ca87428620ba3973196328704d18c22bc105d8
s152486 <ingvild.stoen@nav.no>
2019-10-04 08:28:26 +0200

    REG-5561 Fjerne registredata fra Redux ved sletting av testperson

--- dolly-webapp/src/main/dolly-web/src/ducks/testBruker/index.js
+++ dolly-webapp/src/main/dolly-web/src/ducks/testBruker/index.js
@@ -194,11 +194,11 @@ export default function testbrukerReducer(state = initialState, action) {
 					...state.items,
 					tpsf: state.items.tpsf.filter(item => item.ident !== action.meta.ident),
 					sigrunstub: { ...state.items.sigrunstub, [action.meta.ident]: null },
-					krrstub: { ...state.items.krrstub, [action.meta.ident]: null }
-					// arenaforvalteren: { ...state.items.arenaforvalteren, [action.meta.ident]: null },
-					// aareg: { ...state.items.aareg, [action.meta.ident]: null },
-					// pdlforvalter: state.items.pdlforvalter.filter(item => item.ident !== action.meta.ident),
-					// instdata: delete state.items.instdata[action.meta.ident]
+					krrstub: { ...state.items.krrstub, [action.meta.ident]: null },
+					arenaforvalteren: { ...state.items.arenaforvalteren, [action.meta.ident]: null },
+					aareg: { ...state.items.aareg, [action.meta.ident]: null },
+					pdlforvalter: { ...state.items.aareg, [action.meta.ident]: null },
+					instdata: { ...state.items.aareg, [action.meta.ident]: null }
 					// ! udi-stub også her?
 				}
 			}
