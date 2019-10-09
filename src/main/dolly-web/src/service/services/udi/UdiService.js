203ffad3b3c7045bad17528d28992d9852dde20e
Andreas Ludvigsen <andreas.ludvigsen@nav.no>
2019-10-09 13:44:17 +0200

    REG-5641 fjerne classer fra service endepunkter

--- src/main/dolly-web/src/service/services/udi/UdiService.js
+++ src/main/dolly-web/src/service/services/udi/UdiService.js
@@ -1,14 +1,13 @@
 import Request from '../Request'
-import axios from 'axios'
 import ConfigService from '~/service/Config'
 
-export default class UdiService {
-	static getUdiUrl() {
-		const url = ConfigService.getDatesourceUrl('udi') || 'https://udi-stub.nais.preprod.local'
-		return url + '/api/v1'
-	}
+const defaultUrl = 'https://udi-stub.nais.preprod.local'
+const getUdiBaseUrl = () => ConfigService.getDatesourceUrl('udi') || defaultUrl
+const getUdiUrl = () => `${getUdiBaseUrl()}/api/v1`
 
-	static getTestbruker(ident) {
-		return Request.getWithoutCredentials(`${this.getUdiUrl()}/person/${ident}`)
+export default {
+	getTestbruker(ident) {
+		const endpoint = `${getUdiUrl()}/person/${ident}`
+		return Request.getWithoutCredentials(endpoint)
 	}
 }
