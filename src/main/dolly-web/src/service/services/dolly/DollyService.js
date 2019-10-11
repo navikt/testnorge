c57a978150e691e16d16c0920f4a09993c576f9d
s152486 <ingvild.stoen@nav.no>
2019-10-11 10:02:14 +0200

    REG-5616 Lagt til mulighet for flere utenlands-ID

--- src/main/dolly-web/src/service/services/dolly/DollyService.js
+++ src/main/dolly-web/src/service/services/dolly/DollyService.js
@@ -39,9 +39,9 @@ export default class DollyService {
 		return Request.put(Endpoints.gruppeAttributter(gruppeId), data)
 	}
 
-	static updateGruppeIdenter(gruppeId, data) {
-		return Request.put(Endpoints.gruppeIdenter(gruppeId, data))
-	}
+	// static updateGruppeIdenter(gruppeId, data) {
+	// 	return Request.put(Endpoints.gruppeIdenter(gruppeId, data))
+	// }
 
 	static createBestilling(gruppeId, data) {
 		return Request.post(Endpoints.gruppeBestilling(gruppeId), data)
