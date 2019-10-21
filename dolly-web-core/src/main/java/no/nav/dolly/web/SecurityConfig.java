4e4ff10f4f33c62e0db40d6881b246d8d7b5ad1e
Stian Låte <stian.late@nav.no>
2019-10-21 16:31:01 +0200

    Attempting to add explicit cors configuration bean

--- dolly-web-core/src/main/java/no/nav/dolly/web/SecurityConfig.java
+++ dolly-web-core/src/main/java/no/nav/dolly/web/SecurityConfig.java
@@ -3,9 +3,15 @@ package no.nav.dolly.web;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.freg.security.oidc.auth.common.HttpSecurityConfigurer;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Slf4j
 @Configuration
@@ -25,5 +31,26 @@ public class SecurityConfig implements HttpSecurityConfigurer {
         if (disableFrameOptions) {
             http.headers().frameOptions().disable();
         }
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList(allowedOrigins));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedHeaders(Arrays.asList(
+                "Access-Control-Allow-Headers",
+                "Access-Control-Request-Headers",
+                "Access-Control-Request-Method",
+                "X-Requested-With",
+                "X-XSRF-TOKEN",
+                "Content-Type"
+        ));
+        configuration.setAllowCredentials(true);
+        configuration.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
