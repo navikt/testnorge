55d443157c0d66fee3181c5a67cddab297e5750a
Kristoffer Mjelva <kristoffer.mjelva@nav.no>
2019-10-22 11:11:58 +0200

    Test i InternalController

--- aareg-core/src/main/java/no/nav/registre/aareg/properties/ProvidersProps.java
+++ aareg-core/src/main/java/no/nav/registre/aareg/properties/ProvidersProps.java
@@ -15,17 +15,7 @@ import org.springframework.context.annotation.Configuration;
 public class ProvidersProps {
 
     private Tpsf tpsf = new Tpsf();
-    private SigrunStub sigrunStub = new SigrunStub();
-    private KrrStub krrStub = new KrrStub();
-    private UdiStub udiStub = new UdiStub();
-    private Kodeverk kodeverk = new Kodeverk();
-    private Norg2 norg2 = new Norg2();
     private Fasit fasit = new Fasit();
-    private ArenaForvalter arenaForvalter = new ArenaForvalter();
-    private PdlForvalter pdlForvalter = new PdlForvalter();
-    private PersonOppslag personOppslag = new PersonOppslag();
-    private Syntdata syntdata = new Syntdata();
-    private Instdata instdata = new Instdata();
 
     @Getter
     @Setter
@@ -41,107 +31,7 @@ public class ProvidersProps {
     @Builder
     @NoArgsConstructor
     @AllArgsConstructor
-    public static class SigrunStub {
-
-        private String url;
-    }
-
-    @Getter
-    @Setter
-    @Builder
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class KrrStub {
-
-        private String url;
-    }
-
-    @Getter
-    @Setter
-    @Builder
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class UdiStub {
-
-        private String url;
-    }
-
-    @Getter
-    @Setter
-    @Builder
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class Kodeverk {
-
-        private String url;
-    }
-
-    @Getter
-    @Setter
-    @Builder
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class Norg2 {
-
-        private String url;
-    }
-
-    @Getter
-    @Setter
-    @Builder
-    @NoArgsConstructor
-    @AllArgsConstructor
     public static class Fasit {
-
-        private String url;
-    }
-
-    @Getter
-    @Setter
-    @Builder
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class ArenaForvalter {
-
-        private String url;
-    }
-
-    @Getter
-    @Setter
-    @Builder
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class PdlForvalter {
-
-        private String url;
-    }
-
-    @Getter
-    @Setter
-    @Builder
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class PersonOppslag {
-
-        private String url;
-    }
-
-    @Getter
-    @Setter
-    @Builder
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class Syntdata {
-
-        private String url;
-    }
-
-    @Getter
-    @Setter
-    @Builder
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class Instdata {
 
         private String url;
     }
--- aareg-core/src/main/java/no/nav/registre/aareg/provider/rs/InternalController.java
+++ aareg-core/src/main/java/no/nav/registre/aareg/provider/rs/InternalController.java
@@ -1,8 +1,13 @@
 package no.nav.registre.aareg.provider.rs;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import no.nav.registre.aareg.service.AaregService;
 
 @RestController
 @RequestMapping("/internal")
@@ -16,5 +21,16 @@ public class InternalController {
     @GetMapping("/isAlive")
     public String isAlive() {
         return "OK";
+    }
+
+    @Autowired
+    private AaregService aaregService;
+
+    @GetMapping("/arb")
+    public ResponseEntity hentArbeidsforhold(
+            @RequestParam String ident,
+            @RequestParam String miljoe
+    ) {
+        return aaregService.hentArbeidsforhold(ident, miljoe);
     }
 }
