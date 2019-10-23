e0d9043b2595e312424a5d586389c378b064a1ce
Kristoffer Mjelva <kristoffer.mjelva@nav.no>
2019-10-23 15:48:40 +0200

    REG-5811: Vault-test

--- aareg-core/src/main/java/no/nav/registre/aareg/ApplicationStarter.java
+++ aareg-core/src/main/java/no/nav/registre/aareg/ApplicationStarter.java
@@ -1,12 +1,23 @@
 package no.nav.registre.aareg;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import no.nav.registre.aareg.vault.VaultUtil;
+
 @SpringBootApplication
+@Slf4j
 public class ApplicationStarter {
 
     public static void main(String[] args) {
+        if ("prod".equals(System.getProperty("spring.profiles.active"))) {
+            VaultUtil.initCloudVaultToken();
+        } else {
+            System.setProperty("spring.cloud.vault.token", "00000000-0000-0000-0000-000000000000");
+        }
+        log.info(System.getProperty("spring.profiles.active"));
+        log.info(System.getProperty("spring.cloud.vault.token"));
         SpringApplication.run(ApplicationStarter.class, args);
     }
 }
