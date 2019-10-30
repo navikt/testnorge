12f20eb4792727f330a3568250b764b444f79b84
Kristoffer Mjelva <kristoffer.mjelva@nav.no>
2019-10-30 12:22:05 +0100

    REG-5811: La til security-configs

---
+++ aareg-core/src/main/java/no/nav/registre/aareg/config/provider/ProviderConfig.java
@@ -0,0 +1,50 @@
+package no.nav.registre.aareg.config.provider;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class ProviderConfig implements WebMvcConfigurer {
+
+    @Value("${testnorge.aareg.security.cors.origins:''}")
+    private String[] allowedOrigins;
+
+    @Autowired
+    private final RestMdcInterceptor restMdcInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(restMdcInterceptor)
+                .excludePathPatterns("/api")
+                .addPathPatterns("/api/**");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/api").setViewName("redirect:/swagger-ui.html");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders(
+                        "Access-Control-Allow-Headers",
+                        "Access-Control-Request-Headers",
+                        "Access-Control-Request-Method",
+                        "X-Requested-With",
+                        "X-XSRF-TOKEN",
+                        "Content-Type"
+                )
+                .allowCredentials(true)
+                .maxAge(3600L);
+    }
+}
---
+++ aareg-core/src/main/java/no/nav/registre/aareg/security/SecurityConfig.java
@@ -0,0 +1,24 @@
+package no.nav.registre.aareg.security;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+import no.nav.freg.security.oidc.auth.common.HttpSecurityConfigurer;
+
+@Slf4j
+@Configuration
+@Order(1)
+public class SecurityConfig implements HttpSecurityConfigurer {
+
+    @Value("${testnorge.aareg.security.cors.origins:''}")
+    private String[] allowedOrigins;
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.cors();
+        http.csrf().disable();
+    }
+}
