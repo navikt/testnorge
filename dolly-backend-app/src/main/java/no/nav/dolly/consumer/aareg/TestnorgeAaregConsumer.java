f35af0b06800de300efee6d55c3f6b6b80d7959f
Kristoffer Mjelva <kristoffer.mjelva@nav.no>
2019-10-31 12:16:32 +0100

    REG-5929: La til headere i kall til testnorge-aareg

--- dolly-backend-app/src/main/java/no/nav/dolly/consumer/aareg/TestnorgeAaregConsumer.java
+++ dolly-backend-app/src/main/java/no/nav/dolly/consumer/aareg/TestnorgeAaregConsumer.java
@@ -1,5 +1,11 @@
 package no.nav.dolly.consumer.aareg;
 
+import static java.lang.String.format;
+import static no.nav.dolly.domain.CommonKeys.HEADER_NAV_CALL_ID;
+import static no.nav.dolly.domain.CommonKeys.HEADER_NAV_CONSUMER_ID;
+import static no.nav.dolly.security.sts.StsOidcService.getUserIdToken;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.core.ParameterizedTypeReference;
@@ -10,6 +16,7 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriTemplate;
 
 import java.util.Map;
+import java.util.UUID;
 
 import no.nav.dolly.domain.resultset.aareg.RsAaregOppdaterRequest;
 import no.nav.dolly.domain.resultset.aareg.RsAaregOpprettRequest;
@@ -18,6 +25,7 @@ import no.nav.dolly.domain.resultset.aareg.RsAaregResponse;
 @Component
 public class TestnorgeAaregConsumer {
 
+    private static final String CONSUMER = "Dolly";
     private static final String OPPRETT_ARBEIDSFORHOLD = "/v1/arbeidsforhold";
     private static final String OPPDATER_ARBEIDSFORHOLD = "/v1/arbeidsforhold";
     private static final String HENT_ARBEIDSFORHOLD = "/v1/arbeidsforhold?ident={ident}&miljoe={miljoe}";
@@ -42,23 +50,47 @@ public class TestnorgeAaregConsumer {
     }
 
     public RsAaregResponse opprettArbeidsforhold(RsAaregOpprettRequest request) {
-        RequestEntity postRequest = RequestEntity.post(opprettArbeidsforholdUrl.expand()).body(request);
+        RequestEntity postRequest =
+                RequestEntity.post(opprettArbeidsforholdUrl.expand())
+                        .header(AUTHORIZATION, getUserIdToken())
+                        .header(HEADER_NAV_CONSUMER_ID, CONSUMER)
+                        .header(HEADER_NAV_CALL_ID, getNavCallId())
+                        .body(request);
         return restTemplate.exchange(postRequest, RsAaregResponse.class).getBody();
     }
 
     public RsAaregResponse oppdaterArbeidsforhold(RsAaregOppdaterRequest request) {
-        RequestEntity putRequest = RequestEntity.put(oppdaterArbeidsforholdUrl.expand()).body(request);
+        RequestEntity putRequest =
+                RequestEntity.put(oppdaterArbeidsforholdUrl.expand())
+                        .header(AUTHORIZATION, getUserIdToken())
+                        .header(HEADER_NAV_CONSUMER_ID, CONSUMER)
+                        .header(HEADER_NAV_CALL_ID, getNavCallId())
+                        .body(request);
         return restTemplate.exchange(putRequest, RsAaregResponse.class).getBody();
     }
 
     public ResponseEntity<Map[]> hentArbeidsforhold(String ident, String miljoe) {
-        RequestEntity getRequest = RequestEntity.get(hentArbeidsforholdUrl.expand(ident, miljoe)).build();
+        RequestEntity getRequest =
+                RequestEntity.get(hentArbeidsforholdUrl.expand(ident, miljoe))
+                        .header(AUTHORIZATION, getUserIdToken())
+                        .header(HEADER_NAV_CONSUMER_ID, CONSUMER)
+                        .header(HEADER_NAV_CALL_ID, getNavCallId())
+                        .build();
         return restTemplate.exchange(getRequest, Map[].class);
     }
 
     public Map<String, String> slettArbeidsforholdFraAlleMiljoer(String ident) {
-        RequestEntity deleteRequest = RequestEntity.delete(slettArbeidsforholdUrl.expand(ident)).build();
+        RequestEntity deleteRequest =
+                RequestEntity.delete(slettArbeidsforholdUrl.expand(ident))
+                        .header(AUTHORIZATION, getUserIdToken())
+                        .header(HEADER_NAV_CONSUMER_ID, CONSUMER)
+                        .header(HEADER_NAV_CALL_ID, getNavCallId())
+                        .build();
         return restTemplate.exchange(deleteRequest, new ParameterizedTypeReference<Map<String, String>>() {
         }).getBody();
+    }
+
+    private static String getNavCallId() {
+        return format("%s %s", CONSUMER, UUID.randomUUID().toString());
     }
 }
