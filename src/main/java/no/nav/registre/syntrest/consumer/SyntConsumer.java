00fd10ab95a1edb8d816357a722be55dd52ab46f
K156575 <baard.krohg@gmail.com>
2019-11-01 13:34:27 +0100

    REG-5115: updated the application manager so it takes full responsibility for the applications life cycle. A desicion was made to keep the start application public, for now. The problem is if an application has been deployed from another source (manually by kubectl), the application manager won't know that it's already started, and may try to deploy it again..

--- src/main/java/no/nav/registre/syntrest/consumer/SyntConsumer.java
+++ src/main/java/no/nav/registre/syntrest/consumer/SyntConsumer.java
@@ -1,43 +1,47 @@
 package no.nav.registre.syntrest.consumer;
 
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.registre.syntrest.kubernetes.ApplicationManager;
 import no.nav.registre.syntrest.utils.SyntAppNames;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Objects;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
+@Getter
 public class SyntConsumer {
-
-    private final long SHUTDOWN_TIME_DELAY_SECONDS = 300;
 
     private final ApplicationManager applicationManager;
     private final RestTemplate restTemplate;
-    private final ScheduledExecutorService scheduledExecutorService;
-    private final ScheduledFuture<Integer> scheduledFutureTakedownApplication;
+    // private final ScheduledExecutorService scheduledExecutorService;
     private final String appName;
-    private final AtomicInteger numClients;
+    // private final AtomicInteger numClients;
+
+    // private ScheduledFuture<?> scheduledFutureTakedownApplication;
 
     public SyntConsumer(ApplicationManager applicationManager, RestTemplate restTemplate, ScheduledExecutorService scheduledExecutorService, SyntAppNames name) {
         this.applicationManager = applicationManager;
         this.restTemplate = restTemplate;
-        this.scheduledExecutorService = scheduledExecutorService;
+        // this.scheduledExecutorService = scheduledExecutorService;
         this.appName = name.getName();
-        this.numClients = new AtomicInteger(0);
-        this.scheduledFutureTakedownApplication = null;
+        // this.numClients = new AtomicInteger(0);
+        // this.scheduledFutureTakedownApplication = null;
     }
 
-    public Object synthesizeData(RequestEntity request) {
-        this.numClients.incrementAndGet();
+    public Object synthesizeData(RequestEntity request) throws InterruptedException, ExecutionException {
+        // this.numClients.incrementAndGet();
 
         if (!applicationManager.applicationIsAlive(appName)) {
             int started = applicationManager.startApplication(appName);
@@ -48,34 +52,45 @@ public class SyntConsumer {
         }
 
         Object synthesizedData = accessSyntPackage(request);
+        applicationManager.updateAccessedPackages(this);
 
-        try {
+        return synthesizedData;
+        /*try {
             return synthesizedData;
         } finally {
             this.numClients.decrementAndGet();
             System.out.println("Number of connected clients after this: " + this.numClients.get());
             if (this.numClients.get() <= 0) {
                 log.info("Scheduling shutdown of {}", this.appName);
-                if (!this.scheduledExecutorService.isShutdown()) {
+                *//*if (!this.scheduledExecutorService.isShutdown()) {
                     this.scheduledExecutorService.schedule(this::shutdownApplication, SHUTDOWN_TIME_DELAY_SECONDS, TimeUnit.SECONDS);
                 } else {
                     log.info("More clients were connected to \'{}\' before shutdown. Resetting shutdown timer.", this.appName);
                     this.scheduledExecutorService.shutdown();
                     this.scheduledExecutorService.schedule(this::shutdownApplication, SHUTDOWN_TIME_DELAY_SECONDS, TimeUnit.SECONDS);
-                }
+                }*//*
+                if (Objects.isNull(this.scheduledFutureTakedownApplication.get()) || )
+                this.scheduledFutureTakedownApplication = this.scheduledExecutorService.schedule(
+                        this::shutdownApplication,
+                        SHUTDOWN_TIME_DELAY_SECONDS,
+                        TimeUnit.SECONDS);
             }
-        }
+        }*/
     }
 
-    private void shutdownApplication() {
-        /*this.numClients.decrementAndGet();*/
+    public void shutdownApplication() {
+        applicationManager.shutdownApplication(appName);
+    }
+
+    /*private void shutdownApplication() {
+        *//*this.numClients.decrementAndGet();*//*
         if (this.numClients.get() > 0) {
             log.info("More clients were connected to {} before shutdown.", this.appName);
             return;
         }
         log.info("Shutting down {}", this.appName);
         applicationManager.shutdownApplication(appName);
-    }
+    }*/
 
     // In the syntConsumer because we will allow synt packages of other types to be accessed asynchronously,
     // but calls to the *same SyntPackage* should happen one at a time.
--- src/main/java/no/nav/registre/syntrest/kubernetes/ApplicationManager.java
+++ src/main/java/no/nav/registre/syntrest/kubernetes/ApplicationManager.java
@@ -3,11 +3,19 @@ package no.nav.registre.syntrest.kubernetes;
 import io.kubernetes.client.ApiException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import no.nav.registre.syntrest.consumer.SyntConsumer;
+import no.nav.registre.syntrest.utils.SyntAppNames;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Component
-@RequiredArgsConstructor
 /**
  * Manages lifecycle of the app. Abstracts away the call to the KubernetesController,
  * and make sure the calls to start/end applications happens one at a time.
@@ -17,10 +25,17 @@ import org.springframework.stereotype.Component;
  */
 public class ApplicationManager {
 
-    private final KubernetesController kubernetesController;
+    @Value("${synth-package-unused-uptime}")
+    private long SHUTDOWN_TIME_DELAY_SECONDS;
 
-    public boolean applicationIsAlive(String appId) {
-        return kubernetesController.isAlive(appId);
+    private final KubernetesController kubernetesController;
+    private final ScheduledExecutorService scheduledExecutorService;
+    private Map<String, ScheduledFuture<?>> activeApplications;
+
+    public ApplicationManager(KubernetesController kubernetesController, ScheduledExecutorService scheduledExecutorService) {
+        this.kubernetesController = kubernetesController;
+        this.scheduledExecutorService = scheduledExecutorService;
+        this.activeApplications = new HashMap<>();
     }
 
     public synchronized int startApplication(String appId) {
@@ -33,6 +48,27 @@ public class ApplicationManager {
         }
 
         return returnValue;
+    }
+
+    public synchronized void updateAccessedPackages(SyntConsumer app) {
+        if (activeApplications.containsKey(app.getAppName())) {
+
+            log.info("Updating termination timer for \'{}\'", app.getAppName());
+            activeApplications.get(app.getAppName()).cancel(true);
+            activeApplications.put(app.getAppName(), scheduledExecutorService.schedule(app::shutdownApplication, SHUTDOWN_TIME_DELAY_SECONDS, TimeUnit.SECONDS));
+
+        } else {
+
+            log.info("Starting termination timer for \'{}\'", app.getAppName());
+            activeApplications.put(app.getAppName(), scheduledExecutorService.schedule(app::shutdownApplication, SHUTDOWN_TIME_DELAY_SECONDS, TimeUnit.SECONDS));
+        }
+    }
+
+    // If the application is in the activeApplications-list, this should return true without having to call
+    // the isAlive for the kubernetesController. However, if it is done that way, we will not have control over
+    // the applications started on the cluster in some other ways...
+    public boolean applicationIsAlive(String appId) {
+        return kubernetesController.isAlive(appId);
     }
 
     public synchronized void shutdownApplication(String appId) {
