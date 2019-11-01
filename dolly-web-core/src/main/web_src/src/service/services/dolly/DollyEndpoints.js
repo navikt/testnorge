6cbb79e43780f2c3a838e49b2694a2f6d0da84a1
Andreas Ludvigsen <andreas.ludvigsen@nav.no>
2019-11-01 13:28:40 +0100

    REG-5999 oppdatert endepunkt (fjernet /ny)

--- dolly-web-core/src/main/web_src/src/service/services/dolly/DollyEndpoints.js
+++ dolly-web-core/src/main/web_src/src/service/services/dolly/DollyEndpoints.js
@@ -19,7 +19,7 @@ export default class DollyEndpoints {
 	}
 
 	static gruppeById(gruppeId) {
-		return `${groupBase}/${gruppeId}/ny`
+		return `${groupBase}/${gruppeId}`
 	}
 
 	static gruppeByUser(userId) {
