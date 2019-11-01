9fb433382461296c8ae9bb72846e2cce1489c22d
Kristoffer Mjelva <kristoffer.mjelva@nav.no>
2019-11-01 13:35:16 +0100

    Oppdatert URL. Fungerer ikke på POST inntekt

--- src/main/java/no/nav/registre/inntekt/consumer/rs/InntektstubConsumer.java
+++ src/main/java/no/nav/registre/inntekt/consumer/rs/InntektstubConsumer.java
@@ -1,8 +1,15 @@
 package no.nav.registre.inntekt.consumer.rs;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
@@ -15,6 +22,8 @@ import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriTemplate;
 
+import javax.net.ssl.SSLContext;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 import java.util.Map;
 
@@ -36,6 +45,7 @@ public class InntektstubConsumer {
     private String token;
 
     private RestTemplate restTemplate;
+    private RestTemplate restTemplateUser;
 
     private UriTemplate leggTilInntektUrl;
     private UriTemplate hentEksisterendeIdenterUrl;
@@ -45,12 +55,36 @@ public class InntektstubConsumer {
     public InntektstubConsumer(@Value("${inntektstub.rest.api.url}") String inntektstubUrl,
             @Value("${testnorges.ida.credential.inntektstub.username}") String username,
             @Value("${testnorges.ida.credential.inntektstub.password}") String password) {
-        CloseableHttpClient httpClient = HttpClientBuilder
-                .create()
+
+        /////////
+        final SSLConnectionSocketFactory sslsf;
+        try {
+            sslsf = new SSLConnectionSocketFactory(SSLContext.getDefault(), NoopHostnameVerifier.INSTANCE);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+
+        final Registry<ConnectionSocketFactory> registry = RegistryBuilder.<ConnectionSocketFactory>create()
+                .register("http", new PlainConnectionSocketFactory())
+                .register("https", sslsf)
                 .build();
+
+        final PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager(registry);
+        cm.setMaxTotal(100);
+
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setSSLSocketFactory(sslsf)
+                .setConnectionManager(cm)
+                .build();
+
         HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
         this.restTemplate = new RestTemplate(factory);
         this.restTemplate.getInterceptors().add(new BasicAuthenticationInterceptor(username, password));
+        /////////
+
+        this.restTemplateUser = new RestTemplate();
+        restTemplateUser.getInterceptors().add(new BasicAuthenticationInterceptor(username, password));
+
         this.leggTilInntektUrl = new UriTemplate(inntektstubUrl + "/v1/personer/inntekt");
         this.hentEksisterendeIdenterUrl = new UriTemplate(inntektstubUrl + "/v1/personer");
         this.hentEksisterendeInntekterUrl = new UriTemplate(inntektstubUrl + "/v1/person/{ident}/inntekter");
@@ -59,8 +93,23 @@ public class InntektstubConsumer {
 
     public Map<String, List<RsInntekt>> leggInntekterIInntektstub(Map<String, List<RsInntekt>> inntekter) {
         try {
-            HttpEntity<Map<String, List<RsInntekt>>> entity = new HttpEntity<>(inntekter, getHeaders());
-            return restTemplate.exchange(leggTilInntektUrl.expand(), HttpMethod.POST, entity, RESPONSE_TYPE_OPPRETT_INNTEKT).getBody();
+
+            //httpHeaders.add("Cookie", "XSRF-TOKEN=" + entity.getHeaders().get("X-XSRF-TOKEN"));
+            //            httpHeaders.add("Cookie", "XSRF-TOKEN=77fcbad6-5cfc-4e5c-826f-f1858f960c7e; JSESSIONID=D91AC0B62DBC2A47A5F67CA17A4DF60C");
+            HttpHeaders headers = getHeaders();
+            List<String> xsrf = headers.get("X-XSRF-TOKEN");
+            List<String> jsession = headers.get("JSESSIONID");
+
+            HttpHeaders head = new HttpHeaders();
+            //            head.add("Cookie", "XSRF-TOKEN=" + xsrf.get(0) + "; JSESSIONID=" + jsession.get(0));
+            head.add("X-XSRF-TOKEN", xsrf.get(0));
+            head.add("Content-Type", "application/json;charset=UTF-8");
+
+            HttpEntity<Map<String, List<RsInntekt>>> ht = new HttpEntity<>(inntekter, head);
+
+            //            HttpEntity<Map<String, List<RsInntekt>>> entity = new HttpEntity<>(inntekter, getHeaders());
+
+            return restTemplate.exchange(leggTilInntektUrl.expand(), HttpMethod.POST, ht, RESPONSE_TYPE_OPPRETT_INNTEKT).getBody();
         } catch (HttpClientErrorException e) {
             log.warn(e.getMessage(), e);
             throw e;
@@ -104,9 +153,9 @@ public class InntektstubConsumer {
     private HttpHeaders getSession() {
         HttpEntity request = new HttpEntity("", new HttpHeaders());
         //Have to call this twice to get a valid token
-        HttpEntity<RsUser> response1 = restTemplate.exchange(hentTokenUrl.expand(), HttpMethod.GET, request, RsUser.class);
+        HttpEntity<RsUser> response1 = restTemplateUser.exchange(hentTokenUrl.expand(), HttpMethod.GET, request, RsUser.class);
         request = new HttpEntity("", getHeaders(response1));
-        HttpEntity<RsUser> response = restTemplate.exchange(hentTokenUrl.expand(), HttpMethod.GET, request, RsUser.class);
+        HttpEntity<RsUser> response = restTemplateUser.exchange(hentTokenUrl.expand(), HttpMethod.GET, request, RsUser.class);
         return getHeaders(response);
     }
 
@@ -123,6 +172,9 @@ public class InntektstubConsumer {
                 StringBuilder buf = new StringBuilder(name);
                 buf.insert(0, "X-");
                 headersRet.add(buf.toString(), token);
+                //                headersRet.add(name, token);
+            } else if (name.contains("JSESSIONID")) {
+                //                headersRet.add(name, token);
             }
         }
         return headersRet;
