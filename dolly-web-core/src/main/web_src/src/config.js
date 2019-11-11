231f3fcc6c2ccb0557bf597ce247fe9dd45b0cea
s152486 <ingvild.stoen@nav.no>
2019-11-11 15:18:24 +0100

    Endret Aareg-endepunkt

--- dolly-web-core/src/main/web_src/src/config.js
+++ dolly-web-core/src/main/web_src/src/config.js
@@ -9,8 +9,7 @@ const config = {
 		krrStubUrl: 'https://krr-stub.nais.preprod.local',
 		sigrunStubUrl: 'https://sigrun-skd-stub.nais.preprod.local',
 		tpsfUrl: 'https://tps-forvalteren-u2.nais.preprod.local',
-		udiStubUrl: 'https://udi-stub.nais.preprod.local',
-		aaregUrl: 'https://testnorge-aareg.nais.preprod.local'
+		udiStubUrl: 'https://udi-stub.nais.preprod.local'
 	},
 	systems: {
 		AAREG: 'Arbeidsregister (AAREG)',
