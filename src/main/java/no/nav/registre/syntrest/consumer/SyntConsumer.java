9ee090c73382db720a6bde23bad2c791114caa2f
K156575 <baard.krohg@gmail.com>
2019-11-11 15:45:12 +0100

    REG-5115: la til client-side handling av restclientexception

--- src/main/java/no/nav/registre/syntrest/consumer/SyntConsumer.java
+++ src/main/java/no/nav/registre/syntrest/consumer/SyntConsumer.java
@@ -7,9 +7,13 @@ import no.nav.registre.syntrest.kubernetes.ApplicationManager;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.util.UriTemplate;
+
+import java.util.Arrays;
 
 @Slf4j
 public class SyntConsumer {
@@ -54,17 +58,23 @@ public class SyntConsumer {
     // In the syntConsumer because we will allow synt packages of other types to be accessed asynchronously,
     // but calls to the *same SyntPackage* should happen one at a time.
     private synchronized Object getDataFromSyntPackage(RequestEntity request) throws ResponseStatusException {
-        ResponseEntity response = restTemplate.exchange(request, Object.class);
+        try {
+            ResponseEntity response = restTemplate.exchange(request, Object.class);
 
-        if (response.getStatusCode() != HttpStatus.OK) {
+            if (response.getStatusCode() != HttpStatus.OK) {
             log.warn("Unexpected synth response: {}\n{}", response.getStatusCode(), response.getBody());
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,
                     String.format("Unexpected synth response: %s\n%s",
                             response.getStatusCode().toString(),
                             response.getBody().toString()));
+            }
+            return response.getBody();
+        } catch (RestClientException e) {
+            log.error("Unexpected client-side error: {}", Arrays.toString(e.getStackTrace()));
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR,
+                    String.format("Unexpected client side error: \n%s",
+                            Arrays.toString(e.getStackTrace())));
         }
-
-        return response.getBody();
     }
 
     public void shutdownApplication() {
