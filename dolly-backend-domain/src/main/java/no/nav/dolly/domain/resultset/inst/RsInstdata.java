271930cbb11d7091cf3083cc64de26572620c91c
Kristen Hærum <kristen.haerum@nav.no>
2019-11-19 13:31:00 +0100

    REG-6202 Lagt til Swagger-dok for Inst-adapter

--- dolly-backend-domain/src/main/java/no/nav/dolly/domain/resultset/inst/RsInstdata.java
+++ dolly-backend-domain/src/main/java/no/nav/dolly/domain/resultset/inst/RsInstdata.java
@@ -2,6 +2,7 @@ package no.nav.dolly.domain.resultset.inst;
 
 import java.time.LocalDateTime;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,12 +16,59 @@ import lombok.Setter;
 @AllArgsConstructor
 public class RsInstdata {
 
+    @ApiModelProperty(
+            position = 1,
+            required = true,
+            value = "Hvilken type institusjon oppholdet er registrert på. Kan være kodeverdiene AS (Alders- og sykehjem), FO (Fengsel) eller HS (Helseinsitusjon)"
+    )
     private InstdataInstitusjonstype institusjonstype;
+
+    @ApiModelProperty(
+            position = 2,
+            required = true,
+            dataType = "LocalDateTime",
+            value = "Dato for begynnelsen av oppholdet"
+    )
     private LocalDateTime startdato;
+
+    @ApiModelProperty(
+            position = 3,
+            dataType = "LocalDateTime",
+            value = "Dato oppholdet er forventet å være avsluttet"
+
+    )
     private LocalDateTime forventetSluttdato;
+
+    @ApiModelProperty(
+            position = 4,
+            dataType = "LocalDateTime",
+            value = "Dato oppholdet var avsluttet"
+    )
     private LocalDateTime faktiskSluttdato;
+
+    @ApiModelProperty(
+            position = 5,
+            value = "Kodeverdi for oppholdskategori. Kan være kodeverdiene A (Alders- og sykehjem), D (Dagpasient), F (Ferieopphold), "
+                    + "H (Heldøgnpasient), P (Fødsel), R (Opptreningsinstitusjon), S (Soningsfange) eller V (Varetektsfange)"
+    )
     private InstdataKategori kategori;
+
+    @ApiModelProperty(
+            position = 6,
+            value = "Kodeverdi for kilde. Defaultverdi settes i backend basert på institusjonstype hvis ikke angitt. "+
+                    "(Kan være kodeverdiene APPBRK(Applikasjonsbruker), IT(Infotrygd), AINN(Altinn), PP01(Pensjon), TPS(TPS), INST(INST))"
+    )
     private InstdataKilde kilde;
+
+    @ApiModelProperty(
+            position = 7,
+            value = "Settes i backend basert på institusjonstype hvis ikke utfylt (Unik id for en institusjon i TSS, bruk oppslag-api’et for å finne den)"
+    )
     private String tssEksternId;
+
+    @ApiModelProperty(
+            position = 8,
+            value = "Flag for å markere om personen ble overført til en annen institusjon"
+    )
     private Boolean overfoert;
 }
