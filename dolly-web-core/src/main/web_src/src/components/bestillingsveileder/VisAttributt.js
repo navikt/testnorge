3f879bf674e404fab9ee48b6c12ad5ffe5bff17e
Andreas Ludvigsen <andreas.ludvigsen@nav.no>
2019-11-20 09:43:47 +0100

    REG-5773 - Bug på vis komponent

--- dolly-web-core/src/main/web_src/src/components/bestillingsveileder/VisAttributt.js
+++ dolly-web-core/src/main/web_src/src/components/bestillingsveileder/VisAttributt.js
@@ -1,9 +1,11 @@
 import { useFormikContext } from 'formik'
 import _isNil from 'lodash/isNil'
-import _get from 'lodash/get'
+import _has from 'lodash/has'
 export { pathAttrs } from '~/service/attributter/Attributter'
 
 export const Vis = ({ attributt, children }) => {
+	const { initialValues } = useFormikContext()
+
 	const isChecked = (initial, attributtPath) => {
 		// Ignore if values ikke er satt
 		if (_isNil(attributtPath)) return false
@@ -11,9 +13,8 @@ export const Vis = ({ attributt, children }) => {
 		// Strings er akseptert, men konverter til Array
 		if (!_isNil(attributtPath) && !Array.isArray(attributtPath)) attributtPath = [attributtPath]
 
-		return attributtPath.some(v => Boolean(_get(initial, v)))
+		return attributtPath.some(v => _has(initial, v))
 	}
 
-	const { initialValues } = useFormikContext()
 	return isChecked(initialValues, attributt) && children
 }
