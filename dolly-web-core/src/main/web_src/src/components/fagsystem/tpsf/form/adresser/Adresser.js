863b3c232694ee5c1408d66f7eb9b33fc6838518
Andreas Ludvigsen <andreas.ludvigsen@nav.no>
2019-11-25 12:55:36 +0100

    vise valgmuligheter for adresser

--- dolly-web-core/src/main/web_src/src/components/fagsystem/tpsf/form/adresser/Adresser.js
+++ dolly-web-core/src/main/web_src/src/components/fagsystem/tpsf/form/adresser/Adresser.js
@@ -1,29 +1,92 @@
-import React from 'react'
-import useBoolean from '~/utils/hooks/useBoolean'
+import React, { useState } from 'react'
+import { RadioPanelGruppe } from 'nav-frontend-skjema'
 import { Vis, pathAttrs } from '~/components/bestillingsveileder/VisAttributt'
 import Panel from '~/components/ui/panel/Panel'
 import { Boadresse } from './partials/boadresse/Boadresse'
 import { MatrikkelAdresse } from './partials/MatrikkelAdresse'
+import { AdresseNr } from './partials/AdresseNr'
 import { FormikDatepicker } from '~/components/ui/form/inputs/datepicker/Datepicker'
 
 const paths = [pathAttrs.kategori.boadresse, pathAttrs.kategori.postadresse].flat()
 
 export const Adresser = ({ formikBag }) => {
-	const [type, setOn, setOff] = useBoolean(true)
+	const [boType, setBoType] = useState()
+
+	const handleRadioChange = e => {
+		const nyType = e.target.value
+		setBoType(nyType)
+
+		formikBag.setFieldValue('tpsf.adresseNrInfo', null)
+		formikBag.setFieldValue('tpsf.boadresse', null)
+
+		// Set and clear values
+		switch (nyType) {
+			case 'postnr':
+				formikBag.setFieldValue('tpsf.adresseNrInfo', {
+					nummertype: 'POSTNR',
+					nummer: ''
+				})
+				break
+			case 'kommunenr':
+				formikBag.setFieldValue('tpsf.adresseNrInfo', {
+					nummertype: 'KOMMUNENR',
+					nummer: ''
+				})
+				break
+			case 'gate':
+				formikBag.setFieldValue('tpsf.boadresse', {
+					adressetype: 'GATE',
+					gateadresse: '',
+					postnr: '',
+					poststed: '',
+					kommunenr: '',
+					gatekode: '',
+					husnummer: ''
+				})
+			case 'matrikkel':
+				formikBag.setFieldValue('tpsf.boadresse', {
+					adressetype: 'GATE',
+					mellomnavn: '',
+					gardsnr: '',
+					bruksnr: '',
+					festnr: '',
+					undernr: '',
+					postnr: ''
+				})
+
+			default:
+				break
+		}
+	}
 
 	return (
 		<Vis attributt={paths}>
 			<Panel heading="Adresser" startOpen>
 				<Vis attributt="tpsf.boadresse">
-					<button onClick={setOn}>Gateadresse</button>
-					<button onClick={setOff}>Matrikkeladresse</button>
-					{type && <Boadresse formikBag={formikBag} />}
-					{!type && <MatrikkelAdresse formikBag={formikBag} />}
-					<FormikDatepicker name="tpsf.boadresse.flyttedato" label="Flyttedato" />
+					<RadioPanelGruppe
+						name="botype"
+						legend="Hva slags adresse vil du opprette?"
+						radios={[
+							{ label: 'Postnummer ...', value: 'postnr', id: 'postnr' },
+							{ label: 'Kommunenummer ...', value: 'kommunenr', id: 'kommunenr' },
+							{ label: 'Gateadresse detaljert ...', value: 'gate', id: 'gate' },
+							{ label: 'Matrikkeladresse detaljert ...', value: 'matrikkel', id: 'matrikkel' }
+						]}
+						checked={boType}
+						onChange={handleRadioChange}
+					/>
+
+					{['postnr', 'kommunenr'].includes(boType) && (
+						<AdresseNr formikBag={formikBag} type={boType} />
+					)}
+					{boType === 'gate' && <Boadresse formikBag={formikBag} />}
+					{boType === 'matrikkel' && <MatrikkelAdresse formikBag={formikBag} />}
 				</Vis>
-				<Vis attributt="tpsf.boadresse">
+
+				{/* <Vis attributt="tpsf.postadresse">
 					<span>postadresse komponent</span>
-				</Vis>
+                    <FormikDatepicker name="tpsf.boadresse.flyttedato" label="Flyttedato" />
+				</Vis> */}
 			</Panel>
 		</Vis>
 	)
