19683bf84b22630be4917c7c652a6ee7a7c7fb59
Kristoffer Mjelva <kristoffer.mjelva@nav.no>
2019-11-26 08:25:56 +0100

    Setter og NoArgs på request

--- inst-core/src/main/java/no/nav/registre/inst/provider/rs/requests/SyntetiserInstRequest.java
+++ inst-core/src/main/java/no/nav/registre/inst/provider/rs/requests/SyntetiserInstRequest.java
@@ -3,8 +3,12 @@ package no.nav.registre.inst.provider.rs.requests;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class SyntetiserInstRequest {
 
