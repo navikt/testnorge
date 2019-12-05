66a3df028f4b8ffed8a9b71ebe75a3bd080ecf00
Andreas Ludvigsen <andreas.ludvigsen@nav.no>
2019-12-05 15:50:24 +0100

    lagt til rowIcons til dollyTable

--- dolly-web-core/src/main/web_src/src/pages/gruppe/BestillingListe/BestillingListe.js
+++ dolly-web-core/src/main/web_src/src/pages/gruppe/BestillingListe/BestillingListe.js
@@ -62,6 +62,7 @@ export default function BestillingListe({ bestillinger, searchActive, isFetching
 		<DollyTable
 			data={sortedBestillinger}
 			columns={columns}
+			rowIcon="bestilling"
 			onExpand={bestilling => <BestillingDetaljer bestilling={bestilling} />}
 			pagination
 		/>
