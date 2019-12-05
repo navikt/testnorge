3e72661ab15ae7948b5a482258555aa583ecddd6
Kristoffer Mjelva <kristoffer.mjelva@nav.no>
2019-12-05 12:16:41 +0100

    Fjernet ubrukt import

--- src/main/java/no/nav/registre/inntekt/config/SwaggerConfig.java
+++ src/main/java/no/nav/registre/inntekt/config/SwaggerConfig.java
@@ -1,6 +1,5 @@
 package no.nav.registre.inntekt.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
