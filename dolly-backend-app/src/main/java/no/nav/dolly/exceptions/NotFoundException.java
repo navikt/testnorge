d0b6938f782a0d49b868ab6aa0cfbc40dd0cc706
Kristoffer Mjelva <kristoffer.mjelva@nav.no>
2019-12-06 12:37:14 +0100

    Start jenkins

--- dolly-backend-app/src/main/java/no/nav/dolly/exceptions/NotFoundException.java
+++ dolly-backend-app/src/main/java/no/nav/dolly/exceptions/NotFoundException.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.OK)
 public class NotFoundException extends RuntimeException {
+
     public NotFoundException(String message) {
         super(message);
     }
