a85a1f3edb54eea1734d24d4c2f8145d6e71b115
Andreas Ludvigsen <andreas.ludvigsen@nav.no>
2019-12-09 15:43:04 +0100

    flyttet identhistorikk til eget panel

---
+++ dolly-web-core/src/main/web_src/src/components/fagsystem/tpsf/form/Identhistorikk.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import Panel from '~/components/ui/panel/Panel'
+import { FieldArray } from 'formik'
+import { Vis } from '~/components/bestillingsveileder/VisAttributt'
+import { panelError } from '~/components/ui/form/formUtils'
+import { Kategori } from '~/components/ui/form/kategori/Kategori'
+import { FormikDatepicker } from '~/components/ui/form/inputs/datepicker/Datepicker'
+import { FormikSelect } from '~/components/ui/form/inputs/select/Select'
+import { FieldArrayAddButton, FieldArrayRemoveButton } from '~/components/ui/form/formUtils'
+import { SelectOptionsManager as Options } from '~/service/SelectOptions'
+
+const initialValues = {
+	foedtEtter: null,
+	foedtFoer: null,
+	identtype: null,
+	kjonn: null,
+	regdato: null
+}
+
+const hjelpetekst =
+	'Dette er en oversikt over utgåtte identer, der de eldste skal ligge sist. ' +
+	'Dagens identtype legges inn på forrige side. For å velge dagens kjønn må "Kjønn" hukes av på forrige side og velges under "Diverse" -> "Kjønn" her. ' +
+	'Eksempel: En person med DNR får FNR. Da velges FNR på forrige side. DNR legges inn i denne oversikten. ' +
+	'Hvis fødselsdatoen ble endret i overgangen kan født før og født etter fylles ut. Det samme gjelder for kjønn. ' +
+	'Dersom de står som "Ikke spesifisert" beholdes samme fødselsdato og/eller kjønn.'
+
+export const Identhistorikk = ({ formikBag }) => (
+	<Vis attributt="tpsf.identHistorikk">
+		<Panel
+			heading="Identhistorikk"
+			hasErrors={panelError(formikBag)}
+			informasjonstekst={hjelpetekst}
+			startOpen
+		>
+			<FieldArray name="tpsf.identHistorikk">
+				{arrayHelpers => (
+					<React.Fragment>
+						{formikBag.values.tpsf.identHistorikk.map((curr, idx) => (
+							<div key={idx} style={{ display: 'flex', flexWrap: 'wrap' }}>
+								<FormikSelect
+									name={`tpsf.identHistorikk.${idx}.identtype`}
+									label="Identtype"
+									options={Options('identtype')}
+								/>
+								<FormikSelect
+									name={`tpsf.identHistorikk.${idx}.kjonn`}
+									label="Kjønn"
+									kodeverk="Kjønnstyper"
+								/>
+								<FormikDatepicker name={`tpsf.identHistorikk.${idx}.regdato`} label="Utgått dato" />
+								<FormikDatepicker
+									name={`tpsf.identHistorikk.${idx}.foedtEtter`}
+									label="Født etter"
+								/>
+								<FormikDatepicker name={`tpsf.identHistorikk.${idx}.foedtFoer`} label="Født før" />
+								<FieldArrayRemoveButton onClick={e => arrayHelpers.remove(idx)} />
+							</div>
+						))}
+
+						<FieldArrayAddButton
+							title="Identhistorikk"
+							onClick={e => arrayHelpers.push(initialValues)}
+						/>
+					</React.Fragment>
+				)}
+			</FieldArray>
+		</Panel>
+	</Vis>
+)
