46efc0a1951cbe7870d156689d481205969be558
Andreas Ludvigsen <andreas.ludvigsen@nav.no>
2019-12-09 15:02:04 +0100

    Lagt tilbake hjelpetekster på attributtvelger

--- dolly-web-core/src/main/web_src/src/components/bestillingsveileder/steg/steg1/paneler/KontaktReservasjon.js
+++ dolly-web-core/src/main/web_src/src/components/bestillingsveileder/steg/steg1/paneler/KontaktReservasjon.js
@@ -8,9 +8,13 @@ import {
 export const KontaktReservasjonsPanel = ({ stateModifier }) => {
 	const sm = stateModifier(KontaktReservasjonsPanel.initialValues)
 
+	const infoTekst =
+		'KRR - benyttes for offentlige virksomheter for å avklare om den enkelte bruker har reservert seg mot digital kommunikasjon eller ikke. I tillegg skal varslene som sendes til bruker benytte den kontaktinformasjonen som ligger i registeret. Dette kan enten være mobiltelefonnummer for utsendelse av sms, eller epostadresse for utsendelse av epost.'
+
 	return (
 		<Panel
 			heading={KontaktReservasjonsPanel.heading}
+			informasjonstekst={infoTekst}
 			startOpen
 			checkAttributeArray={sm.batchAdd}
 			uncheckAttributeArray={sm.batchRemove}
