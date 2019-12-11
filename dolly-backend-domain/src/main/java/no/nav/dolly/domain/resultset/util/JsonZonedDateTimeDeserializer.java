b8f00229b7cad66085ec9e0f6b9ac9555dae5679
Kristen Hærum <kristen.haerum@nav.no>
2019-12-11 14:21:41 +0100

    REG-6362 Oppdatering for Sonar

--- dolly-backend-domain/src/main/java/no/nav/dolly/domain/resultset/util/JsonZonedDateTimeDeserializer.java
+++ dolly-backend-domain/src/main/java/no/nav/dolly/domain/resultset/util/JsonZonedDateTimeDeserializer.java
@@ -7,14 +7,13 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 
 public class JsonZonedDateTimeDeserializer extends JsonDeserializer<ZonedDateTime> {
 
-    @Override public ZonedDateTime deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JsonProcessingException {
+    @Override public ZonedDateTime deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
 
-        return nonNull(jsonParser.getValueAsString()) ?  LocalDateTime.parse(jsonParser.getValueAsString()).atZone(ZoneId.systemDefault()) : null;
+        return nonNull(jsonParser.getValueAsString()) ? LocalDateTime.parse(jsonParser.getValueAsString()).atZone(ZoneId.systemDefault()) : null;
     }
 }
