a05ceb47cb0279aac25f8b8049d53e4a21c89be2
Traran <Betsy.Carina.Traran@nav.no>
2020-01-17 15:51:20 +0100

    REG-6768: Validation på oppholdsstatus

--- dolly-web-core/src/main/web_src/src/components/fagsystem/udistub/form/validation.js
+++ dolly-web-core/src/main/web_src/src/components/fagsystem/udistub/form/validation.js
@@ -23,6 +23,77 @@ const arbeidsadgang = Yup.object({
 	typeArbeidsadgang: Yup.string().nullable()
 })
 
+const oppholdSammeVilkaar = Yup.object({
+	oppholdSammeVilkaarPeriode: ifPresent(
+		'$udistub.oppholdStatus.oppholdSammeVilkaar.oppholdSammeVilkaarPeriode',
+		Yup.object({
+			fra: Yup.date().nullable(),
+			til: Yup.date().nullable()
+		})
+	),
+	oppholdSammeVilkaarEffektuering: ifPresent(
+		'$udistub.oppholdStatus.oppholdSammeVilkaar.oppholdSammeVilkaarEffektuering',
+		Yup.date().nullable()
+	),
+	oppholdstillatelseVedtaksDato: ifPresent(
+		'$udistub.oppholdStatus.oppholdSammeVilkaar.oppholdstillatelseVedtaksDato',
+		Yup.date().nullable()
+	),
+	oppholdstillatelseType: ifPresent(
+		'$udistub.oppholdStatus.oppholdSammeVilkaar.oppholdstillatelseType',
+		requiredString
+	)
+})
+
+const oppholdStatus = Yup.object({
+	eosEllerEFTABeslutningOmOppholdsrettPeriode: ifPresent(
+		'$udistub.oppholdStatus.eosEllerEFTABeslutningOmOppholdsrettPeriode',
+		Yup.object({
+			fra: Yup.date().nullable(),
+			til: Yup.date().nullable()
+		})
+	),
+	eosEllerEFTABeslutningOmOppholdsrettEffektuering: ifPresent(
+		'$udistub.oppholdStatus.eosEllerEFTABeslutningOmOppholdsrettEffektuering',
+		Yup.date().nullable()
+	),
+	eosEllerEFTABeslutningOmOppholdsrett: ifPresent(
+		'$udistub.oppholdStatus.eosEllerEFTABeslutningOmOppholdsrett',
+		requiredString
+	),
+	eosEllerEFTAVedtakOmVarigOppholdsrettPeriode: ifPresent(
+		'$udistub.oppholdStatus.eosEllerEFTAVedtakOmVarigOppholdsrettPeriode',
+		Yup.object({
+			fra: Yup.date().nullable(),
+			til: Yup.date().nullable()
+		})
+	),
+	eosEllerEFTAVedtakOmVarigOppholdsrettEffektuering: ifPresent(
+		'$udistub.oppholdStatus.eosEllerEFTAVedtakOmVarigOppholdsrettEffektuering',
+		Yup.date().nullable()
+	),
+	eosEllerEFTAVedtakOmVarigOppholdsrett: ifPresent(
+		'$udistub.oppholdStatus.eosEllerEFTAVedtakOmVarigOppholdsrett',
+		requiredString
+	),
+	eosEllerEFTAOppholdstillatelsePeriode: ifPresent(
+		'$udistub.oppholdStatus.eosEllerEFTAOppholdstillatelsePeriode',
+		Yup.object({
+			fra: Yup.date().nullable(),
+			til: Yup.date().nullable()
+		})
+	),
+	eosEllerEFTAOppholdstillatelseEffektuering: ifPresent(
+		'$udistub.oppholdStatus.eosEllerEFTAOppholdstillatelseEffektuering',
+		Yup.date().nullable()
+	),
+	eosEllerEFTAOppholdstillatelse: ifPresent(
+		'$udistub.oppholdStatus.eosEllerEFTAOppholdstillatelse',
+		requiredString
+	),
+	oppholdSammeVilkaar: ifPresent('$udistub.oppholdStatus.oppholdSammeVilkaar', oppholdSammeVilkaar)
+})
+
 export const validation = {
 	udistub: ifPresent(
 		'$udistub',
@@ -30,7 +101,7 @@ export const validation = {
 			aliaser: ifPresent('$udistub.aliaser', aliaser),
 			arbeidsadgang: ifPresent('$udistub.arbeidsadgang', arbeidsadgang),
 			flyktning: ifPresent('$udistub.flyktning', requiredBoolean).nullable(),
-			oppholdStatus: Yup.object({}).nullable(),
+			oppholdStatus: ifPresent('$udistub.oppholdStatus', oppholdStatus),
 			soeknadOmBeskyttelseUnderBehandling: ifPresent(
 				'$udistub.soeknadOmBeskyttelseUnderBehandling',
 				requiredString
