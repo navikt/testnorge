a30017f5d6f35256d0b822abd3e5ad02cd059cba
Kristen Hærum <kristen.haerum@nav.no>
2020-01-20 14:47:50 +0100

    REG-5940 Lagt tilbake kvpath + attributter i appstarter

--- dolly-backend-app-nais/src/main/java/no/nav/dolly/ApplicationStarter.java
+++ dolly-backend-app-nais/src/main/java/no/nav/dolly/ApplicationStarter.java
@@ -1,14 +1,24 @@
 package no.nav.dolly;
 
+import java.util.Map;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import no.nav.fregpropertyreader.PropertyReader;
 
 public class ApplicationStarter extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
 
+        Map<String, Object> properties = PropertyReader.builder()
+                .readSecret("spring.datasource.username", "/var/run/secrets/nais.io/db/username")
+                .readSecret("spring.datasource.password", "/var/run/secrets/nais.io/db/password")
+                .readSecret("spring.datasource.url", "/var/run/secrets/nais.io/dbPath/jdbc_url")
+                .build();
+
         new SpringApplicationBuilder()
                 .sources(ApplicationConfig.class)
+                .properties(properties)
                 .run(args);
     }
 }
