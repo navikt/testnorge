05963fc2b4d6cf0afa65528e283bf2f1ff92f280
Kristen Hærum <kristen.haerum@nav.no>
2020-01-22 10:57:41 +0100

    REG-5940 Oppdatert deploy konfig VIII

--- dolly-backend-app-nais/src/main/java/no/nav/dolly/ApplicationStarter.java
+++ dolly-backend-app-nais/src/main/java/no/nav/dolly/ApplicationStarter.java
@@ -11,9 +11,9 @@ public class ApplicationStarter extends SpringBootServletInitializer {
     public static void main(String[] args) {
 
         Map<String, Object> properties = PropertyReader.builder()
-                .readSecret("DOLLY_DB_USERNAME", "/var/run/secrets/nais.io/db/username")
-                .readSecret("DOLLY_DB_PASSWORD", "/var/run/secrets/nais.io/db/password")
-                .readSecret("DOLLY_DB_URL", "/var/run/secrets/nais.io/dbPath/jdbc_url")
+                .readSecret("SPRING_DATASOURCE_USERNAME", "/var/run/secrets/nais.io/db/username")
+                .readSecret("SPRING_DATASOURCE_PASSWORD", "/var/run/secrets/nais.io/db/password")
+                .readSecret("SPRING_DATASOURCE_URL", "/var/run/secrets/nais.io/dbPath/jdbc_url")
                 .build();
 
         new SpringApplicationBuilder()
