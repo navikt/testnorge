b4b6b99052bcc889a8508c7392023c10af7745f6
Andreas Ludvigsen <andreas.ludvigsen@nav.no>
2020-01-22 12:45:15 +0100

    Bugfix slik at maler ikke bryter ved validering av data

--- dolly-web-core/src/main/web_src/src/utils/YupValidations.js
+++ dolly-web-core/src/main/web_src/src/utils/YupValidations.js
@@ -1,5 +1,7 @@
 import * as Yup from 'yup'
 import _isUndefined from 'lodash/isUndefined'
+import _isString from 'lodash/isString'
+import Formatters from '~/utils/DataFormatter'
 import { isDate } from 'date-fns'
 
 /**
@@ -9,7 +11,12 @@ export const messages = {
 	required: 'Feltet er påkrevd'
 }
 
-export const requiredDate = Yup.mixed().test('reqDate', messages.required, value => isDate(value))
+// Dato som kommer fra Maler er av typen String
+export const requiredDate = Yup.mixed().test('reqDate', messages.required, value => {
+	let _value = value
+	if (_isString(value)) _value = Formatters.parseDate(_value)
+	return isDate(_value)
+})
 export const requiredString = Yup.string().required(messages.required)
 export const requiredBoolean = Yup.boolean().required(messages.required)
 export const requiredNumber = Yup.number().required(messages.required)
