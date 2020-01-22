89812e90c9bc1a7ba1fa64702dd3b87aee23ad2d
Kristen Hærum <kristen.haerum@nav.no>
2020-01-22 09:22:05 +0100

    REG-5940 Lagt til driver class II

--- dolly-backend-app-nais/src/main/java/no/nav/dolly/ApplicationStarter.java
+++ dolly-backend-app-nais/src/main/java/no/nav/dolly/ApplicationStarter.java
@@ -11,9 +11,9 @@ public class ApplicationStarter extends SpringBootServletInitializer {
     public static void main(String[] args) {
 
         Map<String, Object> properties = PropertyReader.builder()
-                .readSecret("DOLLY_DB_USERNAME", "/var/run/secrets/nais.io/db/username")
-                .readSecret("DOLLY_DB_PASSWORD", "/var/run/secrets/nais.io/db/password")
-                .readSecret("DOLLY_DB_URL", "/var/run/secrets/nais.io/dbPath/jdbc_url")
+                .readSecret("spring.datasource.username", "/var/run/secrets/nais.io/db/username")
+                .readSecret("spring.datasource.password", "/var/run/secrets/nais.io/db/password")
+                .readSecret("spring.datasource.url", "/var/run/secrets/nais.io/dbPath/jdbc_url")
                 .build();
 
         new SpringApplicationBuilder()
