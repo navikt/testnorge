7c5a56346bd1d4a8f9c1b2bdab72cde4983810c9
Kristen Hærum <kristen.haerum@nav.no>
2020-01-23 11:51:38 +0100

    REG-5940 Oppdatert konfigurasjon

--- dolly-web-app-local/src/main/java/no/nav/dolly/web/LocalAppStarter.java
+++ dolly-web-app-local/src/main/java/no/nav/dolly/web/LocalAppStarter.java
@@ -1,16 +1,15 @@
 package no.nav.dolly.web;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Profile;
 
+@Profile("local")
 public class LocalAppStarter {
     public static void main(String... arguments) {
-        // todo find vault alternative
-        //        Map<String, Object> fasitProperties = resolveFasitProperties(arguments);
 
         new SpringApplicationBuilder()
                 .sources(ApplicationConfig.class)
                 .profiles("local")
-                //                .properties(fasitProperties)
                 .run(arguments);
     }
 }
