a25a4599370f7d64630777af44d9ac1e5188db15
Kristoffer Mjelva <kristoffer.mjelva@nav.no>
2020-01-28 09:35:49 +0100

    Fikset exposed internal representation i TForhold

--- tp-core/src/main/java/no/nav/registre/tp/database/models/TForhold.java
+++ tp-core/src/main/java/no/nav/registre/tp/database/models/TForhold.java
@@ -2,8 +2,9 @@ package no.nav.registre.tp.database.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -11,11 +12,14 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
+import java.sql.Date;
+import java.sql.Timestamp;
 
 @Entity(name = "T_FORHOLD")
-@Data
-@AllArgsConstructor
+@Getter
+@Setter
 @NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class TForhold {
 
@@ -25,11 +29,11 @@ public class TForhold {
     @Id
     private Integer forholdId;
     private Integer personId;
-    private java.sql.Date datoSamtykkeGitt;
+    private Date datoSamtykkeGitt;
     private String tssEksternIdFk;
-    private java.sql.Timestamp datoOpprettet;
+    private Timestamp datoOpprettet;
     private String opprettetAv;
-    private java.sql.Timestamp datoEndret;
+    private Timestamp datoEndret;
     private String harUtlandPensj;
     private String endretAv;
     @Column(name = "K_KILDE_TP_T")
@@ -40,7 +44,46 @@ public class TForhold {
     private String kSamtykkeSimT;
     private String harSimulering;
     private String funkForholdId;
-    private java.sql.Timestamp datoBrukFom;
-    private java.sql.Timestamp datoBrukTom;
+    private Timestamp datoBrukFom;
+    private Timestamp datoBrukTom;
 
+    public Date getDatoSamtykkeGitt() {
+        return datoSamtykkeGitt != null ? new Date(datoSamtykkeGitt.getTime()) : null;
+    }
+
+    public void setDatoSamtykkeGitt(Date datoSamtykkeGitt) {
+        this.datoSamtykkeGitt = datoSamtykkeGitt != null ? new Date(datoSamtykkeGitt.getTime()) : null;
+    }
+
+    public Timestamp getDatoOpprettet() {
+        return datoOpprettet != null ? new Timestamp(datoSamtykkeGitt.getTime()) : null;
+    }
+
+    public void setDatoOpprettet(Timestamp datoOpprettet) {
+        this.datoOpprettet = datoOpprettet != null ? new Timestamp(datoOpprettet.getTime()) : null;
+    }
+
+    public Timestamp getDatoEndret() {
+        return datoEndret != null ? new Timestamp(datoEndret.getTime()) : null;
+    }
+
+    public void setDatoEndret(Timestamp datoEndret) {
+        this.datoEndret = datoEndret != null ? new Timestamp(datoEndret.getTime()) : null;
+    }
+
+    public Timestamp getDatoBrukFom() {
+        return datoBrukFom != null ? new Timestamp(datoBrukFom.getTime()) : null;
+    }
+
+    public void setDatoBrukFom(Timestamp datoBrukFom) {
+        this.datoBrukFom = datoBrukFom != null ? new Timestamp(datoBrukFom.getTime()) : null;
+    }
+
+    public Timestamp getDatoBrukTom() {
+        return datoBrukTom != null ? new Timestamp(datoBrukTom.getTime()) : null;
+    }
+
+    public void setDatoBrukTom(Timestamp datoBrukTom) {
+        this.datoBrukTom = datoBrukTom != null ? new Timestamp(datoBrukTom.getTime()) : null;
+    }
 }
