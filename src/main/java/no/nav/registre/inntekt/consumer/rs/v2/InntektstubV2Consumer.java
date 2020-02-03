358547fffcd10b2263dcb8a35fe354710aa1b626
Kristoffer Mjelva <kristoffer.mjelva@nav.no>
2020-02-03 14:50:10 +0100

    REG-5996: La til test av inntektstubconsumer

--- src/main/java/no/nav/registre/inntekt/consumer/rs/v2/InntektstubV2Consumer.java
+++ src/main/java/no/nav/registre/inntekt/consumer/rs/v2/InntektstubV2Consumer.java
@@ -8,26 +8,15 @@ import static no.nav.tjenester.stub.aordningen.inntektsinformasjon.v2.inntekter.
 
 import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.http.config.Registry;
-import org.apache.http.config.RegistryBuilder;
-import org.apache.http.conn.socket.ConnectionSocketFactory;
-import org.apache.http.conn.socket.PlainConnectionSocketFactory;
-import org.apache.http.conn.ssl.NoopHostnameVerifier;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.RequestEntity;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriTemplate;
 
-import javax.net.ssl.SSLContext;
-import java.security.NoSuchAlgorithmException;
 import java.time.Month;
 import java.time.YearMonth;
 import java.util.ArrayList;
@@ -54,36 +43,13 @@ public class InntektstubV2Consumer {
     private final UriTemplate hentEksisterendeInntekterUrl;
 
     public InntektstubV2Consumer(
+            RestTemplateBuilder restTemplateBuilder,
             @Value("${inntektstub-t4.rest.api.url}") String inntektstubUrl
     ) {
-        this.restTemplate = new RestTemplate(getHttpRequestFactory());
+        this.restTemplate = restTemplateBuilder.build();
         this.leggTilInntektUrl = new UriTemplate(inntektstubUrl + "/v2/inntektsinformasjon?valider-inntektskombinasjoner=true&valider-kodeverk=true");
         this.hentEksisterendeIdenterUrl = new UriTemplate(inntektstubUrl + "/v2/personer");
         this.hentEksisterendeInntekterUrl = new UriTemplate(inntektstubUrl + "/v2/inntektsinformasjon?historikk=false&norske-identer={identer}");
-    }
-
-    private final HttpComponentsClientHttpRequestFactory getHttpRequestFactory() {
-        SSLConnectionSocketFactory sslsf;
-        try {
-            sslsf = new SSLConnectionSocketFactory(SSLContext.getDefault(), NoopHostnameVerifier.INSTANCE);
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        }
-
-        Registry<ConnectionSocketFactory> registry = RegistryBuilder.<ConnectionSocketFactory>create()
-                .register("http", new PlainConnectionSocketFactory())
-                .register("https", sslsf)
-                .build();
-
-        PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager(registry);
-        cm.setMaxTotal(100);
-
-        CloseableHttpClient httpClient = HttpClients.custom()
-                .setSSLSocketFactory(sslsf)
-                .setConnectionManager(cm)
-                .build();
-
-        return new HttpComponentsClientHttpRequestFactory(httpClient);
     }
 
     public List<String> hentEksisterendeIdenter() {
