4c850d11f5cdb94d78286985fcc27eb41c99d096
Kristen Hærum <kristen.haerum@nav.no>
2020-02-05 07:48:28 +0100

    REG-5715 Fikset problem med Wiremock og gjenbruk av testport

--- dolly-backend-app/src/test/java/no/nav/dolly/provider/api/config/ConfigGetTest.java
+++ dolly-backend-app/src/test/java/no/nav/dolly/provider/api/config/ConfigGetTest.java
@@ -4,12 +4,15 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.http.HttpMethod.GET;
 
-import no.nav.dolly.domain.resultset.RsDollyProps;
-import no.nav.dolly.provider.RestTestBase;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.annotation.DirtiesContext;
 
+import no.nav.dolly.domain.resultset.RsDollyProps;
+import no.nav.dolly.provider.RestTestBase;
+
+@DirtiesContext
 @DisplayName("GET /api/v1/config")
 class ConfigGetTest extends RestTestBase {
 
