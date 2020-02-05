23fd1153f511a4ba229e50a5141078a6f1ea6bcd
Andreas Ludvigsen <andreas.ludvigsen@nav.no>
2020-02-05 10:32:56 +0100

    bugfix/REG-7008 Endret default URL

--- dolly-web-core/src/main/web_src/src/config.js
+++ dolly-web-core/src/main/web_src/src/config.js
@@ -8,7 +8,7 @@ const config = {
 		kodeverkUrl: 'https://kodeverk.nais.preprod.local',
 		krrStubUrl: 'https://krr-stub.nais.preprod.local',
 		sigrunStubUrl: 'https://sigrun-skd-stub.nais.preprod.local',
-		tpsfUrl: 'https://tps-forvalteren-u2.nais.preprod.local',
+		tpsfUrl: 'https://tps-forvalteren.nais.preprod.local',
 		udiStubUrl: 'https://udi-stub.nais.preprod.local'
 	},
 	systems: {
