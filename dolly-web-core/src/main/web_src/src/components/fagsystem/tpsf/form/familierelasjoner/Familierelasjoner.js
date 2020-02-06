d2291fe00973881ce322fd3595502556113ce976
Traran <Betsy.Carina.Traran@nav.no>
2020-02-06 09:23:28 +0100

    REG-7016: Rettet skrivefeil

--- dolly-web-core/src/main/web_src/src/components/fagsystem/tpsf/form/familierelasjoner/Familierelasjoner.js
+++ dolly-web-core/src/main/web_src/src/components/fagsystem/tpsf/form/familierelasjoner/Familierelasjoner.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import _get from 'lodash/has'
+import _get from 'lodash/get'
 import Panel from '~/components/ui/panel/Panel'
 import { Vis } from '~/components/bestillingsveileder/VisAttributt'
 import { panelError } from '~/components/ui/form/formUtils'
