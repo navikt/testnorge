8687f881c9d115a20ed569c33ce5b1aebc83344b
s152486 <ingvild.stoen@nav.no>
2020-02-11 14:51:40 +0100

    REG-7074 Sletter verdi mellom toggle

--- dolly-web-core/src/main/web_src/src/components/fagsystem/aareg/form/partials/orgnrForm.js
+++ dolly-web-core/src/main/web_src/src/components/fagsystem/aareg/form/partials/orgnrForm.js
@@ -5,39 +5,43 @@ import { FormikSelect } from '~/components/ui/form/inputs/select/Select'
 import { SelectOptionsManager as Options } from '~/service/SelectOptions'
 import { FormikTextInput } from '~/components/ui/form/inputs/textInput/TextInput'
 
-const inputValg = { velg: 'velg', skriv: 'skriv' }
+const inputValg = { fraListe: 'velg', skrivSelv: 'skriv' }
 const initial = (formikBag, path) => {
 	const orgnr = _get(formikBag.values, `${path}.arbeidsgiver.orgnummer`)
 	return !orgnr || Options('orgnummer').some(org => org.value === orgnr)
-		? inputValg.velg
-		: inputValg.skriv
+		? inputValg.fraListe
+		: inputValg.skrivSelv
 }
 
 export const OrgnrForm = ({ formikBag, path }) => {
 	const [inputType, setInputType] = useState(initial(formikBag, path))
 
-	const handleToggleChange = event => setInputType(event.target.value)
+	const handleToggleChange = event => {
+		setInputType(event.target.value)
+		console.log('path :', path)
+		formikBag.setFieldValue(`${path}.arbeidsgiver.orgnummer`, '')
+	}
 
 	return (
 		<div className="toggle--wrapper">
 			<ToggleGruppe onChange={handleToggleChange} name={path}>
 				<ToggleKnapp
-					key={inputValg.velg}
-					value={inputValg.velg}
-					checked={inputType === inputValg.velg}
+					key={inputValg.fraListe}
+					value={inputValg.fraListe}
+					checked={inputType === inputValg.fraListe}
 				>
 					Velg organisasjonsnummer
 				</ToggleKnapp>
 				<ToggleKnapp
-					key={inputValg.skriv}
-					value={inputValg.skriv}
-					checked={inputType === inputValg.skriv}
+					key={inputValg.skrivSelv}
+					value={inputValg.skrivSelv}
+					checked={inputType === inputValg.skrivSelv}
 				>
 					Skriv inn organisasjonsnummer
 				</ToggleKnapp>
 			</ToggleGruppe>
 
-			{inputType === inputValg.velg ? (
+			{inputType === inputValg.fraListe ? (
 				<FormikSelect
 					name={`${path}.arbeidsgiver.orgnummer`}
 					options={Options('orgnummer')}
