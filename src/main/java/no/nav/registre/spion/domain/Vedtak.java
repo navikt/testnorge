85c2d7f09dc182c2d68c3e7ee75dfb9bcba99b03
Hay <aileen.hay@nav.no>
2020-02-20 12:02:58 +0100

    Update vedtak

--- src/main/java/no/nav/registre/spion/domain/Vedtak.java
+++ src/main/java/no/nav/registre/spion/domain/Vedtak.java
@@ -1,6 +1,6 @@
 package no.nav.registre.spion.domain;
-import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.List;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -16,14 +16,13 @@ public class Vedtak {
     private String ytelse;
     private Hashtable<String, String> periode;
     private String fnr;
-    //    private String arbeidsforholdId;
     private String vnr;
     private String vedtaksstatus;
     private String fritakFraAGP;
     private int sykemeldingsgrad;
     private int refusjonsbelopPerPeriode;
     private int dagsats;
-    private String maksDato;
-    private String ferie;
+    private int dagerIgjenTilMaksDato;
+    private List<Hashtable<String, String>> ferie;
 
 }
--- src/main/java/no/nav/registre/spion/service/VedtakService.java
+++ src/main/java/no/nav/registre/spion/service/VedtakService.java
@@ -2,6 +2,7 @@ package no.nav.registre.spion.service;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Hashtable;
 import java.util.Random;
@@ -11,106 +12,84 @@ import no.nav.registre.spion.domain.SykemeldingInformation;
 import no.nav.registre.spion.domain.Vedtak;
 import org.apache.commons.math3.distribution.ParetoDistribution;
 
+
 @Service
 @Slf4j
 public class VedtakService {
 
     public Vedtak generateVedtak(SykemeldingInformation info){
 
-        int periodeDays = getPeriodeDays();
-        log.info("Periodedager: " + periodeDays);
+        int periodeLength = getPeriodeLength();
 
-        Hashtable<String, String>  periode = getPeriode(info.getStartDate(), periodeDays);
-        int refusjonsBelop = getRefusjonsBelop(periodeDays);
+        Hashtable<String, String>  periode = getPeriode(info.getStartDate(), periodeLength);
+        int refusjonsBelop = getRefusjonsBelop(periodeLength);
 
         return new Vedtak(
                 "SP",
                 periode,
                 info.getPatient().getFnr(),
-                getVnr(),
-                getVedtaksstatus(info),
-                getFritakFraAGP(),
+                "020202020",
+                getVedtaksstatus(),
+                Math.random() < 0.25 ? "Ja" : "Nei",
                 getSykemeldingsgrad(),
                 refusjonsBelop,
-                refusjonsBelop/periodeDays,
-                "",
-                "");
+                refusjonsBelop/periodeLength,
+                getMaksDato(periodeLength),
+                new ArrayList<Hashtable<String, String>>());
     }
 
-    private int getPeriodeDays(){
-//        Lengden på en periode kan trekkes fra en pareto-fordeling med minimumsverdi på 1 dag og forventningsverdi på 14 dager.
+    private int getPeriodeLength(){
+        // Lengden på en periode kan trekkes fra en pareto-fordeling med minimumsverdi på 1 dag og forventningsverdi(?) på 14 dager.
+        return -(int)(Math.log(new Random().nextDouble()) / (1.0 / 14.0)) + 1;  //exponential-fordeling - burde bytte til pareto når parameterene er bestemt
+}
 
-        ParetoDistribution par = new ParetoDistribution();
 
-        return (int)par.sample();
-    }
+    private Hashtable<String, String>  getPeriode(String startDate, int periodeLength){
 
-    private Hashtable<String, String>  getPeriode(String startDate, int periodeDays){
-//        Tiden mellom to etterfølgende perioder kan trekkes fra en uniform fordeling på 0-60 dager. Det bør også være en
-//        initielt tidsrom før første periode for å hindre at alle perioder starter samtidig. Lengden på en periode kan
-//        trekkes fra en pareto-fordeling med minimumsverdi på 1 dag og forventningsverdi på 14 dager.
-
-        //Specifying date format that matches the given date
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         Calendar c = Calendar.getInstance();
         try{
-            //Setting the date to the given date
             c.setTime(sdf.parse(startDate));
         }catch(ParseException e){
             e.printStackTrace();
         }
 
-        //Adding periode days
-        c.add(Calendar.DAY_OF_MONTH, periodeDays);
-        //Date after adding the days to the given date
+        c.add(Calendar.DAY_OF_MONTH, periodeLength);
         String endDate = sdf.format(c.getTime());
 
         Hashtable<String, String> periode = new Hashtable<String, String>();
-        periode.put("fraDato", startDate);
-        periode.put("tilDato", endDate);
+        periode.put("FOMDato", startDate);
+        periode.put("TOMDato", endDate);
         return periode;
     }
 
-    private String getVnr(){
-        return "020202020";
-    }
-
-    private String getVedtaksstatus(SykemeldingInformation info){
-//        Status kan trekkes fra en fordeling slik at vi i snitt får 90% innvilget; 9.9% avslått; 0.1% henlagt.
+    private String getVedtaksstatus(){
+        //Status kan trekkes fra en fordeling slik at vi i snitt får 90% innvilget; 9.9% avslått; 0.1% henlagt.
         double r = Math.random();
-        if (r<0.01){
-            return "Henlagt";
-        }else if(r<0.1){
-            return "Avslått";
-        }else{
-            return "Innvilget";
-        }
+        return r<0.01 ? "Henlagt": r<0.1 ? "Avslått" : "Innvilget";
     }
 
-    private String getFritakFraAGP(){
-        double r = Math.random();
-
-        if(r<0.5){
-            return "Ja";
-        }else{
-            return "Nei";
-        }
-    }
 
     private int getSykemeldingsgrad(){
-        double r = Math.random();
-        if(r>=0.5){
-            return 100;
-        }else{
-            return 20 + new Random().nextInt(31);
-        }
+        // Sykmeldingsgrad kan trekkes fra en uniform fordeling mellom 20 og 100. Eventuelt si at 50% av alle sykmeldingsgrader
+        // er på 100%, og så fordele uniformt mellom 20% og 90%.
+        return Math.random()<0.5 ? 100 : 20 + new Random().nextInt(31);
     }
 
-    private int getRefusjonsBelop(int periodedager){
-//        Refusjonsbeløpet kan trekkes fra en uniform fordeling mellom 200 og 2400 og så gange opp
-//        med antallet dager i perioden (dette blir ikke helt riktig da vi også kan ta med ikke-virkedager).
-        double r = Math.random();
-        return (200 + new Random().nextInt(2201))*periodedager;
+    private int getRefusjonsBelop(int periodeLength){
+        // Refusjonsbeløpet kan trekkes fra en uniform fordeling mellom 200 og 2400 og så gange opp
+        // med antallet dager i perioden (dette blir ikke helt riktig da vi også kan ta med ikke-virkedager).
+        return (200 + new Random().nextInt(2201))*periodeLength;
+    }
+
+    private int getMaksDato(int periodeLength){
+
+        ParetoDistribution dist = new ParetoDistribution();
+
+        int num_days = 248;
+        num_days -= Math.random()<0.5 ? periodeLength : (int)(periodeLength*dist.sample());
+
+        return num_days<0 ? 0 : num_days;
     }
 
 }
