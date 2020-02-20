999170e532a3072fadaf0f6434b9bb45d663c0c0
Kristen Hærum <kristen.haerum@nav.no>
2020-02-20 10:44:56 +0100

    REG-7205 Lagt til eksplisitt deserializer for ZonedDateTime

--- dolly-backend-app/src/main/java/no/nav/dolly/mapper/JsonMapperConfig.java
+++ dolly-backend-app/src/main/java/no/nav/dolly/mapper/JsonMapperConfig.java
@@ -5,6 +5,7 @@ import static org.apache.commons.lang3.StringUtils.isBlank;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,6 +18,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.ZonedDateTimeSerializer;
 
 @Configuration
 public class JsonMapperConfig {
@@ -32,9 +34,22 @@ public class JsonMapperConfig {
         simpleModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ISO_DATE_TIME));
         simpleModule.addDeserializer(LocalDate.class, new DollyLocalDateDeserializer());
         simpleModule.addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ISO_DATE));
+        simpleModule.addDeserializer(ZonedDateTime.class, new DollyZonedDateTimeDeserializer());
+        simpleModule.addSerializer(ZonedDateTime.class, new ZonedDateTimeSerializer(DateTimeFormatter.ISO_DATE_TIME));
 
         objectMapper.registerModule(simpleModule);
         return objectMapper;
+    }
+
+    private static class DollyZonedDateTimeDeserializer extends JsonDeserializer<ZonedDateTime> {
+
+        @Override public ZonedDateTime deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+            JsonNode node = jsonParser.getCodec().readTree(jsonParser);
+            if (isBlank(node.asText())) {
+                return null;
+            }
+            return ZonedDateTime.parse(node.asText(), DateTimeFormatter.ISO_DATE_TIME);
+        }
     }
 
     private static class DollyLocalDateDeserializer extends JsonDeserializer<LocalDate> {
