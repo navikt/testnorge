c88dbc18879810dc4922ec3d708201be9aeaf8dc
Kristen Hærum <kristen.haerum@nav.no>
2020-02-20 07:54:54 +0100

    REG-7205 Lagt til endringer slik at tester kjører

--- dolly-backend-app/src/main/java/no/nav/dolly/mapper/JsonMapperConfig.java
+++ dolly-backend-app/src/main/java/no/nav/dolly/mapper/JsonMapperConfig.java
@@ -1,8 +1,11 @@
 package no.nav.dolly.mapper;
 
+import static org.apache.commons.lang3.StringUtils.isBlank;
+
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import com.fasterxml.jackson.core.JsonParser;
@@ -12,6 +15,8 @@ import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 
 @Configuration
 public class JsonMapperConfig {
@@ -24,15 +29,33 @@ public class JsonMapperConfig {
 
         SimpleModule simpleModule = new SimpleModule();
         simpleModule.addDeserializer(LocalDateTime.class, new DollyLocalDateTimeDeserializer());
+        simpleModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ISO_DATE_TIME));
+        simpleModule.addDeserializer(LocalDate.class, new DollyLocalDateDeserializer());
+        simpleModule.addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ISO_DATE));
 
         objectMapper.registerModule(simpleModule);
         return objectMapper;
+    }
+
+    private static class DollyLocalDateDeserializer extends JsonDeserializer<LocalDate> {
+
+        @Override public LocalDate deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+            JsonNode node = jsonParser.getCodec().readTree(jsonParser);
+            if (isBlank(node.asText())) {
+                return null;
+            }
+            String dateTime = node.asText().length() > 10 ? node.asText().substring(0, 10) : node.asText();
+            return LocalDate.parse(dateTime);
+        }
     }
 
     private static class DollyLocalDateTimeDeserializer extends JsonDeserializer<LocalDateTime> {
 
         @Override public LocalDateTime deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
             JsonNode node = jsonParser.getCodec().readTree(jsonParser);
+            if (isBlank(node.asText())) {
+                return null;
+            }
             String dateTime = node.asText().length() > 19 ? node.asText().substring(0, 19) : node.asText();
             return dateTime.length() > 10 ? LocalDateTime.parse(dateTime) : LocalDate.parse(dateTime).atStartOfDay();
         }
--- dolly-backend-domain/src/main/java/no/nav/dolly/domain/resultset/RsDollyProps.java
+++ dolly-backend-domain/src/main/java/no/nav/dolly/domain/resultset/RsDollyProps.java
@@ -1,12 +1,16 @@
 package no.nav.dolly.domain.resultset;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Builder
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class RsDollyProps {
 
     private String tpsfUrl;
