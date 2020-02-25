b05d030865ca3733ea6f709fe4f193388adbc4f6
Hay <Aileen.Hay@nav.no>
2020-02-25 07:57:08 +0100

    REG-7113 Oppdatert validering av fom

--- dolly-web-core/src/main/web_src/src/components/fagsystem/popp/form/validation.js
+++ dolly-web-core/src/main/web_src/src/components/fagsystem/popp/form/validation.js
@@ -3,11 +3,13 @@ import _get from 'lodash/get'
 import _isNil from 'lodash/isNil'
 import { requiredNumber, ifPresent} from '~/utils/YupValidations'
 
-const innenforInntektsdatoerTest = (validation) =>{
-    const errorMsg = 'Tast inn et gyldig år.'
+const innenforInntektsperiodeTest = (validation, validateFomBasedOnAge) =>{
+    const errorMsg = 'T.o.m. dato kan ikke være før f.o.m. dato.'
+    const errorMsgAge =
+        'F.o.m. dato må være før t.o.m. dato, og kan tidligst være året personen fyller 18.'
     return validation.test(
         'range',
-        errorMsg,
+        validateFomBasedOnAge ? errorMsgAge : errorMsg,
         function isWithinTest(val) {
             if (!val) return true
 
@@ -16,9 +18,36 @@ const innenforInntektsdatoerTest = (validation) =>{
             const values = this.options.context
             const arrayPos = path.split('.')[0] // feks: popp[1]
 
+            if(validateFomBasedOnAge){
+                const inntektFom = _get(values, `${arrayPos}.inntektsperiode.fom`)
+
+                const alder = _get(values, 'tpsf.alder')
+                const foedtFoer = _get(values, 'tpsf.foedtFoer')
+
+
+                if(!_isNil(alder)){
+                    if(new Date().getFullYear() - alder + 18 > inntektFom ){
+                        return false
+                    }
+                }else if(!_isNil(foedtFoer)){
+                    let day = foedtFoer.getDate()
+                    let month = foedtFoer.getMonth()
+                    let year = foedtFoer.getFullYear()
+
+                    year = (day==1 && month==0) ?  year - 1 : year
+
+                    if(year + 18 > inntektFom){
+                        return false
+                    }
+                }else{
+                    if(new Date().getFullYear() - 12 > inntektFom){
+                        return false
+                    }
+                }
+            }
+
             const inntektFom = _get(values, `${arrayPos}.inntektsperiode.fom`)
             const inntektTom = _get(values, `${arrayPos}.inntektsperiode.tom`)
-
 
             return dateValue >= inntektFom && dateValue <= (_isNil(inntektTom) ? new Date().getFullYear() : inntektTom)
         }
@@ -31,8 +60,8 @@ export const validation  = {
         Yup.array().of(
             Yup.object({
                 inntektsperiode: Yup.object({
-                    fom: innenforInntektsdatoerTest(requiredNumber),
-                    tom: Yup.number().nullable()
+                    fom: innenforInntektsperiodeTest(requiredNumber, true),
+                    tom: innenforInntektsperiodeTest(Yup.number().nullable())
                 }),
                 beloep: Yup.number()
                     .min(0, 'Tast inn et gyldig beløp')
