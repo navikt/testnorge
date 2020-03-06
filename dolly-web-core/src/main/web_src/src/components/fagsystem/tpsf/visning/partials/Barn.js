29fc6def3409964e4c7d48924eb1f023bc477d44
s152486 <ingvild.stoen@nav.no>
2020-03-06 13:34:29 +0100

    REG-7519 Lagt til postadresse på barn

--- dolly-web-core/src/main/web_src/src/components/fagsystem/tpsf/visning/partials/Barn.js
+++ dolly-web-core/src/main/web_src/src/components/fagsystem/tpsf/visning/partials/Barn.js
@@ -3,6 +3,7 @@ import { TitleValue } from '~/components/ui/titleValue/TitleValue'
 import Formatters from '~/utils/DataFormatter'
 import { TpsfApi } from '~/service/Api'
 import { Adressevisning } from './Boadresse'
+import { Postadresse } from './Postadresse'
 import { Historikk } from '~/components/ui/historikk/Historikk'
 
 export const Barn = ({ data, type }) => {
@@ -19,25 +20,32 @@ export const Barn = ({ data, type }) => {
 		}
 		fetchData()
 	}, [])
-
+	console.log('data :', data)
 	return (
-		<div className="person-visning_content">
-			<TitleValue title={data.identtype} value={data.ident} />
-			<TitleValue title="Fornavn" value={data.fornavn} />
-			<TitleValue title="Mellomnavn" value={data.mellomnavn} />
-			<TitleValue title="Etternavn" value={data.etternavn} />
-			<TitleValue title="Kjønn" value={Formatters.kjonn(data.kjonn, data.alder)} />
-			<TitleValue title="Alder" value={Formatters.formatAlder(data.alder, data.doedsdato)} />
-			<TitleValue title="Diskresjonskode" value={Formatters.showLabel(data.spesreg)} />
-			<TitleValue title="Uten fast bopel" value={data.utenFastBopel && 'Ja'} />
-			{barnInfo && !isLoading && (
-				<TitleValue title="Foreldre" value={finnForeldre(barnInfo[0].relasjoner).join(', ')} />
-			)}
-			<TitleValue title="Er adoptert" value={Formatters.oversettBoolean(type === 'BARN')} />
-			{!data.utenFastBopel && (
-				<Historikk component={Adressevisning} propName="boadresse" data={data.boadresse} />
-			)}
-		</div>
+		<>
+			<div className="person-visning_content">
+				<TitleValue title={data.identtype} value={data.ident} />
+				<TitleValue title="Fornavn" value={data.fornavn} />
+				<TitleValue title="Mellomnavn" value={data.mellomnavn} />
+				<TitleValue title="Etternavn" value={data.etternavn} />
+				<TitleValue title="Kjønn" value={Formatters.kjonn(data.kjonn, data.alder)} />
+				<TitleValue title="Alder" value={Formatters.formatAlder(data.alder, data.doedsdato)} />
+				<TitleValue title="Diskresjonskode" value={Formatters.showLabel(data.spesreg)} />
+				<TitleValue title="Uten fast bopel" value={data.utenFastBopel && 'Ja'} />
+				{barnInfo && !isLoading && (
+					<TitleValue title="Foreldre" value={finnForeldre(barnInfo[0].relasjoner).join(', ')} />
+				)}
+				<TitleValue title="Er adoptert" value={Formatters.oversettBoolean(type === 'BARN')} />
+			</div>
+			<div style={{ width: '100%' }}>
+				{!data.utenFastBopel &&
+					(data.boadresse.length > 0 ? (
+						<Historikk component={Adressevisning} propName="boadresse" data={data.boadresse} />
+					) : (
+						data.postadresse && <Postadresse postadresse={data.postadresse} />
+					))}
+			</div>
+		</>
 	)
 }
 
