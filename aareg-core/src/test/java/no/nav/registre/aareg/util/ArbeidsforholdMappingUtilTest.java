b82f7b49839e1048abba1dc736153c87439110b1
Kristoffer Mjelva <kristoffer.mjelva@nav.no>
2020-03-19 14:04:02 +0100

    REG-7553: La til test av mapping

--- aareg-core/src/test/java/no/nav/registre/aareg/util/ArbeidsforholdMappingUtilTest.java
+++ aareg-core/src/test/java/no/nav/registre/aareg/util/ArbeidsforholdMappingUtilTest.java
@@ -1,6 +1,7 @@
 package no.nav.registre.aareg.util;
 
 import static no.nav.registre.aareg.testutils.ResourceUtils.getResourceFileContent;
+import static no.nav.registre.aareg.util.ArbeidsforholdMappingUtil.mapArbeidsforholdToRsArbeidsforhold;
 import static no.nav.registre.aareg.util.ArbeidsforholdMappingUtil.mapToArbeidsforhold;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -12,9 +13,21 @@ import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.io.IOException;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Collections;
 
+import no.nav.registre.aareg.domain.RsOrganisasjon;
+import no.nav.tjenester.aordningen.arbeidsforhold.v1.Ansettelsesperiode;
+import no.nav.tjenester.aordningen.arbeidsforhold.v1.AntallTimerForTimeloennet;
+import no.nav.tjenester.aordningen.arbeidsforhold.v1.Arbeidsavtale;
+import no.nav.tjenester.aordningen.arbeidsforhold.v1.Arbeidsforhold;
 import no.nav.tjenester.aordningen.arbeidsforhold.v1.Organisasjon;
+import no.nav.tjenester.aordningen.arbeidsforhold.v1.Periode;
+import no.nav.tjenester.aordningen.arbeidsforhold.v1.PermisjonPermittering;
+import no.nav.tjenester.aordningen.arbeidsforhold.v1.Person;
+import no.nav.tjenester.aordningen.arbeidsforhold.v1.Sporingsinformasjon;
+import no.nav.tjenester.aordningen.arbeidsforhold.v1.Utenlandsopphold;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ArbeidsforholdMappingUtilTest {
@@ -40,6 +53,74 @@ public class ArbeidsforholdMappingUtilTest {
 
     @Test
     public void shouldMapArbeidsforholdToRsArbeidsforhold() {
+        var arbeidsforhold = buildArbeidsforhjold();
 
+        var rsArbeidsforhold = mapArbeidsforholdToRsArbeidsforhold(arbeidsforhold);
+
+        assertThat(rsArbeidsforhold.getArbeidsforholdIDnav(), equalTo(arbeidsforhold.getNavArbeidsforholdId()));
+        assertThat(rsArbeidsforhold.getArbeidsforholdID(), equalTo(arbeidsforhold.getArbeidsforholdId()));
+        assertThat(rsArbeidsforhold.getArbeidstaker().getIdent(), equalTo(arbeidsforhold.getArbeidstaker().getOffentligIdent()));
+        assertThat(((RsOrganisasjon) rsArbeidsforhold.getArbeidsgiver()).getOrgnummer(), equalTo(((Organisasjon) arbeidsforhold.getArbeidsgiver()).getOrganisasjonsnummer()));
+        assertThat(rsArbeidsforhold.getArbeidsforholdstype(), equalTo(arbeidsforhold.getType()));
+        assertThat(rsArbeidsforhold.getAnsettelsesPeriode().getFom(), equalTo(arbeidsforhold.getAnsettelsesperiode().getPeriode().getFom().atStartOfDay()));
+        assertThat(rsArbeidsforhold.getArbeidsavtale().getYrke(), equalTo(arbeidsforhold.getArbeidsavtaler().get(0).getYrke()));
+        assertThat(rsArbeidsforhold.getArbeidsavtale().getAvtaltArbeidstimerPerUke(), equalTo(arbeidsforhold.getArbeidsavtaler().get(0).getAntallTimerPrUke()));
+        assertThat(rsArbeidsforhold.getArbeidsavtale().getStillingsprosent(), equalTo(arbeidsforhold.getArbeidsavtaler().get(0).getStillingsprosent()));
+        assertThat(rsArbeidsforhold.getPermisjon().get(0).getPermisjonsprosent(), equalTo(arbeidsforhold.getPermisjonPermitteringer().get(0).getProsent()));
+        assertThat(rsArbeidsforhold.getPermisjon().get(0).getPermisjonsPeriode().getFom(), equalTo(arbeidsforhold.getPermisjonPermitteringer().get(0).getPeriode().getFom().atStartOfDay()));
+        assertThat(rsArbeidsforhold.getAntallTimerForTimeloennet().get(0).getAntallTimer(), equalTo(arbeidsforhold.getAntallTimerForTimeloennet().get(0).getAntallTimer()));
+        assertThat(rsArbeidsforhold.getAntallTimerForTimeloennet().get(0).getPeriode().getFom(), equalTo(arbeidsforhold.getAntallTimerForTimeloennet().get(0).getPeriode().getFom().atStartOfDay()));
+        assertThat(rsArbeidsforhold.getUtenlandsopphold().get(0).getLand(), equalTo(arbeidsforhold.getUtenlandsopphold().get(0).getLandkode()));
+        assertThat(rsArbeidsforhold.getUtenlandsopphold().get(0).getPeriode().getFom(), equalTo(arbeidsforhold.getUtenlandsopphold().get(0).getPeriode().getFom().atStartOfDay()));
+    }
+
+    private Arbeidsforhold buildArbeidsforhjold() {
+        return Arbeidsforhold.builder()
+                .navArbeidsforholdId(123L)
+                .arbeidsforholdId("234")
+                .arbeidstaker(Person.builder()
+                        .offentligIdent("01010101010")
+                        .build())
+                .arbeidsgiver(Organisasjon.builder()
+                        .organisasjonsnummer("123456789")
+                        .build())
+                .opplysningspliktig(Organisasjon.builder()
+                        .organisasjonsnummer("123456789")
+                        .build())
+                .type("type")
+                .ansettelsesperiode(Ansettelsesperiode.builder()
+                        .periode(Periode.builder()
+                                .fom(LocalDate.of(2020, 1, 1))
+                                .build())
+                        .build())
+                .arbeidsavtaler(Collections.singletonList(Arbeidsavtale.builder()
+                        .yrke("yrke")
+                        .antallTimerPrUke(40.0)
+                        .stillingsprosent(100.0)
+                        .sporingsinformasjon(Sporingsinformasjon.builder()
+                                .opprettetTidspunkt(LocalDateTime.now())
+                                .endretTidspunkt(LocalDateTime.now())
+                                .build())
+                        .build()))
+                .permisjonPermitteringer(Collections.singletonList(PermisjonPermittering.builder()
+                        .prosent(100.0)
+                        .periode(Periode.builder()
+                                .fom(LocalDate.of(2020, 4, 1))
+                                .build())
+                        .build()))
+                .antallTimerForTimeloennet(Collections.singletonList(AntallTimerForTimeloennet.builder()
+                        .antallTimer(20.0)
+                        .periode(Periode.builder()
+                                .fom(LocalDate.of(2020, 5, 1))
+                                .build())
+                        .build()))
+                .utenlandsopphold(Collections.singletonList(Utenlandsopphold.builder()
+                        .landkode("land")
+                        .periode(Periode.builder()
+                                .fom(LocalDate.of(2020, 6, 1))
+                                .build())
+                        .build()))
+                .innrapportertEtterAOrdningen(true)
+                .build();
     }
 }
