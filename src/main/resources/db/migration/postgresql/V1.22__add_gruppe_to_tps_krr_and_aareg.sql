e2f2b084f9aef6509a03d2af549b10ae44ea9971
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2020-03-19 11:26:40 +0100

    Endre gruppe_id fra varchar til bigint

--- src/main/resources/db/migration/postgresql/V1.22__add_gruppe_to_tps_krr_and_aareg.sql
+++ src/main/resources/db/migration/postgresql/V1.22__add_gruppe_to_tps_krr_and_aareg.sql
@@ -1,17 +1,17 @@
 alter table tps_identer
-    add column gruppe_id varchar(255);
+    add column gruppe_id bigint;
 
 alter table tps_identer
     add constraint tps_identer_gruppe_id_fkey foreign key (gruppe_id) references gruppe;
 
 alter table aareg
-    add column gruppe_id varchar(255);
+    add column gruppe_id bigint;
 
 alter table aareg
     add constraint aareg_gruppe_id_fkey foreign key (gruppe_id) references gruppe;
 
 alter table krr
-    add column gruppe_id varchar(255);
+    add column gruppe_id bigint;
 
 alter table krr
     add constraint krr_gruppe_id_fkey foreign key (gruppe_id) references gruppe;
