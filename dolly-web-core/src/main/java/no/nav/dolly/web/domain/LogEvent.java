20bc5e754b72429a61453bca3f880114c2f5a2ea
Langvatn <Martin.Gundersen.Langvatn@nav.no>
2020-03-20 17:28:25 +0100

    REG-7658: Nytt logg format

--- dolly-web-core/src/main/java/no/nav/dolly/web/domain/LogEvent.java
+++ dolly-web-core/src/main/java/no/nav/dolly/web/domain/LogEvent.java
@@ -2,6 +2,9 @@ package no.nav.dolly.web.domain;
 
 import lombok.Value;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import no.nav.dolly.web.provider.web.dto.LogEventDTO;
 
 @Value
@@ -11,19 +14,18 @@ public class LogEvent {
     private String message;
     private LogMetadata metadata;
 
-    public LogEvent(LogEventDTO dto, String userAgent){
+    public LogEvent(LogEventDTO dto, String userAgent) {
         metadata = new LogMetadata(userAgent);
         level = dto.getLevel();
         event = dto.getEvent();
         message = dto.getMessage();
     }
 
-    @Override
-    public String toString() {
-        StringBuilder builder = new StringBuilder().append("logged: [ event: \"").append(event).append("\"");
-        if(message != null) {
-            builder.append("message: \"").append(message).append("\"");
-        }
-        return builder.append("metadata: ").append(metadata).append(" ]").toString();
+    public Map<String, String> toPropertyMap() {
+        Map<String, String> properties = new HashMap<>();
+        properties.put("event", event);
+        properties.put("browser", metadata.getNameBrowser());
+        properties.put("user-agent", metadata.getUserAgent());
+        return properties;
     }
 }
--- dolly-web-core/src/main/java/no/nav/dolly/web/service/LogService.java
+++ dolly-web-core/src/main/java/no/nav/dolly/web/service/LogService.java
@@ -1,6 +1,7 @@
 package no.nav.dolly.web.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.stereotype.Service;
 
 import no.nav.dolly.web.domain.LogEvent;
@@ -9,19 +10,22 @@ import no.nav.dolly.web.domain.LogEvent;
 @Service
 public class LogService {
     public void log(LogEvent event){
+        MDC.clear();
+        MDC.setContextMap(event.toPropertyMap());
         switch (event.getLevel()){
             case INFO:
-                log.info(event.toString());
+                log.info(event.getMessage());
                 break;
             case WARNING:
-                log.warn(event.toString());
+                log.warn(event.getMessage());
                 break;
             case ERROR:
-                log.error(event.toString());
+                log.error(event.getMessage());
                 break;
             default:
-                log.debug(event.toString());
+                log.debug(event.getMessage());
                 break;
         }
+        MDC.clear();
     }
 }
