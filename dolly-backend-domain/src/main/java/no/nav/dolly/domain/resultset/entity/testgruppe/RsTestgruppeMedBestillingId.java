ffe6b84b3e3b1395aac63460f94d1ed3b26fcf2b
Kristen Hærum <kristen.haerum@nav.no>
2020-03-23 13:54:28 +0100

    REG-7670 Fikset tom identliste på gruppe

--- dolly-backend-domain/src/main/java/no/nav/dolly/domain/resultset/entity/testgruppe/RsTestgruppeMedBestillingId.java
+++ dolly-backend-domain/src/main/java/no/nav/dolly/domain/resultset/entity/testgruppe/RsTestgruppeMedBestillingId.java
@@ -1,6 +1,11 @@
 package no.nav.dolly.domain.resultset.entity.testgruppe;
 
+import static java.util.Objects.isNull;
+
+import java.util.ArrayList;
 import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -11,9 +16,18 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class RsTestgruppeMedBestillingId extends RsTestgruppe {
 
     private List<IdentBestilling> identer;
+
+    public List<IdentBestilling> getIdenter() {
+
+        if (isNull(identer)) {
+            identer = new ArrayList();
+        }
+        return identer;
+    }
 
     @Getter
     @Setter
