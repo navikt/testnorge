31a7af513fd0fc01cf2c60f214f7a6865432e327
K156575 <baard.krohg@gmail.com>
2020-03-26 11:50:53 +0100

    tester basic application/json bruk 2

--- src/main/java/no/nav/registre/inntektsmeldingstub/service/rs/RsTestObjekt.java
+++ src/main/java/no/nav/registre/inntektsmeldingstub/service/rs/RsTestObjekt.java
@@ -1,13 +1,18 @@
 package no.nav.registre.inntektsmeldingstub.service.rs;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class RsTestObjekt {
+    @JsonProperty
     private String melding;
+    @JsonProperty
     private Integer testTall;
 }
