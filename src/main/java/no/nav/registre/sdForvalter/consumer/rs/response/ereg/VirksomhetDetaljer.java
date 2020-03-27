b927a0966f8fbd2126e3d83817bde4794b0be84b
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2020-03-27 15:04:14 +0100

    Add No args constructor to VirksomhetDetaljer

--- src/main/java/no/nav/registre/sdForvalter/consumer/rs/response/ereg/VirksomhetDetaljer.java
+++ src/main/java/no/nav/registre/sdForvalter/consumer/rs/response/ereg/VirksomhetDetaljer.java
@@ -2,11 +2,13 @@ package no.nav.registre.sdForvalter.consumer.rs.response.ereg;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.NoArgsConstructor;
 import lombok.Value;
 
 import java.time.LocalDate;
 
 @Value
+@NoArgsConstructor(force = true)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class VirksomhetDetaljer {
     @JsonProperty
