d27b2dc9159bcb0fbe71a5f802de8becd2b52d94
Rayfuse <Sarah.Rayfuse@nav.no>
2020-03-27 10:13:24 +0100

    REG-7565

--- dolly-web-core/src/main/web_src/src/ducks/environments/index.js
+++ dolly-web-core/src/main/web_src/src/ducks/environments/index.js
@@ -37,17 +37,14 @@ export const _getEnvironmentsSortedByType = envArray => {
 
 	Object.keys(sortedByType).forEach(key => {
 		const envs = sortedByType[key]
-		const sortereNummer = envs.map(env => env.id.match(/.{1,1}/g))
-		const sorterteEnvs = sortereNummer.map(v => v[0] + v[1]).sort()
+		const sorterteNummer = envs.map(env => env.id.match(/.{1,1}/g))
+		const sorterteEnvs = sorterteNummer.map(v => v[0] + v[1]).sort()
 
-		{
-			sorterteEnvs.map((current, idx) => {
-				return (envs[idx].id = sorterteEnvs[idx])
-			})
-		}
-		sortedByType[key] = envs.sort((a, b) => {
-			return a.label.substring(1) - b.label.substring(1)
+		sorterteEnvs.map((current, idx) => {
+			return (envs[idx].id = sorterteEnvs[idx])
 		})
+
+		sorterteEnvs.map((current, idx) => (envs[idx].id = sorterteEnvs[idx]))
 	})
 	return sortedByType
 }
