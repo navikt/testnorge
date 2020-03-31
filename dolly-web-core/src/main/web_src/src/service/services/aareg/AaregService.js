636e4588dedbb742211ff0fb7113ba19a1f8978d
Hay <Aileen.Hay@nav.no>
2020-03-31 12:24:08 +0200

    REG-7753 endret format

--- dolly-web-core/src/main/web_src/src/service/services/aareg/AaregService.js
+++ dolly-web-core/src/main/web_src/src/service/services/aareg/AaregService.js
@@ -4,15 +4,15 @@ import Request from '~/service/services/Request'
 const getAaregUrl = () => `${config.services.proxyBackend}/aareg`
 
 const options = {
-    headers: {
-        'Nav-Call-Id': 'dolly-frontend',
-        'Nav-Consumer-Id': 'dolly'
-    }
+	headers: {
+		'Nav-Call-Id': 'dolly-frontend',
+		'Nav-Consumer-Id': 'dolly'
+	}
 }
 
 export default {
-    getArbeidsforhold(ident, miljoe) {
-        const endpoint = `${getAaregUrl()}/arbeidsforhold?ident=${ident}&miljoe=${miljoe}`
-        return Request.getWithoutCredentials(endpoint, options)
-    }
+	getArbeidsforhold(ident, miljoe) {
+		const endpoint = `${getAaregUrl()}/arbeidsforhold?ident=${ident}&miljoe=${miljoe}`
+		return Request.getWithoutCredentials(endpoint, options)
+	}
 }
--- dolly-web-core/src/main/web_src/src/service/services/pensjon/PensjonService.js
+++ dolly-web-core/src/main/web_src/src/service/services/pensjon/PensjonService.js
@@ -4,20 +4,20 @@ import Request from '~/service/services/Request'
 const getPensjonUrl = () => `${config.services.proxyBackend}/popp`
 
 const options = {
-    headers: {
-        'Nav-Call-Id': 'dolly-frontend',
-        'Nav-Consumer-Id': 'dolly'
-    }
+	headers: {
+		'Nav-Call-Id': 'dolly-frontend',
+		'Nav-Consumer-Id': 'dolly'
+	}
 }
 
 export default {
-    getPoppInntekt(ident, miljoe) {
-        const endpoint = `${getPensjonUrl()}/inntekt?fnr=${ident}&miljo=${miljoe}`
-        return Request.getWithoutCredentials(endpoint, options)
-    },
+	getPoppInntekt(ident, miljoe) {
+		const endpoint = `${getPensjonUrl()}/inntekt?fnr=${ident}&miljo=${miljoe}`
+		return Request.getWithoutCredentials(endpoint, options)
+	},
 
-    getTilgjengeligeMiljoer() {
-        const endpoint = `${getPensjonUrl()}/miljo`
-        return Request.getWithoutCredentials(endpoint, options)
-    }
+	getTilgjengeligeMiljoer() {
+		const endpoint = `${getPensjonUrl()}/miljo`
+		return Request.getWithoutCredentials(endpoint, options)
+	}
 }
