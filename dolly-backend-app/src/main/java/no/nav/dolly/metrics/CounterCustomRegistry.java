4e060f17b4088e5840e39b5057c69f8e40b47997
Kristen Hærum <kristen.haerum@nav.no>
2020-04-01 14:57:21 +0200

    REG-7729 Oppdatert innsending av counters

--- dolly-backend-app/src/main/java/no/nav/dolly/metrics/CounterCustomRegistry.java
+++ dolly-backend-app/src/main/java/no/nav/dolly/metrics/CounterCustomRegistry.java
@@ -19,68 +19,70 @@ public class CounterCustomRegistry {
 
     private final MeterRegistry registry;
 
-    public void invoke(String name, String[] tags) {
+    public void invoke(String name, List<String> tags) {
 
-        registry.counter(name, tags).increment();
+        tags.forEach(tag ->
+                registry.counter(name, "type", tag).increment()
+        );
     }
 
     public void invoke(String name, RsDollyUtvidetBestilling bestilling) {
 
-        List<String> labels = newArrayList();
+        List<String> tags = newArrayList();
 
         if (nonNull(bestilling.getTpsf())) {
-            addLabel(labels, nonNull(bestilling.getTpsf().getEgenAnsattDatoFom()) &&
+            addTag(tags, nonNull(bestilling.getTpsf().getEgenAnsattDatoFom()) &&
                     isNull(bestilling.getTpsf().getEgenAnsattDatoTom()), "EGENANSATT");
 
-            addLabel(labels, "SPSF".equals(bestilling.getTpsf().getSpesreg()), "KODE6");
-            addLabel(labels, "SPFO".equals(bestilling.getTpsf().getSpesreg()), "KODE7");
+            addTag(tags, "SPSF".equals(bestilling.getTpsf().getSpesreg()), "KODE6");
+            addTag(tags, "SPFO".equals(bestilling.getTpsf().getSpesreg()), "KODE7");
 
-            addLabel(labels, isNotBlank(bestilling.getTpsf().getSpesreg()) &&
+            addTag(tags, isNotBlank(bestilling.getTpsf().getSpesreg()) &&
                     !"SPSF".equals(bestilling.getTpsf().getSpesreg()) &&
                     !"SPFO".equals(bestilling.getTpsf().getSpesreg()), "SPESREG");
 
-            addLabel(labels, isTrue(bestilling.getTpsf().getHarMellomnavn()), "MELLOMNAVN");
-            addLabel(labels, isTrue(bestilling.getTpsf().getHarBankkontonr()), "BANKKONTONUMMER");
-            addLabel(labels, isNotBlank(bestilling.getTpsf().getTelefonnummer_1()), "TELEFONNUMMER");
-            addLabel(labels, isNotBlank(bestilling.getTpsf().getStatsborgerskap()), "STATSBORGERSKAP");
-            addLabel(labels, isNotBlank(bestilling.getTpsf().getInnvandretFraLand()), "INNVANDRET");
-            addLabel(labels, isNotBlank(bestilling.getTpsf().getUtvandretTilLand()), "UTVANDRET");
-            addLabel(labels, isTrue(bestilling.getTpsf().getErForsvunnet()), "FORSVUNNET");
-            addLabel(labels, isTrue(bestilling.getTpsf().getUtenFastBopel()), "UTENFASTBOPEL");
-            addLabel(labels, isNotBlank(bestilling.getTpsf().getSprakKode()), "SPRÅK");
-            addLabel(labels, nonNull(bestilling.getTpsf().getDoedsdato()), "DØDSDATO");
+            addTag(tags, isTrue(bestilling.getTpsf().getHarMellomnavn()), "MELLOMNAVN");
+            addTag(tags, isTrue(bestilling.getTpsf().getHarBankkontonr()), "BANKKONTONUMMER");
+            addTag(tags, isNotBlank(bestilling.getTpsf().getTelefonnummer_1()), "TELEFONNUMMER");
+            addTag(tags, isNotBlank(bestilling.getTpsf().getStatsborgerskap()), "STATSBORGERSKAP");
+            addTag(tags, isNotBlank(bestilling.getTpsf().getInnvandretFraLand()), "INNVANDRET");
+            addTag(tags, isNotBlank(bestilling.getTpsf().getUtvandretTilLand()), "UTVANDRET");
+            addTag(tags, isTrue(bestilling.getTpsf().getErForsvunnet()), "FORSVUNNET");
+            addTag(tags, isTrue(bestilling.getTpsf().getUtenFastBopel()), "UTENFASTBOPEL");
+            addTag(tags, isNotBlank(bestilling.getTpsf().getSprakKode()), "SPRÅK");
+            addTag(tags, nonNull(bestilling.getTpsf().getDoedsdato()), "DØDSDATO");
 
             if (nonNull(bestilling.getTpsf().getRelasjoner())) {
-                addLabel(labels, !bestilling.getTpsf().getRelasjoner().getPartnere().isEmpty(), "PARTNER");
-                addLabel(labels, !bestilling.getTpsf().getRelasjoner().getBarn().isEmpty(), "BARN");
+                addTag(tags, !bestilling.getTpsf().getRelasjoner().getPartnere().isEmpty(), "PARTNER");
+                addTag(tags, !bestilling.getTpsf().getRelasjoner().getBarn().isEmpty(), "BARN");
             }
         }
         if (nonNull(bestilling.getPdlforvalter())) {
-            addLabel(labels, nonNull(bestilling.getPdlforvalter().getFalskIdentitet()), "FALSKID");
-            addLabel(labels, nonNull(bestilling.getPdlforvalter().getKontaktinformasjonForDoedsbo()), "KONTAKTDØDSBO");
-            addLabel(labels, nonNull(bestilling.getPdlforvalter().getUtenlandskIdentifikasjonsnummer()), "UTENLANDSID");
+            addTag(tags, nonNull(bestilling.getPdlforvalter().getFalskIdentitet()), "FALSKID");
+            addTag(tags, nonNull(bestilling.getPdlforvalter().getKontaktinformasjonForDoedsbo()), "KONTAKTDØDSBO");
+            addTag(tags, nonNull(bestilling.getPdlforvalter().getUtenlandskIdentifikasjonsnummer()), "UTENLANDSID");
         }
 
         if (nonNull(bestilling.getPensjonforvalter())) {
-            addLabel(labels, nonNull(bestilling.getPensjonforvalter().getInntekt()), "POPPINNTEKT");
+            addTag(tags, nonNull(bestilling.getPensjonforvalter().getInntekt()), "POPPINNTEKT");
         }
 
-        addLabel(labels, !bestilling.getAareg().isEmpty(), "AAREG");
-        addLabel(labels, nonNull(bestilling.getArenaforvalter()), "ARENA");
-        addLabel(labels, !bestilling.getSigrunstub().isEmpty(), "SIGRUNSTUB");
-        addLabel(labels, !bestilling.getInstdata().isEmpty(), "INST");
-        addLabel(labels, nonNull(bestilling.getInntektstub()), "INNTKSTUB");
-        addLabel(labels, nonNull(bestilling.getKrrstub()), "KRRSTUB");
-        addLabel(labels, nonNull(bestilling.getUdistub()), "UDISTUB");
+        addTag(tags, !bestilling.getAareg().isEmpty(), "AAREG");
+        addTag(tags, nonNull(bestilling.getArenaforvalter()), "ARENA");
+        addTag(tags, !bestilling.getSigrunstub().isEmpty(), "SIGRUNSTUB");
+        addTag(tags, !bestilling.getInstdata().isEmpty(), "INST");
+        addTag(tags, nonNull(bestilling.getInntektstub()), "INNTKSTUB");
+        addTag(tags, nonNull(bestilling.getKrrstub()), "KRRSTUB");
+        addTag(tags, nonNull(bestilling.getUdistub()), "UDISTUB");
 
-        addLabel(labels, isNotBlank(bestilling.getMalBestillingNavn()), "MALBESTILLING");
+        addTag(tags, isNotBlank(bestilling.getMalBestillingNavn()), "MALBESTILLING");
 
-        invoke(name, labels.toArray(new String[labels.size()]));
+        invoke(name, tags);
     }
 
-    private static void addLabel(List labels, boolean check, String label) {
+    private static void addTag(List tags, boolean check, String tag) {
         if (check) {
-            labels.add(label);
+            tags.add(tag);
         }
     }
 }
