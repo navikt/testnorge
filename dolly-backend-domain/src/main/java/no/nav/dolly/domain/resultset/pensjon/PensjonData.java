3a7c75949dfe73707da95914f5a21baec285f8ed
Kristen Hærum <kristen.haerum@nav.no>
2020-04-16 13:58:47 +0200

    REG-7915 Pensjon: fjernet null-verdier i JSON på bestilling

--- dolly-backend-domain/src/main/java/no/nav/dolly/domain/resultset/pensjon/PensjonData.java
+++ dolly-backend-domain/src/main/java/no/nav/dolly/domain/resultset/pensjon/PensjonData.java
@@ -1,5 +1,7 @@
 package no.nav.dolly.domain.resultset.pensjon;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -10,6 +12,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class PensjonData {
 
     @ApiModelProperty(
@@ -22,6 +25,7 @@ public class PensjonData {
     @Setter
     @NoArgsConstructor
     @AllArgsConstructor
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     public static class PoppInntekt {
         @ApiModelProperty(
                 value = "Fra og med år YYYY",
