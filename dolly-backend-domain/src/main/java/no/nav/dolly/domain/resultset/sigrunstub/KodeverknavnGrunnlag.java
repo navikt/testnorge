78130bdfa6211cdda9b8fe426b448fe0743969d5
Kristen Hærum <kristen.haerum@nav.no>
2020-04-16 14:09:29 +0200

    REG-7915 Sigrunstub: fjernet null-verdier i JSON på bestilling

--- dolly-backend-domain/src/main/java/no/nav/dolly/domain/resultset/sigrunstub/KodeverknavnGrunnlag.java
+++ dolly-backend-domain/src/main/java/no/nav/dolly/domain/resultset/sigrunstub/KodeverknavnGrunnlag.java
@@ -1,5 +1,7 @@
 package no.nav.dolly.domain.resultset.sigrunstub;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -7,6 +9,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class KodeverknavnGrunnlag {
     private String tekniskNavn;
     private String verdi;
--- dolly-backend-domain/src/main/java/no/nav/dolly/domain/resultset/sigrunstub/OpprettSkattegrunnlag.java
+++ dolly-backend-domain/src/main/java/no/nav/dolly/domain/resultset/sigrunstub/OpprettSkattegrunnlag.java
@@ -2,6 +2,8 @@ package no.nav.dolly.domain.resultset.sigrunstub;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,6 +15,7 @@ import lombok.Setter;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class OpprettSkattegrunnlag {
 
     public enum Tjeneste {BEREGNET_SKATT, SUMMERT_SKATTEGRUNNLAG}
