692c27c367b603303379ca8414a438f546713806
Stig Helge Strøm <stig.helge.strom@nav.no>
2020-04-19 10:52:43 +0200

    Oppsett av app med enkel ws endepunkt

---
+++ src/main/java/no/nav/brregstub/ws/BrregEndpoint.java
@@ -0,0 +1,49 @@
+package no.nav.brregstub.ws;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import no.nav.brregstub.tjenestekontrakter.hentroller.Grunndata;
+import no.nav.brregstub.tjenestekontrakter.ws.HentRoller;
+import no.nav.brregstub.tjenestekontrakter.ws.HentRollerResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import javax.xml.bind.JAXB;
+import javax.xml.bind.JAXBElement;
+import javax.xml.namespace.QName;
+import java.io.StringWriter;
+
+
+@Endpoint
+public class BrregEndpoint {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(BrregEndpoint.class);
+
+    private static final String NAMESPACE_URI = "http://no/brreg/saksys/grunndata/ws";
+
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "hentRoller")
+    @ResponsePayload
+    public JAXBElement<HentRollerResponse> hentRollerResponse(
+            @RequestPayload JAXBElement<HentRoller> request) throws JsonProcessingException {
+        LOGGER.debug("Received request object: {}");
+
+        var response = new HentRollerResponse();
+        var grunndata = new Grunndata();
+        var melding = new Grunndata.Melding();
+        var organisasjonsnummer = new Grunndata.Melding.Organisasjonsnummer();
+        organisasjonsnummer.setValue("123");
+        melding.setOrganisasjonsnummer(organisasjonsnummer);
+        grunndata.setMelding(melding);
+        StringWriter sw = new StringWriter();
+        JAXB.marshal(grunndata, sw);
+        String xmlString = sw.toString();
+        response.setReturn(xmlString);
+
+        return new JAXBElement<>(new QName("hentRollerResponse"), HentRollerResponse.class, response);
+    }
+
+}
