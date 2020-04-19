6809356baeb34ec40382599414c6b78f6879370c
Stig Helge Strøm <stig.helge.strom@nav.no>
2020-04-19 11:41:18 +0200

    Utvidet brregendpoint til å eksponere hentRolleutskrift

--- src/main/java/no/nav/brregstub/ws/BrregEndpoint.java
+++ src/main/java/no/nav/brregstub/ws/BrregEndpoint.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import no.nav.brregstub.tjenestekontrakter.hentroller.Grunndata;
 import no.nav.brregstub.tjenestekontrakter.ws.HentRoller;
 import no.nav.brregstub.tjenestekontrakter.ws.HentRollerResponse;
+import no.nav.brregstub.tjenestekontrakter.ws.HentRolleutskrift;
+import no.nav.brregstub.tjenestekontrakter.ws.HentRolleutskriftResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
@@ -44,6 +46,19 @@ public class BrregEndpoint {
         response.setReturn(xmlString);
 
         return new JAXBElement<>(new QName("hentRollerResponse"), HentRollerResponse.class, response);
+    }
+
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "hentRolleutskrift")
+    @ResponsePayload
+    public JAXBElement<HentRolleutskriftResponse> hentRolleutskrift(
+            @RequestPayload JAXBElement<HentRolleutskrift> request) throws JsonProcessingException {
+        LOGGER.debug("Received request object: {}");
+
+        var response = new HentRolleutskriftResponse();
+
+
+        return new JAXBElement<>(new QName("hentRolleutskriftResponse"), HentRolleutskriftResponse.class, response);
     }
 
 }
