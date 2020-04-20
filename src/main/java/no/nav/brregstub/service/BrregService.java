8cbd12148fd30146a7386c3cbcc8d722c4e5c982
Stig Helge Strøm <stig.helge.strom@nav.no>
2020-04-20 12:06:21 +0200

    Lagt til logger

--- src/main/java/no/nav/brregstub/service/BrregService.java
+++ src/main/java/no/nav/brregstub/service/BrregService.java
@@ -1,6 +1,8 @@
 package no.nav.brregstub.service;
 
 import no.nav.brregstub.tjenestekontrakter.hentroller.Grunndata;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
@@ -10,11 +12,14 @@ import java.io.IOException;
 @Service
 public class BrregService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(BrregService.class);
+
     public Grunndata hentRoller(String orgnr) {
         ClassPathResource resource = new ClassPathResource("response/HentRollerRespnse.xml");
         try {
             return JAXB.unmarshal(resource.getFile(), Grunndata.class);
         } catch (IOException e) {
+            LOGGER.error("Feil ved hentRoller", e);
             throw new RuntimeException("Feil ved hentRoller", e);
         }
     }
@@ -24,6 +29,7 @@ public class BrregService {
         try {
             return JAXB.unmarshal(resource.getFile(), no.nav.brregstub.tjenestekontrakter.rolleutskrift.Grunndata.class);
         } catch (IOException e) {
+            LOGGER.error("Feil ved hentRolleutskrift", e);
             throw new RuntimeException("Feil ved hentRolleutskrift", e);
         }
     }
