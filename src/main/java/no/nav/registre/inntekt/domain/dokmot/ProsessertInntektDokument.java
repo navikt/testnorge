e6d62b4ee9e0dfb0886ce3cac9b07efc90b4bc36
K156575 <baard.krohg@gmail.com>
2020-04-22 13:39:06 +0200

    oppdatering med dokumentinfoID

--- src/main/java/no/nav/registre/inntekt/domain/dokmot/ProsessertInntektDokument.java
+++ src/main/java/no/nav/registre/inntekt/domain/dokmot/ProsessertInntektDokument.java
@@ -3,16 +3,29 @@ package no.nav.registre.inntekt.domain.dokmot;
 import lombok.Value;
 
 import no.nav.registre.inntekt.consumer.rs.dokmot.dto.DokmotResponse;
+import no.nav.registre.inntekt.consumer.rs.dokmot.dto.DokumentInfo;
+
+import java.util.List;
 
 @Value
 public class ProsessertInntektDokument {
     private final String fnr;
     private final String journalpostId;
+    private final String dokumentInfoId;
     private final String xml;
 
     public ProsessertInntektDokument(InntektDokument dokument, DokmotResponse response) {
+        String dokumentInfoIdTmp;
+
         this.fnr = dokument.getArbeidstakerFnr();
         this.journalpostId = response.getJournalpostId();
+        List<DokumentInfo> tmp = response.getDokumenter();
+        dokumentInfoIdTmp = null;
+        if (!tmp.isEmpty()) {
+            // Inntektsmeldinger har bare ett dokument per journalpost.
+            dokumentInfoIdTmp = tmp.get(0).getDokumentInfoId();
+        }
+        this.dokumentInfoId = dokumentInfoIdTmp;
         this.xml = dokument.getXml();
     }
 }
--- src/main/java/no/nav/registre/inntekt/provider/rs/response/ProsessertInntektDokumentDTO.java
+++ src/main/java/no/nav/registre/inntekt/provider/rs/response/ProsessertInntektDokumentDTO.java
@@ -12,10 +12,13 @@ public class ProsessertInntektDokumentDTO {
     @JsonProperty
     private final String journalpostId;
     @JsonProperty
+    private final String dokumentInfoId;
+    @JsonProperty
     private final String xml;
 
     public ProsessertInntektDokumentDTO(ProsessertInntektDokument dokument, boolean includeXml) {
         this.journalpostId = dokument.getJournalpostId();
+        this.dokumentInfoId = dokument.getDokumentInfoId();
         this.xml = includeXml ? dokument.getXml() : null;
     }
 }
