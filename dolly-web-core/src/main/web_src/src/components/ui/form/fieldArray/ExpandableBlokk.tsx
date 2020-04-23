7b63c4e8ce421eb1b14c813c425a82ffc5f9618e
Traran <Betsy.Carina.Traran@nav.no>
2020-04-23 16:51:21 +0200

    REG-7940: Gjøre hele header klikkbar

--- dolly-web-core/src/main/web_src/src/components/ui/form/fieldArray/ExpandableBlokk.tsx
+++ dolly-web-core/src/main/web_src/src/components/ui/form/fieldArray/ExpandableBlokk.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import cn from 'classnames'
 import ExpandButton from '~/components/ui/button/ExpandButton'
 
 type Numbering = {
@@ -16,9 +17,11 @@ const Numbering = ({ idx }: Numbering) => <span className="dfa-blokk-number">{id
 export default function ExpandableBlokk({ header, idx, children }: ExpandableBlokk) {
 	const [isExpanded, setIsExpanded] = useState(false)
 
+	const headerClass = cn('dfa-blokk_header', { clickable: true })
+
 	return (
 		<div className="dfa-blokk">
-			<div className="dfa-blokk_header">
+			<div className={headerClass} onClick={() => setIsExpanded(!isExpanded)}>
 				<Numbering idx={idx} />
 				<h2>{header}</h2>
 				<ExpandButton expanded={isExpanded} onClick={() => setIsExpanded(!isExpanded)} />
