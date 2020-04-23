8e8c459aa714043c32c37f360e25f2b4bb61c1d3
Kristoffer Mjelva <kristoffer.mjelva@nav.no>
2020-04-23 11:24:57 +0200

    REG-7858: debuglogging

--- ident-pool-core/src/main/java/no/nav/identpool/ajourhold/AjourholdService.java
+++ ident-pool-core/src/main/java/no/nav/identpool/ajourhold/AjourholdService.java
@@ -123,6 +123,7 @@ public class AjourholdService {
             int year,
             int antallPerDag
     ) {
+        log.info("adjustForYear: year: {}. antallPerDag: {}", year, antallPerDag);
         if (antallPerDag < MIN_ANTALL_IDENTER_PER_DAG) {
             LocalDate dateOfYear = LocalDate.of(year, 1, 1);
             if (dateOfYear.isBefore(LocalDate.now()) && ChronoUnit.YEARS.between(dateOfYear, LocalDate.now()) <= 3) {
