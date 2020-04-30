16ab56b97d0bf4937cb62f92a5fff8abb030404f
s152486 <ingvild.stoen@nav.no>
2020-04-30 14:54:15 +0200

    REG-7696 Endret melding når bestilling tar lang tid

--- dolly-web-core/src/main/web_src/src/components/bestilling/statusListe/BestillingProgresjon/BestillingProgresjon.js
+++ dolly-web-core/src/main/web_src/src/components/bestilling/statusListe/BestillingProgresjon/BestillingProgresjon.js
@@ -139,7 +139,8 @@ export default class BestillingProgresjon extends PureComponent {
 						<div>
 							<Icon kind={'report-problem-circle'} />
 							<h5 className="feil-status-text">
-								Dette tar lengre tid enn forventet. Noe kan ha gått galt med bestillingen din.
+								Dette tar lengre tid enn forventet. Hvis det er en kompleks bestilling kan du gi
+								Dolly litt mer tid før du eventuelt avbryter.
 							</h5>
 						</div>
 						<NavButton type="fare" onClick={this._onCancelBtn}>
