98aea0c0c880f298fa263a3459537b5c09c173a6
kristoffernaver <kristoffer.mjelva@nav.no>
2020-04-30 15:13:13 +0200

    REG-7840: Oppdatert wsdl-URL

--- aareg-core/src/main/java/no/nav/registre/aareg/consumer/ws/BehandleArbeidsforholdV1Proxy.java
+++ aareg-core/src/main/java/no/nav/registre/aareg/consumer/ws/BehandleArbeidsforholdV1Proxy.java
@@ -23,7 +23,7 @@ public class BehandleArbeidsforholdV1Proxy {
 
     private static final int DEFAULT_TIMEOUT = 5_000;
 
-    private static final String WSDL_URL = "wsdl/no/nav/registre/testnorge/consumers/tjenestespesifikasjon/arbeidsforhold/BehandleArbeidsforhold.wsdl";
+    private static final String WSDL_URL = "wsdl/no/nav/tjeneste/domene/behandleArbeidsforhold/v1/BehandleArbeidsforhold.wsdl";
     private static final String NAMESPACE = "http://nav.no/tjeneste/domene/behandleArbeidsforhold/v1/";
     private static final QName BEHANDLE_ARBEIDSFORHOLD_V1 = new QName(NAMESPACE, "BehandleArbeidsforhold_v1");
 
