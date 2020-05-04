d413466fce46380d276c6bdc2610a61b7fb10e25
Hay <Aileen.Hay@nav.no>
2020-05-04 12:55:34 +0200

    REG-8109 liten endring info tekst

--- dolly-web-core/src/main/web_src/src/pages/soekMiniNorge/SoekMiniNorge.tsx
+++ dolly-web-core/src/main/web_src/src/pages/soekMiniNorge/SoekMiniNorge.tsx
@@ -4,7 +4,7 @@ import './SoekMiniNorge.less'
 import { AlertStripeInfo } from 'nav-frontend-alertstriper'
 
 const infoTekst =
-	'Syntetiske testdata er tilgjengelig i Dolly gjennom tekniske APIer og Mini-Norge. Mini-Norge er NAVs syntetiske basispopulasjon med egenskaper tilsvarende normalen i Norge. Antall innbyggere er pr mars 2020 ca 200 000. Befolkningen er dynamisk og det gjøres løpende endringer - nye personer fødes, folk skifter jobb osv. Mini-Norge kan brukes av alle og er godt egnet til basistester, volumtester m.m. der det ikke er behov for spesialtilfeller.\n\n' +
+	'Syntetiske testdata er tilgjengelig i Dolly gjennom tekniske APIer og Mini-Norge. Mini-Norge er NAVs syntetiske basispopulasjon med egenskaper tilsvarende normalen i Norge. Antall innbyggere er pr mars 2020 ca 200 000. Befolkningen er dynamisk og det gjøres løpende endringer – nye personer fødes, folk skifter jobb osv. Mini-Norge kan brukes av alle og er godt egnet til basistester, volumtester m.m. der det ikke er behov for spesialtilfeller.\n\n' +
 	'Mini-Norge er kun tilgjengelig i Q2 som er et helsyntetisk testmiljø.\n\n' +
 	'Beta-versjon for søk i Mini-Norge jobbes med fortløpende.'
 
