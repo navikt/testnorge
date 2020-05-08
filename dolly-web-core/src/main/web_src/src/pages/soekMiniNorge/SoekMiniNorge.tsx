2fe216313696edcc8e5d05727db1acb2bcd33de3
Hay <Aileen.Hay@nav.no>
2020-05-08 11:10:20 +0200

    REG-8111 liten css fiks

--- dolly-web-core/src/main/web_src/src/pages/soekMiniNorge/SoekMiniNorge.tsx
+++ dolly-web-core/src/main/web_src/src/pages/soekMiniNorge/SoekMiniNorge.tsx
@@ -8,8 +8,8 @@ export default class SoekMiniNorge extends PureComponent {
 	render() {
 		return (
 			<div className="soek-page">
-				<h1 className="header">Søk i Mini-Norge</h1>
-				<h3 className="header">(beta)</h3>
+				<h1 className="soek-page__header">Søk i Mini-Norge</h1>
+				<h3 className="soek-page__header">(beta)</h3>
 				<Search />
 			</div>
 		)
