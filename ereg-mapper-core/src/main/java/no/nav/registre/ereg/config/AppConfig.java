ee96c8d8ca485d51cb66a5313f02f9a909af0411
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2020-05-23 12:39:27 +0200

    Add logging to requestes and responses

--- ereg-mapper-core/src/main/java/no/nav/registre/ereg/config/AppConfig.java
+++ ereg-mapper-core/src/main/java/no/nav/registre/ereg/config/AppConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.BufferingClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import no.nav.registre.testnorge.common.config.LoggableDispatcherServlet;
 
 @Configuration
 public class AppConfig {
@@ -12,6 +15,11 @@ public class AppConfig {
     @Bean
     RestTemplate restTemplate() {
         return new RestTemplate(new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory()));
+    }
+
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new LoggableDispatcherServlet();
     }
 
 }
