d776abcb29bc7a1edc5bc1dbac01bfc4f6c3e41e
Kristoffer Mjelva <kristoffer.mjelva@nav.no>
2020-06-02 14:09:20 +0200

    REG-8330: Opprettet KafkaConfig som setter properties manuelt

---
+++ src/main/java/no/nav/registre/populasjoner/config/KafkaConfig.java
@@ -0,0 +1,48 @@
+package no.nav.registre.populasjoner.config;
+
+import io.confluent.kafka.serializers.KafkaAvroDeserializer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@EnableKafka
+@Configuration
+public class KafkaConfig {
+
+    @Value("${spring.kafka.properties.bootstrap-servers}")
+    private String brokers;
+
+    @Value("${app.kafka.group-id-prefix}-pdlDokumenter-1")
+    private String kafkaGroup;
+
+    @Value("${spring.kafka.properties.schema.registry.url}")
+    private String schemaUrl;
+
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        Map<String, Object> config = new HashMap<>();
+
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, brokers);
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, kafkaGroup);
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class);
+        config.put("schema.registry.url", schemaUrl);
+
+        return new DefaultKafkaConsumerFactory<>(config);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+}
