141b160f3b6dd513967533f18f54ea7d1a32cab3
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2020-06-04 00:32:11 +0200

    add: test app context

---
+++ apps/testnorge-ereg-mapper/ereg-mapper-core/src/test/java/no/nav/registre/ereg/testnorge/ApplicationContextTest.java
@@ -0,0 +1,23 @@
+package no.nav.registre.ereg.testnorge;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import no.nav.registre.ereg.config.TestConfig;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = TestConfig.class)
+@TestPropertySource(locations = "classpath:application-test.properties")
+public class ApplicationContextTest {
+
+    /**
+     * Used to test app runtime dependency and properties
+     */
+    @Test
+    public void load_app_context() {
+    }
+}
---
+++ apps/testnorge-inntekt/src/test/java/no/nav/registre/inntekt/testnorge/ApplicationContextTest.java
@@ -0,0 +1,28 @@
+package no.nav.registre.inntekt.testnorge;
+
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import no.nav.registre.inntekt.config.AppConfig;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
+public class ApplicationContextTest {
+
+    /**
+     * //TODO fix
+     * Used to test app runtime dependency and properties
+     */
+    @Test
+    @Ignore
+    public void load_app_context() {
+    }
+}
