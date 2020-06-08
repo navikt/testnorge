4019dc92964c13d1859c88d71390dcd4581a1724
stigus <stian.gustavsson@nav.no>
2020-06-08 16:51:28 +0200

    Ryddet litt opp i auto importering

--- apps/testnorge-aaregstub/src/main/java/no/nav/registre/aaregstub/config/production/SwaggerConfig.java
+++ apps/testnorge-aaregstub/src/main/java/no/nav/registre/aaregstub/config/production/SwaggerConfig.java
@@ -16,7 +16,6 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
--- apps/testnorge-arena/arena-core/src/main/java/no/nav/registre/arena/core/config/SwaggerConfig.java
+++ apps/testnorge-arena/arena-core/src/main/java/no/nav/registre/arena/core/config/SwaggerConfig.java
@@ -15,7 +15,6 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
