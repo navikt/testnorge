35b4a22c35e7f9082aa8768bdb728f1a1595e6e6
stigus <stian.gustavsson@nav.no>
2020-06-09 15:40:17 +0200

    Forsøkt å ordne flere tilfeller

--- apps/testnorge-ereg-mapper/ereg-mapper-cloud/src/main/java/no/nav/registre/ereg/mapper/provider/rs/InternalController.java
+++ apps/testnorge-ereg-mapper/ereg-mapper-cloud/src/main/java/no/nav/registre/ereg/mapper/provider/rs/InternalController.java
@@ -1,6 +1,5 @@
 package no.nav.registre.ereg.mapper.provider.rs;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
