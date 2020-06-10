d7f1d9eac275ce281aafcaacd9cb5e8c1ed90c08
Kristoffer Mjelva <kristoffer.mjelva@nav.no>
2020-06-10 16:22:16 +0200

    REG-8457: La til støtte for flere tilleggsytelser

---
+++ libs/testnorge-domain/src/main/java/no/nav/registre/testnorge/domain/dto/arena/testnorge/tilleggsstoenad/tilsyn/FamiliemedlemKoder.java
@@ -0,0 +1,12 @@
+package no.nav.registre.testnorge.domain.dto.arena.testnorge.tilleggsstoenad.tilsyn;
+
+public enum FamiliemedlemKoder {
+    KRPER,
+    KRFRA,
+    KRTIL,
+    ANFNR,
+    TILAV,
+    FANAV,
+    BEGRU,
+    LAND,
+}
