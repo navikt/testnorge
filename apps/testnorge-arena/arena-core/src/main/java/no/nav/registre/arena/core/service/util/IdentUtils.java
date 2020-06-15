a2237e068766031384174ce2b53b1ce5211c1d5f
Hay <Aileen.Hay@nav.no>
2020-06-15 13:07:26 +0200

    REG-8528 fjernet code smells

--- apps/testnorge-arena/arena-core/src/main/java/no/nav/registre/arena/core/service/util/IdentUtils.java
+++ apps/testnorge-arena/arena-core/src/main/java/no/nav/registre/arena/core/service/util/IdentUtils.java
@@ -1,9 +1,14 @@
 package no.nav.registre.arena.core.service.util;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
 import java.time.LocalDate;
 
 import static java.lang.Integer.parseInt;
 
+@Service
+@RequiredArgsConstructor
 public class IdentUtils {
 
     private static final String DNR = "DNR";
@@ -33,22 +38,22 @@ public class IdentUtils {
      * INDIVID(POS 7-9) 500-999 OG ÅR < 40 => ÅRHUNDRE = 2000
      */
     private static int getFullYear(String ident) {
-        int year = parseInt(ident.substring(4, 6));
+        int aar = parseInt(ident.substring(4, 6));
         int individ = parseInt(ident.substring(6, 9));
 
         // Find century
-        int century;
-        if (individ < 500 || (individ >= 900 && year > 39)) {
-            century = 1900;
-        } else if (year < 40) {
-            century = 2000;
-        } else if (individ < 750 && year > 54) {
-            century = 1800;
+        int aarhundre;
+        if (individ < 500 || (individ >= 900 && aar > 39)) {
+            aarhundre = 1900;
+        } else if (aar < 40) {
+            aarhundre = 2000;
+        } else if (individ < 750 && aar > 54) {
+            aarhundre = 1800;
         } else {
-            century = 2000;
+            aarhundre = 2000;
         }
 
-        return LocalDate.of(century + year, 1, 1).getYear();
+        return LocalDate.of(aarhundre + aar, 1, 1).getYear();
     }
 
     public static String getIdentType(String ident) {
