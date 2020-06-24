f30a130f2830071ccda1e2cfcdf695524ccd1464
Kristoffer Mjelva <kristoffer.mjelva@nav.no>
2020-06-24 14:54:42 +0200

    REG-8330: Debuglogging

--- src/main/java/no/nav/registre/populasjoner/kafka/PdlDokumentListener.java
+++ src/main/java/no/nav/registre/populasjoner/kafka/PdlDokumentListener.java
@@ -1,13 +1,10 @@
 package no.nav.registre.populasjoner.kafka;
 
-import static no.nav.registre.populasjoner.kafka.CollectionUtils.chunk;
-
-import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -15,9 +12,6 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.Collection;
-import java.util.List;
-import java.util.stream.Collectors;
 
 import no.nav.registre.populasjoner.kafka.domain.PdlDokument;
 import no.nav.registre.populasjoner.kafka.person.IdentDetaljDto;
@@ -45,17 +39,29 @@ public class PdlDokumentListener {
     public void onMessage(@Payload String message) {
         log.info("Mottok melding på topic");
 
-        ConsumerRecords<String, String> records = new ObjectMapper().convertValue(message, new TypeReference<>() {
-        });
-
-        List<DocumentIdWrapper> documentList = KafkaUtilities.asStream(records)
-                .map(this::convert)
-                .collect(Collectors.toList());
-
-        Collection<List<DocumentIdWrapper>> chunk = chunk(documentList, 15);
-        for (List<DocumentIdWrapper> documentIdWrappers : chunk) {
-            service.processBulk(documentIdWrappers);
+        JsonNode jsonNode = null;
+        try {
+            jsonNode = new ObjectMapper().convertValue(message, JsonNode.class);
+        } catch (Exception e) {
+            log.error("Kunne ikke konvertere melding til jsonNode");
         }
+
+        if (jsonNode != null) {
+            JsonNode hentIdenter = jsonNode.findValue("hentIdenter");
+            log.info("fra json-node: {}", hentIdenter.toPrettyString());
+        }
+
+        //        ConsumerRecords<String, String> records = new ObjectMapper().convertValue(message, new TypeReference<>() {
+        //        });
+
+        //        List<DocumentIdWrapper> documentList = KafkaUtilities.asStream(records)
+        //                .map(this::convert)
+        //                .collect(Collectors.toList());
+        //
+        //        Collection<List<DocumentIdWrapper>> chunk = chunk(documentList, 15);
+        //        for (List<DocumentIdWrapper> documentIdWrappers : chunk) {
+        //            service.processBulk(documentIdWrappers);
+        //        }
     }
 
     private DocumentIdWrapper convert(ConsumerRecord<String, String> record) {
