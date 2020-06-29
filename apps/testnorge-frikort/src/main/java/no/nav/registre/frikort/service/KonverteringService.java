8f3c6f79de5aa645df8fff8884c88d86fb029de7
Kristoffer Mjelva <kristoffer.mjelva@nav.no>
2020-06-29 08:55:07 +0200

    REG-7312: BorgerId som String for å beholde ledende 0-er

--- apps/testnorge-frikort/src/main/java/no/nav/registre/frikort/service/KonverteringService.java
+++ apps/testnorge-frikort/src/main/java/no/nav/registre/frikort/service/KonverteringService.java
@@ -2,8 +2,6 @@ package no.nav.registre.frikort.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import no.nav.registre.frikort.consumer.rs.response.SyntFrikortResponse;
-import no.nav.registre.frikort.domain.xml.*;
 import org.springframework.stereotype.Service;
 
 import javax.xml.bind.JAXBException;
@@ -15,6 +13,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import no.nav.registre.frikort.consumer.rs.response.SyntFrikortResponse;
+import no.nav.registre.frikort.domain.xml.Borger;
+import no.nav.registre.frikort.domain.xml.Egenandel;
+import no.nav.registre.frikort.domain.xml.EgenandelListe;
+import no.nav.registre.frikort.domain.xml.Egenandelsmelding;
+import no.nav.registre.frikort.domain.xml.Samhandler;
+import no.nav.registre.frikort.domain.xml.SamhandlerListe;
 
 @Slf4j
 @Service
@@ -29,7 +34,7 @@ public class KonverteringService {
             var egenandelsmeldingListe = lagEgenandelsmeldingListe(egenandeler);
 
             var xmlMeldinger = new ArrayList<String>(egenandelsmeldingListe.size());
-            for (Egenandelsmelding melding : egenandelsmeldingListe){
+            for (var melding : egenandelsmeldingListe) {
                 xmlMeldinger.add(konverterEgenandelsmeldingTilXMLString(melding));
             }
 
@@ -40,16 +45,15 @@ public class KonverteringService {
         }
     }
 
-    private List<Egenandelsmelding> lagEgenandelsmeldingListe(Map<String, List<SyntFrikortResponse>> egenandeler){
+    private List<Egenandelsmelding> lagEgenandelsmeldingListe(Map<String, List<SyntFrikortResponse>> egenandeler) {
         var egenandelsmeldingListe = new ArrayList<Egenandelsmelding>();
 
         egenandeler.forEach((id, infoListe) -> {
-            for (SyntFrikortResponse res : infoListe) {
+            for (var res : infoListe) {
+                var listeAvEgenandeler = lagEgenandelsListe(res, id);
+                var listeAvSamhandlere = lagSamhandlerListe(res, listeAvEgenandeler);
 
-                EgenandelListe listeAvEgenandeler = lagEgenandelsListe(res, id);
-                SamhandlerListe listeAvSamhandlere = lagSamhandlerListe(res, listeAvEgenandeler);
-
-                Egenandelsmelding egenandelsmelding = Egenandelsmelding.builder()
+                var egenandelsmelding = Egenandelsmelding.builder()
                         .avsender(res.getKildesystemkode())
                         .listeAvSamhandlere(listeAvSamhandlere)
                         .datoSendt(LocalDateTime.now())
@@ -62,13 +66,15 @@ public class KonverteringService {
         return egenandelsmeldingListe;
     }
 
-    private EgenandelListe lagEgenandelsListe(SyntFrikortResponse res, String id){
+    private EgenandelListe lagEgenandelsListe(
+            SyntFrikortResponse res,
+            String id
+    ) {
         var egenandelListe = new ArrayList<Egenandel>();
 
-        long borgerid = Long.parseLong(id);
-        Borger borger = Borger.builder().borgerid(borgerid).build();
+        var borger = Borger.builder().borgerid(id).build();
 
-        Egenandel egenandel = Egenandel.builder()
+        var egenandel = Egenandel.builder()
                 .betaltEgenandel(Boolean.parseBoolean(res.getBetalt()))
                 .borger(borger)
                 .datoMottatt(LocalDateTime.parse(res.getDato_mottatt(), formatter))
@@ -84,10 +90,13 @@ public class KonverteringService {
         return EgenandelListe.builder().egenandelListe(egenandelListe).build();
     }
 
-    private SamhandlerListe lagSamhandlerListe(SyntFrikortResponse res, EgenandelListe listeAvEgenandeler){
+    private SamhandlerListe lagSamhandlerListe(
+            SyntFrikortResponse res,
+            EgenandelListe listeAvEgenandeler
+    ) {
         List<Samhandler> samhandlerListe = new ArrayList<>();
 
-        Samhandler samhandler = Samhandler.builder()
+        var samhandler = Samhandler.builder()
                 .type(res.getSamhandlertypekode())
                 .innsendingstype(res.getInnsendingstypekode())
                 .listeAvEgenandeler(listeAvEgenandeler)
@@ -98,7 +107,7 @@ public class KonverteringService {
     }
 
     private String konverterEgenandelsmeldingTilXMLString(Egenandelsmelding melding) throws JAXBException {
-        StringWriter sw = new StringWriter();
+        var sw = new StringWriter();
 
         // convert object to XML
         marshaller.marshal(melding, sw);
