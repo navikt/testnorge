c709ad7fa00adb5fafe7af5fd3ed2fcc653513d4
Stian <stian.gustavsson@nav.no>
2020-07-13 12:52:53 +0200

    REG-8513 lagt til post for Detaljert Sykemeld og noe av mappingen

--- dolly-backend-app/src/main/java/no/nav/dolly/bestilling/sykemelding/domain/mapper/DetaljertSykemeldingMappingStrategy.java
+++ dolly-backend-app/src/main/java/no/nav/dolly/bestilling/sykemelding/domain/mapper/DetaljertSykemeldingMappingStrategy.java
@@ -1,8 +1,14 @@
 package no.nav.dolly.bestilling.sykemelding.domain.mapper;
 
+import static java.util.Objects.isNull;
+
 import org.springframework.stereotype.Component;
 
+import ma.glasnost.orika.CustomMapper;
 import ma.glasnost.orika.MapperFactory;
+import ma.glasnost.orika.MappingContext;
+import no.nav.dolly.bestilling.sykemelding.domain.DetaljertSykemeldingRequest;
+import no.nav.dolly.domain.resultset.sykemelding.RsDetaljertSykemelding;
 import no.nav.dolly.mapper.MappingStrategy;
 
 @Component
@@ -11,5 +17,19 @@ public class DetaljertSykemeldingMappingStrategy implements MappingStrategy {
     @Override
     public void register(MapperFactory factory) {
 
+        factory.classMap(RsDetaljertSykemelding.class, DetaljertSykemeldingRequest.class)
+                .customize(new CustomMapper<RsDetaljertSykemelding, DetaljertSykemeldingRequest>() {
+                    @Override
+                    public void mapAtoB(RsDetaljertSykemelding rsSykemelding, DetaljertSykemeldingRequest request, MappingContext context) {
+
+                        if (isNull(rsSykemelding.getDetaljer())) {
+                            request.setDetaljer(DetaljertSykemeldingRequest.Detaljer.builder()
+                                    .arbeidsforEtterEndtPeriode(true)
+                                    .build());
+                        }
+                    }
+                })
+                .byDefault()
+                .register();
     }
 }
