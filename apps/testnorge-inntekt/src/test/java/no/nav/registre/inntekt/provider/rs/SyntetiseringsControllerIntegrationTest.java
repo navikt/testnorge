579a6d2641b59f8794b42f407689a4bfa8f62c7a
stigus <stian.gustavsson@nav.no>
2020-07-16 12:41:39 +0200

    REG-8735 Mer grunnoppsett av integrasjonstester

---
+++ apps/testnorge-inntekt/src/test/java/no/nav/registre/inntekt/provider/rs/SyntetiseringsControllerIntegrationTest.java
@@ -0,0 +1,27 @@
+package no.nav.registre.inntekt.provider.rs;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.github.tomakehurst.wiremock.client.WireMock;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 0)
+@AutoConfigureMockMvc
+@DirtiesContext
+@TestPropertySource(locations = "classpath:application-test.properties")
+class SyntetiseringsControllerIntegrationTest {
+
+    @AfterEach
+    void tearDown() {
+        WireMock.reset();
+    }
+}
