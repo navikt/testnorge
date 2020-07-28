6f7cfd758bcef7a026bf8b6dcd2d45952cb7a4dc
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2020-07-28 13:21:25 +0200

    add: Mini-Norge population

---
+++ apps/testnorge-populasjoner/src/main/java/no/nav/registre/populasjoner/adapter/IdenterAdapter.java
@@ -0,0 +1,7 @@
+package no.nav.registre.populasjoner.adapter;
+
+import java.util.Set;
+
+public interface IdenterAdapter {
+    Set<String> getIdenter();
+}
---
+++ apps/testnorge-populasjoner/src/main/java/no/nav/registre/populasjoner/domain/Populasjon.java
@@ -0,0 +1,6 @@
+package no.nav.registre.populasjoner.domain;
+
+public enum Populasjon {
+    MINI_NORGE,
+    TENOR
+}
