9434fa97fdd8cf9f4fe4a0e37a8a4ab7a05c0b64
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2020-07-29 10:20:49 +0200

    feat: rest endepunkt for opprettelse av arbeidsforhold

--- apps/arbeidsforhold-api/src/main/java/no/nav/registre/testnorge/arbeidsforhold/consumer/dto/ArbeidsforholdDTO.java
+++ apps/arbeidsforhold-api/src/main/java/no/nav/registre/testnorge/arbeidsforhold/consumer/dto/ArbeidsforholdDTO.java
@@ -5,14 +5,46 @@ import lombok.Builder;
 import lombok.NoArgsConstructor;
 import lombok.Value;
 
+import java.util.Collections;
 import java.util.List;
+
+import no.nav.registre.testnorge.arbeidsforhold.domain.Arbeidsforhold;
 
 @Value
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(force = true)
 public class ArbeidsforholdDTO {
+
+    public ArbeidsforholdDTO(Arbeidsforhold arbeidsforhold) {
+        ansettelsesperiode = AnsettelsesperiodeDTO
+                .builder()
+                .periode(PeriodeDTO
+                        .builder()
+                        .fom(arbeidsforhold.getFom())
+                        .tom(arbeidsforhold.getTom())
+                        .build()
+                )
+                .build();
+        arbeidsforholdId = arbeidsforhold.getArbeidsforholdId();
+        arbeidsgiver = ArbeidsgiverDTO
+                .builder()
+                .organisasjonsnummer(arbeidsforhold.getOrgnummer())
+                .type("Organisasjon")
+                .build();
+        arbeidsavtaler = Collections.singletonList(ArbeidsavtaleDTO
+                .builder()
+                .stillingsprosent(arbeidsforhold.getStillingsprosent())
+                .yrke(arbeidsforhold.getYrke())
+                .build()
+        );
+        arbeidstaker = ArbeidstakerDTO.builder().offentligIdent(arbeidsforhold.getIdent()).type("Person").build();
+    }
+
+    AnsettelsesperiodeDTO ansettelsesperiode;
     String arbeidsforholdId;
     ArbeidsgiverDTO arbeidsgiver;
     List<ArbeidsavtaleDTO> arbeidsavtaler;
+    ArbeidstakerDTO arbeidstaker;
+    String type = "ordinaertArbeidsforhold";
 }
--- apps/arbeidsforhold-api/src/main/java/no/nav/registre/testnorge/arbeidsforhold/domain/Arbeidsforhold.java
+++ apps/arbeidsforhold-api/src/main/java/no/nav/registre/testnorge/arbeidsforhold/domain/Arbeidsforhold.java
@@ -3,6 +3,8 @@ package no.nav.registre.testnorge.arbeidsforhold.domain;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
+import java.time.LocalDate;
+
 import no.nav.registre.testnorge.arbeidsforhold.consumer.dto.ArbeidsforholdDTO;
 
 @Slf4j
@@ -12,13 +14,15 @@ public class Arbeidsforhold {
     private final String orgnummer;
     private final Double stillingsprosent;
     private final String yrke;
+    private final LocalDate fom;
+    private final LocalDate tom;
+    private final String ident;
 
     public Arbeidsforhold(ArbeidsforholdDTO dto) {
         arbeidsforholdId = dto.getArbeidsforholdId();
         orgnummer = dto.getArbeidsgiver().getOrganisasjonsnummer();
 
-
-        if(dto.getArbeidsavtaler().isEmpty()){
+        if (dto.getArbeidsavtaler().isEmpty()) {
             throw new RuntimeException("Finner ikke arbeidesforhold");
         }
 
@@ -26,10 +30,22 @@ public class Arbeidsforhold {
             log.warn("Fant flere arbeidsavtaler. Velger den første i listen");
         }
 
-
         var arbeidsavtale = dto.getArbeidsavtaler().get(0);
         stillingsprosent = arbeidsavtale.getStillingsprosent();
         yrke = arbeidsavtale.getYrke();
+        fom = dto.getAnsettelsesperiode().getPeriode().getFom();
+        tom = dto.getAnsettelsesperiode().getPeriode().getTom();
+        ident = dto.getArbeidstaker().getOffentligIdent();
+    }
+
+    public Arbeidsforhold(no.nav.registre.testnorge.dto.arbeidsforhold.v1.ArbeidsforholdDTO dto) {
+        arbeidsforholdId = dto.getArbeidsforholdId();
+        orgnummer = dto.getOrgnummer();
+        stillingsprosent = dto.getStillingsprosent();
+        yrke = dto.getYrke();
+        fom = dto.getFom();
+        tom = dto.getTom();
+        ident = dto.getIdent();
     }
 
     public no.nav.registre.testnorge.dto.arbeidsforhold.v1.ArbeidsforholdDTO toDTO() {
@@ -39,6 +55,9 @@ public class Arbeidsforhold {
                 .orgnummer(orgnummer)
                 .stillingsprosent(stillingsprosent)
                 .yrke(yrke)
+                .fom(fom)
+                .tom(tom)
+                .ident(ident)
                 .build();
     }
 }
