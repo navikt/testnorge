2343ab5fa5019ba2e0326f939ccfa033339848aa
stigus <stian.gustavsson@nav.no>
2020-07-29 13:11:48 +0200

    Fikser error i Person-Api ApplicationContextTest

--- apps/person-api/src/test/java/no/nav/registre/testnorge/person/ApplicationContextTest.java
+++ apps/person-api/src/test/java/no/nav/registre/testnorge/person/ApplicationContextTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -11,6 +12,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWireMock(port = 0)
 @TestPropertySource(locations = "classpath:application-test.properties")
+@ActiveProfiles("test")
 public class ApplicationContextTest {
 
     /**
