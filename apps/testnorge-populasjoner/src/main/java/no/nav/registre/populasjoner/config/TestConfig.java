bd5a7888f9814f076d866a1988c907b036fd437a
stigus <stian.gustavsson@nav.no>
2020-07-31 11:27:24 +0200

    Fikset ApplicationContextTest, kjører nå

--- apps/testnorge-populasjoner/src/main/java/no/nav/registre/populasjoner/config/TestConfig.java
+++ apps/testnorge-populasjoner/src/main/java/no/nav/registre/populasjoner/config/TestConfig.java
@@ -1,19 +1,25 @@
 package no.nav.registre.populasjoner.config;
 
+import no.nav.common.utils.Credentials;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.client.RestTemplate;
 
-import no.nav.common.utils.Credentials;
-
 @Configuration
 @Profile("test")
 public class TestConfig {
 
+    @Value("${service.username}")
+    private String user;
+
+    @Value("${service.password}")
+    private String pass;
+
     @Bean
     public Credentials serviceUserCredentials() {
-        return new Credentials("username", "password");
+        return new Credentials(user, pass);
     }
 
     @Bean
