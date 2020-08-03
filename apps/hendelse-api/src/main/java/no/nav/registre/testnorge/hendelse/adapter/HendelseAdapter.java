03c43cc295b3caef40ce862254027e8fc0b3c8ac
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2020-08-03 14:47:19 +0200

    feat: Add search for date, ident and type

--- apps/hendelse-api/src/main/java/no/nav/registre/testnorge/hendelse/adapter/HendelseAdapter.java
+++ apps/hendelse-api/src/main/java/no/nav/registre/testnorge/hendelse/adapter/HendelseAdapter.java
@@ -5,12 +5,20 @@ import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
 
+import java.sql.Date;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import no.nav.registre.testnorge.dto.hendelse.v1.HendelseType;
 import no.nav.registre.testnorge.hendelse.domain.Hendelse;
 import no.nav.registre.testnorge.hendelse.repository.HendelseRepository;
+import no.nav.registre.testnorge.hendelse.repository.model.HendelseModel;
 
+
+/**
+ * Opptimalisert søk i databasen istede for å filtere i java service'en. Derfor er det mange kombinasjoner som nesten er like.
+ */
 @Slf4j
 @Component
 @RequiredArgsConstructor
@@ -24,19 +32,62 @@ public class HendelseAdapter {
         hendelseRepository.save(hendelse.toHendelseModel());
     }
 
-    public List<Hendelse> hentHendelser(String ident) {
-        return hendelseRepository
-                .findByIdent(ident)
-                .stream()
-                .map(Hendelse::new)
-                .collect(Collectors.toList());
+    public List<Hendelse> hentHendelser(HendelseType hendelse, String ident, LocalDate localDate) {
+        if (localDate == null && ident == null && hendelse == null) {
+            return hentHendelser();
+        }
+        if (localDate == null && ident == null) {
+            return hentHendelser(hendelse);
+        }
+        if (localDate == null && hendelse == null) {
+            return hentHendelser(ident);
+        }
+        if (ident == null && hendelse == null) {
+            return hentHendelser(localDate);
+        }
+
+        if (localDate == null) {
+            return hentHendelser(hendelse, ident);
+        }
+        if (hendelse == null) {
+            return hentHendelser(ident, localDate);
+        }
+        if (ident == null) {
+            return hentHendelser(hendelse, localDate);
+        }
+        return convert(hendelseRepository.findBy(ident, Date.valueOf(localDate), hendelse));
     }
 
-    public List<Hendelse> hentHendelser() {
-        return hendelseRepository
-                .findAll()
-                .stream()
-                .map(Hendelse::new)
-                .collect(Collectors.toList());
+
+    private List<Hendelse> hentHendelser(String ident, LocalDate localDate) {
+        return convert(hendelseRepository.findBy(ident, Date.valueOf(localDate)));
+    }
+
+    private List<Hendelse> hentHendelser(HendelseType type) {
+        return convert(hendelseRepository.findByHendelse(type));
+    }
+
+    private List<Hendelse> hentHendelser(HendelseType type, String ident) {
+        return convert(hendelseRepository.findByIdentAndHendelse(ident, type));
+    }
+
+    private List<Hendelse> hentHendelser(HendelseType type, LocalDate localDate) {
+        return convert(hendelseRepository.findBy(Date.valueOf(localDate), type));
+    }
+
+    private List<Hendelse> hentHendelser(String ident) {
+        return convert(hendelseRepository.findByIdent(ident));
+    }
+
+    private List<Hendelse> hentHendelser(LocalDate localDate) {
+        return convert(hendelseRepository.findBy(Date.valueOf(localDate)));
+    }
+
+    private List<Hendelse> hentHendelser() {
+        return convert(hendelseRepository.findAll());
+    }
+
+    public List<Hendelse> convert(List<HendelseModel> models) {
+        return models.stream().map(Hendelse::new).collect(Collectors.toList());
     }
 }
