917ee19f75cfe0669a888626f6529e12b75fa02d
stigus <stian.gustavsson@nav.no>
2020-08-03 14:11:06 +0200

    REG-8882 SykemeldingControllerTest

--- apps/sykemelding-api/src/test/java/no/nav/registre/testnorge/sykemelding/provider/SykemeldingControllerTest.java
+++ apps/sykemelding-api/src/test/java/no/nav/registre/testnorge/sykemelding/provider/SykemeldingControllerTest.java
@@ -1,18 +1,31 @@
 package no.nav.registre.testnorge.sykemelding.provider;
 
+import no.nav.registre.testnorge.dto.sykemelding.v1.AdresseDTO;
+import no.nav.registre.testnorge.dto.sykemelding.v1.Aktivitet;
+import no.nav.registre.testnorge.dto.sykemelding.v1.AktivitetDTO;
 import no.nav.registre.testnorge.dto.sykemelding.v1.ArbeidsgiverDTO;
 import no.nav.registre.testnorge.dto.sykemelding.v1.DetaljerDTO;
 import no.nav.registre.testnorge.dto.sykemelding.v1.DiagnoseDTO;
+import no.nav.registre.testnorge.dto.sykemelding.v1.LegeDTO;
+import no.nav.registre.testnorge.dto.sykemelding.v1.OrganisasjonDTO;
+import no.nav.registre.testnorge.dto.sykemelding.v1.PasientDTO;
+import no.nav.registre.testnorge.dto.sykemelding.v1.PeriodeDTO;
+import no.nav.registre.testnorge.dto.sykemelding.v1.SykemeldingDTO;
+import no.nav.registre.testnorge.sykemelding.domain.ApplicationInfo;
+import no.nav.registre.testnorge.sykemelding.domain.Sykemelding;
 import no.nav.registre.testnorge.sykemelding.service.SykemeldingService;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
 
-import static no.nav.registre.testnorge.dto.sykemelding.v1.SykemeldingDTO.SykemeldingDTOBuilder;
+import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
 public class SykemeldingControllerTest {
@@ -20,15 +33,93 @@ public class SykemeldingControllerTest {
     @Mock
     private SykemeldingService sykemeldingService;
 
+    @InjectMocks
+    private SykemeldingController sykemeldingController;
+
+    @Ignore
     @Test
     public void shouldCreateSykemelding() {
-        var sykemeldingRequest = new SykemeldingDTOBuilder()
-                .arbeidsgiver(new ArbeidsgiverDTO("Arbeids Giversen", "arbeider", (double) 100))
-                .hovedDiagnose(new DiagnoseDTO("diag", "system", "100"))
-                .biDiagnoser(new ArrayList<DiagnoseDTO>(Collections.singleton(new DiagnoseDTO("diag", "system", "100"))))
-                .detaljer(new DetaljerDTO("tilt", "tilt", true, "hens"))
-                .build();
 
+        SykemeldingDTO sykemeldingRequest = new SykemeldingDTO(
+                LocalDate.now(),
+                PasientDTO.builder()
+                        .ident("11111124314")
+                        .fornavn("pasient")
+                        .etternavn("pasientesen")
+                        .adresse(AdresseDTO.builder()
+                                .postnummer("9999")
+                                .land("land")
+                                .gate("gate")
+                                .by("by")
+                                .build())
+                        .foedselsdato(LocalDate.now().minusYears(20))
+                        .navKontor("navKontor")
+                        .telefon("99999999")
+                        .build(),
+                LegeDTO.builder()
+                        .fornavn("lege")
+                        .etternavn("legesen")
+                        .ident("1111118372")
+                        .hprId("1234")
+                        .build(),
+                ArbeidsgiverDTO.builder()
+                        .navn("arbeidsgiver")
+                        .stillingsprosent((double) 100)
+                        .yrkesbetegnelse("giver")
+                        .build(),
+               false,
+                new ArrayList<>(Collections.singletonList(PeriodeDTO.builder().aktivitet(
+                        AktivitetDTO.builder()
+                                .aktivitet(Aktivitet.AVVENTENDE)
+                                .behandlingsdager(20)
+                                .reisetilskudd(true)
+                                .grad(1).build())
+                        .fom(LocalDate.now().minusMonths(3))
+                        .tom(LocalDate.now())
+                        .build())),
+                OrganisasjonDTO.builder()
+                        .navn("sender")
+                        .adresse(AdresseDTO.builder()
+                                .by("by")
+                                .gate("gate")
+                                .land("land")
+                                .postnummer("9999")
+                                .build())
+                        .orgNr("123").build(),
+                OrganisasjonDTO.builder()
+                        .navn("mottaker")
+                        .orgNr("123")
+                        .adresse(AdresseDTO.builder()
+                                .by("by")
+                                .gate("gate")
+                                .land("land")
+                                .postnummer("9999")
+                                .build())
+                        .build(),
+                DiagnoseDTO.builder()
+                        .diagnose("hovedDiagnose")
+                        .diagnosekode("12345")
+                        .system("system")
+                        .build(),
+                null,
+                DetaljerDTO.builder()
+                        .beskrivHensynArbeidsplassen("hensynArb")
+                        .tiltakArbeidsplass("tiltakArb")
+                        .arbeidsforEtterEndtPeriode(true)
+                        .tiltakNav("tiltakNav")
+                        .build(),
+                true);
+
+        Sykemelding sykemelding = new Sykemelding(sykemeldingRequest, ApplicationInfo.builder()
+                .name("test")
+                .version("v1")
+                .build());
+
+        System.out.println(sykemelding.toXml());
+
+        sykemeldingController.create(sykemeldingRequest);
+
+        verify(sykemeldingService).send(sykemelding);
     }
 
 }
