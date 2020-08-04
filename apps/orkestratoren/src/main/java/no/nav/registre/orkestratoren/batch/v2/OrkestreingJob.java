65caeda5dcb497cf299ecbdd6633b75e93d9c6c1
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2020-08-04 12:08:44 +0200

    feat: setup cron job

--- apps/orkestratoren/src/main/java/no/nav/registre/orkestratoren/batch/v2/OrkestreingJob.java
+++ apps/orkestratoren/src/main/java/no/nav/registre/orkestratoren/batch/v2/OrkestreingJob.java
@@ -1,13 +1,24 @@
 package no.nav.registre.orkestratoren.batch.v2;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import no.nav.registre.orkestratoren.provider.rs.requests.SyntetiserFrikortRequest;
+import no.nav.registre.orkestratoren.service.SykemeldingOrkestreringsService;
 
 @Component
 @EnableScheduling
 @Slf4j
+@RequiredArgsConstructor
 public class OrkestreingJob {
 
+    private final SykemeldingOrkestreringsService sykemeldingOrkestreringsService;
 
+    @Scheduled(cron = "0 0 7 * * *")
+    public void sykemeldingOrkestreringsBatch() {
+        sykemeldingOrkestreringsService.orkistrer();
+    }
 }
