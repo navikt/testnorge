3bb16736f6a253a9c36d96f68db64f7271314ea1
Stian <stian.gustavsson@nav.no>
2020-08-11 10:56:58 +0200

    REG-8717 Skrevet om tester, fjernet unødvendig test

--- dolly-backend-app/src/test/java/no/nav/dolly/bestilling/skjermingsregister/SkjermingsRegisterClientTest.java
+++ dolly-backend-app/src/test/java/no/nav/dolly/bestilling/skjermingsregister/SkjermingsRegisterClientTest.java
@@ -88,21 +88,11 @@ public class SkjermingsRegisterClientTest {
     }
 
     @Test
-    public void should_return_ok_for_get_aktiv_skjermet_eksisterer_allerede() {
-
-        BestillingProgress progress = new BestillingProgress();
-        when(skjermingsRegisterConsumer.postSkjerming(any())).thenReturn(new ResponseEntity<>(Collections.singletonList(response), HttpStatus.BAD_REQUEST));
-        when(skjermingsRegisterConsumer.getSkjerming(any())).thenReturn(new ResponseEntity<>(response, HttpStatus.OK));
-
-        skjermingsRegisterClient.gjenopprett(bestilling, person, progress, false);
-        assertThat(progress.getSkjermingsregisterStatus()).isNotNull().isEqualTo("OK");
-    }
-
-    @Test
     public void should_return_created_for_post_aktiv_skjermet() {
 
         ResponseEntity<List<SkjermingsDataResponse>> responseEntity;
         when(skjermingsRegisterConsumer.postSkjerming(any())).thenReturn(responseEntity = new ResponseEntity<>(Collections.singletonList(response), HttpStatus.CREATED));
+        when(skjermingsRegisterConsumer.getSkjerming(any())).thenReturn(new ResponseEntity<>(response, HttpStatus.NOT_FOUND));
 
         skjermingsRegisterClient.gjenopprett(bestilling, person, new BestillingProgress(), false);
         assertThat(responseEntity.getStatusCode()).isNotNull().isEqualTo(HttpStatus.CREATED);
@@ -114,6 +104,7 @@ public class SkjermingsRegisterClientTest {
         person.getPersondetaljer().get(0).setEgenAnsattDatoTom(now().minusDays(1));
         ResponseEntity<String> responseEntity;
         when(skjermingsRegisterConsumer.putSkjerming(any())).thenReturn(responseEntity = new ResponseEntity<>("", HttpStatus.OK));
+        when(skjermingsRegisterConsumer.getSkjerming(any())).thenReturn(new ResponseEntity<>(response, HttpStatus.OK));
 
         skjermingsRegisterClient.gjenopprett(bestilling, person, new BestillingProgress(), false);
         assertThat(responseEntity.getStatusCode()).isNotNull().isEqualTo(HttpStatus.OK);
