aa0bb515a9db8bc835de9e0a5bb523df476f76b7
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2020-08-12 08:08:55 +0200

    feat: Legg til scheduling i rapportering

--- apps/rapportering-api/src/main/java/no/nav/registre/testnorge/rapportering/config/AppConfig.java
+++ apps/rapportering-api/src/main/java/no/nav/registre/testnorge/rapportering/config/AppConfig.java
@@ -3,11 +3,13 @@ package no.nav.registre.testnorge.rapportering.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import no.nav.registere.testnorge.core.ApplicationCoreConfig;
 
 @EnableJpaAuditing
 @Configuration
+@EnableScheduling
 @Import(ApplicationCoreConfig.class)
 public class AppConfig {
 }
