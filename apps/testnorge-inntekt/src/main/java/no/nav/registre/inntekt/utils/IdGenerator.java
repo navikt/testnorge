d65844bbdc63a2773f4531116b040569e84503f5
K156575 <baard.krohg@gmail.com>
2020-08-19 13:11:12 +0200

    La til robusthet for randomNumberString metoden, slik at man ikke kan lage strenger med negativ eller null lengde.

--- apps/testnorge-inntekt/src/main/java/no/nav/registre/inntekt/utils/IdGenerator.java
+++ apps/testnorge-inntekt/src/main/java/no/nav/registre/inntekt/utils/IdGenerator.java
@@ -4,6 +4,10 @@ import java.util.Random;
 
 public class IdGenerator {
     public static String randomNumberString(int length) {
+        if (length <= 0) {
+            throw new RuntimeException("Lengde av streng kan ikke være mindre eller lik null.");
+        }
+
         Random r = new Random();
         int min = (int)Math.pow(10, length-1);
         long n = min + r.nextInt(min*9);
