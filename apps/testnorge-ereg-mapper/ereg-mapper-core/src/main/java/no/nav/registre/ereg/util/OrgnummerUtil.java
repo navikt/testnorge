a05b8f7d99dfe3d38d6eceb6ae9772c237c5e0b8
S152486 <ingvild.stoen@nav.no>
2020-08-19 13:25:08 +0200

    Gi gyldige orgnr i generator

--- apps/testnorge-ereg-mapper/ereg-mapper-core/src/main/java/no/nav/registre/ereg/util/OrgnummerUtil.java
+++ apps/testnorge-ereg-mapper/ereg-mapper-core/src/main/java/no/nav/registre/ereg/util/OrgnummerUtil.java
@@ -1,9 +1,6 @@
 package no.nav.registre.ereg.util;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.concurrent.ThreadLocalRandom;
@@ -19,10 +16,14 @@ public class OrgnummerUtil {
         int random = ThreadLocalRandom.current().nextInt(80000000, 99999999);
         int weightedSum = 0;
         String randomString = String.valueOf(random);
+
         for (int index = 0; index < randomString.length(); index++) {
-            weightedSum += ((int) weights.charAt(index) + (int) randomString.charAt(index));
+            int vekt = Character.getNumericValue(weights.charAt(index));
+            int numericValue = Character.getNumericValue(randomString.charAt(index));
+
+            weightedSum += (vekt * numericValue);
         }
-        int controlDigit = (weightedSum % 11) - 6;
+        int controlDigit = 11 - (weightedSum % 11);
 
         if (controlDigit == 1 || controlDigit < 0) {
             return generate(restTemplate);
@@ -30,21 +31,21 @@ public class OrgnummerUtil {
 
         String withControl = randomString + controlDigit;
 
-        try {
-            ResponseEntity<Object> responseEntityq2 = restTemplate.getForEntity("https://modapp-q2.adeo.no/ereg/api/v1/organisasjon/"
-                    + withControl +
-                    "?inkluderHierarki=false&inkluderHistorikk=false", Object.class);
-
-            ResponseEntity<Object> responseEntityq0 = restTemplate.getForEntity("https://modapp-q0.adeo.no/ereg/api/v1/organisasjon/"
-                    + withControl +
-                    "?inkluderHierarki=false&inkluderHistorikk=false", Object.class);
-
-            if (responseEntityq0.getStatusCode() == HttpStatus.OK || responseEntityq2.getStatusCode() == HttpStatus.OK) {
-                return generate(restTemplate);
-            }
-        } catch (HttpClientErrorException e) {
-            return withControl;
-        }
+//        try {
+//            ResponseEntity<Object> responseEntityq2 = restTemplate.getForEntity("https://modapp-q2.adeo.no/ereg/api/v1/organisasjon/"
+//                    + withControl +
+//                    "?inkluderHierarki=false&inkluderHistorikk=false", Object.class);
+//
+//            ResponseEntity<Object> responseEntityq0 = restTemplate.getForEntity("https://modapp-q0.adeo.no/ereg/api/v1/organisasjon/"
+//                    + withControl +
+//                    "?inkluderHierarki=false&inkluderHistorikk=false", Object.class);
+//
+//            if (responseEntityq0.getStatusCode() == HttpStatus.OK || responseEntityq2.getStatusCode() == HttpStatus.OK) {
+//                return generate(restTemplate);
+//            }
+//        } catch (HttpClientErrorException e) {
+//            return withControl;
+//        }
         return withControl;
 
     }
