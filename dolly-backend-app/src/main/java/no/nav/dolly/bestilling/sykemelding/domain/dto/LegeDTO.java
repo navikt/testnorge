fa8613a3d52c3d27571cc2c81472eb8276d9d96e
Stian <stian.gustavsson@nav.no>
2020-08-20 15:11:35 +0200

    Fikset LegeDto error

--- dolly-backend-app/src/main/java/no/nav/dolly/bestilling/sykemelding/domain/dto/LegeDTO.java
+++ dolly-backend-app/src/main/java/no/nav/dolly/bestilling/sykemelding/domain/dto/LegeDTO.java
@@ -1,9 +1,13 @@
 package no.nav.dolly.bestilling.sykemelding.domain.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
 import lombok.Value;
 
 @Value
+@NoArgsConstructor(force = true)
+@AllArgsConstructor
 @Builder
 public class LegeDTO {
     String fnr;
--- dolly-backend-app/src/main/java/no/nav/dolly/bestilling/sykemelding/domain/dto/LegeListeDTO.java
+++ dolly-backend-app/src/main/java/no/nav/dolly/bestilling/sykemelding/domain/dto/LegeListeDTO.java
@@ -2,11 +2,13 @@ package no.nav.dolly.bestilling.sykemelding.domain.dto;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.Value;
 
 @Value
 @NoArgsConstructor(force = true)
+@AllArgsConstructor
 public class LegeListeDTO {
     List<LegeDTO> leger;
 }
