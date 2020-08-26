a1794efe5ce113fd382e736da4ccbe6813cf6aea
Stian <stian.gustavsson@nav.no>
2020-08-26 09:35:37 +0200

    Kommet videre på graphql query

--- dolly-backend-app/src/main/java/no/nav/dolly/bestilling/saf/SafConsumer.java
+++ dolly-backend-app/src/main/java/no/nav/dolly/bestilling/saf/SafConsumer.java
@@ -6,19 +6,29 @@ import static no.nav.dolly.domain.CommonKeys.HEADER_NAV_CALL_ID;
 import static no.nav.dolly.domain.CommonKeys.HEADER_NAV_CONSUMER_ID;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
+import org.apache.http.Consts;
 import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.google.common.io.CharStreams;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import no.nav.dolly.bestilling.saf.domain.SafMetadataRequest;
 import no.nav.dolly.bestilling.saf.domain.SafRequest;
+import no.nav.dolly.consumer.pdlperson.GraphQLRequest;
 import no.nav.dolly.metrics.Timed;
 import no.nav.dolly.properties.ProvidersProps;
 import no.nav.dolly.security.sts.StsOidcService;
@@ -53,7 +63,25 @@ public class SafConsumer {
     }
 
     @Timed(name = "providers", tags = { "operation", "hent-dokument" })
-    public ResponseEntity<String> getMetadata(String environment, SafMetadataRequest request) {
+    public ResponseEntity<JsonNode> getMetadata(String environment, String ident) {
+
+        Map<String, Object> variables = new HashMap<>();
+        variables.put("ident", ident);
+        variables.put("historikk", true);
+
+        String query = null;
+        InputStream queryStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("pdlperson/pdlquery.graphql");
+        try {
+            Reader reader = new InputStreamReader(queryStream, Consts.UTF_8);
+            query = CharStreams.toString(reader);
+        } catch (IOException e) {
+            log.error("Lesing av query ressurs feilet");
+        }
+
+        GraphQLRequest graphQLRequest = GraphQLRequest.builder()
+                .query(query)
+                .variables(variables)
+                .build();
 
         String callId = getNavCallId();
         log.info("SafMetadataRequest sendt, callId: {}, consumerId: {}, miljø: {}", callId, CONSUMER, environment);
@@ -64,8 +92,9 @@ public class SafConsumer {
                         .header(AUTHORIZATION,stsOidcService.getIdToken(environment.contains(PREPROD_ENV) ? PREPROD_ENV : TEST_ENV))
                         .header(HEADER_NAV_CALL_ID, callId)
                         .header(HEADER_NAV_CONSUMER_ID, CONSUMER)
-                        .contentType(MediaType.APPLICATION_JSON).build(),
-                String.class);
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(graphQLRequest),
+                JsonNode.class);
     }
 
     private static String getNavCallId() {
