d77782841995257f00d08a08ef366be4780b4007
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2020-09-08 18:20:12 +0200

    Resgister requests

--- libs/testnorge-core/src/main/java/no/nav/registre/testnorge/libs/core/config/ApplicationCoreConfig.java
+++ libs/testnorge-core/src/main/java/no/nav/registre/testnorge/libs/core/config/ApplicationCoreConfig.java
@@ -3,10 +3,11 @@ package no.nav.registre.testnorge.libs.core.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
-import no.nav.registre.testnorge.libs.dependencyanalysis.provider.DependenciesController;
+import no.nav.registre.testnorge.libs.config.LoggingWebConfig;
 import no.nav.registre.testnorge.libs.core.provider.InternalController;
+import no.nav.registre.testnorge.libs.dependencyanalysis.provider.DependenciesController;
 
 @Configuration
-@Import({InternalController.class, DependenciesController.class, ApplicationProperties.class})
+@Import({InternalController.class, DependenciesController.class, ApplicationProperties.class, LoggingWebConfig.class})
 public class ApplicationCoreConfig {
 }
