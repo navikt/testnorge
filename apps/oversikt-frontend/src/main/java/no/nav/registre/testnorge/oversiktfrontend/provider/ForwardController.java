59a73e28da6cea5f21ef7df28b1df115b72579f7
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2020-09-18 10:12:26 +0200

    fiks

--- apps/oversikt-frontend/src/main/java/no/nav/registre/testnorge/oversiktfrontend/provider/ForwardController.java
+++ apps/oversikt-frontend/src/main/java/no/nav/registre/testnorge/oversiktfrontend/provider/ForwardController.java
@@ -1,10 +1,11 @@
 package no.nav.registre.testnorge.oversiktfrontend.provider;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class ForwardController {
+
     @RequestMapping(value = {"/app/**"})
     public String forwardToRoot() {
         return "forward:/";
