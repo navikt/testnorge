ea093c9b1826ae96bcee61f5d9bc9ceb6582d961
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2020-09-22 12:02:38 +0200

    init vault token

--- apps/opprett-person-pdl/src/main/java/no/nav/registre/testnorge/opprettpersonpdl/OpprettPersonPdlApplicationStarter.java
+++ apps/opprett-person-pdl/src/main/java/no/nav/registre/testnorge/opprettpersonpdl/OpprettPersonPdlApplicationStarter.java
@@ -4,9 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+import no.nav.registre.testnorge.libs.core.util.VaultUtil;
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class OpprettPersonPdlApplicationStarter {
     public static void main(String[] args) {
+        if ("prod".equals(System.getProperty("spring.profiles.active"))) {
+            VaultUtil.initCloudVaultToken();
+        }
         SpringApplication.run(OpprettPersonPdlApplicationStarter.class, args);
     }
 }
