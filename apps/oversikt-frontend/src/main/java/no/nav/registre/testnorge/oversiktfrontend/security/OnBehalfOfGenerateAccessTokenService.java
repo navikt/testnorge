74c55b804a70fe41b87f9cfc700b67c36855f8a3
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2020-09-22 13:27:20 +0200

    Legg til oversikt-frontend (#240)

--- apps/oversikt-frontend/src/main/java/no/nav/registre/testnorge/oversiktfrontend/security/OnBehalfOfGenerateAccessTokenService.java
+++ apps/oversikt-frontend/src/main/java/no/nav/registre/testnorge/oversiktfrontend/security/OnBehalfOfGenerateAccessTokenService.java
@@ -1,4 +1,4 @@
-package no.nav.registre.testnorge.libs.oauth2.service;
+package no.nav.registre.testnorge.oversiktfrontend.security;
 
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
@@ -6,8 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -20,18 +19,23 @@ import java.util.Map;
 import no.nav.registre.testnorge.libs.oauth2.domain.AccessScopes;
 import no.nav.registre.testnorge.libs.oauth2.domain.AccessToken;
 import no.nav.registre.testnorge.libs.oauth2.domain.ClientCredential;
+import no.nav.registre.testnorge.oversiktfrontend.config.OversiktFrontendClientCredentials;
+
 
 @Slf4j
 @Service
 public class OnBehalfOfGenerateAccessTokenService {
     private final WebClient webClient;
     private final AuthenticationTokenResolver tokenResolver;
+    private final ClientCredential clientCredential;
 
     public OnBehalfOfGenerateAccessTokenService(
             @Value("${http.proxy:#{null}}") String proxyHost,
             @Value("${AAD_ISSUER_URI}") String issuerUrl,
-            SecureAuthenticationTokenResolver tokenResolver
+            OversiktFrontendClientCredentials clientCredential,
+            AuthenticationTokenResolver tokenResolver
     ) {
+        this.clientCredential = clientCredential;
 
         WebClient.Builder builder = WebClient
                 .builder()
@@ -39,7 +43,7 @@ public class OnBehalfOfGenerateAccessTokenService {
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE);
 
         if (proxyHost != null) {
-            log.info("Setter opp proxy host {} for OAuth 2.0 On-Behalf-Of Flow", proxyHost);
+            log.info("Setter opp proxy host {} for Client Credentials", proxyHost);
             var uri = URI.create(proxyHost);
 
             HttpClient httpClient = HttpClient
@@ -56,26 +60,17 @@ public class OnBehalfOfGenerateAccessTokenService {
         this.webClient = builder.build();
     }
 
-    public AccessToken generateToken(ClientCredential clientCredential, AccessScopes accessScopes) {
-        if (tokenResolver.isClientCredentials()) {
-            throw new BadCredentialsException("Kan ikke gjennomfore On Behalf of fra Client Credentials.");
-        }
+    public AccessToken generateToken(AccessScopes accessScopes) {
         if (accessScopes.getScopes().isEmpty()) {
             throw new RuntimeException("Kan ikke opprette accessToken uten clients");
         }
-        tokenResolver.verifyAuthentication();
-
-        Map<String, String> contextMap = MDC.getCopyOfContextMap();
-        contextMap.put("oid", tokenResolver.getOid());
-        MDC.setContextMap(contextMap);
-
-        JwtAuthenticationToken jwtAuthenticationToken = tokenResolver.jwtAuthenticationToken();
+        OAuth2AccessToken accessToken = tokenResolver.getToken();
 
         var body = BodyInserters
                 .fromFormData("scope", String.join(" ", accessScopes.getScopes()))
                 .with("client_id", clientCredential.getClientId())
                 .with("client_secret", clientCredential.getClientSecret())
-                .with("assertion", jwtAuthenticationToken.getToken().getTokenValue())
+                .with("assertion", accessToken.getTokenValue())
                 .with("requested_token_use", "on_behalf_of")
                 .with("grant_type", "urn:ietf:params:oauth:grant-type:jwt-bearer");
 
