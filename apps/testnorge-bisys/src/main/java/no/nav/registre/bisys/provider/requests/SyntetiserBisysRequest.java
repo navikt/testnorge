e2ac07db9945f954c70fa019204af28c1b9f8dd1
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2020-09-24 08:50:34 +0200

    Fiks bisys

--- apps/testnorge-bisys/src/main/java/no/nav/registre/bisys/provider/requests/SyntetiserBisysRequest.java
+++ apps/testnorge-bisys/src/main/java/no/nav/registre/bisys/provider/requests/SyntetiserBisysRequest.java
@@ -3,8 +3,10 @@ package no.nav.registre.bisys.provider.requests;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class SyntetiserBisysRequest {
 
