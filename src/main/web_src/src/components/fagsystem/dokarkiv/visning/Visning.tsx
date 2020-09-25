dc1cc4934a5334aaf60739293ebaade3945819d1
Stian <stian.gustavsson@nav.no>
2020-09-25 10:40:07 +0200

    Merget inn master, lagt til ErrorBoundary på nye komponenter

--- src/main/web_src/src/components/fagsystem/dokarkiv/visning/Visning.tsx
+++ src/main/web_src/src/components/fagsystem/dokarkiv/visning/Visning.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import _get from 'lodash/get'
 import SubOverskrift from '~/components/ui/subOverskrift/SubOverskrift'
 import { TitleValue } from '~/components/ui/titleValue/TitleValue'
 import { DollyApi } from '~/service/Api'
 import LoadableComponent from '~/components/ui/loading/LoadableComponent'
 import { DollyFieldArray } from '~/components/ui/form/fieldArray/DollyFieldArray'
+import { ErrorBoundary } from '~/components/ui/appError/ErrorBoundary'
 
 interface DokarkivVisning {
 	ident: string
@@ -52,62 +52,66 @@ export const DokarkivVisning = ({ ident }: DokarkivVisning) => {
 	// Viser data fra Joark Dokumentinfo
 	return (
 		<div>
-			<LoadableComponent
-				onFetch={() =>
-					DollyApi.getTransaksjonid('DOKARKIV', ident)
-						.then(({ data }: { data: Array<TransaksjonId> }) => {
-							return data.map((bestilling: TransaksjonId) => {
-								return DollyApi.getDokarkivDokumentinfo(
-									bestilling.transaksjonId.journalpostId,
-									bestilling.miljoe
-								)
-									.then((response: Dokumentinfo) => {
-										if (response) {
-											if (response.data.feil) {
-												return response.data
+			<ErrorBoundary>
+				<LoadableComponent
+					onFetch={() =>
+						DollyApi.getTransaksjonid('DOKARKIV', ident)
+							.then(({ data }: { data: Array<TransaksjonId> }) => {
+								return data.map((bestilling: TransaksjonId) => {
+									return DollyApi.getDokarkivDokumentinfo(
+										bestilling.transaksjonId.journalpostId,
+										bestilling.miljoe
+									)
+										.then((response: Dokumentinfo) => {
+											if (response) {
+												if (response.data.feil) {
+													return response.data
+												}
+												const journalpost = response.data.data.journalpost
+												return {
+													kanal: journalpost.kanalnavn,
+													brevkode: journalpost.dokumenter[0].brevkode,
+													tittel: journalpost.dokumenter[0].tittel,
+													tema: journalpost.temanavn,
+													journalfoerendeEnhet: journalpost.journalfoerendeEnhet,
+													journalpostId: journalpost.journalpostId,
+													dokumentInfoId: journalpost.dokumenter[0].dokumentInfoId,
+													miljoe: bestilling.miljoe
+												}
 											}
-											const journalpost = response.data.data.journalpost
-											return {
-												kanal: journalpost.kanalnavn,
-												brevkode: journalpost.dokumenter[0].brevkode,
-												tittel: journalpost.dokumenter[0].tittel,
-												tema: journalpost.temanavn,
-												journalfoerendeEnhet: journalpost.journalfoerendeEnhet,
-												journalpostId: journalpost.journalpostId,
-												dokumentInfoId: journalpost.dokumenter[0].dokumentInfoId,
-												miljoe: bestilling.miljoe
-											}
-										}
-									})
-									.catch(error => console.error(error))
+										})
+										.catch(error => console.error(error))
+								})
 							})
-						})
-						.then((data: Array<Promise<any>>) => {
-							return Promise.all(data)
-						})
-				}
-				render={(data: Array<Dokument>) =>
-					data &&
-					data.length > 0 && (
-						<>
-							<SubOverskrift label="Dokumenter" iconKind="dokarkiv" />
-							{data.length > 1 ? (
-								<DollyFieldArray data={data} nested>
-									{(dokument: Dokument, idx: number) => (
-										<div key={idx} className="person-visning_content">
-											<EnkelDokarkivVisning dokument={dokument} />
+							.then((data: Array<Promise<any>>) => {
+								return Promise.all(data)
+							})
+					}
+					render={(data: Array<Dokument>) =>
+						data &&
+						data.length > 0 && (
+							<ErrorBoundary>
+								<>
+									<SubOverskrift label="Dokumenter" iconKind="dokarkiv" />
+									{data.length > 1 ? (
+										<DollyFieldArray data={data} nested>
+											{(dokument: Dokument, idx: number) => (
+												<div key={idx} className="person-visning_content">
+													<EnkelDokarkivVisning dokument={dokument} />
+												</div>
+											)}
+										</DollyFieldArray>
+									) : (
+										<div className="person-visning_content">
+											<EnkelDokarkivVisning dokument={data[0]} />
 										</div>
 									)}
-								</DollyFieldArray>
-							) : (
-								<div className="person-visning_content">
-									<EnkelDokarkivVisning dokument={data[0]} />
-								</div>
-							)}
-						</>
-					)
-				}
-			/>
+								</>
+							</ErrorBoundary>
+						)
+					}
+				/>
+			</ErrorBoundary>
 		</div>
 	)
 }
@@ -118,16 +122,18 @@ const EnkelDokarkivVisning = ({ dokument }: EnkeltDokument) => {
 			return <p style={{ margin: 0 }}>{dokument.feil}</p>
 		}
 		return (
-			<>
-				<TitleValue title="Kanal" value={dokument.kanal} />
-				<TitleValue title="Brevkode" value={dokument.brevkode} />
-				<TitleValue title="Tittel" value={dokument.tittel} />
-				<TitleValue title="Tema" value={dokument.tema} />
-				<TitleValue title="Journalførende enhet" value={dokument.journalfoerendeEnhet} />
-				<TitleValue title="Journalpost-ID" value={dokument.journalpostId} />
-				<TitleValue title="Dokumentinfo-ID" value={dokument.dokumentInfoId} />
-				<TitleValue title="Miljø" value={dokument.miljoe} />
-			</>
+			<ErrorBoundary>
+				<>
+					<TitleValue title="Kanal" value={dokument.kanal} />
+					<TitleValue title="Brevkode" value={dokument.brevkode} />
+					<TitleValue title="Tittel" value={dokument.tittel} />
+					<TitleValue title="Tema" value={dokument.tema} />
+					<TitleValue title="Journalførende enhet" value={dokument.journalfoerendeEnhet} />
+					<TitleValue title="Journalpost-ID" value={dokument.journalpostId} />
+					<TitleValue title="Dokumentinfo-ID" value={dokument.dokumentInfoId} />
+					<TitleValue title="Miljø" value={dokument.miljoe} />
+				</>
+			</ErrorBoundary>
 		)
 	}
 }
