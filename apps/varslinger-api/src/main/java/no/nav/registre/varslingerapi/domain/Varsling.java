1fa8b0d32207c51d47b657dfd23a23cd89aeafc2
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2020-09-28 17:10:19 +0200

    Opprett varslinger

---
+++ apps/varslinger-api/src/main/java/no/nav/registre/varslingerapi/domain/Varsling.java
@@ -0,0 +1,54 @@
+package no.nav.registre.varslingerapi.domain;
+
+
+import lombok.Value;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+
+import no.nav.registre.testnorge.libs.dto.varslingerapi.v1.VarslingDTO;
+import no.nav.registre.varslingerapi.repository.model.VarslingModel;
+
+@Value
+public class Varsling {
+
+    String varslingId;
+    LocalDate fom;
+    LocalDate tom;
+
+    public Varsling(VarslingModel model) {
+        this.varslingId = model.getVarslingId();
+        this.fom = toLocalDate(model.getFom());
+        this.tom = toLocalDate(model.getTom());
+    }
+
+
+    public Varsling(VarslingDTO dto) {
+        this.varslingId = dto.getVarslingId();
+        this.fom = dto.getFom();
+        this.tom = dto.getTom();
+    }
+
+    private static LocalDate toLocalDate(Date date) {
+        return date != null ? date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate() : null;
+    }
+
+    public VarslingDTO toDTO() {
+        return VarslingDTO
+                .builder()
+                .varslingId(varslingId)
+                .fom(fom)
+                .tom(tom)
+                .build();
+    }
+
+
+    public VarslingModel toModel() {
+        return VarslingModel
+                .builder()
+                .varslingId(varslingId)
+                .build();
+    }
+
+}
