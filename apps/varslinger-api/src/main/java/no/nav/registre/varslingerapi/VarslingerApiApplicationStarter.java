08737260e91a81c8a33dbba2b7f0712971b78528
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2020-09-29 08:36:30 +0200

    Add tokens

--- apps/varslinger-api/src/main/java/no/nav/registre/varslingerapi/VarslingerApiApplicationStarter.java
+++ apps/varslinger-api/src/main/java/no/nav/registre/varslingerapi/VarslingerApiApplicationStarter.java
@@ -3,9 +3,16 @@ package no.nav.registre.varslingerapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import no.nav.registre.testnorge.libs.core.util.VaultUtil;
+
 @SpringBootApplication
 public class VarslingerApiApplicationStarter {
     public static void main(String[] args) {
+
+        if ("prod".equals(System.getProperty("spring.profiles.active"))) {
+            VaultUtil.initCloudVaultToken();
+        }
+
         SpringApplication.run(VarslingerApiApplicationStarter.class, args);
     }
 }
