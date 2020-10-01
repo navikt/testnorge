c9c8adc5089fe8ff06300201c53a2561354181eb
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2020-10-01 09:21:21 +0200

    Oppretter testnorge-varslinger-api (#266)

--- libs/testnorge-database/src/main/java/no/nav/registre/testnorge/libs/database/config/FlywayConfiguration.java
+++ libs/testnorge-database/src/main/java/no/nav/registre/testnorge/libs/database/config/FlywayConfiguration.java
@@ -1,17 +1,26 @@
 package no.nav.registre.testnorge.libs.database.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import static java.lang.String.format;
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.flyway.FlywayConfigurationCustomizer;
+import org.springframework.cloud.vault.config.databases.VaultDatabaseProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Slf4j
 @Configuration
-@ConditionalOnProperty(value = {"spring.flyway.enabled", "spring.cloud.vault.database.enabled"} )
+@ConditionalOnProperty("spring.cloud.vault.database.enabled")
 public class FlywayConfiguration {
 
     @Bean
-    public FlywayConfigurationCustomizer flywayConfig(@Value("${spring.cloud.vault.database.role}") String role) {
-        return c -> c.initSql(String.format("SET ROLE \"%s\"", role));
+    public FlywayConfigurationCustomizer flywayConfig(
+            VaultDatabaseProperties properties
+    ) {
+        return configuration -> {
+            configuration.initSql(format("SET ROLE \"%s\"", properties.getRole()));
+            log.info("Flyway configured.");
+        };
     }
 }
