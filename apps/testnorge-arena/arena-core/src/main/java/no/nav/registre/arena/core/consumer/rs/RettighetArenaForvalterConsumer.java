fd16ecf62bc43e8e839fe46cfa1426b025739173
Hay <Aileen.Hay@nav.no>
2020-10-07 12:30:33 +0200

    Kode rydding

--- apps/testnorge-arena/arena-core/src/main/java/no/nav/registre/arena/core/consumer/rs/RettighetArenaForvalterConsumer.java
+++ apps/testnorge-arena/arena-core/src/main/java/no/nav/registre/arena/core/consumer/rs/RettighetArenaForvalterConsumer.java
@@ -1,23 +1,13 @@
 package no.nav.registre.arena.core.consumer.rs;
 
-import static no.nav.registre.arena.core.consumer.rs.util.Headers.CALL_ID;
-import static no.nav.registre.arena.core.consumer.rs.util.Headers.CONSUMER_ID;
-import static no.nav.registre.arena.core.consumer.rs.util.Headers.NAV_CALL_ID;
-import static no.nav.registre.arena.core.consumer.rs.util.Headers.NAV_CONSUMER_ID;
-
 import lombok.extern.slf4j.Slf4j;
 
 import no.nav.registre.arena.core.consumer.rs.command.PostRettighetCommand;
 import no.nav.registre.testnorge.libs.dependencyanalysis.DependencyOn;
 import no.nav.registre.testnorge.domain.dto.arena.testnorge.vedtak.NyttVedtakResponse;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.http.RequestEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.HttpStatusCodeException;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.util.UriTemplate;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -35,7 +25,6 @@ public class RettighetArenaForvalterConsumer {
     private final WebClient webClient;
 
     public RettighetArenaForvalterConsumer(
-            RestTemplateBuilder restTemplateBuilder,
             @Value("${arena-forvalteren.rest-api.url}") String arenaForvalterServerUrl
     ) {
         this.webClient = WebClient.builder().baseUrl(arenaForvalterServerUrl).build();
