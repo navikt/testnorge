0ffb7fac37cdaec3d22c54a9c0bf7ec4367a489a
Hay <Aileen.Hay@nav.no>
2020-10-09 12:57:39 +0200

    Oppdatert tester for webclient

--- apps/testnorge-arena/arena-core/src/test/java/no/nav/registre/arena/core/consumer/rs/RettighetTiltakBrukereArenaForvalterConsumerTest.java
+++ apps/testnorge-arena/arena-core/src/test/java/no/nav/registre/arena/core/consumer/rs/RettighetTiltakBrukereArenaForvalterConsumerTest.java
@@ -7,6 +7,11 @@ import static org.springframework.test.web.client.match.MockRestRequestMatchers.
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestToUriTemplate;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
+import okhttp3.mockwebserver.Dispatcher;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import okhttp3.mockwebserver.RecordedRequest;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,6 +26,7 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.client.MockRestServiceServer;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -29,27 +35,23 @@ import no.nav.registre.arena.core.consumer.rs.request.RettighetRequest;
 import no.nav.registre.arena.core.consumer.rs.request.RettighetTiltaksdeltakelseRequest;
 import no.nav.registre.arena.core.consumer.rs.request.RettighetTiltakspengerRequest;
 
-@RunWith(SpringRunner.class)
-@AutoConfigureWireMock(port = 0)
-@RestClientTest(RettighetArenaForvalterConsumer.class)
 @TestPropertySource(locations = "classpath:application-test.properties")
 @ActiveProfiles("test")
 public class RettighetTiltakBrukereArenaForvalterConsumerTest {
 
-    @Autowired
     private RettighetArenaForvalterConsumer consumer;
 
-    @Autowired
-    private MockRestServiceServer server;
-
-    @Value("${arena-forvalteren.rest-api.url}")
-    private String serverUrl;
+    private MockWebServer mockWebServer;
 
     private List<RettighetRequest> rettigheter;
     private String fnr1 = "270699494213";
 
     @Before
-    public void setUp() {
+    public void setUp() throws IOException {
+        this.mockWebServer = new MockWebServer();
+        this.mockWebServer.start();
+        this.consumer = new RettighetArenaForvalterConsumer(mockWebServer.url("/").toString());
+
         RettighetTiltaksdeltakelseRequest tiltaksdeltakelseRequest = new RettighetTiltaksdeltakelseRequest();
         tiltaksdeltakelseRequest.setPersonident(fnr1);
         RettighetTiltakspengerRequest tiltakspengerRequest = new RettighetTiltakspengerRequest();
@@ -63,30 +65,37 @@ public class RettighetTiltakBrukereArenaForvalterConsumerTest {
 
     @Test
     public void shouldOppretteRettighetTiltak() {
-        stubArenaForvalterOpprettRettighetTiltaksdeltakelse(serverUrl + "/v1/tiltaksdeltakelse");
-        stubArenaForvalterOpprettRettighetTiltakspenger(serverUrl + "/v1/tiltakspenger");
+        stubArenaForvalterOpprettRettighet();
 
         var response = consumer.opprettRettighet(rettigheter);
-
-        server.verify();
 
         assertThat(response.get(fnr1).get(0).getFeiledeRettigheter().size(), equalTo(0));
         assertThat(response.get(fnr1).get(1).getNyeRettigheterTiltak().size(), equalTo(1));
     }
 
-    private void stubArenaForvalterOpprettRettighetTiltaksdeltakelse(String expectedUri) {
-        server.expect(requestToUriTemplate(expectedUri))
-                .andExpect(method(HttpMethod.POST))
-                .andRespond(withSuccess()
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .body(getResourceFileContent("files/tiltak/tiltaksdeltakelse_forvalter_response.json")));
+    private void stubArenaForvalterOpprettRettighet() {
+
+        final Dispatcher dispatcher = new Dispatcher() {
+            @Override
+            public MockResponse dispatch(RecordedRequest request) {
+                switch (request.getPath()) {
+                    case "/v1/tiltaksdeltakelse":
+                        return new MockResponse().setResponseCode(200)
+                                .addHeader("Content-Type", "application/json; charset=utf-8")
+                                .setBody(getResourceFileContent("files/tiltak/tiltaksdeltakelse_forvalter_response.json"));
+                    case "/v1/tiltakspenger":
+                        return new MockResponse().setResponseCode(200)
+                                .addHeader("Content-Type", "application/json; charset=utf-8")
+                                .setBody(getResourceFileContent("files/tiltak/tiltakspenger_forvalter_response.json"));
+                }
+                return new MockResponse().setResponseCode(404);
+            }
+        };
+        mockWebServer.setDispatcher(dispatcher);
     }
 
-    private void stubArenaForvalterOpprettRettighetTiltakspenger(String expectedUri) {
-        server.expect(requestToUriTemplate(expectedUri))
-                .andExpect(method(HttpMethod.POST))
-                .andRespond(withSuccess()
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .body(getResourceFileContent("files/tiltak/tiltakspenger_forvalter_response.json")));
+    @After
+    public void tearDown() throws IOException {
+        mockWebServer.shutdown();
     }
 }
