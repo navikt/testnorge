6b17a2bdd9881061226d2c4e30b269aa17d1836e
K156575 <baard.krohg@gmail.com>
2020-10-12 14:22:24 +0200

    byttet fra data til value for immutability

--- apps/testnorge-skd/skd-core/src/main/java/no/nav/registre/skd/consumer/dto/Relasjon.java
+++ apps/testnorge-skd/skd-core/src/main/java/no/nav/registre/skd/consumer/dto/Relasjon.java
@@ -4,23 +4,24 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.Value;
 
-@Data
+@Value
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(force = true)
 public class Relasjon {
 
-    private final String kortnavn;
-    private final String datoDo;
-    private final String typeRelBeskr;
-    private final String mellomnavn;
-    private final String etternavn;
-    private final Integer adresseStatus;
-    private final String adrStatusBeskr;
-    private final String spesregType;
-    private final String fornavn;
-    private final String fnrRelasjon;
-    private final String typeRelasjon;
+    String kortnavn;
+    String datoDo;
+    String typeRelBeskr;
+    String mellomnavn;
+    String etternavn;
+    Integer adresseStatus;
+    String adrStatusBeskr;
+    String spesregType;
+    String fornavn;
+    String fnrRelasjon;
+    String typeRelasjon;
 
 }
--- apps/testnorge-skd/skd-core/src/main/java/no/nav/registre/skd/consumer/response/RelasjonsResponse.java
+++ apps/testnorge-skd/skd-core/src/main/java/no/nav/registre/skd/consumer/response/RelasjonsResponse.java
@@ -2,19 +2,19 @@ package no.nav.registre.skd.consumer.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.Value;
 import no.nav.registre.skd.consumer.dto.Relasjon;
 
 import java.util.List;
 
-@Data
+@Value
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(force = true)
 public class RelasjonsResponse {
 
-    private final String fnr;
-    private final List<Relasjon> relasjoner;
+    String fnr;
+    List<Relasjon> relasjoner;
 
 }
