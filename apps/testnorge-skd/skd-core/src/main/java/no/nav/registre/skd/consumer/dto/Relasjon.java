1a9a8702a379841f6ba70b6df44a745a07eb45a0
K156575 <baard.krohg@gmail.com>
2020-10-12 14:06:50 +0200

    Fix for jackson deserialisering

--- apps/testnorge-skd/skd-core/src/main/java/no/nav/registre/skd/consumer/dto/Relasjon.java
+++ apps/testnorge-skd/skd-core/src/main/java/no/nav/registre/skd/consumer/dto/Relasjon.java
@@ -1,10 +1,14 @@
 package no.nav.registre.skd.consumer.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor(force = true)
 public class Relasjon {
 
     private final String kortnavn;
@@ -12,7 +16,7 @@ public class Relasjon {
     private final String typeRelBeskr;
     private final String mellomnavn;
     private final String etternavn;
-    private final int adresseStatus;
+    private final Integer adresseStatus;
     private final String adrStatusBeskr;
     private final String spesregType;
     private final String fornavn;
--- apps/testnorge-skd/skd-core/src/main/java/no/nav/registre/skd/consumer/response/RelasjonsResponse.java
+++ apps/testnorge-skd/skd-core/src/main/java/no/nav/registre/skd/consumer/response/RelasjonsResponse.java
@@ -1,20 +1,20 @@
 package no.nav.registre.skd.consumer.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import no.nav.registre.skd.consumer.dto.Relasjon;
 
 import java.util.List;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor(force = true)
 public class RelasjonsResponse {
 
     private final String fnr;
     private final List<Relasjon> relasjoner;
 
-    public RelasjonsResponse(String fnr, List<Relasjon> relasjoner) {
-        this.fnr = fnr;
-        this.relasjoner = relasjoner;
-    }
 }
