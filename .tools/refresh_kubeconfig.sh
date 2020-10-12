887adbc989becdfedd667c69910e9c6cd4108afc
K156575 <baard.krohg@gmail.com>
2020-10-12 18:50:36 +0200

    Oppdatere kubeconfig La til et script som går gjennom regla for å oppdatere kubeconfigs for mac-brukere. Kan kjøres med -d dersom docker er oppdatert og laster da ned riktig versjon av kubectl. Kan også modifiseres med -k for å angi stien til kubeconfigs-mappa. Oppdaterer også KUBECONFIGS-variabelen, men bør ha denne som export i shellet selv også.

---
+++ .tools/refresh_kubeconfig.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+### Husk å oppdatere kubectl dersom docker blir oppdatert ved å kjøre med -d
+docker_update=false
+KUBECONFIGS_PATH="$HOME/kubeconfigs"
+
+while getopts "d?:k:" opt; do
+    case "$opt" in
+        d) docker_update=true ;;
+	k) KUBECONFIGS_PATH=$2 ;;
+    esac
+done
+
+if [ $docker_update = true ]
+then
+    echo "Updating kubectl..."
+    curl -L "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/darwin/amd64/kubectl" -o /usr/local/bin/kubectl && chmod +x /usr/local/bin/kubectl
+fi
+
+echo "Updating kubeconfigs in $KUBECONFIGS_PATH..."
+cd $KUBECONFIGS_PATH
+git reset --hard
+git pull && git checkout naisdevice
+cd ..
+
+echo "Logging in..."
+kubectl config use-context dev-fss
+open https://microsoft.com/devicelogin
+kubectl get pods
+
