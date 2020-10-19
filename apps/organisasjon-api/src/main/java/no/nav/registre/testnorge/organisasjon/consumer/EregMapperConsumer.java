b5de4cff4d4a629f853932461a210a128ad7e433
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2020-10-19 13:07:25 +0200

    test

--- apps/organisasjon-api/src/main/java/no/nav/registre/testnorge/organisasjon/consumer/EregMapperConsumer.java
+++ apps/organisasjon-api/src/main/java/no/nav/registre/testnorge/organisasjon/consumer/EregMapperConsumer.java
@@ -1,11 +1,13 @@
 package no.nav.registre.testnorge.organisasjon.consumer;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import no.nav.registre.testnorge.libs.common.command.CreateEregMappingCommand;
 import no.nav.registre.testnorge.organisasjon.domain.Organisasjon;
 
+@Component
 public class EregMapperConsumer {
     private final WebClient webClient;
 
