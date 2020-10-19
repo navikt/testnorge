18569dc87538411a28034d7e3733d79e6d22c194
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2020-10-19 13:04:53 +0200

    add serivce

--- apps/organisasjon-api/src/main/java/no/nav/registre/testnorge/organisasjon/service/OrganisasjonService.java
+++ apps/organisasjon-api/src/main/java/no/nav/registre/testnorge/organisasjon/service/OrganisasjonService.java
@@ -1,11 +1,13 @@
 package no.nav.registre.testnorge.organisasjon.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import no.nav.registre.testnorge.organisasjon.consumer.EregConsumer;
 import no.nav.registre.testnorge.organisasjon.consumer.EregMapperConsumer;
 import no.nav.registre.testnorge.organisasjon.domain.Organisasjon;
 
+@Service
 @RequiredArgsConstructor
 public class OrganisasjonService {
     private final EregMapperConsumer eregMapperConsumer;
