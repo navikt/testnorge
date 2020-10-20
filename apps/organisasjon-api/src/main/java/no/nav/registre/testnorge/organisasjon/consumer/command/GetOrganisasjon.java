36104e0b042a63ca447eaf35d647f054d70281c2
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2020-10-20 13:03:58 +0200

    Oppdater feilmelding

--- apps/organisasjon-api/src/main/java/no/nav/registre/testnorge/organisasjon/consumer/command/GetOrganisasjon.java
+++ apps/organisasjon-api/src/main/java/no/nav/registre/testnorge/organisasjon/consumer/command/GetOrganisasjon.java
@@ -1,8 +1,8 @@
 package no.nav.registre.testnorge.organisasjon.consumer.command;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriTemplate;
 
@@ -27,28 +27,23 @@ public class GetOrganisasjon implements Callable<OrganisasjonDTO> {
 
     @Override
     public OrganisasjonDTO call() {
+        log.info("Henter {} fra EREG i {}...", orgnummer, miljo);
+        ResponseEntity<OrganisasjonDTO> entity;
         try {
-            log.debug("Henter {} fra Ereg i {}...", orgnummer, miljo);
-            ResponseEntity<OrganisasjonDTO> entity = restTemplate.getForEntity(
+            entity = restTemplate.getForEntity(
                     new UriTemplate(this.url + "/organisasjon/{orgnummer}?inkluderHierarki=true&inkluderHistorikk=false")
                             .expand(miljo, orgnummer),
                     OrganisasjonDTO.class
             );
-
-            if (entity.hasBody()) {
-                log.debug("Hentet {} fra Ereg i {}", orgnummer, miljo);
-                return entity.getBody();
-            }
-            if (entity.getStatusCode() == HttpStatus.NOT_FOUND) {
-                log.warn("{} ikke funnet for miljo: {}", orgnummer, miljo);
-            } else {
-                log.error("Kan ikke hente ut {} fra: {}", orgnummer, miljo);
-            }
+        } catch (HttpClientErrorException.NotFound e) {
+            log.info("Finner ikke orgnaisasjon {} fra EREG i {}", orgnummer, miljo);
             return null;
-
-        } catch (Exception e) {
-            log.error("Kan ikke hente ut {} fra: {}", orgnummer, miljo, e);
+        }
+        if (entity.hasBody()) {
+            log.info("Hentet {} fra EREG i {}", orgnummer, miljo);
+            return entity.getBody();
         }
         return null;
+
     }
 }
