cfb247caa6a16cd480dad76dfab2ad958bb0922a
stigus <stian.gustavsson@nav.no>
2020-10-21 10:02:07 +0200

    Begynt på ident-service app

--- apps/ident-service/src/main/java/no/nav/registre/testnorge/identservice/config/DevVaultConfig.java
+++ apps/ident-service/src/main/java/no/nav/registre/testnorge/identservice/config/DevVaultConfig.java
@@ -6,7 +6,6 @@ import org.springframework.vault.annotation.VaultPropertySource;
 
 @Configuration
 @Profile("dev")
-@VaultPropertySource(value = "kv/preprod/fss/testnorge-ident-service/dev", ignoreSecretNotFound = true)
-//TODO tilbake til false over, trenger å opprette source over i vault
+@VaultPropertySource(value = "kv/preprod/fss/testnorge-ident-service/dev", ignoreSecretNotFound = false)
 public class DevVaultConfig {
 }
