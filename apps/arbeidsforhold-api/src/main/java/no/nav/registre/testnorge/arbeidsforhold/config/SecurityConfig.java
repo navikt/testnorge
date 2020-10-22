5377d59d723ca0a1eaed8cd14578274bce6add9a
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2020-10-22 11:53:20 +0200

    fix

--- apps/arbeidsforhold-api/src/main/java/no/nav/registre/testnorge/arbeidsforhold/config/SecurityConfig.java
+++ apps/arbeidsforhold-api/src/main/java/no/nav/registre/testnorge/arbeidsforhold/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package no.nav.registre.testnorge.arbeidsforhold.config;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.context.annotation.Configuration;
@@ -8,7 +7,6 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
 import java.util.List;
@@ -16,6 +14,7 @@ import java.util.List;
 import no.nav.registre.testnorge.libs.oauth2.config.OAuth2ResourceServerConfiguration;
 
 
+@Order(1)
 @EnableWebSecurity
 @Configuration
 @Profile({"prod", "dev"})
