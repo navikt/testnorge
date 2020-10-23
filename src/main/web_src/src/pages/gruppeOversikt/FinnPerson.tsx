e294c044835320cce9e4bda01c477928e836ffd4
Betsy Carina Traran <betsy.carina.traran@nav.no>
2020-10-23 14:06:51 +0200

    Styling og småfix

--- src/main/web_src/src/pages/gruppeOversikt/FinnPerson.tsx
+++ src/main/web_src/src/pages/gruppeOversikt/FinnPerson.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+//@ts-ignore
 import AsyncSelect from 'react-select/async'
 import { useAsyncFn } from 'react-use'
 import { Redirect } from 'react-router-dom'
@@ -7,19 +8,49 @@ import { TpsfApi } from '~/service/Api'
 import NavButton from '~/components/ui/button/NavButton/NavButton'
 import DollyModal from '~/components/ui/modal/DollyModal'
 import { Label } from '~/components/ui/form/inputs/label/Label'
+import { Søkeknapp } from 'nav-frontend-ikonknapper'
+import { AsyncFn } from 'react-use/lib/useAsync'
 
-export default function FinnPerson({ naviger }) {
+type FinnPerson = {
+	naviger: Function
+}
+
+type Option = {
+	value: string
+	label: string
+}
+
+type Person = {
+	ident: string
+	fornavn: string
+	mellomnavn?: string
+	etternavn: string
+}
+
+type Respons = {
+	value: {
+		data: {
+			gruppe?: {
+				id: number
+			}
+			error?: string
+			message?: string
+		}
+	}
+}
+
+export default function FinnPerson({ naviger }: FinnPerson) {
 	const [isFinnModalOpen, openFinnModal, closeFinnModal] = useBoolean(false)
-	const [soekTekst, setSoekTekst] = useState('')
-	const [ident, setIdent] = useState(null)
-	const [gruppe, setGruppe] = useState('')
 	const [redirectToGruppe, setRedirect] = useBoolean()
+
+	const [ident, setIdent] = useState(null)
+	const [gruppe, setGruppe] = useState(null)
 	const [feilmelding, setFeilmelding] = useState(null)
 
-	const [options, fetchOptions] = useAsyncFn(async tekst => {
-		const { data } = await TpsfApi.soekPersoner(tekst)
-		const options = []
-		data.map(person => {
+	const [options, fetchOptions]: AsyncFn<any> = useAsyncFn(async tekst => {
+		const { data }: any = await TpsfApi.soekPersoner(tekst)
+		const options: Array<Option> = []
+		data.map((person: Person) => {
 			const navn = person.mellomnavn
 				? `${person.fornavn} ${person.mellomnavn} ${person.etternavn}`
 				: `${person.fornavn} ${person.etternavn}`
@@ -31,13 +62,20 @@ export default function FinnPerson({ naviger }) {
 		return options
 	}, [])
 
-	const handleChange = tekst => {
-		setSoekTekst(tekst)
+	const handleChange = (tekst: string) => {
 		fetchOptions(tekst)
+		setFeilmelding(null)
+	}
+
+	const handleClose = () => {
+		setIdent(null)
+		setGruppe(null)
+		setFeilmelding(null)
+		closeFinnModal()
 	}
 
 	const navigerTilPerson = () => {
-		naviger(ident).then(response => {
+		naviger(ident).then((response: Respons) => {
 			if (response.value.data.error) {
 				setFeilmelding(response.value.data.message)
 			} else {
@@ -51,42 +89,46 @@ export default function FinnPerson({ naviger }) {
 
 	return (
 		<>
-			<NavButton type="standard" onClick={openFinnModal}>
-				Finn testperson
-			</NavButton>
+			{/* 
+				// @ts-ignore */}
+			<Søkeknapp onClick={openFinnModal} style={{ marginTop: '10px' }} kompakt="">
+				<span>Finn testperson</span>
+			</Søkeknapp>
 
-			<DollyModal
-				isOpen={isFinnModalOpen}
-				closeModal={closeFinnModal}
-				width="40%"
-				overflow="visible"
-			>
+			<DollyModal isOpen={isFinnModalOpen} closeModal={handleClose} width="40%" overflow="visible">
 				<h1>Søk etter testperson</h1>
 				<p>
 					Du kan søke på både FNR/DNR/BOST og navn, eller deler av disse. Du vil da få en liste av
 					aktuelle personer. Velg en person for å bli tatt direkte til testdatagruppen personen
 					finnes i.
 				</p>
+				{/* 
+				// @ts-ignore */}
 				<Label name="Testperson" label="Testperson">
 					<AsyncSelect
 						defaultOptions={false}
 						loadOptions={fetchOptions}
 						onInputChange={handleChange}
-						inputValue={soekTekst}
 						options={options}
-						onChange={e => setIdent(e.value)}
+						onChange={(e: Option) => setIdent(e.value)}
 						cacheOptions={true}
 						label="Person"
 						placeholder="Begynn å skrive inn FNR/DNR/BOST eller navn..."
 					/>
 				</Label>
-				{feilmelding && <div className="error-message">{feilmelding}</div>}
-				<NavButton type="standard" onClick={closeFinnModal} style={{ marginRight: '10px' }}>
-					Avbryt
-				</NavButton>
-				<NavButton type="hoved" onClick={navigerTilPerson}>
-					Gå til person
-				</NavButton>
+				{feilmelding && (
+					<div className="error-message" style={{ marginBottom: '10px' }}>
+						{feilmelding}
+					</div>
+				)}
+				<div className="flexbox--all-center" style={{ marginTop: '20px' }}>
+					<NavButton type="standard" onClick={handleClose} style={{ marginRight: '10px' }}>
+						Avbryt
+					</NavButton>
+					<NavButton type="hoved" onClick={navigerTilPerson}>
+						Gå til person
+					</NavButton>
+				</div>
 			</DollyModal>
 		</>
 	)
