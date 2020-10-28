8b892086026201914310afdaf8308c3d36584ce5
S152486 <ingvild.stoen@nav.no>
2020-10-28 08:32:08 +0100

    Revert "Endret fra insecure til secure config"
    This reverts commit b3e43e066d9bace79dff66940193d44b91b18fc2.

--- apps/opprett-person-pdl/src/main/java/no/nav/registre/testnorge/opprettpersonpdl/config/ApplicationConfig.java
+++ apps/opprett-person-pdl/src/main/java/no/nav/registre/testnorge/opprettpersonpdl/config/ApplicationConfig.java
@@ -5,9 +5,8 @@ import org.springframework.context.annotation.Import;
 
 import no.nav.registre.testnorge.libs.core.config.ApplicationCoreConfig;
 import no.nav.registre.testnorge.libs.oauth2.config.InsecureOAuth2ServerToServerConfiguration;
-import no.nav.registre.testnorge.libs.oauth2.config.SecureOAuth2ServerToServerConfiguration;
 
 @Configuration
-@Import({ApplicationCoreConfig.class, SecureOAuth2ServerToServerConfiguration.class})
+@Import({ApplicationCoreConfig.class, InsecureOAuth2ServerToServerConfiguration.class})
 public class ApplicationConfig {
 }
