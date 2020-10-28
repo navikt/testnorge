a90f38d536f4cd8f7d0681d0b55ccf1e6edc1858
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2020-10-28 12:03:36 +0100

    Opprett mn-person-service (#305)

---
+++ apps/mn-person-service/src/main/java/no/nav/registre/testnorge/mn/personservice/credentials/PersonApiClientProperties.java
@@ -0,0 +1,31 @@
+package no.nav.registre.testnorge.mn.personservice.credentials;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import no.nav.registre.testnorge.libs.oauth2.domain.ClientCredential;
+
+@Configuration
+public class PersonApiClientProperties extends ClientCredential {
+    private final Integer threads;
+    private final String baseUrl;
+
+    public PersonApiClientProperties(
+            @Value("${consumers.personapi.client_id}") String clientId,
+            @Value("${consumers.personapi.client_secret}") String clientSecret,
+            @Value("${consumers.personapi.threads}") Integer threads,
+            @Value("${consumers.personapi.url}") String baseUrl
+    ) {
+        super(clientId, clientSecret);
+        this.threads = threads;
+        this.baseUrl = baseUrl;
+    }
+
+    public Integer getThreads() {
+        return threads;
+    }
+
+    public String getBaseUrl() {
+        return baseUrl;
+    }
+}
