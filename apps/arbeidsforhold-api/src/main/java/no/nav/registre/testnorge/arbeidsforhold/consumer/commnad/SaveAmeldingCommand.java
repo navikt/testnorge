5fe6f1fcd7797bbc416ed11a3c077bbcbcee23bf
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2020-10-29 12:47:51 +0100

    test

--- apps/arbeidsforhold-api/src/main/java/no/nav/registre/testnorge/arbeidsforhold/consumer/commnad/SaveAmeldingCommand.java
+++ apps/arbeidsforhold-api/src/main/java/no/nav/registre/testnorge/arbeidsforhold/consumer/commnad/SaveAmeldingCommand.java
@@ -1,13 +1,17 @@
 package no.nav.registre.testnorge.arbeidsforhold.consumer.commnad;
 
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBElement;
+import javax.xml.bind.Marshaller;
+import java.io.StringWriter;
 
 import no.nav.registre.testnorge.libs.dependencyanalysis.DependencyOn;
 import no.nav.registre.testnorge.xsd.arbeidsforhold.v2_1.EDAGM;
@@ -19,15 +23,20 @@ public class SaveAmeldingCommand implements Runnable {
     private final WebClient webClient;
     private final EDAGM edagm;
 
+    @SneakyThrows
     @Override
     public void run() {
-
+        JAXBContext jaxbContext = JAXBContext.newInstance(ObjectFactory.class);
         ObjectFactory objectFactory = new ObjectFactory();
+        JAXBElement<EDAGM> melding = objectFactory.createMelding(edagm);
+        Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+        StringWriter sw = new StringWriter();
+        jaxbMarshaller.marshal(melding, sw);
 
         webClient
                 .post()
                 .uri(builder -> builder.path("/oppsummeringsdokument").build())
-                .body(BodyInserters.fromPublisher(Mono.just(objectFactory.createMelding(edagm)), JAXBElement.class))
+                .body(BodyInserters.fromPublisher(Mono.just(sw.toString()), String.class))
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_XML_VALUE)
                 .retrieve()
                 .bodyToMono(Void.class)
--- apps/mn-synt-arbeidsforhold-service/src/main/java/no/nav/registre/testnorge/mn/syntarbeidsforholdservice/provider/IdentController.java
+++ apps/mn-synt-arbeidsforhold-service/src/main/java/no/nav/registre/testnorge/mn/syntarbeidsforholdservice/provider/IdentController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.Set;
 
 import no.nav.registre.testnorge.mn.syntarbeidsforholdservice.service.ArbeidsfoholdService;
@@ -20,13 +22,28 @@ public class IdentController {
     private final ArbeidsfoholdService service;
 
     @PostMapping("/{ident}")
-    public ResponseEntity<?> opprettPerson(@PathVariable("ident") String ident) {
+    public ResponseEntity<?> opprettArbeidsforholdForIdent(@PathVariable("ident") String ident) {
         service.startArbeidsforhold(ident);
-        return ResponseEntity.ok().build();
+
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{ident}")
+                .buildAndExpand(ident)
+                .toUri();
+        return ResponseEntity.created(uri).build();
     }
 
     @GetMapping
-    public ResponseEntity<Set<String>> getPersoner() {
+    public ResponseEntity<Set<String>> getIdenter() {
         return ResponseEntity.ok(service.getIdenterWithArbeidsforhold());
+    }
+
+    @GetMapping("/{ident}")
+    public ResponseEntity<String> getIdent(@PathVariable("ident") String ident) {
+        Set<String> identer = service.getIdenterWithArbeidsforhold();
+        if (identer.contains(ident)) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(ident);
     }
 }
