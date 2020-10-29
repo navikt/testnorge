cffc4e6c4a7c374d2a47328ae442863c98738f5a
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2020-10-29 12:34:56 +0100

    testing

--- apps/arbeidsforhold-api/src/main/java/no/nav/registre/testnorge/arbeidsforhold/consumer/commnad/SaveAmeldingCommand.java
+++ apps/arbeidsforhold-api/src/main/java/no/nav/registre/testnorge/arbeidsforhold/consumer/commnad/SaveAmeldingCommand.java
@@ -7,8 +7,11 @@ import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import javax.xml.bind.JAXBElement;
+
 import no.nav.registre.testnorge.libs.dependencyanalysis.DependencyOn;
 import no.nav.registre.testnorge.xsd.arbeidsforhold.v2_1.EDAGM;
+import no.nav.registre.testnorge.xsd.arbeidsforhold.v2_1.ObjectFactory;
 
 @DependencyOn(value = "aareg-synt", external = true)
 @RequiredArgsConstructor
@@ -18,10 +21,13 @@ public class SaveAmeldingCommand implements Runnable {
 
     @Override
     public void run() {
+
+        ObjectFactory objectFactory = new ObjectFactory();
+
         webClient
                 .post()
                 .uri(builder -> builder.path("/oppsummeringsdokument").build())
-                .body(BodyInserters.fromPublisher(Mono.just(edagm), EDAGM.class))
+                .body(BodyInserters.fromPublisher(Mono.just(objectFactory.createMelding(edagm)), JAXBElement.class))
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_XML_VALUE)
                 .retrieve()
                 .bodyToMono(Void.class)
