4bed8a3ce4e7a0aed9be28b38920cd850af9cc2d
K156575 <baard.krohg@gmail.com>
2020-11-04 08:34:20 +0100

    La til OkHttp3 i gradle

--- apps/testnorge-skd/skd-core/src/test/java/no/nav/registre/skd/commands/tpsf/HentMeldingerCommandTest.java
+++ apps/testnorge-skd/skd-core/src/test/java/no/nav/registre/skd/commands/tpsf/HentMeldingerCommandTest.java
@@ -3,26 +3,28 @@ package no.nav.registre.skd.commands.tpsf;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import no.nav.registre.skd.skdmelding.RsMeldingstype1Felter;
+import okhttp3.mockwebserver.Dispatcher;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
+import okhttp3.mockwebserver.RecordedRequest;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.web.reactive.function.client.WebClient;
+import wiremock.org.eclipse.jetty.http.HttpStatus;
 
 import java.io.IOException;
 import java.util.Collections;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.fail;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
+@ActiveProfiles("test")
 public class HentMeldingerCommandTest {
 
     public static MockWebServer mockBackEnd;
@@ -44,23 +46,32 @@ public class HentMeldingerCommandTest {
     }
 
     @Test
-    public void hentMeldingerEnSide() {
+    public void hentMeldingerEnSide() throws JsonProcessingException {
+        stubHentTreMeldinger();
+        var requestIds = Collections.nCopies(3, "10101010101");
+        var response = new HentMeldingerCommand(webClient, requestIds).call();
+        assertThat(response.size(), is(3));
+    }
+
+
+    private void stubHentTreMeldinger() throws JsonProcessingException {
         var mockRsMeldingstype = RsMeldingstype1Felter.builder()
                 .tkNr("19203")
                 .bibehold("aoweifj")
                 .build();
-        var mockResponse = Collections.nCopies(70, mockRsMeldingstype);
-        var requestIds = Collections.nCopies(70, "10101010101");
-        try {
-            mockBackEnd.enqueue(new MockResponse()
-                    .setBody(objectMapper.writeValueAsString(mockResponse))
-                    .addHeader("Content-Type", "application/json"));
-        } catch (JsonProcessingException e) {
-            fail("Object Mapper kunne ikke mappe RsMeldingstype1Felter-liste");
-        }
-
-        var response = new HentMeldingerCommand(webClient, requestIds).call();
-        assertThat(response.size(), is(70));
+        var mockResponse = objectMapper.writeValueAsString(Collections.nCopies(3, mockRsMeldingstype));
+        final Dispatcher dispatcher = new Dispatcher() {
+            @Override
+            public MockResponse dispatch(RecordedRequest recordedRequest) throws InterruptedException {
+                switch (recordedRequest.getPath()) {
+                case "/v1/endringsmelding/skd/meldinger":
+                    return new MockResponse().setResponseCode(HttpStatus.OK_200)
+                            .addHeader("Content-Type", "application/json")
+                            .setBody(mockResponse);
+                }
+                return new MockResponse().setResponseCode(HttpStatus.BAD_REQUEST_400);
+            }
+        };
+        mockBackEnd.setDispatcher(dispatcher);
     }
-
 }
