6ec9f48000b552f0af160fb9a673382d8b7efdeb
Stian <stian.gustavsson@nav.no>
2020-11-04 10:04:45 +0100

    Fiks ette parprogg med Kristen

--- apps/ident-service/src/main/java/no/nav/registre/testnorge/identservice/VaultConfig.java
+++ apps/ident-service/src/main/java/no/nav/registre/testnorge/identservice/VaultConfig.java
@@ -7,8 +7,6 @@ import org.springframework.vault.annotation.VaultPropertySource;
 @Configuration
 @Profile("dev")
 @VaultPropertySource(value = "kv/preprod/fss/testnorge-ident-service/local", ignoreSecretNotFound = false)
-@VaultPropertySource(value = "oracle/dev/creds/tpsforvalteren_t1-user", propertyNamePrefix = "spring.datasource.", ignoreSecretNotFound = false)
-@VaultPropertySource(value = "oracle/dev/config/tpsforvalteren_t1", propertyNamePrefix = "tps-forvalteren.datasource.", ignoreSecretNotFound = false)
 @VaultPropertySource(value = "serviceuser/test/srvtps-forvalteren", propertyNamePrefix = "srvtps.forvalteren.", ignoreSecretNotFound = false)
 class VaultConfig {
 
