3ff27a4df93454eec84d9876875a4dd6087f00c5
Stian <stian.gustavsson@nav.no>
2020-11-06 10:20:21 +0100

    Fiks med Kristen

--- apps/ident-service/src/main/java/no/nav/registre/testnorge/identservice/IdentServiceApplicationStarter.java
+++ apps/ident-service/src/main/java/no/nav/registre/testnorge/identservice/IdentServiceApplicationStarter.java
@@ -6,11 +6,13 @@ import org.apache.catalina.authenticator.jaspic.AuthConfigFactoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
 
 import javax.security.auth.message.config.AuthConfigFactory;
 
 import static java.util.Objects.isNull;
 
+@Profile("prod")
 @SpringBootApplication
 @Import(value = {ApplicationCoreConfig.class, SecureOAuth2ServerToServerConfiguration.class})
 public class IdentServiceApplicationStarter {
