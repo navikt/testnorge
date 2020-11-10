5d712f28e6815e37ba4d618af546638fd75cce22
stigus <stian.gustavsson@nav.no>
2020-11-10 11:31:59 +0100

    Fix på Liste->String

--- apps/ident-service/src/main/java/no/nav/registre/testnorge/identservice/service/SjekkIdenterService.java
+++ apps/ident-service/src/main/java/no/nav/registre/testnorge/identservice/service/SjekkIdenterService.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 import static org.apache.commons.lang3.StringUtils.isNotBlank;
@@ -23,10 +21,7 @@ public class SjekkIdenterService {
     @SneakyThrows
     public ResponseEntity<IdentMedStatus> finnLedigeIdenter(String ident) {
 
-        List<String> identer = new ArrayList<>();
-        identer.add(ident);
-
-        String identStatus = filtrerPaaIdenterTilgjengeligIMiljo.filtrerPaaIdenter(identer);
+        String identStatus = filtrerPaaIdenterTilgjengeligIMiljo.filtrerPaaIdenter(ident);
         Map.Entry<String, String> identMedStatus = Map.entry(ident, identStatus);
 
         return ResponseEntity.ok().body(mapToIdentMedStatusSet(identMedStatus));
