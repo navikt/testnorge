aa144a4f923453f7d65ce3003448ae7c263a83c6
stigus <stian.gustavsson@nav.no>
2020-11-10 13:55:04 +0100

    OpenApi endringer

--- apps/ident-service/src/main/java/no/nav/registre/testnorge/identservice/provider/IdentServiceController.java
+++ apps/ident-service/src/main/java/no/nav/registre/testnorge/identservice/provider/IdentServiceController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/identer")
+@RequestMapping("/api/v1/identservice")
 @RequiredArgsConstructor
 public class IdentServiceController {
 
