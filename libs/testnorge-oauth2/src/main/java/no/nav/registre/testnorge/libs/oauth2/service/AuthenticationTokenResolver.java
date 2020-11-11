7ab063ecf8bde693d2a4346fe0cbb010ddc67613
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2020-11-11 14:50:06 +0100

    testing

--- libs/testnorge-oauth2/src/main/java/no/nav/registre/testnorge/libs/oauth2/service/AuthenticationTokenResolver.java
+++ libs/testnorge-oauth2/src/main/java/no/nav/registre/testnorge/libs/oauth2/service/AuthenticationTokenResolver.java
@@ -1,7 +1,5 @@
 package no.nav.registre.testnorge.libs.oauth2.service;
 
-import org.springframework.security.oauth2.core.AbstractOAuth2Token;
-
 public interface AuthenticationTokenResolver {
     String getTokenValue();
 
