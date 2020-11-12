f1f86fd7457486c1163020bb4cd90911d47eb83a
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2020-11-12 20:07:34 +0100

    testing

--- libs/testnorge-core/src/main/java/no/nav/registre/testnorge/libs/core/config/ApplicationCoreConfig.java
+++ libs/testnorge-core/src/main/java/no/nav/registre/testnorge/libs/core/config/ApplicationCoreConfig.java
@@ -3,7 +3,6 @@ package no.nav.registre.testnorge.libs.core.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
-import no.nav.registre.testnorge.libs.autoconfigdependencyanalysis.config.DependencyAnalysisAutoConfiguration;
 import no.nav.registre.testnorge.libs.config.LoggingWebConfig;
 import no.nav.registre.testnorge.libs.core.provider.InternalController;
 import no.nav.registre.testnorge.libs.dependencyanalysis.provider.DependenciesController;
