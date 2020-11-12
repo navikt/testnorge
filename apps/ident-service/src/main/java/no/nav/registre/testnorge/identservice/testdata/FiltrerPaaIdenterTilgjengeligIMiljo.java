a1e46915ff563e6fc50afccb6188cf4ebe2d83c6
stigus <stian.gustavsson@nav.no>
2020-11-12 09:38:58 +0100

    Testing

--- apps/ident-service/src/main/java/no/nav/registre/testnorge/identservice/testdata/FiltrerPaaIdenterTilgjengeligIMiljo.java
+++ apps/ident-service/src/main/java/no/nav/registre/testnorge/identservice/testdata/FiltrerPaaIdenterTilgjengeligIMiljo.java
@@ -60,6 +60,8 @@ public class FiltrerPaaIdenterTilgjengeligIMiljo {
         JsonNode tpsSvar = nonNull(node.get("tpsSvar")) ? node.get("tpsSvar") : node;
         JsonNode tpsSvarStatus = nonNull(tpsSvar.get("svarStatus")) ? tpsSvar.get("svarStatus") : node;
         JsonNode tpsStatusKode = nonNull(tpsSvarStatus.get("returStatus")) ? tpsSvar.get("returStatus") : node;
+        log.info(tpsStatusKode.asText());
+        log.info(node.asText());
 
         return tpsStatusKode.asText().contains("00") ? ResponseEntity.ok(ident) : ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
