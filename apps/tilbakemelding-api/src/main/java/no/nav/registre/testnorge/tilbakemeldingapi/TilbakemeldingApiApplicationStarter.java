6d9abb5d641e161410ca054064f0639682bdc34d
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2020-11-12 17:30:20 +0100

    testing

--- apps/tilbakemelding-api/src/main/java/no/nav/registre/testnorge/tilbakemeldingapi/TilbakemeldingApiApplicationStarter.java
+++ apps/tilbakemelding-api/src/main/java/no/nav/registre/testnorge/tilbakemeldingapi/TilbakemeldingApiApplicationStarter.java
@@ -2,8 +2,12 @@ package no.nav.registre.testnorge.tilbakemeldingapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import no.nav.registre.testnorge.libs.autodependencyanalysis.config.AutoRegistrationDependencyAnalysisConfiguration;
 
 @SpringBootApplication
+@Import(AutoRegistrationDependencyAnalysisConfiguration.class)
 public class TilbakemeldingApiApplicationStarter {
     public static void main(String[] args) {
         SpringApplication.run(TilbakemeldingApiApplicationStarter.class, args);
