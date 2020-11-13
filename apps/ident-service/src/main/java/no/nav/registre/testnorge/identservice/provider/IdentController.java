2fb8a7f2da9650589bfedf9d8655d577c193e27b
stigus <stian.gustavsson@nav.no>
2020-11-13 09:38:07 +0100

    Riktig import

--- apps/ident-service/src/main/java/no/nav/registre/testnorge/identservice/provider/IdentController.java
+++ apps/ident-service/src/main/java/no/nav/registre/testnorge/identservice/provider/IdentController.java
@@ -1,10 +1,10 @@
 package no.nav.registre.testnorge.identservice.provider;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import lombok.RequiredArgsConstructor;
 import no.nav.registre.testnorge.identservice.service.IdentAppService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
