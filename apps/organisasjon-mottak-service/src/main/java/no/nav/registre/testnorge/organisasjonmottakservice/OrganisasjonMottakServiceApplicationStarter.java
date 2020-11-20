fdcce20427e1cd139fd674750a6e0de7ce9b8fc7
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2020-11-20 10:38:07 +0100

    testing

--- apps/organisasjon-mottak-service/src/main/java/no/nav/registre/testnorge/organisasjonmottakservice/OrganisasjonMottakServiceApplicationStarter.java
+++ apps/organisasjon-mottak-service/src/main/java/no/nav/registre/testnorge/organisasjonmottakservice/OrganisasjonMottakServiceApplicationStarter.java
@@ -7,9 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.HashSet;
 import java.util.stream.Collectors;
 
+@Slf4j
 @SpringBootApplication
 public class OrganisasjonMottakServiceApplicationStarter {
     public static void main(String[] args) {
+
         SpringApplication.run(OrganisasjonMottakServiceApplicationStarter.class, args);
     }
 }
--- apps/organisasjon-mottak-service/src/main/java/no/nav/registre/testnorge/organisasjonmottakservice/service/OrganiasjonService.java
+++ apps/organisasjon-mottak-service/src/main/java/no/nav/registre/testnorge/organisasjonmottakservice/service/OrganiasjonService.java
@@ -1,18 +1,26 @@
 package no.nav.registre.testnorge.organisasjonmottakservice.service;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import no.nav.registre.testnorge.organisasjonmottakservice.consumer.EregConsumer;
+import java.util.stream.Collectors;
+
 import no.nav.registre.testnorge.organisasjonmottakservice.domain.Flatfil;
 import no.nav.registre.testnorge.organisasjonmottakservice.domain.Knyttning;
 import no.nav.registre.testnorge.organisasjonmottakservice.domain.Organiasjon;
 
 @Slf4j
 @Service
-@RequiredArgsConstructor
 public class OrganiasjonService {
+
+    public OrganiasjonService(FlatfilService flatfilService) {
+        this.flatfilService = flatfilService;
+
+        log.info("############################################");
+        log.info("{}", String.join(",", System.getProperties().keySet().stream().map(Object::toString).collect(Collectors.toSet())));
+        log.info("############################################");
+    }
+
     private final FlatfilService flatfilService;
     //private final EregConsumer eregConsumer;
 
