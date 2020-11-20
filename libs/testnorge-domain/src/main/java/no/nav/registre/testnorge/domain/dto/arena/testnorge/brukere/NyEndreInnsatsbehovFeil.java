6fe7b0811c62efd9e52df6f4e2b98ca906089b4f
Hay <Aileen.Hay@nav.no>
2020-11-20 14:17:49 +0100

    KOde rydding

--- libs/testnorge-domain/src/main/java/no/nav/registre/testnorge/domain/dto/arena/testnorge/brukere/NyEndreInnsatsbehovFeil.java
+++ libs/testnorge-domain/src/main/java/no/nav/registre/testnorge/domain/dto/arena/testnorge/brukere/NyEndreInnsatsbehovFeil.java
@@ -1,6 +1,10 @@
 package no.nav.registre.testnorge.domain.dto.arena.testnorge.brukere;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
