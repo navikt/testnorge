0ab83a3881a8fac1ffa2c87747f778b9a54791d3
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2020-11-20 10:28:27 +0100

    test

--- apps/organisasjon-mottak-service/src/main/java/no/nav/registre/testnorge/organisasjonmottakservice/OrganisasjonMottakServiceApplicationStarter.java
+++ apps/organisasjon-mottak-service/src/main/java/no/nav/registre/testnorge/organisasjonmottakservice/OrganisasjonMottakServiceApplicationStarter.java
@@ -4,11 +4,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashSet;
+import java.util.stream.Collectors;
+
 @Slf4j
 @SpringBootApplication
 public class OrganisasjonMottakServiceApplicationStarter {
     public static void main(String[] args) {
-        log.info("{}", String.join(",", System.getenv().keySet()));
+        log.info("############################################");
+        log.info("{}", String.join(",", System.getProperties().keySet().stream().map(Object::toString).collect(Collectors.toSet())));
+        log.info("############################################");
         SpringApplication.run(OrganisasjonMottakServiceApplicationStarter.class, args);
     }
 }
