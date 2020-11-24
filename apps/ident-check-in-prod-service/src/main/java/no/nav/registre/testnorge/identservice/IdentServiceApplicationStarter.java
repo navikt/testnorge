343085bbbec75db6da0a00029c3176e145682ef7
stigus <stian.gustavsson@nav.no>
2020-11-24 13:16:34 +0100

    Lagt til @import

--- apps/ident-check-in-prod-service/src/main/java/no/nav/registre/testnorge/identservice/IdentServiceApplicationStarter.java
+++ apps/ident-check-in-prod-service/src/main/java/no/nav/registre/testnorge/identservice/IdentServiceApplicationStarter.java
@@ -1,11 +1,18 @@
 package no.nav.registre.testnorge.identservice;
 
+import no.nav.registre.testnorge.libs.autoconfigdependencyanalysis.config.DependencyAnalysisAutoConfiguration;
+import no.nav.registre.testnorge.libs.core.config.ApplicationCoreConfig;
+import no.nav.registre.testnorge.libs.oauth2.config.SecureOAuth2ServerToServerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.Import;
 
-@Profile("prod")
 @SpringBootApplication
+@Import({
+        ApplicationCoreConfig.class,
+        SecureOAuth2ServerToServerConfiguration.class,
+        DependencyAnalysisAutoConfiguration.class
+})
 public class IdentServiceApplicationStarter {
     public static void main(String[] args) {
         SpringApplication.run(IdentServiceApplicationStarter.class, args);
