fb3d6292d2728c4c2c08a0ff12894c60ea6b3f22
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2020-11-25 13:43:50 +0100

    Opprett endringsmelding-service

---
+++ apps/endringsmelding-service/src/main/java/no/nav/registre/testnorge/endringsmeldingservice/EndringsmeldingServiceApplicationStarter.java
@@ -0,0 +1,8 @@
+package no.nav.registre.testnorge.endringsmeldingservice;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndringsmeldingServiceApplicationStarter {
+
+}
