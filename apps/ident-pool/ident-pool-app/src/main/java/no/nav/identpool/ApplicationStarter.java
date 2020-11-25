b5ad7aa16917bcfd81652588a488772480c4d014
krharum <bc05aab8d40e1cec7718d2efaef17f0817190a61>
2020-11-25 07:38:42 +0100

    Oppdatert konfig VI

--- apps/ident-pool/ident-pool-app/src/main/java/no/nav/identpool/ApplicationStarter.java
+++ apps/ident-pool/ident-pool-app/src/main/java/no/nav/identpool/ApplicationStarter.java
@@ -1,10 +1,24 @@
 package no.nav.identpool;
 
-import org.springframework.boot.SpringApplication;
+import java.util.Map;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 public class ApplicationStarter {
+
     public static void main(String[] arguments) {
-        Class<?>[] configClass = new Class[] { ApplicationConfig.class, ScheduleConfig.class };
-        SpringApplication.run(configClass, arguments);
+
+        Map<String, Object> properties = PropertyReader.builder()
+                .readSecret("spring.cloud.vault.token", "/var/run/secrets/nais.io/vault/vault_token")
+                .readSecret("spring.datasource.username", "/var/run/secrets/nais.io/db/username")
+                .readSecret("spring.datasource.password", "/var/run/secrets/nais.io/db/password")
+                .readSecret("spring.datasource.url", "/var/run/secrets/nais.io/dbPath/jdbc_url")
+                .build();
+
+        new SpringApplicationBuilder()
+                .sources(ApplicationConfig.class, ScheduleConfig.class)
+                .properties(properties)
+                .build();
     }
 }
+
+
