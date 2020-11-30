b6edc441f044e033ac30c2cc6035740b840338b5
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2020-11-30 15:13:06 +0100

    Migerere til GCP (#353)

--- apps/profil-api/src/main/java/no/nav/registre/testnorge/profil/ProfilApiApplicationStarter.java
+++ apps/profil-api/src/main/java/no/nav/registre/testnorge/profil/ProfilApiApplicationStarter.java
@@ -4,15 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-import no.nav.registre.testnorge.libs.autoconfigdependencyanalysis.config.DependencyAnalysisAutoConfiguration;
 import no.nav.registre.testnorge.libs.core.config.ApplicationCoreConfig;
 import no.nav.registre.testnorge.libs.oauth2.config.SecureOAuth2ServerToServerConfiguration;
 
 @SpringBootApplication
 @Import({
         ApplicationCoreConfig.class,
-        SecureOAuth2ServerToServerConfiguration.class,
-        DependencyAnalysisAutoConfiguration.class
+        SecureOAuth2ServerToServerConfiguration.class
 })
 public class ProfilApiApplicationStarter {
 
