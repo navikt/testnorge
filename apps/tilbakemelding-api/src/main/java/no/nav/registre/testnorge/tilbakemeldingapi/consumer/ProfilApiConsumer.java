40aeccad465b400f4635c1f80da8872e04a4f02e
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2020-12-04 09:38:47 +0100

    Set outbound rules

--- apps/tilbakemelding-api/src/main/java/no/nav/registre/testnorge/tilbakemeldingapi/consumer/ProfilApiConsumer.java
+++ apps/tilbakemelding-api/src/main/java/no/nav/registre/testnorge/tilbakemeldingapi/consumer/ProfilApiConsumer.java
@@ -3,13 +3,8 @@ package no.nav.registre.testnorge.tilbakemeldingapi.consumer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.netty.http.client.HttpClient;
-import reactor.netty.tcp.ProxyProvider;
-
-import java.net.URI;
 
 import no.nav.registre.testnorge.libs.dependencyanalysis.DependencyOn;
 import no.nav.registre.testnorge.libs.dto.profil.v1.ProfilDTO;
@@ -27,27 +22,13 @@ public class ProfilApiConsumer {
     private final AccessScopes accessScopes;
 
     public ProfilApiConsumer(
-            @Value("${http.proxy:#{null}}") String proxyHost,
             @Value("${consumer.profil-api.url}") String url,
             @Value("${consumer.profil-api.client-id}") String clientId,
             AccessTokenService accessTokenService
     ) {
         this.accessScopes = new AccessScopes("api://" + clientId + "/.default");
         this.accessTokenService = accessTokenService;
-
-        WebClient.Builder builder = WebClient.builder();
-        if (proxyHost != null) {
-            var uri = URI.create(proxyHost);
-            HttpClient httpClient = HttpClient
-                    .create()
-                    .tcpConfiguration(tcpClient -> tcpClient.proxy(proxy -> proxy
-                            .type(ProxyProvider.Proxy.HTTP)
-                            .host(uri.getHost())
-                            .port(uri.getPort())
-                    ));
-            builder.clientConnector(new ReactorClientHttpConnector(httpClient));
-        }
-        this.webClient = builder
+        this.webClient = WebClient.builder()
                 .baseUrl(url)
                 .build();
     }
