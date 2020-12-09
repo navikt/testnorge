cd70e510349bc1e0f522147819499373e721da7b
kristenhaerum <kristen.herum@nav.no>
2020-12-09 17:43:08 +0100

    Oppdatert konfigurasjon VIII

--- apps/organisasjon-forvalter/src/main/java/no/nav/organisasjonforvalter/ApplicationStarter.java
+++ apps/organisasjon-forvalter/src/main/java/no/nav/organisasjonforvalter/ApplicationStarter.java
@@ -5,12 +5,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+import java.util.Map;
+
 @SpringBootApplication
 public class ApplicationStarter {
 
     public static void main(String[] args) {
+
         if ("prod".equals(System.getProperty("spring.profiles.active"))) {
-            VaultUtil.initCloudVaultToken();
+            Map<String, Object> properties = PropertyReader.builder()
+                    .readSecret("spring.cloud.vault.token", "/var/run/secrets/nais.io/vault/vault_token")
+                    .build();
         }
         SpringApplication.run(ApplicationStarter.class, args);
     }
