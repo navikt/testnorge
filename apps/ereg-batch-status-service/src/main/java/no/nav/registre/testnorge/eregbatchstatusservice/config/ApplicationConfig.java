30c9dac324fb917ad0f6d142673dbb8727d6bebd
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2020-12-11 10:25:23 +0100

    update id

--- apps/ereg-batch-status-service/src/main/java/no/nav/registre/testnorge/eregbatchstatusservice/config/ApplicationConfig.java
+++ apps/ereg-batch-status-service/src/main/java/no/nav/registre/testnorge/eregbatchstatusservice/config/ApplicationConfig.java
@@ -4,13 +4,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 import no.nav.registre.testnorge.libs.core.config.ApplicationCoreConfig;
-import no.nav.registre.testnorge.libs.oauth2.config.SecureOAuth2ServerToServerConfiguration;
 
 @Configuration
-@Import({
-        ApplicationCoreConfig.class,
-        SecureOAuth2ServerToServerConfiguration.class
-})
+@Import(ApplicationCoreConfig.class)
 public class ApplicationConfig {
 
 }
