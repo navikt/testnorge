6688b1d4734bd1cbf0313af6ab32201a5e5f69e6
kristenhaerum <kristen.herum@nav.no>
2020-12-14 18:05:43 +0100

    Lagt til flere navn i request

--- apps/generer-navn-service/src/main/java/no/nav/registre/testnorge/generernavnservice/provider/GenerateNavnController.java
+++ apps/generer-navn-service/src/main/java/no/nav/registre/testnorge/generernavnservice/provider/GenerateNavnController.java
@@ -1,13 +1,17 @@
 package no.nav.registre.testnorge.generernavnservice.provider;
 
 import lombok.RequiredArgsConstructor;
+import no.nav.registre.testnorge.generernavnservice.domain.Navn;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import no.nav.registre.testnorge.generernavnservice.service.GenerateNavnService;
 import no.nav.registre.testnorge.libs.dto.generernavnservice.v1.NavnDTO;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/navn")
@@ -17,8 +21,8 @@ public class GenerateNavnController {
     private final GenerateNavnService service;
 
     @GetMapping
-    public ResponseEntity<NavnDTO> generateName() {
-        NavnDTO dto = service.getRandomNavn().toDTO();
-        return ResponseEntity.ok(dto);
+    public List<Navn> generateName(@RequestParam Integer antall) {
+
+        return service.getRandomNavn(antall);
     }
 }
--- apps/generer-navn-service/src/main/java/no/nav/registre/testnorge/generernavnservice/service/GenerateNavnService.java
+++ apps/generer-navn-service/src/main/java/no/nav/registre/testnorge/generernavnservice/service/GenerateNavnService.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import no.nav.registre.testnorge.generernavnservice.domain.Navn;
 
@@ -13,14 +15,17 @@ public class GenerateNavnService {
     private static final List<String> SUBSTANTIVER = FileReader.readLinesFromResources("static/substantiv.csv");
     private static final Random RANDOM = new Random();
 
-    public Navn getRandomNavn() {
-        return new Navn(
-                convertToName(ADJEKTIVER.get(RANDOM.nextInt(ADJEKTIVER.size()))),
-                convertToName(SUBSTANTIVER.get(RANDOM.nextInt(SUBSTANTIVER.size())))
-        );
+    public List<Navn> getRandomNavn(Integer antall) {
+
+        return IntStream.range(0, antall).boxed()
+                .map(navn -> Navn.builder()
+                .adjektiv(toTitleCase(ADJEKTIVER.get(RANDOM.nextInt(ADJEKTIVER.size()))))
+                .substantiv(toTitleCase(SUBSTANTIVER.get(RANDOM.nextInt(SUBSTANTIVER.size()))))
+                        .build())
+                .collect(Collectors.toList());
     }
 
-    private String convertToName(String value) {
+    private String toTitleCase(String value) {
         return value.substring(0, 1).toUpperCase() + value.substring(1);
     }
 }
