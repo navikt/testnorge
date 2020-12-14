5b29988c5fcb2a6282c76d9a405cddf4320f5f09
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2020-12-14 14:46:09 +0100

    Legg til organisasjon-bestilling-service (#382)

---
+++ apps/organisasjon-bestilling-service/src/main/java/no/nav/registre/testnorge/organisasjonbestillingservice/config/ApplicationConfig.java
@@ -0,0 +1,17 @@
+package no.nav.registre.testnorge.organisasjonbestillingservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import no.nav.registre.testnorge.libs.core.config.ApplicationCoreConfig;
+import no.nav.registre.testnorge.libs.oauth2.config.SecureOAuth2ServerToServerConfiguration;
+
+@Configuration
+@Import({
+        ApplicationCoreConfig.class,
+        SecureOAuth2ServerToServerConfiguration.class
+})
+@EnableJpaAuditing
+public class ApplicationConfig {
+}
---
+++ apps/organisasjon-bestilling-service/src/main/java/no/nav/registre/testnorge/organisasjonbestillingservice/service/OrderService.java
@@ -0,0 +1,74 @@
+package no.nav.registre.testnorge.organisasjonbestillingservice.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import no.nav.registre.testnorge.libs.dto.organiasjonbestilling.v1.ItemDTO;
+import no.nav.registre.testnorge.libs.dto.organiasjonbestilling.v1.Status;
+import no.nav.registre.testnorge.organisasjonbestillingservice.consumer.EregBatchStatusConsumer;
+import no.nav.registre.testnorge.organisasjonbestillingservice.domain.Order;
+import no.nav.registre.testnorge.organisasjonbestillingservice.repository.OrderRepository;
+import no.nav.registre.testnorge.organisasjonbestillingservice.repository.model.OrderModel;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+    private final OrderRepository repository;
+    private final EregBatchStatusConsumer consumer;
+
+    public Long register(Order order) {
+        return repository
+                .findBy(order.getMiljo(), order.getBatchId(), order.getUuid())
+                .orElseGet(() -> repository.save(order.toModel()))
+                .getId();
+    }
+
+    public Set<String> getOrderUuids() {
+        return StreamSupport
+                .stream(repository.findAll().spliterator(), false)
+                .map(OrderModel::getUuid)
+                .collect(Collectors.toSet());
+    }
+
+    public ItemDTO getStatusBy(Long id) {
+        var model = repository.findById(id);
+        if (model.isEmpty()) {
+            return null;
+        }
+        var order = new Order(model.get());
+        var kode = consumer.getStatusKode(order);
+        return new ItemDTO(id, toStatus(kode));
+    }
+
+    public List<ItemDTO> getStatusBy(String uuid) {
+        var list = repository.findBy(uuid);
+        return list.stream()
+                .map(value -> getStatusBy(value.getId()))
+                .collect(Collectors.toList());
+    }
+
+    public void delete(String uuid) {
+        var orders = repository.findBy(uuid);
+        orders.forEach(order -> {
+            repository.deleteById(order.getId());
+        });
+    }
+
+    private Status toStatus(Long kode) {
+        if (kode < 0) {
+            return Status.RUNNING;
+        }
+        if (kode == 0) {
+            return Status.COMPLETED;
+        }
+        if (kode < 16) {
+            return Status.ERROR;
+        }
+        return Status.FAILED;
+    }
+}
