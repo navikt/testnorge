fca858a7e92736030dea63718129d59d5ef8dad5
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2020-12-23 11:24:57 +0100

    trigger build

---
+++ apps/arbeidsforhold-frontend/src/main/js/src/api/index.ts
@@ -0,0 +1,25 @@
+type Fetch = {
+    url: string,
+    method: "GET" | "POST" | "PUT" | "DELETE"
+    body?: unknown
+}
+
+const _fetch = ({url, method, body}: Fetch) => (
+    window.fetch(url, {
+        method: method,
+        credentials: 'include',
+        body: !!body ? JSON.stringify(body) : null
+    }).then(response => {
+        if (!response.ok) {
+            throw new Error("Response fra endepunkt var ikke ok")
+        }
+        return response
+    }).catch((error) => {
+        console.error(error);
+        throw error;
+    })
+);
+
+export default {
+    fetch: _fetch
+}
