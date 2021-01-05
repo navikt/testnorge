0ed3ff172eb66c5310f508f80d93d1d863f0ed46
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-01-05 10:16:41 +0100

    testing

--- apps/organisasjon-mottak-service/src/main/java/no/nav/registre/testnorge/organisasjonmottak/listener/OrganaisjonMottakListener.java
+++ apps/organisasjon-mottak-service/src/main/java/no/nav/registre/testnorge/organisasjonmottak/listener/OrganaisjonMottakListener.java
@@ -32,6 +32,7 @@ public class OrganaisjonMottakListener {
 
     @KafkaListener(topics = OrganisasjonTopic.ORGANISASJON_OPPRETT_ORGANISASJON)
     public void register(@Payload Organisasjon value, @Header(KafkaHeaders.RECEIVED_MESSAGE_KEY) String uuid) {
+        log.info("Oppretter ny organiasjon med uuid: {}", uuid);
         organisasjonService.save(
                 new no.nav.registre.testnorge.organisasjonmottak.domain.Organisasjon(value),
                 value.getMetadata().getMiljo(),
@@ -41,6 +42,7 @@ public class OrganaisjonMottakListener {
 
     @KafkaListener(topics = OrganisasjonTopic.ORGANISASJON_SET_NAVN)
     public void register(@Payload Navn value, @Header(KafkaHeaders.RECEIVED_MESSAGE_KEY) String uuid) {
+        log.info("Legger til navn på organasisjon med uuid: {}", uuid);
         organisasjonService.save(
                 new no.nav.registre.testnorge.organisasjonmottak.domain.Navn(value),
                 value.getMetadata().getMiljo(),
@@ -50,6 +52,7 @@ public class OrganaisjonMottakListener {
 
     @KafkaListener(topics = OrganisasjonTopic.ORGANISASJON_SET_ANSATTE)
     public void register(@Payload Ansatte value, @Header(KafkaHeaders.RECEIVED_MESSAGE_KEY) String uuid) {
+        log.info("Legger til ansatte på organasisjon med uuid: {}", uuid);
         organisasjonService.save(
                 new no.nav.registre.testnorge.organisasjonmottak.domain.Ansatte(value),
                 value.getMetadata().getMiljo(),
@@ -59,6 +62,7 @@ public class OrganaisjonMottakListener {
 
     @KafkaListener(topics = OrganisasjonTopic.ORGANISASJON_SET_NAVN_DETALJER)
     public void register(@Payload DetaljertNavn value, @Header(KafkaHeaders.RECEIVED_MESSAGE_KEY) String uuid) {
+        log.info("Legger til detaljert navn på organasisjon med uuid: {}", uuid);
         organisasjonService.save(
                 new no.nav.registre.testnorge.organisasjonmottak.domain.DetaljertNavn(value),
                 value.getMetadata().getMiljo(),
@@ -68,6 +72,7 @@ public class OrganaisjonMottakListener {
 
     @KafkaListener(topics = OrganisasjonTopic.ORGANISASJON_SET_KNYTNING)
     public void register(@Payload Knytning value, @Header(KafkaHeaders.RECEIVED_MESSAGE_KEY) String uuid) {
+        log.info("Legger til knytning på organasisjon med uuid: {}", uuid);
         organisasjonService.save(
                 new no.nav.registre.testnorge.organisasjonmottak.domain.Knytning(value),
                 value.getMetadata().getMiljo(),
@@ -77,6 +82,7 @@ public class OrganaisjonMottakListener {
 
     @KafkaListener(topics = OrganisasjonTopic.ORGANISASJON_SET_FORRETNINGSADRESSE)
     public void register(@Payload Forretningsadresse value, @Header(KafkaHeaders.RECEIVED_MESSAGE_KEY) String uuid) {
+        log.info("Legger til forretningsadresse på organasisjon med uuid: {}", uuid);
         organisasjonService.save(
                 new no.nav.registre.testnorge.organisasjonmottak.domain.Forretningsadresse(value),
                 value.getMetadata().getMiljo(),
@@ -86,6 +92,7 @@ public class OrganaisjonMottakListener {
 
     @KafkaListener(topics = OrganisasjonTopic.ORGANISASJON_SET_POSTADRESSE)
     public void register(@Payload Postadresse value, @Header(KafkaHeaders.RECEIVED_MESSAGE_KEY) String uuid) {
+        log.info("Legger til postadresse på organasisjon med uuid: {}", uuid);
         organisasjonService.save(
                 new no.nav.registre.testnorge.organisasjonmottak.domain.Postadresse(value),
                 value.getMetadata().getMiljo(),
@@ -95,6 +102,7 @@ public class OrganaisjonMottakListener {
 
     @KafkaListener(topics = OrganisasjonTopic.ORGANISASJON_SET_EPOST)
     public void register(@Payload Epost value, @Header(KafkaHeaders.RECEIVED_MESSAGE_KEY) String uuid) {
+        log.info("Legger til epost på organasisjon med uuid: {}", uuid);
         organisasjonService.save(
                 new no.nav.registre.testnorge.organisasjonmottak.domain.Epost(value),
                 value.getMetadata().getMiljo(),
@@ -104,6 +112,7 @@ public class OrganaisjonMottakListener {
 
     @KafkaListener(topics = OrganisasjonTopic.ORGANISASJON_SET_INTERNETTADRESSE)
     public void register(@Payload Internettadresse value, @Header(KafkaHeaders.RECEIVED_MESSAGE_KEY) String uuid) {
+        log.info("Legger til internettadresse på organasisjon med uuid: {}", uuid);
         organisasjonService.save(
                 new no.nav.registre.testnorge.organisasjonmottak.domain.Internettadresse(value),
                 value.getMetadata().getMiljo(),
@@ -113,6 +122,7 @@ public class OrganaisjonMottakListener {
 
     @KafkaListener(topics = OrganisasjonTopic.ORGANISASJON_SET_NAERINGSKODE)
     public void register(@Payload Naeringskode value, @Header(KafkaHeaders.RECEIVED_MESSAGE_KEY) String uuid) {
+        log.info("Legger til næringskode på organasisjon med uuid: {}", uuid);
         organisasjonService.save(
                 new no.nav.registre.testnorge.organisasjonmottak.domain.Naeringskode(value),
                 value.getMetadata().getMiljo(),
