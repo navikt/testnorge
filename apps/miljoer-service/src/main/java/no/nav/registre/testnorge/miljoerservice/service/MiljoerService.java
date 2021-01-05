f2aa66a345d2e7de0a158a35b1f10b79184e5a64
stigus <stian.gustavsson@nav.no>
2021-01-05 15:06:34 +0100

    Sortert liste, thrower exception ved tom liste

--- apps/miljoer-service/src/main/java/no/nav/registre/testnorge/miljoerservice/service/MiljoerService.java
+++ apps/miljoer-service/src/main/java/no/nav/registre/testnorge/miljoerservice/service/MiljoerService.java
@@ -5,10 +5,14 @@ import lombok.extern.slf4j.Slf4j;
 import no.nav.registre.testnorge.libs.dependencyanalysis.DependencyOn;
 import no.nav.registre.testnorge.miljoerservice.response.MiljoerResponse;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.net.URI;
+
+import static java.util.Objects.isNull;
 
 @Service
 @RequiredArgsConstructor
@@ -25,11 +29,19 @@ public class MiljoerService {
 
     public MiljoerResponse getAktiveMiljoer() {
         log.info("Henter aktive miljøer fra TPSF..");
-        return webClient
+        MiljoerResponse response = webClient
                 .get()
                 .uri(URI.create(tpsfServerUrl + TPSF_GET_ENVIRONMENTS))
                 .retrieve()
                 .bodyToMono(MiljoerResponse.class)
                 .block();
+
+        if (isNull(response)) {
+            throw new HttpClientErrorException(HttpStatus.INTERNAL_SERVER_ERROR, "Fant ingen miljøer");
+
+        }
+        response.getEnvironments().sort(String::compareTo);
+
+        return response;
     }
 }
