93377f8bc1adf725121a6c6d8fce89fb3f95fe64
stigus <stian.gustavsson@nav.no>
2021-01-05 14:05:34 +0100

    Henter gyldige miljoer fra TPSF og lagt til dependency-analysis

--- apps/miljoer-service/src/main/java/no/nav/registre/testnorge/miljoerservice/provider/MiljoeController.java
+++ apps/miljoer-service/src/main/java/no/nav/registre/testnorge/miljoerservice/provider/MiljoeController.java
@@ -1,13 +1,12 @@
 package no.nav.registre.testnorge.miljoerservice.provider;
 
 import lombok.RequiredArgsConstructor;
+import no.nav.registre.testnorge.miljoerservice.response.MiljoerResponse;
 import no.nav.registre.testnorge.miljoerservice.service.MiljoerService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/miljoer")
@@ -17,7 +16,7 @@ public class MiljoeController {
     private final MiljoerService service;
 
     @GetMapping
-    public ResponseEntity<List<String>> hentAktiveMiljoer() {
-        return ResponseEntity.ok(service.getMiljoer());
+    public ResponseEntity<MiljoerResponse> hentAktiveMiljoer() {
+        return ResponseEntity.ok(service.getAktiveMiljoer());
     }
 }
--- apps/miljoer-service/src/main/java/no/nav/registre/testnorge/miljoerservice/service/MiljoerService.java
+++ apps/miljoer-service/src/main/java/no/nav/registre/testnorge/miljoerservice/service/MiljoerService.java
@@ -1,14 +1,35 @@
 package no.nav.registre.testnorge.miljoerservice.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import no.nav.registre.testnorge.libs.dependencyanalysis.DependencyOn;
+import no.nav.registre.testnorge.miljoerservice.response.MiljoerResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.net.URI;
 
 @Service
+@RequiredArgsConstructor
+@DependencyOn(value = "tps-forvalteren", external = true)
+@Slf4j
 public class MiljoerService {
 
-    public List<String> getMiljoer() {
-        return new ArrayList<>();
+    private static final String TPSF_GET_ENVIRONMENTS = "/v1/environments";
+
+    private final WebClient webClient = WebClient.builder().build();
+
+    @Value("${tps-forvalteren.rest-api.url}")
+    private String tpsfServerUrl;
+
+    public MiljoerResponse getAktiveMiljoer() {
+        log.info("Henter aktive miljøer fra TPSF..");
+        return webClient
+                .get()
+                .uri(URI.create(tpsfServerUrl + TPSF_GET_ENVIRONMENTS))
+                .retrieve()
+                .bodyToMono(MiljoerResponse.class)
+                .block();
     }
 }
