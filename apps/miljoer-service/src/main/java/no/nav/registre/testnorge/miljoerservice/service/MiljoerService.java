ec8447453d4426eaa35fca5fa4bea13d2d8140e7
stigus <stian.gustavsson@nav.no>
2021-01-06 10:40:44 +0100

    Mindre endringer i MiljoerService

--- apps/miljoer-service/src/main/java/no/nav/registre/testnorge/miljoerservice/service/MiljoerService.java
+++ apps/miljoer-service/src/main/java/no/nav/registre/testnorge/miljoerservice/service/MiljoerService.java
@@ -5,11 +5,10 @@ import no.nav.registre.testnorge.libs.dependencyanalysis.DependencyOn;
 import no.nav.registre.testnorge.miljoerservice.response.MiljoerResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.reactive.function.client.WebClient;
-
-import java.net.URI;
 
 import static java.util.Objects.isNull;
 
@@ -26,20 +25,20 @@ public class MiljoerService {
     }
 
     public MiljoerResponse getAktiveMiljoer() {
-        log.info("Henter aktive miljøer fra TPSF..");
-        MiljoerResponse response = webClient
+        log.info("Henter aktive miljøer fra TPSF.");
+        ResponseEntity<MiljoerResponse> response = webClient
                 .get()
-                .uri(URI.create(TPSF_GET_ENVIRONMENTS))
+                .uri(uriBuilder -> uriBuilder.path(TPSF_GET_ENVIRONMENTS).build())
                 .retrieve()
-                .bodyToMono(MiljoerResponse.class)
+                .toEntity(MiljoerResponse.class)
                 .block();
 
-        if (isNull(response)) {
+        assert response != null;
+        if (isNull(response.getBody()) || isNull(response.getBody().getEnvironments())) {
             throw new HttpClientErrorException(HttpStatus.INTERNAL_SERVER_ERROR, "Fant ingen miljøer");
-
         }
-        response.getEnvironments().sort(String::compareTo);
+        response.getBody().getEnvironments().sort(String::compareTo);
 
-        return response;
+        return response.getBody();
     }
 }
