96bd471e184f2f5092bb3cec45cfc2004b4b8346
stigus <stian.gustavsson@nav.no>
2021-01-06 10:21:06 +0100

    Lagt til Miljoer-service i oversikt-frontend igjen, liten endring i Webclient

--- apps/miljoer-service/src/main/java/no/nav/registre/testnorge/miljoerservice/service/MiljoerService.java
+++ apps/miljoer-service/src/main/java/no/nav/registre/testnorge/miljoerservice/service/MiljoerService.java
@@ -1,6 +1,5 @@
 package no.nav.registre.testnorge.miljoerservice.service;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.registre.testnorge.libs.dependencyanalysis.DependencyOn;
 import no.nav.registre.testnorge.miljoerservice.response.MiljoerResponse;
@@ -15,23 +14,22 @@ import java.net.URI;
 import static java.util.Objects.isNull;
 
 @Service
-@RequiredArgsConstructor
 @DependencyOn(value = "tps-forvalteren", external = true)
 @Slf4j
 public class MiljoerService {
 
     private static final String TPSF_GET_ENVIRONMENTS = "/v1/environments";
+    private final WebClient webClient;
 
-    private final WebClient webClient = WebClient.builder().build();
-
-    @Value("${tps-forvalteren.rest-api.url}")
-    private String tpsfServerUrl;
+    public MiljoerService(@Value("${tps-forvalteren.rest-api.url}") String tpsfServerUrl) {
+        this.webClient = WebClient.builder().baseUrl(tpsfServerUrl).build();
+    }
 
     public MiljoerResponse getAktiveMiljoer() {
         log.info("Henter aktive miljøer fra TPSF..");
         MiljoerResponse response = webClient
                 .get()
-                .uri(URI.create(tpsfServerUrl + TPSF_GET_ENVIRONMENTS))
+                .uri(URI.create(TPSF_GET_ENVIRONMENTS))
                 .retrieve()
                 .bodyToMono(MiljoerResponse.class)
                 .block();
