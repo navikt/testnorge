1c7bd1df2785bcbfea683c67721f1904096f4205
stigus <stian.gustavsson@nav.no>
2021-01-06 11:29:28 +0100

    Returnerer bare Liste med Strings istedenfor hele MiljoerResponse objekt

--- apps/miljoer-service/src/main/java/no/nav/registre/testnorge/miljoerservice/provider/MiljoeController.java
+++ apps/miljoer-service/src/main/java/no/nav/registre/testnorge/miljoerservice/provider/MiljoeController.java
@@ -2,12 +2,13 @@ package no.nav.registre.testnorge.miljoerservice.provider;
 
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import no.nav.registre.testnorge.miljoerservice.response.MiljoerResponse;
 import no.nav.registre.testnorge.miljoerservice.service.MiljoerService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/miljoer")
@@ -18,7 +19,7 @@ public class MiljoeController {
 
     @GetMapping
     @Operation(description = "Tjeneste for å sjekke hvilke miljøer i test og preprod som er tilgjengelige nå")
-    public ResponseEntity<MiljoerResponse> hentAktiveMiljoer() {
-        return ResponseEntity.ok(service.getAktiveMiljoer());
+    public ResponseEntity<List<String>> hentAktiveMiljoer() {
+        return ResponseEntity.ok(service.getAktiveMiljoer().getEnvironments());
     }
 }
