5a70f40c426d9183a02abf19f1d2f8d52856536a
Stian Gustavsson <66469591+stigus@users.noreply.github.com>
2021-01-12 15:22:07 +0100

    Aareg fix (#421)
    Fikset til riktig returnert type og lagt på client-id for lokal kjøring

--- apps/testnorge-aareg/aareg-core/src/main/java/no/nav/registre/aareg/consumer/rs/MiljoerConsumer.java
+++ apps/testnorge-aareg/aareg-core/src/main/java/no/nav/registre/aareg/consumer/rs/MiljoerConsumer.java
@@ -6,11 +6,14 @@ import no.nav.registre.testnorge.libs.oauth2.domain.AccessScopes;
 import no.nav.registre.testnorge.libs.oauth2.domain.AccessToken;
 import no.nav.registre.testnorge.libs.oauth2.service.AccessTokenService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
 
 @Component
 @DependencyOn(value = "testnorge-miljoer-service")
@@ -21,32 +24,33 @@ public class MiljoerConsumer {
     private final AccessScopes accessScopes;
 
     public MiljoerConsumer(@Value("${miljoer-service.rest-api.url}") String miljoeUrl,
-                           @Value("${miljoer-service.client-id}") String clientId,
+                           @Value("${miljoer-service.client-id}") String miljoeServiceClientId,
                            AccessTokenService accessTokenService
     ) {
-        this.accessScopes = new AccessScopes("api://" + clientId + "/.default");
+        this.accessScopes = new AccessScopes("api://" + miljoeServiceClientId + "/.default");
         this.accessTokenService = accessTokenService;
         this.webClient = WebClient.builder()
                 .baseUrl(miljoeUrl)
                 .build();
-
     }
 
     public MiljoerResponse hentMiljoer() {
+
         AccessToken accessToken = accessTokenService.generateToken(accessScopes);
-        MiljoerResponse response = webClient
+        List<String> response = webClient
                 .get()
                 .uri(uriBuilder -> uriBuilder
                         .path("/v1/miljoer")
                         .build())
                 .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken.getTokenValue())
                 .retrieve()
-                .bodyToMono(MiljoerResponse.class)
+                .bodyToMono(new ParameterizedTypeReference<List<String>>() {
+                })
                 .block();
 
         if (response == null) {
             throw new HttpClientErrorException(HttpStatus.INTERNAL_SERVER_ERROR, "Fant ingen miljøer");
         }
-        return response;
+        return new MiljoerResponse(response);
     }
 }
