aa149a78a33ed03b8eb98a404e5e63f7f0eb948e
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-01-19 08:57:18 +0100

    cleanup

--- apps/batch-adeo-proxy/src/main/java/no/nav/registre/testnorge/batchadeoproxy/config/ApplicationConfig.java
+++ apps/batch-adeo-proxy/src/main/java/no/nav/registre/testnorge/batchadeoproxy/config/ApplicationConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
 import no.nav.registre.testnorge.libs.core.config.ApplicationCoreConfig;
-import no.nav.registre.testnorge.libs.localdevelopment.LocalDevelopmentConfig;
 import no.nav.registre.testnorge.libs.oauth2.config.SecureOAuth2ServerToServerConfiguration;
 
 @Configuration
