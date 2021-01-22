9b48b261f241558f26431aac999aaea265dc4e53
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-01-22 15:30:56 +0100

    frontend

--- libs/analysis-auto-configuration/src/main/java/no/nav/registre/testnorge/libs/analysisautoconfiguration/config/credentials/ApplikasjonsanalyseServiceGCPProperties.java
+++ libs/analysis-auto-configuration/src/main/java/no/nav/registre/testnorge/libs/analysisautoconfiguration/config/credentials/ApplikasjonsanalyseServiceGCPProperties.java
@@ -1,9 +1,6 @@
 package no.nav.registre.testnorge.libs.analysisautoconfiguration.config.credentials;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-
-import no.nav.registre.testnorge.libs.oauth2.config.NaisServerProperties;
 
 
 @Configuration
