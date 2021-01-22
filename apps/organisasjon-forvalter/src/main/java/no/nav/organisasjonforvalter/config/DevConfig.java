34073653d8d5409a67af1d66398f479499faa7a4
kristenhaerum <kristen.herum@nav.no>
2021-01-22 13:52:03 +0100

    Lagt til støtte for magic token

---
+++ apps/organisasjon-forvalter/src/main/java/no/nav/organisasjonforvalter/config/DevConfig.java
@@ -0,0 +1,12 @@
+package no.nav.organisasjonforvalter.config;
+
+import no.nav.registre.testnorge.libs.localdevelopment.LocalDevelopmentConfig;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
+@Import(LocalDevelopmentConfig.class)
+public class DevConfig {
+}
