5f786e1ad313c9501d852d85ce726476c41d55a8
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-01-22 16:28:10 +0100

    create

--- libs/analysis-auto-configuration/src/main/java/no/nav/registre/testnorge/libs/analysisautoconfiguration/config/ApplicationProperties.java
+++ libs/analysis-auto-configuration/src/main/java/no/nav/registre/testnorge/libs/analysisautoconfiguration/config/ApplicationProperties.java
@@ -10,17 +10,14 @@ public class ApplicationProperties {
     private final String name;
     private final String cluster;
     private final String namespace;
-    private final String basePackage;
 
     public ApplicationProperties(
             @Value("${application.name:${spring.application.name}}") String name,
-            @Value("${application.cluster:${spring.application.cluster}}") String cluster,
-            @Value("${application.namespace:${spring.application.namespace}}") String namespace,
-            @Value("${application.basepackage:no.nav.registre}") String basePackage
+            @Value("${application.cluster:${spring.application.cluster:unknown}}") String cluster,
+            @Value("${application.namespace:${spring.application.namespace:unknown}}") String namespace
     ) {
         this.name = name;
         this.cluster = cluster;
         this.namespace = namespace;
-        this.basePackage = basePackage;
     }
 }
