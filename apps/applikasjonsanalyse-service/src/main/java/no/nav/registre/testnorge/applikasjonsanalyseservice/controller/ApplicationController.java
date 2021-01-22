05cfc9747f71ddeec0c6f145c2b2a9b6a6ee10eb
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-01-22 17:04:44 +0100

    fix

--- apps/applikasjonsanalyse-service/src/main/java/no/nav/registre/testnorge/applikasjonsanalyseservice/controller/ApplicationController.java
+++ apps/applikasjonsanalyse-service/src/main/java/no/nav/registre/testnorge/applikasjonsanalyseservice/controller/ApplicationController.java
@@ -7,9 +7,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import java.net.URI;
 
 import no.nav.registre.testnorge.applikasjonsanalyseservice.adapter.ApplicationAdapter;
 import no.nav.registre.testnorge.applikasjonsanalyseservice.domain.ApplicationInfo;
@@ -22,9 +19,8 @@ public class ApplicationController {
     private final ApplicationAdapter applicationAdapter;
 
     @PutMapping
-    public ResponseEntity<HttpStatus> save(@RequestBody ApplicationInfoDTO dto){
+    public ResponseEntity<HttpStatus> save(@RequestBody ApplicationInfoDTO dto) {
         applicationAdapter.save(new ApplicationInfo(dto));
         return ResponseEntity.ok().build();
     }
-
 }
