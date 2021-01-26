d25bf17d6764c7a8ab1bd9d539ffbf557a624dad
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-01-26 16:12:44 +0100

    fix

---
+++ apps/jenkins-batch-status-service/src/main/java/no/nav/registre/testnorge/jenkinsbatchstatusservice/config/credentials/OrganisasjonBestillingServiceProperties.java
@@ -0,0 +1,12 @@
+package no.nav.registre.testnorge.jenkinsbatchstatusservice.config.credentials;
+
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import no.nav.registre.testnorge.libs.oauth2.config.NaisServerProperties;
+
+@Configuration
+@ConfigurationProperties(prefix = "consumers.organisasjon-bestilling-service")
+public class OrganisasjonBestillingServiceProperties extends NaisServerProperties {
+}
