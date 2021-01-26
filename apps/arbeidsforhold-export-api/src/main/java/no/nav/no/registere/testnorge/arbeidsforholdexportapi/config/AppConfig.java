825fdbb6429d56e2e5dafd373b2bf843507cb699
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-01-26 17:55:05 +0100

    fix

--- apps/arbeidsforhold-export-api/src/main/java/no/nav/no/registere/testnorge/arbeidsforholdexportapi/config/AppConfig.java
+++ apps/arbeidsforhold-export-api/src/main/java/no/nav/no/registere/testnorge/arbeidsforholdexportapi/config/AppConfig.java
@@ -6,11 +6,13 @@ import org.springframework.context.annotation.Import;
 import no.nav.registre.testnorge.libs.autoconfigdependencyanalysis.config.DependencyAnalysisAutoConfiguration;
 import no.nav.registre.testnorge.libs.core.config.AnalysisFSSAutoConfiguration;
 import no.nav.registre.testnorge.libs.core.config.ApplicationCoreConfig;
+import no.nav.registre.testnorge.libs.oauth2.config.SecureOAuth2ServerToServerConfiguration;
 
 @Configuration
 @Import({
         ApplicationCoreConfig.class,
         DependencyAnalysisAutoConfiguration.class,
+        SecureOAuth2ServerToServerConfiguration.class,
         AnalysisFSSAutoConfiguration.class
 })
 public class AppConfig {
--- apps/endringsmelding-service/src/main/java/no/nav/registre/testnorge/endringsmeldingservice/config/ApplicationConfig.java
+++ apps/endringsmelding-service/src/main/java/no/nav/registre/testnorge/endringsmeldingservice/config/ApplicationConfig.java
@@ -5,9 +5,14 @@ import org.springframework.context.annotation.Import;
 
 import no.nav.registre.testnorge.libs.core.config.AnalysisFSSAutoConfiguration;
 import no.nav.registre.testnorge.libs.core.config.ApplicationCoreConfig;
+import no.nav.registre.testnorge.libs.oauth2.config.SecureOAuth2ServerToServerConfiguration;
 
 @Configuration
-@Import({ApplicationCoreConfig.class, AnalysisFSSAutoConfiguration.class})
+@Import({
+        ApplicationCoreConfig.class,
+        SecureOAuth2ServerToServerConfiguration.class,
+        AnalysisFSSAutoConfiguration.class
+})
 public class ApplicationConfig {
 
 }
