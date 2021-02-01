4d456e0ff951e760ed6b2f2524fe378b565c201d
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-02-01 12:52:10 +0100

    slett arbeidsforhold-api

--- apps/arbeidsforhold-api/src/main/java/no/nav/registre/testnorge/arbeidsforhold/repository/elastic/OppsummeringsdokumentRepository.java
+++
@@ -1,8 +0,0 @@
-package no.nav.registre.testnorge.arbeidsforhold.repository.elastic;
-
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-import no.nav.registre.testnorge.arbeidsforhold.repository.elastic.model.OppsummeringsdokumentModel;
-
-public interface OppsummeringsdokumentRepository extends ElasticsearchRepository<OppsummeringsdokumentModel, String> {
-}
