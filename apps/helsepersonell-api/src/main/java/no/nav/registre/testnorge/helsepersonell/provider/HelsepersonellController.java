1d51ced05c137a1415e3ab959cd784ccd3f3f27a
stigus <stian.gustavsson@nav.no>
2021-02-03 09:54:45 +0100

    Mindre endringer

--- apps/helsepersonell-api/src/main/java/no/nav/registre/testnorge/helsepersonell/provider/HelsepersonellController.java
+++ apps/helsepersonell-api/src/main/java/no/nav/registre/testnorge/helsepersonell/provider/HelsepersonellController.java
@@ -1,44 +1,22 @@
 package no.nav.registre.testnorge.helsepersonell.provider;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.CacheControl;
-import org.springframework.http.ResponseEntity;
+import lombok.RequiredArgsConstructor;
+import no.nav.registre.testnorge.helsepersonell.adapter.HelsepersonellAdapter;
+import no.nav.registre.testnorge.libs.dto.helsepersonell.v1.HelsepersonellListeDTO;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.concurrent.TimeUnit;
-
-import no.nav.registre.testnorge.helsepersonell.adapter.HelsepersonellAdapter;
-import no.nav.registre.testnorge.libs.dto.helsepersonell.v1.HelsepersonellListeDTO;
-
 
 @RestController
 @RequestMapping("/api/v1/helsepersonell")
+@RequiredArgsConstructor
 public class HelsepersonellController {
 
     private final HelsepersonellAdapter adapter;
-    private final Integer helsepersonellCacheHours;
-
-
-    public HelsepersonellController(
-            HelsepersonellAdapter adapter,
-            @Value("${helsepersonell.controller.cache.hours}") Integer helsepersonellCacheHours
-    ) {
-        this.adapter = adapter;
-        this.helsepersonellCacheHours = helsepersonellCacheHours;
-    }
 
     @GetMapping
-    public ResponseEntity<HelsepersonellListeDTO> getHelsepersonell() {
-        var cacheControl
-                = CacheControl.maxAge(helsepersonellCacheHours, TimeUnit.HOURS)
-                .noTransform()
-                .mustRevalidate();
-        var helsepersonell = adapter.getHelsepersonell();
-        return ResponseEntity
-                .ok()
-                .cacheControl(cacheControl)
-                .body(helsepersonell.toDTO());
+    public HelsepersonellListeDTO getHelsepersonell() {
+        return adapter.getHelsepersonell().toDTO();
     }
 }
