ffec29890dcca5ad39fb03f97a51745cebe72583
Stian Gustavsson <66469591+stigus@users.noreply.github.com>
2021-02-05 10:22:44 +0100

    Bugfix/synt sykemelding connection timeout (#470)
    * Lagt på https for å forhindre connection timeout mot endepunkt
    * Lagt på sikkerhet på kall mot arbeidsforhold-api og endret til Webclient
    * Nytt elsam endepunkt
    * Nytt helsepersonell endepunkt

--- apps/synt-sykemelding-api/src/main/java/no/nav/registre/testnorge/synt/sykemelding/consumer/ArbeidsforholdConsumer.java
+++ apps/synt-sykemelding-api/src/main/java/no/nav/registre/testnorge/synt/sykemelding/consumer/ArbeidsforholdConsumer.java
@@ -1,31 +1,51 @@
 package no.nav.registre.testnorge.synt.sykemelding.consumer;
 
 import lombok.SneakyThrows;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
-
+import lombok.extern.slf4j.Slf4j;
 import no.nav.registre.testnorge.libs.dependencyanalysis.DependencyOn;
 import no.nav.registre.testnorge.libs.dto.oppsummeringsdokumentservice.v1.ArbeidsforholdDTO;
+import no.nav.registre.testnorge.libs.oauth2.domain.AccessScopes;
+import no.nav.registre.testnorge.libs.oauth2.service.AccessTokenService;
 import no.nav.registre.testnorge.synt.sykemelding.consumer.command.GetArbeidsforholdCommand;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Component
+@Slf4j
 @DependencyOn("testnorge-arbeidsforhold-api")
 public class ArbeidsforholdConsumer {
-    private final RestTemplate restTemplate;
-    private final String url;
+    private final AccessTokenService accessTokenService;
+    private final WebClient webClient;
+    private final String clientId;
 
     public ArbeidsforholdConsumer(
-            RestTemplateBuilder restTemplateBuilder,
+            AccessTokenService accessTokenService,
+            @Value("${consumers.arbeidsforhold.client_id}") String clientId,
             @Value("${consumers.arbeidsforhold.url}") String url
     ) {
-        this.restTemplate = restTemplateBuilder.build();
-        this.url = url;
+        this.accessTokenService = accessTokenService;
+        this.clientId = clientId;
+        this.webClient = WebClient
+                .builder()
+                .baseUrl(url)
+                .build();
     }
 
     @SneakyThrows
     public ArbeidsforholdDTO getArbeidsforhold(String ident, String orgnummer, String arbeidsforholdId) {
-        return new GetArbeidsforholdCommand(restTemplate, url, ident, orgnummer, arbeidsforholdId).call();
+
+        log.info("Henter arbeidsforhold for {} i org {} med id {}", ident, orgnummer, arbeidsforholdId);
+
+        ArbeidsforholdDTO arbeidsforholdDTO = new GetArbeidsforholdCommand(
+                webClient,
+                accessTokenService.generateToken(new AccessScopes("api://" + clientId + "/.default")).getTokenValue(),
+                ident,
+                orgnummer,
+                arbeidsforholdId
+        ).call();
+
+        log.info("Arbeidsforhold for ident: {} hentet", arbeidsforholdDTO.getIdent());
+        return arbeidsforholdDTO;
     }
 }
