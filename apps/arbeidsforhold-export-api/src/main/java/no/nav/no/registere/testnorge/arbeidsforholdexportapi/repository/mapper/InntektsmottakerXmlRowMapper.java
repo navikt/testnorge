38571049ca9a92428c71e52b8628c153866ee26d
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-02-08 10:29:25 +0100

    Log number of rows

--- apps/arbeidsforhold-export-api/src/main/java/no/nav/no/registere/testnorge/arbeidsforholdexportapi/repository/mapper/InntektsmottakerXmlRowMapper.java
+++ apps/arbeidsforhold-export-api/src/main/java/no/nav/no/registere/testnorge/arbeidsforholdexportapi/repository/mapper/InntektsmottakerXmlRowMapper.java
@@ -1,5 +1,6 @@
 package no.nav.no.registere.testnorge.arbeidsforholdexportapi.repository.mapper;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
@@ -8,9 +9,13 @@ import java.sql.SQLException;
 import no.nav.no.registere.testnorge.arbeidsforholdexportapi.domain.OpplysningspliktigList;
 import no.nav.registre.testnorge.xsd.arbeidsforhold.v2_0.EDAGM;
 
+@Slf4j
 public class InntektsmottakerXmlRowMapper implements RowMapper<EDAGM> {
     @Override
     public EDAGM mapRow(ResultSet rs, int rowNum) throws SQLException {
+        if(rowNum % 1000 == 0){
+            log.info("Antall rader behandlet {}.", rowNum);
+        }
         return OpplysningspliktigList.from(rs.getString("INNTEKTSMOTTAKER_XML"));
     }
 }
