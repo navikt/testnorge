18d9d2f944991ebd1ac7c198c0aae4adb28bde09
Hay <Aileen.Hay@nav.no>
2021-02-09 12:50:27 +0100

    Lagt til parallellkjoring i testnorge arena og oppdatert batch kjoring

--- apps/testnorge-arena/src/main/java/no/nav/registre/testnorge/arena/service/VedtakshistorikkService.java
+++ apps/testnorge-arena/src/main/java/no/nav/registre/testnorge/arena/service/VedtakshistorikkService.java
@@ -17,7 +17,10 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -75,8 +78,35 @@ public class VedtakshistorikkService {
             String miljoe,
             int antallNyeIdenter
     ) {
-        var vedtakshistorikk = vedtakshistorikkSyntConsumer.syntetiserVedtakshistorikk(antallNyeIdenter);
         Map<String, List<NyttVedtakResponse>> responses = new HashMap<>();
+        var intStream = IntStream.range(0, antallNyeIdenter).boxed().collect(Collectors.toList());
+        ForkJoinPool forkJoinPool = null;
+        try {
+            forkJoinPool = new ForkJoinPool(10);
+            forkJoinPool.submit(() ->
+                    intStream.parallelStream().forEach(i ->
+                            opprettHistorikkForIdent(avspillergruppeId, miljoe, responses)
+                    )
+            ).get();
+        } catch (InterruptedException e) {
+            log.error("Kunne ikke opprette vedtakshistorikk.", e);
+            Thread.currentThread().interrupt();
+        } catch (ExecutionException e) {
+            log.error("Kunne ikke opprette vedtakshistorikk.", e);
+        } finally {
+            if (forkJoinPool != null) {
+                forkJoinPool.shutdown();
+            }
+        }
+        return responses;
+    }
+
+    private void opprettHistorikkForIdent(
+            Long avspillergruppeId,
+            String miljoe,
+            Map<String, List<NyttVedtakResponse>> responses
+    ) {
+        var vedtakshistorikk = vedtakshistorikkSyntConsumer.syntetiserVedtakshistorikk(1);
         for (var vedtakshistorikken : vedtakshistorikk) {
             vedtakshistorikken.setTilsynFamiliemedlemmer(fjernTilsynFamiliemedlemmerVedtakMedUgyldigeDatoer(vedtakshistorikken.getTilsynFamiliemedlemmer()));
             vedtakshistorikken.setUngUfoer(fjernAapUngUfoerMedUgyldigeDatoer(vedtakshistorikken.getUngUfoer()));
@@ -86,7 +116,7 @@ public class VedtakshistorikkService {
             LocalDate tidligsteDatoBarnetillegg = datoUtils.finnTidligeDatoBarnetillegg(vedtakshistorikken.getBarnetillegg());
 
             if (tidligsteDato == null) {
-                continue;
+                return;
             }
 
             var minimumAlder = Math.toIntExact(ChronoUnit.YEARS.between(tidligsteDato.minusYears(MIN_ALDER_AAP), LocalDate.now()));
@@ -98,7 +128,6 @@ public class VedtakshistorikkService {
                 opprettVedtaksHistorikkResponse(avspillergruppeId, miljoe, responses, vedtakshistorikken, tidligsteDatoBarnetillegg, minimumAlder, maksimumAlder);
             }
         }
-        return responses;
     }
 
     private int getMaksimumAlder(
