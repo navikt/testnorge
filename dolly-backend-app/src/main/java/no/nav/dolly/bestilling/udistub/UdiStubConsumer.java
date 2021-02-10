88f813a2792db392b58c6d0b53cc7d58f1988a80
Stian Gustavsson <66469591+stigus@users.noreply.github.com>
2021-02-10 14:02:19 +0100

    Lagt på sikkerhet i kall mot UDI-stub og endret til bruk av webclient (#62)
    * Lagt på sikkerhet i kall mot UDI-stub og endret til bruk av webclient
    * Mulighet for forskjellig ClientId for dev og prod udi-stub
    * Formatering og test fikser

--- dolly-backend-app/src/main/java/no/nav/dolly/bestilling/udistub/UdiStubConsumer.java
+++ dolly-backend-app/src/main/java/no/nav/dolly/bestilling/udistub/UdiStubConsumer.java
@@ -1,49 +1,66 @@
 package no.nav.dolly.bestilling.udistub;
 
-import static java.lang.String.format;
-import static no.nav.dolly.domain.CommonKeysAndUtils.HEADER_NAV_CALL_ID;
-import static no.nav.dolly.domain.CommonKeysAndUtils.HEADER_NAV_CONSUMER_ID;
-
-import java.net.URI;
-import java.util.UUID;
-import org.springframework.http.MediaType;
-import org.springframework.http.RequestEntity;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.dolly.bestilling.udistub.domain.UdiPerson;
 import no.nav.dolly.bestilling.udistub.domain.UdiPersonResponse;
 import no.nav.dolly.errorhandling.ErrorStatusDecoder;
 import no.nav.dolly.metrics.Timed;
 import no.nav.dolly.properties.ProvidersProps;
+import no.nav.dolly.security.oauth2.domain.AccessScopes;
+import no.nav.dolly.security.oauth2.service.TokenService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+
+import static java.lang.String.format;
+import static no.nav.dolly.domain.CommonKeysAndUtils.HEADER_NAV_CALL_ID;
+import static no.nav.dolly.domain.CommonKeysAndUtils.HEADER_NAV_CONSUMER_ID;
 
 @Slf4j
 @Service
-@RequiredArgsConstructor
 public class UdiStubConsumer {
 
     private static final String CONSUMER = "Dolly";
     private static final String NAV_PERSON_IDENT = "Nav-Personident";
     private static final String UDISTUB_PERSON = "/api/v1/person";
 
-    private final RestTemplate restTemplate;
-    private final ProvidersProps providersProps;
+    private final TokenService accessTokenService;
+    private final WebClient webClient;
     private final ErrorStatusDecoder errorStatusDecoder;
+    private final String clientId;
 
-    @Timed(name = "providers", tags = { "operation", "udi_createPerson" })
+    public UdiStubConsumer(
+            ErrorStatusDecoder errorStatusDecoder,
+            TokenService accessTokenService,
+            ProvidersProps providersProps,
+            @Value("${UDI_STUB_CLIENT_ID}") String clientId
+    ) {
+        this.accessTokenService = accessTokenService;
+        this.webClient = WebClient.builder()
+                .baseUrl(providersProps.getUdiStub().getUrl()).build();
+        this.errorStatusDecoder = errorStatusDecoder;
+        this.clientId = clientId;
+    }
+
+    @Timed(name = "providers", tags = { "operation", "udi_getPerson" })
     public UdiPersonResponse getUdiPerson(String ident) {
 
         try {
-            ResponseEntity<UdiPersonResponse> response = restTemplate.exchange(RequestEntity.get(
-                    URI.create(format("%s%s/%s/", providersProps.getUdiStub().getUrl(), UDISTUB_PERSON, ident)))
-                            .header(HEADER_NAV_CALL_ID, getNavCallId())
-                            .header(HEADER_NAV_CONSUMER_ID, CONSUMER)
-                            .build(),
-                    UdiPersonResponse.class);
-            return response.hasBody() ? response.getBody() : null;
+            return webClient
+                    .get()
+                    .uri(uriBuilder -> uriBuilder.path(UDISTUB_PERSON + "/" + ident).build())
+                    .header(HEADER_NAV_CALL_ID, getNavCallId())
+                    .header(HEADER_NAV_CONSUMER_ID, CONSUMER)
+                    .header(HttpHeaders.AUTHORIZATION, getBearerToken())
+                    .retrieve()
+                    .bodyToMono(UdiPersonResponse.class)
+                    .block();
 
         } catch (RuntimeException e) {
             return null;
@@ -51,40 +68,61 @@ public class UdiStubConsumer {
     }
 
     @Timed(name = "providers", tags = { "operation", "udi_createPerson" })
-    public ResponseEntity<UdiPersonResponse> createUdiPerson(UdiPerson udiPerson) {
+    public UdiPersonResponse createUdiPerson(UdiPerson udiPerson) {
 
-        return restTemplate.exchange(RequestEntity.post(URI.create(providersProps.getUdiStub().getUrl() + UDISTUB_PERSON))
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .header(HEADER_NAV_CALL_ID, getNavCallId())
-                        .header(HEADER_NAV_CONSUMER_ID, CONSUMER)
-                        .body(udiPerson),
-                UdiPersonResponse.class);
+        return webClient
+                .post()
+                .uri(uriBuilder -> uriBuilder.path(UDISTUB_PERSON).build())
+                .accept(MediaType.APPLICATION_JSON)
+                .header(HEADER_NAV_CALL_ID, getNavCallId())
+                .header(HEADER_NAV_CONSUMER_ID, CONSUMER)
+                .header(HttpHeaders.AUTHORIZATION, getBearerToken())
+                .body(BodyInserters.fromPublisher(Mono.just(udiPerson), UdiPerson.class))
+                .retrieve()
+                .bodyToMono(UdiPersonResponse.class)
+                .block();
     }
 
-    @Timed(name = "providers", tags = { "operation", "udi_updatePerson" })
-    public ResponseEntity<UdiPersonResponse> updateUdiPerson(UdiPerson udiPerson) {
 
-        return restTemplate.exchange(RequestEntity.put(URI.create(providersProps.getUdiStub().getUrl() + UDISTUB_PERSON))
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .header(HEADER_NAV_CALL_ID, getNavCallId())
-                        .header(HEADER_NAV_CONSUMER_ID, CONSUMER)
-                        .body(udiPerson),
-                UdiPersonResponse.class);
+    @Timed(name = "providers", tags = { "operation", "udi_updatePerson" })
+    public UdiPersonResponse updateUdiPerson(UdiPerson udiPerson) {
+
+        return webClient
+                .put()
+                .uri(uriBuilder -> uriBuilder.path(UDISTUB_PERSON).build())
+                .header(HEADER_NAV_CALL_ID, getNavCallId())
+                .header(HEADER_NAV_CONSUMER_ID, CONSUMER)
+                .header(HttpHeaders.AUTHORIZATION, getBearerToken())
+                .body(BodyInserters.fromPublisher(Mono.just(udiPerson), UdiPerson.class))
+                .retrieve()
+                .bodyToMono(UdiPersonResponse.class)
+                .block();
     }
 
     @Timed(name = "providers", tags = { "operation", "udi_deletePerson" })
     public void deleteUdiPerson(String ident) {
 
         try {
-            restTemplate.exchange(RequestEntity.delete(URI.create(providersProps.getUdiStub().getUrl() + UDISTUB_PERSON))
+            webClient
+                    .put()
+                    .uri(uriBuilder -> uriBuilder.path(UDISTUB_PERSON).build())
                     .header(HEADER_NAV_CALL_ID, getNavCallId())
                     .header(HEADER_NAV_CONSUMER_ID, CONSUMER)
                     .header(NAV_PERSON_IDENT, ident)
-                    .build(), Object.class);
+                    .header(HttpHeaders.AUTHORIZATION, getBearerToken())
+                    .exchange()
+                    .block();
 
         } catch (RuntimeException e) {
             errorStatusDecoder.decodeRuntimeException(e);
         }
+    }
+
+    private String getBearerToken() {
+        String tokenValue = accessTokenService.getAccessToken(
+                new AccessScopes("api://" + clientId + "/.default")
+        ).getTokenValue();
+        return "Bearer " + tokenValue;
     }
 
     private static String getNavCallId() {
