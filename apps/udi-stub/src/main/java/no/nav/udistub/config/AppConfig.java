02ccc0c95ce0f65a0db991f72d4bd03cc220797c
kristenhaerum <kristen.herum@nav.no>
2021-02-10 13:42:31 +0100

    Oppdatert config

--- apps/udi-stub/src/main/java/no/nav/udistub/config/AppConfig.java
+++ apps/udi-stub/src/main/java/no/nav/udistub/config/AppConfig.java
@@ -3,8 +3,10 @@ package no.nav.udistub.config;
 import no.nav.registre.testnorge.libs.core.config.ApplicationCoreConfig;
 import no.nav.registre.testnorge.libs.database.config.VaultHikariConfiguration;
 import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
+@Configuration
 @Import({ApplicationCoreConfig.class,
         FlywayAutoConfiguration.class,
         VaultHikariConfiguration.class})
