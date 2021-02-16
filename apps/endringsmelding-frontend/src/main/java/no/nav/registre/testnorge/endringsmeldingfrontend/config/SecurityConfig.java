3ce780067802c486434581b7f0c711f21f4b7ec1
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-02-16 17:41:53 +0100

    test

--- apps/endringsmelding-frontend/src/main/java/no/nav/registre/testnorge/endringsmeldingfrontend/config/SecurityConfig.java
+++ apps/endringsmelding-frontend/src/main/java/no/nav/registre/testnorge/endringsmeldingfrontend/config/SecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configurers.oauth2.client.OAuth2LoginConfigurer;
 
 
 @Slf4j
@@ -18,8 +17,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/internal/isAlive", "/internal/isReady", "/bundle.*.js", "/main.*.css", "/login", "/oauth2/authorization/aad").permitAll()
-                .anyRequest().fullyAuthenticated()
+                .antMatchers("/api/**")
+                .fullyAuthenticated()
                 .and()
                 .oauth2Client()
                 .and()
