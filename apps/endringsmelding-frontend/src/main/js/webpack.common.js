791880802c1cf8b11d2ee8453d3a7089165c305f
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-02-16 10:33:57 +0100

    testing

--- apps/endringsmelding-frontend/src/main/js/webpack.common.js
+++ apps/endringsmelding-frontend/src/main/js/webpack.common.js
@@ -1,61 +1,61 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const path = require("path");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: [MiniCssExtractPlugin.loader, "css-loader"],
-            },
-            {
-                test: /\.less$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'less-loader'
-                ]
-            },
-            {
-                test: /\.(png|jpe?g|gif|svg)$/i,
-                use: [
-                    {
-                        loader: 'file-loader',
-                    },
-                ],
-            },
-            {
-                test: /\.js|.ts(x?)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                },
-            },
-            {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: "html-loader",
-                    },
-                ],
-            },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.less$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
         ],
-    },
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "src"),
+      },
+      {
+        test: /\.js|.ts(x?)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
         },
-        extensions: [".tsx", ".ts", ".js"],
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./public/index.html",
-            filename: "./index.html",
-        }),
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
     ],
-    output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
     },
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: 'public', to: '.' }],
+    }),
+    new HtmlWebPackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+    }),
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
 };
