ed6a208fb44bb595ab9143ba3aa5def9decf5f15
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-02-16 15:36:51 +0100

    login

--- apps/endringsmelding-frontend/src/main/java/no/nav/registre/testnorge/endringsmeldingfrontend/config/SecurityConfig.java
+++ apps/endringsmelding-frontend/src/main/java/no/nav/registre/testnorge/endringsmeldingfrontend/config/SecurityConfig.java
@@ -17,7 +17,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/internal/isAlive", "/internal/isReady").permitAll()
+                .antMatchers("/internal/isAlive", "/internal/isReady", "/page/login").permitAll()
                 .anyRequest().fullyAuthenticated()
                 .and()
                 .oauth2Client()
