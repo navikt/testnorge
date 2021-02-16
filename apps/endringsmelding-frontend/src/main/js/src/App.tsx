4c39250ed4c16f80137056aa1830e152e4eb9618
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-02-16 13:42:44 +0100

    test

--- apps/endringsmelding-frontend/src/main/js/src/App.tsx
+++ apps/endringsmelding-frontend/src/main/js/src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/page/login">
+        <Route path="/login">
           <Header />
           <LoginPage />
         </Route>
