3fe6969df6e11cfc0ca632e816827ea14b2164c2
kristenhaerum <kristen.herum@nav.no>
2021-02-17 15:44:16 +0100

    Forenklet application starter

--- apps/udi-stub/src/main/java/no/nav/udistub/UdiStubApplicationStarter.java
+++ apps/udi-stub/src/main/java/no/nav/udistub/UdiStubApplicationStarter.java
@@ -3,10 +3,9 @@ package no.nav.udistub;
 import no.nav.registre.testnorge.libs.core.util.VaultUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class UdiStubApplicationStarter extends SpringBootServletInitializer {
+public class UdiStubApplicationStarter {
 
     public static void main(String[] args) {
 
