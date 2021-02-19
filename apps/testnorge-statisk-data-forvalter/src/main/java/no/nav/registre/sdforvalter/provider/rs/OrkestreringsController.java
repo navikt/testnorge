c537f8c85ce7437d853de2d3055fd70c254f3cef
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-02-19 10:43:18 +0100

    test

--- apps/testnorge-statisk-data-forvalter/src/main/java/no/nav/registre/sdforvalter/provider/rs/OrkestreringsController.java
+++ apps/testnorge-statisk-data-forvalter/src/main/java/no/nav/registre/sdforvalter/provider/rs/OrkestreringsController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -78,7 +79,7 @@ public class OrkestreringsController {
     }
 
     @PostMapping(value = "/organisasjoner")
-    public ResponseEntity<HttpStatus> saveBy(@RequestParam String miljoe, Orgnummer orgnummer) {
+    public ResponseEntity<HttpStatus> saveBy(@RequestParam String miljoe, @RequestBody Orgnummer orgnummer) {
         environmentInitializationService.opprett(miljoe, orgnummer.getListe());
         return ResponseEntity.ok().build();
     }
