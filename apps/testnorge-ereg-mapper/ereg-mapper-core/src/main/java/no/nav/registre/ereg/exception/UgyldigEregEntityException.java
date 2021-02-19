b2253ec34ee0b9a515b4ece0b3efd80208bf22c4
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-02-19 09:57:15 +0100

    fjern ereg-mapper

--- apps/testnorge-ereg-mapper/ereg-mapper-core/src/main/java/no/nav/registre/ereg/exception/UgyldigEregEntityException.java
+++
@@ -1,8 +0,0 @@
-package no.nav.registre.ereg.exception;
-
-public class UgyldigEregEntityException extends RuntimeException {
-
-    public UgyldigEregEntityException(String message) {
-        super(message);
-    }
-}
--- apps/testnorge-ereg-mapper/ereg-mapper-core/src/main/java/no/nav/registre/ereg/exception/UgyldigJenkinsCrumbException.java
+++
@@ -1,8 +0,0 @@
-package no.nav.registre.ereg.exception;
-
-public class UgyldigJenkinsCrumbException extends RuntimeException {
-
-    public UgyldigJenkinsCrumbException(String message) {
-        super(message);
-    }
-}
