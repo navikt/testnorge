ac58ed96cbe2edebbad64e41c2064145a112a670
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-02-19 08:40:39 +0100

    testing

---
+++ apps/testnorge-statisk-data-forvalter/src/main/java/no/nav/registre/sdforvalter/config/DevConfig.java
@@ -0,0 +1,14 @@
+package no.nav.registre.sdforvalter.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
+
+import no.nav.registre.testnorge.libs.localdevelopment.LocalDevelopmentConfig;
+
+@Configuration
+@Profile("dev")
+@Import(LocalDevelopmentConfig.class)
+public class DevConfig {
+
+}
