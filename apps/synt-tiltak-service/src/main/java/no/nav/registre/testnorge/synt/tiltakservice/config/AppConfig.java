0051c67d2e4ac66373d64582a3f09490f6f3a752
Hay <aileen.hay@nav.no>
2021-02-23 09:29:44 +0100

    Oppdatert sikkerhet og configs

--- apps/synt-tiltak-service/src/main/java/no/nav/registre/testnorge/synt/tiltakservice/config/AppConfig.java
+++ apps/synt-tiltak-service/src/main/java/no/nav/registre/testnorge/synt/tiltakservice/config/AppConfig.java
@@ -1,4 +1,17 @@
 package no.nav.registre.testnorge.synt.tiltakservice.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import no.nav.registre.testnorge.libs.core.config.AnalysisGCPAutoConfiguration;
+import no.nav.registre.testnorge.libs.core.config.ApplicationCoreConfig;
+import no.nav.registre.testnorge.libs.oauth2.config.SecureOAuth2ServerToServerConfiguration;
+
+@Configuration
+@Import({
+        ApplicationCoreConfig.class,
+        SecureOAuth2ServerToServerConfiguration.class,
+        AnalysisGCPAutoConfiguration.class
+})
 public class AppConfig {
 }
