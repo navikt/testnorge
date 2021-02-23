3d4702d5efbda37f96c9541a8f4708b1d212e397
Kristen Hærum <kristen.herum@nav.no>
2021-02-23 14:44:55 +0100

    Fjernet overflødige felter i API (#68)

--- dolly-backend-domain/src/main/java/no/nav/dolly/domain/resultset/udistub/model/opphold/RsUdiAvslagEllerBortfall.java
+++ dolly-backend-domain/src/main/java/no/nav/dolly/domain/resultset/udistub/model/opphold/RsUdiAvslagEllerBortfall.java
@@ -1,14 +1,13 @@
 package no.nav.dolly.domain.resultset.udistub.model.opphold;
 
-import java.time.LocalDateTime;
-
 import com.fasterxml.jackson.annotation.JsonInclude;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -20,8 +19,6 @@ public class RsUdiAvslagEllerBortfall {
 
     private LocalDateTime avgjorelsesDato;
 
-    private UdiGrunnlagOverig avslagGrunnlagOverig;
-    private UdiGrunnlagEos avslagGrunnlagTillatelseGrunnlagEOS;
     private UdiOppholdsrettType avslagOppholdsrettBehandlet;
     private UdiGrunnlagEos avslagOppholdstillatelseBehandletGrunnlagEOS;
     private UdiGrunnlagOverig avslagOppholdstillatelseBehandletGrunnlagOvrig;
--- dolly-backend-domain/src/main/java/no/nav/dolly/domain/resultset/udistub/model/opphold/UdiAvslagEllerBortfall.java
+++ dolly-backend-domain/src/main/java/no/nav/dolly/domain/resultset/udistub/model/opphold/UdiAvslagEllerBortfall.java
@@ -1,13 +1,13 @@
 package no.nav.dolly.domain.resultset.udistub.model.opphold;
 
-import java.time.LocalDate;
 import com.fasterxml.jackson.annotation.JsonInclude;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDate;
 
 @Getter
 @Setter
@@ -19,8 +19,6 @@ public class UdiAvslagEllerBortfall {
 
     private LocalDate avgjorelsesDato;
 
-    private UdiGrunnlagOverig avslagGrunnlagOverig;
-    private UdiGrunnlagEos avslagGrunnlagTillatelseGrunnlagEOS;
     private UdiOppholdsrettType avslagOppholdsrettBehandlet;
     private UdiGrunnlagEos avslagOppholdstillatelseBehandletGrunnlagEOS;
     private UdiGrunnlagOverig avslagOppholdstillatelseBehandletGrunnlagOvrig;
