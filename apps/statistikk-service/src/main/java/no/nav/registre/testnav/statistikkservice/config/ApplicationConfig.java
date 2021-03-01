89be397209ecdf9d8ef428db907fb9321d8c5c5f
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-03-01 14:09:07 +0100

    fix

--- apps/statistikk-service/src/main/java/no/nav/registre/testnav/statistikkservice/config/ApplicationConfig.java
+++ apps/statistikk-service/src/main/java/no/nav/registre/testnav/statistikkservice/config/ApplicationConfig.java
@@ -8,11 +8,13 @@ import no.nav.registre.testnorge.libs.core.config.AnalysisGCPAutoConfiguration;
 import no.nav.registre.testnorge.libs.core.config.ApplicationCoreConfig;
 import no.nav.registre.testnorge.libs.database.config.FlywayConfiguration;
 import no.nav.registre.testnorge.libs.database.config.VaultHikariConfiguration;
+import no.nav.registre.testnorge.libs.oauth2.config.SecureOAuth2ServerToServerConfiguration;
 
 @EnableJpaAuditing
 @Configuration
 @Import({
         ApplicationCoreConfig.class,
+        SecureOAuth2ServerToServerConfiguration.class,
         AnalysisGCPAutoConfiguration.class
 })
 public class ApplicationConfig {
