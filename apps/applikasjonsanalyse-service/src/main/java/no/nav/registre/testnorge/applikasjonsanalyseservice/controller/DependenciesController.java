7ad656897fd9b2715b67d16210e3e15cd917ba69
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-03-10 15:23:04 +0100

    fix

--- apps/applikasjonsanalyse-service/src/main/java/no/nav/registre/testnorge/applikasjonsanalyseservice/controller/DependenciesController.java
+++ apps/applikasjonsanalyse-service/src/main/java/no/nav/registre/testnorge/applikasjonsanalyseservice/controller/DependenciesController.java
@@ -1,12 +1,14 @@
 package no.nav.registre.testnorge.applikasjonsanalyseservice.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.CacheControl;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 import no.nav.registre.testnorge.applikasjonsanalyseservice.domain.Properties;
 import no.nav.registre.testnorge.applikasjonsanalyseservice.service.ApplikasjonsanalyseService;
@@ -20,9 +22,16 @@ public class DependenciesController {
 
     @GetMapping
     public ResponseEntity<Set<ApplicationDependenciesDTO>> getDependencies() {
+        var cacheControl
+                = CacheControl.maxAge(10, TimeUnit.MINUTES)
+                .noTransform()
+                .mustRevalidate();
 
         var applikasjonsanalyseList = service.getApplikasjonsanalyseList(new Properties("navikt", "testnorge"));
 
-        return ResponseEntity.ok(applikasjonsanalyseList.toDTO());
+        return ResponseEntity
+                .ok()
+                .cacheControl(cacheControl)
+                .body(applikasjonsanalyseList.toDTO());
     }
 }
