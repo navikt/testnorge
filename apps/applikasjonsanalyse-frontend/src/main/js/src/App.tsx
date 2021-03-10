1017d0483be873d7c1b2d63f3a475aa4d803d06f
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-03-10 13:30:58 +0100

    update frontend

---
+++ apps/applikasjonsanalyse-frontend/src/main/js/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import TestnorgeDependencyPage from '@/page/ApplikasjonsanalysePage';
+
+function App() {
+  return (
+    <div className="app">
+      <TestnorgeDependencyPage />
+    </div>
+  );
+}
+
+export default App;
