347b350e8f3112af900b4065963226aa272bd92b
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-03-11 14:44:44 +0100

    fix

--- libs/testnorge-oauth2/src/main/java/no/nav/registre/testnorge/libs/oauth2/domain/AzureClientCredentials.java
+++ libs/testnorge-oauth2/src/main/java/no/nav/registre/testnorge/libs/oauth2/domain/AzureClientCredentials.java
@@ -8,9 +8,6 @@ public class AzureClientCredentials {
     private final String clientId;
     private final String clientSecret;
 
-    /**
-     * @param clientId TODO nullable fra client id når alle apper er overført til AzureAd
-     */
     public AzureClientCredentials(
             @Value("${azure.app.client.id:#{null}}") String clientId,
             @Value("${azure.app.client.secret:#{null}}") String clientSecret
