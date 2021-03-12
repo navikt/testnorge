9afb31e2abf7127f1e8450fcd65ef20b113da230
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-03-12 10:08:28 +0100

    testing

--- libs/kafka-producers/src/main/java/no/nav/registre/testnorge/libs/kafkaproducers/organisasjon/v2/KafkaProducer.java
+++ libs/kafka-producers/src/main/java/no/nav/registre/testnorge/libs/kafkaproducers/organisasjon/v2/KafkaProducer.java
@@ -7,6 +7,7 @@ import org.apache.avro.specific.SpecificRecord;
 import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.config.SslConfigs;
+import org.apache.kafka.common.security.auth.SecurityProtocol;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -23,40 +24,31 @@ public abstract class KafkaProducer<T extends SpecificRecord> {
     @SneakyThrows
     public KafkaProducer(String groupId) {
         InetSocketAddress inetSocketAddress = new InetSocketAddress(0);
+        var keystorePassword = System.getenv("KAFKA_CREDSTORE_PASSWORD");
         Map<String, Object> props = new HashMap<>();
+
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, System.getenv("KAFKA_BROKERS"));
         props.put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, System.getenv("KAFKA_KEYSTORE_PATH"));
-        props.put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, System.getenv("KAFKA_CREDSTORE_PASSWORD"));
+        props.put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, keystorePassword);
         props.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, System.getenv("KAFKA_TRUSTSTORE_PATH"));
-        props.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, System.getenv("KAFKA_CREDSTORE_PASSWORD"));
-        props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SSL");
+        props.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, keystorePassword);
+        props.put(SslConfigs.SSL_KEY_PASSWORD_CONFIG, keystorePassword);
+        props.put(SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG, "jks");
+        props.put(SslConfigs.SSL_KEYSTORE_TYPE_CONFIG, "PKCS12");
+        props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, SecurityProtocol.SSL.name);
 
         props.put(ProducerConfig.CLIENT_ID_CONFIG, groupId + inetSocketAddress.getHostString());
         props.put(ProducerConfig.ACKS_CONFIG, "1");
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
 
-        props.put("basic.auth.credentials.source", "USER_INFO");
+        props.put(AbstractKafkaSchemaSerDeConfig.BASIC_AUTH_CREDENTIALS_SOURCE, "USER_INFO");
 
-        var schemaRegistry = System.getenv("KAFKA_SCHEMA_REGISTRY") != null ? new URL(System.getenv("KAFKA_SCHEMA_REGISTRY")) : null;
-        var schemausr = System.getenv("KAFKA_SCHEMA_REGISTRY_USER");
-        var schemapass = System.getenv("KAFKA_SCHEMA_REGISTRY_PASSWORD");
-        var schemaregusrinfo = schemausr + ":" + schemapass;
+        var username = System.getenv("KAFKA_SCHEMA_REGISTRY_USER");
+        var password = System.getenv("KAFKA_SCHEMA_REGISTRY_PASSWORD");
 
-        props.put("basic.auth.user.info", schemaRegistry != null && schemaRegistry.getUserInfo() != null ? schemaRegistry.getUserInfo() : schemaregusrinfo);
-
-        // TODO: Only set inn production
-        if(schemaRegistry != null) {
-            props.put(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, new URI(
-                    schemaRegistry.getProtocol(),
-                    null,
-                    schemaRegistry.getHost(),
-                    schemaRegistry.getPort(),
-                    schemaRegistry.getPath(),
-                    schemaRegistry.getQuery(),
-                    schemaRegistry.getRef()
-            ).toString());
-        }
+        props.put(AbstractKafkaSchemaSerDeConfig.USER_INFO_CONFIG, username + ":" + password);
+        props.put(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, System.getenv("KAFKA_SCHEMA_REGISTRY"));
 
         this.kafkaTemplate = new KafkaTemplate<>(new DefaultKafkaProducerFactory<>(props));
     }
