1259a7ef1d743f02cd0d8c45e04f7fe74933b614
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-03-15 14:09:44 +0100

    test

--- bridge/kafka-onprem-bridge/src/main/java/no/nav/registre/testnorge/bridge/kafkaonprembridge/config/ApplicationConfig.java
+++ bridge/kafka-onprem-bridge/src/main/java/no/nav/registre/testnorge/bridge/kafkaonprembridge/config/ApplicationConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
 import no.nav.registre.testnorge.libs.core.config.ApplicationCoreConfig;
+import no.nav.registre.testnorge.libs.kafkaconfig.config.KafkaProperties;
 import no.nav.registre.testnorge.libs.kafkaproducers.organisasjon.v2.EndringsdokumentV2Producer;
 import no.nav.registre.testnorge.libs.kafkaproducers.organisasjon.v2.OpprettelsesdokumentV2Producer;
 import no.nav.registre.testnorge.libs.oauth2.config.SecureOAuth2ServerToServerConfiguration;
@@ -15,7 +16,8 @@ import no.nav.registre.testnorge.libs.oauth2.config.SecureOAuth2ServerToServerCo
         ApplicationCoreConfig.class,
         SecureOAuth2ServerToServerConfiguration.class,
         OpprettelsesdokumentV2Producer.class,
-        EndringsdokumentV2Producer.class
+        EndringsdokumentV2Producer.class,
+        KafkaProperties.class
 })
 @Profile({"dev", "prod"})
 @RequiredArgsConstructor
