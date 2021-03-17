5699a0e8f597d8e813de47272ec146dcb644fe60
Aileen Hay <58416744+H153910@users.noreply.github.com>
2021-03-17 09:24:50 +0100

    Syntrest/legge til amelding inntekt (#522)
    Lagt til inntekt for amelding

---
+++ apps/syntrest/src/main/java/no/nav/registre/syntrest/domain/amelding/Spesifikasjon.java
@@ -0,0 +1,24 @@
+package no.nav.registre.syntrest.domain.amelding;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Spesifikasjon {
+
+    @JsonAlias({ "OPPTJENINGSLAND", "opptjeningsland" })
+    private String opptjeningsland;
+}
