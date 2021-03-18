e31f4fe348e0ee970c4d375ff7fc981ee8eed0b0
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-03-18 09:24:45 +0100

    testing

--- libs/kafka-producers/src/main/java/no/nav/registre/testnorge/libs/kafkaproducers/organisasjon/v2/EndringsdokumentV2Producer.java
+++ libs/kafka-producers/src/main/java/no/nav/registre/testnorge/libs/kafkaproducers/organisasjon/v2/EndringsdokumentV2Producer.java
@@ -7,24 +7,23 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.concurrent.ListenableFuture;
 
 import no.nav.registre.testnorge.libs.avro.organisasjon.v1.Endringsdokument;
-import no.nav.registre.testnorge.libs.avro.organisasjon.v1.Opprettelsesdokument;
 import no.nav.registre.testnorge.libs.kafkaconfig.topic.v2.OrganisasjonTopic;
 
 @Slf4j
 @Component
 public class EndringsdokumentV2Producer extends RecreateKafkaProducer<Endringsdokument> {
-    private final String groupid;
+    private final String groupId;
 
-    EndringsdokumentV2Producer(@Value("${kafka.groupid}") String groupid) {
-        this.groupid = groupid;
+    EndringsdokumentV2Producer(@Value("${kafka.groupid}") String groupId) {
+        this.groupId = groupId;
     }
 
     @Override
     KafkaProducer<Endringsdokument> create() {
-        return new KafkaProducer<>(groupid) {
+        return new KafkaProducer<>(groupId) {
             @Override
             ListenableFuture<SendResult<String, Endringsdokument>> send(String key, Endringsdokument value) {
-                log.info("Sender endringsdokument {} for organisasjon {}.", key, value.getOrganisasjon().getOrgnummer());
+                log.info("Sender endringsdokument med uuid {} for organisasjon {}.", key, value.getOrganisasjon().getOrgnummer());
                 return getKafkaTemplate().send(OrganisasjonTopic.ORGANISASJON_ENDRE_ORGANISASJON, key, value);
             }
         };
--- libs/kafka-producers/src/main/java/no/nav/registre/testnorge/libs/kafkaproducers/organisasjon/v2/OpprettelsesdokumentV2Producer.java
+++ libs/kafka-producers/src/main/java/no/nav/registre/testnorge/libs/kafkaproducers/organisasjon/v2/OpprettelsesdokumentV2Producer.java
@@ -12,17 +12,18 @@ import no.nav.registre.testnorge.libs.kafkaconfig.topic.v2.OrganisasjonTopic;
 @Slf4j
 @Component
 public class OpprettelsesdokumentV2Producer extends RecreateKafkaProducer<Opprettelsesdokument> {
-    private final String groupid;
+    private final String groupId;
 
     public OpprettelsesdokumentV2Producer(@Value("${kafka.groupid}") String groupid) {
-        this.groupid = groupid;
+        this.groupId = groupid;
     }
 
     @Override
     KafkaProducer<Opprettelsesdokument> create() {
-        return new KafkaProducer<>(groupid) {
+        return new KafkaProducer<>(groupId) {
             @Override
             ListenableFuture<SendResult<String, Opprettelsesdokument>> send(String key, Opprettelsesdokument value) {
+                log.info("Sender opprettelsesdokument med uuid {} for organisasjon {}.", key, value.getOrganisasjon().getOrgnummer());
                 return getKafkaTemplate().send(OrganisasjonTopic.ORGANISASJON_OPPRETT_ORGANISASJON, key, value);
             }
         };
