1ba665b51424dd1b7a1fec6623ace0db36fed48d
stigus <stian.gustavsson@nav.no>
2021-03-18 10:06:01 +0100

    Test exceptionhandler

--- apps/synt-sykemelding-api/src/main/java/no/nav/registre/testnorge/synt/sykemelding/consumer/command/GetArbeidsforholdCommand.java
+++ apps/synt-sykemelding-api/src/main/java/no/nav/registre/testnorge/synt/sykemelding/consumer/command/GetArbeidsforholdCommand.java
@@ -2,12 +2,17 @@ package no.nav.registre.testnorge.synt.sykemelding.consumer.command;
 
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import no.nav.registre.testnorge.libs.dto.oppsummeringsdokumentservice.v1.ArbeidsforholdDTO;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 import java.util.concurrent.Callable;
 
+@Slf4j
 @RequiredArgsConstructor
 public class GetArbeidsforholdCommand implements Callable<ArbeidsforholdDTO> {
     private final WebClient webClient;
@@ -27,5 +32,11 @@ public class GetArbeidsforholdCommand implements Callable<ArbeidsforholdDTO> {
                 .retrieve()
                 .bodyToMono(ArbeidsforholdDTO.class)
                 .block();
+    }
+
+    @ExceptionHandler(WebClientResponseException.class)
+    public ResponseEntity<String> handleWebClientResponseException(WebClientResponseException ex) {
+        log.error("Error from WebClient - Status {}, Body {}", ex.getRawStatusCode(), ex.getResponseBodyAsString(), ex);
+        return ResponseEntity.status(ex.getRawStatusCode()).body(ex.getResponseBodyAsString());
     }
 }
