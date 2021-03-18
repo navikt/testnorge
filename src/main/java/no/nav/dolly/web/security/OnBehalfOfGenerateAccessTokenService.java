dcebc435c12642f39fef4e430b18c7f24a1beba8
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-03-18 15:47:56 +0100

    Logging of deploy (#140)

--- src/main/java/no/nav/dolly/web/security/OnBehalfOfGenerateAccessTokenService.java
+++ src/main/java/no/nav/dolly/web/security/OnBehalfOfGenerateAccessTokenService.java
@@ -1,10 +1,6 @@
 package no.nav.dolly.web.security;
 
 import lombok.extern.slf4j.Slf4j;
-import no.nav.dolly.web.security.domain.AccessScopes;
-import no.nav.dolly.web.security.domain.AccessToken;
-import no.nav.dolly.web.security.domain.ClientCredential;
-import no.nav.dolly.web.security.domain.DollyFrontendClientCredential;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -13,10 +9,16 @@ import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.netty.http.client.HttpClient;
 import reactor.netty.tcp.ProxyProvider;
 
 import java.net.URI;
+
+import no.nav.dolly.web.security.domain.AccessScopes;
+import no.nav.dolly.web.security.domain.AccessToken;
+import no.nav.dolly.web.security.domain.ClientCredential;
+import no.nav.dolly.web.security.domain.DollyFrontendClientCredential;
 
 
 @Slf4j
@@ -80,12 +82,21 @@ class OnBehalfOfGenerateAccessTokenService {
 
             log.info("Access token opprettet for OAuth 2.0 On-Behalf-Of Flow");
             return token;
+
+        } catch (WebClientResponseException e) {
+            var secret = clientCredential.getClientSecret().substring(0, 3) + "******************";
+            log.error(
+                    "Feil ved henting av access token for {} med client secret: {}.\nError: \n{}.",
+                    String.join(" ", accessScopes.getScopes()),
+                    secret,
+                    e.getResponseBodyAsString()
+            );
+            throw e;
         } catch (Exception e) {
             log.error(
-                    String.format(
-                            "Feil ved henting av access token for %s. %nFeilmelding: %s.",
-                            String.join(" ", accessScopes.getScopes()),
-                            e.getMessage()),
+                    "Feil ved henting av access token for {}.\n Error: \n{}.",
+                    String.join(" ", accessScopes.getScopes()),
+                    e.getMessage(),
                     e);
             throw e;
         }
