a0cdb945a42a68f6f90f02abec8daca7ac0b35e6
Kristen Hærum <kristen.herum@nav.no>
2021-03-19 18:20:30 +0100

    Feature/tps identer vedlikehold (#521)
    Opprettet app for sletting og vedlikehold av identer

---
+++ apps/tps-identer-vedlikehold/src/main/java/no/nav/tpsidenter/vedlikehold/config/ApplicationConfig.java
@@ -0,0 +1,26 @@
+package no.nav.tpsidenter.vedlikehold.config;
+
+import no.nav.registre.testnorge.libs.core.config.ApplicationCoreConfig;
+import no.nav.registre.testnorge.libs.core.config.ApplicationProperties;
+import no.nav.registre.testnorge.libs.slack.consumer.SlackConsumer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@Import({ApplicationCoreConfig.class,})
+@EnableScheduling
+public class ApplicationConfig {
+
+    @Bean
+    public SlackConsumer slackConsumer(
+            @Value("${consumers.slack.token}") String token,
+            @Value("${consumers.slack.baseUrl}") String baseUrl,
+            @Value("${http.proxy:#{null}}") String proxyHost,
+            ApplicationProperties properties
+    ) {
+        return new SlackConsumer(token, baseUrl, proxyHost, properties.getName());
+    }
+}
