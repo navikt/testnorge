a702402b6d96798ab285ef23343626ad085f02f6
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-03-22 14:03:20 +0100

    sykemlding

---
+++ apps/synt-sykemelding-api/src/main/java/no/nav/registre/testnorge/synt/sykemelding/config/credentials/HelsepersonellServiceProperties.java
@@ -0,0 +1,12 @@
+package no.nav.registre.testnorge.synt.sykemelding.config.credentials;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import no.nav.registre.testnorge.libs.oauth2.config.NaisServerProperties;
+
+
+@Configuration
+@ConfigurationProperties(prefix = "consumers.testnav-helsepersonell-service")
+public class HelsepersonellServiceProperties extends NaisServerProperties {
+}
