3782814294b1b023e5dce2f6146d71e21a597842
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-03-23 15:29:39 +0100

    fix

---
+++ apps/synt-sykemelding-api/src/main/java/no/nav/registre/testnorge/synt/sykemelding/config/credentials/OrganisasjonServiceProperties.java
@@ -0,0 +1,15 @@
+package no.nav.registre.testnorge.synt.sykemelding.config.credentials;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import no.nav.registre.testnorge.libs.oauth2.config.NaisServerProperties;
+
+@Configuration
+@ConfigurationProperties(prefix = "consumers.testnav-organisasjon-service")
+public class OrganisasjonServiceProperties extends NaisServerProperties {
+
+}
