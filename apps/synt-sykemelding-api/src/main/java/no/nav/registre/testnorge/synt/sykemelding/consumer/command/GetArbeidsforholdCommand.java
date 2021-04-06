e10c184f39d0b4a0b25e1e7fea6e6adb5fdd9151
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-04-06 10:29:19 +0200

    fix

--- apps/synt-sykemelding-api/src/main/java/no/nav/registre/testnorge/synt/sykemelding/consumer/command/GetArbeidsforholdCommand.java
+++ apps/synt-sykemelding-api/src/main/java/no/nav/registre/testnorge/synt/sykemelding/consumer/command/GetArbeidsforholdCommand.java
@@ -3,7 +3,6 @@ package no.nav.registre.testnorge.synt.sykemelding.consumer.command;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import no.nav.registre.testnorge.libs.dto.oppsummeringsdokumentservice.v1.ArbeidsforholdDTO;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.client.HttpClientErrorException;
@@ -11,6 +10,8 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 import java.util.concurrent.Callable;
+
+import no.nav.registre.testnorge.libs.dto.oppsummeringsdokumentservice.v1.ArbeidsforholdDTO;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -27,8 +28,12 @@ public class GetArbeidsforholdCommand implements Callable<ArbeidsforholdDTO> {
 
         return webClient
                 .get()
-                .uri("/api/v1/arbeidsforhold/" + ident + "/" + orgnummer + "/" + arbeidsforholdId)
+                .uri(builder -> builder
+                        .path("/api/v1/arbeidsforhold/{ident}/{orgnummer}/{arbeidsforholdId}")
+                        .build(ident, orgnummer, arbeidsforholdId)
+                )
                 .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
+                .header("miljo", "q1")
                 .retrieve()
                 .onStatus(HttpStatus::is4xxClientError, response -> Mono.error(
                         new HttpClientErrorException(HttpStatus.NOT_FOUND, "Fant ikke arbeidsforhold")))
