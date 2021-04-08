6a51b0f3a1ec6545751df84be7c5af2caa3fafd4
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-04-08 15:49:00 +0200

    fix

--- apps/testnorge-statisk-data-forvalter/src/main/java/no/nav/registre/sdforvalter/consumer/rs/domain/OrgTreeList.java
+++ apps/testnorge-statisk-data-forvalter/src/main/java/no/nav/registre/sdforvalter/consumer/rs/domain/OrgTreeList.java
@@ -1,6 +1,7 @@
 package no.nav.registre.sdforvalter.consumer.rs.domain;
 
 import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,6 +9,7 @@ import java.util.List;
 import no.nav.registre.sdforvalter.domain.Ereg;
 import no.nav.registre.sdforvalter.domain.EregListe;
 
+@Slf4j
 @Value
 public class OrgTreeList {
     List<OrgTree> list;
@@ -19,6 +21,8 @@ public class OrgTreeList {
                 list.add(OrgTree.from(ereg, eregListe.getListe()));
             }
         }
+        var sum = list.stream().map(OrgTree::size).reduce(0, Integer::sum);
+        log.info("{}/{} organisasjoner er sattopp i organisasjon traer", sum, eregListe.size());
     }
 
     public boolean contains(Ereg ereg) {
