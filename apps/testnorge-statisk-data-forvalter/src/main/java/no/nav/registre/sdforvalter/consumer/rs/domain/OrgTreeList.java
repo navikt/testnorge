bea5ed1ebf47d3373fff8925bc46ffe5acb58d76
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-04-08 15:56:06 +0200

    save

--- apps/testnorge-statisk-data-forvalter/src/main/java/no/nav/registre/sdforvalter/consumer/rs/domain/OrgTreeList.java
+++ apps/testnorge-statisk-data-forvalter/src/main/java/no/nav/registre/sdforvalter/consumer/rs/domain/OrgTreeList.java
@@ -5,6 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import no.nav.registre.sdforvalter.domain.Ereg;
 import no.nav.registre.sdforvalter.domain.EregListe;
@@ -22,10 +24,26 @@ public class OrgTreeList {
             }
         }
         var sum = list.stream().map(OrgTree::size).reduce(0, Integer::sum);
-        log.info("{}/{} organisasjoner er sattopp i organisasjon traer", sum, eregListe.size());
+        if (eregListe.size() != sum) {
+            log.warn("Bare {}/{} organisasjoner er sattopp i organisasjon traer.", sum, eregListe.size());
+            log.warn("Organisasjonene finnes ikke i organisasjon treet {}.", notInTree(eregListe));
+        }
     }
 
     public boolean contains(Ereg ereg) {
         return list.stream().anyMatch(value -> value.contains(ereg));
     }
+
+    private Set<String> notInTree(EregListe eregListe) {
+        return eregListe
+                .stream()
+                .filter(value -> !isInnOrgTree(value))
+                .map(Ereg::getOrgnr)
+                .collect(Collectors.toSet());
+    }
+
+    private boolean isInnOrgTree(Ereg ereg) {
+        return list.stream().anyMatch(value -> value.contains(ereg));
+    }
+
 }
