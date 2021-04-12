a7b73dd9e8bb94ac0da682f264e8ff7b064f59c8
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-04-12 10:27:05 +0200

    Legg til stotte for frilanser i testnorge-aareg (#552)

--- apps/testnorge-aareg/aareg-core/src/main/java/no/nav/registre/aareg/consumer/rs/AaregRestConsumer.java
+++ apps/testnorge-aareg/aareg-core/src/main/java/no/nav/registre/aareg/consumer/rs/AaregRestConsumer.java
@@ -1,9 +1,9 @@
 package no.nav.registre.aareg.consumer.rs;
 
-import static no.nav.registre.aareg.util.ArbeidsforholdMappingUtil.mapToArbeidsforhold;
 import static no.nav.registre.aareg.domain.CommonKeys.HEADER_NAV_CONSUMER_TOKEN;
 import static no.nav.registre.aareg.domain.CommonKeys.HEADER_NAV_PERSON_IDENT;
 import static no.nav.registre.aareg.domain.CommonKeys.RESPONSE_TYPE_LIST_ARBEIDSFORHOLD;
+import static no.nav.registre.aareg.util.ArbeidsforholdMappingUtil.mapToArbeidsforhold;
 import static org.springframework.http.HttpHeaders.ACCEPT;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
@@ -11,8 +11,6 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import com.fasterxml.jackson.databind.JsonNode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
-import no.nav.registre.testnorge.domain.dto.aordningen.arbeidsforhold.Arbeidsforhold;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -25,6 +23,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import no.nav.registre.aareg.security.sts.StsOidcService;
+import no.nav.registre.testnorge.domain.dto.aordningen.arbeidsforhold.Arbeidsforhold;
 
 @Slf4j
 @Component
@@ -35,12 +34,13 @@ public class AaregRestConsumer {
     private final RestTemplate restTemplate;
     private final StsOidcService stsOidcService;
 
-    public ResponseEntity<List<Arbeidsforhold>> hentArbeidsforhold(
-            String ident,
-            String miljoe
-    ) {
+    public ResponseEntity<List<Arbeidsforhold>> hentArbeidsforhold(String ident, String miljoe) {
+
+        log.info("Henter arbeidsforhold for ident {} i miljø {}...", ident, miljoe);
+        var url = URI.create(aaregArbeidsforholdFasitConsumer.getUrlForEnv(miljoe) +
+                "?arbeidsforholdtype=ordinaertArbeidsforhold,maritimtArbeidsforhold,forenkletOppgjoersordning,frilanserOppdragstakerHonorarPersonerMm");
         var getRequest = RequestEntity
-                .get(URI.create(aaregArbeidsforholdFasitConsumer.getUrlForEnv(miljoe)))
+                .get(url)
                 .header(ACCEPT, APPLICATION_JSON_VALUE)
                 .header(AUTHORIZATION, stsOidcService.getIdToken(miljoe))
                 .header(HEADER_NAV_CONSUMER_TOKEN, stsOidcService.getIdToken(miljoe))
@@ -56,11 +56,13 @@ public class AaregRestConsumer {
                 }
             }
         } catch (ResourceAccessException e) {
-            log.warn("Kunne ikke hente ident fra miljø", e);
+            log.warn("Kunne ikke hente ident {} fra miljø {}.", ident, miljoe, e);
         } catch (HttpStatusCodeException e) {
-            log.info("Kunne ikke hente ident fra aareg", e);
+            log.info("Kunne ikke hente ident {} fra aareg i {}.", ident, miljoe, e);
             return ResponseEntity.status(e.getStatusCode()).build();
         }
+
+        log.info("Fant {} arbeidsforhold for ident {} i miljø {}.", arbeidsforhold.size(), ident, miljoe);
         return ResponseEntity.ok().body(arbeidsforhold);
     }
 }
