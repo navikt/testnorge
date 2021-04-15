0773c5e22af8451d19e76911e3212289a38a0e0b
stigus <stian.gustavsson@nav.no>
2021-04-15 14:17:14 +0200

    Teste deploy

--- apps/miljoer-service/src/main/java/no/nav/registre/testnorge/miljoerservice/consumer/TpsfConsumer.java
+++ apps/miljoer-service/src/main/java/no/nav/registre/testnorge/miljoerservice/consumer/TpsfConsumer.java
@@ -1,13 +1,19 @@
 package no.nav.registre.testnorge.miljoerservice.consumer;
 
 import lombok.extern.slf4j.Slf4j;
+import no.nav.registre.testnorge.libs.oauth2.config.NaisServerProperties;
+import no.nav.registre.testnorge.libs.oauth2.service.AccessTokenService;
+import no.nav.registre.testnorge.miljoerservice.config.credentias.TpsForvalterenProxyServiceProperties;
 import no.nav.registre.testnorge.miljoerservice.response.MiljoerResponse;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.util.retry.Retry;
+
+import java.time.Duration;
 
 import static java.util.Objects.isNull;
 
@@ -16,18 +22,32 @@ import static java.util.Objects.isNull;
 public class TpsfConsumer {
 
     private final WebClient webClient;
+    private final NaisServerProperties serverProperties;
+    private final AccessTokenService accessTokenService;
 
-    public TpsfConsumer(@Value("${tps-forvalteren.rest-api.url}") String tpsfServerUrl) {
-        this.webClient = WebClient.builder().baseUrl(tpsfServerUrl).build();
+    public TpsfConsumer(
+            TpsForvalterenProxyServiceProperties serverProperties,
+            AccessTokenService accessTokenService
+    ) {
+        this.serverProperties = serverProperties;
+        this.accessTokenService = accessTokenService;
+
+        this.webClient = WebClient
+                .builder()
+                .baseUrl(serverProperties.getUrl())
+                .build();
     }
 
     public MiljoerResponse getAktiveMiljoer() {
         log.info("Henter aktive miljøer fra TPSF.");
+        var accessToken = accessTokenService.generateToken(serverProperties);
         ResponseEntity<MiljoerResponse> response = webClient
                 .get()
                 .uri(uriBuilder -> uriBuilder.path("/v1/environments").build())
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken.getTokenValue())
                 .retrieve()
                 .toEntity(MiljoerResponse.class)
+                .retryWhen(Retry.fixedDelay(3, Duration.ofSeconds(3)))
                 .block();
 
         if (response == null || isNull(response.getBody()) || isNull(response.getBody().getEnvironments())) {
