a97f4a8f811c934312ce5090d4fc2b58d145c7d8
Hay <aileen.hay@nav.no>
2021-04-15 12:55:41 +0200

    Oppdatert null håndtering

--- apps/syntrest/src/main/java/no/nav/registre/syntrest/controllers/SyntController.java
+++ apps/syntrest/src/main/java/no/nav/registre/syntrest/controllers/SyntController.java
@@ -148,7 +148,7 @@ public class SyntController {
         InputValidator.validateInput(numToGenerate);
         InputValidator.validateInput(InputValidator.INPUT_STRING_TYPE.MELDEGRUPPE, meldegruppe);
 
-        var queryString = createQueryString("arbeidstimer", isNull(arbeidstimer) ? null : arbeidstimer.toString(), "");
+        var queryString = isNull(arbeidstimer) ? "" : createQueryString("arbeidstimer", arbeidstimer.toString(), "");
         var expandedPath = urlUtils.expandPath(meldekortConsumer.getUrl(), String.valueOf(numToGenerate), meldegruppe);
         var response = meldekortConsumer.synthesizeData(expandedPath, queryString);
         doResponseValidation(response);
