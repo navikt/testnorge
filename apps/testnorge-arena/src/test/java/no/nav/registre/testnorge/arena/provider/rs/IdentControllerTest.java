b1e5ab146a8fe63b4501188feaf82c29a341cba5
Hay <Aileen.Hay@nav.no>
2021-04-16 08:04:02 +0200

    Oppdatert imports

--- apps/testnorge-arena/src/test/java/no/nav/registre/testnorge/arena/provider/rs/IdentControllerTest.java
+++ apps/testnorge-arena/src/test/java/no/nav/registre/testnorge/arena/provider/rs/IdentControllerTest.java
@@ -16,6 +16,8 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import no.nav.registre.testnorge.arena.service.IdentService;
+
 @RunWith(MockitoJUnitRunner.class)
 public class IdentControllerTest {
 
