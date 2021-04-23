5141441fe64e99632ac60d748a71e8200f436f5a
kristenhaerum <kristen.herum@nav.no>
2021-04-23 08:46:57 +0200

    Oppdatert config II ...

--- apps/organisasjon-forvalter/src/main/java/no/nav/organisasjonforvalter/config/ApplicationConfig.java
+++ apps/organisasjon-forvalter/src/main/java/no/nav/organisasjonforvalter/config/ApplicationConfig.java
@@ -2,12 +2,11 @@ package no.nav.organisasjonforvalter.config;
 
 import no.nav.registre.testnorge.libs.core.config.ApplicationCoreConfig;
 import no.nav.registre.testnorge.libs.oauth2.config.SecureOAuth2ServerToServerConfiguration;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
-@Import({
-        ApplicationCoreConfig.class,
-        SecureOAuth2ServerToServerConfiguration.class
-})
+@Import({ApplicationCoreConfig.class,
+        SecureOAuth2ServerToServerConfiguration.class})
+@Configuration
 public class ApplicationConfig {
-
 }
