feeccb3ef44d1e74f0c4b479ac8a139ed4747841
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-04-26 13:17:51 +0200

    test

--- frontend-libs/dolly-komponenter/src/index.ts
+++ frontend-libs/dolly-komponenter/src/index.ts
@@ -4,5 +4,5 @@ export * from '@/components/date-picker';
 export * from '@/components/form';
 export * from '@/components/header';
 export * from '@/components/page';
-export * from '@/components/profil';
+export { Profil } from '@/components/profil';
 export * from '@/components/select';
