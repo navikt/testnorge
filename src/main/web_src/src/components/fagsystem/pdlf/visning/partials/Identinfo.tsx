5118c353ceabd6398ae9c8587fe07038d5bc5477
stigus <stian.gustavsson@nav.no>
2021-04-28 11:57:32 +0200

    Typescript endringer, endringer på visning av PDL data på ident

--- src/main/web_src/src/components/fagsystem/pdlf/visning/partials/Identinfo.tsx
+++ src/main/web_src/src/components/fagsystem/pdlf/visning/partials/Identinfo.tsx
@@ -4,19 +4,35 @@ import { TitleValue } from '~/components/ui/titleValue/TitleValue'
 import { ErrorBoundary } from '~/components/ui/appError/ErrorBoundary'
 import Formatters from '~/utils/DataFormatter'
 
-export const IdentInfo = ({ data, visTittel = true }) => {
-	if (!data) {
+type Data = {
+	pdlResponse: {
+		identer: [Ident]
+	}
+}
+
+type Ident = {
+	gruppe: string
+	ident: string
+	historisk: boolean
+}
+
+export const IdentInfo = ({ pdlResponse }: Data) => {
+	if (!pdlResponse) {
 		return null
 	}
+
 	return (
 		<ErrorBoundary>
 			<div>
-				{visTittel && <SubOverskrift label="Ident info" iconKind="personinformasjon" />}
-				{data.identer.map((ident, index) => (
+				<SubOverskrift label="Ident informasjon" iconKind="personinformasjon" />
+				{pdlResponse.identer.map((ident: Ident, index: number) => (
 					<div key={index}>
 						<h4 style={{ marginTop: '0px' }}>{ident.gruppe}</h4>
 						<div className="person-visning_content">
-							<TitleValue title="Ident" value={ident.ident} />
+							<TitleValue
+								title={ident.gruppe.includes('AKTORID') ? 'ID' : 'ident'}
+								value={ident.ident}
+							/>
 							<TitleValue title="Historisk" value={Formatters.oversettBoolean(ident.historisk)} />
 						</div>
 					</div>
