98fc3459218b1003f4cfce42de4b9e2e20c60d36
kristenhaerum <kristen.herum@nav.no>
2021-04-29 09:31:04 +0200

    Redeploye org-bestilling

--- apps/organisasjon-bestilling-service/src/main/java/no/nav/registre/testnorge/organisasjonbestillingservice/OrganisasjonBestillingServiceApplicationStarter.java
+++ apps/organisasjon-bestilling-service/src/main/java/no/nav/registre/testnorge/organisasjonbestillingservice/OrganisasjonBestillingServiceApplicationStarter.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OrganisasjonBestillingServiceApplicationStarter {
+
     public static void main(String[] args) {
         SpringApplication.run(OrganisasjonBestillingServiceApplicationStarter.class, args);
     }
