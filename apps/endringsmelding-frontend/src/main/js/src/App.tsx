3c9e4007fa08c7628155b78b2e4296c04224b03b
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-05-05 14:51:40 +0200

    fix

--- apps/endringsmelding-frontend/src/main/js/src/App.tsx
+++ apps/endringsmelding-frontend/src/main/js/src/App.tsx
@@ -1,28 +1,25 @@
 import React from 'react';
 
-import { Header } from '@navikt/dolly-komponenter';
-import { ProfilLoader } from '@/components/profil';
+import { Header, ProfilLoader } from '@navikt/dolly-komponenter';
 import { EndringsmeldingPage } from '@/pages';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import LoginPage from '@/pages/login-page/LoginPage';
+import ProfilService from './service/ProfilService';
 
-function App() {
-  return (
+const App = () => (
+  <div>
+    <Header title="Endringsmeldinger" profile={<ProfilLoader {...ProfilService} />} />
     <Router>
       <Switch>
         <Route path="/login">
-          <Header />
           <LoginPage />
         </Route>
         <Route path="/">
-          <Header>
-            <ProfilLoader />
-          </Header>
           <EndringsmeldingPage />
         </Route>
       </Switch>
     </Router>
-  );
-}
+  </div>
+);
 
 export default App;
