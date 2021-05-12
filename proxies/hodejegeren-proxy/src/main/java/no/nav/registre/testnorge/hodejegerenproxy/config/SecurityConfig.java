7932cb7f2e1b568a7242862ce9ae3087c502d2b7
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-05-12 15:35:10 +0200

    Use String Gateway

--- proxies/hodejegeren-proxy/src/main/java/no/nav/registre/testnorge/hodejegerenproxy/config/SecurityConfig.java
+++ proxies/hodejegeren-proxy/src/main/java/no/nav/registre/testnorge/hodejegerenproxy/config/SecurityConfig.java
@@ -2,45 +2,76 @@ package no.nav.registre.testnorge.hodejegerenproxy.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2Error;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidatorResult;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtValidators;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoders;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 import java.util.List;
 
-import no.nav.registre.testnorge.libs.oauth2.config.OAuth2ResourceServerConfiguration;
 
-
-@EnableWebSecurity
 @Configuration
-@Profile({"dev", "prod"})
-public class SecurityConfig extends OAuth2ResourceServerConfiguration {
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    private final OAuth2ResourceServerProperties oAuth2ResourceServerProperties;
+    private final List<String> acceptedAudience;
 
     public SecurityConfig(
             OAuth2ResourceServerProperties oAuth2ResourceServerProperties,
             @Value("${spring.security.oauth2.resourceserver.jwt.accepted-audience}") List<String> acceptedAudience
     ) {
-        super(oAuth2ResourceServerProperties, acceptedAudience);
+        this.oAuth2ResourceServerProperties = oAuth2ResourceServerProperties;
+        this.acceptedAudience = acceptedAudience;
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .headers().frameOptions().disable()
-                .and()
-                .csrf()
-                .disable()
-                .authorizeRequests()
-                .antMatchers("/internal/isReady", "/internal/isAlive").permitAll()
-                .anyRequest()
-                .fullyAuthenticated()
+
+    public class AudienceValidator implements OAuth2TokenValidator<Jwt> {
+        public OAuth2TokenValidatorResult validate(Jwt jwt) {
+            var error = new OAuth2Error("invalid_token", String.format("None of required audience values '%s' found in token", acceptedAudience), null);
+            return jwt.getAudience().stream().anyMatch(acceptedAudience::contains)
+                    ? OAuth2TokenValidatorResult.success()
+                    : OAuth2TokenValidatorResult.failure(error);
+        }
+    }
+
+
+    private ReactiveJwtDecoder jwtDecoder() {
+        NimbusReactiveJwtDecoder jwtDecoder = (NimbusReactiveJwtDecoder)
+                ReactiveJwtDecoders.fromOidcIssuerLocation(oAuth2ResourceServerProperties.getJwt().getIssuerUri());
+
+        OAuth2TokenValidator<Jwt> audienceValidator = new AudienceValidator();
+        OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(oAuth2ResourceServerProperties.getJwt().getIssuerUri());
+        OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator<>(withIssuer, audienceValidator);
+
+        jwtDecoder.setJwtValidator(withAudience);
+
+        return jwtDecoder;
+    }
+
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http.csrf().disable()
+                .authorizeExchange()
+                .pathMatchers("/internal/isReady", "/internal/isAlive").permitAll()
+                .anyExchange().permitAll()
                 .and()
                 .oauth2ResourceServer()
                 .jwt()
-                .decoder(jwtDecoder());
+                .jwtDecoder(jwtDecoder());
+        return http.build();
     }
+
 }
+
---
+++ proxies/hodejegeren-proxy/src/main/java/no/nav/registre/testnorge/hodejegerenproxy/router/InternalHandler.java
@@ -0,0 +1,18 @@
+package no.nav.registre.testnorge.hodejegerenproxy.router;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+@Component
+public class InternalHandler {
+    public Mono<ServerResponse> isAlive(ServerRequest request) {
+        return ServerResponse.ok().body(BodyInserters.fromValue("OK"));
+    }
+    public Mono<ServerResponse> isReady(ServerRequest request) {
+        return ServerResponse.ok().body(BodyInserters.fromValue("OK"));
+    }
+}
