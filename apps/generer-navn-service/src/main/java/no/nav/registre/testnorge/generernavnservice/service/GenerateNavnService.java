027fd3e358f19a77c253274b4783a1e7cce5557b
Kristen Hærum <kristen.herum@nav.no>
2021-05-20 15:56:18 +0200

    Oppdatert navneservice med mellomnavn + sjekk-endepunkt (#637)
    Oppdatert navneservice med mellomnavn + sjekk-endepunkt

--- apps/generer-navn-service/src/main/java/no/nav/registre/testnorge/generernavnservice/service/GenerateNavnService.java
+++ apps/generer-navn-service/src/main/java/no/nav/registre/testnorge/generernavnservice/service/GenerateNavnService.java
@@ -1,26 +1,28 @@
 package no.nav.registre.testnorge.generernavnservice.service;
 
+import no.nav.registre.testnorge.libs.dto.generernavnservice.v1.NavnDTO;
 import org.springframework.stereotype.Service;
 
+import java.security.SecureRandom;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-import no.nav.registre.testnorge.generernavnservice.domain.Navn;
-
 @Service
 public class GenerateNavnService {
     private static final List<String> ADJEKTIVER = FileReader.readLinesFromResources("static/adjektiv.csv");
+    private static final List<String> ADVERBER = FileReader.readLinesFromResources("static/adverb.csv");
     private static final List<String> SUBSTANTIVER = FileReader.readLinesFromResources("static/substantiv.csv");
-    private static final Random RANDOM = new Random();
+    private static final Random RANDOM = new SecureRandom();
 
-    public List<Navn> getRandomNavn(Integer antall) {
+    public List<NavnDTO> getRandomNavn(Integer antall) {
 
         return IntStream.range(0, antall).boxed()
-                .map(navn -> Navn.builder()
-                .adjektiv(toTitleCase(ADJEKTIVER.get(RANDOM.nextInt(ADJEKTIVER.size()))))
-                .substantiv(toTitleCase(SUBSTANTIVER.get(RANDOM.nextInt(SUBSTANTIVER.size()))))
+                .map(navn -> NavnDTO.builder()
+                        .adjektiv(toTitleCase(ADJEKTIVER.get(RANDOM.nextInt(ADJEKTIVER.size()))))
+                        .adverb(toTitleCase(ADVERBER.get(RANDOM.nextInt(ADVERBER.size()))))
+                        .substantiv(toTitleCase(SUBSTANTIVER.get(RANDOM.nextInt(SUBSTANTIVER.size()))))
                         .build())
                 .collect(Collectors.toList());
     }
---
+++ apps/generer-navn-service/src/main/java/no/nav/registre/testnorge/generernavnservice/service/VerifyNavnService.java
@@ -0,0 +1,27 @@
+package no.nav.registre.testnorge.generernavnservice.service;
+
+import no.nav.registre.testnorge.libs.dto.generernavnservice.v1.NavnDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static java.util.Objects.isNull;
+
+@Service
+public class VerifyNavnService {
+    private static final List<String> ADJEKTIVER = FileReader.readLinesFromResources("static/adjektiv.csv");
+    private static final List<String> ADVERBER = FileReader.readLinesFromResources("static/adverb.csv");
+    private static final List<String> SUBSTANTIVER = FileReader.readLinesFromResources("static/substantiv.csv");
+
+    private static boolean verify(String value, List<String> approvedValues) {
+
+        return isNull(value) || approvedValues.stream().anyMatch(validValue -> validValue.equalsIgnoreCase(value));
+    }
+
+    public boolean verifyNavn(NavnDTO navnDTO) {
+
+        return verify(navnDTO.getAdjektiv(), ADJEKTIVER) &&
+                verify(navnDTO.getAdverb(), ADVERBER) &&
+                verify(navnDTO.getSubstantiv(), SUBSTANTIVER);
+    }
+}
