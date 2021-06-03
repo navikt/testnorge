b39a1454a9f7c19fbdb9bf74a0e72b0d1d0b0ec9
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-06-03 13:15:52 +0200

    fix

--- apps/inntektsmelding-service/src/main/java/no/nav/registre/testnav/inntektsmeldingservice/config/AppConfig.java
+++ apps/inntektsmelding-service/src/main/java/no/nav/registre/testnav/inntektsmeldingservice/config/AppConfig.java
@@ -2,10 +2,12 @@ package no.nav.registre.testnav.inntektsmeldingservice.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import no.nav.registre.testnorge.libs.core.config.ApplicationCoreConfig;
 import no.nav.registre.testnorge.libs.oauth2.config.SecureOAuth2ServerToServerConfiguration;
 
+@EnableJpaAuditing
 @Configuration
 @Import({
         ApplicationCoreConfig.class,
