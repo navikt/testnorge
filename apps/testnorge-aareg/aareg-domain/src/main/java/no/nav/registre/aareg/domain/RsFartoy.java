059645b5195198796f15f9e4546e3cfcabd6150b
stigus <stian.gustavsson@nav.no>
2021-06-18 10:58:37 +0200

    Lagt til Fartoy

---
+++ apps/testnorge-aareg/aareg-domain/src/main/java/no/nav/registre/aareg/domain/RsFartoy.java
@@ -0,0 +1,19 @@
+package no.nav.registre.aareg.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RsFartoy {
+
+    private String skipsregister;
+    private String skipstype;
+    private String fartsomraade;
+}
