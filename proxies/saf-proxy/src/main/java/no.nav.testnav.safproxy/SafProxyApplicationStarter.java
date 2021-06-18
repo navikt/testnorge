469acd98abc34c5fd62d1c19c9a52f5b2b1fb4a7
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-06-18 14:39:45 +0200

    Fiks alle miljo

--- proxies/saf-proxy/src/main/java/no.nav.testnav.safproxy/SafProxyApplicationStarter.java
+++ proxies/saf-proxy/src/main/java/no.nav.testnav.safproxy/SafProxyApplicationStarter.java
@@ -4,16 +4,19 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.route.Route;
 import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.PredicateSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpHeaders;
 
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 import no.nav.registre.testnorge.libs.service.StsOidcTokenService;
-import no.nav.testnav.safproxy.filter.AddRequestHeaderGatewayFilterFactory;
 import no.nav.testnav.safproxy.config.GetHeader;
+import no.nav.testnav.safproxy.filter.AddRequestHeaderGatewayFilterFactory;
 
 @SpringBootApplication
 public class SafProxyApplicationStarter {
@@ -27,14 +30,14 @@ public class SafProxyApplicationStarter {
         return new StsOidcTokenService(url, username, password);
     }
 
-//    @Bean
-//    public StsOidcTokenService stsTestOidcTokenService(
-//            @Value("${sts.test.token.provider.url}") String url,
-//            @Value("${sts.test.token.provider.username}") String username,
-//            @Value("${sts.test.token.provider.password}") String password
-//    ) {
-//        return new StsOidcTokenService(url, username, password);
-//    }
+    @Bean
+    public StsOidcTokenService stsTestOidcTokenService(
+            @Value("${sts.test.token.provider.url}") String url,
+            @Value("${sts.test.token.provider.username}") String username,
+            @Value("${sts.test.token.provider.password}") String password
+    ) {
+        return new StsOidcTokenService(url, username, password);
+    }
 
 
     public static void main(String[] args) {
@@ -49,17 +52,34 @@ public class SafProxyApplicationStarter {
 
 
     @Bean
-    public RouteLocator customRouteLocator(RouteLocatorBuilder builder, StsOidcTokenService stsPreprodOidcTokenService) {
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder, StsOidcTokenService stsTestOidcTokenService, StsOidcTokenService stsPreprodOidcTokenService) {
         var preprodFilter = addAuthenticationHeaderFilter(stsPreprodOidcTokenService::getToken);
-        //var testFilter = addAuthenticationHeaderFilter(stsTestOidcTokenService::getToken);
+        var testFilter = addAuthenticationHeaderFilter(stsTestOidcTokenService::getToken);
         return builder
                 .routes()
-                .route("saf-q2", spec -> spec
-                        .path("/q2/**")
-                        .filters(filterSpec -> filterSpec
-                                .rewritePath("/q2/(?<segment>.*)", "/${segment}")
-                                .filter(preprodFilter)
-                        ).uri("https://saf-q2.dev.adeo.no/")
-                ).build();
+                .route(createRoute("q1", preprodFilter))
+                .route(createRoute("q2", preprodFilter))
+                .route(createRoute("q4", preprodFilter))
+                .route(createRoute("q5", preprodFilter))
+                .route(createRoute("qx", preprodFilter))
+                .route(createRoute("t0", testFilter))
+                .route(createRoute("t1", testFilter))
+                .route(createRoute("t2", testFilter))
+                .route(createRoute("t3", testFilter))
+                .route(createRoute("t4", testFilter))
+                .route(createRoute("t5", testFilter))
+                .route(createRoute("t6", testFilter))
+                .route(createRoute("t13", testFilter))
+                .build();
     }
+
+    private Function<PredicateSpec, Route.AsyncBuilder> createRoute(String miljo, GatewayFilter filter) {
+        return spec -> spec
+                .path("/" + miljo + "/**")
+                .filters(filterSpec -> filterSpec
+                        .rewritePath("/" + miljo + "/(?<segment>.*)", "/${segment}")
+                        .filter(filter)
+                ).uri("https://saf-" + miljo + ".dev.adeo.no/");
+    }
+
 }
