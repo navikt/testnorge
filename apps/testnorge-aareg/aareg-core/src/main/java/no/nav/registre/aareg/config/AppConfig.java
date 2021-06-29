d6f47d14057f1cf1c8dc320c37321f038bef99aa
stigus <stian.gustavsson@nav.no>
2021-06-29 11:10:53 +0200

    Fjerner duplikat Objectmapper Bean

--- apps/testnorge-aareg/aareg-core/src/main/java/no/nav/registre/aareg/config/AppConfig.java
+++ apps/testnorge-aareg/aareg-core/src/main/java/no/nav/registre/aareg/config/AppConfig.java
@@ -1,6 +1,5 @@
 package no.nav.registre.aareg.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import no.nav.registre.testnorge.consumers.hodejegeren.HodejegerenConsumer;
 import no.nav.registre.testnorge.libs.core.config.ApplicationCoreConfig;
 import no.nav.registre.testnorge.libs.oauth2.config.InsecureOAuth2ServerToServerConfiguration;
@@ -26,11 +25,6 @@ public class AppConfig {
     @Bean
     public Random rand() {
         return new Random();
-    }
-
-    @Bean
-    public ObjectMapper objectMapper() {
-        return new ObjectMapper();
     }
 
     @Bean
