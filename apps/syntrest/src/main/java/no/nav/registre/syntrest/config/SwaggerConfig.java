7ca883b623a4a3fac165b5f5ae240f1f5de9208a
Aileen Hay <aileen.hay@nav.no>
2021-07-29 08:30:11 +0200

    Fikset codesmell og fjernet ubrukt kode

--- apps/syntrest/src/main/java/no/nav/registre/syntrest/config/SwaggerConfig.java
+++ apps/syntrest/src/main/java/no/nav/registre/syntrest/config/SwaggerConfig.java
@@ -16,7 +16,6 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 
@@ -29,7 +28,7 @@ public class SwaggerConfig implements WebMvcConfigurer {
 
     @Bean
     public Docket api() {
-        var contentTypeJson = new HashSet(Collections.singletonList("application/json"));
+        var contentTypeJson = new HashSet<>(Collections.singletonList("application/json"));
         return new Docket(DocumentationType.SWAGGER_2)
                 .ignoredParameterTypes(ApiIgnore.class)
                 .select()
