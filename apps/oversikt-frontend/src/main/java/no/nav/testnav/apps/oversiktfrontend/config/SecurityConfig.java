8bc11011449e2ee6fb8556e17083e4b193e7eea3
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-08-03 09:03:19 +0200

    Bruk composit build for frontend apper

---
+++ apps/oversikt-frontend/src/main/java/no/nav/testnav/apps/oversiktfrontend/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package no.nav.testnav.apps.oversiktfrontend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http.authorizeExchange()
+                .pathMatchers("/internal/isReady", "/internal/isAlive").permitAll()
+                .anyExchange().authenticated()
+                .and().oauth2Login();
+        return http.build();
+    }
+}
