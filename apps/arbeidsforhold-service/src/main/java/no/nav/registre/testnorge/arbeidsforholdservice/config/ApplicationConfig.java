69caa1459b64a1b68bc2d99243809eaccfb45cfc
stigus <stian.gustavsson@nav.no>
2021-08-04 10:08:08 +0200

    Deploy dev

--- apps/arbeidsforhold-service/src/main/java/no/nav/registre/testnorge/arbeidsforholdservice/config/ApplicationConfig.java
+++ apps/arbeidsforhold-service/src/main/java/no/nav/registre/testnorge/arbeidsforholdservice/config/ApplicationConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Import;
 import no.nav.registre.testnorge.libs.core.config.ApplicationCoreConfig;
 import no.nav.registre.testnorge.libs.oauth2.config.SecureOAuth2ServerToServerConfiguration;
 
-@Slf4j
 @Configuration
 @Import({
         ApplicationCoreConfig.class,
