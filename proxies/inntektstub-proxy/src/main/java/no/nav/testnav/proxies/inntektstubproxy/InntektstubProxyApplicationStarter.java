7b8d9499f262b05bc557dc502fce41906567187d
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-08-06 12:06:44 +0200

    Opprett

---
+++ proxies/inntektstub-proxy/src/main/java/no/nav/testnav/proxies/inntektstubproxy/InntektstubProxyApplicationStarter.java
@@ -0,0 +1,31 @@
+package no.nav.testnav.proxies.inntektstubproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+
+import no.nav.testnav.libs.reactivecore.config.CoreConfig;
+import no.nav.testnav.libs.reactiveproxy.config.DevConfig;
+import no.nav.testnav.libs.reactiveproxy.config.SecurityConfig;
+
+@Import({
+        CoreConfig.class,
+        DevConfig.class,
+        SecurityConfig.class
+})
+@SpringBootApplication
+public class InntektstubProxyApplicationStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(InntektstubProxyApplicationStarter.class, args);
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(spec -> spec.path("/**").uri("https://inntektstub.dev.adeo.no/"))
+                .build();
+    }
+}
