5f7271a9284dc6fdfcf71971fedb00d3247ff7f8
stigus <stian.gustavsson@nav.no>
2021-08-13 16:22:02 +0200

    Fikset Return type

--- apps/profil-api/src/main/java/no/nav/registre/testnorge/profil/consumer/command/GetProfileImageCommand.java
+++ apps/profil-api/src/main/java/no/nav/registre/testnorge/profil/consumer/command/GetProfileImageCommand.java
@@ -2,6 +2,8 @@ package no.nav.registre.testnorge.profil.consumer.command;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Optional;
@@ -18,20 +20,19 @@ public class GetProfileImageCommand implements Callable<Optional<byte[]>> {
     public Optional<byte[]> call() {
 
         try {
-            return Optional.empty();
-//            return Optional.of(webClient
-//                    .get()
-//                    .uri(builder -> builder.path("/v1.0/me/photos/240x240/$value").build())
-//                    .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
-//                    .retrieve()
-//                    .onStatus(
-//                            HttpStatus::isError,
-//                            clientResponse -> clientResponse
-//                                    .bodyToMono(String.class)
-//                                    .map(IllegalStateException::new)
-//                    )
-//                    .bodyToMono(byte[].class)
-//                    .block());
+            return Optional.of(webClient
+                    .get()
+                    .uri(builder -> builder.path("/v1.0/me/photos/240x240/$value").build())
+                    .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
+                    .retrieve()
+                    .onStatus(
+                            HttpStatus::isError,
+                            clientResponse -> clientResponse
+                                    .bodyToMono(String.class)
+                                    .map(IllegalStateException::new)
+                    )
+                    .bodyToMono(byte[].class)
+                    .block());
         } catch (IllegalStateException e) {
             log.warn("Bilde for bruker ikke funnet, melding: ", e);
             return Optional.empty();
