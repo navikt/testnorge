110484cb7b651f122967a2334bae4e98c1a02eb7
stigus <stian.gustavsson@nav.no>
2021-08-13 14:03:27 +0200

    Test V2

--- apps/profil-api/src/main/java/no/nav/registre/testnorge/profil/provider/ProfilController.java
+++ apps/profil-api/src/main/java/no/nav/registre/testnorge/profil/provider/ProfilController.java
@@ -7,11 +7,10 @@ import no.nav.testnav.libs.dto.profil.v1.ProfilDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import org.webjars.NotFoundException;
 
 @RestController
@@ -27,11 +26,13 @@ public class ProfilController {
         return ResponseEntity.ok(profil.toDTO());
     }
 
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ExceptionHandler({ NotFoundException.class })
     @GetMapping(value = "/bilde", produces = MediaType.IMAGE_JPEG_VALUE)
     public ResponseEntity<byte[]> getImage() {
-        byte[] image = azureAdProfileConsumer.getProfilImage();
-        return ResponseEntity.ok(image);
+        try {
+            byte[] image = azureAdProfileConsumer.getProfilImage();
+            return ResponseEntity.ok(image);
+        } catch (NotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Bilde ikke funnet for bruker", e);
+        }
     }
 }
