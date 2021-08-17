518676c7e25ec20f57372c96d97ff5f96d24e020
Aileen Hay <aileen.hay@nav.no>
2021-08-17 13:01:42 +0200

    Fjernet ubrukt import

--- apps/varslinger-service/src/main/java/no/nav/registre/varslingerservice/provider/VarslingerController.java
+++ apps/varslinger-service/src/main/java/no/nav/registre/varslingerservice/provider/VarslingerController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
