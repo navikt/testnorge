53fd438b5b81c75b02304b7d56f1f6c206163f86
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-08-17 10:48:55 +0200

    Opprett redirect for dolly-frontend

---
+++ apps/dolly-frontend-redirect/src/main/java/no/nav/registre/testnav/dollyfrontendredirect/controller/RedirectController.java
@@ -0,0 +1,19 @@
+package no.nav.registre.testnav.dollyfrontendredirect.controller;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping("/**")
+public class RedirectController {
+
+    @GetMapping
+    public void method(HttpServletResponse httpServletResponse) {
+        httpServletResponse.setHeader(HttpHeaders.LOCATION, "https://dolly-frontend-dev.ekstern.dev.nav.no/");
+        httpServletResponse.setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY);
+    }
+}
