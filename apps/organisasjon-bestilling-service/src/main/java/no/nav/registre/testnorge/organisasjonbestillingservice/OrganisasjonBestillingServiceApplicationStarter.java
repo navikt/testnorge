5b86865be98d7725689c55b72fae95ce57dab32b
kristenhaerum <kristen.herum@nav.no>
2021-08-20 07:58:49 +0200

    Oppdatering for oversikt frontend

--- apps/organisasjon-bestilling-service/src/main/java/no/nav/registre/testnorge/organisasjonbestillingservice/OrganisasjonBestillingServiceApplicationStarter.java
+++ apps/organisasjon-bestilling-service/src/main/java/no/nav/registre/testnorge/organisasjonbestillingservice/OrganisasjonBestillingServiceApplicationStarter.java
@@ -9,4 +9,5 @@ public class OrganisasjonBestillingServiceApplicationStarter {
     public static void main(String[] args) {
         SpringApplication.run(OrganisasjonBestillingServiceApplicationStarter.class, args);
     }
+
 }
