f7b8e4725b0cee27bd4d1c340d7dd83b170cdca6
stigus <stian.gustavsson@nav.no>
2021-08-27 11:26:41 +0200

    Endret på visning

--- apps/dolly-frontend/src/main/js/src/components/fagsystem/dokarkiv/visning/DokarkivVisning.tsx
+++ apps/dolly-frontend/src/main/js/src/components/fagsystem/dokarkiv/visning/DokarkivVisning.tsx
@@ -2,24 +2,32 @@ import React from 'react'
 import {Dokument, Journalpost} from '~/service/services/JoarkDokumentService'
 import {TitleValue} from '~/components/ui/titleValue/TitleValue'
 import {DollyFieldArray} from '~/components/ui/form/fieldArray/DollyFieldArray'
-import SubOverskrift from '~/components/ui/subOverskrift/SubOverskrift'
+import styled from 'styled-components'
 
 type Props = {
 	journalpost: Journalpost
 }
 
+const H4 = styled.h4`
+	width: 100%;
+`
+
 export default ({ journalpost }: Props) => (
-	<>
+	<div className="person-visning_content">
 		<TitleValue title="Tittel" value={journalpost.tittel} />
 		<TitleValue title="Kanal" value={journalpost.kanal} />
 		<TitleValue title="Tema" value={journalpost.tema} />
-
-		<SubOverskrift label="Avsender" iconKind="dokarkiv" />
-		<TitleValue title="Type" value={journalpost.avsenderMottaker?.type} />
-		<TitleValue title="ID" value={journalpost.avsenderMottaker?.id} />
-		<TitleValue title="Navn" value={journalpost.avsenderMottaker?.navn} />
-
 		<TitleValue title="Journalpost-ID" value={journalpost.journalpostId} />
+
+		{journalpost.avsenderMottaker ? (
+			<>
+				<H4>Avsender</H4>
+				<TitleValue title="Type" value={journalpost.avsenderMottaker.type} />
+				<TitleValue title="ID" value={journalpost.avsenderMottaker.id} />
+				<TitleValue title="Navn" value={journalpost.avsenderMottaker.navn} />
+			</>
+		) : null}
+
 		<DollyFieldArray header={'Vedlegg'} data={journalpost.dokumenter} nested>
 			{(dokument: Dokument, idx: number) => (
 				<div key={idx} className="person-visning_content">
@@ -28,5 +36,5 @@ export default ({ journalpost }: Props) => (
 				</div>
 			)}
 		</DollyFieldArray>
-	</>
+	</div>
 )
