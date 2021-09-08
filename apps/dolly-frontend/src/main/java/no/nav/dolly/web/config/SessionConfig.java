3f5a8a89077d07546f655e0533a8d1607b8d41f2
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-09-08 21:53:17 +0200

    redis session

--- apps/dolly-frontend/src/main/java/no/nav/dolly/web/config/SessionConfig.java
+++ apps/dolly-frontend/src/main/java/no/nav/dolly/web/config/SessionConfig.java
@@ -2,11 +2,13 @@ package no.nav.dolly.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
 
+@Profile("prod")
 @Configuration
 @EnableRedisHttpSession
 public class SessionConfig extends AbstractHttpSessionApplicationInitializer {
