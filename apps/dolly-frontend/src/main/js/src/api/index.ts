d14c1c529e828fedb15bac0a1bc0ebff20c9c9c2
stigus <stian.gustavsson@nav.no>
2021-09-13 14:37:14 +0200

    Fix

--- apps/dolly-frontend/src/main/js/src/api/index.ts
+++ apps/dolly-frontend/src/main/js/src/api/index.ts
@@ -36,7 +36,7 @@ const fetchJson = <T>(url: string, config: Config, body?: object): Promise<T> =>
 		url,
 		{
 			method: config.method,
-			headers: { ...config.headers },
+			headers: { ...config.headers, 'Content-Type': 'application/json' },
 		},
 		body
 	).then((response: Response) => response.json() as Promise<T>)
