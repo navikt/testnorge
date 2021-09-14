e7b1086db1531ed76e41a880d6e4433a06d014d3
Betsy Carina Traran <betsy.carina.traran@nav.no>
2021-09-14 09:46:55 +0200

    Caching

--- apps/geografiske-kodeverk-service/src/main/java/no/nav/registre/testnav/geografiskekodeverkservice/provider/KodeverkController.java
+++ apps/geografiske-kodeverk-service/src/main/java/no/nav/registre/testnav/geografiskekodeverkservice/provider/KodeverkController.java
@@ -3,38 +3,60 @@ package no.nav.registre.testnav.geografiskekodeverkservice.provider;
 import lombok.RequiredArgsConstructor;
 import no.nav.registre.testnav.geografiskekodeverkservice.domain.Kodeverk;
 import no.nav.registre.testnav.geografiskekodeverkservice.service.KodeverkService;
+import org.springframework.http.CacheControl;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/api/v1")
-@RequiredArgsConstructor
 
 public class KodeverkController {
 
     private final KodeverkService kodeverkService;
+    private final CacheControl cacheControl;
+
+    public KodeverkController(KodeverkService kodeverkService) {
+        this.kodeverkService = kodeverkService;
+        this.cacheControl = CacheControl.maxAge(1, TimeUnit.HOURS).noTransform().mustRevalidate();
+    }
 
     @GetMapping(value = "/kommuner")
-    public List<Kodeverk> getKommuner(@RequestParam(required = false) String kommunenr) {
-        return kodeverkService.getKommuner(kommunenr);
+    public ResponseEntity<List<Kodeverk>> getKommuner(@RequestParam(required = false) String kommunenr) {
+        return ResponseEntity
+                .ok()
+                .cacheControl(cacheControl)
+                .body(kodeverkService.getKommuner(kommunenr));
     }
 
     @GetMapping(value = "/landkoder")
-    public List<Kodeverk> getLandkoder(@RequestParam(required = false) String land) {
-        return kodeverkService.getLandkoder(land);
+    public ResponseEntity<List<Kodeverk>> getLandkoder(@RequestParam(required = false) String land) {
+        return ResponseEntity
+                .ok()
+                .cacheControl(cacheControl)
+                .body(kodeverkService.getLandkoder(land));
     }
 
     @GetMapping(value = "/postnummer")
-    public List<Kodeverk> getPostnummer(@RequestParam(required = false) String poststed) {
-        return kodeverkService.getPostnummer(poststed);
+    public ResponseEntity<List<Kodeverk>> getPostnummer(@RequestParam(required = false) String poststed) {
+        return ResponseEntity
+                .ok()
+                .cacheControl(cacheControl)
+                .body(kodeverkService.getPostnummer(poststed));
+
     }
 
     @GetMapping(value = "/embeter")
-    public List<Kodeverk> getEmbeter() {
-        return kodeverkService.getEmbeter();
+    public ResponseEntity<List<Kodeverk>> getEmbeter() {
+        return ResponseEntity
+                .ok()
+                .cacheControl(cacheControl)
+                .body(kodeverkService.getEmbeter());
+
     }
 }
