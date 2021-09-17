c295e4221f2287df5dc458a0d16258462cce2f92
stigus <stian.gustavsson@nav.no>
2021-09-17 15:56:30 +0200

    Fikset manglende felt

--- apps/dolly-frontend/src/main/js/src/components/bestillingsveileder/stegVelger/steg/steg3/OppsummeringKommentarForm.tsx
+++ apps/dolly-frontend/src/main/js/src/components/bestillingsveileder/stegVelger/steg/steg3/OppsummeringKommentarForm.tsx
@@ -4,10 +4,8 @@ import { FormikTextInput } from '~/components/ui/form/inputs/textInput/TextInput
 export const OppsummeringKommentarForm = () => {
 	return (
 		<div className="input-oppsummering">
-			<div className="flexbox--align-center">
-				<h2>Send med kommentar</h2>
-				<FormikTextInput name="beskrivelse" />
-			</div>
+			<h2>Send med kommentar</h2>
+			<FormikTextInput name="beskrivelse" />
 		</div>
 	)
 }
