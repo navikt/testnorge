95dc26f034a671db61332f16500a7c437de5dcce
stigus <stian.gustavsson@nav.no>
2021-09-21 10:30:06 +0200

    Generer navn service tilgang til backend

--- apps/person-service/src/main/java/no/nav/testnav/apps/personservice/PersonServiceApplicationStarter.java
+++ apps/person-service/src/main/java/no/nav/testnav/apps/personservice/PersonServiceApplicationStarter.java
@@ -1,12 +1,11 @@
 package no.nav.testnav.apps.personservice;
 
+import no.nav.testnav.libs.reactivecore.config.CoreConfig;
+import no.nav.testnav.libs.reactivesecurity.config.SecureOAuth2ServerToServerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.reactive.config.EnableWebFlux;
-
-import no.nav.testnav.libs.reactivecore.config.CoreConfig;
-import no.nav.testnav.libs.reactivesecurity.config.SecureOAuth2ServerToServerConfiguration;
 
 @Import({
         CoreConfig.class,
