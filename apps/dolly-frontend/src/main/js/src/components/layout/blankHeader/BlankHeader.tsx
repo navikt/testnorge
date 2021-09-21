590a08923deaa0ad9b3668ab21a59ad8aef69c28
Aileen Hay <aileen.hay@nav.no>
2021-09-21 08:49:02 +0200

    Fjernet navlink som ikke funket

--- apps/dolly-frontend/src/main/js/src/components/layout/blankHeader/BlankHeader.tsx
+++ apps/dolly-frontend/src/main/js/src/components/layout/blankHeader/BlankHeader.tsx
@@ -8,13 +8,13 @@ import { NavLink } from 'react-router-dom'
 export default () => {
 	return (
 		<header className="blank-header">
-			<NavLink to="/login" className="home-nav">
+			<div className="home-nav">
 				<div className="img-logo">
 					<img alt="NAV logo" src={logo} />
 				</div>
 				<Icon size={30} kind="dolly" className="dollysheep" />
 				<h1>Dolly</h1>
-			</NavLink>
+			</div>
 		</header>
 	)
 }
