387e68c824fa804d3fff059be1ae0d0c477b07d6
kristenhaerum <kristen.herum@nav.no>
2021-09-22 08:23:22 +0200

    Oppdaterer libs for OrdreRequestDto

--- libs/data-transfer-objects/src/main/java/no/nav/testnav/libs/dto/pdlforvalter/v1/OrdreRequestDTO.java
+++ libs/data-transfer-objects/src/main/java/no/nav/testnav/libs/dto/pdlforvalter/v1/OrdreRequestDTO.java
@@ -1,15 +1,17 @@
 package no.nav.testnav.libs.dto.pdlforvalter.v1;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class OrdreRequestDTO {
 
-    List<String> identer;
+    private List<String> identer;
 }
