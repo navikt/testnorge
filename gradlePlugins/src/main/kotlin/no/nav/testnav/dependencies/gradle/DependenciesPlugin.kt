6026040d17926602c459885662a20cb377b426e9
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-09-27 12:41:50 +0200

    bump spring versions (#833)

--- gradlePlugins/src/main/kotlin/no/nav/testnav/dependencies/gradle/DependenciesPlugin.kt
+++ gradlePlugins/src/main/kotlin/no/nav/testnav/dependencies/gradle/DependenciesPlugin.kt
@@ -9,9 +9,9 @@ class DependenciesPlugin : Plugin<Project> {
 
     companion object {
         val lombokVersion = "1.18.20"
-        val springBoot = "2.5.4"
-        val springCloud = "2020.0.3"
-        val springSession = "2020.0.3"
+        val springBoot = "2.5.5"
+        val springCloud = "2020.0.4"
+        val springSession = "2021.0.2"
         val logstashLogbackEncoder = "6.6"
         val springdocOpenapi = "1.5.9"
         val jackson = "2.11.2"
