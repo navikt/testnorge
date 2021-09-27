cda903fadaaf0edbbf95510d0db92099e4fcf1f5
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-09-27 14:15:50 +0200

    Oppdater til Java 17

--- apps/testnorge-hodejegeren/src/test/java/no/nav/registre/hodejegeren/comptests/ApplicationTestConfig.java
+++ apps/testnorge-hodejegeren/src/test/java/no/nav/registre/hodejegeren/comptests/ApplicationTestConfig.java
@@ -1,19 +1,7 @@
 package no.nav.registre.hodejegeren.comptests;
 
-import static org.mockito.Mockito.mock;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-
-import java.util.Random;
 
 @Configuration
 public class ApplicationTestConfig {
-
-    @Bean
-    @Primary
-    public Random randMock() {
-        return mock(Random.class);
-    }
 }
