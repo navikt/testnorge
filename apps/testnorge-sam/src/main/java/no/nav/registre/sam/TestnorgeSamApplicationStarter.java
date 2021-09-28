3b1a23cd03d3fd6eee8a19063cb51ab4ce1e935b
Aileen Hay <aileen.hay@nav.no>
2021-09-28 15:32:31 +0200

    Endret til composite build

--- apps/testnorge-sam/src/main/java/no/nav/registre/sam/TestnorgeSamApplicationStarter.java
+++ apps/testnorge-sam/src/main/java/no/nav/registre/sam/TestnorgeSamApplicationStarter.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApplicationStarter {
+public class TestnorgeSamApplicationStarter {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApplicationStarter.class, args);
+        SpringApplication.run(TestnorgeSamApplicationStarter.class, args);
     }
 }
