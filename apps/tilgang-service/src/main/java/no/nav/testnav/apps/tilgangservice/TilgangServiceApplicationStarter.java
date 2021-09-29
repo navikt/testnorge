0b3355650dbe0e1038f74106deab549cee1d6250
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-09-29 09:30:26 +0200

    WIP

---
+++ apps/tilgang-service/src/main/java/no/nav/testnav/apps/tilgangservice/TilgangServiceApplicationStarter.java
@@ -0,0 +1,12 @@
+package no.nav.testnav.apps.tilgangservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class TilgangServiceApplicationStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(TilgangServiceApplicationStarter.class, args);
+    }
+}
