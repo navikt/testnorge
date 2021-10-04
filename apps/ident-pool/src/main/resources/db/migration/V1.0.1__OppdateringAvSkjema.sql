1e0b2104369ce845a02100d99edb91854e4e9aaf
Kristen Hærum <kristen.herum@nav.no>
2021-10-04 11:21:01 +0200

    Update V1.0.1__OppdateringAvSkjema.sql

--- apps/ident-pool/src/main/resources/db/migration/V1.0.1__OppdateringAvSkjema.sql
+++ apps/ident-pool/src/main/resources/db/migration/V1.0.1__OppdateringAvSkjema.sql
@@ -7,11 +7,3 @@ alter column foedselsdato type date;
 
 alter table ajourhold
 alter column feilmelding type text;
-
-
--------------------------------------
--- C R E A T E   S E Q U E N C E S --
--------------------------------------
-create sequence ajourhold_seq start with 263293;
-
-create sequence personidentifikator_seq start with 13280165;
