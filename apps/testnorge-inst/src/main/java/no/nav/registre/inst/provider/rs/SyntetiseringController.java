afc28115e3778d799c3bd4eae7433aac2ca83622
Aileen Hay <aileen.hay@nav.no>
2021-10-05 13:18:34 +0200

    Kode rydding

--- apps/testnorge-inst/src/main/java/no/nav/registre/inst/provider/rs/SyntetiseringController.java
+++ apps/testnorge-inst/src/main/java/no/nav/registre/inst/provider/rs/SyntetiseringController.java
@@ -4,7 +4,12 @@ import lombok.RequiredArgsConstructor;
 import no.nav.registre.inst.provider.rs.requests.SyntetiserInstRequest;
 import no.nav.registre.inst.provider.rs.responses.OppholdResponse;
 import no.nav.registre.inst.service.SyntetiseringService;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.constraints.NotBlank;
 import java.util.List;
