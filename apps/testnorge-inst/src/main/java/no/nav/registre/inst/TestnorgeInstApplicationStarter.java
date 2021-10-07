3debfc41436f6cd702b5eff9366b926f9a5a7993
Aileen Hay <58416744+H153910@users.noreply.github.com>
2021-10-07 11:37:49 +0200

    Testnorge inst/bytte til synt inst gcp (#846)
    Byttet til bruk av synt-inst-gcp

--- apps/testnorge-inst/src/main/java/no/nav/registre/inst/TestnorgeInstApplicationStarter.java
+++ apps/testnorge-inst/src/main/java/no/nav/registre/inst/TestnorgeInstApplicationStarter.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApplicationStarter {
+public class TestnorgeInstApplicationStarter {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApplicationStarter.class, args);
+        SpringApplication.run(TestnorgeInstApplicationStarter.class, args);
     }
 }
--- apps/testnorge-inst/src/test/java/no/nav/registre/inst/consumer/rs/InstSyntetisererenConsumerTest.java
+++ apps/testnorge-inst/src/test/java/no/nav/registre/inst/consumer/rs/InstSyntetisererenConsumerTest.java
@@ -1,49 +1,86 @@
 package no.nav.registre.inst.consumer.rs;
 
+import lombok.extern.slf4j.Slf4j;
+import no.nav.registre.inst.consumer.rs.credential.SyntInstGcpProperties;
+import no.nav.testnav.libs.servletsecurity.domain.AzureClientCredentials;
+import no.nav.testnav.libs.servletsecurity.service.AccessTokenService;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.get;
-import static com.github.tomakehurst.wiremock.client.WireMock.ok;
-import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
-import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import okhttp3.mockwebserver.Dispatcher;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import okhttp3.mockwebserver.RecordedRequest;
+
+import java.io.IOException;
+
 import static no.nav.registre.inst.testutils.ResourceUtils.getResourceFileContent;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureWireMock(port = 0)
+@Slf4j
 @TestPropertySource(locations = "classpath:application-test.properties")
 @ActiveProfiles("test")
 public class InstSyntetisererenConsumerTest {
 
-    @Autowired
-    private InstSyntetisererenConsumer instSyntetisererenConsumer;
+    private InstSyntetisererenConsumer consumer;
 
-    private int numToGenerate = 2;
+    private MockWebServer mockWebServer;
+
+    private final int numToGenerate = 2;
+
+    @Before
+    public void setUp() throws IOException {
+        this.mockWebServer = new MockWebServer();
+        this.mockWebServer.start();
+        Dispatcher dispatcher = getSyntInstDispatcher();
+        mockWebServer.setDispatcher(dispatcher);
+
+        var creds = new SyntInstGcpProperties();
+        creds.setUrl(mockWebServer.url("/synt-inst").toString());
+        creds.setCluster("test");
+        creds.setNamespace("test");
+        creds.setName("synthdata-inst-gcp-test");
+
+        this.consumer = new InstSyntetisererenConsumer(
+                creds,
+                new AccessTokenService(null, mockWebServer.url("/token").toString(), null,
+                        new AzureClientCredentials("dummy", "dummy")));
+
+    }
 
     @Test
     public void shouldGetMeldinger() {
-        stubInstSyntetisererenConsumer();
-        var result = instSyntetisererenConsumer.hentInstMeldingerFromSyntRest(numToGenerate);
+        var result = consumer.hentInstMeldingerFromSyntRest(numToGenerate);
 
         assertThat(result.size(), is(numToGenerate));
         assertThat(result.get(0).getTssEksternId(), equalTo("440"));
         assertThat(result.get(1).getTssEksternId(), equalTo("441"));
     }
 
-    private void stubInstSyntetisererenConsumer() {
-        stubFor(get(urlEqualTo("/synthdata-inst/api/v1/generate/inst?numToGenerate=" + numToGenerate))
-                .willReturn(ok()
-                        .withHeader("Content-Type", "application/json")
-                        .withBody(getResourceFileContent("institusjonsmelding.json"))));
+    private Dispatcher getSyntInstDispatcher() {
+        return new Dispatcher() {
+            @Override
+            public MockResponse dispatch(RecordedRequest request) {
+                return switch (request.getPath()) {
+                    case "/synt-inst/api/v1/generate/inst/" + numToGenerate -> new MockResponse().setResponseCode(200)
+                            .addHeader("Content-Type", "application/json")
+                            .setBody(getResourceFileContent("institusjonsmelding.json"));
+                    case "/token/oauth2/v2.0/token" -> new MockResponse().setResponseCode(200)
+                            .addHeader("Content-Type", "application/json")
+                            .setBody("{\"access_token\": \"dummy\"}");
+                    default -> new MockResponse().setResponseCode(404);
+                };
+            }
+        };
+    }
+
+    @After
+    public void tearDown() throws IOException {
+        mockWebServer.shutdown();
     }
 }
