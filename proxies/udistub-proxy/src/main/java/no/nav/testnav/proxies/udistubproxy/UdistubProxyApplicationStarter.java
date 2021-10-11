43fa6cf5f111f9560709166050d88d8776db55fa
stigus <stian.gustavsson@nav.no>
2021-10-11 11:15:26 +0200

    Lagt til proxy for udi-stub

---
+++ proxies/udistub-proxy/src/main/java/no/nav/testnav/proxies/udistubproxy/UdistubProxyApplicationStarter.java
@@ -0,0 +1,42 @@
+package no.nav.testnav.proxies.udistubproxy;
+
+import no.nav.testnav.libs.reactivecore.config.CoreConfig;
+import no.nav.testnav.libs.reactiveproxy.config.DevConfig;
+import no.nav.testnav.libs.reactiveproxy.config.SecurityConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+
+@Import({
+        CoreConfig.class,
+        DevConfig.class,
+        SecurityConfig.class
+})
+@SpringBootApplication
+public class UdistubProxyApplicationStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(UdistubProxyApplicationStarter.class, args);
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+
+        return builder
+                .routes()
+                .route(createRoute("udistub", "https://udi-stub.dev.intern.nav.no"))
+                .route(createRoute("udistub-dev", "https://udi-stub-dev.dev.intern.nav.no"))
+                .build();
+    }
+
+    private Function<PredicateSpec, Buildable<Route>> createRoute(String segment, String host) {
+        return spec -> spec
+                .path("/" + segment + "/**")
+                .filters(filterSpec -> filterSpec
+                        .rewritePath("/" + segment + "/(?<segment>.*)", "/${segment}")
+                ).uri(host);
+    }
+
+}
