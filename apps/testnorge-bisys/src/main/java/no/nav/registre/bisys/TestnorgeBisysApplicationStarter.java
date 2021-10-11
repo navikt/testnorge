ef9820d66c3f6b691378756955151531aa48888f
Aileen Hay <aileen.hay@nav.no>
2021-10-11 12:36:16 +0200

    Endret til composite build

--- apps/testnorge-bisys/src/main/java/no/nav/registre/bisys/TestnorgeBisysApplicationStarter.java
+++ apps/testnorge-bisys/src/main/java/no/nav/registre/bisys/TestnorgeBisysApplicationStarter.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApplicationStarter {
+public class TestnorgeBisysApplicationStarter {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApplicationStarter.class, args);
+        SpringApplication.run(TestnorgeBisysApplicationStarter.class, args);
     }
 }
