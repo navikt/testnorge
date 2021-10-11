fd42a1a5337c8be9d616a8eb2a7ca5776845f0de
Aileen Hay <aileen.hay@nav.no>
2021-10-11 13:28:14 +0200

    Oppdatert consumer og kode ryddet

--- apps/testnorge-bisys/src/main/java/no/nav/registre/bisys/config/AppConfig.java
+++ apps/testnorge-bisys/src/main/java/no/nav/registre/bisys/config/AppConfig.java
@@ -1,6 +1,6 @@
 package no.nav.registre.bisys.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import no.nav.testnav.libs.servletsecurity.config.SecureOAuth2ServerToServerConfiguration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,19 +8,17 @@ import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.client.RestTemplate;
 
-import no.nav.registre.bisys.consumer.rs.BisysSyntetisererenConsumer;
-import no.nav.registre.bisys.service.SyntetiseringService;
 import no.nav.registre.testnorge.consumers.hodejegeren.HodejegerenConsumer;
 import no.nav.testnav.libs.servletcore.config.ApplicationCoreConfig;
 
 @Configuration
-@Import(ApplicationCoreConfig.class)
+@Import(value = {
+        ApplicationCoreConfig.class,
+        SecureOAuth2ServerToServerConfiguration.class
+})
 public class AppConfig {
 
-    @Value("${SYNTREST_REST_API}")
-    private String syntrestServerUrl;
-
-    @Value("${HODEJEGEREN_REST_API}")
+    @Value("${consumers.hodejegeren.url}")
     private String hodejegerenUrl;
 
     @Bean
@@ -29,24 +27,8 @@ public class AppConfig {
     }
 
     @Bean
-    public BisysSyntetisererenConsumer bisysSyntetisererenConsumer() {
-        return new BisysSyntetisererenConsumer(syntrestServerUrl);
-    }
-
-    @Bean
     @DependsOn("restTemplate")
     public HodejegerenConsumer hodejegerenConsumer() {
         return new HodejegerenConsumer(hodejegerenUrl, restTemplate());
-    }
-
-    @Bean
-    public SyntetiseringService syntetiseringService(
-            @Autowired BisysSyntetisererenConsumer bisysSyntetisererenConsumer,
-            @Value("${USE_HISTORICAL_MOTTATTDATO}") boolean useHistoricalMottattdato) {
-
-        return SyntetiseringService.builder()
-                .hodejegerenConsumer(hodejegerenConsumer())
-                .bisysSyntetisererenConsumer(bisysSyntetisererenConsumer)
-                .useHistoricalMottattdato(useHistoricalMottattdato).build();
     }
 }
