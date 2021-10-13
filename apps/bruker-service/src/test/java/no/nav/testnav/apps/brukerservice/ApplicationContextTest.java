13b773d5f1f93f26032d171cc903f5282f98fb58
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-10-13 11:58:33 +0200

    fix test

--- apps/bruker-service/src/test/java/no/nav/testnav/apps/brukerservice/ApplicationContextTest.java
+++ apps/bruker-service/src/test/java/no/nav/testnav/apps/brukerservice/ApplicationContextTest.java
@@ -3,9 +3,13 @@ package no.nav.testnav.apps.brukerservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+
+import no.nav.testnav.apps.brukerservice.initializer.WireMockInitializer;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@ContextConfiguration(initializers = {WireMockInitializer.class})
 class ApplicationContextTest {
     @Test
     @SuppressWarnings("java:S2699")
