a9d2abf81f5cecab9d398dd87055e3d94fd0da8d
stigus <stian.gustavsson@nav.no>
2021-10-13 14:20:43 +0200

    Laget proxy for sykemelding-api

---
+++ proxies/sykemelding-api-proxy/src/main/java/no/nav/testnav/proxies/sykemeldingapiproxy/SykemeldingApiProxyApplicationStarter.java
@@ -0,0 +1,36 @@
+package no.nav.testnav.proxies.sykemeldingapiproxy;
+
+import no.nav.testnav.libs.reactivecore.config.CoreConfig;
+import no.nav.testnav.libs.reactiveproxy.config.DevConfig;
+import no.nav.testnav.libs.reactiveproxy.config.SecurityConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+
+@Import({
+        CoreConfig.class,
+        DevConfig.class,
+        SecurityConfig.class
+})
+@SpringBootApplication
+public class SykemeldingApiProxyApplicationStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(SykemeldingApiProxyApplicationStarter.class, args);
+    }
+
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(spec -> spec
+                        .path("/**")
+                        .uri("https://testnorge-sykemelding-api.dev.intern.nav.no/")
+
+                )
+                .build();
+    }
+
+}
