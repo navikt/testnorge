da22d159710da1d8fe0bac9af59cf8a08474aa7c
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-10-13 16:12:29 +0200

    testing

--- libs/reactive-security/src/main/java/no/nav/testnav/libs/reactivesecurity/domain/AccessToken.java
+++ libs/reactive-security/src/main/java/no/nav/testnav/libs/reactivesecurity/domain/AccessToken.java
@@ -1,10 +1,12 @@
 package no.nav.testnav.libs.reactivesecurity.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class AccessToken {
 
+    @JsonProperty
     private final String access_token;
 
     public AccessToken(String token) {
--- libs/reactive-security/src/main/java/no/nav/testnav/libs/reactivesecurity/domain/tokenx/v1/WellKnown.java
+++ libs/reactive-security/src/main/java/no/nav/testnav/libs/reactivesecurity/domain/tokenx/v1/WellKnown.java
@@ -1,6 +1,7 @@
 package no.nav.testnav.libs.reactivesecurity.domain.tokenx.v1;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
@@ -15,6 +16,9 @@ import lombok.Value;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class WellKnown {
 
+    @JsonProperty
     String token_endpoint;
+
+    @JsonProperty
     String end_session_endpoint;
 }
