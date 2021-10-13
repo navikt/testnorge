c42801b1c444b546aa31a140938243f45a5d2109
stigus <stian.gustavsson@nav.no>
2021-10-13 15:23:57 +0200

    SykemeldingApi mulighet for både sykemelding og synt-sykemelding

--- proxies/sykemelding-api-proxy/src/main/java/no/nav/testnav/proxies/sykemeldingapiproxy/SykemeldingApiProxyApplicationStarter.java
+++ proxies/sykemelding-api-proxy/src/main/java/no/nav/testnav/proxies/sykemeldingapiproxy/SykemeldingApiProxyApplicationStarter.java
@@ -5,10 +5,15 @@ import no.nav.testnav.libs.reactiveproxy.config.DevConfig;
 import no.nav.testnav.libs.reactiveproxy.config.SecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.Route;
 import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.Buildable;
+import org.springframework.cloud.gateway.route.builder.PredicateSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+
+import java.util.function.Function;
 
 @Import({
         CoreConfig.class,
@@ -24,13 +29,20 @@ public class SykemeldingApiProxyApplicationStarter {
 
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route(spec -> spec
-                        .path("/**")
-                        .uri("https://testnorge-sykemelding-api.dev.intern.nav.no/")
 
-                )
+        return builder
+                .routes()
+                .route(createRoute("sykemelding", "https://testnorge-sykemelding-api.dev.intern.nav.no"))
+                .route(createRoute("synt-sykemelding", "https://testnorge-synt-sykemelding-api.dev.intern.nav.no"))
                 .build();
+    }
+
+    private Function<PredicateSpec, Buildable<Route>> createRoute(String segment, String host) {
+        return spec -> spec
+                .path("/" + segment + "/**")
+                .filters(filterSpec -> filterSpec
+                        .rewritePath("/" + segment + "/(?<segment>.*)", "/${segment}")
+                ).uri(host);
     }
 
 }
