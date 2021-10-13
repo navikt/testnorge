58fc59fef42892fc23bfc0e90bd0f96e18a04478
Aileen Hay <aileen.hay@nav.no>
2021-10-13 09:25:17 +0200

    Slått sammen core og local

--- apps/testnorge-nav-endringsmeldinger/src/test/java/no/nav/registre/endringsmeldinger/service/EndringsmeldingServiceTest.java
+++ apps/testnorge-nav-endringsmeldinger/src/test/java/no/nav/registre/endringsmeldinger/service/EndringsmeldingServiceTest.java
@@ -27,7 +27,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
 
 import no.nav.registre.endringsmeldinger.consumer.rs.NavEndringsmeldingerSyntetisererenConsumer;
 import no.nav.registre.endringsmeldinger.consumer.rs.TpsfConsumer;
