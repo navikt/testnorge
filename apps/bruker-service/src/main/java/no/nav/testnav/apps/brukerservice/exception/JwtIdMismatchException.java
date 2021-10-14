f64b0ebd91ce5ffff942ed3dbae857e81739bb6e
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-10-14 09:54:46 +0200

    fix

--- apps/bruker-service/src/main/java/no/nav/testnav/apps/brukerservice/exception/JwtIdMismatchException.java
+++ apps/bruker-service/src/main/java/no/nav/testnav/apps/brukerservice/exception/JwtIdMismatchException.java
@@ -2,6 +2,6 @@ package no.nav.testnav.apps.brukerservice.exception;
 
 public class JwtIdMismatchException extends RuntimeException {
     public JwtIdMismatchException(String id, String generatedId) {
-        super("Det er en missmatch mellom id " + id + " og generert id " + generatedId + ".");
+        super("Det er en mismatch mellom id " + id + " og generert id " + generatedId + ".");
     }
 }
