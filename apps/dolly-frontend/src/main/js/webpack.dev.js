2ec1d68aa2980370173f2e0dd5c983312f130dda
Aileen Hay <aileen.hay@nav.no>
2021-10-15 10:43:21 +0200

    Lagt til personOrgTilgangservice støtte backend

--- apps/dolly-frontend/src/main/js/webpack.dev.js
+++ apps/dolly-frontend/src/main/js/webpack.dev.js
@@ -190,6 +190,11 @@ module.exports = (env) =>
 					changeOrigin: true,
 					secure: false,
 				},
+				'/testnav-person-organisasjon-tilgang-service/api': {
+					target: env.backend,
+					changeOrigin: true,
+					secure: false,
+				},
 			},
 		},
 		plugins: [
