970a2f9dddeac59f49266af086af8c2d21219ab1
Aileen Hay <aileen.hay@nav.no>
2021-10-15 10:43:21 +0200

    Lagt til personOrgTilgangservice støtte backend

--- apps/dolly-frontend/src/main/js/src/service/services/personOrganisasjonTilgang/PersonOrganisasjonTilgangService.tsx
+++ apps/dolly-frontend/src/main/js/src/service/services/personOrganisasjonTilgang/PersonOrganisasjonTilgangService.tsx
@@ -2,8 +2,15 @@ import Request from '~/service/services/Request'
 
 const personOrgTilgangServiceUrl =
 	'/testnav-person-organisasjon-tilgang-service/api/v1/person/organisasjoner'
+const orgServiceUrl = '/testnav-person-organisasjon-service/api/v1/person/organisasjoner'
 
 export default {
+	getOrganisasjonInfo(orgnummer: string, miljoe: string) {
+		return Request.post(`${orgServiceUrl}`, { miljo: miljoe ? miljoe : 'q1' }).then((response) => {
+			if (response != null) return response
+		})
+	},
+
 	getOrganisasjoner() {
 		return Request.get(`${personOrgTilgangServiceUrl}`).then((response) => {
 			return response
