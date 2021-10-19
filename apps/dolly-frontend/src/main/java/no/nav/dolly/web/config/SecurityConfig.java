65147b1a5c921ce8d398bbeefffc0d038659da70
Aileen Hay <aileen.hay@nav.no>
2021-10-19 09:56:40 +0200

    Lagt til sikkerhet for bankid

--- apps/dolly-frontend/src/main/java/no/nav/dolly/web/config/SecurityConfig.java
+++ apps/dolly-frontend/src/main/java/no/nav/dolly/web/config/SecurityConfig.java
@@ -1,21 +1,47 @@
 package no.nav.dolly.web.config;
 
-import lombok.extern.slf4j.Slf4j;
-import no.nav.testnav.libs.reactivesessionsecurity.handler.LogoutSuccessHandler;
+import com.nimbusds.jose.jwk.JWK;
 
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
+import no.nav.testnav.libs.reactivesessionsecurity.handler.LogoutSuccessHandler;
+import no.nav.testnav.libs.reactivesessionsecurity.manager.AuthorizationCodeReactiveAuthenticationManger;
+import no.nav.testnav.libs.reactivesessionsecurity.resolver.logut.IdportenOcidLogoutUrlResolver;
+
+
 @Slf4j
+@Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
+    private final String jwk;
+    private final String wellKnownUrl;
+    private final String postLogoutRedirectUri;
+
+    public SecurityConfig(
+            @Value("${spring.security.oauth2.client.provider.idporten.issuer-uri}.well-known/openid-configuration") String wellKnownUrl,
+            @Value("${spring.security.oauth2.client.registration.idporten.post-logout-redirect-uri}") String postLogoutRedirectUri,
+            @Value("${IDPORTEN_CLIENT_JWK}") String jwk
+    ) {
+        this.jwk = jwk;
+        this.wellKnownUrl = wellKnownUrl;
+        this.postLogoutRedirectUri = postLogoutRedirectUri;
+    }
+
+    @SneakyThrows
     @Bean
     public SecurityWebFilterChain configure(ServerHttpSecurity http) {
-
+        var authenticationManger = new AuthorizationCodeReactiveAuthenticationManger(JWK.parse(jwk));
         var logoutSuccessHandler = new LogoutSuccessHandler();
+        logoutSuccessHandler.applyOn("idporten", new IdportenOcidLogoutUrlResolver(wellKnownUrl, postLogoutRedirectUri));
 
         return http.cors()
                 .and().csrf().disable()
@@ -26,57 +52,17 @@ public class SecurityConfig {
                         "/oauth2/callback",
                         "/favicon.ico",
                         "/login",
+                        "/bruker",
                         "/main.*.css",
                         "/bundle.*.js",
                         "/*.png"
                 ).permitAll()
                 .anyExchange().authenticated()
-                .and().oauth2Login()
-                .and().formLogin().loginPage("/login")
+                .and().oauth2Login(oAuth2LoginSpec -> oAuth2LoginSpec.authenticationManager(authenticationManger))
+                .formLogin().loginPage("/login")
                 .and().logout(logoutSpec -> logoutSpec
                         .logoutUrl("/logout")
-                        .logoutSuccessHandler(logoutSuccessHandler)
-                ).build();
+                        .logoutSuccessHandler(logoutSuccessHandler))
+                .build();
     }
-
-    //    TODO Use when idporten is ready
-    //    private final String jwk;
-    //    private final String wellKnownUrl;
-    //    private final String postLogoutRedirectUri;
-    //
-    //    public SecurityConfig(
-    //            @Value("${spring.security.oauth2.client.provider.idporten.issuer-uri}.well-known/openid-configuration") String wellKnownUrl,
-    //            @Value("${spring.security.oauth2.client.registration.idporten.post-logout-redirect-uri}") String postLogoutRedirectUri,
-    //            @Value("${IDPORTEN_CLIENT_JWK}") String jwk
-    //    ) {
-    //        this.jwk = jwk;
-    //        this.wellKnownUrl = wellKnownUrl;
-    //        this.postLogoutRedirectUri = postLogoutRedirectUri;
-    //    }
-    //    @SneakyThrows
-    //    @Bean
-    //    public SecurityWebFilterChain configure(ServerHttpSecurity http) {
-    //        var authenticationManger = new AuthorizationCodeReactiveAuthenticationManger(JWK.parse(jwk));
-    //        var logoutSuccessHandler = new LogoutSuccessHandler();
-    //        logoutSuccessHandler.applyOn("idporten", new IdportenOcidLogoutUrlResolver(wellKnownUrl, postLogoutRedirectUri));
-    //
-    //        return http.cors()
-    //                .and().csrf().disable()
-    //                .authorizeExchange()
-    //                .pathMatchers(
-    //                        "/internal/isReady",
-    //                        "/internal/isAlive",
-    //                        "/favicon.ico",
-    //                        "/login",
-    //                        "/main.*.css",
-    //                        "/bundle.*.js"
-    //                ).permitAll()
-    //                .anyExchange().authenticated()
-    //                .and().oauth2Login(oAuth2LoginSpec -> oAuth2LoginSpec.authenticationManager(authenticationManger))
-    //                .formLogin().loginPage("/login")
-    //                .and().logout(logoutSpec -> logoutSpec
-    //                        .logoutUrl("/logout")
-    //                        .logoutSuccessHandler(logoutSuccessHandler))
-    //                .build();
-    //    }
 }
