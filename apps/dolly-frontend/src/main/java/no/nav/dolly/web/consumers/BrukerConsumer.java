444d28d798e7ae9f0725d38d7f8b2f4ac6970923
Aileen Hay <aileen.hay@nav.no>
2021-10-20 12:59:15 +0200

    Oppdatert sessioncontroller

--- apps/dolly-frontend/src/main/java/no/nav/dolly/web/consumers/BrukerConsumer.java
+++ apps/dolly-frontend/src/main/java/no/nav/dolly/web/consumers/BrukerConsumer.java
@@ -1,2 +1,41 @@
-package no.nav.dolly.web.consumers;public class BrukerConsumer {
+package no.nav.dolly.web.consumers;
+
+import lombok.extern.slf4j.Slf4j;
+import no.nav.dolly.web.consumers.command.GetBrukerCommand;
+import no.nav.dolly.web.consumers.command.GetTokenCommand;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import no.nav.dolly.web.consumers.dto.BrukerDTO;
+import no.nav.testnav.libs.reactivesessionsecurity.exchange.user.TestnavBrukerServiceProperties;
+import no.nav.testnav.libs.reactivesessionsecurity.exchange.TokenExchange;
+
+@Slf4j
+@Component
+public class BrukerConsumer {
+    private final WebClient webClient;
+    private final TestnavBrukerServiceProperties serviceProperties;
+    private final TokenExchange tokenExchange;
+
+    public BrukerConsumer(TestnavBrukerServiceProperties serviceProperties, TokenExchange tokenExchange) {
+        this.serviceProperties = serviceProperties;
+        this.tokenExchange = tokenExchange;
+        this.webClient = WebClient
+                .builder()
+                .baseUrl(serviceProperties.getUrl())
+                .build();
+    }
+
+    public Mono<BrukerDTO> getBruker(String orgnummer, ServerWebExchange serverWebExchange) {
+        return tokenExchange.generateToken(serviceProperties, serverWebExchange)
+                .flatMap(accessToken -> new GetBrukerCommand(webClient, accessToken.getTokenValue(), orgnummer).call());
+    }
+
+    public Mono<String> getToken(String id, ServerWebExchange serverWebExchange){
+        return tokenExchange.generateToken(serviceProperties, serverWebExchange)
+                .flatMap(accessToken -> new GetTokenCommand(webClient, accessToken.getTokenValue(), id).call());
+    }
+
 }
--- apps/dolly-frontend/src/main/java/no/nav/dolly/web/consumers/dto/OrganisasjonDTO.java
+++ apps/dolly-frontend/src/main/java/no/nav/dolly/web/consumers/dto/OrganisasjonDTO.java
@@ -1,2 +1,12 @@
-package no.nav.dolly.web.consumers.dto;public class OrganisasjonDTO {
+package no.nav.dolly.web.consumers.dto;
+
+import java.time.LocalDateTime;
+
+public record OrganisasjonDTO(
+        String navn,
+        String orgnisasjonsnummer,
+        String orgnisasjonsfrom,
+        LocalDateTime gyldigTil
+) {
 }
+
