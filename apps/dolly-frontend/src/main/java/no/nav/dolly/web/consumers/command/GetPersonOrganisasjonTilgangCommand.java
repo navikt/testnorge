4725a0791c7e89f8777b846a836f8ede98101709
Aileen Hay <aileen.hay@nav.no>
2021-10-20 12:59:15 +0200

    Oppdatert sessioncontroller

--- apps/dolly-frontend/src/main/java/no/nav/dolly/web/consumers/command/GetPersonOrganisasjonTilgangCommand.java
+++ apps/dolly-frontend/src/main/java/no/nav/dolly/web/consumers/command/GetPersonOrganisasjonTilgangCommand.java
@@ -1,2 +1,29 @@
-package no.nav.dolly.web.consumers.command;public class GetPersonOrganisasjonTilgangCommand {
+package no.nav.dolly.web.consumers.command;
+
+import lombok.RequiredArgsConstructor;
+import no.nav.dolly.web.consumers.dto.OrganisasjonDTO;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.concurrent.Callable;
+
+
+@RequiredArgsConstructor
+public class GetPersonOrganisasjonTilgangCommand implements Callable<Mono<OrganisasjonDTO>> {
+    private final WebClient webClient;
+    private final String token;
+    private final String organisasjonsnummer;
+
+    @Override
+    public Mono<OrganisasjonDTO> call() {
+        return webClient
+                .get()
+                .uri(builder -> builder.path("/api/v1/person/organisasjoner/{organisasjonsnummer}").build(organisasjonsnummer))
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+                .retrieve()
+                .bodyToMono(OrganisasjonDTO.class);
+    }
 }
+
+
--- apps/dolly-frontend/src/main/java/no/nav/dolly/web/consumers/dto/BrukerDTO.java
+++ apps/dolly-frontend/src/main/java/no/nav/dolly/web/consumers/dto/BrukerDTO.java
@@ -1,2 +1,11 @@
-package no.nav.dolly.web.consumers.dto;public class BrukerDTO {
+package no.nav.dolly.web.consumers.dto;
+
+import java.time.LocalDateTime;
+
+public record BrukerDTO(
+        String id,
+        String brukernavn,
+        String organisasjonsnummer,
+        LocalDateTime opprettet,
+        LocalDateTime sistInnlogget) {
 }
