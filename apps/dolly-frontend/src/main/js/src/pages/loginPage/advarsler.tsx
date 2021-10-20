5557ada7df0b97c44cdf09cc413891e350107de0
Aileen Hay <aileen.hay@nav.no>
2021-10-20 09:05:07 +0200

    Ryddet

--- apps/dolly-frontend/src/main/js/src/pages/loginPage/advarsler.tsx
+++
@@ -1,7 +0,0 @@
-export const Advarsler = {
-	organisation_error:
-		'Du ble logget inn med BankId, men er ikke tilknyttet noen organisasjoner som har ' +
-		'tilgang til Dolly. Ta kontakt med administrator i din organisasjon dersom du ønsker tilgang' +
-		', eller velg en annen innloggingsmetode.',
-	unknown_error: 'Ukjent feil oppsto. Ta kontakt med Dolly hvis denne feilen vedvarer.',
-}
