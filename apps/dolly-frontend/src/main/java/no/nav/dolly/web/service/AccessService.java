dc29beeae4bc5c14eded1e56280e80211e66fe4c
Aileen Hay <aileen.hay@nav.no>
2021-10-20 12:59:15 +0200

    Oppdatert sessioncontroller

--- apps/dolly-frontend/src/main/java/no/nav/dolly/web/service/AccessService.java
+++ apps/dolly-frontend/src/main/java/no/nav/dolly/web/service/AccessService.java
@@ -1,2 +1,18 @@
-package no.nav.dolly.web.service;public class AccessService {
+package no.nav.dolly.web.service;
+
+import lombok.RequiredArgsConstructor;
+import no.nav.dolly.web.consumers.PersonOrganisasjonTilgangConsumer;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+
+@Service
+@RequiredArgsConstructor
+public class AccessService {
+    private final PersonOrganisasjonTilgangConsumer personOrganisasjonTilgangConsumer;
+
+    public Mono<Boolean> hasAccess(String organisasjonsnummer, ServerWebExchange serverWebExchange) {
+        return personOrganisasjonTilgangConsumer.hasAccess(organisasjonsnummer, serverWebExchange);
+    }
 }
