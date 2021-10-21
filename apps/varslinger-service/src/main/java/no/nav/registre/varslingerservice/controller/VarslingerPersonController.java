345297a86aea949eed8be8aa28a0b73e4e65fab9
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-10-21 11:55:07 +0200

    stash

--- apps/varslinger-service/src/main/java/no/nav/registre/varslingerservice/controller/VarslingerPersonController.java
+++ apps/varslinger-service/src/main/java/no/nav/registre/varslingerservice/controller/VarslingerPersonController.java
@@ -7,18 +7,13 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import no.nav.registre.varslingerservice.adapter.PersonVarslingAdapter;
-import no.nav.registre.varslingerservice.adapter.VarslingerAdapter;
-import no.nav.registre.varslingerservice.domain.Varsling;
-import no.nav.testnav.libs.dto.varslingerapi.v1.VarslingDTO;
 
 @RestController
 @RequestMapping("/api/v1/varslinger/person/ids")
