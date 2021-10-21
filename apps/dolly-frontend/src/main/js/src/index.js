b17c331c750d4ddda8c9169e318c2a9e44e63dd8
Aileen Hay <aileen.hay@nav.no>
2021-10-21 10:43:59 +0200

    Fjernet connector

--- apps/dolly-frontend/src/main/js/src/index.js
+++ apps/dolly-frontend/src/main/js/src/index.js
@@ -15,7 +15,7 @@ import { ErrorBoundary } from '~/components/ui/appError/ErrorBoundary'
 import Logger from './logger'
 import { v4 as uuid } from 'uuid'
 import LoginPage from '~/pages/loginPage'
-import BrukerPageConnector from '~/pages/brukerPage'
+import BrukerPage from '~/pages/brukerPage'
 
 window.uuid = uuid()
 window.onerror = (message) => {
@@ -38,7 +38,7 @@ render(
 			<ErrorBoundary>
 				<Switch>
 					<Route path="/bruker">
-						<BrukerPageConnector />
+						<BrukerPage />
 					</Route>
 					<Route path="/login">
 						<LoginPage />
--- apps/dolly-frontend/src/main/js/src/pages/brukerPage/BrukerPage.tsx
+++ apps/dolly-frontend/src/main/js/src/pages/brukerPage/BrukerPage.tsx
@@ -6,19 +6,20 @@ import { HalloweenBackground } from '~/components/ui/background/Background'
 
 type BrukerPageProps = {
 	brukerData: any
+	brukerProfil: any
 }
 
-export default ({ brukerData }: BrukerPageProps) => {
-	if (brukerData !== null) {
-		window.location.replace(window.location.protocol + '//' + window.location.host)
-	}
+export default ({ brukerData, brukerProfil }: BrukerPageProps) => {
+	console.log(brukerData)
+	console.log(brukerProfil)
+	// if (brukerData !== null) {
+	// 	window.location.replace(window.location.protocol + '//' + window.location.host)
+	// }
 
 	return (
 		<React.Fragment>
 			<BlankHeader />
-			<HalloweenBackground>
-				<BrukerModal />
-			</HalloweenBackground>
+			<HalloweenBackground>{/*<BrukerModal />*/}</HalloweenBackground>
 		</React.Fragment>
 	)
 }
