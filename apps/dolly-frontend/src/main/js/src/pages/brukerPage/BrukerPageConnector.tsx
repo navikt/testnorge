155c91987771a8acde98a04ee10f1b275eb2257d
Aileen Hay <aileen.hay@nav.no>
2021-10-21 10:43:59 +0200

    Fjernet connector

--- apps/dolly-frontend/src/main/js/src/pages/brukerPage/BrukerPageConnector.tsx
+++
@@ -1,8 +0,0 @@
-import { connect } from 'react-redux'
-import BrukerPage from '~/pages/brukerPage/BrukerPage'
-
-const mapStateToProps = (state: any, ownProps: any) => ({
-	brukerData: state.bruker.brukerData,
-})
-
-export default connect(mapStateToProps)(BrukerPage)
