c950e78866dc88ea2ad32838c98b83d869ccb052
kristenhaerum <kristen.herum@nav.no>
2021-10-21 08:09:48 +0200

    Rydding ...

--- apps/pdl-forvalter/src/main/java/no/nav/pdl/forvalter/dto/Paginering.java
+++ apps/pdl-forvalter/src/main/java/no/nav/pdl/forvalter/dto/Paginering.java
@@ -1,6 +1,5 @@
 package no.nav.pdl.forvalter.dto;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,8 +11,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Paginering {
 
-    @Schema(defaultValue = "0")
     private Integer sidenummer;
-    @Schema(defaultValue = "10")
     private Integer sidestoerrelse;
 }
