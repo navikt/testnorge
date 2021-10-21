99f55a36736521fc79bf173a0bd6e3e955a4f9cd
stigus <stian.gustavsson@nav.no>
2021-10-21 12:59:46 +0200

    Logging

--- apps/testnorge-aareg/src/main/java/no/nav/registre/aareg/util/JsonDateDeserializer.java
+++ apps/testnorge-aareg/src/main/java/no/nav/registre/aareg/util/JsonDateDeserializer.java
@@ -3,13 +3,17 @@ package no.nav.registre.aareg.util;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
 
+@Slf4j
 public class JsonDateDeserializer extends JsonDeserializer<LocalDateTime> {
 
-    @Override public LocalDateTime deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
+    @Override
+    public LocalDateTime deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
+        log.info("Prøver å parse: {}", jp.getValueAsString());
         return LocalDateTime.parse(jp.getValueAsString());
     }
 }
