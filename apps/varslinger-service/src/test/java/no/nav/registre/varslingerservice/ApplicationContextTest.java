2dfd0eaf8e70dc154702a7c56e3826d6b94eff91
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-10-21 10:45:11 +0200

    fix

--- apps/varslinger-service/src/test/java/no/nav/registre/varslingerservice/ApplicationContextTest.java
+++ apps/varslinger-service/src/test/java/no/nav/registre/varslingerservice/ApplicationContextTest.java
@@ -1,15 +1,8 @@
 package no.nav.registre.varslingerservice;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
 
 
 @SpringBootTest
