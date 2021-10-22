4033451e2b01e010251faad709a2a486d9aefdc4
Aileen Hay <aileen.hay@nav.no>
2021-10-22 08:25:58 +0200

    Liten fiks i sessionservice

--- apps/dolly-frontend/src/main/js/src/service/services/session/SessionService.ts
+++ apps/dolly-frontend/src/main/js/src/service/services/session/SessionService.ts
@@ -2,9 +2,9 @@ import Api from '~/api'
 
 export default {
 	addToSession(orgnummer: string) {
-		Api.fetch(`/api/v1/session/user?organisasjonsnummer=${orgnummer}`, { method: 'PUT' })
+		Api.fetch(`/session/user?organisasjonsnummer=${orgnummer}`, { method: 'PUT' })
 	},
 	clear() {
-		Api.fetch('/api/v1/session/user', { method: 'DELETE' })
+		Api.fetch('/session/user', { method: 'DELETE' })
 	},
 }
