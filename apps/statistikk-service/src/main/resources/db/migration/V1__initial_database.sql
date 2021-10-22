b7b7bc97f4eb2638dd3ce51d22c49b7135f194f3
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-10-22 08:56:18 +0200

    Slett testnav-statistikk-service (#898)

--- apps/statistikk-service/src/main/resources/db/migration/V1__initial_database.sql
+++
@@ -1,9 +0,0 @@
-create table statistikk
-(
-    type        VARCHAR(100) PRIMARY KEY,
-    description VARCHAR(256),
-    value       DECIMAL      NOT NULL,
-    value_type  VARCHAR(100) NOT NULL,
-    created_at  TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    updated_at  TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-);
