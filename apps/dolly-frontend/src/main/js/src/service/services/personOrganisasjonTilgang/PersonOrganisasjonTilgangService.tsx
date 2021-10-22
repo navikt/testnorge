74f20cd7f1f10dd6adbd7fe95c6f8c598b7ca31e
Aileen Hay <aileen.hay@nav.no>
2021-10-22 13:38:34 +0200

    Fjernet ubrukte metoder

--- apps/dolly-frontend/src/main/js/src/service/services/personOrganisasjonTilgang/PersonOrganisasjonTilgangService.tsx
+++ apps/dolly-frontend/src/main/js/src/service/services/personOrganisasjonTilgang/PersonOrganisasjonTilgangService.tsx
@@ -2,23 +2,10 @@ import Request from '~/service/services/Request'
 
 const personOrgTilgangServiceUrl =
 	'/testnav-person-organisasjon-tilgang-service/api/v1/person/organisasjoner'
-const orgServiceUrl = '/testnav-person-organisasjon-service/api/v1/person/organisasjoner'
 
 export default {
-	getOrganisasjonInfo(orgnummer: string, miljoe: string) {
-		return Request.post(`${orgServiceUrl}`, { miljo: miljoe ? miljoe : 'q1' }).then((response) => {
-			if (response != null) return response
-		})
-	},
-
 	getOrganisasjoner() {
 		return Request.get(`${personOrgTilgangServiceUrl}`).then((response) => {
-			return response
-		})
-	},
-
-	getOrganisasjon(orgnr: string) {
-		return Request.get(`${personOrgTilgangServiceUrl}/${orgnr}`).then((response) => {
 			return response
 		})
 	},
