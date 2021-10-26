add40fa39cafa8b33091c7a9fff3048bc30559c7
Aileen Hay <aileen.hay@nav.no>
2021-10-26 08:51:26 +0200

    Fikset imports etter rebase

---
+++ apps/dolly-frontend/src/main/java/no/nav/dolly/web/credentials/TestnavBrukerServiceProperties.java
@@ -0,0 +1,11 @@
+package no.nav.dolly.web.credentials;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import no.nav.testnav.libs.securitycore.domain.ServerProperties;
+
+@Configuration
+@ConfigurationProperties(prefix = "consumers.testnav-bruker-service")
+public class TestnavBrukerServiceProperties extends ServerProperties {
+}
