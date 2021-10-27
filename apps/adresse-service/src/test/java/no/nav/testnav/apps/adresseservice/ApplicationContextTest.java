7e016ed0e71bb4e789bd153fe0a95c7c54c13270
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-10-27 09:31:31 +0200

    fix

--- apps/adresse-service/src/test/java/no/nav/testnav/apps/adresseservice/ApplicationContextTest.java
+++ apps/adresse-service/src/test/java/no/nav/testnav/apps/adresseservice/ApplicationContextTest.java
@@ -2,12 +2,19 @@ package no.nav.testnav.apps.adresseservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class ApplicationContextTest {
+
+    @MockBean
+    public JwtDecoder jwtDecoder;
+
     @Test
+    @SuppressWarnings("java:S2699")
     void load_app_context() {
     }
 }
