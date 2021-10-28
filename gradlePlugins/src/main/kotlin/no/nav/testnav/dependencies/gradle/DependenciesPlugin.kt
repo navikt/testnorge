b44b2c9f2c3290dc01faad01a5124a8b02f4e696
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-10-28 10:51:33 +0200

    Bump spring (#917)

--- gradlePlugins/src/main/kotlin/no/nav/testnav/dependencies/gradle/DependenciesPlugin.kt
+++ gradlePlugins/src/main/kotlin/no/nav/testnav/dependencies/gradle/DependenciesPlugin.kt
@@ -9,12 +9,13 @@ class DependenciesPlugin : Plugin<Project> {
 
     companion object {
         val lombokVersion = "1.18.20"
-        val springBoot = "2.5.5"
+        val springBoot = "2.5.6"
         val springCloud = "2020.0.4"
-        val springSession = "2021.0.2"
+        val springSession = "2021.0.3"
         val logstashLogbackEncoder = "6.6"
-        val springdocOpenapi = "1.5.9"
-        val jackson = "2.11.2"
+        val springdocOpenapi = "1.5.12"
+        val jackson = "2.13.0"
         val orikaVersion = "1.5.4"
+        val validationApi = "2.0.1.Final"
     }
 }
