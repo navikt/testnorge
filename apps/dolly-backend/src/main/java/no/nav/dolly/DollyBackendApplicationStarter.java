5804cf66979ba968ecc7d149a92f69da83808330
stigus <stian.gustavsson@nav.no>
2021-10-29 16:43:15 +0200

    Testing

--- apps/dolly-backend/src/main/java/no/nav/dolly/DollyBackendApplicationStarter.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/DollyBackendApplicationStarter.java
@@ -1,11 +1,12 @@
 package no.nav.dolly;
 
 import lombok.extern.slf4j.Slf4j;
-import no.nav.testnav.libs.servletcore.util.VaultUtil;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @EnableAutoConfiguration
 @Slf4j
@@ -14,13 +15,16 @@ public class DollyBackendApplicationStarter {
 
     public static void main(String[] args) {
 
+        Map<String, Object> properties = new HashMap<>();
         if (deployEnvironments.stream().anyMatch(env -> env.equals(System.getProperty("spring.profiles.active")))) {
-            log.info("Starter lesing av Vault token ...");
-            VaultUtil.initCloudVaultToken();
+            properties = PropertyReader.builder()
+                    .readSecret("spring.cloud.vault.token", "/var/run/secrets/nais.io/vault/vault_token")
+                    .build();
         }
 
         new SpringApplicationBuilder()
                 .sources(ApplicationConfig.class)
+                .properties(properties)
                 .run(args);
     }
 }
