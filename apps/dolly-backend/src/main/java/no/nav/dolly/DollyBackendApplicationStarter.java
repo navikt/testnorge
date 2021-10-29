bde5d6e1e23e2ecdcfc3059420a2f5637538ad85
stigus <stian.gustavsson@nav.no>
2021-10-29 16:33:02 +0200

    Fix for spring token init på deploy

--- apps/dolly-backend/src/main/java/no/nav/dolly/DollyBackendApplicationStarter.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/DollyBackendApplicationStarter.java
@@ -1,12 +1,26 @@
 package no.nav.dolly;
 
-import org.springframework.boot.SpringApplication;
+import lombok.extern.slf4j.Slf4j;
+import no.nav.testnav.libs.servletcore.util.VaultUtil;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+import java.util.List;
 
 @EnableAutoConfiguration
+@Slf4j
 public class DollyBackendApplicationStarter {
+    private static final List<String> deployEnvironments = List.of("prod", "dev");
+
     public static void main(String[] args) {
 
-        SpringApplication.run(ApplicationConfig.class, args);
+        if (deployEnvironments.stream().anyMatch(env -> env.equals(System.getProperty("spring.profiles.active")))) {
+            log.info("Starter lesing av Vault token ...");
+            VaultUtil.initCloudVaultToken();
+        }
+
+        new SpringApplicationBuilder()
+                .sources(ApplicationConfig.class)
+                .run(args);
     }
 }
