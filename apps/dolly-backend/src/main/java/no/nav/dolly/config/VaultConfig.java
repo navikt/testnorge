664bf69a86bb73c3a3d836e1f2ab71eedb58d8c7
stigus <stian.gustavsson@nav.no>
2021-10-29 13:34:37 +0200

    Egen VaultConfig for lokal og dev,prod

---
+++ apps/dolly-backend/src/main/java/no/nav/dolly/config/VaultConfig.java
@@ -0,0 +1,26 @@
+package no.nav.dolly.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.vault.authentication.ClientAuthentication;
+import org.springframework.vault.authentication.TokenAuthentication;
+import org.springframework.vault.client.VaultEndpoint;
+import org.springframework.vault.config.AbstractVaultConfiguration;
+
+@Slf4j
+@Configuration
+@Profile({ "prod", "dev" })
+@RequiredArgsConstructor
+public class VaultConfig extends AbstractVaultConfiguration {
+    @Override
+    public VaultEndpoint vaultEndpoint() {
+        return VaultEndpoint.create("vault.adeo.no", 443);
+    }
+
+    @Override
+    public ClientAuthentication clientAuthentication() {
+        return new TokenAuthentication(System.getProperty("spring.cloud.vault.token"));
+    }
+}
