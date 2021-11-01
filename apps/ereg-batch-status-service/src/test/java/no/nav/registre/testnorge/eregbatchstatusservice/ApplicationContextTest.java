f39ba199edd0503d961066eb436daaab8d7826f0
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-11-02 00:44:23 +0100

    Add app to dependabot

--- apps/ereg-batch-status-service/src/test/java/no/nav/registre/testnorge/eregbatchstatusservice/ApplicationContextTest.java
+++ apps/ereg-batch-status-service/src/test/java/no/nav/registre/testnorge/eregbatchstatusservice/ApplicationContextTest.java
@@ -1,40 +1,20 @@
 package no.nav.registre.testnorge.eregbatchstatusservice;
 
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.ActiveProfiles;
 
-import java.util.Collections;
-import java.util.List;
-
-import no.nav.testnav.libs.servletsecurity.decoder.MultipleIssuersJwtDecoder;
-import no.nav.testnav.libs.servletsecurity.properties.AzureAdResourceServerProperties;
-import no.nav.testnav.libs.servletsecurity.properties.ResourceServerProperties;
-
-
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureWireMock(port = 0)
-@TestPropertySource(locations = "classpath:application-test.properties")
-public class ApplicationContextTest {
+@SpringBootTest
+@ActiveProfiles("test")
+class ApplicationContextTest {
 
     @MockBean
     public JwtDecoder jwtDecoder;
 
-
-    /**
-     * Used to test app runtime dependency and properties
-     */
     @Test
     @SuppressWarnings("java:S2699")
-    public void load_app_context() {
+    void load_app_context() {
     }
 }
