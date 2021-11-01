8415979064a44ca9b7324cdb78cc9ef6be00b099
stigus <stian.gustavsson@nav.no>
2021-11-01 09:41:26 +0100

    Fiks?!

--- apps/dolly-backend/src/main/java/no/nav/dolly/DollyBackendApplicationStarter.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/DollyBackendApplicationStarter.java
@@ -5,18 +5,16 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @EnableAutoConfiguration
 @Slf4j
 public class DollyBackendApplicationStarter {
-    private static final List<String> deployEnvironments = List.of("prod", "dev");
 
     public static void main(String[] args) {
 
         Map<String, Object> properties = new HashMap<>();
-        if (deployEnvironments.stream().anyMatch(env -> env.equals(System.getProperty("spring.profiles.active")))) {
+        if (!"local".equals(System.getProperty("spring.profiles.active"))) {
             log.info("Profile er i if: {}", System.getProperty("spring.profiles.active"));
             properties = PropertyReader.builder()
                     .readSecret("spring.cloud.vault.token", "/var/run/secrets/nais.io/vault/vault_token")
