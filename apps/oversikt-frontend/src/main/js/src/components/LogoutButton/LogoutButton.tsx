651dd20e5c7f948c7a41acdd4a1bb4a7e6e3176a
Aileen Hay <aileen.hay@nav.no>
2021-11-02 08:31:09 +0100

    Kode rydding

--- apps/oversikt-frontend/src/main/js/src/components/LogoutButton/LogoutButton.tsx
+++ apps/oversikt-frontend/src/main/js/src/components/LogoutButton/LogoutButton.tsx
@@ -7,24 +7,16 @@ import './LogoutButton.less'
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 	onClick: () => void
-	iconSize?: number
-	loading?: boolean
 }
 
-export default function LogoutButton({ onClick, loading = false, disabled }: ButtonProps) {
+export default function LogoutButton({ onClick }: ButtonProps) {
 	const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
 		event.stopPropagation()
 		onClick()
 	}
 
 	return (
-		<button
-			type="button"
-			className="logout-button"
-			onClick={handleClick}
-			disabled={disabled || loading}
-			title="Logg ut"
-		>
+		<button type="button" className="logout-button" onClick={handleClick} title="Logg ut">
 			<SVG src={Logout} className="svg-icon" />
 		</button>
 	)
