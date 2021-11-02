b616aeed7db9c4b44a6de6e154bff6b6f1b23f0e
stigus <stian.gustavsson@nav.no>
2021-11-02 10:41:32 +0100

    Lagt til PDL endringer

--- apps/dolly-backend/src/main/java/no/nav/dolly/bestilling/pdldata/PdlDataConsumer.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/bestilling/pdldata/PdlDataConsumer.java
@@ -1,14 +1,18 @@
 package no.nav.dolly.bestilling.pdldata;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import no.nav.dolly.bestilling.pdldata.command.PdlDataCheckIdentCommand;
 import no.nav.dolly.bestilling.pdldata.command.PdlDataOppdateringCommand;
+import no.nav.dolly.bestilling.pdldata.command.PdlDataOpprettingCommand;
 import no.nav.dolly.bestilling.pdldata.command.PdlDataOrdreCommand;
 import no.nav.dolly.bestilling.pdldata.command.PdlDataSlettCommand;
 import no.nav.dolly.config.credentials.PdlDataForvalterProperties;
 import no.nav.dolly.metrics.Timed;
 import no.nav.dolly.util.CheckAliveUtil;
+import no.nav.dolly.util.JacksonExchangeStrategyUtil;
+import no.nav.testnav.libs.dto.pdlforvalter.v1.AvailibilityResponseDTO;
+import no.nav.testnav.libs.dto.pdlforvalter.v1.BestillingRequestDTO;
 import no.nav.testnav.libs.dto.pdlforvalter.v1.PersonUpdateRequestDTO;
 import no.nav.testnav.libs.servletsecurity.exchange.TokenExchange;
 import org.springframework.stereotype.Service;
@@ -25,22 +29,20 @@ public class PdlDataConsumer {
     private final TokenExchange tokenService;
     private final WebClient webClient;
     private final PdlDataForvalterProperties serviceProperties;
-    private final ObjectMapper objectMapper;
 
     public PdlDataConsumer(TokenExchange tokenService, PdlDataForvalterProperties serviceProperties, ObjectMapper objectMapper) {
         this.tokenService = tokenService;
         this.serviceProperties = serviceProperties;
         this.webClient = WebClient.builder()
                 .baseUrl(serviceProperties.getUrl())
+                .exchangeStrategies(JacksonExchangeStrategyUtil.getJacksonStrategy(objectMapper))
                 .build();
-        this.objectMapper = objectMapper;
     }
 
     @Timed(name = "providers", tags = { "operation", "pdl_sendOrdre" })
-    public String sendOrdre(String ident) {
+    public String sendOrdre(String ident, boolean isTpsfMaster) {
 
-        return new PdlDataOrdreCommand(webClient, ident, serviceProperties.getAccessToken(tokenService)).call()
-                .block();
+        return new PdlDataOrdreCommand(webClient, ident, isTpsfMaster, serviceProperties.getAccessToken(tokenService)).call().block();
     }
 
     @Timed(name = "providers", tags = { "operation", "pdl_delete" })
@@ -54,13 +56,22 @@ public class PdlDataConsumer {
                 .block();
     }
 
-    public void oppdaterPdl(String ident, PersonUpdateRequestDTO request) throws JsonProcessingException {
+    public String opprettPdl(BestillingRequestDTO request) {
 
-        var body = objectMapper.writeValueAsString(request);
-        new PdlDataOppdateringCommand(webClient, ident, body, serviceProperties.getAccessToken(tokenService)).call();
+        return new PdlDataOpprettingCommand(webClient, request, serviceProperties.getAccessToken(tokenService)).call().block();
     }
 
-    @Timed(name = "providers", tags = { "operation", "pdl_alive" })
+    public String oppdaterPdl(String ident, PersonUpdateRequestDTO request) {
+
+        return new PdlDataOppdateringCommand(webClient, ident, request, serviceProperties.getAccessToken(tokenService)).call().block();
+    }
+
+    public List<AvailibilityResponseDTO> identCheck(List<String> identer) {
+
+        return List.of(new PdlDataCheckIdentCommand(webClient, identer, serviceProperties.getAccessToken(tokenService)).call().block());
+    }
+
+    @Timed(name = "providers", tags = { "operation", "pdl_dataforvalter_alive" })
     public Map<String, String> checkAlive() {
         return CheckAliveUtil.checkConsumerAlive(serviceProperties, webClient, tokenService);
     }
--- apps/dolly-backend/src/main/java/no/nav/dolly/bestilling/pdldata/command/PdlDataOpprettingCommand.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/bestilling/pdldata/command/PdlDataOpprettingCommand.java
@@ -2,7 +2,7 @@ package no.nav.dolly.bestilling.pdldata.command;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import no.nav.testnav.libs.dto.pdlforvalter.v1.PersonUpdateRequestDTO;
+import no.nav.testnav.libs.dto.pdlforvalter.v1.BestillingRequestDTO;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.BodyInserters;
@@ -13,20 +13,19 @@ import java.util.concurrent.Callable;
 
 @Slf4j
 @RequiredArgsConstructor
-public class PdlDataOppdateringCommand implements Callable<Mono<String>> {
+public class PdlDataOpprettingCommand implements Callable<Mono<String>> {
 
-    private static final String PDL_FORVALTER_URL = "/api/v1/personer/{ident}";
+    private static final String PDL_FORVALTER_URL = "/api/v1/personer";
 
     private final WebClient webClient;
-    private final String ident;
-    private final PersonUpdateRequestDTO body;
+    private final BestillingRequestDTO body;
     private final String token;
 
     public Mono<String> call() {
 
         return webClient
-                .put()
-                .uri(PDL_FORVALTER_URL, ident)
+                .post()
+                .uri(PDL_FORVALTER_URL)
                 .header(HttpHeaders.AUTHORIZATION, token)
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(BodyInserters.fromValue(body))
