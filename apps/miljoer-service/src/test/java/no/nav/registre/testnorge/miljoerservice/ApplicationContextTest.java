e17daa4529f796a4bf5ddcdc6c65cd3b472af59e
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-11-03 14:52:48 +0100

    fix

--- apps/miljoer-service/src/test/java/no/nav/registre/testnorge/miljoerservice/ApplicationContextTest.java
+++ apps/miljoer-service/src/test/java/no/nav/registre/testnorge/miljoerservice/ApplicationContextTest.java
@@ -1,30 +1,18 @@
 package no.nav.registre.testnorge.miljoerservice;
 
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
 
-
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureWireMock(port = 0)
-@TestPropertySource(locations = "classpath:application-test.properties")
+@SpringBootTest
 public class ApplicationContextTest {
 
     @MockBean
     public JwtDecoder jwtDecoder;
 
-    /**
-     * Used to test app runtime dependency and properties
-     */
     @Test
     @SuppressWarnings("java:S2699")
-    public void load_app_context() {
+    void load_app_context() {
     }
 }
