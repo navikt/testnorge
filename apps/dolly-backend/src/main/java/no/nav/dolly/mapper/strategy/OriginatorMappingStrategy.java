67be0f5f155144476e931627b37c6dc086de9fdf
stigus <stian.gustavsson@nav.no>
2021-11-03 12:00:04 +0100

    Fix for null verdi under mapping av navSyntetiskIdent

--- apps/dolly-backend/src/main/java/no/nav/dolly/mapper/strategy/OriginatorMappingStrategy.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/mapper/strategy/OriginatorMappingStrategy.java
@@ -10,6 +10,8 @@ import no.nav.dolly.mapper.MappingStrategy;
 import no.nav.testnav.libs.dto.pdlforvalter.v1.BestillingRequestDTO;
 import org.springframework.stereotype.Component;
 
+import static java.util.Objects.nonNull;
+
 @Component
 public class OriginatorMappingStrategy implements MappingStrategy {
 
@@ -23,7 +25,9 @@ public class OriginatorMappingStrategy implements MappingStrategy {
 
                         destinasjon.setPerson(kilde.getPerson());
                         mapperFacade.map(kilde.getOpprettNyPerson(), destinasjon);
-                        destinasjon.setSyntetisk((Boolean) context.getProperty("navSyntetiskIdent"));
+
+                        Object navSyntetiskIdent = context.getProperty("navSyntetiskIdent");
+                        destinasjon.setSyntetisk(nonNull(navSyntetiskIdent) ? (Boolean) navSyntetiskIdent : false);
                     }
                 })
                 .exclude("person")
@@ -36,7 +40,9 @@ public class OriginatorMappingStrategy implements MappingStrategy {
                     public void mapAtoB(RsTpsfUtvidetBestilling kilde, TpsfBestilling destinasjon, MappingContext context) {
 
                         destinasjon.setAntall(1);
-                        destinasjon.setNavSyntetiskIdent((Boolean) context.getProperty("navSyntetiskIdent"));
+
+                        Object navSyntetiskIdent = context.getProperty("navSyntetiskIdent");
+                        destinasjon.setNavSyntetiskIdent(nonNull(navSyntetiskIdent) ? (Boolean) navSyntetiskIdent : false);
                     }
                 })
                 .byDefault()
