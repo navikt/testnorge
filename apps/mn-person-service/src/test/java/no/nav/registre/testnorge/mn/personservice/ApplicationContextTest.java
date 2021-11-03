3b3f5b096be13fa15071590d678907c0eb2c5530
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-11-03 15:00:38 +0100

    fix

--- apps/mn-person-service/src/test/java/no/nav/registre/testnorge/mn/personservice/ApplicationContextTest.java
+++ apps/mn-person-service/src/test/java/no/nav/registre/testnorge/mn/personservice/ApplicationContextTest.java
@@ -1,12 +1,15 @@
 package no.nav.registre.testnorge.mn.personservice;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-public class ApplicationContextTest {
+@ActiveProfiles("test")
+class ApplicationContextTest {
 
     @MockBean
     public JwtDecoder jwtDecoder;
