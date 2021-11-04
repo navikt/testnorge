2e57d4f3527d588b7d2008b73d9212ef46e22906
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-11-04 13:19:12 +0100

    Remove some of JUnit4

--- proxies/statisk-data-forvalter-proxy/src/test/java/no/nav/testnav/proxies/statiskdataforvalterproxy/ApplicationContextTest.java
+++ proxies/statisk-data-forvalter-proxy/src/test/java/no/nav/testnav/proxies/statiskdataforvalterproxy/ApplicationContextTest.java
@@ -1,24 +1,20 @@
 package no.nav.testnav.proxies.statiskdataforvalterproxy;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.test.context.ActiveProfiles;
 
+@SpringBootTest
+@ActiveProfiles("test")
+class ApplicationContextTest {
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureWireMock(port = 0)
-@TestPropertySource(locations = "classpath:application-test.properties")
-public class ApplicationContextTest {
+    @MockBean
+    public JwtDecoder jwtDecoder;
 
-    /**
-     * Used to test app runtime dependency and properties
-     */
     @Test
     @SuppressWarnings("java:S2699")
-    public void load_app_context() {
+    void load_app_context() {
     }
 }
