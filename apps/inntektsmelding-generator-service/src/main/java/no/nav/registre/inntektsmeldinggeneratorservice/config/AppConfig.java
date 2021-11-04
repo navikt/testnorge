ba91ede9b6beb9da47b8cd54474b60c1a3674b48
Aileen Hay <aileen.hay@nav.no>
2021-11-04 12:28:25 +0100

    Oppdatert småfeil

--- apps/inntektsmelding-generator-service/src/main/java/no/nav/registre/inntektsmeldinggeneratorservice/config/AppConfig.java
+++ apps/inntektsmelding-generator-service/src/main/java/no/nav/registre/inntektsmeldinggeneratorservice/config/AppConfig.java
@@ -1,5 +1,6 @@
 package no.nav.registre.inntektsmeldinggeneratorservice.config;
 
+import no.nav.testnav.libs.servletsecurity.config.SecureOAuth2ServerToServerConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -8,7 +9,10 @@ import org.springframework.web.client.RestTemplate;
 import no.nav.testnav.libs.servletcore.config.ApplicationCoreConfig;
 
 @Configuration
-@Import(ApplicationCoreConfig.class)
+@Import({
+        ApplicationCoreConfig.class,
+        SecureOAuth2ServerToServerConfiguration.class
+})
 public class AppConfig {
 
     @Bean
--- apps/inntektsmelding-generator-service/src/test/java/no/nav/registre/inntektsmeldinggeneratorservice/ApplicationContextTest.java
+++ apps/inntektsmelding-generator-service/src/test/java/no/nav/registre/inntektsmeldinggeneratorservice/ApplicationContextTest.java
@@ -1,18 +1,23 @@
 package no.nav.registre.inntektsmeldinggeneratorservice;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 0)
+@TestPropertySource(locations = "classpath:application-test.properties")
 public class ApplicationContextTest {
 
-    @MockBean
-    public JwtDecoder jwtDecoder;
-
+    /**
+     * Used to test app runtime dependency and properties
+     */
     @Test
     @SuppressWarnings("java:S2699")
-    void load_app_context() {
+    public void load_app_context() {
     }
 }
