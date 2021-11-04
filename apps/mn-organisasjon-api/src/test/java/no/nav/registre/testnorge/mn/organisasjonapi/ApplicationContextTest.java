4f712915ec4e4385460b1a62db79ef8d561d44bc
Aileen Hay <aileen.hay@nav.no>
2021-11-04 08:50:00 +0100

    Fikset test i mn organisasjon api

--- apps/mn-organisasjon-api/src/test/java/no/nav/registre/testnorge/mn/organisasjonapi/ApplicationContextTest.java
+++ apps/mn-organisasjon-api/src/test/java/no/nav/registre/testnorge/mn/organisasjonapi/ApplicationContextTest.java
@@ -1,14 +1,15 @@
 package no.nav.registre.testnorge.mn.organisasjonapi;
 
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@TestPropertySource(locations = "classpath:application-test.properties")
 class ApplicationContextTest {
 
     @MockBean
