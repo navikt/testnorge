803784a4527c08652eaa7fc75d9fea5c28b4ab0d
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-11-05 12:36:26 +0100

    Fiks: organiasjon-forvalter (#1267)

--- apps/organisasjon-forvalter/src/test/java/no/nav/organisasjonforvalter/ApplicationContextTest.java
+++ apps/organisasjon-forvalter/src/test/java/no/nav/organisasjonforvalter/ApplicationContextTest.java
@@ -2,23 +2,14 @@ package no.nav.organisasjonforvalter;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureWireMock(port = 0)
-@TestPropertySource(locations = "classpath:application-test.properties")
+@SpringBootTest
+@ActiveProfiles("test")
 public class ApplicationContextTest {
 
-    @MockBean
-    public JwtDecoder jwtDecoder;
-    /**
-     * Used to test app runtime dependency and properties
-     */
     @Test
     @SuppressWarnings("java:S2699")
-    public void load_app_context() {
+    void load_app_context() {
     }
 }
