72dbebcae7498d70a8e2257c47f6f4bd2b8da0a6
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-11-05 12:37:26 +0100

    fiks: testnav-person-export-api (#1270)
    * fiks: testnav-person-export-api
    
    * fiks: tps-ident-service

--- apps/tps-ident-service/src/main/java/no/nav/registre/tpsidentservice/config/ApplicationConfig.java
+++ apps/tps-ident-service/src/main/java/no/nav/registre/tpsidentservice/config/ApplicationConfig.java
@@ -4,11 +4,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 import no.nav.testnav.libs.servletcore.config.ApplicationCoreConfig;
+import no.nav.testnav.libs.servletsecurity.config.SecureOAuth2ServerToServerConfiguration;
 
 @Configuration
 @Import({
-        ApplicationCoreConfig.class
+        ApplicationCoreConfig.class,
+        SecureOAuth2ServerToServerConfiguration.class
 })
 public class ApplicationConfig {
-
 }
