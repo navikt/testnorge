5ad1a1b3c6d6eecac3d70470f112e3272c57c78d
Aileen Hay <aileen.hay@nav.no>
2021-11-09 13:58:32 +0100

    Endret til fall bakgrunn

--- apps/dolly-frontend/src/main/js/src/pages/brukerPage/BrukerPage.tsx
+++ apps/dolly-frontend/src/main/js/src/pages/brukerPage/BrukerPage.tsx
@@ -2,15 +2,15 @@ import React from 'react'
 import BlankHeader from '~/components/layout/blankHeader/BlankHeader'
 import BrukerModal from '~/pages/brukerPage/BrukerModel'
 import './BrukerPage.less'
-import { HalloweenBackground } from '~/components/ui/background/Background'
+import { FallBackground } from '~/components/ui/background/Background'
 
 export default () => {
 	return (
 		<React.Fragment>
 			<BlankHeader />
-			<HalloweenBackground>
+			<FallBackground>
 				<BrukerModal />
-			</HalloweenBackground>
+			</FallBackground>
 		</React.Fragment>
 	)
 }
