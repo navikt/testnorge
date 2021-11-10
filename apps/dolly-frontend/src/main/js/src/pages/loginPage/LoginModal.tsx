e4c41dff28a081a0bf6cd3c20260d039db59e094
Aileen Hay <aileen.hay@nav.no>
2021-11-10 10:01:17 +0100

    Lagt til state i logout url

--- apps/dolly-frontend/src/main/js/src/pages/loginPage/LoginModal.tsx
+++ apps/dolly-frontend/src/main/js/src/pages/loginPage/LoginModal.tsx
@@ -7,7 +7,8 @@ const Advarsler = {
 		'Du ble logget inn med BankId, men er ikke tilknyttet noen organisasjoner som har ' +
 		'tilgang til Dolly. Ta kontakt med administrator i din organisasjon dersom du ønsker tilgang' +
 		', eller velg en annen innloggingsmetode.',
-	unknown_error: 'Ukjent feil oppsto. Ta kontakt med Dolly hvis denne feilen vedvarer.',
+	unknown_error:
+		'Ukjent feil oppsto og du ble logget ut. Ta kontakt med Dolly hvis denne feilen vedvarer.',
 }
 
 const getAdvarsel: () => string = () => {
