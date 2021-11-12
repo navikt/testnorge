5b59f01f8940774f6c74cdf55cc59306a6f86184
Aileen Hay <aileen.hay@nav.no>
2021-11-12 08:21:28 +0100

    KOde rydding

--- apps/dolly-frontend/src/main/java/no/nav/dolly/web/provider/web/LogController.java
+++ apps/dolly-frontend/src/main/java/no/nav/dolly/web/provider/web/LogController.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
