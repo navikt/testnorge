b11834b8f530bb3e2b01d0bbce62c95ccd84dd52
Aileen Hay <aileen.hay@nav.no>
2021-11-12 10:54:28 +0100

    Første versjon frontchannel utlogging dev

--- apps/dolly-frontend/src/main/java/no/nav/dolly/web/provider/web/FrontChannelLogoutDevController.java
+++ apps/dolly-frontend/src/main/java/no/nav/dolly/web/provider/web/FrontChannelLogoutDevController.java
@@ -1,13 +1,13 @@
 package no.nav.dolly.web.provider.web;
 
 import lombok.RequiredArgsConstructor;
+import no.nav.testnav.libs.reactivesessionsecurity.repository.OicdReactiveMapSessionRepository;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.core.session.SessionInformation;
-import org.springframework.session.ReactiveSessionRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/oauth2/logout")
@@ -15,24 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @Profile({"dev", "test"})
 public class FrontChannelLogoutDevController {
 
-    private final ReactiveSessionRepository reactiveSessionRepository;
+    private final OicdReactiveMapSessionRepository reactiveSessionRepository;
 
     @GetMapping()
-    public void logout(@RequestParam String sid) {
-
-        var sessionsToDelete = reactiveSessionRepository.getAllSessions().stream().filter(session -> {
-            var securityContext = (SecurityContextImpl) session.getAttribute("SPRING_SECURITY_CONTEXT");
-            if (securityContext == null) {
-                return;
-            }
-            var principal = (DefaultOidcUser) securityContext.getAuthentication().getPrincipal();
-            return Objects.equals(sid, principal.getClaims().get("sid"));
-        }).toList();
-
-        for (Session session: sessionsToDelete){
-            reactiveSessionRepository.deleteById(session.);
-        }
+    public Mono<Void> logout(@RequestParam String sid) {
+        return reactiveSessionRepository.deleteBySid(sid);
     }
 
 }
-
---
+++ libs/reactive-session-security/src/main/java/no/nav/testnav/libs/reactivesessionsecurity/repository/OicdReactiveMapSessionRepository.java
@@ -0,0 +1,80 @@
+package no.nav.testnav.libs.reactivesessionsecurity.repository;
+
+import org.springframework.security.core.context.SecurityContextImpl;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.session.MapSession;
+import org.springframework.session.ReactiveSessionRepository;
+import org.springframework.session.Session;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.util.Map;
+import java.util.Objects;
+
+public class OicdReactiveSessionRepository implements ReactiveSessionRepository<MapSession> {
+
+    private Integer defaultMaxInactiveInterval;
+
+    private final Map<String, Session> sessions;
+
+
+    public OicdReactiveSessionRepository(Map<String, Session> sessions) {
+        if (sessions == null) {
+            throw new IllegalArgumentException("sessions cannot be null");
+        }
+        this.sessions = sessions;
+    }
+
+    public void setDefaultMaxInactiveInterval(int defaultMaxInactiveInterval) {
+        this.defaultMaxInactiveInterval = defaultMaxInactiveInterval;
+    }
+
+    @Override
+    public Mono<Void> save(MapSession session) {
+        return Mono.fromRunnable(() -> {
+            if (!session.getId().equals(session.getOriginalId())) {
+                this.sessions.remove(session.getOriginalId());
+            }
+            this.sessions.put(session.getId(), new MapSession(session));
+        });
+    }
+
+    @Override
+    public Mono<MapSession> findById(String id) {
+        return Mono.defer(() -> Mono.justOrEmpty(this.sessions.get(id))
+                .filter((session) -> !session.isExpired())
+                .map(MapSession::new)
+                .switchIfEmpty(deleteById(id).then(Mono.empty())));
+    }
+
+    @Override
+    public Mono<Void> deleteById(String id) {
+        return Mono.fromRunnable(() -> this.sessions.remove(id));
+    }
+
+    @Override
+    public Mono<MapSession> createSession() {
+        return Mono.defer(() -> {
+            MapSession result = new MapSession();
+            if (this.defaultMaxInactiveInterval != null) {
+                result.setMaxInactiveInterval(Duration.ofSeconds(this.defaultMaxInactiveInterval));
+            }
+            return Mono.just(result);
+        });
+    }
+
+    public Mono<Void> deleteBySid(String sid) {
+        if (sid == null) return null;
+
+        for(Map.Entry<String,Session> entry: sessions.entrySet()){
+            var securityContext = (SecurityContextImpl) entry.getValue().getAttribute("SPRING_SECURITY_CONTEXT");
+            if (securityContext == null) {
+                continue;
+            }
+            var principal = (DefaultOidcUser) securityContext.getAuthentication().getPrincipal();
+            if (Objects.equals(sid, principal.getClaims().get("sid"))) return this.deleteById(entry.getKey());
+        }
+
+        return null;
+    }
+}
