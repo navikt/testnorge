a63bbd24789cc9bcca52c55617ee5c4a3e2f1864
kristenhaerum <kristen.herum@nav.no>
2021-11-12 14:48:25 +0100

    Lagt til Json mapping

---
+++ apps/tps-messaging-service/src/main/java/no/nav/testnav/apps/tpsmessagingservice/config/JsonMapperConfig.java
@@ -0,0 +1,82 @@
+package no.nav.testnav.apps.tpsmessagingservice.config;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.ZonedDateTimeSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.apache.commons.lang3.StringUtils.isBlank;
+
+@Configuration
+public class JsonMapperConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true);
+        objectMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+
+        SimpleModule simpleModule = new SimpleModule();
+        simpleModule.addDeserializer(LocalDateTime.class, new DollyLocalDateTimeDeserializer());
+        simpleModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ISO_DATE_TIME));
+        simpleModule.addDeserializer(LocalDate.class, new DollyLocalDateDeserializer());
+        simpleModule.addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ISO_DATE));
+        simpleModule.addDeserializer(ZonedDateTime.class, new DollyZonedDateTimeDeserializer());
+        simpleModule.addSerializer(ZonedDateTime.class, new ZonedDateTimeSerializer(DateTimeFormatter.ISO_DATE_TIME));
+
+        objectMapper.registerModule(simpleModule);
+        return objectMapper;
+    }
+
+    private static class DollyZonedDateTimeDeserializer extends JsonDeserializer<ZonedDateTime> {
+
+        @Override public ZonedDateTime deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+            JsonNode node = jsonParser.getCodec().readTree(jsonParser);
+            if (isBlank(node.asText())) {
+                return null;
+            }
+            return ZonedDateTime.parse(node.asText(), DateTimeFormatter.ISO_DATE_TIME);
+        }
+    }
+
+    private static class DollyLocalDateDeserializer extends JsonDeserializer<LocalDate> {
+
+        @Override public LocalDate deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+            JsonNode node = jsonParser.getCodec().readTree(jsonParser);
+            if (isBlank(node.asText())) {
+                return null;
+            }
+            String dateTime = node.asText().length() > 10 ? node.asText().substring(0, 10) : node.asText();
+            return LocalDate.parse(dateTime);
+        }
+    }
+
+    private static class DollyLocalDateTimeDeserializer extends JsonDeserializer<LocalDateTime> {
+
+        @Override public LocalDateTime deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+            JsonNode node = jsonParser.getCodec().readTree(jsonParser);
+            if (isBlank(node.asText())) {
+                return null;
+            }
+            String dateTime = node.asText().length() > 19 ? node.asText().substring(0, 19) : node.asText();
+            return dateTime.length() > 10 ? LocalDateTime.parse(dateTime) : LocalDate.parse(dateTime).atStartOfDay();
+        }
+    }
+}
