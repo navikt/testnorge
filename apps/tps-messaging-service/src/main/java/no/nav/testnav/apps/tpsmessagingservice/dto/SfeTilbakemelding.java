4ccc675840c63ecc77982cd9aa10775f736b0809
kristenhaerum <kristen.herum@nav.no>
2021-11-15 19:45:50 +0100

    Lagt til utskrift på svarmelding

--- apps/tps-messaging-service/src/main/java/no/nav/testnav/apps/tpsmessagingservice/dto/SfeTilbakemelding.java
+++ apps/tps-messaging-service/src/main/java/no/nav/testnav/apps/tpsmessagingservice/dto/SfeTilbakemelding.java
@@ -4,10 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import no.nav.testnav.libs.dto.tpsmessagingservice.v1.EndringsmeldingResponseDTO;
 
 @Data
 @Builder
+@ToString(callSuper = true)
 @NoArgsConstructor
 @AllArgsConstructor
 public class SfeTilbakemelding {
--- apps/tps-messaging-service/src/main/java/no/nav/testnav/apps/tpsmessagingservice/utils/EndringsmeldingUtil.java
+++ apps/tps-messaging-service/src/main/java/no/nav/testnav/apps/tpsmessagingservice/utils/EndringsmeldingUtil.java
@@ -1,6 +1,7 @@
 package no.nav.testnav.apps.tpsmessagingservice.utils;
 
 import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
 import no.nav.testnav.apps.tpsmessagingservice.dto.EndringsmeldingRequest;
 import no.nav.testnav.apps.tpsmessagingservice.dto.EndringsmeldingResponse;
 import no.nav.testnav.libs.dto.tpsmessagingservice.v1.EndringsmeldingResponseDTO;
@@ -14,6 +15,7 @@ import java.io.StringWriter;
 import static java.util.Objects.nonNull;
 import static org.apache.commons.lang3.StringUtils.isNotBlank;
 
+@Slf4j
 @UtilityClass
 public class EndringsmeldingUtil {
 
@@ -28,6 +30,7 @@ public class EndringsmeldingUtil {
     public static EndringsmeldingResponseDTO getResponseStatus(EndringsmeldingResponse response) {
 
         if (nonNull(response)) {
+            log.info("Svarmelding mottatt fra TPS: {}", response);
             return EndringsmeldingResponseDTO.builder()
                     .returStatus(isStatusOk(response.getSfeTilbakemelding().getSvarStatus().getReturStatus()) ? STATUS_OK : STATUS_ERROR)
                     .returMelding(response.getSfeTilbakemelding().getSvarStatus().getReturMelding())
