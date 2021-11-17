304fb4fb6383dbf433d724a50ae375db32a604dc
stigus <stian.gustavsson@nav.no>
2021-11-17 08:54:37 +0100

    Forbedret oppsett en del, mindre hardkodede verdier

--- apps/dolly-frontend/src/main/js/tests/hovedsideFixture.ts
+++ apps/dolly-frontend/src/main/js/tests/hovedsideFixture.ts
@@ -2,17 +2,6 @@ import { ClientFunction, RequestMock } from 'testcafe'
 import { ReactSelector, waitForReact } from 'testcafe-react-selectors'
 import { mockHeaders } from '@smartive/testcafe-utils'
 
-const headereJson = {
-	'content-type': 'application/json; charset=UTF-8',
-	'access-control-allow-origin': 'http://localhost:3000',
-	'access-control-allow-credentials': 'true',
-	'strict-transport-security': 'max-age=15724800; includeSubDomains',
-}
-
-const headereText = Object.apply({}, headereJson)
-
-headereText['content-type'] = 'text/html; charset=UTF-8'
-
 const miljoer = new RegExp(/\/miljoer/)
 const dollyLogg = new RegExp(/\/dolly-logg/)
 const azureAuth = new RegExp(/\/oauth2\/authorization\/aad/)
@@ -25,53 +14,61 @@ const bestillingGruppe = new RegExp(/\/bestilling\/gruppe/)
 const varslinger = new RegExp(/\/varslinger/)
 const ids = new RegExp(/\/ids/)
 
+const gjeldendeProfil = {
+	visningsNavn: 'Cafe, Test',
+	epost: 'Testcafe@nav.no',
+	avdeling: '1234 Testytest',
+	organisasjon: 'TESTCAFE',
+	type: 'Testbruker',
+}
+
+const gjeldendeBruker = {
+	brukerId: '1234-5678-12',
+	brukernavn: 'Cafe, Test',
+	brukertype: 'BASIC',
+	epost: 'testcafe@nav.no',
+	favoritter: [],
+}
+
+const gruppeHentet = {
+	id: 1,
+	navn: 'Testytest',
+	hensikt: 'Testing av testytest',
+	opprettetAv: { brukerId: '1234-5678-12', brukernavn: 'Cafe, Test', epost: 'Testcafe@nav.no' },
+	sistEndretAv: { brukerId: '1234-5678-12', brukernavn: 'Cafe, Test', epost: 'Testcafe@nav.no' },
+	datoEndret: '1980-01-12',
+	antallIdenter: 0,
+	antallIBruk: 0,
+	erEierAvGruppe: true,
+	favorittIGruppen: false,
+	erLaast: false,
+	identer: [],
+}
 const cookieMock = RequestMock()
 	.onRequestTo(miljoer)
-	.respond(
-		'["q1","q2","q4","q5","qx","t0","t1","t13","t2","t3","t4","t5","t6","u5"]',
-		200,
-		headereJson
-	)
+	.respond('["q1","q2","q4","q5","qx","t0","t1","t13","t2","t3","t4","t5","t6","u5"]', 200)
 	.onRequestTo(dollyLogg)
-	.respond(null, 200, headereText)
+	.respond(null, 200)
 	.onRequestTo(hentGrupper)
-	.respond(
-		'[{"id":1,"navn":"Testytest","hensikt":"Testing av testytest","opprettetAv":{"brukerId":"1234-5678-12","brukernavn":"Cafe, Test","epost":"Testcafe@nav.no"},"sistEndretAv":{"brukerId":"1234-5678-12","brukernavn":"Cafe, Test","epost":"Testcafe@nav.no"},"datoEndret":"1980-01-12","antallIdenter":0,"antallIBruk":0,"erEierAvGruppe":true,"favorittIGruppen":false,"erLaast":false}]',
-		200,
-		headereText
-	)
+	.respond('[]', 200)
 	.onRequestTo(spesifikkGruppe)
-	.respond(
-		'{"id":1,"navn":"Testytest","hensikt":"Testing av testytest","opprettetAv":{"brukerId":"1234-5678-12","brukernavn":"Cafe, Test","epost":"Testcafe@nav.no"},"sistEndretAv":{"brukerId":"1234-5678-12","brukernavn":"Cafe, Test","epost":"Testcafe@nav.no"},"datoEndret":"1980-01-12","antallIdenter":0,"antallIBruk":0,"erEierAvGruppe":true,"favorittIGruppen":false,"erLaast":false,"identer":[]}',
-		200,
-		headereText
-	)
+	.respond(gruppeHentet, 200)
 	.onRequestTo(varslinger)
-	.respond('[{"varslingId":"VELKOMMEN_TIL_DOLLY","fom":null,"tom":null}]', 200, headereText)
+	.respond('[{"varslingId":"VELKOMMEN_TIL_DOLLY","fom":null,"tom":null}]', 200)
 	.onRequestTo(ids)
-	.respond('["VELKOMMEN_TIL_DOLLY"]', 200, headereText)
+	.respond('["VELKOMMEN_TIL_DOLLY"]', 200)
 	.onRequestTo(azureAuth)
-	.respond("<script>window.location.href='http://localhost:3000/';</script>", 200, headereText)
+	.respond("<script>window.location.href='http://localhost:3000/';</script>", 200)
 	.onRequestTo(dollyLogg)
-	.respond(null, 200, headereJson)
+	.respond(null, 200)
 	.onRequestTo(bestillingGruppe)
-	.respond('[]', 201, headereJson)
+	.respond(spesifikkGruppe, 201)
 	.onRequestTo(current)
-	.respond(
-		'{"brukerId":"1234-5678-12","brukernavn":"Cafe, Test","brukertype":"BASIC","epost":"testcafe@nav.no","favoritter":[]}',
-		200,
-		mockHeaders
-	)
+	.respond(gjeldendeBruker, 200, mockHeaders)
 	.onRequestTo(profil)
-	.respond(
-		'{"visningsNavn":"Cafe, Test","epost":"Testcafe@nav.no","avdeling":"1234 Testytest","organisasjon":"TESTCAFE","type":"Testbruker"}',
-		200,
-		mockHeaders
-	)
+	.respond(gjeldendeProfil, 200, mockHeaders)
 	.onRequestTo(bilde)
 	.respond(null, 404, mockHeaders)
-
-const noNewVarslingerMock = RequestMock().onRequestTo(varslinger).respond('[]', 200, headereText)
 
 fixture`Hovedside`.page`http://localhost:3000`.requestHooks(cookieMock).beforeEach(async () => {
 	await waitForReact()
--- apps/dolly-frontend/src/main/js/tests/loginFixture.ts
+++ apps/dolly-frontend/src/main/js/tests/loginFixture.ts
@@ -2,35 +2,28 @@ import { ClientFunction, RequestMock } from 'testcafe'
 import { ReactSelector, waitForReact } from 'testcafe-react-selectors'
 import { mockHeaders } from '@smartive/testcafe-utils'
 
-const headereJson = {
-	'content-type': 'application/json; charset=UTF-8',
-	'access-control-allow-origin': 'http://localhost:3000',
-	'access-control-allow-credentials': 'true',
-	'strict-transport-security': 'max-age=15724800; includeSubDomains',
-}
-
-const headereText = Object.apply({}, headereJson)
-
-headereText['content-type'] = 'text/html; charset=UTF-8'
-
 const miljoer = new RegExp(/\/miljoer/)
 const dollyLogg = new RegExp(/\/dolly-logg/)
 const azureAuth = new RegExp(/\/oauth2\/authorization\/aad/)
 const current = new RegExp(/\/current/)
 
+const currentBruker = {
+	brukerId: '1234-5678-12',
+	brukernavn: 'Cafe, Test',
+	brukertype: 'BASIC',
+	epost: 'testcafe@nav.no',
+	favoritter: [],
+}
+
 const cookieMock = RequestMock()
 	.onRequestTo(miljoer)
-	.respond('["t4"]', 200, headereJson)
+	.respond('["t4"]', 200)
 	.onRequestTo(dollyLogg)
-	.respond(null, 200, headereText)
+	.respond(null, 200)
 	.onRequestTo(azureAuth)
-	.respond("<script>window.location.href='http://localhost:3000/';</script>", 200, headereText)
+	.respond("<script>window.location.href='http://localhost:3000/';</script>", 200)
 	.onRequestTo(current)
-	.respond(
-		'{"brukerId":"1234-5678-12","brukernavn":"Cafe, Test","brukertype":"BASIC","epost":"testcafe@nav.no","favoritter":[]}',
-		200,
-		mockHeaders
-	)
+	.respond(currentBruker, 200, mockHeaders)
 
 fixture`Loginside`.page`http://localhost:3000/login`
 	.requestHooks(cookieMock)
