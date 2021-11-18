2c8249b7c42d567b609d880c79877815810838c1
Aileen Hay <aileen.hay@nav.no>
2021-11-18 10:15:12 +0100

    Fjernet importer grupper for bankid

--- apps/dolly-frontend/src/main/js/src/pages/gruppeOversikt/GruppeOversikt.js
+++ apps/dolly-frontend/src/main/js/src/pages/gruppeOversikt/GruppeOversikt.js
@@ -78,6 +78,7 @@ export default function GruppeOversikt({
 				setSidetall={setSidetall}
 				sideStoerrelse={sideStoerrelse}
 				setSideStoerrelse={setSideStoerrelse}
+				brukerProfil={brukerProfil}
 			/>
 		</div>
 	)
