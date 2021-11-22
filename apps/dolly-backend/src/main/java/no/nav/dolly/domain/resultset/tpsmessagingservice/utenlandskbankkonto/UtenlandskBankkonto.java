e2bc1440d6f9d5b459d99f3417665ba3f5f0dbb8
stigus <stian.gustavsson@nav.no>
2021-11-22 15:56:06 +0100

    Mapper UtenlandskBankkonto

--- apps/dolly-backend/src/main/java/no/nav/dolly/domain/resultset/tpsmessagingservice/utenlandskbankkonto/UtenlandskBankkonto.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/domain/resultset/tpsmessagingservice/utenlandskbankkonto/UtenlandskBankkonto.java
@@ -3,11 +3,19 @@ package no.nav.dolly.domain.resultset.tpsmessagingservice.utenlandskbankkonto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
+import lombok.Data;
 
+@Data
 @Builder
 @JsonInclude(value = JsonInclude.Include.NON_EMPTY)
-public record UtenlandskBankkonto(String kontonummer, String swift,
-                                  String landkode, String iban,
-                                  String banknavn, String valuta, String bankAdresse1,
-                                  String bankAdresse2, String bankAdresse3) {
+public final class UtenlandskBankkonto {
+    private final String kontonummer;
+    private final String swift;
+    private final String landkode;
+    private final String iban;
+    private final String banknavn;
+    private final String valuta;
+    private final String bankAdresse1;
+    private final String bankAdresse2;
+    private final String bankAdresse3;
 }
