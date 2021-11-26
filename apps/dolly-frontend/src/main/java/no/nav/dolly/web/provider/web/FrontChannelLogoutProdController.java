941374d61fba22a5d30066f64755d75c96f6bc08
Aileen Hay <aileen.hay@nav.no>
2021-11-26 10:29:53 +0100

    Logout test 2

--- apps/dolly-frontend/src/main/java/no/nav/dolly/web/provider/web/FrontChannelLogoutProdController.java
+++ apps/dolly-frontend/src/main/java/no/nav/dolly/web/provider/web/FrontChannelLogoutProdController.java
@@ -6,8 +6,10 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.core.ScanOptions;
 import org.springframework.security.core.context.SecurityContextImpl;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.session.MapSession;
 import org.springframework.session.Session;
 import org.springframework.session.data.redis.ReactiveRedisSessionRepository;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,6 +18,8 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.util.function.Tuple2;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Objects;
 
 @RestController
@@ -41,18 +45,22 @@ public class FrontChannelLogoutProdController {
     @GetMapping("/test")
     public Mono<Void> testLogout() {
         var redisOperations = sessionRepository.getSessionRedisOperations();
-        return redisOperations.delete(redisOperations.scan(ScanOptions.NONE)
-                .flatMap(sessionKey -> Mono.zip(Mono.just(sessionKey), sessionContainsSpringSec(sessionKey)))
-                .filter(Tuple2::getT2)
-                .map(Tuple2::getT1)
-                .collectList()
-                .flatMapMany(Flux::fromIterable)
-        ).then();
+        return redisOperations.scan(ScanOptions.NONE)
+                        .flatMap(this::expireSessionWithSid)
+                .then();
     }
 
-    private Mono<Boolean> sessionContainsSpringSec(String sessionKey) {
+    private Mono<Void> expireSessionWithSid(String sessionKey) {
         return Mono.just((Session) ThreadUtil.Instance().resolve(sessionRepository.findById(sessionKey)))
-                .map(session -> session.getAttribute("SPRING_SECURITY_CONTEXT") != null);
+                .flatMap(session -> Mono.zip(Mono.just(session), session.getAttribute("SPRING_SECURITY_CONTEXT")))
+                .filter(tuple -> ObjectUtils.isEmpty(tuple.getT2()))
+                .map(Tuple2::getT1)
+                .map(session -> {
+                    session.setLastAccessedTime(Instant.now().minusSeconds(3000));
+                    session.setMaxInactiveInterval(Duration.ofMillis(1));
+                    return session;
+                }).then();
+
     }
 
     private Mono<Boolean> sessionContainsSid(String sessionKey, String sid) {
