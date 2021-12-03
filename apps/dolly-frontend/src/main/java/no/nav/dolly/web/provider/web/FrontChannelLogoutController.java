b343a1db5a2beca73bfd2aa2f9883b712f42b2f1
Aileen Hay <aileen.hay@nav.no>
2021-12-03 09:02:48 +0100

    Siste test

--- apps/dolly-frontend/src/main/java/no/nav/dolly/web/provider/web/FrontChannelLogoutController.java
+++ apps/dolly-frontend/src/main/java/no/nav/dolly/web/provider/web/FrontChannelLogoutController.java
@@ -35,16 +35,20 @@ public class FrontChannelLogoutController {
         var store = manager.getSessionStore();
 
         var sessionIds = getAllSessionIds();
+        var sessions = Flux.concat(sessionIds
+                .stream()
+                .map(sessionId -> manager.getSessionStore().retrieveSession(sessionId))
+                .toList());
 
-        return Mono.just(sessionIds)
-                .flatMapMany(Flux::fromIterable)
-                .map(store::retrieveSession)
-                .flatMap(session -> Mono.zip(Mono.just(session), session
-                        .mapNotNull(ses -> (SecurityContextImpl) ses.getAttribute("SPRING_SECURITY_CONTEXT"))
-                        .map(securityContext -> (DefaultOidcUser) securityContext.getAuthentication().getPrincipal())
-                        .map(principal -> Objects.equals(sid, principal.getClaims().get("sid")))))
-                .filter(Tuple2::getT2)
-                .flatMap(Tuple2::getT1)
+        return sessions
+                .filter(session -> session.getAttribute("SPRING_SECURITY_CONTEXT") != null)
+                .filter(session -> {
+                    var securityContext = (SecurityContextImpl) session.getAttribute("SPRING_SECURITY_CONTEXT");
+                    if (securityContext.getAuthentication().getPrincipal() instanceof DefaultOidcUser user) {
+                        return user.getIdToken().getClaim("sid").equals(sid);
+                    }
+                    return false;
+                })
                 .flatMap(WebSession::invalidate)
                 .then();
     }
