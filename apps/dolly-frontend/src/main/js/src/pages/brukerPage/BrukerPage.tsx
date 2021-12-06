fdf80ab2967a4c297c3bfc387e66ca8ea2229682
Aileen Hay <aileen.hay@nav.no>
2021-12-06 10:54:21 +0100

    Bakgrunn fiks

--- apps/dolly-frontend/src/main/js/src/pages/brukerPage/BrukerPage.tsx
+++ apps/dolly-frontend/src/main/js/src/pages/brukerPage/BrukerPage.tsx
@@ -2,15 +2,15 @@ import React from 'react'
 import BlankHeader from '~/components/layout/blankHeader/BlankHeader'
 import BrukerModal from '~/pages/brukerPage/BrukerModel'
 import './BrukerPage.less'
-import { FallBackground } from '~/components/ui/background/Background'
+import { ChristmasBackground } from '~/components/ui/background/Background'
 
 export default () => {
 	return (
 		<React.Fragment>
 			<BlankHeader />
-			<FallBackground>
+			<ChristmasBackground>
 				<BrukerModal />
-			</FallBackground>
+			</ChristmasBackground>
 		</React.Fragment>
 	)
 }
