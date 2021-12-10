5335716760045eef39fbd564b9777447027d4527
kristenhaerum <kristen.herum@nav.no>
2021-12-10 09:28:03 +0100

    Fikser runtime exception på dekoding

--- apps/dolly-backend/src/main/java/no/nav/dolly/mapper/BestillingTpsMessagingStatusMapper.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/mapper/BestillingTpsMessagingStatusMapper.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import no.nav.dolly.domain.jpa.BestillingProgress;
 import no.nav.dolly.domain.resultset.RsStatusRapport;
 import org.apache.logging.log4j.util.Strings;
@@ -22,6 +23,7 @@ import static java.util.Collections.emptyList;
 import static java.util.Objects.nonNull;
 import static no.nav.dolly.domain.resultset.SystemTyper.TPS_MESSAGING;
 
+@Slf4j
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class BestillingTpsMessagingStatusMapper {
 
@@ -29,60 +31,67 @@ public final class BestillingTpsMessagingStatusMapper {
 
     public static List<RsStatusRapport> buildTpsMessagingStatusMap(List<BestillingProgress> progressList) {
 
-        //melding     miljø     identer
-        Map<String, Map<String, Set<String>>> statusMap = new HashMap<>();
+        try {
+            //melding     miljø     identer
+            Map<String, Map<String, Set<String>>> statusMap = new HashMap<>();
 
-        var intermediateStatus = progressList.stream()
-                .filter(progress -> nonNull(progress.getTpsMessagingStatus()))
-                .map(progress ->
-                        Stream.of(progress.getTpsMessagingStatus().split("\\$"))
-                                .filter(Strings::isNotBlank)
-                                .map(melding ->
-                                        Stream.of(melding.split("#")[1].split(","))
-                                                .map(status -> StatusTemp.builder()
-                                                        .ident(progress.getIdent())
-                                                        .melding(cleanOK(String.format("%s %s", melding.split("#")[0],
-                                                                status.split(":")[1]).replace("=", ":")))
-                                                        .miljoe(status.split(":")[0])
-                                                        .build())
-                                                .toList())
-                                .flatMap(Collection::stream)
-                                .toList())
-                .flatMap(Collection::stream)
-                .toList();
+            var intermediateStatus = progressList.stream()
+                    .filter(progress -> nonNull(progress.getTpsMessagingStatus()))
+                    .map(progress ->
+                            Stream.of(progress.getTpsMessagingStatus().split("\\$"))
+                                    .filter(Strings::isNotBlank)
+                                    .map(melding ->
+                                            Stream.of(melding.split("#")[1].split(","))
+                                                    .map(status -> StatusTemp.builder()
+                                                            .ident(progress.getIdent())
+                                                            .melding(cleanOK(String.format("%s %s", melding.split("#")[0],
+                                                                    status.split(":")[1]).replace("=", ":")))
+                                                            .miljoe(status.split(":")[0])
+                                                            .build())
+                                                    .toList())
+                                    .flatMap(Collection::stream)
+                                    .toList())
+                    .flatMap(Collection::stream)
+                    .toList();
 
-        intermediateStatus.stream()
-                .filter(status -> OKEY.equals(status.getMelding()) && intermediateStatus.stream()
-                        .noneMatch(status2 -> !OKEY.equals(status2.getMelding()) && status.getMiljoe().equals(status2.getMiljoe())) ||
-                        !OKEY.equals(status.getMelding()))
-                .forEach(entry -> {
-                    if (statusMap.containsKey(entry.getMelding())) {
-                        if (statusMap.get(entry.getMelding()).containsKey(entry.getMiljoe())) {
-                            statusMap.get(entry.getMelding()).get(entry.getMiljoe()).add(entry.getIdent());
+            intermediateStatus.stream()
+                    .filter(status -> OKEY.equals(status.getMelding()) && intermediateStatus.stream()
+                            .noneMatch(status2 -> !OKEY.equals(status2.getMelding()) && status.getMiljoe().equals(status2.getMiljoe())) ||
+                            !OKEY.equals(status.getMelding()))
+                    .forEach(entry -> {
+                        if (statusMap.containsKey(entry.getMelding())) {
+                            if (statusMap.get(entry.getMelding()).containsKey(entry.getMiljoe())) {
+                                statusMap.get(entry.getMelding()).get(entry.getMiljoe()).add(entry.getIdent());
+                            } else {
+                                statusMap.get(entry.getMelding()).put(entry.getMiljoe(), new HashSet<>(Set.of(entry.getIdent())));
+                            }
                         } else {
-                            statusMap.get(entry.getMelding()).put(entry.getMiljoe(), new HashSet<>(Set.of(entry.getIdent())));
+                            statusMap.put(entry.getMelding(), new HashMap<>(Map.of(entry.getMiljoe(), new HashSet<>(Set.of(entry.getIdent())))));
                         }
-                    } else {
-                        statusMap.put(entry.getMelding(), new HashMap<>(Map.of(entry.getMiljoe(), new HashSet<>(Set.of(entry.getIdent())))));
-                    }
-                });
+                    });
 
-        return !statusMap.isEmpty() ? List.of(RsStatusRapport.builder()
-                .id(TPS_MESSAGING)
-                .navn(TPS_MESSAGING.getBeskrivelse())
-                .statuser(statusMap.entrySet().stream()
-                        .map(status -> RsStatusRapport.Status.builder()
-                                .melding(status.getKey())
-                                .detaljert(status.getValue().entrySet().stream()
-                                        .map(miljoIdenter -> RsStatusRapport.Detaljert.builder()
-                                                .miljo(miljoIdenter.getKey())
-                                                .identer(new ArrayList<>(miljoIdenter.getValue()))
-                                                .build())
-                                        .toList())
-                                .build())
-                        .toList())
-                .build()) :
-                emptyList();
+            return !statusMap.isEmpty() ? List.of(RsStatusRapport.builder()
+                    .id(TPS_MESSAGING)
+                    .navn(TPS_MESSAGING.getBeskrivelse())
+                    .statuser(statusMap.entrySet().stream()
+                            .map(status -> RsStatusRapport.Status.builder()
+                                    .melding(status.getKey())
+                                    .detaljert(status.getValue().entrySet().stream()
+                                            .map(miljoIdenter -> RsStatusRapport.Detaljert.builder()
+                                                    .miljo(miljoIdenter.getKey())
+                                                    .identer(new ArrayList<>(miljoIdenter.getValue()))
+                                                    .build())
+                                            .toList())
+                                    .build())
+                            .toList())
+                    .build()) :
+                    emptyList();
+
+        } catch (RuntimeException e) {
+
+            log.error(e.getMessage(), e);
+            return emptyList();
+        }
     }
 
     private static String cleanOK(String status) {
