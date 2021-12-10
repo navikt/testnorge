5ef49401fb15eb3620e6379f5f3722d9d8b5877b
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-12-10 10:55:56 +0100

    chore: slett applikasjonsanalyse (#2006)

--- apps/applikasjonsanalyse-frontend/src/main/js/src/page/ApplikasjonsanalysePage/index.ts
+++
@@ -1,3 +0,0 @@
-import ApplikasjonsanalysePage from './ApplikasjonsanalysePage';
-
-export default ApplikasjonsanalysePage;
--- apps/applikasjonsanalyse-service/src/main/java/no/nav/registre/testnorge/applikasjonsanalyseservice/domain/Properties.java
+++
@@ -1,9 +0,0 @@
-package no.nav.registre.testnorge.applikasjonsanalyseservice.domain;
-
-import lombok.Value;
-
-@Value
-public class Properties {
-    String organisation;
-    String repository;
-}
