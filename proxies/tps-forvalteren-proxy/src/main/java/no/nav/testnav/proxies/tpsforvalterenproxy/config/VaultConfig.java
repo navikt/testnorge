dfaffc227abc844ef45bff28ff39f7e708a1ec7b
kristenhaerum <kristen.herum@nav.no>
2021-12-15 08:43:39 +0100

    Lagt til basic auth + oppdatert app tilganger

---
+++ proxies/tps-forvalteren-proxy/src/main/java/no/nav/testnav/proxies/tpsforvalterenproxy/config/VaultConfig.java
@@ -0,0 +1,31 @@
+package no.nav.testnav.proxies.tpsforvalterenproxy.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.vault.annotation.VaultPropertySource;
+import org.springframework.vault.authentication.ClientAuthentication;
+import org.springframework.vault.authentication.TokenAuthentication;
+import org.springframework.vault.client.VaultEndpoint;
+import org.springframework.vault.config.AbstractVaultConfiguration;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+@VaultPropertySource(value = "serviceuser/dev/srvtps-forvalteren", propertyNamePrefix = "credentials.", ignoreSecretNotFound = false)
+public class VaultConfig extends AbstractVaultConfiguration {
+
+    @Override
+    public VaultEndpoint vaultEndpoint() {
+        return VaultEndpoint.create("vault.adeo.no", 443);
+    }
+
+    @Override
+    public ClientAuthentication clientAuthentication() {
+        var token = System.getProperty("spring.cloud.vault.token");
+        if (token == null) {
+            throw new IllegalArgumentException("Påkreved property 'spring.cloud.vault.token' er ikke satt.");
+        }
+        return new TokenAuthentication(System.getProperty("spring.cloud.vault.token"));
+    }
+}
