538db8f30b6d3a149e23cb0dc7ec0dafd5fa18df
kristenhaerum <kristen.herum@nav.no>
2021-12-16 14:09:26 +0100

    Oppdatert config III

--- proxies/tps-forvalteren-proxy/src/main/java/no/nav/testnav/proxies/tpsforvalterenproxy/TpsForvaterenProxyApplicationStarter.java
+++ proxies/tps-forvalteren-proxy/src/main/java/no/nav/testnav/proxies/tpsforvalterenproxy/TpsForvaterenProxyApplicationStarter.java
@@ -23,9 +23,9 @@ import static org.springframework.http.HttpHeaders.encodeBasicAuth;
 @SpringBootApplication
 public class TpsForvaterenProxyApplicationStarter {
 
-    @Value("{credentials.username}")
+    @Value("${username}")
     private String username;
-    @Value("{credentials.password}")
+    @Value("${password}")
     private String password;
     @Value("${proxy.url}")
     private String url;
