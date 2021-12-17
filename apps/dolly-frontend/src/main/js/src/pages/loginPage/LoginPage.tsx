ad08edb2dc27d105dd5f1042a654bf1fda23ce5b
Aileen Hay <58416744+H153910@users.noreply.github.com>
2021-12-17 10:40:10 +0100

    BYttet til sno bakgrunn (#2027)

--- apps/dolly-frontend/src/main/js/src/pages/loginPage/LoginPage.tsx
+++ apps/dolly-frontend/src/main/js/src/pages/loginPage/LoginPage.tsx
@@ -2,15 +2,15 @@ import React from 'react'
 import BlankHeader from '~/components/layout/blankHeader/BlankHeader'
 import LoginModal from '~/pages/loginPage/LoginModal'
 import './LoginPage.less'
-import { ChristmasBackground } from '~/components/ui/background/Background'
+import { SnowingBackground } from '~/components/ui/background/Background'
 
 export default () => {
 	return (
 		<React.Fragment>
 			<BlankHeader />
-			<ChristmasBackground>
+			<SnowingBackground>
 				<LoginModal />
-			</ChristmasBackground>
+			</SnowingBackground>
 		</React.Fragment>
 	)
 }
