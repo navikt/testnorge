ac32f2e9e8b01060ef51cdfc94670055eb4869e3
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-12-17 12:15:33 +0100

    feat: Leg til snoe

--- apps/dolly-frontend/src/main/js/src/pages/brukerPage/BrukerPage.tsx
+++ apps/dolly-frontend/src/main/js/src/pages/brukerPage/BrukerPage.tsx
@@ -2,15 +2,20 @@ import React from 'react'
 import BlankHeader from '~/components/layout/blankHeader/BlankHeader'
 import BrukerModal from '~/pages/brukerPage/BrukerModel'
 import './BrukerPage.less'
-import { SnowingBackground } from '~/components/ui/background/Background'
+import { ChristmasBackground as Background } from '~/components/ui/background/Background'
+
+import './../../snow.scss'
 
 export default () => {
 	return (
 		<React.Fragment>
 			<BlankHeader />
-			<SnowingBackground>
+			{Array.from(Array(50).keys()).map(() => (
+				<div className="snowflake" />
+			))}
+			<Background>
 				<BrukerModal />
-			</SnowingBackground>
+			</Background>
 		</React.Fragment>
 	)
 }
