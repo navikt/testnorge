fd8450a33771a5d7af017e0bbef96e0aae7591de
Aileen Hay <aileen.hay@nav.no>
2022-01-04 09:19:56 +0100

    Lagt til vinter bakgrunn

--- apps/dolly-frontend/src/main/js/src/pages/loginPage/LoginPage.tsx
+++ apps/dolly-frontend/src/main/js/src/pages/loginPage/LoginPage.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import BlankHeader from '~/components/layout/blankHeader/BlankHeader'
 import LoginModal from '~/pages/loginPage/LoginModal'
 import './LoginPage.less'
-import { ChristmasBackground as Background } from '~/components/ui/background/Background'
+import { WinterBackground as Background } from '~/components/ui/background/Background'
 
 import './../../snow.scss'
 
