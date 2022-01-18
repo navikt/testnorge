3ccbc71665ff050dd5aa7f4e3e6caf7cabce2a9e
Aileen Hay <aileen.hay@nav.no>
2022-01-18 10:47:46 +0100

    Oppdatering steg 1 paneler

--- apps/dolly-frontend/src/main/js/src/components/bestillingsveileder/stegVelger/steg/steg1/paneler/Udi.js
+++ apps/dolly-frontend/src/main/js/src/components/bestillingsveileder/stegVelger/steg/steg1/paneler/Udi.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Panel from '~/components/ui/panel/Panel'
 import { Attributt, AttributtKategori } from '../Attributt'
 
-export const UdiPanel = ({ stateModifier }) => {
+export const UdiPanel = ({ stateModifier, importTestnorge }) => {
 	const sm = stateModifier(UdiPanel.initialValues)
 
 	const infoTekst =
@@ -26,7 +26,7 @@ export const UdiPanel = ({ stateModifier }) => {
 			</AttributtKategori>
 
 			<AttributtKategori title="Alias">
-				<Attributt attr={sm.attrs.aliaser} />
+				<Attributt disabled={importTestnorge} attr={sm.attrs.aliaser} />
 			</AttributtKategori>
 
 			<AttributtKategori title="Annet">
