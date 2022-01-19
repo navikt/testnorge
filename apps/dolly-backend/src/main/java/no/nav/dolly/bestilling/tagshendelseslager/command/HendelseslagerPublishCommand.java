565f78ef19de1d73adbfc019cbc2d1840806d8b6
kristenhaerum <kristen.herum@nav.no>
2022-01-19 15:45:10 +0100

    Oppdatert test ++

--- apps/dolly-backend/src/main/java/no/nav/dolly/bestilling/tagshendelseslager/command/HendelseslagerPublishCommand.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/bestilling/tagshendelseslager/command/HendelseslagerPublishCommand.java
@@ -3,21 +3,24 @@ package no.nav.dolly.bestilling.tagshendelseslager.command;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.testnav.libs.securitycore.config.UserConstant;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
 import java.util.concurrent.Callable;
+import java.util.stream.Collectors;
 
 import static no.nav.dolly.util.TokenXUtil.getUserJwt;
 
 @Slf4j
+@Order(1)
 @RequiredArgsConstructor
 public class HendelseslagerPublishCommand implements Callable<Mono<String>> {
 
     private static final String PDL_PUBLISH = "/internal/publish";
-    private static final String PDL_PDL_IDENTHENDELSE = "/pdl-identhendelse";
+    private static final String PDL_IDENTHENDELSE = "/pdl-identhendelse";
     private static final String PERSON_IDENTER = "Nav-Personidenter";
 
     private final WebClient webClient;
@@ -29,12 +32,12 @@ public class HendelseslagerPublishCommand implements Callable<Mono<String>> {
         return webClient
                 .get()
                 .uri(uriBuilder -> uriBuilder
-                        .path(PDL_PDL_IDENTHENDELSE)
+                        .path(PDL_IDENTHENDELSE)
                         .path(PDL_PUBLISH)
-                        .queryParam(PERSON_IDENTER, identer)
                         .build())
                 .header(HttpHeaders.AUTHORIZATION, token)
                 .header(UserConstant.USER_HEADER_JWT, getUserJwt())
+                .header(PERSON_IDENTER, identer.stream().collect(Collectors.joining(",")))
                 .retrieve()
                 .bodyToMono(String.class);
     }
