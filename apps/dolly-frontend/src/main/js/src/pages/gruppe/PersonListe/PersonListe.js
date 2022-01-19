383feb04271359c0474de42ab82fbbd79582e0cf
Aileen Hay <aileen.hay@nav.no>
2022-01-19 15:17:30 +0100

    Kilde fiks

--- apps/dolly-frontend/src/main/js/src/pages/gruppe/PersonListe/PersonListe.js
+++ apps/dolly-frontend/src/main/js/src/pages/gruppe/PersonListe/PersonListe.js
@@ -222,7 +222,7 @@ export default function PersonListe({
 						bestillingsIdListe={bruker.ident.bestillingId}
 						gruppeId={bruker.ident.gruppeId}
 						iLaastGruppe={iLaastGruppe}
-						kildePdl={bruker?.kilde === 'PDL'}
+						kildePdl={bruker?.kilde === 'PDL' || bruker?.kilde === 'TESTNORGE'}
 					/>
 				)}
 			/>
