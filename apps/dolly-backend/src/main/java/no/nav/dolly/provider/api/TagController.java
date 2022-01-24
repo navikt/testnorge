0da61188b4ddf6392289b26c0f62c877142f9b94
stigus <stian.gustavsson@nav.no>
2022-01-24 13:13:56 +0000

    Lagt på requestbody

--- apps/dolly-backend/src/main/java/no/nav/dolly/provider/api/TagController.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/provider/api/TagController.java
@@ -13,6 +13,7 @@ import org.springframework.http.MediaType;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -47,7 +48,7 @@ public class TagController {
     @PostMapping("/gruppe/{gruppeId}")
     @Transactional
     @Operation(description = "Send tags på gruppe")
-    public Object sendTagsPaaGruppe(List<Tags> tags, Long gruppeId) {
+    public Object sendTagsPaaGruppe(@RequestBody List<Tags> tags, Long gruppeId) {
         Optional<Testgruppe> byId = testgruppeRepository.findById(gruppeId);
         Testgruppe testgruppe = byId
                 .orElseThrow(() -> new NotFoundException(String.format("Fant ikke gruppe på id: %s", gruppeId)));
