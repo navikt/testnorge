26da5733a0de3399f2e1585f14bd31ae35261b1d
Aileen Hay <aileen.hay@nav.no>
2022-01-31 10:55:07 +0100

    Forenklet pdloppholdsadresse

--- apps/dolly-frontend/src/main/js/src/components/fagsystem/pdl/visning/partials/adresser/PdlOppholdsadresse.tsx
+++ apps/dolly-frontend/src/main/js/src/components/fagsystem/pdl/visning/partials/adresser/PdlOppholdsadresse.tsx
@@ -1,12 +1,8 @@
 import React from 'react'
 import SubOverskrift from '~/components/ui/subOverskrift/SubOverskrift'
-import { Vegadresse } from '~/components/fagsystem/pdlf/visning/partials/Vegadresse'
-import { Matrikkeladresse } from '~/components/fagsystem/pdlf/visning/partials/Matrikkeladresse'
-import { UtenlandskAdresse } from '~/components/fagsystem/pdlf/visning/partials/UtenlandskAdresse'
-import { TitleValue } from '~/components/ui/titleValue/TitleValue'
+import { Adresse } from '~/components/fagsystem/pdlf/visning/partials/Oppholdsadresse'
 import { Oppholdsadresse } from '~/pages/gruppe/PersonVisning/PersonMiljoeinfo/PdlDataTyper'
 import { ArrayHistorikk } from '~/components/ui/historikk/ArrayHistorikk'
-import Formatters from '~/utils/DataFormatter'
 
 type PdlOppholdsadresseProps = {
 	data: Array<Oppholdsadresse>
@@ -17,20 +13,10 @@ type AdresseProps = {
 	idx?: number
 }
 
-const Adresse = ({ data, idx }: AdresseProps) => {
+const AdresseVisning = ({ data, idx }: AdresseProps) => {
 	return (
 		<div className="person-visning_content">
-			{data.vegadresse && <Vegadresse adresse={data} idx={idx} />}
-			{data.matrikkeladresse && <Matrikkeladresse adresse={data} idx={idx} />}
-			{data.utenlandskAdresse && <UtenlandskAdresse adresse={data} idx={idx} />}
-			{data.oppholdAnnetSted && (
-				<div className="person-visning_content" key={idx}>
-					<TitleValue
-						title="Opphold annet sted"
-						value={Formatters.showLabel('oppholdAnnetSted', data.oppholdAnnetSted)}
-					/>
-				</div>
-			)}
+			<Adresse adresse={data} idx={idx} />
 		</div>
 	)
 }
@@ -45,7 +31,7 @@ export const PdlOppholdsadresse = ({ data }: PdlOppholdsadresseProps) => {
 		<>
 			<SubOverskrift label="Oppholdsadresse" iconKind="adresse" />
 			<ArrayHistorikk
-				component={Adresse}
+				component={AdresseVisning}
 				data={gyldigeAdresser}
 				historiskData={historiskeAdresser}
 				header={''}
