3c6e5994ea51cfcd4066c35ea3e4b05a0ece5b81
Aileen Hay <aileen.hay@nav.no>
2022-01-31 11:13:58 +0100

    KOde ryddet

--- apps/dolly-frontend/src/main/js/src/components/fagsystem/pdl/visning/partials/nasjonalitet/PdlNasjonalitet.tsx
+++ apps/dolly-frontend/src/main/js/src/components/fagsystem/pdl/visning/partials/nasjonalitet/PdlNasjonalitet.tsx
@@ -1,15 +1,6 @@
 import React from 'react'
-import { AdresseKodeverk } from '~/config/kodeverk'
 import SubOverskrift from '~/components/ui/subOverskrift/SubOverskrift'
-import { TitleValue } from '~/components/ui/titleValue/TitleValue'
-import { DollyFieldArray } from '~/components/ui/form/fieldArray/DollyFieldArray'
-import { ErrorBoundary } from '~/components/ui/appError/ErrorBoundary'
-import {
-	Statsborgerskap,
-	UtflyttingFraNorge,
-	InnflyttingTilNorge,
-	HentPerson,
-} from '~/pages/gruppe/PersonVisning/PersonMiljoeinfo/PdlDataTyper'
+import { HentPerson } from '~/pages/gruppe/PersonVisning/PersonMiljoeinfo/PdlDataTyper'
 import { PdlStatsborgerskap } from '~/components/fagsystem/pdl/visning/partials/nasjonalitet/PdlStatsborgerskap'
 import { PdlInnflytting } from '~/components/fagsystem/pdl/visning/partials/nasjonalitet/PdlInnflytting'
 import { PdlUtflytting } from '~/components/fagsystem/pdl/visning/partials/nasjonalitet/PdlUtflytting'
@@ -34,42 +25,6 @@ export const PdlNasjonalitet = ({ data, visTittel = true }: NasjonalitetProps) =
 			<PdlStatsborgerskap statsborgerskapListe={statsborgerskap} />
 			<PdlInnflytting innflytting={innflyttingTilNorge} />
 			<PdlUtflytting utflytting={utflyttingFraNorge} />
-			{/*{innflyttingTilNorge?.length > 0 && (*/}
-			{/*	<ErrorBoundary>*/}
-			{/*		<DollyFieldArray data={innflyttingTilNorge} header={'Innvandret'} nested>*/}
-			{/*			{(innvandring: InnflyttingTilNorge) => (*/}
-			{/*				<>*/}
-			{/*					<TitleValue*/}
-			{/*						title="Fraflyttingsland"*/}
-			{/*						value={innvandring.fraflyttingsland}*/}
-			{/*						kodeverk={AdresseKodeverk.InnvandretUtvandretLand}*/}
-			{/*					/>*/}
-			{/*					<TitleValue*/}
-			{/*						title="Fraflyttingssted"*/}
-			{/*						value={innvandring.fraflyttingsstedIUtlandet}*/}
-			{/*					/>*/}
-			{/*				</>*/}
-			{/*			)}*/}
-			{/*		</DollyFieldArray>*/}
-			{/*	</ErrorBoundary>*/}
-			{/*)}*/}
-
-			{/*{utflyttingFraNorge?.length > 0 && (*/}
-			{/*	<ErrorBoundary>*/}
-			{/*		<DollyFieldArray data={utflyttingFraNorge} header={'Utvandret'} nested>*/}
-			{/*			{(utvandring: UtflyttingFraNorge) => (*/}
-			{/*				<>*/}
-			{/*					<TitleValue*/}
-			{/*						title="Tilflyttingsland"*/}
-			{/*						value={utvandring.tilflyttingsland}*/}
-			{/*						kodeverk={AdresseKodeverk.InnvandretUtvandretLand}*/}
-			{/*					/>*/}
-			{/*					<TitleValue title="Tilflyttingssted" value={utvandring.tilflyttingsstedIUtlandet} />*/}
-			{/*				</>*/}
-			{/*			)}*/}
-			{/*		</DollyFieldArray>*/}
-			{/*	</ErrorBoundary>*/}
-			{/*)}*/}
 		</div>
 	)
 }
