43a835f302a3065fcd634606410f8d68cc79a0d1
Aileen Hay <aileen.hay@nav.no>
2022-01-31 12:16:42 +0100

    Skriveleif

--- apps/person-search-service/src/main/java/no/nav/registre/testnorge/personsearchservice/adapter/PersonSearchAdapter.java
+++ apps/person-search-service/src/main/java/no/nav/registre/testnorge/personsearchservice/adapter/PersonSearchAdapter.java
@@ -174,7 +174,7 @@ public class PersonSearchAdapter {
                                 QueryBuilders.matchQuery("hentPerson.sivilstand.type", value),
                                 ScoreMode.Avg
                         ));
-                        addHistoriskQuery(queryBuilder. "hentPerson.sivilstand");
+                        addHistoriskQuery(queryBuilder, "hentPerson.sivilstand");
                     }
                 });
     }
