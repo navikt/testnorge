221b6235a4ad475633bd7c675c5337d62fbc635d
Aileen Hay <aileen.hay@nav.no>
2022-02-02 11:35:41 +0100

    Endret title tilbake

--- apps/dolly-frontend/src/main/js/src/components/Title/Title.tsx
+++ apps/dolly-frontend/src/main/js/src/components/Title/Title.tsx
@@ -11,8 +11,14 @@ const Title = styled.div`
 	align-items: flex-end;
 `
 
-export default ({ title }: Props) => (
+const BetaTag = styled.h3`
+	margin-bottom: 20px;
+	padding-bottom: 8px;
+`
+
+export default ({ title, beta = false }: Props) => (
 	<Title>
 		<h1>{title}</h1>
+		{beta && <BetaTag>(beta)</BetaTag>}
 	</Title>
 )
