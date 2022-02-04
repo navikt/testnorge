216b3dd005fceb8f568ef35f2d095bd18d58d13d
Aileen Hay <aileen.hay@nav.no>
2022-02-04 15:24:52 +0100

    Lagt til test og smaafiks

--- apps/synt-vedtakshistorikk-service/src/main/java/no/nav/testnav/apps/syntvedtakshistorikkservice/SyntVedtakshistorikkServiceApplicationStarter.java
+++ apps/synt-vedtakshistorikk-service/src/main/java/no/nav/testnav/apps/syntvedtakshistorikkservice/SyntVedtakshistorikkServiceApplicationStarter.java
@@ -1,18 +1,8 @@
 package no.nav.testnav.apps.syntvedtakshistorikkservice;
 
-import no.nav.testnav.libs.reactivecore.config.CoreConfig;
-import no.nav.testnav.libs.reactivesecurity.config.SecureOAuth2ServerToServerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
-import org.springframework.web.reactive.config.EnableWebFlux;
 
-
-@Import({
-        CoreConfig.class,
-        SecureOAuth2ServerToServerConfiguration.class
-})
-@EnableWebFlux
 @SpringBootApplication
 public class SyntVedtakshistorikkServiceApplicationStarter {
 
--- apps/synt-vedtakshistorikk-service/src/main/java/no/nav/testnav/apps/syntvedtakshistorikkservice/provider/VedtakshistorikkController.java
+++ apps/synt-vedtakshistorikk-service/src/main/java/no/nav/testnav/apps/syntvedtakshistorikkservice/provider/VedtakshistorikkController.java
@@ -1,11 +1,15 @@
 package no.nav.testnav.apps.syntvedtakshistorikkservice.provider;
 
 import lombok.RequiredArgsConstructor;
+import no.nav.testnav.apps.syntvedtakshistorikkservice.provider.request.SyntetiserArenaRequest;
+import no.nav.testnav.apps.syntvedtakshistorikkservice.service.VedtakshistorikkService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
 
 import no.nav.testnav.libs.domain.dto.arena.testnorge.vedtak.NyttVedtakResponse;
 
@@ -17,12 +21,17 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class VedtakshistorikkController {
 
-    @GetMapping("/vedtakshistorikk")
-    public Mono<ResponseEntity<Map<String, List<NyttVedtakResponse>>>> genererVedtakshistorikk() {
-        return null;
-//        return ResponseEntity
-//                .status(HttpStatus.OK)
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .body(vedtakshistorikkService.genererVedtakshistorikk(syntetiserArenaRequest.getAvspillergruppeId(), syntetiserArenaRequest.getMiljoe(), syntetiserArenaRequest.getAntallNyeIdenter()));
+    private final VedtakshistorikkService vedtakshistorikkService;
+
+    @PostMapping("/vedtakshistorikk")
+    public ResponseEntity<Map<String, List<NyttVedtakResponse>>> genererVedtakshistorikk(
+            @RequestBody SyntetiserArenaRequest syntetiserArenaRequest
+    ) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(vedtakshistorikkService.genererVedtakshistorikk(
+                        syntetiserArenaRequest.getMiljoe(),
+                        syntetiserArenaRequest.getAntallNyeIdenter()));
     }
 }
