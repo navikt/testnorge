b10deb78eeaaddd8bcee842454c3ef52c225fd28
kristenhaerum <kristen.herum@nav.no>
2022-02-08 14:20:57 +0100

    Oppdatering av DTO for bestilling

--- libs/data-transfer-objects/src/main/java/no/nav/testnav/libs/dto/pdlforvalter/v1/BestillingRequestDTO.java
+++ libs/data-transfer-objects/src/main/java/no/nav/testnav/libs/dto/pdlforvalter/v1/BestillingRequestDTO.java
@@ -24,7 +24,6 @@ public class BestillingRequestDTO {
     private Boolean syntetisk;
 
     private String opprettFraIdent;
-    private String eksisterendeIdent;
 
     private PersonDTO person;
 }
