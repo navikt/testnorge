ffdddd7b680f9080c38c54e6f8d7b4400d446c87
Betsy Carina Traran <betsy.carina.traran@nav.no>
2022-02-14 18:03:33 +0100

    Hente pdldata på relatert person

--- apps/dolly-frontend/src/main/js/src/components/fagsystem/pdlf/visning/partials/RelatertPerson.tsx
+++ apps/dolly-frontend/src/main/js/src/components/fagsystem/pdlf/visning/partials/RelatertPerson.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { TitleValue } from '~/components/ui/titleValue/TitleValue'
 import Formatters from '~/utils/DataFormatter'
 import { AdresseKodeverk } from '~/config/kodeverk'
 import { PersonData } from '~/components/fagsystem/pdlf/PdlTypes'
+import { PdlDataVisning } from '~/pages/gruppe/PersonVisning/PersonMiljoeinfo/PdlDataVisning'
+import { DollyApi } from '~/service/Api'
 
 type RelatertPersonData = {
 	data: PersonData
@@ -11,27 +13,44 @@ type RelatertPersonData = {
 
 export const RelatertPerson = ({ data, tittel }: RelatertPersonData) => {
 	if (!data) return null
+
+	const [relatertPersonPdl, setRelatertPersonPdl] = useState(null)
+
+	useEffect(() => {
+		getRelatertPersonPdl()
+	}, [])
+
+	const getRelatertPersonPdl = async () => {
+		await DollyApi.getPersonFraPdl(data.ident).then((response) => {
+			setRelatertPersonPdl(response.data)
+		})
+	}
+
 	return (
-		<div className="person-visning_content">
-			<h4 style={{ width: '100%', marginTop: '0' }}>{tittel}</h4>
-			<TitleValue title="Ident" value={data.ident} />
-			<TitleValue title="Fornavn" value={data.navn?.[0].fornavn} />
-			<TitleValue title="Mellomnavn" value={data.navn?.[0].mellomnavn} />
-			<TitleValue title="Etternavn" value={data.navn?.[0].etternavn} />
-			<TitleValue title="Kjønn" value={data.kjoenn?.[0].kjoenn} />
-			<TitleValue
-				title="Fødselsdato"
-				value={Formatters.formatDate(data.foedsel?.[0].foedselsdato)}
-			/>
-			<TitleValue
-				title="Statsborgerskap"
-				value={data.statsborgerskap?.[0].landkode}
-				kodeverk={AdresseKodeverk.StatsborgerskapLand}
-			/>
-			<TitleValue
-				title="Gradering"
-				value={Formatters.showLabel('gradering', data.adressebeskyttelse?.[0].gradering)}
-			/>
-		</div>
+		<>
+			<div className="person-visning_content">
+				<h4 style={{ width: '100%', marginTop: '0' }}>{tittel}</h4>
+				<TitleValue title="Ident" value={data.ident} />
+				<TitleValue title="Fornavn" value={data.navn?.[0].fornavn} />
+				<TitleValue title="Mellomnavn" value={data.navn?.[0].mellomnavn} />
+				<TitleValue title="Etternavn" value={data.navn?.[0].etternavn} />
+				<TitleValue title="Kjønn" value={data.kjoenn?.[0].kjoenn} />
+				<TitleValue
+					title="Fødselsdato"
+					value={Formatters.formatDate(data.foedsel?.[0].foedselsdato)}
+				/>
+				<TitleValue
+					title="Statsborgerskap"
+					value={data.statsborgerskap?.[0].landkode}
+					kodeverk={AdresseKodeverk.StatsborgerskapLand}
+				/>
+				<TitleValue
+					title="Gradering"
+					value={Formatters.showLabel('gradering', data.adressebeskyttelse?.[0].gradering)}
+				/>
+			</div>
+			{/*<PdlPersonMiljoeInfo data={data} loading={false} />*/}
+			<PdlDataVisning pdlData={relatertPersonPdl} />
+		</>
 	)
 }
