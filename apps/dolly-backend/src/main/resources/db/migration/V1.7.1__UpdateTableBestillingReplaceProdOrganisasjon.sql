b45e92b77a0548b14524a1401327b87411af6c89
stigus <stian.gustavsson@nav.no>
2022-02-15 14:18:03 +0100

    Brukt replace for å bytte sjokkerende elektriker prod orgnr på gamle bestillinger i DB med det nye syntetiske org nummeret. Juridisk enhet er lik så den trengte ikke å byttes

---
+++ apps/dolly-backend/src/main/resources/db/migration/V1.7.1__UpdateTableBestillingReplaceProdOrganisasjon.sql
@@ -0,0 +1,6 @@
+--------------------------------
+-- U P D A T E   T A B L E S  --
+--------------------------------
+
+update bestilling
+set best_kriterier = REPLACE(best_kriterier, '824771332', '947064649');
