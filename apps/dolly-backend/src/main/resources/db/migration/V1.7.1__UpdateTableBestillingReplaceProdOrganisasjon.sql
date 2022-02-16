3617d340e29be3780a6e68bcb0dae3258d1db264
stigus <stian.gustavsson@nav.no>
2022-02-16 10:21:04 +0100

    Gjort flyway mer robust med hjelp av Kristen

--- apps/dolly-backend/src/main/resources/db/migration/V1.7.1__UpdateTableBestillingReplaceProdOrganisasjon.sql
+++ apps/dolly-backend/src/main/resources/db/migration/V1.7.1__UpdateTableBestillingReplaceProdOrganisasjon.sql
@@ -3,4 +3,9 @@
 --------------------------------
 
 update bestilling
-set best_kriterier = REPLACE(best_kriterier, '824771332', '947064649');
+set best_kriterier = REPLACE(best_kriterier, '"virksomhetsnummer":"824771332"', '"virksomhetsnummer":"947064649"');
+update bestilling
+set best_kriterier = REPLACE(best_kriterier, '"virksomhet":"824771332"', '"virksomhet":"947064649"');
+update bestilling
+set best_kriterier = REPLACE(best_kriterier, '"orgnummer":"824771332"', '"orgnummer":"947064649"');
+commit;
