41941495bd30cf743703aa181667115e9bd52cab
Stian Gustavsson <66469591+stigus@users.noreply.github.com>
2022-02-16 11:14:32 +0100

    Brukt replace for å bytte sjokkerende elektriker prod orgnr på gamle … (#2130)
    * Brukt replace for å bytte sjokkerende elektriker prod orgnr på gamle bestillinger i DB med det nye syntetiske org nummeret. Juridisk enhet er lik så den trengte ikke å byttes
    * Hardkodet returnerte miljoer fra miljoer service
    * Gjort flyway mer robust med hjelp av Kristen

--- apps/miljoer-service/src/main/java/no/nav/registre/testnorge/miljoerservice/provider/MiljoeController.java
+++ apps/miljoer-service/src/main/java/no/nav/registre/testnorge/miljoerservice/provider/MiljoeController.java
@@ -2,7 +2,7 @@ package no.nav.registre.testnorge.miljoerservice.provider;
 
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import no.nav.registre.testnorge.miljoerservice.consumer.TpsfConsumer;
+import no.nav.registre.testnorge.miljoerservice.service.MiljoerService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,11 +15,11 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MiljoeController {
 
-    private final TpsfConsumer tpsfConsumer;
+    private final MiljoerService miljoerService;
 
     @GetMapping
     @Operation(description = "Tjeneste for å sjekke hvilke miljøer i test og preprod som er tilgjengelige nå")
     public ResponseEntity<List<String>> hentAktiveMiljoer() {
-        return ResponseEntity.ok(tpsfConsumer.getAktiveMiljoer().getEnvironments());
+        return ResponseEntity.ok(miljoerService.getMiljoer());
     }
 }
