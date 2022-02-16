c3438e84a45458c6d46d82089d19a52e1e854fe6
Aileen Hay <aileen.hay@nav.no>
2022-02-16 12:27:57 +0100

    Fjernet ubrukt import

--- apps/testnorge-aareg/src/main/java/no/nav/registre/aareg/syntetisering/RsSyntetiskPeriode.java
+++ apps/testnorge-aareg/src/main/java/no/nav/registre/aareg/syntetisering/RsSyntetiskPeriode.java
@@ -1,7 +1,6 @@
 package no.nav.registre.aareg.syntetisering;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
