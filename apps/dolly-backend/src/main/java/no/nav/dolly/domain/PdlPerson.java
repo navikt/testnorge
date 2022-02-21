754a0f5e2fe342fd2ad9735fa10e79e181ad883e
kristenhaerum <kristen.herum@nav.no>
2022-02-21 15:04:31 +0100

    Fiks pga endret kodebase II

--- apps/dolly-backend/src/main/java/no/nav/dolly/domain/PdlPerson.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/domain/PdlPerson.java
@@ -1,5 +1,6 @@
 package no.nav.dolly.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.AllArgsConstructor;
@@ -519,6 +520,7 @@ public class PdlPerson {
         private UtenlandskAdresseIFrittFormat utenlandskAdresseIFrittFormat;
 
         @Override
+        @JsonIgnore
         public boolean isAdresseNorge() {
             return false;
         }
