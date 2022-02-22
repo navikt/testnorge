246ed3fdad0b5edad72ba6371baf2254c2ea49cd
stigus <stian.gustavsson@nav.no>
2022-02-22 18:34:42 +0100

    Tester ut Wonderwall sidecar på dev

--- libs/reactive-frontend/src/main/java/no/nav/testnav/libs/reactivefrontend/filter/AddAuthenticationHeaderToRequestGatewayFilterFactory.java
+++ libs/reactive-frontend/src/main/java/no/nav/testnav/libs/reactivefrontend/filter/AddAuthenticationHeaderToRequestGatewayFilterFactory.java
@@ -2,7 +2,6 @@ package no.nav.testnav.libs.reactivefrontend.filter;
 
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
-import org.springframework.http.HttpHeaders;
 import reactor.core.publisher.Mono;
 
 public class AddAuthenticationHeaderToRequestGatewayFilterFactory extends AbstractGatewayFilterFactory<GenerateToken> {
@@ -10,7 +9,8 @@ public class AddAuthenticationHeaderToRequestGatewayFilterFactory extends Abstra
     public GatewayFilter apply(GenerateToken generateToken) {
         return (exchange, chain) -> generateToken
                 .getToken(exchange)
-                .map(token -> exchange.getRequest().mutate().header(HttpHeaders.AUTHORIZATION, "Bearer " + token).build())
+//                .map(token -> exchange.getRequest().mutate().header(HttpHeaders.AUTHORIZATION, "Bearer " + token).build())
+                .map(token -> exchange.getRequest().mutate().header("Auth2", "Bearer " + token).build())
                 .flatMap(value -> chain.filter(exchange.mutate().request(value).build()))
                 .switchIfEmpty(Mono.defer(() -> chain.filter(exchange)));
     }
