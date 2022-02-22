aa9bb859741c84e1f718b131f84d063b23f4e58b
kristenhaerum <kristen.herum@nav.no>
2022-02-22 07:50:39 +0100

    Oppdatert test

--- apps/tps-messaging-service/src/test/java/no/nav/testnav/apps/tpsmessagingservice/ApplicationContextTest.java
+++ apps/tps-messaging-service/src/test/java/no/nav/testnav/apps/tpsmessagingservice/ApplicationContextTest.java
@@ -1,5 +1,6 @@
 package no.nav.testnav.apps.tpsmessagingservice;
 
+import no.nav.testnav.libs.servletsecurity.exchange.TokenExchange;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -12,6 +13,9 @@ class ApplicationContextTest {
 
     @MockBean
     public JwtDecoder jwtDecoder;
+
+    @MockBean
+    public TokenExchange exchange;
 
     @Test
     @SuppressWarnings("java:S2699")
