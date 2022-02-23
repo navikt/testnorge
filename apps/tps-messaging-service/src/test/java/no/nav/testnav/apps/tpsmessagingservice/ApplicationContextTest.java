f3271bc3dcfa1a071f4010567675d737701cb778
Kristen Hærum <kristen.herum@nav.no>
2022-02-23 08:06:42 +0100

    Feature/oppdatere miljoer (#2145)
    * Bytte fra innebygd til ekstern testmiljoer i tps messaging service

--- apps/tps-messaging-service/src/test/java/no/nav/testnav/apps/tpsmessagingservice/ApplicationContextTest.java
+++ apps/tps-messaging-service/src/test/java/no/nav/testnav/apps/tpsmessagingservice/ApplicationContextTest.java
@@ -1,5 +1,6 @@
 package no.nav.testnav.apps.tpsmessagingservice;
 
+import no.nav.testnav.libs.servletsecurity.exchange.TokenExchange;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,6 +15,9 @@ class ApplicationContextTest {
 
     @MockBean
     public JwtDecoder jwtDecoder;
+
+    @MockBean
+    public TokenExchange exchange;
 
     @Test
     @SuppressWarnings("java:S2699")
