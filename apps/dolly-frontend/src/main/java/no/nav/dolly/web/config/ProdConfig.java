db524ca9cfbeb5c59594067846dd8f3cd4325c48
stigus <stian.gustavsson@nav.no>
2022-02-23 09:40:07 +0100

    Tester ut Wonderwall sidecar på dev

--- apps/dolly-frontend/src/main/java/no/nav/dolly/web/config/ProdConfig.java
+++ apps/dolly-frontend/src/main/java/no/nav/dolly/web/config/ProdConfig.java
@@ -1,26 +1,10 @@
 package no.nav.dolly.web.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
-
-import no.nav.testnav.libs.reactivesessionsecurity.config.OicdRedisSessionConfiguration;
-import redis.clients.jedis.Jedis;
 
 @Configuration
 @Profile("prod")
-@Import({
-        OicdRedisSessionConfiguration.class
-})
 public class ProdConfig {
 
-    @Bean
-    public Jedis jedis(
-            @Value("${spring.redis.host}") String host,
-            @Value("${spring.redis.port}") Integer port
-    ){
-        return new Jedis(host, port);
-    }
 }
