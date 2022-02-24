787d35dd8c52d9225814333338a9dedae587aa8c
stigus <stian.gustavsson@nav.no>
2022-02-24 16:32:15 +0100

    Logger hvilke cookies som ikke blir satt

--- libs/reactive-frontend/src/main/java/no/nav/testnav/libs/reactivefrontend/filter/IgnoreSetCookieFilter.java
+++ libs/reactive-frontend/src/main/java/no/nav/testnav/libs/reactivefrontend/filter/IgnoreSetCookieFilter.java
@@ -13,8 +13,9 @@ public class IgnoreSetCookieFilter implements WebFilter {
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
         exchange.getResponse().beforeCommit(() -> {
             if (exchange.getResponse().getHeaders().containsKey("set-cookie")) {
+                log.warn("Fjerner 'set-cookie' fra remote response siden vi ønsker at frontend skal håndtere cookies selv, ikke fra en remote app. Cookie: {}",
+                        exchange.getResponse().getHeaders().get("set-cookie"));
                 exchange.getResponse().getHeaders().remove("set-cookie");
-                log.warn("Fjerner 'set-cookie' fra remote response siden vi ønsker at frontend skal håndtere cookies selv, ikke fra en remote app.");
             }
             return Mono.empty();
         });
