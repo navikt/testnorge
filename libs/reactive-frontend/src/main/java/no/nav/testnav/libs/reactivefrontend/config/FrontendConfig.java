35fe98be1fb632658a1d0aad3075479990480b7a
stigus <stian.gustavsson@nav.no>
2022-02-24 16:05:53 +0100

    Revert "Satt tilbake ignorering av cookies, fungerer ikke etter deploy"
    This reverts commit 843bf61c4933768f8af5dcd4f586be0ecdc40d44.

--- libs/reactive-frontend/src/main/java/no/nav/testnav/libs/reactivefrontend/config/FrontendConfig.java
+++ libs/reactive-frontend/src/main/java/no/nav/testnav/libs/reactivefrontend/config/FrontendConfig.java
@@ -1,13 +1,11 @@
 package no.nav.testnav.libs.reactivefrontend.config;
 
-import no.nav.testnav.libs.reactivefrontend.filter.IgnoreSetCookieFilter;
 import no.nav.testnav.libs.reactivefrontend.filter.SessionTimeoutCookieFilter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Import({
         SessionTimeoutCookieFilter.class,
-        IgnoreSetCookieFilter.class
 })
 @Configuration
 public class FrontendConfig {
