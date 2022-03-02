a2f7b3c0c617c7684cc4984047f330bbd04c60d2
stigus <stian.gustavsson@nav.no>
2022-03-02 12:11:47 +0100

    Tester redis kickout etter ett minutt på dev

--- libs/reactive-session-security/src/main/java/no/nav/testnav/libs/reactivesessionsecurity/resolver/RedisTokenResolver.java
+++ libs/reactive-session-security/src/main/java/no/nav/testnav/libs/reactivesessionsecurity/resolver/RedisTokenResolver.java
@@ -31,7 +31,7 @@ public class RedisTokenResolver extends Oauth2AuthenticationToken implements Tok
                         .mapNotNull(oAuth2AuthorizedClient -> {
                             log.info("Henter token fra Redis som utløper: {}", oAuth2AuthorizedClient.getAccessToken().getExpiresAt());
 //                    if (oAuth2AuthorizedClient.getAccessToken().getExpiresAt().isBefore(ZonedDateTime.now().toInstant().plusSeconds(180))) {
-                            if (oAuth2AuthorizedClient.getAccessToken().getIssuedAt().isBefore(ZonedDateTime.now().toInstant().plusSeconds(80))) {
+                            if (oAuth2AuthorizedClient.getAccessToken().getIssuedAt().isAfter(ZonedDateTime.now().toInstant().plusSeconds(80))) {
                                 log.warn("Auth client har utløpt, fjerner den som authenticated");
                                 authenticationToken.setAuthenticated(false);
                                 authenticationToken.eraseCredentials();
