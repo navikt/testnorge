d663e7446c46c945d32bd9164cb44d22a586293f
stigus <stian.gustavsson@nav.no>
2022-03-04 11:23:20 +0100

    Amelding treghet

--- apps/amelding-service/src/main/java/no/nav/registre/testnav/ameldingservice/config/SecurityConfig.java
+++ apps/amelding-service/src/main/java/no/nav/registre/testnav/ameldingservice/config/SecurityConfig.java
@@ -3,33 +3,38 @@ package no.nav.registre.testnav.ameldingservice.config;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.testnav.libs.reactivesecurity.manager.JwtReactiveAuthenticationManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Slf4j
 @Configuration
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
 @RequiredArgsConstructor
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
     private final JwtReactiveAuthenticationManager jwtReactiveAuthenticationManager;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/api/**")
-                .fullyAuthenticated()
-                .and().oauth2ResourceServer()
-                .jwt(spec -> spec.authenticationManager((AuthenticationManager) jwtReactiveAuthenticationManager))
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        return http.csrf().disable()
+                .authorizeExchange()
+                .pathMatchers(
+                        "/swagger-ui.html",
+                        "/swagger-ui/**",
+                        "/webjars/**",
+                        "/v3/api-docs/**",
+                        "/internal/isReady",
+                        "/internal/isAlive"
+                ).permitAll()
+                .anyExchange().authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt(spec -> spec.authenticationManager(jwtReactiveAuthenticationManager))
                 .and().build();
     }
 }
