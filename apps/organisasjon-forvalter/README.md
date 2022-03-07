ee03a00cba013049ec4471cb56bd8556fa390106
Aileen Hay <aileen.hay@nav.no>
2022-03-07 14:43:18 +0100

    Lagd endepunkt for aa hente alle orgs for brukerid

--- apps/organisasjon-forvalter/README.md
+++ apps/organisasjon-forvalter/README.md
@@ -6,3 +6,16 @@ grand_parent: Applikasjoner
 ---
 
 # Organisasjon Forvalter
+
+
+## Lokal kjøring
+Ha naisdevice kjørende og kjør OrganisasjonForvalterApplicationStarter med følgende argumenter:
+``` 
+-Dspring.profiles.active=dev
+-Dspring.cloud.vault.token=[vault-token]
+```
+
+og legg til i VM options:
+``` 
+--add-opens java.base/java.lang=ALL-UNNAMED
+``` 
--- apps/organisasjon-forvalter/src/main/java/no/nav/organisasjonforvalter/service/OrganisasjonService.java
+++ apps/organisasjon-forvalter/src/main/java/no/nav/organisasjonforvalter/service/OrganisasjonService.java
@@ -3,10 +3,13 @@ package no.nav.organisasjonforvalter.service;
 import lombok.RequiredArgsConstructor;
 import ma.glasnost.orika.MapperFacade;
 import no.nav.organisasjonforvalter.dto.responses.RsOrganisasjon;
+import no.nav.organisasjonforvalter.dto.responses.UnderenhetResponse;
+import no.nav.organisasjonforvalter.jpa.entity.Organisasjon;
 import no.nav.organisasjonforvalter.jpa.repository.OrganisasjonRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -18,5 +21,9 @@ public class OrganisasjonService {
     public List<RsOrganisasjon> getOrganisasjoner(List<String> orgnumre) {
 
         return mapperFacade.mapAsList(organisasjonRepository.findAllByOrganisasjonsnummerIn(orgnumre), RsOrganisasjon.class);
+    }
+
+    public List<RsOrganisasjon> getOrganisasjoner(String brukerid) {
+        return mapperFacade.mapAsList(organisasjonRepository.findOrganisasjonerByBrukerId(brukerid), RsOrganisasjon.class);
     }
 }
