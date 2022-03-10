5e0f11e331161b2310de45139fab1ab6b20ddc31
Aileen Hay <58416744+H153910@users.noreply.github.com>
2022-03-10 09:21:44 +0100

    Dolly frontend/oppdatere org valg (#2183)
    Oppdatert org valg

--- apps/dolly-frontend/src/main/js/src/ducks/environments/index.js
+++ apps/dolly-frontend/src/main/js/src/ducks/environments/index.js
@@ -17,7 +17,7 @@ const initialState = {
 export default handleActions(
 	{
 		[onSuccess(getEnvironments)](state, action) {
-			state.data = _getEnvironmentsSortedByType(action.payload.data)
+			state.data = _getEnvironmentsSortedByType(action.payload)
 		},
 	},
 	initialState
