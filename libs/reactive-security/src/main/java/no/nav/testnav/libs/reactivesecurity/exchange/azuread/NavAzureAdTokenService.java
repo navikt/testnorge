0140feb6512f9f6d118748423d0477423d981812
Aileen Hay <aileen.hay@nav.no>
2022-03-15 13:02:48 +0100

    Test oppsett

--- libs/reactive-security/src/main/java/no/nav/testnav/libs/reactivesecurity/exchange/azuread/NavAzureAdTokenService.java
+++ libs/reactive-security/src/main/java/no/nav/testnav/libs/reactivesecurity/exchange/azuread/NavAzureAdTokenService.java
@@ -29,14 +29,13 @@ public class NavAzureAdTokenService implements ExchangeToken {
 
     public NavAzureAdTokenService(
             @Value("${http.proxy:#{null}}") String proxyHost,
-            @Value("${AAD_ISSUER_URI}") String issuerUrl,
             AzureNavProxyClientCredential azureNavProxyClientCredential
     ) {
         this.clientCredential = azureNavProxyClientCredential;
         log.info("Init AzureAd Nav token service.");
         WebClient.Builder builder = WebClient
                 .builder()
-                .baseUrl(issuerUrl + "/oauth2/v2.0/token")
+                .baseUrl(azureNavProxyClientCredential.getTokenEndpoint())
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE);
 
         if (proxyHost != null) {
