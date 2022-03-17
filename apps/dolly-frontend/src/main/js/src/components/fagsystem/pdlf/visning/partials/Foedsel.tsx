3852990eb74197fa7174a256b7aadd7b82a11ff1
Betsy Carina Traran <betsy.carina.traran@nav.no>
2022-03-17 16:31:13 +0100

    FÃosForsøk på visning av endret person

--- apps/dolly-frontend/src/main/js/src/components/fagsystem/pdlf/visning/partials/Foedsel.tsx
+++ apps/dolly-frontend/src/main/js/src/components/fagsystem/pdlf/visning/partials/Foedsel.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { TitleValue } from '~/components/ui/titleValue/TitleValue'
 import SubOverskrift from '~/components/ui/subOverskrift/SubOverskrift'
 import { ErrorBoundary } from '~/components/ui/appError/ErrorBoundary'
@@ -16,6 +16,8 @@ import { FoedselForm } from '~/components/fagsystem/pdlf/form/partials/foedsel/F
 import { Formik } from 'formik'
 import { initialFoedsel } from '~/components/fagsystem/pdlf/form/initialValues'
 import ModalActionKnapper from '~/components/ui/modal/ModalActionKnapper'
+import { PdlforvalterApi } from '~/service/Api'
+import Loading from '~/components/ui/loading/Loading'
 
 type FoedselTypes = {
 	data: Array<FoedselData>
@@ -28,52 +30,70 @@ type FoedselVisningTypes = {
 
 enum Modus {
 	Les = 'LES',
+	LesEndret = 'LES_ENDRET',
 	Skriv = 'SKRIV',
+	Loading = 'LOADING',
 }
 
 export const Foedsel = ({ data, put, fetch }: FoedselTypes) => {
 	if (!data || data.length === 0) return null
 	const [visningModus, setVisningModus] = useState(Modus.Les)
+	const [endretFoedsel, setEndretFoedsel] = useState(null)
 
 	const initFoedsel = Object.assign(initialFoedsel, data[0]) //TODO hent fra riktig objekt
-	const initialValues = { foedsel: initFoedsel }
+	const initialValues = endretFoedsel ? { foedsel: endretFoedsel } : { foedsel: initFoedsel }
+
+	// useEffect(() => {}, [endretFoedsel])
 
 	const handleSubmit = async (data) => {
-		console.log('data xxxxxxxx: ', data) //TODO - SLETT MEG
+		setVisningModus(Modus.Loading)
+		// console.log('data xxxxxxxx: ', data) //TODO - SLETT MEG
+
 		const attributt = Object.keys(data)[0]
 		await put(attributt, data.foedsel.id, data.foedsel)
-		// const status = await put(attributt, data.foedsel.id, data.foedsel)
-		// console.log('status: ', status) //TODO - SLETT MEG
-		await fetch()
-		setVisningModus(Modus.Les)
+		// await fetch()
+		const hentPerson = await PdlforvalterApi.getPersoner(['19087908022'])
+		console.log('hentPerson: ', hentPerson) //TODO - SLETT MEG
+		await setEndretFoedsel(hentPerson.data[0].person.foedsel[0])
+		setVisningModus(Modus.LesEndret)
 	}
+
+	console.log('endretFoedsel: ', endretFoedsel) //TODO - SLETT MEG
+	console.log('initFoedsel: ', initFoedsel) //TODO - SLETT MEG
+	console.log('initialValues: ', initialValues) //TODO - SLETT MEG
+
+	const FoedselLes = ({ foedsel, idx }) => (
+		<div className="person-visning_content" key={idx}>
+			<TitleValue title="Fødselsdato" value={Formatters.formatDate(foedsel.foedselsdato)} />
+			<TitleValue title="Fødselsår" value={foedsel.foedselsaar} />
+			<TitleValue title="Fødested" value={foedsel.foedested} />
+			<TitleValue title="Fødekommune">
+				{foedsel.fodekommune && (
+					<KodeverkConnector navn="Kommuner" value={foedsel.fodekommune}>
+						{(v: Kodeverk, verdi: KodeverkValues) => (
+							<span>{verdi ? verdi.label : foedsel.fodekommune}</span>
+						)}
+					</KodeverkConnector>
+				)}
+			</TitleValue>
+			<TitleValue
+				title="Fødeland"
+				value={foedsel.foedeland}
+				kodeverk={AdresseKodeverk.StatsborgerskapLand}
+			/>
+			<Button kind="edit" onClick={() => setVisningModus(Modus.Skriv)} />
+			{/*<Button kind="trashcan" onClick={() => console.log('klikk slett!')} />*/}
+		</div>
+	)
 
 	const FoedselVisning = ({ item, idx }: FoedselVisningTypes) => {
 		return (
 			<>
-				{visningModus === Modus.Les && (
-					<div className="person-visning_content" key={idx}>
-						<TitleValue title="Fødselsdato" value={Formatters.formatDate(item.foedselsdato)} />
-						<TitleValue title="Fødselsår" value={item.foedselsaar} />
-						<TitleValue title="Fødested" value={item.foedested} />
-						<TitleValue title="Fødekommune">
-							{item.fodekommune && (
-								<KodeverkConnector navn="Kommuner" value={item.fodekommune}>
-									{(v: Kodeverk, verdi: KodeverkValues) => (
-										<span>{verdi ? verdi.label : item.fodekommune}</span>
-									)}
-								</KodeverkConnector>
-							)}
-						</TitleValue>
-						<TitleValue
-							title="Fødeland"
-							value={item.foedeland}
-							kodeverk={AdresseKodeverk.StatsborgerskapLand}
-						/>
-						<Button kind="edit" onClick={() => setVisningModus(Modus.Skriv)} />
-						{/*<Button kind="trashcan" onClick={() => console.log('klikk slett!')} />*/}
-					</div>
+				{visningModus === Modus.Loading && <Loading label="Endrer....." />}
+				{visningModus === Modus.LesEndret && endretFoedsel && (
+					<FoedselLes foedsel={endretFoedsel} idx={idx} />
 				)}
+				{visningModus === Modus.Les && <FoedselLes foedsel={item} idx={idx} />}
 				{visningModus === Modus.Skriv && (
 					<Formik initialValues={initialValues} onSubmit={handleSubmit} enableReinitialize>
 						{(formikBag) => {
