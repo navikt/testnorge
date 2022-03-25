efaa2c56347c7a6a6f8c319d2a909da4b4426636
Aileen Hay <aileen.hay@nav.no>
2022-03-25 09:37:09 +0100

    Byttet til insecure serverlet security

--- apps/synt-vedtakshistorikk-service/src/main/java/no/nav/testnav/apps/syntvedtakshistorikkservice/config/ApplicationConfig.java
+++ apps/synt-vedtakshistorikk-service/src/main/java/no/nav/testnav/apps/syntvedtakshistorikkservice/config/ApplicationConfig.java
@@ -1,14 +1,15 @@
 package no.nav.testnav.apps.syntvedtakshistorikkservice.config;
 
 import no.nav.testnav.libs.servletcore.config.ApplicationCoreConfig;
-import no.nav.testnav.libs.servletsecurity.config.SecureOAuth2ServerToServerConfiguration;
+import no.nav.testnav.libs.servletsecurity.config.InsecureJwtServerToServerConfiguration;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({
+@Import(value = {
         ApplicationCoreConfig.class,
-        SecureOAuth2ServerToServerConfiguration.class
+        InsecureJwtServerToServerConfiguration.class
 })
 public class ApplicationConfig {
 }
