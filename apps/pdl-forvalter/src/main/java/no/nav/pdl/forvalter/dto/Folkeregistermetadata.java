de90d3cb44c6b7d634dede081f559ae03f848419
kristenhaerum <kristen.herum@nav.no>
2022-03-28 15:15:00 +0200

    Lagt til manglende serializable

--- apps/pdl-forvalter/src/main/java/no/nav/pdl/forvalter/dto/Folkeregistermetadata.java
+++ apps/pdl-forvalter/src/main/java/no/nav/pdl/forvalter/dto/Folkeregistermetadata.java
@@ -5,13 +5,14 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Folkeregistermetadata {
+public class Folkeregistermetadata implements Serializable {
 
     private LocalDate ajourholdstidspunkt;
     private Boolean gjeldende;
