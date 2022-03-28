4b3c79ff45119b5c70c3155f565c112eb63df596
Aileen Hay <aileen.hay@nav.no>
2022-03-28 14:31:10 +0200

    Fork join pool bean

---
+++ apps/synt-vedtakshistorikk-service/src/main/java/no/nav/testnav/apps/syntvedtakshistorikkservice/config/ForkJoinWorkerThreadFactory.java
@@ -0,0 +1,22 @@
+package no.nav.testnav.apps.syntvedtakshistorikkservice.config;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinWorkerThread;
+
+public class ForkJoinWorkerThreadFactory implements ForkJoinPool.ForkJoinWorkerThreadFactory {
+
+    @Override
+    public final ForkJoinWorkerThread newThread(ForkJoinPool pool) {
+
+        return new MyForkJoinWorkerThread(pool);
+    }
+
+    private static final class MyForkJoinWorkerThread extends ForkJoinWorkerThread {
+
+        private MyForkJoinWorkerThread(final ForkJoinPool pool) {
+            super(pool);
+            // set the correct classloader here
+            setContextClassLoader(Thread.currentThread().getContextClassLoader());
+        }
+    }
+}
