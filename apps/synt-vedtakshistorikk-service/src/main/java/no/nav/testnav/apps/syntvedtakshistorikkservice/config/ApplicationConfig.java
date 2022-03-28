c38124aa54700e88275d82f6faa37f0aa5e01f76
Aileen Hay <aileen.hay@nav.no>
2022-03-28 09:18:42 +0200

    Byttet til secure oath

--- apps/synt-vedtakshistorikk-service/src/main/java/no/nav/testnav/apps/syntvedtakshistorikkservice/config/ApplicationConfig.java
+++ apps/synt-vedtakshistorikk-service/src/main/java/no/nav/testnav/apps/syntvedtakshistorikkservice/config/ApplicationConfig.java
@@ -1,15 +1,15 @@
 package no.nav.testnav.apps.syntvedtakshistorikkservice.config;
 
 import no.nav.testnav.libs.servletcore.config.ApplicationCoreConfig;
-import no.nav.testnav.libs.servletsecurity.config.InsecureJwtServerToServerConfiguration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import no.nav.testnav.libs.servletsecurity.config.SecureOAuth2ServerToServerConfiguration;
 
 @Configuration
 @Import(value = {
         ApplicationCoreConfig.class,
-        InsecureJwtServerToServerConfiguration.class
+        SecureOAuth2ServerToServerConfiguration.class
 })
 public class ApplicationConfig {
 }
