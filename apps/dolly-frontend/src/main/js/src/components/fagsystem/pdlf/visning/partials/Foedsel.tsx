3ffd858c21c6ab8adb7657a3479b428a2c2295b0
Betsy Carina Traran <betsy.carina.traran@nav.no>
2022-03-30 11:21:51 +0200

    Styling

--- apps/dolly-frontend/src/main/js/src/components/fagsystem/pdlf/visning/partials/Foedsel.tsx
+++ apps/dolly-frontend/src/main/js/src/components/fagsystem/pdlf/visning/partials/Foedsel.tsx
@@ -15,11 +15,10 @@ import Button from '~/components/ui/button/Button'
 import { FoedselForm } from '~/components/fagsystem/pdlf/form/partials/foedsel/Foedsel'
 import { Formik } from 'formik'
 import { initialFoedsel } from '~/components/fagsystem/pdlf/form/initialValues'
-import ModalActionKnapper from '~/components/ui/modal/ModalActionKnapper'
 import Loading from '~/components/ui/loading/Loading'
 import _get from 'lodash/get'
 import styled from 'styled-components'
-import { useAsync } from 'react-use'
+import NavButton from '~/components/ui/button/NavButton/NavButton'
 
 type FoedselTypes = {
 	data: Array<FoedselData>
@@ -36,58 +35,57 @@ enum Modus {
 	Loading = 'LOADING',
 }
 
-const EditFormButtons = styled(ModalActionKnapper)`
-	position: relative;
-	.dollymodal {
-		background-color: red;
-
-		&_buttons {
-			margin: 20px;
-
-			.button {
-				position: relative;
-			}
+const FieldArrayEdit = styled.div`
+	&&& {
+		button {
+			position: relative;
+			top: 0;
+			right: 0;
+			margin-right: 10px;
 		}
 	}
 `
 
+const Knappegruppe = styled.div`
+	margin: 10px 0 5px 0;
+	align-content: baseline;
+`
+
 export const Foedsel = ({ data, put, fetch, tmpPersoner, ident }: FoedselTypes) => {
 	if (!data || data.length === 0) return null
+
 	const [visningModus, setVisningModus] = useState(Modus.Les)
 	const [errorMessage, setErrorMessage] = useState(null)
-	console.log('errorMessage: ', errorMessage) //TODO - SLETT MEG
+
 	const initFoedsel = Object.assign(initialFoedsel, data[0]) //TODO hent fra riktig objekt
 	const initialValues = { foedsel: initFoedsel }
 
-	// const handleSubmit = useAsync(async (data) => {
-	// 	if (!data) return
-	// 	setVisningModus(Modus.Loading)
-	// 	const attributt = Object.keys(data)[0]
-	// 	await put(attributt, data.foedsel.id, data.foedsel)
-	// 	await fetch()
-	// 	setVisningModus(Modus.Les)
-	// }, [])
+	// const foedselTest = {
+	// 	fodekommune: null,
+	// 	foedeland: 'LIER',
+	// 	foedested: null,
+	// 	foedselsaar: 1979,
+	// 	foedselsdato: '1979-08-19T00:00:00',
+	// 	kilde: 'Dolly',
+	// 	master: 'FREG',
+	// 	id: 1,
+	// }
 
 	const handleSubmit = async (data) => {
 		setVisningModus(Modus.Loading)
 		const attributt = Object.keys(data)[0]
 		await put(attributt, data.foedsel.id, data.foedsel)
-			.then((response) => {
-				if (response)
-					fetch().then((test) => {
-						if (test) setVisningModus(Modus.Les)
+			.then((putResponse) => {
+				if (putResponse)
+					fetch().then((fetchResponse) => {
+						if (fetchResponse) setVisningModus(Modus.Les)
 					})
-				// setVisningModus(Modus.Les)
 			})
-			// .then(setVisningModus(Modus.Les))
 			.catch((error) => {
-				console.log('error: ', error) //TODO - SLETT MEG
 				fetch()
 				setErrorMessage(error.toString())
 				setVisningModus(Modus.Les)
 			})
-		// console.log('test: ', test) //TODO - SLETT MEG
-		// if (test) await fetch()
 	}
 
 	const FoedselLes = ({ foedsel, idx }) => (
@@ -140,23 +138,30 @@ export const Foedsel = ({ data, put, fetch, tmpPersoner, ident }: FoedselTypes) 
 						{(formikBag) => {
 							return (
 								<>
-									<div className="flexbox--full-width">
+									<FieldArrayEdit>
 										<div className="flexbox--flex-wrap">
 											<FoedselForm formikBag={formikBag} path="foedsel" />
 										</div>
-										{/*<ModalActionKnapper*/}
-										{/*	submitknapp="Endre"*/}
-										{/*	disabled={!formikBag.isValid || formikBag.isSubmitting}*/}
-										{/*	onSubmit={() => formikBag.handleSubmit()}*/}
-										{/*	onAvbryt={() => setVisningModus(Modus.Les)}*/}
-										{/*/>*/}
-										<EditFormButtons
-											submitknapp="Endre"
-											disabled={!formikBag.isValid || formikBag.isSubmitting}
-											onSubmit={() => formikBag.handleSubmit()}
-											onAvbryt={() => setVisningModus(Modus.Les)}
-										/>
-									</div>
+										<Knappegruppe>
+											<NavButton
+												type="standard"
+												htmlType="reset"
+												onClick={() => setVisningModus(Modus.Les)}
+												disabled={!formikBag.isValid || formikBag.isSubmitting}
+												style={{ top: '1.75px' }}
+											>
+												Avbryt
+											</NavButton>
+											<NavButton
+												type="hoved"
+												htmlType="submit"
+												onClick={() => formikBag.handleSubmit()}
+												disabled={!formikBag.isValid || formikBag.isSubmitting}
+											>
+												Endre
+											</NavButton>
+										</Knappegruppe>
+									</FieldArrayEdit>
 								</>
 							)
 						}}
