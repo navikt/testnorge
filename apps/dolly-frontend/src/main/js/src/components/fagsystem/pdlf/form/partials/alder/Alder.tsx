3d50477f9ccb91d69b1bab999bbb0b687010cc6b
stigus <stian.gustavsson@nav.no>
2022-03-31 16:46:46 +0200

    Fikset en typescript warning og endret slik at alder number er godtatt fra maler

--- apps/dolly-frontend/src/main/js/src/components/fagsystem/pdlf/form/partials/alder/Alder.tsx
+++ apps/dolly-frontend/src/main/js/src/components/fagsystem/pdlf/form/partials/alder/Alder.tsx
@@ -7,6 +7,7 @@ import { FormikProps } from 'formik'
 interface AlderForm {
 	formikBag: FormikProps<{}>
 }
+
 export const Alder = ({ formikBag }: AlderForm) => {
 	const paths = {
 		alder: 'pdldata.opprettNyPerson.alder',
@@ -23,7 +24,6 @@ export const Alder = ({ formikBag }: AlderForm) => {
 
 	return (
 		<div className="flexbox--flex-wrap">
-			{/*// @ts-ignore*/}
 			<FormikTextInput name={paths.alder} type="number" label="Alder" disabled={disableAlder} />
 			<FormikDatepicker
 				name={paths.foedtEtter}
