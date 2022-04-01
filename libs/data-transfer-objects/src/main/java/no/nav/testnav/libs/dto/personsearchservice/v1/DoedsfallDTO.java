9c4847ff2d57960160e78d228527103321016733
Aileen Hay <aileen.hay@nav.no>
2022-04-01 07:58:49 +0200

    Oppdatert dto

--- libs/data-transfer-objects/src/main/java/no/nav/testnav/libs/dto/personsearchservice/v1/DoedsfallDTO.java
+++ libs/data-transfer-objects/src/main/java/no/nav/testnav/libs/dto/personsearchservice/v1/DoedsfallDTO.java
@@ -1,2 +1,16 @@
-package no.nav.testnav.libs.dto.personsearchservice.v1;public class DoedsfallDTO {
+package no.nav.testnav.libs.dto.personsearchservice.v1;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.Value;
+
+import java.time.LocalDate;
+
+@Value
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(force = true)
+public class DoedsfallDTO {
+    LocalDate doedsdato;
 }
