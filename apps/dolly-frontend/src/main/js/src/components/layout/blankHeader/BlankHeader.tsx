0d6918806f4764a77a4aed19c6dca9b96e9d0e60
Aileen Hay <aileen.hay@nav.no>
2022-04-01 13:28:05 +0200

    Dokumentasjon i login header

--- apps/dolly-frontend/src/main/js/src/components/layout/blankHeader/BlankHeader.tsx
+++ apps/dolly-frontend/src/main/js/src/components/layout/blankHeader/BlankHeader.tsx
@@ -3,6 +3,7 @@ import Icon from '~/components/ui/icon/Icon'
 // @ts-ignore
 import logo from '~/assets/img/nav-logo-hvit.png'
 import './BlankHeader.less'
+import Logger from '~/logger'
 
 export default () => {
 	return (
@@ -13,6 +14,11 @@ export default () => {
 				</div>
 				<Icon size={30} kind="dolly" className="dollysheep" />
 				<h1>Dolly</h1>
+			</div>
+			<div className="menu-links">
+				<a href="https://navikt.github.io/testnorge/applications/dolly/" target="_blank">
+					Dokumentasjon
+				</a>
 			</div>
 		</header>
 	)
