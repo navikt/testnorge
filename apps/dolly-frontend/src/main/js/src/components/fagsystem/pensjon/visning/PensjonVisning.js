a768ca5f42497947e6ed0fb3f3e06f07cd84a46c
Aileen Hay <aileen.hay@nav.no>
2022-04-11 13:30:13 +0200

    Viser periode

--- apps/dolly-frontend/src/main/js/src/components/fagsystem/pensjon/visning/PensjonVisning.js
+++ apps/dolly-frontend/src/main/js/src/components/fagsystem/pensjon/visning/PensjonVisning.js
@@ -8,13 +8,17 @@ import Panel from '~/components/ui/panel/Panel'
 
 export const PensjonVisning = ({ data, loading }) => {
 	if (loading) return <Loading label="Laster pensjonforvalter-data" />
-	if (!data || data.length === 0) return false
+	if (!data || !data.inntekter || data.inntekter.length === 0) return false
+
+	const inntektsaar = data.inntekter.map((inntekt) => inntekt.inntektAar)
+	const foerste = Math.min(...inntektsaar)
+	const siste = Math.max(...inntektsaar)
 
 	return (
 		<ErrorBoundary>
 			<SubOverskrift label="Pensjonsgivende inntekt (POPP)" iconKind="pensjon" />
 
-			<Panel heading="Pensjonsgivende inntekter">
+			<Panel heading={`Pensjonsgivende inntekter (${foerste} - ${siste})`}>
 				{' '}
 				<DollyFieldArray data={data.inntekter} nested>
 					{(inntekt, idx) => (
