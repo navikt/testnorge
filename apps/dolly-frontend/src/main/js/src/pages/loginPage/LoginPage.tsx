d5c751d2c589c9c37b738b0bc9ae3d8c701993bb
Betsy Carina Traran <betsy.carina.traran@nav.no>
2022-04-19 09:53:36 +0200

    Tilbakestill til vaarbakgrunn

--- apps/dolly-frontend/src/main/js/src/pages/loginPage/LoginPage.tsx
+++ apps/dolly-frontend/src/main/js/src/pages/loginPage/LoginPage.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import BlankHeader from '~/components/layout/blankHeader/BlankHeader'
 import LoginModal from '~/pages/loginPage/LoginModal'
 import './LoginPage.less'
-import { PaaskeBackground as Background } from '~/components/ui/background/Background'
+import { SpringBackground as Background } from '~/components/ui/background/Background'
 
-import './../../snow.scss'
+import './../../flowers.scss'
 
 export default () => {
 	return (
 		<>
 			<BlankHeader />
-			{Array.from(Array(15).keys()).map(() => (
-				<div className="snowflake" />
+			{Array.from(Array(50).keys()).map(() => (
+				<div className="flower" />
 			))}
 			<Background>
 				<LoginModal />
