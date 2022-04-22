3011e54208e727ac519354bd187c1f2e469661dc
kristenhaerum <kristen.herum@nav.no>
2022-04-22 12:35:52 +0200

    Lagt til sikkerhet

--- apps/testnav-ident-pool/src/main/java/no/nav/testnav/identpool/config/OpenApiConfig.java
+++ apps/testnav-ident-pool/src/main/java/no/nav/testnav/identpool/config/OpenApiConfig.java
@@ -1,16 +1,25 @@
 package no.nav.testnav.identpool.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import no.nav.testnav.libs.securitycore.config.UserConstant;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
+
 @Configuration
 public class OpenApiConfig implements WebMvcConfigurer {
+
+    private final static String BEARER_AUTH = "bearerAuth";
+    private final static String USER_JWT = "user-jwt";
 
     @Bean
     public OpenAPI openApi() {
@@ -32,7 +41,30 @@ public class OpenApiConfig implements WebMvcConfigurer {
                         .license(new License()
                                 .name("MIT License")
                                 .url("https://opensource.org/licenses/MIT")
-                        ));
+                        ))
+                .addSecurityItem(
+                        new SecurityRequirement()
+                                .addList(BEARER_AUTH, Arrays.asList("read", "write"))
+                                .addList(USER_JWT, Arrays.asList("read", "write"))
+                )
+                .components(
+                        new Components()
+                                .addSecuritySchemes(BEARER_AUTH,
+                                        new SecurityScheme()
+                                                .description("Legg inn token kun, uten \"Bearer \"")
+                                                .name(BEARER_AUTH)
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT"))
+                                .addSecuritySchemes(USER_JWT,
+                                        new SecurityScheme()
+                                                .name(UserConstant.USER_HEADER_JWT)
+                                                .type(SecurityScheme.Type.APIKEY)
+                                                .in(SecurityScheme.In.HEADER)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                );
     }
 
     @Override
--- apps/testnav-ident-pool/src/main/java/no/nav/testnav/identpool/config/SecurityConfig.java
+++ apps/testnav-ident-pool/src/main/java/no/nav/testnav/identpool/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
@@ -11,9 +12,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/**").permitAll()
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .csrf().disable();
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers("/api/**")
+                .fullyAuthenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
     }
 }
