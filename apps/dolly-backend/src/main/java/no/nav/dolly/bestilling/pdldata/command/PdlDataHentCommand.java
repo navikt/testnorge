8f3c1c9de6db36b1f800281b8b7c997c1479c4a8
Kristen Hærum <kristen.herum@nav.no>
2022-04-28 11:13:44 +0200

    Rette feil i Dolly backend (#2307)

--- apps/dolly-backend/src/main/java/no/nav/dolly/bestilling/pdldata/command/PdlDataHentCommand.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/bestilling/pdldata/command/PdlDataHentCommand.java
@@ -45,6 +45,7 @@ public class PdlDataHentCommand implements Callable<Mono<FullPersonDTO[]>> {
                 .header(UserConstant.USER_HEADER_JWT, getUserJwt())
                 .retrieve()
                 .bodyToMono(FullPersonDTO[].class)
+                .doOnError(error -> log.error(WebClientFilter.getMessage(error), error))
                 .retryWhen(Retry.backoff(3, Duration.ofSeconds(5))
                         .filter(WebClientFilter::is5xxException))
                 .onErrorResume(throwable -> throwable instanceof WebClientResponseException.NotFound,
--- apps/dolly-backend/src/main/java/no/nav/dolly/bestilling/pdldata/command/PdlDataSlettUtenomCommand.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/bestilling/pdldata/command/PdlDataSlettUtenomCommand.java
@@ -38,6 +38,7 @@ public class PdlDataSlettUtenomCommand implements Callable<Flux<Void>> {
                 .header(UserConstant.USER_HEADER_JWT, getUserJwt())
                 .retrieve()
                 .bodyToFlux(Void.class)
+                .doOnError(error -> log.error(WebClientFilter.getMessage(error), error))
                 .retryWhen(Retry.backoff(3, Duration.ofSeconds(5))
                         .filter(WebClientFilter::is5xxException))
                 .onErrorResume(throwable -> throwable instanceof WebClientResponseException.NotFound,
