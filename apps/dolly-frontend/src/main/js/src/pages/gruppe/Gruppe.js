2e1f3b37b86154961297dd9841610366e29ecf24
Aileen Hay <58416744+H153910@users.noreply.github.com>
2022-05-04 07:51:07 +0200

    Midlertidig disable opprett personer (#2325)

--- apps/dolly-frontend/src/main/js/src/pages/gruppe/Gruppe.js
+++ apps/dolly-frontend/src/main/js/src/pages/gruppe/Gruppe.js
@@ -108,8 +108,10 @@ export default function Gruppe({
 					<NavButton
 						type="hoved"
 						onClick={visStartBestilling}
-						disabled={erLaast}
-						title={erLaast ? 'Denne gruppen er låst, og du kan ikke legge til flere personer.' : ''}
+						disabled
+						title={'Opprettelse av personer er midlertidig utilgjengelig pga vedlikehold'}
+						// disabled={erLaast}
+						// title={erLaast ? 'Denne gruppen er låst, og du kan ikke legge til flere personer.' : ''}
 					>
 						Opprett personer
 					</NavButton>
