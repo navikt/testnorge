1ba3b911c00cb708fefc4500f863ddef294246b2
kristenhaerum <kristen.herum@nav.no>
2022-05-18 15:36:35 +0200

    Sonar trivia + #deploy-pdl-forvalter-dev

--- apps/pdl-forvalter/src/main/java/no/nav/pdl/forvalter/dto/PersonUtenIdentifikatorRequest.java
+++ apps/pdl-forvalter/src/main/java/no/nav/pdl/forvalter/dto/PersonUtenIdentifikatorRequest.java
@@ -1,6 +1,7 @@
 package no.nav.pdl.forvalter.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,6 +10,7 @@ import no.nav.testnav.libs.dto.pdlforvalter.v1.RelatertBiPersonDTO;
 
 @Getter
 @Setter
+@EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
 @AllArgsConstructor
 public class PersonUtenIdentifikatorRequest extends RelatertBiPersonDTO {
