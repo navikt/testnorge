9fe2c569239f9620338962733ab7043258f1cb98
Aileen Hay <aileen.hay@nav.no>
2022-05-20 13:34:20 +0200

    Fikset code smells

--- apps/dolly-frontend/src/main/js/src/components/ui/button/PartnerImportButton/PartnerImportButton.tsx
+++ apps/dolly-frontend/src/main/js/src/components/ui/button/PartnerImportButton/PartnerImportButton.tsx
@@ -7,7 +7,6 @@ import Icon from '~/components/ui/icon/Icon'
 import Loading from '~/components/ui/loading/Loading'
 import { DollyApi } from '~/service/Api'
 import './PartnerImportButton.less'
-import { ErrorBoundary } from '../../appError/ErrorBoundary'
 
 type Props = {
 	action: Function
@@ -17,69 +16,71 @@ type Props = {
 }
 
 export const PartnerImportButton = ({ action, gruppeId, partnerIdent, gruppeIdenter }: Props) => {
-	if (!partnerIdent) return null
+	if (!partnerIdent) {
+		return null
+	}
 	const [loading, setLoading] = useState(false)
 	const [modalIsOpen, openModal, closeModal] = useBoolean(false)
-	const [error, setError] = useState(null)
+	const [feilmelding, setFeilmelding] = useState(null)
 
 	const disabled = gruppeIdenter.includes(partnerIdent)
 
-	const handleImport = async (gruppeId: string, ident: string) => {
+	const handleImport = async (ident: string) => {
 		setLoading(true)
-		setError(null)
+		setFeilmelding(null)
 		await DollyApi.importerPartner(gruppeId, ident)
 			.then((response) => {
 				setLoading(false)
 				action()
 			})
-			.catch((error) => {
-				setError('Noe gikk galt')
+			.catch((_error) => {
+				setFeilmelding('Noe gikk galt')
 				setLoading(false)
 			})
 	}
 
-	if (loading) return <Loading label="importerer..." />
+	if (loading) {
+		return <Loading label="importerer..." />
+	}
 
 	return (
-		<ErrorBoundary>
-			<div>
-				<Button
-					onClick={openModal}
-					disabled={disabled}
-					title={disabled ? 'Partner er allerede i gruppen' : ''}
-					kind="relasjoner"
-				>
-					IMPORTER PARTNER
-				</Button>
-				{error && (
-					<div className="error-message" style={{ margin: '5px 0 0 30px' }}>
-						{error}
+		<div>
+			<Button
+				onClick={openModal}
+				disabled={disabled}
+				title={disabled ? 'Partner er allerede i gruppen' : ''}
+				kind="relasjoner"
+			>
+				IMPORTER PARTNER
+			</Button>
+			{feilmelding && (
+				<div className="error-message" style={{ margin: '5px 0 0 30px' }}>
+					{feilmelding}
+				</div>
+			)}
+			<DollyModal isOpen={modalIsOpen} closeModal={closeModal} width="40%" overflow="auto">
+				<div className="partnerImportModal">
+					<div className="partnerImportModal partnerImportModal-content">
+						<Icon size={50} kind="personinformasjon" />
+						<h1>Importer partner</h1>
+						<h4>
+							Er du sikker på at du vil importere og legge til valgt persons partner i gruppen?
+						</h4>
 					</div>
-				)}
-				<DollyModal isOpen={modalIsOpen} closeModal={closeModal} width="40%" overflow="auto">
-					<div className="partnerImportModal">
-						<div className="partnerImportModal partnerImportModal-content">
-							<Icon size={50} kind="personinformasjon" />
-							<h1>Importer partner</h1>
-							<h4>
-								Er du sikker på at du vil importere og legge til valgt persons partner i gruppen?
-							</h4>
-						</div>
-						<div className="partnerImportModal-actions">
-							<NavButton onClick={closeModal}>Nei</NavButton>
-							<NavButton
-								onClick={() => {
-									closeModal()
-									handleImport(gruppeId, partnerIdent)
-								}}
-								type="hoved"
-							>
-								Ja
-							</NavButton>
-						</div>
+					<div className="partnerImportModal-actions">
+						<NavButton onClick={closeModal}>Nei</NavButton>
+						<NavButton
+							onClick={() => {
+								closeModal()
+								handleImport(partnerIdent)
+							}}
+							type="hoved"
+						>
+							Ja
+						</NavButton>
 					</div>
-				</DollyModal>
-			</div>
-		</ErrorBoundary>
+				</div>
+			</DollyModal>
+		</div>
 	)
 }
