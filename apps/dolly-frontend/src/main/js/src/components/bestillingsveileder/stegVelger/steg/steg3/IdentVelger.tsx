e84bf1790f3574acf8a9b22c2ee9973682775484
Aileen Hay <aileen.hay@nav.no>
2022-05-27 12:40:56 +0200

    Kode rydding

--- apps/dolly-frontend/src/main/js/src/components/bestillingsveileder/stegVelger/steg/steg3/IdentVelger.tsx
+++ apps/dolly-frontend/src/main/js/src/components/bestillingsveileder/stegVelger/steg/steg3/IdentVelger.tsx
@@ -4,7 +4,6 @@ import styled from 'styled-components'
 import { RadioPanelGruppe } from 'nav-frontend-skjema'
 import Hjelpetekst from '~/components/hjelpetekst'
 import { FormikProps } from 'formik'
-import { AlertStripeAdvarsel } from 'nav-frontend-alertstriper'
 
 type Form = {
 	formikBag: FormikProps<{}>
@@ -55,8 +54,6 @@ const informasjonstekst =
 	'Om ikke lenge kommer Dolly til å gå fra å opprette personer som har ekte identifikasjonsnummer til å kun opprette personer med syntetisk identifikasjonsnummer. Frem til det vil det være mulig å velge selv om man ønsker å opprette personene med standard eller syntetisk identifikasjonsnummer. Siden syntetisk identifikasjonsnummer en dag kommer til å bli den nye standarden oppfordrer vi alle til å ta dette i bruk allerede nå.'
 
 export const IdentVelger = ({ formikBag }: Form) => {
-	const erArenaBestilling = formikBag.values.hasOwnProperty('arenaforvalter')
-
 	const [type, setType] = useState(
 		_get(formikBag.values, `pdldata.opprettNyPerson.syntetisk`) === true
 			? IdentType.SYNTETISK
