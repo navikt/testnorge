50d7df1ef731a7ab8c7e326169435797d6dfc731
Aileen Hay <aileen.hay@nav.no>
2022-06-01 09:46:43 +0200

    Relasjoner checked fiks

--- apps/dolly-frontend/src/main/js/src/pages/testnorgePage/search/partials/Relasjoner.tsx
+++ apps/dolly-frontend/src/main/js/src/pages/testnorgePage/search/partials/Relasjoner.tsx
@@ -75,18 +75,23 @@ export const Relasjoner = ({ formikBag }: RelasjonerProps) => {
 			<div className="options-title">Har forelder</div>
 			<DollyCheckbox
 				label={foreldreRoller.FAR}
+				checked={_get(formikBag.values, paths.forelderBarnRelasjoner).includes(foreldreRoller.FAR)}
 				onChange={() => handleForelderChange(foreldreRoller.FAR)}
 				checkboxMargin={false}
 				size="small"
 			/>
 			<DollyCheckbox
 				label={foreldreRoller.MOR}
+				checked={_get(formikBag.values, paths.forelderBarnRelasjoner).includes(foreldreRoller.MOR)}
 				onChange={() => handleForelderChange(foreldreRoller.MOR)}
 				checkboxMargin={false}
 				size="small"
 			/>
 			<DollyCheckbox
 				label={foreldreRoller.MEDMOR}
+				checked={_get(formikBag.values, paths.forelderBarnRelasjoner).includes(
+					foreldreRoller.MEDMOR
+				)}
 				onChange={() => handleForelderChange(foreldreRoller.MEDMOR)}
 				checkboxMargin={false}
 				size="small"
