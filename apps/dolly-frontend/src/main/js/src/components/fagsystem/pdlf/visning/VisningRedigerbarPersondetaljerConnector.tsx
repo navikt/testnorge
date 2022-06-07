b7478db5d43e2dc982f0b94edc0edd1157fedfa3
Betsy Carina Traran <betsy.carina.traran@nav.no>
2022-06-07 10:15:06 +0200

    Fikset merge conflict

--- apps/dolly-frontend/src/main/js/src/components/fagsystem/pdlf/visning/VisningRedigerbarPersondetaljerConnector.tsx
+++ apps/dolly-frontend/src/main/js/src/components/fagsystem/pdlf/visning/VisningRedigerbarPersondetaljerConnector.tsx
@@ -1,11 +1,11 @@
-import { actions } from '~/ducks/redigertePersoner'
+import { hentPdlforvalterPersoner } from '~/ducks/redigertePersoner'
 import { connect } from 'react-redux'
 import { Dispatch } from 'redux'
 import { VisningRedigerbarPersondetaljer } from '~/components/fagsystem/pdlf/visning/VisningRedigerbarPersondetaljer'
 
 const mapDispatchToProps = (dispatch: Dispatch, ownProps: any) => {
 	return {
-		getPdlForvalter: () => dispatch(actions.hentPdlforvalterPersoner(ownProps.ident)),
+		getPdlForvalter: () => dispatch(hentPdlforvalterPersoner(ownProps.ident)),
 	}
 }
 
