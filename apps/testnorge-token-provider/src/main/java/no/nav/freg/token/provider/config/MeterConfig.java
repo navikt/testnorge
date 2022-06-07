e4c3a5dbd0e7d1b89bc5e0e7e6e45c64364ccc23
kristenhaerum <kristen.herum@nav.no>
2022-06-07 11:45:51 +0200

    Lagt til MeterConfig for Prometheus

---
+++ apps/testnorge-token-provider/src/main/java/no/nav/freg/token/provider/config/MeterConfig.java
@@ -0,0 +1,23 @@
+package no.nav.freg.token.provider.config;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import lombok.RequiredArgsConstructor;
+import no.nav.freg.token.provider.meter.WebClientTagsProvider;
+import org.springframework.boot.actuate.metrics.AutoTimer;
+import org.springframework.boot.actuate.metrics.web.reactive.client.MetricsWebClientFilterFunction;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
+
+@Configuration
+@RequiredArgsConstructor
+public class MeterConfig {
+
+    private final MeterRegistry meterRegistry;
+
+    @Bean
+    public ExchangeFilterFunction metricsWebClientFilterFunction() {
+        return new MetricsWebClientFilterFunction(meterRegistry, new WebClientTagsProvider(),
+                "webClientMetrics", AutoTimer.ENABLED);
+    }
+}
