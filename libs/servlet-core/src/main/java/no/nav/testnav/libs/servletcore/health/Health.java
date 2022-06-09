7afe6795643099caef5813240b694701026ba0ab
kristenhaerum <kristen.herum@nav.no>
2022-06-09 13:18:02 +0200

    Oppdatert health metrikker II #deploy-pdl-forvalter-dev

---
+++ libs/servlet-core/src/main/java/no/nav/testnav/libs/servletcore/health/Health.java
@@ -0,0 +1,33 @@
+package no.nav.testnav.libs.servletcore.health;
+
+import org.springframework.boot.actuate.health.Status;
+
+import static org.springframework.boot.actuate.health.Health.status;
+
+public class Health {
+    public static String UP = "UP";
+    public static String PAUSED = "PAUSED";
+    public static String DISABLED = "DISABLED";
+    public static String OUT_OF_SERVICE = "OUT_OF_SERVICE";
+    public static String DOWN = "DOWN";
+
+    public static org.springframework.boot.actuate.health.Health.Builder up() {
+        return status(new Status(UP));
+    }
+
+    public static org.springframework.boot.actuate.health.Health.Builder paused() {
+        return status(new Status(PAUSED));
+    }
+
+    public static org.springframework.boot.actuate.health.Health.Builder disabled() {
+        return status(new Status(DISABLED));
+    }
+
+    public static org.springframework.boot.actuate.health.Health.Builder outOfService() {
+        return status(new Status(OUT_OF_SERVICE));
+    }
+
+    public static org.springframework.boot.actuate.health.Health.Builder down() {
+        return status(new Status(DOWN));
+    }
+}
