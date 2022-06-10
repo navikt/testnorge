ec5aec610d64709f4354cf272c2728953db211ff
kristenhaerum <kristen.herum@nav.no>
2022-06-10 14:48:56 +0200

    Sonar trivia

--- libs/reactive-core/src/main/java/no/nav/testnav/libs/reactivecore/health/Health.java
+++ libs/reactive-core/src/main/java/no/nav/testnav/libs/reactivecore/health/Health.java
@@ -1,33 +1,16 @@
 package no.nav.testnav.libs.reactivecore.health;
 
+import lombok.experimental.UtilityClass;
 import org.springframework.boot.actuate.health.Status;
 
 import static org.springframework.boot.actuate.health.Health.status;
 
+@UtilityClass
 public class Health {
-    public static String UP = "UP";
-    public static String PAUSED = "PAUSED";
-    public static String DISABLED = "DISABLED";
-    public static String OUT_OF_SERVICE = "OUT_OF_SERVICE";
-    public static String DOWN = "DOWN";
 
-    public static org.springframework.boot.actuate.health.Health.Builder up() {
-        return status(new Status(UP));
-    }
-
-    public static org.springframework.boot.actuate.health.Health.Builder paused() {
-        return status(new Status(PAUSED));
-    }
-
-    public static org.springframework.boot.actuate.health.Health.Builder disabled() {
-        return status(new Status(DISABLED));
-    }
-
-    public static org.springframework.boot.actuate.health.Health.Builder outOfService() {
-        return status(new Status(OUT_OF_SERVICE));
-    }
-
-    public static org.springframework.boot.actuate.health.Health.Builder down() {
-        return status(new Status(DOWN));
-    }
+    public static final String UP = "UP";
+    public static final String PAUSED = "PAUSED";
+    public static final String DISABLED = "DISABLED";
+    public static final String OUT_OF_SERVICE = "OUT_OF_SERVICE";
+    public static final String DOWN = "DOWN";
 }
