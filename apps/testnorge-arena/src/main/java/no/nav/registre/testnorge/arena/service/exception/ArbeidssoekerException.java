42656cf5b5c2c327dc2d474300a114037f1da788
Aileen Hay <aileen.hay@nav.no>
2022-06-17 09:33:01 +0200

    Fjernet testnorge-arena

--- apps/testnorge-arena/src/main/java/no/nav/registre/testnorge/arena/service/exception/ArbeidssoekerException.java
+++
@@ -1,8 +0,0 @@
-package no.nav.registre.testnorge.arena.service.exception;
-
-public class ArbeidssoekerException extends RuntimeException {
-
-    public ArbeidssoekerException(String message) {
-        super(message);
-    }
-}
