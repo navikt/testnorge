7f1da11bbe106ebd2ee45bdafa885d575103f988
Kristen Hærum <kristen.herum@nav.no>
2022-06-20 09:02:35 +0200

    Feature/excel legge til bankkonto (#2449)
    * Refaktorert PersonExcelService + lagt til BankkontoExcelService #deploy-backend-dev
    * Lagt til parallell kjøring av personer og bankkontoer

--- apps/dolly-backend/src/main/java/no/nav/dolly/bestilling/tpsmessagingservice/command/HentPersonCommand.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/bestilling/tpsmessagingservice/command/HentPersonCommand.java
@@ -3,9 +3,10 @@ package no.nav.dolly.bestilling.tpsmessagingservice.command;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.dolly.util.WebClientFilter;
-import no.nav.testnav.libs.dto.tpsmessagingservice.v1.TpsIdentStatusDTO;
+import no.nav.testnav.libs.dto.tpsmessagingservice.v1.PersonMiljoeDTO;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 import reactor.util.retry.Retry;
 
 import java.time.Duration;
@@ -14,31 +15,28 @@ import java.util.concurrent.Callable;
 
 @RequiredArgsConstructor
 @Slf4j
-public class HentIdenterCommand implements Callable<List<TpsIdentStatusDTO>> {
+public class HentPersonCommand implements Callable<Flux<PersonMiljoeDTO>> {
 
     private static final String MILJOER_PARAM = "miljoer";
-    private static final String IDENTER_URL = "/api/v1/identer";
-    private static final String IDENTER_PARAM = "identer";
+    private static final String PERSONER_URL = "/api/v1/personer/{ident}";
 
     private final WebClient webClient;
+    private final String ident;
     private final List<String> miljoer;
-    private final List<String> identer;
     private final String token;
 
     @Override
-    public List<TpsIdentStatusDTO> call() {
+    public Flux<PersonMiljoeDTO> call() {
 
-        return List.of(webClient.get()
+        return webClient.get()
                 .uri(uriBuilder -> uriBuilder
-                        .path(IDENTER_URL)
+                        .path(PERSONER_URL)
                         .queryParam(MILJOER_PARAM, miljoer)
-                        .queryParam(IDENTER_PARAM, identer)
-                        .build())
-                .header(HttpHeaders.AUTHORIZATION, token)
+                        .build(ident))
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
                 .retrieve()
-                .bodyToMono(TpsIdentStatusDTO[].class)
+                .bodyToFlux(PersonMiljoeDTO.class)
                 .retryWhen(Retry.backoff(3, Duration.ofSeconds(5))
-                        .filter(WebClientFilter::is5xxException))
-                .block());
+                        .filter(WebClientFilter::is5xxException));
     }
 }
