2e354c52398bc529ec2b3bdf8a8ee3f8d80b5ed6
Aileen Hay <58416744+H153910@users.noreply.github.com>
2022-06-20 09:49:28 +0200

    Fjernet testnorge-frikort (#2452)

--- apps/testnorge-frikort/src/main/java/no/nav/registre/frikort/exceptions/UgyldigEgenandelskodeException.java
+++
@@ -1,8 +0,0 @@
-package no.nav.registre.frikort.exceptions;
-
-public class UgyldigEgenandelskodeException extends RuntimeException {
-
-    public UgyldigEgenandelskodeException(String message) {
-        super(message);
-    }
-}
