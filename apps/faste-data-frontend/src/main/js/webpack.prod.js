fd368e533530b1b0637e93d650a226ee54094c0b
stigus <stian.gustavsson@nav.no>
2022-06-21 07:47:09 +0200

    Fixed js lib nstall error during build

--- apps/faste-data-frontend/src/main/js/webpack.prod.js
+++ apps/faste-data-frontend/src/main/js/webpack.prod.js
@@ -2,21 +2,13 @@ const path = require('path');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
   stats: 'minimal',
   plugins: [
-    new OptimizeCssAssetsPlugin({
-      assetNameRegExp: /\.css$/g,
-      cssProcessor: require('cssnano'),
-      cssProcessorPluginOptions: {
-        preset: ['default', { discardComments: { removeAll: true } }],
-      },
-      canPrint: true,
-    }),
     new MiniCssExtractPlugin({
       filename: '[name].[contenthash:8].css',
     }),
@@ -28,6 +20,6 @@ module.exports = merge(common, {
     clean: true,
   },
   optimization: {
-    minimize: true,
+    minimizer: new CssMinimizerPlugin(),
   },
 });
