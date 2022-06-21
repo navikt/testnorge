6689fe7c6b5c9b1ea36fe2957a65e0adb970c01d
kristenhaerum <kristen.herum@nav.no>
2022-06-21 09:52:02 +0200

    Fix for JMS health #deploy-messaging-service-dev

---
+++ apps/tps-messaging-service/src/main/java/no/nav/testnav/apps/tpsmessagingservice/factory/HealthConnectionFactory.java
@@ -0,0 +1,35 @@
+package no.nav.testnav.apps.tpsmessagingservice.factory;
+
+import com.ibm.mq.constants.CMQC;
+import com.ibm.mq.jms.MQQueueConnectionFactory;
+import com.ibm.msg.client.jms.JmsConstants;
+import com.ibm.msg.client.wmq.common.CommonConstants;
+import no.nav.testnav.apps.tpsmessagingservice.dto.QueueManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+
+@Configuration
+public class HealthConnectionFactory {
+
+    private static final int UTF_8_WITH_PUA = 1208;
+
+    @Bean
+    public ConnectionFactory connectionFactory(QueueManager queueManager) throws JMSException {
+
+        MQQueueConnectionFactory factory = new MQQueueConnectionFactory();
+        factory.setCCSID(UTF_8_WITH_PUA);
+        factory.setTransportType(CommonConstants.WMQ_CM_CLIENT);
+        factory.setIntProperty(JmsConstants.JMS_IBM_ENCODING, CMQC.MQENC_NATIVE);
+        factory.setBooleanProperty(JmsConstants.USER_AUTHENTICATION_MQCSP, true);
+        factory.setIntProperty(JmsConstants.JMS_IBM_CHARACTER_SET, UTF_8_WITH_PUA);
+        factory.setQueueManager(queueManager.queueManagerName());
+        factory.setHostName(queueManager.host());
+        factory.setPort(queueManager.port());
+        factory.setChannel(queueManager.channel());
+
+        return factory;
+    }
+}
