960ed29aa7f046f8e902f7aa7ac75a5fc317c2aa
stigus <stian.gustavsson@nav.no>
2022-06-24 09:54:20 +0200

    Skrevet om utenlandsk adresse mapping #deploy-organisasjon-forvalter-dev

--- apps/organisasjon-forvalter/src/main/java/no/nav/organisasjonforvalter/util/UtenlandskAdresseUtil.java
+++ apps/organisasjon-forvalter/src/main/java/no/nav/organisasjonforvalter/util/UtenlandskAdresseUtil.java
@@ -6,6 +6,8 @@ import org.apache.logging.log4j.util.Strings;
 
 import java.util.List;
 
+import static io.micrometer.core.instrument.util.StringUtils.isBlank;
+
 @UtilityClass
 public class UtenlandskAdresseUtil {
 
@@ -15,16 +17,19 @@ public class UtenlandskAdresseUtil {
     private static final String ADRESSE_POSTKODE = "3000";
 
 
-    public static AdresseRequest prepareUtenlandskAdresse(AdresseRequest request) {
+    public static void prepareUtenlandskAdresse(AdresseRequest request) {
+
+        if (isBlank(request.getPostnr())) {
+            request.setPostnr(ADRESSE_POSTKODE);
+        }
+        if (isBlank(request.getPoststed())) {
+            request.setPoststed(ADRESSE_3_UTLAND);
+        }
 
         if (request.getAdresselinjer().stream().anyMatch(Strings::isNotBlank)) {
-            return request;
+            return;
         }
-        
+
         request.setAdresselinjer(List.of(ADRESSE_NAVN_NUMMER, ADRESSE_BY_STED));
-        request.setPostnr(ADRESSE_POSTKODE);
-        request.setPoststed(ADRESSE_3_UTLAND);
-        request.setLandkode(request.getLandkode());
-        return request;
     }
 }
