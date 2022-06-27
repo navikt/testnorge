59eaab5b091bd22b85656221734aec501ec756b2
stigus <stian.gustavsson@nav.no>
2022-06-27 16:36:25 +0200

    Fikset issues under merge av SWR og lagt til noe lazy loading #deploy-frontend-dev

--- apps/dolly-frontend/src/main/js/src/components/utlogging/logoutBruker.tsx
+++ apps/dolly-frontend/src/main/js/src/components/utlogging/logoutBruker.tsx
@@ -1,5 +1,5 @@
 const logoutBruker = (feilmelding?: string) => {
-	window.location.href = '/logout'
+	window.location.href = '/logout' + (feilmelding ? '?state=' + feilmelding : '')
 	console.error(feilmelding)
 }
 export default logoutBruker
