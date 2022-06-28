f2ba7592756a45dda2742baf4fe0224c563e9839
Aileen Hay <58416744+H153910@users.noreply.github.com>
2022-06-28 15:31:40 +0200

    Feature/fjerne gammel ident pool (#2476)
    Fjernet gammel ident-pool og ident-pool-proxy

--- apps/ident-pool/src/main/java/no/nav/identpool/exception/IdentAlleredeIBrukException.java
+++
@@ -1,7 +0,0 @@
-package no.nav.identpool.exception;
-
-public class IdentAlleredeIBrukException extends RuntimeException {
-    public IdentAlleredeIBrukException(String message) {
-        super(message);
-    }
-}
