9dcc0817cbde2c13d833f9a5f1bc31b8f2b07e31
Aileen Hay <aileen.hay@nav.no>
2022-06-29 09:05:44 +0200

    Byttet til local

--- apps/testnorge-statisk-data-forvalter/src/main/java/no/nav/registre/sdforvalter/config/DevConfig.java
+++ apps/testnorge-statisk-data-forvalter/src/main/java/no/nav/registre/sdforvalter/config/DevConfig.java
@@ -9,7 +9,7 @@ import org.springframework.vault.client.VaultEndpoint;
 import org.springframework.vault.config.AbstractVaultConfiguration;
 
 @Configuration
-@Profile("dev")
+@Profile("local")
 @VaultPropertySource(value = "kv/preprod/fss/testnorge-statisk-data-forvalter/local", ignoreSecretNotFound = false)
 @VaultPropertySource(value = "azuread/prod/creds/team-dolly-lokal-app", ignoreSecretNotFound = false)
 public class DevConfig  extends AbstractVaultConfiguration {
