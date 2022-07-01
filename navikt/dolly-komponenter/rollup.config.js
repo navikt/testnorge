0e12b991aca985ca62302a30084609be5ff97a7e
stigus <stian.gustavsson@nav.no>
2022-07-01 14:47:20 +0200

    Fikset komponent lib #publish-libs

--- navikt/dolly-komponenter/rollup.config.js
+++ navikt/dolly-komponenter/rollup.config.js
@@ -1,7 +1,11 @@
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
+import image from '@rollup/plugin-image';
 import postcss from 'rollup-plugin-postcss';
-
+import NpmImport from 'less-plugin-npm-import';
+import { visualizer } from 'rollup-plugin-visualizer';
 const packageJson = require('./package.json');
 
 export default {
@@ -19,9 +23,21 @@ export default {
     },
   ],
   plugins: [
+    peerDepsExternal(),
     resolve(),
+    commonjs(),
+    image(),
     // visualizer(),
-    postcss(),
+    postcss({
+      use: [
+        [
+          'less',
+          {
+            plugins: [new NpmImport({ prefix: '~' })],
+          },
+        ],
+      ],
+    }),
     typescript({ useTsconfigDeclarationDir: true }),
   ],
 };
