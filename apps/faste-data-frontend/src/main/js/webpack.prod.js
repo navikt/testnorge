1dd37a0325b981d312ce23aeae2e10c5c49762b0
Aileen Hay <aileen.hay@nav.no>
2022-07-01 10:28:42 +0200

    Oppdatert package.json

--- apps/faste-data-frontend/src/main/js/webpack.prod.js
+++ apps/faste-data-frontend/src/main/js/webpack.prod.js
@@ -2,7 +2,7 @@ const path = require('path');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
