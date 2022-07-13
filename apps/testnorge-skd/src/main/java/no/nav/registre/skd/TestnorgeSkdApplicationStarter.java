ec0a53e4497b187ac20c55d8fdb7a4e9bced0a23
stigus <stian.gustavsson@nav.no>
2022-07-13 12:27:32 +0200

    Testytest

--- apps/testnorge-skd/src/main/java/no/nav/registre/skd/TestnorgeSkdApplicationStarter.java
+++ apps/testnorge-skd/src/main/java/no/nav/registre/skd/TestnorgeSkdApplicationStarter.java
@@ -1,10 +1,15 @@
 package no.nav.registre.skd;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.health.HealthContributorRegistry;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestnorgeSkdApplicationStarter {
+
+    @Autowired
+    HealthContributorRegistry healthContributorRegistry;
 
     public static void main(String[] args) {
         SpringApplication.run(TestnorgeSkdApplicationStarter.class, args);
