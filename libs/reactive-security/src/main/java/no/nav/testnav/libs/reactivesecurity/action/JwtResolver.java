070b5ff4d0889ee8317f825fbba075d8007db9bb
stigus <stian.gustavsson@nav.no>
2022-07-14 18:25:41 +0200

    Deploy

--- libs/reactive-security/src/main/java/no/nav/testnav/libs/reactivesecurity/action/JwtResolver.java
+++ libs/reactive-security/src/main/java/no/nav/testnav/libs/reactivesecurity/action/JwtResolver.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.CredentialsExpiredException;
 import org.springframework.security.core.context.ReactiveSecurityContextHolder;
 import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextImpl;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import reactor.core.publisher.Mono;
@@ -17,7 +18,7 @@ abstract class JwtResolver {
     Mono<JwtAuthenticationToken> getJwtAuthenticationToken() {
         return ReactiveSecurityContextHolder
                 .getContext()
-                .switchIfEmpty(Mono.error(new IllegalStateException("ReactiveSecurityContext is empty")))
+                .switchIfEmpty(Mono.fromSupplier(SecurityContextImpl::new))
                 .map(SecurityContext::getAuthentication)
                 .map(JwtAuthenticationToken.class::cast)
                 .doOnError(throwable -> log.error("Klarte ikke hente Jwt Auth Token: ", throwable))
