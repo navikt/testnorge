b143115204c66ddbab336fd5592a7f6fa8434b88
dainius <dainiusfigoras@gmail.com>
2022-07-18 14:32:22 +0200

    #deploy-frontend-dev

--- apps/dolly-frontend/src/main/js/src/utils/ErrorMessageWithFocus.tsx
+++ apps/dolly-frontend/src/main/js/src/utils/ErrorMessageWithFocus.tsx
@@ -1,30 +1,30 @@
-import { useEffect } from "react";
-import * as React from "react";
-import {ErrorMessage, useFormikContext} from 'formik'
+import { useEffect } from 'react'
+import * as React from 'react'
+import { ErrorMessage, useFormikContext } from 'formik'
 import { ErrorMessageProps } from 'formik/dist/ErrorMessage'
 
 const ErrorFocus = (): JSX.Element => {
-    const { isSubmitting, isValidating, errors } = useFormikContext()
+	const { isSubmitting, isValidating, errors } = useFormikContext()
 
-    useEffect(() => {
-        const keys = Object.keys(errors)
+	useEffect(() => {
+		const keys = Object.keys(errors)
 
-        if (keys.length > 0 && isSubmitting && !isValidating) {
-            const errorElement = document.querySelector(`div[class="error-message"]`)
+		if (keys.length > 0 && isSubmitting && !isValidating) {
+			const errorElement = document.querySelector(`div[class="error-message"]`)
 
-            if (errorElement) {
-                errorElement.scrollIntoView({ behavior: 'smooth' })
-            }
-        }
-    }, [isSubmitting, isValidating, errors])
-    return null
+			if (errorElement) {
+				errorElement.scrollIntoView({ behavior: 'smooth' })
+			}
+		}
+	}, [isSubmitting, isValidating, errors])
+	return null
 }
 
 export const ErrorMessageWithFocus = (props: ErrorMessageProps): JSX.Element => {
-    return (
-        <>
-            <ErrorMessage {...props} />
-            <ErrorFocus />
-        </>
-    )
+	return (
+		<>
+			<ErrorMessage {...props} />
+			<ErrorFocus />
+		</>
+	)
 }
