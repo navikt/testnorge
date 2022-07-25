ce51d83c2aacc681a2f056bc0afac00f425b27a1
dainius <dainiusfigoras@gmail.com>
2022-07-25 12:23:14 +0200

    manglende konfigurasjonsfiler #deploy-bankkonto-service-dev

---
+++ apps/bankkonto-service/src/main/java/no/nav/testnav/apps/bankkontoservice/config/JMSConfig.java
@@ -0,0 +1,10 @@
+package no.nav.testnav.apps.bankkontoservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.annotation.EnableJms;
+
+@EnableJms
+@Configuration
+public class JMSConfig {
+
+}
