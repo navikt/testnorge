4efa1f5e4e3a902b981dad23c48ba24ac630465e
dainius <dainiusfigoras@gmail.com>
2022-07-27 15:38:57 +0200

    fikse å generere forskjellige bankkontoer for forskjellige identer #deploy-backend-dev

--- libs/data-transfer-objects/src/main/java/no/nav/testnav/libs/dto/tpsmessagingservice/v1/BankkontonrUtlandDTO.java
+++ libs/data-transfer-objects/src/main/java/no/nav/testnav/libs/dto/tpsmessagingservice/v1/BankkontonrUtlandDTO.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.With;
 
 import java.time.LocalDateTime;
 
@@ -16,6 +17,7 @@ import java.time.LocalDateTime;
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class BankkontonrUtlandDTO {
 
+    @With
     private String kontonummer;
     private String swift;
     private String iban;
