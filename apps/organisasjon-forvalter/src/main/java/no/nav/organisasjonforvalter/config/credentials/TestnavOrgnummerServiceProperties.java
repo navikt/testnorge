a4a3372187eae23cd49da98e7364cbb670efcb60
kristenhaerum <kristen.herum@nav.no>
2022-08-03 09:30:51 +0200

    Oppdatert resterende referanser til ognummer-service

--- apps/organisasjon-forvalter/src/main/java/no/nav/organisasjonforvalter/config/credentials/TestnavOrgnummerServiceProperties.java
+++ apps/organisasjon-forvalter/src/main/java/no/nav/organisasjonforvalter/config/credentials/TestnavOrgnummerServiceProperties.java
@@ -1,5 +1,4 @@
-package no.nav.registre.testnav.genererorganisasjonpopulasjonservice.credentials;
-
+package no.nav.organisasjonforvalter.config.credentials;
 
 import no.nav.testnav.libs.securitycore.domain.ServerProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -7,5 +6,5 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConfigurationProperties(prefix = "consumers.testnav-orgnummer-service")
-public class OrgnummerServiceProperties extends ServerProperties {
+public class TestnavOrgnummerServiceProperties extends ServerProperties {
 }
