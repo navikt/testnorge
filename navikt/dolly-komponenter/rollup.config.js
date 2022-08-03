3a9ee0c77b8bec73c9769aeab72407b260853937
stigus <stian.gustavsson@nav.no>
2022-08-03 15:44:58 +0200

    dolly komponenter publish #publish-libs

--- navikt/dolly-komponenter/rollup.config.js
+++ navikt/dolly-komponenter/rollup.config.js
@@ -1,5 +1,5 @@
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import resolve from '@rollup/plugin-node-resolve';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
 import image from '@rollup/plugin-image';
@@ -24,7 +24,7 @@ export default {
   ],
   plugins: [
     peerDepsExternal(),
-    resolve(),
+    nodeResolve({ moduleDirectories: ['node_modules', 'lib'] }),
     commonjs(),
     image(),
     // visualizer(),
