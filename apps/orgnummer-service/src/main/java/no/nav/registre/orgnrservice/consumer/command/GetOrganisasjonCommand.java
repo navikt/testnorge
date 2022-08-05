7845977b71e9feb1e226dc5c80b12b3bd1e9f3f2
Kristen Hærum <kristen.herum@nav.no>
2022-08-05 08:38:57 +0200

    Gitt nytt navn til orgnummer-service i mono-repo #deploy-orgnummer-se… (#2524)
    * Gitt nytt navn til orgnummer-service i mono-repo #deploy-orgnummer-service
    * Refaktoret asynkrone konsumentkall pga Sonar-feil

---
+++ apps/orgnummer-service/src/main/java/no/nav/registre/orgnrservice/consumer/command/GetOrganisasjonCommand.java
@@ -0,0 +1,52 @@
+package no.nav.registre.orgnrservice.consumer.command;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import no.nav.testnav.libs.commands.utils.WebClientFilter;
+import no.nav.testnav.libs.dto.organisasjon.v1.OrganisasjonDTO;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+import reactor.core.publisher.Flux;
+import reactor.util.retry.Retry;
+
+import java.time.Duration;
+import java.util.concurrent.Callable;
+
+@Slf4j
+@RequiredArgsConstructor
+public class GetOrganisasjonCommand implements Callable<Flux<OrganisasjonDTO>> {
+
+    private final WebClient webClient;
+    private final String token;
+    private final String orgnummer;
+    private final String miljo;
+
+    @Override
+    public Flux<OrganisasjonDTO> call() {
+
+        log.info("Henter organisasjon med orgnummer {} fra {}...", orgnummer, miljo);
+
+        return webClient
+                .get()
+                .uri(builder -> builder
+                        .path("/api/v1/organisasjoner/{orgnummer}")
+                        .build(orgnummer))
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+                .header("miljo", this.miljo)
+                .retrieve()
+                .bodyToFlux(OrganisasjonDTO.class)
+                .doFinally(value -> log.info("Organisasjon {} hentet fra {}", orgnummer, miljo))
+                .retryWhen(Retry.backoff(3, Duration.ofSeconds(5))
+                        .filter(WebClientFilter::is5xxException))
+                .onErrorResume(throwable -> {
+                    if (throwable instanceof WebClientResponseException webClientResponseException &&
+                            webClientResponseException.getStatusCode() == HttpStatus.NOT_FOUND) {
+                        log.trace("Organisasjon med orgnummer {} ikke funnet i {}", orgnummer, miljo);
+                        return Flux.empty();
+                    }
+                    return Flux.error(throwable);
+                });
+    }
+}
