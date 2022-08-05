e60fc75ad9fb4e9d358ed4876f64f2a1fba28180
stigus <stian.gustavsson@nav.no>
2022-08-05 09:35:26 +0200

    dolly komponenter publish, endringer på DatePicker #publish-libs

--- navikt/dolly-komponenter/src/date-picker/DatePicker.tsx
+++ navikt/dolly-komponenter/src/date-picker/DatePicker.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { ChangeEventHandler, useRef, useState } from 'react';
+import { format, isValid, parse } from 'date-fns';
+import FocusTrap from 'focus-trap-react';
 import { DayPicker } from 'react-day-picker';
-import nb from 'date-fns/locale/nb';
-import 'react-day-picker/dist/style.css';
+import { usePopper } from 'react-popper';
 
 type Props = {
   id: string;
@@ -12,21 +13,100 @@ type Props = {
 };
 
 export default ({ id, label, onBlur, required = false, error, ...props }: Props) => {
-  const [date, setDate] = useState<Date>();
+  const [selected, setSelected] = useState<Date>();
+  const [inputValue, setInputValue] = useState<string>('');
+  const [isPopperOpen, setIsPopperOpen] = useState(false);
+
+  const popperRef = useRef<HTMLDivElement>(null);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const [popperElement, setPopperElement] = useState<HTMLDivElement | null>(null);
+
+  const popper = usePopper(popperRef.current, popperElement, {
+    placement: 'bottom-start',
+  });
+
+  const closePopper = () => {
+    setIsPopperOpen(false);
+    buttonRef?.current?.focus();
+  };
+
+  const handleInputChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setInputValue(e.currentTarget.value);
+    const date = parse(e.currentTarget.value, 'y-MM-dd', new Date());
+    if (isValid(date)) {
+      setSelected(date);
+    } else {
+      setSelected(undefined);
+    }
+  };
+
+  const handleButtonClick = () => {
+    setIsPopperOpen(true);
+  };
+
+  const handleDaySelect = (date: Date) => {
+    setSelected(date);
+    if (date) {
+      setInputValue(format(date, 'y-MM-dd'));
+      onBlur(date.toISOString());
+      closePopper();
+    } else {
+      setInputValue('');
+    }
+  };
 
   return (
-    <DayPicker
-      key={id}
-      mode={'single'}
-      onSelect={(value) => {
-        onBlur(value.toISOString());
-        setDate(value);
-      }}
-      selected={date}
-      footer={label}
-      locale={nb}
-      required={required}
-      {...props}
-    />
+    <div>
+      <div ref={popperRef}>
+        <input
+          type="text"
+          placeholder={format(new Date(), 'y-MM-dd')}
+          value={inputValue}
+          onChange={handleInputChange}
+          className="input-reset pa2 ma2 bg-white black ba"
+        />
+        <button
+          ref={buttonRef}
+          type="button"
+          className="pa2 bg-white button-reset ba"
+          aria-label={label}
+          onClick={handleButtonClick}
+        >
+          <span role="img" aria-label="kalender icon">
+            📅
+          </span>
+        </button>
+      </div>
+      {isPopperOpen && (
+        <FocusTrap
+          active
+          focusTrapOptions={{
+            initialFocus: false,
+            allowOutsideClick: true,
+            clickOutsideDeactivates: true,
+            onDeactivate: closePopper,
+          }}
+        >
+          <div
+            tabIndex={-1}
+            style={popper.styles.popper}
+            className="dialog-sheet"
+            {...popper.attributes.popper}
+            ref={setPopperElement}
+            role="dialog"
+          >
+            <DayPicker
+              initialFocus={isPopperOpen}
+              mode="single"
+              defaultMonth={selected}
+              selected={selected}
+              required={required}
+              onSelect={handleDaySelect}
+              {...props}
+            />
+          </div>
+        </FocusTrap>
+      )}
+    </div>
   );
 };
