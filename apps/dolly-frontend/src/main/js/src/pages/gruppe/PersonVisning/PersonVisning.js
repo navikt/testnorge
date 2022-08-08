0da481ea94178a0c09de487a514cf2e737e10a4e
Aileen Hay <58416744+H153910@users.noreply.github.com>
2022-08-08 13:00:47 +0200

    Aapnet partner import for doede (#2542)

--- apps/dolly-frontend/src/main/js/src/pages/gruppe/PersonVisning/PersonVisning.js
+++ apps/dolly-frontend/src/main/js/src/pages/gruppe/PersonVisning/PersonVisning.js
@@ -105,7 +105,7 @@ export const PersonVisning = ({
 						</Button>
 					)}
 
-					{!iLaastGruppe && isAlive && (
+					{!iLaastGruppe && (
 						<PartnerImportButton
 							gruppeId={gruppeId}
 							partnerIdent={pdlPartner()}
