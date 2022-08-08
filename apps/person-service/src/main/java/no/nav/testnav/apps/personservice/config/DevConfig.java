1cba9c41d1ef807dc8b5c38f8f8f797b8a6ef040
kristenhaerum <kristen.herum@nav.no>
2022-08-08 12:33:42 +0200

    Sonar trivia

--- apps/person-service/src/main/java/no/nav/testnav/apps/personservice/config/DevConfig.java
+++ apps/person-service/src/main/java/no/nav/testnav/apps/personservice/config/DevConfig.java
@@ -1,7 +1,6 @@
 package no.nav.testnav.apps.personservice.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 import org.springframework.vault.annotation.VaultPropertySource;
 import org.springframework.vault.authentication.ClientAuthentication;
@@ -12,8 +11,7 @@ import org.springframework.vault.config.AbstractVaultConfiguration;
 @Configuration
 @Profile("dev")
 @VaultPropertySource(value = "azuread/prod/creds/team-dolly-lokal-app", ignoreSecretNotFound = false)
-//@VaultPropertySource(value = "kv/preprod/fss/testnorge-person-api-dev/dev", ignoreSecretNotFound = false)
-public class DevConfig  extends AbstractVaultConfiguration {
+public class DevConfig extends AbstractVaultConfiguration {
 
     @Override
     public VaultEndpoint vaultEndpoint() {
