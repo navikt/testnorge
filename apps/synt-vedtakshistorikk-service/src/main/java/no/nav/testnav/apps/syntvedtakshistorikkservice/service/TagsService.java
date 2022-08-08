1d8489f9d78f57a3708bb247e1b51f6ee77b3817
Aileen Hay <58416744+H153910@users.noreply.github.com>
2022-08-08 07:58:51 +0200

    Synt vedtakshistorikk service/oppdatere sivilstand (#2538)
    Oppdatert henting av partner identer

--- apps/synt-vedtakshistorikk-service/src/main/java/no/nav/testnav/apps/syntvedtakshistorikkservice/service/TagsService.java
+++ apps/synt-vedtakshistorikk-service/src/main/java/no/nav/testnav/apps/syntvedtakshistorikkservice/service/TagsService.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 import static java.util.Objects.nonNull;
 
@@ -18,16 +19,14 @@ import static java.util.Objects.nonNull;
 @RequiredArgsConstructor
 public class TagsService {
     public static final List<Tags> SYNT_TAGS = List.of(Tags.ARENASYNT);
-    private static final List<String> GYLDIG_SIVILSTAND = Arrays.asList("GIFT", "REGISTRERT_PARTNER", "SEPARERT", "SEPARERT_PARTNER");
-
     private final PdlProxyConsumer pdlProxyConsumer;
 
     public boolean opprettetTagsPaaIdenterOgPartner(List<PersonDTO> personer) {
         var identer = new ArrayList<>(personer.stream().map(PersonDTO::getIdent).toList());
         var partnere = personer.stream()
                 .map(PersonDTO::getSivilstand)
-                .filter(sivilstand -> GYLDIG_SIVILSTAND.contains(sivilstand.getType()))
                 .map(SivilstandDTO::getRelatertVedSivilstand)
+                .filter(Objects::nonNull)
                 .toList();
 
         identer.addAll(partnere);
@@ -35,7 +34,7 @@ public class TagsService {
     }
 
     public void removeTagsPaaIdentOgPartner(PersonDTO person) {
-        if (nonNull(person.getSivilstand()) && GYLDIG_SIVILSTAND.contains(person.getSivilstand().getType())) {
+        if (nonNull(person.getSivilstand()) && nonNull(person.getSivilstand().getRelatertVedSivilstand())) {
             pdlProxyConsumer.deleteTags(Arrays.asList(person.getIdent(), person.getSivilstand().getRelatertVedSivilstand()), SYNT_TAGS);
         } else {
             pdlProxyConsumer.deleteTags(Collections.singletonList(person.getIdent()), SYNT_TAGS);
