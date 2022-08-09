78649e3bc089e99860abfed8481850fef5183de7
stigus <stian.gustavsson@nav.no>
2022-08-09 10:55:34 +0200

    dolly komponenter publish, lagt til nav fremdriftslinje #publish-libs

---
+++ navikt/dolly-komponenter/src/progressbar/Progress.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ProgressBar from '@navikt/fremdriftslinje';
+import styled from 'styled-components';
+
+const Progress = styled(ProgressBar)`
+  background-color: rgb(255, 255, 255);
+  width: 100%;
+  border-radius: 4px;
+  border-color: rgb(106, 106, 106);
+  border-width: 1px;
+`;
+
+export default Progress;
