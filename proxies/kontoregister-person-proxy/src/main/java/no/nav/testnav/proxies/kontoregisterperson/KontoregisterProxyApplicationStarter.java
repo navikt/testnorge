cf887dc2d0c6d7dd3bc8f1cbc5f8e0502ffcfd0f
dainius <dainiusfigoras@gmail.com>
2022-08-10 15:45:46 +0200

    riktig url for kontoregister #deploy-kontoregister-person-proxy-dev

--- proxies/kontoregister-person-proxy/src/main/java/no/nav/testnav/proxies/kontoregisterperson/KontoregisterProxyApplicationStarter.java
+++ proxies/kontoregister-person-proxy/src/main/java/no/nav/testnav/proxies/kontoregisterperson/KontoregisterProxyApplicationStarter.java
@@ -35,7 +35,7 @@ public class KontoregisterProxyApplicationStarter {
                 .bearerAuthenticationHeaderFilter(() -> tokenService.exchange(properties).map(AccessToken::getTokenValue));
 
         return builder.routes()
-                .route(spec -> spec.path("/api/v1/**")
+                .route(spec -> spec.path("/kontoregister/**")
                         .filters(filterSpec -> filterSpec.filter(addAuthenticationHeaderDevFilter))
                         .uri(properties.getUrl()))
                 .build();
