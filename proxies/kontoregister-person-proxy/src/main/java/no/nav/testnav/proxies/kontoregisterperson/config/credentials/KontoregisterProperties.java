53591b330eb479772402e745bb809dfda6156fb6
dainius <dainiusfigoras@gmail.com>
2022-08-11 14:40:38 +0200

    bruker properties for kontoregister

--- proxies/kontoregister-person-proxy/src/main/java/no/nav/testnav/proxies/kontoregisterperson/config/credentials/KontoregisterProperties.java
+++ proxies/kontoregister-person-proxy/src/main/java/no/nav/testnav/proxies/kontoregisterperson/config/credentials/KontoregisterProperties.java
@@ -8,9 +8,4 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ConfigurationProperties(prefix = "consumers.kontoregister")
 public class KontoregisterProperties extends ServerProperties {
-    @Override
-    public String toAzureAdScope() {
-        // det er scope som må bruker for hente token
-        return "api://d750bf57-b75f-425b-b8df-24a0c3673768/.default";
-    }
 }
