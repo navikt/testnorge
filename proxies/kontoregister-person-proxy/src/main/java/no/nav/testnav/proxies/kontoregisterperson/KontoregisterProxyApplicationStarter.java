8e536c9986388fe0b4a434c21c1b25ff6ad62b43
dainius <dainiusfigoras@gmail.com>
2022-08-11 14:48:04 +0200

    fikser tings #deploy-kontoregister-person-proxy-dev

--- proxies/kontoregister-person-proxy/src/main/java/no/nav/testnav/proxies/kontoregisterperson/KontoregisterProxyApplicationStarter.java
+++ proxies/kontoregister-person-proxy/src/main/java/no/nav/testnav/proxies/kontoregisterperson/KontoregisterProxyApplicationStarter.java
@@ -35,7 +35,7 @@ public class KontoregisterProxyApplicationStarter {
                 .bearerAuthenticationHeaderFilter(() -> tokenService.exchange(properties).map(AccessToken::getTokenValue));
 
         return builder.routes()
-                .route(spec -> spec.path("/kontoregister/**")
+                .route(spec -> spec.path("/**")
                         .filters(filterSpec -> filterSpec.filter(addAuthenticationHeaderDevFilter))
                         .uri(properties.getUrl()))
                 .build();
