b32c63cb053ddcd5374b055c45365f105cdb76a0
Betsy Carina Traran <betsy.carina.traran@nav.no>
2022-08-15 23:46:49 +0200

    Fix

--- apps/dolly-frontend/src/main/js/src/service/SelectOptionsOppslag.tsx
+++ apps/dolly-frontend/src/main/js/src/service/SelectOptionsOppslag.tsx
@@ -57,7 +57,6 @@ export const SelectOptionsOppslag = {
 			}
 			const personListe: Array<PersonListe> = []
 			response.data.forEach((id: Person) => {
-				// console.log('id: ', id) //TODO - SLETT MEG
 				personListe.push({
 					value: id.person.ident,
 					label: `${id.person.ident} - ${id.person.navn[0].fornavn} ${id.person.navn[0].etternavn}`,
