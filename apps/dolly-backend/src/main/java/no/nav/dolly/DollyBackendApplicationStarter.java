4c9dec6a7576922c8a268d0c3fd4f4add63a9908
kristenhaerum <kristen.herum@nav.no>
2022-08-19 16:39:14 +0200

    Rettet feil startup fil

--- apps/dolly-backend/src/main/java/no/nav/dolly/DollyBackendApplicationStarter.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/DollyBackendApplicationStarter.java
@@ -1,9 +1,9 @@
 package no.nav.dolly;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableAutoConfiguration
+@SpringBootApplication
 public class DollyBackendApplicationStarter {
 
     public static void main(String[] args) {
