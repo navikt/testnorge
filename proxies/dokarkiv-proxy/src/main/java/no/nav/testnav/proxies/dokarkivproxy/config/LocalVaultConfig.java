5abed816bc74fab580cefa5fed260f88f0d727f2
kristenhaerum <kristen.herum@nav.no>
2022-08-24 15:55:54 +0200

    Lagt til trygdeetaten som tenant + on-behalf-of-token til dokarkiv

---
+++ proxies/dokarkiv-proxy/src/main/java/no/nav/testnav/proxies/dokarkivproxy/config/LocalVaultConfig.java
@@ -0,0 +1,34 @@
+package no.nav.testnav.proxies.dokarkivproxy.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.vault.annotation.VaultPropertySource;
+import org.springframework.vault.authentication.ClientAuthentication;
+import org.springframework.vault.authentication.TokenAuthentication;
+import org.springframework.vault.client.VaultEndpoint;
+import org.springframework.vault.config.AbstractVaultConfiguration;
+
+import java.util.Objects;
+
+import static java.util.Objects.isNull;
+
+@Configuration
+@Profile("dev")
+@VaultPropertySource(value = "azuread/prod/creds/team-dolly-lokal-app", ignoreSecretNotFound = false)
+public class LocalVaultConfig extends AbstractVaultConfiguration {
+
+    @Override
+    public VaultEndpoint vaultEndpoint() {
+        return VaultEndpoint.create("vault.adeo.no", 443);
+    }
+
+    @Override
+    public ClientAuthentication clientAuthentication() {
+
+        var token = System.getProperty("spring.cloud.vault.token");
+        if (isNull(token)) {
+            throw new IllegalArgumentException("Påkreved property 'spring.cloud.vault.token' er ikke satt.");
+        }
+        return new TokenAuthentication(System.getProperty("spring.cloud.vault.token"));
+    }
+} 
