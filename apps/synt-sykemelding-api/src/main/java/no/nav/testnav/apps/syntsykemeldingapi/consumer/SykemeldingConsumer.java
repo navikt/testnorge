ad065d21461e1c49e79f0b1b043195424d74b05e
Aileen Hay <aileen.hay@nav.no>
2022-08-24 15:03:41 +0200

    Fjernet responsenentity

--- apps/synt-sykemelding-api/src/main/java/no/nav/testnav/apps/syntsykemeldingapi/consumer/SykemeldingConsumer.java
+++ apps/synt-sykemelding-api/src/main/java/no/nav/testnav/apps/syntsykemeldingapi/consumer/SykemeldingConsumer.java
@@ -3,16 +3,12 @@ package no.nav.testnav.apps.syntsykemeldingapi.consumer;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.testnav.apps.syntsykemeldingapi.config.credentials.SykemeldingProperties;
 import no.nav.testnav.apps.syntsykemeldingapi.consumer.command.PostSykemeldingCommand;
-import no.nav.testnav.apps.syntsykemeldingapi.exception.LagreSykemeldingException;
 import no.nav.testnav.libs.dto.sykemelding.v1.SykemeldingDTO;
 import no.nav.testnav.libs.securitycore.domain.ServerProperties;
 import no.nav.testnav.libs.servletsecurity.exchange.TokenExchange;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
-
-import static java.util.Objects.isNull;
 
 @Slf4j
 @Component
@@ -36,12 +32,8 @@ public class SykemeldingConsumer {
     }
 
     public void opprettSykemelding(SykemeldingDTO sykemelding) {
-        var response = tokenExchange.exchange(serviceProperties).flatMap(accessToken ->
+        tokenExchange.exchange(serviceProperties).flatMap(accessToken ->
                         new PostSykemeldingCommand(webClient, accessToken.getTokenValue(), sykemelding).call())
                 .block();
-
-        if (isNull(response) || !HttpStatus.OK.equals(response.getStatusCode())) {
-            throw new LagreSykemeldingException("Feil oppsto i innsending av sykemelding");
-        }
     }
 }
--- apps/synt-sykemelding-api/src/main/java/no/nav/testnav/apps/syntsykemeldingapi/consumer/command/PostSykemeldingCommand.java
+++ apps/synt-sykemelding-api/src/main/java/no/nav/testnav/apps/syntsykemeldingapi/consumer/command/PostSykemeldingCommand.java
@@ -5,7 +5,6 @@ import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.testnav.apps.syntsykemeldingapi.exception.LagreSykemeldingException;
 import no.nav.testnav.libs.dto.sykemelding.v1.SykemeldingDTO;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
@@ -15,7 +14,7 @@ import static no.nav.testnav.apps.syntsykemeldingapi.util.Headers.AUTHORIZATION;
 
 @Slf4j
 @RequiredArgsConstructor
-public class PostSykemeldingCommand implements Callable<Mono<ResponseEntity<String>>> {
+public class PostSykemeldingCommand implements Callable<Mono<String>> {
 
     private final WebClient webClient;
     private final String token;
@@ -23,7 +22,7 @@ public class PostSykemeldingCommand implements Callable<Mono<ResponseEntity<Stri
 
     @SneakyThrows
     @Override
-    public Mono<ResponseEntity<String>> call() {
+    public Mono<String> call() {
         return webClient.post()
                 .uri(builder ->
                         builder.path("/sykemelding/api/v1/sykemeldinger").build()
@@ -31,7 +30,7 @@ public class PostSykemeldingCommand implements Callable<Mono<ResponseEntity<Stri
                 .header(AUTHORIZATION, "Bearer " + token)
                 .bodyValue(sykemelding)
                 .retrieve()
-                .toEntity(String.class)
+                .bodyToMono(String.class)
                 .onErrorResume(throwable -> {
                     log.error("Feil oppsto i innsending av sykemelding", throwable);
                     throw new LagreSykemeldingException("Feil oppsto i innsending av sykemelding");
