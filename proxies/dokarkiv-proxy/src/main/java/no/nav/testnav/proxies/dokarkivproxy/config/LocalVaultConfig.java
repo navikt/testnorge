d22a0dc8c343897005dbafae38200530447f0d2d
kristenhaerum <kristen.herum@nav.no>
2022-08-26 08:28:35 +0200

    Oppdatert etter tilbakemelding

--- proxies/dokarkiv-proxy/src/main/java/no/nav/testnav/proxies/dokarkivproxy/config/LocalVaultConfig.java
+++ proxies/dokarkiv-proxy/src/main/java/no/nav/testnav/proxies/dokarkivproxy/config/LocalVaultConfig.java
@@ -25,7 +25,7 @@ public class LocalVaultConfig extends AbstractVaultConfiguration {
 
         var token = System.getProperty("spring.cloud.vault.token");
         if (isNull(token)) {
-            throw new IllegalArgumentException("Påkreved property 'spring.cloud.vault.token' er ikke satt.");
+            throw new IllegalArgumentException("Påkrevd property 'spring.cloud.vault.token' er ikke satt.");
         }
         return new TokenAuthentication(System.getProperty("spring.cloud.vault.token"));
     }
