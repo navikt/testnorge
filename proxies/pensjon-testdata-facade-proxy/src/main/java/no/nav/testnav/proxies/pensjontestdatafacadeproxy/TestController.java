a04018d83dcfe0419e908165a6b13a7974b25847
dainius <dainiusfigoras@gmail.com>
2022-08-29 21:29:31 +0200

    test endpoint #deploy-pensjon-testdata-facade-proxy-dev

--- proxies/pensjon-testdata-facade-proxy/src/main/java/no/nav/testnav/proxies/pensjontestdatafacadeproxy/TestController.java
+++ proxies/pensjon-testdata-facade-proxy/src/main/java/no/nav/testnav/proxies/pensjontestdatafacadeproxy/TestController.java
@@ -2,9 +2,13 @@ package no.nav.testnav.proxies.pensjontestdatafacadeproxy;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.concurrent.ExecutionException;
 
 @RestController
 public class TestController {
@@ -15,6 +19,24 @@ public class TestController {
     public Object getMiljo1() {
         log.info("getMijo1");
         return restTemplate.getForEntity("https://pensjon-testdata-facade.dev.intern.nav.no/api/v1/miljo", String.class);
+    }
+
+    @GetMapping("miljo3")
+    public Object getMiljo3() throws ExecutionException, InterruptedException {
+        log.info("getMijo3");
+        var webClient = WebClient.builder()
+                .baseUrl("https://pensjon-testdata-facade.dev.intern.nav.no")
+                .build();
+        var result = webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/api/v1/miljo")
+                        .build())
+                .header(HttpHeaders.AUTHORIZATION, "dolly")
+                .retrieve()
+                .toEntity(String.class)
+                .toFuture()
+                .get();
+        return result;
     }
 
     @GetMapping("miljo2")
