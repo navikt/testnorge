1ebf6c0a4bafa5d99de4c05cd1b2daff62200d89
dainius <dainiusfigoras@gmail.com>
2022-08-29 21:04:02 +0200

    test endpoint #deploy-pensjon-testdata-facade-proxy-dev

--- proxies/pensjon-testdata-facade-proxy/src/main/java/no/nav/testnav/proxies/pensjontestdatafacadeproxy/TestController.java
+++ proxies/pensjon-testdata-facade-proxy/src/main/java/no/nav/testnav/proxies/pensjontestdatafacadeproxy/TestController.java
@@ -12,8 +12,14 @@ public class TestController {
     private RestTemplate restTemplate = new RestTemplate();
 
     @GetMapping("miljo1")
-    public Object getMiljo() {
-        log.info("getMijo");
+    public Object getMiljo1() {
+        log.info("getMijo1");
         return restTemplate.getForEntity("https://pensjon-testdata-facade.dev.intern.nav.no/api/v1/miljo", String.class);
+    }
+
+    @GetMapping("miljo2")
+    public Object getMiljo2() {
+        log.info("getMijo2");
+        return restTemplate.getForEntity("https://sam-q4.dev.intern.nav.no/isReady", String.class);
     }
 }
