f2acb682207c3035acf2522bc4fdac055d8eb1d7
dainius <dainiusfigoras@gmail.com>
2022-08-30 10:08:51 +0200

    ingress #deploy-pensjon-testdata-facade-proxy-dev

--- proxies/pensjon-testdata-facade-proxy/src/main/java/no/nav/testnav/proxies/pensjontestdatafacadeproxy/PensjonTestdataFacadeProxyApplicationStarter.java
+++ proxies/pensjon-testdata-facade-proxy/src/main/java/no/nav/testnav/proxies/pensjontestdatafacadeproxy/PensjonTestdataFacadeProxyApplicationStarter.java
@@ -3,20 +3,13 @@ package no.nav.testnav.proxies.pensjontestdatafacadeproxy;
 import no.nav.testnav.libs.reactivecore.config.CoreConfig;
 import no.nav.testnav.libs.reactiveproxy.config.DevConfig;
 import no.nav.testnav.libs.reactiveproxy.config.SecurityConfig;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.filter.GatewayFilterChain;
-import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpHeaders;
-import org.springframework.stereotype.Component;
-import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Mono;
 
 @Import({
         CoreConfig.class,
@@ -25,8 +18,6 @@ import reactor.core.publisher.Mono;
 })
 @SpringBootApplication
 public class PensjonTestdataFacadeProxyApplicationStarter {
-    final Logger logger = LoggerFactory.getLogger(PensjonTestdataFacadeProxyApplicationStarter.class);
-
     public static void main(String[] args) {
         SpringApplication.run(PensjonTestdataFacadeProxyApplicationStarter.class, args);
     }
@@ -37,38 +28,9 @@ public class PensjonTestdataFacadeProxyApplicationStarter {
                 .route(spec -> spec
                         .path("/api/**")
                         .filters(gatewayFilterSpec -> gatewayFilterSpec
-                                //.removeRequestHeader(HttpHeaders.AUTHORIZATION)
                                 .addRequestHeader(HttpHeaders.AUTHORIZATION, "dolly")
-                                //.addRequestHeader("Nav-Call-Id", "dolly")
-                                //.addRequestHeader("Nav-Consumer-Id", "dolly")
                         ) //Auth header er required men sjekkes ikke utover det
-                        .uri("http://pensjon-testdata-facade.pensjontestdata.svc.nais.local/"))
+                        .uri("https://pensjon-testdata-facade.dev.intern.nav.no/"))
                 .build();
     }
-
-//    @Component
-//    public class LoggingGlobalPreFilter implements GlobalFilter {
-//
-//        final Logger logger = LoggerFactory.getLogger(LoggingGlobalPreFilter.class);
-//
-//        @Override
-//        public Mono<Void> filter(
-//                ServerWebExchange exchange,
-//                GatewayFilterChain chain) {
-//            logger.info("Global Pre Filter executed");
-//            logger.info("####### headers: " + exchange.getRequest().getHeaders());
-//            logger.info("####### headers: " + exchange.getRequest().getRemoteAddress() + " " + exchange.getRequest().getPath());
-//            return chain.filter(exchange);
-//        }
-//    }
-//
-//    @Bean
-//    public GlobalFilter postGlobalFilter() {
-//        return (exchange, chain) -> {
-//            return chain.filter(exchange)
-//                    .then(Mono.fromRunnable(() -> {
-//                        logger.info("####### response headers: " + exchange.getResponse().getHeaders());
-//                    }));
-//        };
-//    }
 }
