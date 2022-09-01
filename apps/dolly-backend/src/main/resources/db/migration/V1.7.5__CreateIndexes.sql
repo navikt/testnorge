49439455d6e002517ef0551afc4a67d672ed8114
dainius <dainiusfigoras@gmail.com>
2022-09-01 11:23:05 +0200

    ident indeks i bestilling_progress #deploy-backend-dev

---
+++ apps/dolly-backend/src/main/resources/db/migration/V1.7.5__CreateIndexes.sql
@@ -0,0 +1,6 @@
+------------------------------
+-- CREATE INDEX  --
+------------------------------
+
+CREATE INDEX IF NOT EXISTS bestilling_progress_ident_idx ON bestilling_progress ( ident DESC )
+;
