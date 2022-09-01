918ed9db91c521429e412e1270be69c271e6a0fd
dainius <dainiusfigoras@gmail.com>
2022-09-01 11:25:46 +0200

    tilhoerer_gruppe indeks i test_ident #deploy-backend-dev

--- apps/dolly-backend/src/main/resources/db/migration/V1.7.5__CreateIndexes.sql
+++ apps/dolly-backend/src/main/resources/db/migration/V1.7.5__CreateIndexes.sql
@@ -4,3 +4,6 @@
 
 CREATE INDEX IF NOT EXISTS bestilling_progress_ident_idx ON bestilling_progress ( ident DESC )
 ;
+
+CREATE INDEX IF NOT EXISTS test_ident_tilhoerer_gruppe_idx ON test_ident ( tilhoerer_gruppe DESC )
+;
