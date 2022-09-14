ee911266980ba4669667474abb6556a031f314af
Aileen Hay <58416744+H153910@users.noreply.github.com>
2022-09-14 10:30:36 +0200

    Fjernet testnorge-sam (#2681)

--- apps/testnorge-sam/src/main/java/no/nav/registre/sam/database/TSamMeldingRepository.java
+++
@@ -1,9 +0,0 @@
-package no.nav.registre.sam.database;
-
-import org.springframework.data.repository.CrudRepository;
-
-import no.nav.registre.sam.domain.database.TSamMelding;
-
-public interface TSamMeldingRepository extends CrudRepository<TSamMelding, Long> {
-
-}
