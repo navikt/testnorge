a69a002498b0aa45431c2322df7c0bf6ae0983e5
Aileen Hay <58416744+H153910@users.noreply.github.com>
2022-09-19 13:39:43 +0200

    Fjernet testnorge-inst (#2692)

--- apps/testnorge-inst/src/main/java/no/nav/registre/inst/exception/UgyldigIdentResponseException.java
+++
@@ -1,8 +0,0 @@
-package no.nav.registre.inst.exception;
-
-public class UgyldigIdentResponseException extends RuntimeException {
-
-    public UgyldigIdentResponseException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
