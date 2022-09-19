ae31eb6b0721b0378ebc1d4b8041dbde460ec35f
kristenhaerum <kristen.herum@nav.no>
2022-09-19 15:00:02 +0200

    Lagt til Azure token for pdl-testdata

--- proxies/pdl-proxy/src/main/java/no/nav/testnav/proxies/pdlproxy/PdlProxyApplicationStarter.java
+++ proxies/pdl-proxy/src/main/java/no/nav/testnav/proxies/pdlproxy/PdlProxyApplicationStarter.java
@@ -4,7 +4,11 @@ import no.nav.testnav.libs.reactivecore.config.CoreConfig;
 import no.nav.testnav.libs.reactiveproxy.config.DevConfig;
 import no.nav.testnav.libs.reactiveproxy.config.SecurityConfig;
 import no.nav.testnav.libs.reactiveproxy.filter.AddAuthenticationRequestGatewayFilterFactory;
+import no.nav.testnav.libs.reactivesecurity.config.SecureOAuth2ServerToServerConfiguration;
+import no.nav.testnav.libs.reactivesecurity.exchange.azuread.TrygdeetatenAzureAdTokenService;
+import no.nav.testnav.libs.securitycore.domain.AccessToken;
 import no.nav.testnav.libs.securitytokenservice.StsOidcTokenService;
+import no.nav.testnav.proxies.pdlproxy.config.credentials.PdlTestdataProperties;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -22,7 +26,8 @@ import java.util.function.Function;
 @Import({
         CoreConfig.class,
         DevConfig.class,
-        SecurityConfig.class
+        SecurityConfig.class,
+        SecureOAuth2ServerToServerConfiguration.class
 })
 @SpringBootApplication
 public class PdlProxyApplicationStarter {
@@ -45,7 +50,13 @@ public class PdlProxyApplicationStarter {
                                            @Value("${hendelse.lager.api.key}") String hendelselagerApiKey,
                                            @Value("${person.aktor.admin.api}") String aktoerAdminApiKey,
                                            @Value("${elastic.username}") String elasticUsername,
-                                           @Value("${elastic.password}") String elasticPassword) {
+                                           @Value("${elastic.password}") String elasticPassword,
+                                           TrygdeetatenAzureAdTokenService tokenService,
+                                           PdlTestdataProperties pdlTestdataProperties) {
+
+        var addPdlTestdataAuthenticationHeaderFilter = AddAuthenticationRequestGatewayFilterFactory
+                .bearerAuthenticationHeaderFilter(() -> tokenService.exchange(pdlTestdataProperties)
+                        .map(AccessToken::getTokenValue));
 
         var addAuthorizationAndNavConsumerTokenToRouteFilter = AddAuthenticationRequestGatewayFilterFactory
                 .bearerAuthenticationAndNavConsumerTokenHeaderFilter(stsOidcTokenService::getToken);
@@ -60,7 +71,7 @@ public class PdlProxyApplicationStarter {
                 .routes()
                 .route(createRoute("pdl-api", "http://pdl-api.pdl.svc.nais.local", addAuthorizationAndNavConsumerTokenToRouteFilter))
                 .route(createRoute("pdl-api-q1", "http://pdl-api-q1.pdl.svc.nais.local", addAuthorizationAndNavConsumerTokenToRouteFilter))
-                .route(createRoute("pdl-testdata", "http://pdl-testdata.pdl.svc.nais.local", addAuthorizationAndNavConsumerTokenToRouteFilter))
+                .route(createRoute("pdl-testdata", pdlTestdataProperties.getUrl(), addPdlTestdataAuthenticationHeaderFilter))
                 .route(createRoute("pdl-identhendelse", "https://pdl-identhendelse-lager.dev.intern.nav.no", addHendelselagerApiKeyAuthenticationHeader))
                 .route(createRoute("pdl-npid", "https://pdl-aktor.dev.intern.nav.no", addAktoerAdminApiKeyAuthenticationHeader))
                 .route(createRoute("pdl-elastic", "https://pdl-es-q.adeo.no", addElasticSearchBasicAuthenticationHeader))
