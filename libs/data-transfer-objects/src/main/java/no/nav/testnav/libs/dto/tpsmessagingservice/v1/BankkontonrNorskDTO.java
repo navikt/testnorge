a43b77772bd5c9dcee116c53bc907aad70438a9a
kristenhaerum <kristen.herum@nav.no>
2022-09-20 09:04:26 +0200

    Oppdatert returnerer null-felter #deploy-backend-dev

--- libs/data-transfer-objects/src/main/java/no/nav/testnav/libs/dto/tpsmessagingservice/v1/BankkontonrNorskDTO.java
+++ libs/data-transfer-objects/src/main/java/no/nav/testnav/libs/dto/tpsmessagingservice/v1/BankkontonrNorskDTO.java
@@ -1,6 +1,5 @@
 package no.nav.testnav.libs.dto.tpsmessagingservice.v1;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,7 +12,6 @@ import java.time.LocalDateTime;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class BankkontonrNorskDTO {
 
     private String kontonummer;
