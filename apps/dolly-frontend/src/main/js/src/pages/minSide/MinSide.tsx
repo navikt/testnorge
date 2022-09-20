e5fec1ee656480791e33aa074f36253659c8a2e4
Aileen Hay <aileen.hay@nav.no>
2022-09-20 09:54:59 +0200

    Fjernet import av z-ident grupper

--- apps/dolly-frontend/src/main/js/src/pages/minSide/MinSide.tsx
+++ apps/dolly-frontend/src/main/js/src/pages/minSide/MinSide.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Maler from './maler/Maloversikt'
-import GruppeImport from './GruppeImport'
 import Profil from './Profil'
 
 import './MinSide.less'
@@ -9,12 +8,10 @@ import { useBrukerProfil } from '~/utils/hooks/useBruker'
 export default () => {
 	const { brukerProfil } = useBrukerProfil()
 
-	const AzureADProfil = brukerProfil && brukerProfil.type && brukerProfil.type === 'AzureAD'
 	return (
 		<>
 			<h1>Min side</h1>
 			<Profil />
-			{AzureADProfil && <GruppeImport />}
 			{brukerProfil && <Maler brukernavn={brukerProfil?.visningsNavn} />}
 		</>
 	)
