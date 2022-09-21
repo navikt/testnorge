29a86a9682c1022e9a25f19a38682cc4cfc66522
kristenhaerum <kristen.herum@nav.no>
2022-09-21 07:27:30 +0200

    Lagt til Azure-token for pdl-api + refaktorert #deploy-pdl-proxy

--- proxies/pdl-proxy/src/main/java/no/nav/testnav/proxies/pdlproxy/PdlProxyApplicationStarter.java
+++ proxies/pdl-proxy/src/main/java/no/nav/testnav/proxies/pdlproxy/PdlProxyApplicationStarter.java
@@ -7,8 +7,10 @@ import no.nav.testnav.libs.reactiveproxy.filter.AddAuthenticationRequestGatewayF
 import no.nav.testnav.libs.reactivesecurity.config.SecureOAuth2ServerToServerConfiguration;
 import no.nav.testnav.libs.reactivesecurity.exchange.azuread.TrygdeetatenAzureAdTokenService;
 import no.nav.testnav.libs.securitycore.domain.AccessToken;
-import no.nav.testnav.libs.securitytokenservice.StsOidcTokenService;
+import no.nav.testnav.proxies.pdlproxy.config.credentials.PdlApiProperties;
+import no.nav.testnav.proxies.pdlproxy.config.credentials.PdlQ1ApiProperties;
 import no.nav.testnav.proxies.pdlproxy.config.credentials.PdlTestdataProperties;
+import no.nav.testnav.proxies.pdlproxy.dto.CredentialsHolder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -36,49 +38,45 @@ public class PdlProxyApplicationStarter {
         SpringApplication.run(PdlProxyApplicationStarter.class, args);
     }
 
-    @Value("${hendelse.lager.api.key}")
-    private String hendelselagerApiKey;
-
-    @Value("${person.aktor.admin.api}")
-    private String aktoerAdminApiKey;
-
-    @Value("${elastic.username}")
-    private String elasticUsername;
-
-    @Value("${elastic.password}")
-    private String elasticPassword;
-
     @Bean
-    public StsOidcTokenService stsOidcTokenService(
-            @Value("${sts.token.provider.url}") String url,
-            @Value("${sts.token.provider.username}") String username,
-            @Value("${sts.token.provider.password}") String password) {
+    public CredentialsHolder credentialsHolder(
+            @Value("${hendelse.lager.api.key}") String hendelselagerApiKey,
+            @Value("${person.aktor.admin.api}") String aktoerAdminApiKey,
+            @Value("${elastic.username}") String elasticUsername,
+            @Value("${elastic.password}") String elasticPassword) {
 
-        return new StsOidcTokenService(url, username, password);
+        return new CredentialsHolder(hendelselagerApiKey, aktoerAdminApiKey, elasticUsername, elasticPassword);
     }
 
     @Bean
-    public RouteLocator customRouteLocator(RouteLocatorBuilder builder, StsOidcTokenService stsOidcTokenService,
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder,
+                                           CredentialsHolder credentialsHolder,
                                            TrygdeetatenAzureAdTokenService tokenService,
-                                           PdlTestdataProperties pdlTestdataProperties) {
+                                           PdlTestdataProperties pdlTestdataProperties,
+                                           PdlApiProperties pdlApiProperties,
+                                           PdlQ1ApiProperties pdlQ1ApiProperties) {
 
+        var addPdlApiAuthenticationHeaderFilter = AddAuthenticationRequestGatewayFilterFactory
+                .bearerAuthenticationHeaderFilter(() -> tokenService.exchange(pdlApiProperties)
+                        .map(AccessToken::getTokenValue));
+        var addPdlQ1ApiAuthenticationHeaderFilter = AddAuthenticationRequestGatewayFilterFactory
+                .bearerAuthenticationHeaderFilter(() -> tokenService.exchange(pdlQ1ApiProperties)
+                        .map(AccessToken::getTokenValue));
         var addPdlTestdataAuthenticationHeaderFilter = AddAuthenticationRequestGatewayFilterFactory
                 .bearerAuthenticationHeaderFilter(() -> tokenService.exchange(pdlTestdataProperties)
                         .map(AccessToken::getTokenValue));
 
-        var addAuthorizationAndNavConsumerTokenToRouteFilter = AddAuthenticationRequestGatewayFilterFactory
-                .bearerAuthenticationAndNavConsumerTokenHeaderFilter(stsOidcTokenService::getToken);
         var addHendelselagerApiKeyAuthenticationHeader = AddAuthenticationRequestGatewayFilterFactory
-                .apiKeyAuthenticationHeaderFilter(hendelselagerApiKey);
+                .apiKeyAuthenticationHeaderFilter(credentialsHolder.hendelselagerApiKey());
         var addAktoerAdminApiKeyAuthenticationHeader = AddAuthenticationRequestGatewayFilterFactory
-                .apiKeyAuthenticationHeaderFilter(aktoerAdminApiKey);
+                .apiKeyAuthenticationHeaderFilter(credentialsHolder.aktoerAdminApiKey());
         var addElasticSearchBasicAuthenticationHeader = AddAuthenticationRequestGatewayFilterFactory
-                .basicAuthAuthenticationHeaderFilter(elasticUsername, elasticPassword);
+                .basicAuthAuthenticationHeaderFilter(credentialsHolder.elasticUsername(), credentialsHolder.elasticPassword());
 
         return builder
                 .routes()
-                .route(createRoute("pdl-api", "http://pdl-api.pdl.svc.nais.local", addAuthorizationAndNavConsumerTokenToRouteFilter))
-                .route(createRoute("pdl-api-q1", "http://pdl-api-q1.pdl.svc.nais.local", addAuthorizationAndNavConsumerTokenToRouteFilter))
+                .route(createRoute("pdl-api", pdlApiProperties.getUrl(), addPdlApiAuthenticationHeaderFilter))
+                .route(createRoute("pdl-api-q1", pdlQ1ApiProperties.getUrl(), addPdlQ1ApiAuthenticationHeaderFilter))
                 .route(createRoute("pdl-testdata", pdlTestdataProperties.getUrl(), addPdlTestdataAuthenticationHeaderFilter))
                 .route(createRoute("pdl-identhendelse", "https://pdl-identhendelse-lager.dev.intern.nav.no", addHendelselagerApiKeyAuthenticationHeader))
                 .route(createRoute("pdl-npid", "https://pdl-aktor.dev.intern.nav.no", addAktoerAdminApiKeyAuthenticationHeader))
