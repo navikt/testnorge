4d4e3536fa3a1673509c213f8e62186f4b514d91
Aileen Hay <aileen.hay@nav.no>
2022-09-28 09:36:01 +0200

    Liten type fiks

--- apps/dolly-frontend/src/main/js/src/components/ui/form/inputs/datepicker/Datepicker.js
+++ apps/dolly-frontend/src/main/js/src/components/ui/form/inputs/datepicker/Datepicker.js
@@ -70,7 +70,7 @@ const P_FormikDatepicker = ({ fastfield, addHour = false, ...props }) => (
 				if (props.afterChange) props.afterChange(date)
 				let val = date
 				if (addHour) {
-					val = addHours(1, new Date(date))
+					val = addHours(1, new Date(date)) + ''
 				}
 				return field.onChange(SyntEvent(field.name, val))
 			}
