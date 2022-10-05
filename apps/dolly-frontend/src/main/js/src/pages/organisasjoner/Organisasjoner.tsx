9a72f02168c608f867f65f0712f68d2314dfcfd0
Aileen Hay <aileen.hay@nav.no>
2022-10-05 08:41:13 +0200

    Kode rydding

--- apps/dolly-frontend/src/main/js/src/pages/organisasjoner/Organisasjoner.tsx
+++ apps/dolly-frontend/src/main/js/src/pages/organisasjoner/Organisasjoner.tsx
@@ -11,10 +11,7 @@ import { dollySlack } from '~/components/dollySlack/dollySlack'
 import TomOrgListe from './TomOrgliste'
 import { useNavigate } from 'react-router-dom'
 import { useCurrentBruker } from '~/utils/hooks/useBruker'
-import {
-	useOrganisasjonBestilling,
-	useOrganisasjonerForBruker,
-} from '~/utils/hooks/useOrganisasjoner'
+import { useOrganisasjonBestilling } from '~/utils/hooks/useOrganisasjoner'
 import { sokSelector } from '~/ducks/bestillingStatus'
 import { useDispatch } from 'react-redux'
 import { resetPaginering } from '~/ducks/finnPerson'
@@ -47,15 +44,12 @@ export default ({ search, sidetall }: OrganisasjonerProps) => {
 
 	const { bestillinger, bestillingerById, loading } = useOrganisasjonBestilling(brukerId)
 
-	//TODO brukes denne?
-	const { loading: loadingOrganisasjoner } = useOrganisasjonerForBruker(brukerId)
-
 	const byttVisning = (value: string) => {
 		dispatch(resetPaginering())
 		setVisning(value)
 	}
 
-	const isFetching = loading || loadingOrganisasjoner
+	const isFetching = loading
 
 	const searchfieldPlaceholderSelector = () => {
 		if (visning === VISNING_BESTILLINGER) {
