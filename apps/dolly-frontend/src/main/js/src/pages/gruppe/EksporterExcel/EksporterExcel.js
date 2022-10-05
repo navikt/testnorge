97fe62ba3afeb5c092e744b252e190aea46a340b
Aileen Hay <58416744+H153910@users.noreply.github.com>
2022-10-05 12:22:35 +0200

    Exporter til excel for organisasjoner (#2741)

--- apps/dolly-frontend/src/main/js/src/pages/gruppe/EksporterExcel/EksporterExcel.js
+++ apps/dolly-frontend/src/main/js/src/pages/gruppe/EksporterExcel/EksporterExcel.js
@@ -5,25 +5,25 @@ import Logger from '~/logger'
 import Loading from '~/components/ui/loading/Loading'
 import './EksporterExcel.less'
 
-const downloadExcelString = (gruppeId, fileString) => {
+const downloadExcelString = (filPrefix, fileString) => {
 	const dagensDato = format(new Date(), 'yyyy-MM-dd')
 	const link = document.createElement('a')
 
 	link.href = fileString
-	link.download = gruppeId + '_' + dagensDato + '.xlsx'
+	link.download = filPrefix + '_' + dagensDato + '.xlsx'
 	link.target = '_blank'
 	document.body.appendChild(link)
 	link.click()
 	document.body.removeChild(link)
 }
 
-export const EksporterExcel = ({ gruppeId, action, loading }) => {
+export const EksporterExcel = ({ exportId, filPrefix, action, loading }) => {
 	if (loading) return <Loading label="henter fil..." />
 
 	const download = async () => {
 		Logger.log({ event: 'Eksporterer til excel' })
-		const data = await action(gruppeId)
-		downloadExcelString(gruppeId, data?.value)
+		const data = await action(exportId)
+		downloadExcelString(filPrefix, data?.value)
 	}
 
 	return (
