b1bf50507a07dd503c35e98ae02f4e528145d8db
stigus <stian.gustavsson@nav.no>
2022-10-05 14:44:00 +0200

    Testcafe videre testing på visning, lagt til gjennomgang av bestillinger også med en for hver bestillingsstatus type

--- apps/dolly-frontend/src/main/js/tests/dollyvisningFixture.ts
+++ apps/dolly-frontend/src/main/js/tests/dollyvisningFixture.ts
@@ -3,16 +3,29 @@
 import { RequestMock } from 'testcafe'
 import { ReactSelector, waitForReact } from 'testcafe-react-selectors'
 import {
+	aaregMock,
 	backendBestillingerMock,
+	backendTransaksjonMock,
+	brregstubMock,
 	gjeldendeBrukerMock,
 	gjeldendeGruppeMock,
 	gjeldendeProfilMock,
+	instMock,
+	joarkDokumentMock,
+	joarkJournalpostMock,
 	kodeverkMock,
+	kontoregisterMock,
+	krrstubMock,
 	malerMock,
 	miljoeMock,
 	nyGruppeMock,
+	pensjonMock,
+	sigrunstubMock,
+	skjermingMock,
+	udistubMock,
 } from './util/TestcafeMocks'
 import { pdlBulkpersonerMock, pdlForvalterMock } from './util/TestcafePdlMocks'
+import { scrollThroughPage } from './util/TestcafeUtils'
 
 const miljoer = new RegExp(/\/miljoer/)
 const current = new RegExp(/current/)
@@ -24,10 +37,21 @@ const hentGruppeBestilling = new RegExp(/dolly-backend\/api\/v1\/bestilling\/gru
 const pdlPersonBolk = new RegExp(/\/api\/v1\/pdlperson\/identer/)
 const pdlForvalter = new RegExp(/testnav-pdl-forvalter\/api\/v1\/personer/)
 const spesifikkGruppe = new RegExp(/\/gruppe$/)
+const kontoregister = new RegExp(/testnav-kontoregister-person-proxy\/api/)
+const backendTransaksjon = new RegExp(/dolly-backend\/api\/v1\/transaksjonid/)
 const tags = new RegExp(/\/tags$/)
 const kodeverk = new RegExp(/\/kodeverk\//)
-const brregStub = new RegExp(/testnav-brregstub/)
+const aareg = new RegExp(/dolly-backend\/api\/v1\/aareg\/arbeidsforhold/)
+const inst = new RegExp(/testnav-inst-service\/api\/v1\/ident/)
+const skjerming = new RegExp(/dolly-backend\/api\/v1\/skjerming/)
+const pensjon = new RegExp(/testnav-pensjon-testdata-facade-proxy\/api\/v1\/inntekt/)
+const krrstub = new RegExp(/testnav-krrstub-proxy\/api\/v2\/sdp/)
+const udistub = new RegExp(/dolly-backend\/api\/v1\/udistub/)
+const brregstub = new RegExp(/testnav-brregstub/)
+const sigrunstub = new RegExp(/testnav-sigrunstub-proxy\/api\/v1\/lignetinntekt/)
 const bestillingMaler = new RegExp(/\/bestilling\/malbestilling/)
+const joarkDokJournalpost = new RegExp(/testnav-joark-dokument-service\/api\/v2\/journalpost/)
+const joarkDokDokument = new RegExp(/dokumentType=ORIGINAL/)
 
 const remainingCallsResponseOk = new RegExp(/api/)
 
@@ -54,10 +78,32 @@ const cookieMock = RequestMock()
 	.respond(pdlBulkpersonerMock, 200)
 	.onRequestTo(pdlForvalter)
 	.respond(pdlForvalterMock, 200)
+	.onRequestTo(kontoregister)
+	.respond(kontoregisterMock, 200)
 	.onRequestTo(tags)
 	.respond({}, 200)
-	.onRequestTo(brregStub)
-	.respond({}, 200)
+	.onRequestTo(backendTransaksjon)
+	.respond(backendTransaksjonMock, 200)
+	.onRequestTo(brregstub)
+	.respond(brregstubMock, 200)
+	.onRequestTo(joarkDokJournalpost)
+	.respond(joarkJournalpostMock, 200)
+	.onRequestTo(joarkDokDokument)
+	.respond(joarkDokumentMock, 200)
+	.onRequestTo(krrstub)
+	.respond(krrstubMock, 200)
+	.onRequestTo(aareg)
+	.respond(aaregMock, 200)
+	.onRequestTo(skjerming)
+	.respond(skjermingMock, 200)
+	.onRequestTo(inst)
+	.respond(instMock, 200)
+	.onRequestTo(pensjon)
+	.respond(pensjonMock, 200)
+	.onRequestTo(sigrunstub)
+	.respond(sigrunstubMock, 200)
+	.onRequestTo(udistub)
+	.respond(udistubMock, 200)
 	.onRequestTo(kodeverk)
 	.respond(kodeverkMock, 200)
 	.onRequestTo(remainingCallsResponseOk)
@@ -67,11 +113,35 @@ fixture`Hovedside`.page`http://localhost:3000`.requestHooks(cookieMock).beforeEa
 	await waitForReact()
 })
 
+test('Naviger til bestilling tab ved hjelp av keyboard og åpne bestilling med tre forskjellige statuser', async (testController) => {
+	await testController
+		.click(ReactSelector('TableRow').withKey('1'))
+		.click(ReactSelector('ToggleGroup').withText('Bestillinger'))
+		.pressKey('tab')
+		.pressKey('shift+tab')
+		.pressKey('right')
+		.pressKey('enter')
+		.click(ReactSelector('TableRow').withKey('1'))
+		.click(ReactSelector('TableRow').withKey('2'))
+		.click(ReactSelector('TableRow').withKey('3'))
+
+	await testController
+		.expect(ReactSelector('AppError').exists)
+		.eql(
+			false,
+			'ErrorBoundary utløst, en komponent kaster Error under visning av bestillingsstatus'
+		)
+})
+
 test('Gå inn på testgruppe og åpne en ident med data i alle fagsystem', async (testController) => {
 	await testController
 		.click(ReactSelector('TableRow').withKey('1'))
 		.click(ReactSelector('TableRow').withKey('12345678912'))
-		.wait(5000)
+		.wait(1500)
+
+	await scrollThroughPage(testController, 60)
+
+	await testController
 		.expect(ReactSelector('AppError').exists)
 		.eql(false, 'ErrorBoundary utløst, en komponent kaster Error under visning av ident')
 })
--- apps/dolly-frontend/src/main/js/tests/util/TestcafeMocks.tsx
+++ apps/dolly-frontend/src/main/js/tests/util/TestcafeMocks.tsx
@@ -25,6 +25,278 @@ export const kodeverkMock = {
 	],
 }
 
+export const krrstubMock = {
+	id: 1,
+	navn: 'Digipost',
+	adresse: '984661185',
+	x509Sertifikat: 'SuperSecretSertifikat',
+}
+
+export const backendTransaksjonMock = [
+	{
+		id: 1,
+		bestillingId: 1,
+		ident: '12345678912',
+		system: 'INNTKMELD',
+		miljoe: 'q1',
+		transaksjonId: {
+			journalpostId: '999999999',
+			dokumentInfoId: '999999999',
+		},
+		datoEndret: '2022-01-01T11:58:58.227916',
+	},
+	{
+		id: 2,
+		bestillingId: 1,
+		ident: '12345678912',
+		system: 'INNTKMELD',
+		miljoe: 'q2',
+		transaksjonId: {
+			journalpostId: '999999999',
+			dokumentInfoId: '999999999',
+		},
+		datoEndret: '2022-01-01T11:58:59.289252',
+	},
+]
+
+export const sigrunstubMock = {
+	responseList: [
+		{
+			personidentifikator: '12345678912',
+			grunnlag: [
+				{
+					inntektsaar: '2022',
+					tjeneste: 'Beregnet skatt',
+					grunnlag: 'formuePrimaerbolig',
+					verdi: '12345',
+					testDataEier: '',
+				},
+			],
+			svalbardGrunnlag: [],
+		},
+	],
+}
+
+export const aaregMock = [
+	{
+		ansettelsesperiode: {
+			periode: {
+				fom: '2002-10-03',
+			},
+		},
+		arbeidsavtaler: [{}],
+		arbeidsforholdId: '1',
+		arbeidsgiver: {
+			type: 'ORGANISASJON',
+			organisasjonsnummer: '221322132',
+		},
+		arbeidstaker: {
+			type: 'PERSON',
+			offentligIdent: '12345678912',
+		},
+		innrapportertEtterAOrdningen: false,
+		permisjonPermitteringer: [],
+		type: 'forenkletOppgjoersordning',
+	},
+]
+
+export const instMock = [
+	{
+		norskident: '12345678912',
+		tssEksternId: '88888888888',
+		institusjonstype: 'AS',
+		oppholdstype: 'A',
+		startdato: '2022-01-01',
+		sluttdato: null,
+		registrertAv: 'Dolly',
+	},
+]
+
+export const udistubMock = {
+	person: {
+		aliaser: [],
+		arbeidsadgang: {
+			harArbeidsAdgang: 'JA',
+			typeArbeidsadgang: 'BESTEMT_ARBEIDSGIVER_ELLER_OPPDRAGSGIVER',
+			hjemmel: 'kapplah',
+		},
+		flyktning: true,
+		foedselsDato: '1992-01-11',
+		ident: '12345678912',
+		navn: {
+			etternavn: 'Cafe',
+			fornavn: 'Test',
+		},
+		oppholdStatus: {
+			eosEllerEFTAVedtakOmVarigOppholdsrett: 'VARIG',
+		},
+		soeknadOmBeskyttelseUnderBehandling: 'NEI',
+	},
+	reason: null,
+	status: null,
+}
+
+export const skjermingMock = {
+	endretDato: '2022-10-03 11:59:24',
+	etternavn: 'Test',
+	fornavn: 'Cafe',
+	opprettetDato: '2022-10-03 11:59:24',
+	personident: '12345678912',
+	skjermetFra: '2022-01-01 11:48:14',
+	skjermetTil: null,
+}
+
+export const brregstubMock = {
+	fnr: '12345678912',
+	fodselsdato: '1992-01-11',
+	navn: {
+		navn1: 'Cafe',
+		navn2: null,
+		navn3: 'Test',
+	},
+	adresse: {
+		adresse1: 'Testeveien 2077',
+		adresse2: null,
+		adresse3: null,
+		postnr: '3697',
+		poststed: 'UKJENT',
+		landKode: 'NO',
+		kommunenr: '3819',
+	},
+	enheter: [
+		{
+			registreringsdato: '2022-10-03',
+			rollebeskrivelse: 'Bostyrer',
+			orgNr: 905203975,
+			foretaksNavn: {
+				navn1: 'Mitt helt eget selskap',
+				navn2: null,
+				navn3: null,
+			},
+			forretningsAdresse: {
+				adresse1: 'Testeveien 51',
+				adresse2: null,
+				adresse3: null,
+				postnr: '4372',
+				poststed: 'EGERSUND',
+				landKode: 'NO',
+				kommunenr: '1101',
+			},
+			postAdresse: null,
+		},
+	],
+	hovedstatus: 0,
+	understatuser: [0],
+}
+
+export const pensjonMock = {
+	miljo: 'q1',
+	fnr: '12345678912',
+	inntekter: [
+		{
+			belop: 11843,
+			inntektAar: 2018,
+		},
+		{
+			belop: 11532,
+			inntektAar: 2017,
+		},
+		{
+			belop: 11341,
+			inntektAar: 2016,
+		},
+		{
+			belop: 11065,
+			inntektAar: 2015,
+		},
+		{
+			belop: 10796,
+			inntektAar: 2014,
+		},
+		{
+			belop: 10410,
+			inntektAar: 2013,
+		},
+		{
+			belop: 10033,
+			inntektAar: 2012,
+		},
+	],
+}
+
+export const kontoregisterMock = {
+	aktivKonto: {
+		kontohaver: '12345678912',
+		kontonummer: '99999999999',
+		gyldigFom: '2022-01-01T11:58:24.030845',
+		opprettetAv: 'Dolly',
+		kilde: 'testnav-kontoregister-person-proxy-trygdeetaten',
+	},
+}
+
+export const joarkJournalpostMock = {
+	journalpostId: 123123123,
+	tittel: 'Syntetisk Inntektsmelding',
+	tema: 'Sykepenger',
+	avsenderMottaker: {
+		type: 'ORGNR',
+		id: '947064649',
+		navn: 'Dolly Dollesen',
+	},
+	dokumenter: [
+		{
+			dokumentInfoId: 231231231,
+			tittel: 'Syntetisk Inntektsmelding',
+		},
+	],
+}
+
+export const joarkDokumentMock =
+	'<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\n' +
+	'<melding xmlns="http://seres.no/xsd/NAV/Inntektsmelding_M/20181211">\n' +
+	'    <Skjemainnhold>\n' +
+	'        <ytelse>Sykepenger</ytelse>\n' +
+	'        <aarsakTilInnsending>Ny</aarsakTilInnsending>\n' +
+	'        <arbeidsgiver>\n' +
+	'            <virksomhetsnummer>947064649</virksomhetsnummer>\n' +
+	'            <kontaktinformasjon>\n' +
+	'                <kontaktinformasjonNavn>Dolly Dollesen</kontaktinformasjonNavn>\n' +
+	'                <telefonnummer>99999999</telefonnummer>\n' +
+	'            </kontaktinformasjon>\n' +
+	'        </arbeidsgiver>\n' +
+	'        <arbeidstakerFnr>11419217524</arbeidstakerFnr>\n' +
+	'        <naerRelasjon>false</naerRelasjon>\n' +
+	'        <arbeidsforhold>\n' +
+	'            <foersteFravaersdag xsi:nil="true" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"/>\n' +
+	'            <beregnetInntekt>\n' +
+	'                <beloep>12345.0</beloep>\n' +
+	'                <aarsakVedEndring xsi:nil="true" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"/>\n' +
+	'            </beregnetInntekt>\n' +
+	'            <avtaltFerieListe/>\n' +
+	'            <utsettelseAvForeldrepengerListe/>\n' +
+	'            <graderingIForeldrepengerListe/>\n' +
+	'        </arbeidsforhold>\n' +
+	'        <refusjon>\n' +
+	'            <refusjonsbeloepPrMnd xsi:nil="true" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"/>\n' +
+	'            <refusjonsopphoersdato xsi:nil="true" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"/>\n' +
+	'            <endringIRefusjonListe/>\n' +
+	'        </refusjon>\n' +
+	'        <sykepengerIArbeidsgiverperioden>\n' +
+	'            <bruttoUtbetalt xsi:nil="true" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"/>\n' +
+	'            <begrunnelseForReduksjonEllerIkkeUtbetalt xsi:nil="true" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"/>\n' +
+	'        </sykepengerIArbeidsgiverperioden>\n' +
+	'        <startdatoForeldrepengeperiode xsi:nil="true" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"/>\n' +
+	'        <opphoerAvNaturalytelseListe/>\n' +
+	'        <gjenopptakelseNaturalytelseListe/>\n' +
+	'        <avsendersystem>\n' +
+	'            <systemnavn>Dolly</systemnavn>\n' +
+	'            <systemversjon>2.0</systemversjon>\n' +
+	'            <innsendingstidspunkt>2022-10-03T11:48:20</innsendingstidspunkt>\n' +
+	'        </avsendersystem>\n' +
+	'        <pleiepengerPerioder/>\n' +
+	'    </Skjemainnhold>\n' +
+	'</melding>\n'
+
 export const varslingerVelkommenResponseMock = [
 	{ varslingId: 'VELKOMMEN_TIL_DOLLY', fom: null, tom: null },
 ]
@@ -77,7 +349,7 @@ export const backendBestillingerMock = [
 	{
 		id: 1,
 		antallIdenter: 1,
-		antallLevert: 0,
+		antallLevert: 1,
 		ferdig: true,
 		sistOppdatert: '2022-01-01T15:36:35.474418',
 		bruker: gjeldendeBrukerMock,
@@ -96,6 +368,76 @@ export const backendBestillingerMock = [
 				],
 			},
 		],
+		bestilling: {},
+	},
+	{
+		id: 2,
+		antallIdenter: 1,
+		antallLevert: 1,
+		ferdig: true,
+		sistOppdatert: '2022-01-01T15:45:39.696068',
+		bruker: gjeldendeBrukerMock,
+		gruppeId: 1,
+		stoppet: false,
+		environments: [''],
+		status: [
+			{
+				id: 'PDL_FORVALTER',
+				navn: 'Persondataløsningen (PDL)',
+				statuser: [
+					{
+						melding: 'OK',
+						identer: ['01418221999'],
+					},
+				],
+			},
+			{
+				id: 'INNTK',
+				navn: 'Inntektskomponenten (INNTK)',
+				statuser: [
+					{
+						melding: 'Kombinasjonen av feltene i inntekten er ikke gyldig',
+						identer: ['01418221999'],
+					},
+				],
+			},
+			{
+				id: 'PEN_FORVALTER',
+				navn: 'Pensjon (PEN)',
+				statuser: [
+					{
+						melding: 'OK',
+						detaljert: [
+							{
+								miljo: 'q1',
+								identer: ['01418221999'],
+							},
+							{
+								miljo: 'q2',
+								identer: ['01418221999'],
+							},
+							{
+								miljo: 'q4',
+								identer: ['01418221999'],
+							},
+						],
+					},
+				],
+			},
+		],
+		bestilling: {},
+	},
+	{
+		id: 3,
+		antallIdenter: 1,
+		antallLevert: 1,
+		ferdig: true,
+		sistOppdatert: '2022-01-01T15:45:39.696068',
+		bruker: gjeldendeBrukerMock,
+		gruppeId: 1,
+		stoppet: false,
+		environments: [''],
+		opprettetFraId: 1,
 		bestilling: {
 			pdldata: {
 				opprettNyPerson: {
