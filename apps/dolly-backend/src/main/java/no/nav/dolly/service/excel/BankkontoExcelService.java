58a6d0019d30cf4ca1b11525e4178ae62eaf81bb
Dainius <dainiusfigoras@gmail.com>
2022-10-06 13:50:32 +0200

    eksporterer bankkonto data fra kontoregister (#2746)
    * eksporterer bankkonto data fra kontoregister
    #deploy-backend-dev
    
    * små fikser
    
    * små forbedringer
    
    * Boadresse dato validering fiks (#2736)
    
    * Feature/org controller nytt endepunkt for henting av organisasjoner (#2744)
    
    Lagt til endepunkt for henting av egne organisasjoner
    
    * Dolly frontend/henter org for bruker fra dolly backend (#2745)
    
    Endret henting av egne org
    
    * Dolly frontend/egen dokumentasjon side (#2743)
    
    Egen dokumentasjon side med links
    
    * feiler som skal vises globalt, leges til redux state (#2738)
    
    * feiler som skal vises globalt, leges til redux state
    
    * sletter ekstra symbol
    
    * Lagt på user jwt-token på Swagger #deploy-organisasjon-forvalter-dev (#2742)
    
    Lagt på user jwt-token på Swagger + secrets fra Kubernets
    
    * Exporter til excel for organisasjoner (#2741)
    
    * Feature/organisasjon maler (#2735)
    
    * Organisasjon maler kolonne og funksjonalitet backend
    * Organisasjoner oppsett for frontend og refaktor av sortering/filtrering av maler med søk
    * Ferdigstilt design og funksjonalitet for organisasjon maler og endret en del typescript og eslint regler
    
    * Update Background.tsx
    
    * Endret skjerming til hovedperson + refaktorering (#2747)
    
    Endret skjerming til hovedperson + refaktorering
    
    * NP fiks PutCommand
    
    * Feature/org controller nytt endepunkt for henting av organisasjoner (#2750)
    
    Lagt tilbake endepunkt for getOrganisasjoner
    
    * #deploy-backend-dev
    
    Co-authored-by: Aileen Hay <58416744+H153910@users.noreply.github.com>
    Co-authored-by: Kristen Hærum <kristen.herum@nav.no>
    Co-authored-by: Stian Gustavsson <stian.gustavsson@nav.no>

--- apps/dolly-backend/src/main/java/no/nav/dolly/service/excel/BankkontoExcelService.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/service/excel/BankkontoExcelService.java
@@ -1,14 +1,15 @@
 package no.nav.dolly.service.excel;
 
 import lombok.RequiredArgsConstructor;
+import no.nav.dolly.bestilling.kontoregisterservice.KontoregisterConsumer;
 import no.nav.dolly.bestilling.tpsmessagingservice.TpsMessagingConsumer;
 import no.nav.dolly.domain.jpa.Bestilling;
 import no.nav.dolly.domain.jpa.BestillingProgress;
 import no.nav.dolly.domain.jpa.Testgruppe;
-import no.nav.dolly.domain.jpa.Testident;
 import no.nav.dolly.repository.IdentRepository;
 import no.nav.testnav.libs.dto.kontoregisterservice.v1.BankkontonrNorskDTO;
 import no.nav.testnav.libs.dto.kontoregisterservice.v1.BankkontonrUtlandDTO;
+import no.nav.testnav.libs.dto.kontoregisterservice.v1.KontoDTO;
 import no.nav.testnav.libs.dto.tpsmessagingservice.v1.PersonDTO;
 import no.nav.testnav.libs.dto.tpsmessagingservice.v1.PersonMiljoeDTO;
 import org.apache.poi.ss.usermodel.IgnoredErrorType;
@@ -18,30 +19,45 @@ import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static java.util.Objects.isNull;
 import static java.util.Objects.nonNull;
-import static no.nav.dolly.service.excel.ExcelUtil.BANKKONTO_COL_WIDTHS;
-import static no.nav.dolly.service.excel.ExcelUtil.BANKKONTO_FANE;
-import static no.nav.dolly.service.excel.ExcelUtil.BANKKONTO_HEADER;
+import static no.nav.dolly.service.excel.ExcelUtil.*;
 import static org.apache.commons.lang3.StringUtils.isNotBlank;
 
 @Service
 @RequiredArgsConstructor
 public class BankkontoExcelService {
 
-    private final IdentRepository identRepository;
+    private static final LocalDateTime NYTT_KONTOREGISTER_FRA_DATO = LocalDateTime.of(2022, 8, 30, 0, 0);
     private final TpsMessagingConsumer tpsMessagingConsumer;
+    private final KontoregisterConsumer kontoregisterConsumer;
+
+    private static String getAdresse1(KontoDTO konto) {
+
+        return nonNull(konto) && nonNull(konto.getUtenlandskKontoInfo()) && isNotBlank(konto.getUtenlandskKontoInfo().getBankadresse1()) ?
+                konto.getUtenlandskKontoInfo().getBankadresse1() : "";
+    }
 
     private static String getAdresse1(BankkontonrUtlandDTO bankkontonrUtland) {
 
         return nonNull(bankkontonrUtland) && isNotBlank(bankkontonrUtland.getBankAdresse1()) ?
                 bankkontonrUtland.getBankAdresse1().trim() : "";
+    }
+
+    private static String getAdresse2(KontoDTO konto) {
+
+        return nonNull(konto) && nonNull(konto.getUtenlandskKontoInfo()) && isNotBlank(konto.getUtenlandskKontoInfo().getBankadresse2()) ?
+                konto.getUtenlandskKontoInfo().getBankadresse2() : "";
     }
 
     private static String getAdresse2(BankkontonrUtlandDTO bankkontonrUtland) {
@@ -50,10 +66,22 @@ public class BankkontoExcelService {
                 bankkontonrUtland.getBankAdresse2().trim() : "";
     }
 
+    private static String getAdresse3(KontoDTO konto) {
+
+        return nonNull(konto) && nonNull(konto.getUtenlandskKontoInfo()) && isNotBlank(konto.getUtenlandskKontoInfo().getBankadresse3()) ?
+                konto.getUtenlandskKontoInfo().getBankadresse3() : "";
+    }
+
     private static String getAdresse3(BankkontonrUtlandDTO bankkontonrUtland) {
 
         return nonNull(bankkontonrUtland) && isNotBlank(bankkontonrUtland.getBankAdresse3()) ?
                 bankkontonrUtland.getBankAdresse3().trim() : "";
+    }
+
+    private static String getSwift(KontoDTO konto) {
+
+        return nonNull(konto) && nonNull(konto.getUtenlandskKontoInfo()) && isNotBlank(konto.getUtenlandskKontoInfo().getSwiftBicKode()) ?
+                konto.getUtenlandskKontoInfo().getSwiftBicKode() : "";
     }
 
     private static String getSwift(BankkontonrUtlandDTO bankkontonrUtland) {
@@ -62,10 +90,22 @@ public class BankkontoExcelService {
                 bankkontonrUtland.getSwift().trim() : "";
     }
 
+    private static String getIban(KontoDTO konto) {
+
+        return nonNull(konto) && nonNull(konto.getUtenlandskKontoInfo()) && isNotBlank(konto.getKontonummer()) ?
+                konto.getKontonummer() : "";
+    }
+
     private static String getIban(BankkontonrUtlandDTO bankkontonrUtland) {
 
         return nonNull(bankkontonrUtland) && isNotBlank(bankkontonrUtland.getIban()) ?
                 bankkontonrUtland.getIban().trim() : "";
+    }
+
+    private static String getBanknavn(KontoDTO konto) {
+
+        return nonNull(konto) && nonNull(konto.getUtenlandskKontoInfo()) && isNotBlank(konto.getUtenlandskKontoInfo().getBanknavn()) ?
+                konto.getUtenlandskKontoInfo().getBanknavn() : "";
     }
 
     private static String getBanknavn(BankkontonrUtlandDTO bankkontonrUtland) {
@@ -74,10 +114,22 @@ public class BankkontoExcelService {
                 bankkontonrUtland.getBanknavn().trim() : "";
     }
 
+    private static String getBankkontonrNorge(KontoDTO konto) {
+
+        return nonNull(konto) && isNull(konto.getUtenlandskKontoInfo()) ?
+                konto.getKontonummer() : "";
+    }
+
     private static String getBankkontonrNorge(BankkontonrNorskDTO bankkontonrNorsk) {
 
         return nonNull(bankkontonrNorsk) && isNotBlank(bankkontonrNorsk.getKontonummer()) ?
                 bankkontonrNorsk.getKontonummer().replace(".", "") : "";
+    }
+
+    private static String getBankkontonrUtland(KontoDTO konto) {
+
+        return nonNull(konto) && nonNull(konto.getUtenlandskKontoInfo()) ?
+                konto.getKontonummer() : "";
     }
 
     private static String getBankkontonrUtland(BankkontonrUtlandDTO bankkontonrUtland) {
@@ -86,10 +138,22 @@ public class BankkontoExcelService {
                 bankkontonrUtland.getKontonummer().trim() : "";
     }
 
+    private static String getLandkode(KontoDTO konto) {
+
+        return nonNull(konto) && nonNull(konto.getUtenlandskKontoInfo()) && isNotBlank(konto.getUtenlandskKontoInfo().getBankLandkode()) ?
+                konto.getUtenlandskKontoInfo().getBankLandkode() : "";
+    }
+
     private static String getLandkode(BankkontonrUtlandDTO bankkontonrUtland) {
 
         return nonNull(bankkontonrUtland) && isNotBlank(bankkontonrUtland.getLandkode()) ?
                 bankkontonrUtland.getLandkode() : "";
+    }
+
+    private static String getValuta(KontoDTO konto) {
+
+        return nonNull(konto) && nonNull(konto.getUtenlandskKontoInfo()) && isNotBlank(konto.getUtenlandskKontoInfo().getValutakode()) ?
+                konto.getUtenlandskKontoInfo().getValutakode() : "";
     }
 
     private static String getValuta(BankkontonrUtlandDTO bankkontonrUtland) {
@@ -98,9 +162,9 @@ public class BankkontoExcelService {
                 bankkontonrUtland.getValuta() : "";
     }
 
-    public Mono<Void> prepareBankkontoSheet(XSSFWorkbook workbook, List<String> testidenter) {
+    public Mono<Void> prepareBankkontoSheet(XSSFWorkbook workbook, Testgruppe testgruppe) {
 
-        var rows = getBankkontoDetaljer(testidenter);
+        var rows = getBankkontoDetaljer(testgruppe);
 
         if (!rows.isEmpty()) {
             var sheet = workbook.createSheet(BANKKONTO_FANE);
@@ -121,28 +185,56 @@ public class BankkontoExcelService {
         return Mono.empty();
     }
 
-    private List<Object[]> getBankkontoDetaljer(List<String> identer) {
+    private Mono<List<Object[]>> tpsBankkonto(List<String> identer) {
+        return Flux.range(0, identer.size())
+                .flatMap(index -> tpsMessagingConsumer.getPersoner(List.of(identer.get(index)), List.of("q1")))
+                .filter(PersonMiljoeDTO::isOk)
+                .map(PersonMiljoeDTO::getPerson)
+                .map(person -> unpackBankkonto(person))
+                .collectList();
+    }
 
-        var bankKontoIdenter = identRepository.findByIdentIn(identer)
-                .stream()
-                .map(Testident::getTestgruppe)
-                .map(Testgruppe::getBestillinger)
-                .flatMap(Collection::stream)
+    private Mono<List<Object[]>> kontoregisterBankkonto(List<String> identer) {
+        return Flux.range(0, identer.size())
+                .flatMap(index -> kontoregisterConsumer.sendHentKontoRequest(identer.get(index)))
+                .filter(konto -> konto != null && konto.getAktivKonto() != null)
+                .map(konto -> unpackBankkonto(konto.getAktivKonto()))
+                .collectList();
+    }
+
+    private List<Object[]> getBankkontoDetaljer(Testgruppe testgruppe) {
+
+        var bankKontoIdenter = testgruppe.getBestillinger().stream()
                 .filter(bestilling -> nonNull(bestilling.getBestKriterier()))
                 .filter(bestilling -> bestilling.getBestKriterier().contains("Bankkonto"))
                 .map(Bestilling::getProgresser)
                 .flatMap(Collection::stream)
-                .map(BestillingProgress::getIdent)
-                .distinct()
-                .toList();
+                .collect(Collectors.teeing(
+                        Collectors.filtering(
+                                p -> !p.getBestilling().getSistOppdatert().isAfter(NYTT_KONTOREGISTER_FRA_DATO),
+                                Collectors.toList()
+                        ),
+                        Collectors.filtering(
+                                p -> p.getBestilling().getSistOppdatert().isAfter(NYTT_KONTOREGISTER_FRA_DATO)
+                                        && !p.getKontoregisterStatus().contains("Feil"),
+                                Collectors.toList()
+                        ),
+                        (tps, kontoregister) -> {
+                            var result = new ConcurrentHashMap<String, List<String>>();
+                            result.put("tps", tps.stream().map(BestillingProgress::getIdent).distinct().toList());
+                            result.put("kontoregister", kontoregister.stream().map(BestillingProgress::getIdent).distinct().toList());
+                            return result;
+                        }
+                ));
 
-        return Flux.range(0, bankKontoIdenter.size())
-                        .flatMap(index -> tpsMessagingConsumer.getPersoner(List.of(bankKontoIdenter.get(index)), List.of("q1")))
-                        .filter(PersonMiljoeDTO::isOk)
-                        .map(PersonMiljoeDTO::getPerson)
-                        .map(person -> unpackBankkonto(person))
-                .collectList()
-                .block();
+        return List.of(
+                        tpsBankkonto(bankKontoIdenter.get("tps")),
+                        kontoregisterBankkonto(bankKontoIdenter.get("kontoregister"))
+                )
+                .parallelStream()
+                .map(Mono::block)
+                .flatMap(Collection::stream)
+                .toList();
     }
 
     private Object[] unpackBankkonto(PersonDTO person) {
@@ -159,6 +251,23 @@ public class BankkontoExcelService {
                 getAdresse1(person.getBankkontonrUtland()),
                 getAdresse2(person.getBankkontonrUtland()),
                 getAdresse3(person.getBankkontonrUtland())
+        };
+    }
+
+    private Object[] unpackBankkonto(KontoDTO konto) {
+
+        return new Object[]{
+                konto.getKontohaver(),
+                getBankkontonrNorge(konto),
+                getBankkontonrUtland(konto),
+                getBanknavn(konto),
+                getIban(konto),
+                getLandkode(konto),
+                getValuta(konto),
+                getSwift(konto),
+                getAdresse1(konto),
+                getAdresse2(konto),
+                getAdresse3(konto)
         };
     }
 }
