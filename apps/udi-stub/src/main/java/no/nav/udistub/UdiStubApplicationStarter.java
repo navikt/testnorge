cb3f38f621e7acc32a45832a327b9121d94c414c
Kristen Hærum <kristen.herum@nav.no>
2022-10-12 12:09:36 +0200

    Feature/udi stub  fjerne dev (#2765)
    * Fjernet udi-stub-dev

--- apps/udi-stub/src/main/java/no/nav/udistub/UdiStubApplicationStarter.java
+++ apps/udi-stub/src/main/java/no/nav/udistub/UdiStubApplicationStarter.java
@@ -1,6 +1,5 @@
 package no.nav.udistub;
 
-import no.nav.testnav.libs.servletcore.util.VaultUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,10 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UdiStubApplicationStarter {
 
     public static void main(String[] args) {
-
-        if ("prod".equals(System.getProperty("spring.profiles.active"))) {
-            VaultUtil.initCloudVaultToken();
-        }
 
         SpringApplication.run(UdiStubApplicationStarter.class, args);
     }
