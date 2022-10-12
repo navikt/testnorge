59d0b62f0405a0506e8355a2ab16a10c37345536
kristenhaerum <kristen.herum@nav.no>
2022-10-12 12:03:28 +0200

    Sonar trivia

--- apps/udi-stub/src/main/java/no/nav/udistub/UdiStubApplicationStarter.java
+++ apps/udi-stub/src/main/java/no/nav/udistub/UdiStubApplicationStarter.java
@@ -1,6 +1,5 @@
 package no.nav.udistub;
 
-import no.nav.testnav.libs.servletcore.util.VaultUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
