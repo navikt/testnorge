703f4d32e351cebbe833fa3e6c00e46093299abb
kristenhaerum <kristen.herum@nav.no>
2022-10-13 10:59:11 +0200

    Forsøk på å fikse deploy #deploy-udi-stub

--- apps/udi-stub/src/main/java/no/nav/udistub/config/AppConfig.java
+++ apps/udi-stub/src/main/java/no/nav/udistub/config/AppConfig.java
@@ -3,8 +3,6 @@ package no.nav.udistub.config;
 import no.nav.testnav.libs.database.config.FlywayConfiguration;
 import no.nav.testnav.libs.database.config.VaultHikariConfiguration;
 import no.nav.testnav.libs.servletcore.config.ApplicationCoreConfig;
-import no.nav.testnav.libs.servletsecurity.config.SecureOAuth2ServerToServerConfiguration;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,12 +20,4 @@ import org.springframework.vault.core.VaultTemplate;
 })
 public class AppConfig {
 
-    @ConditionalOnMissingBean(VaultOperations.class)
-    @Bean
-    public VaultOperations vaultOperations(@Value("${spring.cloud.vault.host}") String vaultHost,
-                                           @Value("${spring.cloud.vault.port}") Integer vaultPort,
-                                           @Value("${spring.cloud.vault.token}") String token) {
-
-        return new VaultTemplate(VaultEndpoint.create(vaultHost, vaultPort), new TokenAuthentication(token));
-    }
 }
