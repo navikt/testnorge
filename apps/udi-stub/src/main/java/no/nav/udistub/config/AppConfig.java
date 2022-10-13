c07b1b68f08e7d2496fca6257c5285ed0853b548
kristenhaerum <kristen.herum@nav.no>
2022-10-13 11:07:00 +0200

    Forsøk på å fikse deploy #deploy-udi-stub

--- apps/udi-stub/src/main/java/no/nav/udistub/config/AppConfig.java
+++ apps/udi-stub/src/main/java/no/nav/udistub/config/AppConfig.java
@@ -3,14 +3,9 @@ package no.nav.udistub.config;
 import no.nav.testnav.libs.database.config.FlywayConfiguration;
 import no.nav.testnav.libs.database.config.VaultHikariConfiguration;
 import no.nav.testnav.libs.servletcore.config.ApplicationCoreConfig;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.context.annotation.Bean;
+import no.nav.testnav.libs.servletsecurity.config.SecureOAuth2ServerToServerConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.vault.authentication.TokenAuthentication;
-import org.springframework.vault.client.VaultEndpoint;
-import org.springframework.vault.core.VaultOperations;
-import org.springframework.vault.core.VaultTemplate;
 
 @Configuration
 @Import({ApplicationCoreConfig.class,
