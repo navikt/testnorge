029d41597bf725224ea7976189d3d87789b9e9e7
Aileen Hay <aileen.hay@nav.no>
2022-10-14 15:19:24 +0200

    Lagt til init cloud vault token #deploy-udi-stub

--- apps/udi-stub/src/main/java/no/nav/udistub/UdiStubApplicationStarter.java
+++ apps/udi-stub/src/main/java/no/nav/udistub/UdiStubApplicationStarter.java
@@ -1,5 +1,6 @@
 package no.nav.udistub;
 
+import no.nav.testnav.libs.servletcore.util.VaultUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UdiStubApplicationStarter {
 
     public static void main(String[] args) {
+
+        if ("prod".equals(System.getProperty("spring.profiles.active"))) {
+            VaultUtil.initCloudVaultToken();
+        }
+
         SpringApplication.run(UdiStubApplicationStarter.class, args);
     }
 }
