c00de5dd428c59ec11ec7b937b45b86fdf05753e
Aileen Hay <aileen.hay@nav.no>
2022-10-18 15:22:42 +0200

    Fjernet log

--- apps/dolly-frontend/src/main/js/src/components/inntektStub/validerInntekt/Inntekt.js
+++ apps/dolly-frontend/src/main/js/src/components/inntektStub/validerInntekt/Inntekt.js
@@ -17,7 +17,6 @@ const sjekkFelt = (formik, field, options, values, path) => {
 		_get(fieldValue, fieldPath) !== false
 	) {
 		if (fieldValue['inntektstype'] !== '' && !formik.errors?.hasOwnProperty('inntektstub')) {
-			console.log('field: ', field) //TODO - SLETT MEG
 			formik.setFieldError(`inntektstub.${field}`, 'Feltet er påkrevd')
 		}
 		return { feilmelding: 'Feltet er påkrevd' }
