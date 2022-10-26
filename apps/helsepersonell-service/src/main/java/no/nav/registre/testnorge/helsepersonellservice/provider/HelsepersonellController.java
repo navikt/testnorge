330897ecf4c05fe5646febe374826f0b9347888a
stigus <stian.gustavsson@nav.no>
2022-10-26 16:12:04 +0200

    Lagt til caching i helsepersonell-service

--- apps/helsepersonell-service/src/main/java/no/nav/registre/testnorge/helsepersonellservice/provider/HelsepersonellController.java
+++ apps/helsepersonell-service/src/main/java/no/nav/registre/testnorge/helsepersonellservice/provider/HelsepersonellController.java
@@ -3,6 +3,7 @@ package no.nav.registre.testnorge.helsepersonellservice.provider;
 import no.nav.registre.testnorge.helsepersonellservice.service.HelsepersonellService;
 import no.nav.testnav.libs.dto.helsepersonell.v1.HelsepersonellListeDTO;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.CacheControl;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
+
+import static no.nav.registre.testnorge.helsepersonellservice.config.CachingConfig.CACHE_HELSEPERSONELL;
 
 
 @RestController
@@ -28,6 +31,7 @@ public class HelsepersonellController {
         this.helsepersonellCacheHours = helsepersonellCacheHours;
     }
 
+    @Cacheable(CACHE_HELSEPERSONELL)
     @GetMapping
     public ResponseEntity<HelsepersonellListeDTO> getHelsepersonell() {
         var cacheControl
