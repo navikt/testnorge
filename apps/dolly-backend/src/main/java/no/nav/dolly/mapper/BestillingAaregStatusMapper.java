3d3fcc023c973aa3d537119cc1d6d46750e35a32
kristenhaerum <kristen.herum@nav.no>
2022-10-28 13:08:43 +0200

    Rettet feil #deploy-backend-dev

--- apps/dolly-backend/src/main/java/no/nav/dolly/mapper/BestillingAaregStatusMapper.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/mapper/BestillingAaregStatusMapper.java
@@ -10,13 +10,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import static java.util.Collections.emptyList;
 import static java.util.Collections.singletonList;
 import static no.nav.dolly.domain.resultset.SystemTyper.AAREG;
 import static no.nav.dolly.mapper.AbstractRsStatusMiljoeIdentForhold.checkAndUpdateStatus;
-import static no.nav.dolly.mapper.AbstractRsStatusMiljoeIdentForhold.decodeMsg;
 import static org.apache.commons.lang3.StringUtils.isNotBlank;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -40,15 +38,15 @@ public final class BestillingAaregStatusMapper {
         return errorEnvIdents.isEmpty() ? emptyList() :
                 singletonList(RsStatusRapport.builder().id(AAREG).navn(AAREG.getBeskrivelse())
                         .statuser(errorEnvIdents.entrySet().stream().map(status ->
-                                RsStatusRapport.Status.builder()
-                                        .melding(decodeMsg(status.getKey()))
-                                        .detaljert(status.getValue().entrySet().stream().map(miljo ->
-                                                RsStatusRapport.Detaljert.builder()
-                                                        .miljo(miljo.getKey())
-                                                        .identer(new ArrayList<>(miljo.getValue()))
-                                                        .build())
-                                                .collect(Collectors.toList()))
-                                        .build())
+                                        RsStatusRapport.Status.builder()
+                                                .melding(status.getKey().replace(";", ","))
+                                                .detaljert(status.getValue().entrySet().stream().map(miljo ->
+                                                                RsStatusRapport.Detaljert.builder()
+                                                                        .miljo(miljo.getKey())
+                                                                        .identer(new ArrayList<>(miljo.getValue()))
+                                                                        .build())
+                                                        .toList())
+                                                .build())
                                 .toList())
                         .build());
     }
