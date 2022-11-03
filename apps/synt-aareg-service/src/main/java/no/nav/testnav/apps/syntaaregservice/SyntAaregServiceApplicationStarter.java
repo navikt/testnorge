194934724e0a77d32acd2802ef3d22b2c6b9714b
Aileen Hay <aileen.hay@nav.no>
2022-11-03 10:40:14 +0100

    Initial oppsett

---
+++ apps/synt-aareg-service/src/main/java/no/nav/testnav/apps/syntaaregservice/SyntAaregServiceApplicationStarter.java
@@ -0,0 +1,13 @@
+package no.nav.testnav.apps.syntaaregservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyntAaregServiceApplicationStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SyntAaregServiceApplicationStarter.class, args);
+    }
+
+}
