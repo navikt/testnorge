9bfba2d94da93528d1a07991520cab103bd4e482
Betsy Carina Traran <betsy.carina.traran@nav.no>
2022-11-07 12:11:24 +0100

    Tabs start

---
+++ apps/dolly-frontend/src/main/js/src/components/ui/miljoTabs/MiljoTabs.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Alert, Tabs } from '@navikt/ds-react'
+
+export const MiljoTabs = ({ miljoListe, children }) => {
+	return (
+		<Tabs size="small" defaultValue="q1">
+			<Tabs.List>
+				{/*<Tabs.Tab value="q1" label="Q1" icon={<Icon kind={'feedback-check-circle'} />} />*/}
+				<Tabs.Tab value="q1" label="Q1" style={{ color: '#06893a', fontWeight: 'bold' }} />
+				<Tabs.Tab value="q2" label="Q2" />
+			</Tabs.List>
+			<Tabs.Panel value="q1" style={{ marginTop: '10px' }}>
+				{children}
+			</Tabs.Panel>
+			<Tabs.Panel value="q2" style={{ marginTop: '10px', marginBottom: '15px' }}>
+				<Alert variant="info" size="small" inline>
+					Fant ingen pensjon-data i dette miljøet
+				</Alert>
+			</Tabs.Panel>
+		</Tabs>
+	)
+}
