a89997ed4d04dadb31f673191e8183bdf93c4c4a
Betsy Carina Traran <betsy.carina.traran@nav.no>
2022-11-08 09:11:16 +0100

    Miljo-tabs komponent

--- apps/dolly-frontend/src/main/js/src/components/ui/miljoTabs/MiljoTabs.tsx
+++ apps/dolly-frontend/src/main/js/src/components/ui/miljoTabs/MiljoTabs.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Alert, Tabs } from '@navikt/ds-react'
 
-export const MiljoTabs = ({ miljoListe, children }) => {
+export const MiljoTabs = ({ miljoListe, bestilteMiljoer, forsteMiljo, data, children }) => {
 	return (
 		<Tabs size="small" defaultValue="q1">
 			<Tabs.List>
 				{/*<Tabs.Tab value="q1" label="Q1" icon={<Icon kind={'feedback-check-circle'} />} />*/}
 				<Tabs.Tab value="q1" label="Q1" style={{ color: '#06893a', fontWeight: 'bold' }} />
 				<Tabs.Tab value="q2" label="Q2" />
+				{miljoListe.forEach((miljo) => {
+					return <Tabs.Tab value={miljo} label={miljo.toUpperCase()} />
+				})}
 			</Tabs.List>
 			<Tabs.Panel value="q1" style={{ marginTop: '10px' }}>
 				{children}
