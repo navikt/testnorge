c6769ff5e1c2f37e9447c07ddb1a70d9734bf7c1
dainius <dainiusfigoras@gmail.com>
2022-11-10 13:08:09 +0100

    RootComponent.tsx som master version

--- apps/dolly-frontend/src/main/js/src/RootComponent.tsx
+++ apps/dolly-frontend/src/main/js/src/RootComponent.tsx
@@ -4,21 +4,27 @@ import { Route, Routes } from 'react-router-dom'
 import { ErrorBoundary } from '~/components/ui/appError/ErrorBoundary'
 import BrukerPage from '~/pages/brukerPage'
 import LoginPage from '~/pages/loginPage'
-import StatusPage from '~/pages/statusPage'
 import { history, store } from '~/Store'
 import { HistoryRouter as Router } from 'redux-first-history/rr6'
+import { SWRConfig } from 'swr'
 import { App } from '~/app/App'
 
 export const RootComponent = () => (
 	<Provider store={store}>
 		<Router history={history}>
 			<ErrorBoundary>
-				<Routes>
-					<Route path="/bruker" element={<BrukerPage />} />
-					<Route path="/login" element={<LoginPage />} />
-					<Route path="/status" element={<StatusPage />} />
-					<Route path="*" element={<App />} />
-				</Routes>
+				<SWRConfig
+					value={{
+						dedupingInterval: 5000,
+						revalidateOnFocus: false,
+					}}
+				>
+					<Routes>
+						<Route path="/bruker" element={<BrukerPage />} />
+						<Route path="/login" element={<LoginPage />} />
+						<Route path="*" element={<App />} />
+					</Routes>
+				</SWRConfig>
 			</ErrorBoundary>
 		</Router>
 	</Provider>
