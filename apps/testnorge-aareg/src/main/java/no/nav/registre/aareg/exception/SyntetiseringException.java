cac2220d70205e16771fe5e7000aba7f29542332
Aileen Hay <58416744+H153910@users.noreply.github.com>
2022-11-15 14:09:39 +0100

    Slettet testnorge-aareg (#2875)

--- apps/testnorge-aareg/src/main/java/no/nav/registre/aareg/exception/SyntetiseringException.java
+++
@@ -1,8 +0,0 @@
-package no.nav.registre.aareg.exception;
-
-public class SyntetiseringException extends RuntimeException {
-
-    public SyntetiseringException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
