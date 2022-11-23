0d789801895388ea70e20672cd2bf7a33ef5f311
dainius <dainiusfigoras@gmail.com>
2022-11-23 15:13:06 +0100

    uburkt imports

--- apps/testnorge-tp/src/main/java/no/nav/registre/tp/consumer/command/CreateMissingPersonsCommand.java
+++ apps/testnorge-tp/src/main/java/no/nav/registre/tp/consumer/command/CreateMissingPersonsCommand.java
@@ -1,16 +1,8 @@
 package no.nav.registre.tp.consumer.command;
 
-import lombok.RequiredArgsConstructor;
-import no.nav.registre.tp.util.WebClientFilter;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.reactive.function.client.WebClientResponseException;
-import reactor.core.publisher.Mono;
-import reactor.util.retry.Retry;
 
-import java.time.Duration;
 import java.util.List;
-import java.util.concurrent.Callable;
 
 public class CreateMissingPersonsCommand extends TpPostStringListCommand {
 
