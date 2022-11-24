4a7c3ba870801c7126fce019f737a973039fdd3a
dainius <dainiusfigoras@gmail.com>
2022-11-24 15:27:23 +0100

    azure ad/tokenx issuer og sjekk til testnorge-tp

--- apps/testnorge-tp/src/main/java/no/nav/registre/tp/config/SecurityConfig.java
+++ apps/testnorge-tp/src/main/java/no/nav/registre/tp/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 /**
  * Remove this call with AzureAd config
@@ -15,13 +16,24 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http
-                .httpBasic()
-                .and()
-                .headers().frameOptions().disable()
-                .and()
-                .csrf().disable()
-                .formLogin().disable();
+    protected void configure(HttpSecurity http) throws Exception {
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/**")
+                .fullyAuthenticated()
+                .and().oauth2ResourceServer().jwt();
     }
+
+//    @Override
+//    public void configure(HttpSecurity http) throws Exception {
+//        http
+//                .httpBasic()
+//                .and()
+//                .headers().frameOptions().disable()
+//                .and()
+//                .csrf().disable()
+//                .formLogin().disable();
+//    }
 }
