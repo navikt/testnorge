3dbb337304ec621cc8496a8bbfd70ce3a6bb6465
dainius <dainiusfigoras@gmail.com>
2022-11-28 14:34:59 +0100

    azure token for hodejegeren og testnorge-tp

--- apps/testnorge-statisk-data-forvalter/src/main/java/no/nav/registre/sdforvalter/consumer/rs/hodejegeren/HodejegerenConsumer.java
+++ apps/testnorge-statisk-data-forvalter/src/main/java/no/nav/registre/sdforvalter/consumer/rs/hodejegeren/HodejegerenConsumer.java
@@ -1,8 +1,11 @@
 package no.nav.registre.sdforvalter.consumer.rs.hodejegeren;
 
 import lombok.extern.slf4j.Slf4j;
+import no.nav.registre.sdforvalter.consumer.rs.credential.HodejegerenProperties;
 import no.nav.registre.sdforvalter.consumer.rs.hodejegeren.command.GetAlleIdenterCommand;
 import no.nav.registre.sdforvalter.consumer.rs.hodejegeren.command.GetLevendeIdenterCommand;
+import no.nav.testnav.libs.securitycore.domain.ServerProperties;
+import no.nav.testnav.libs.servletsecurity.exchange.TokenExchange;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
@@ -15,11 +18,17 @@ import java.util.List;
 public class HodejegerenConsumer {
 
     private final WebClient webClient;
+    private final TokenExchange tokenExchange;
+    private final ServerProperties serviceProperties;
 
     public HodejegerenConsumer(
             @Value("${consumers.testnorge-hodejegeren.url}") String hodejegerenServerUrl,
+            HodejegerenProperties serviceProperties,
+            TokenExchange tokenExchange,
             ExchangeFilterFunction metricsWebClientFilterFunction
     ) {
+        this.serviceProperties = serviceProperties;
+        this.tokenExchange = tokenExchange;
 
         this.webClient = WebClient.builder()
                 .baseUrl(hodejegerenServerUrl)
@@ -32,11 +41,15 @@ public class HodejegerenConsumer {
      * @return En liste med fnr som eksisterer i gruppen
      */
     public List<String> getPlaygroupFnrs(Long playgroupId) {
-        return new GetAlleIdenterCommand(playgroupId, webClient).call();
+        return tokenExchange.exchange(serviceProperties)
+                .flatMap(accessToken -> new GetAlleIdenterCommand(playgroupId, webClient, accessToken.getTokenValue()).call())
+                .block();
     }
 
     public List<String> getLivingFnrs(Long playgroupId, String environment) {
-        return new GetLevendeIdenterCommand(playgroupId, environment, webClient).call();
+        return tokenExchange.exchange(serviceProperties)
+                .flatMap(accessToken -> new GetLevendeIdenterCommand(playgroupId, environment, webClient, accessToken.getTokenValue()).call())
+                .block();
     }
 
 }
