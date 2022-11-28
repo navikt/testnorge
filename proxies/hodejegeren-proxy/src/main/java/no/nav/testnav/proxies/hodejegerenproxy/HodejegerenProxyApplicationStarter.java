7f08ed538e71bdae0ff58f981c9b936497f278a8
dainius <dainiusfigoras@gmail.com>
2022-11-28 13:59:43 +0100

    NavAzureAdTokenService

--- proxies/hodejegeren-proxy/src/main/java/no/nav/testnav/proxies/hodejegerenproxy/HodejegerenProxyApplicationStarter.java
+++ proxies/hodejegeren-proxy/src/main/java/no/nav/testnav/proxies/hodejegerenproxy/HodejegerenProxyApplicationStarter.java
@@ -1,11 +1,16 @@
 package no.nav.testnav.proxies.hodejegerenproxy;
 
+import no.nav.testnav.libs.reactiveproxy.filter.AddAuthenticationRequestGatewayFilterFactory;
+import no.nav.testnav.libs.reactivesecurity.exchange.azuread.NavAzureAdTokenService;
+import no.nav.testnav.libs.securitycore.domain.AccessToken;
+import no.nav.testnav.proxies.hodejegerenproxy.config.credentials.HodejegerenProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import no.nav.testnav.libs.reactivesecurity.config.SecureOAuth2ServerToServerConfiguration;
 
 import no.nav.testnav.libs.reactivecore.config.CoreConfig;
 import no.nav.testnav.libs.reactiveproxy.config.DevConfig;
@@ -15,18 +20,23 @@ import no.nav.testnav.libs.reactiveproxy.config.SecurityConfig;
 @Import({
         CoreConfig.class,
         DevConfig.class,
-        SecurityConfig.class
+        SecurityConfig.class,
+        SecureOAuth2ServerToServerConfiguration.class
 })
 @SpringBootApplication
 public class HodejegerenProxyApplicationStarter {
     public static void main(String[] args) {
         SpringApplication.run(HodejegerenProxyApplicationStarter.class, args);
     }
-
     @Bean
-    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder, NavAzureAdTokenService tokenService, HodejegerenProperties properties) {
+        var addAuthenticationHeaderDevFilter = AddAuthenticationRequestGatewayFilterFactory
+                .bearerAuthenticationHeaderFilter(() -> tokenService.exchange(properties).map(AccessToken::getTokenValue));
+
         return builder.routes()
-                .route(spec -> spec.path("/api/**").uri("https://testnorge-hodejegeren.dev.intern.nav.no"))
+                .route(spec -> spec.path("/api/**")
+                        .filters(filterSpec -> filterSpec.filter(addAuthenticationHeaderDevFilter))
+                        .uri("https://testnorge-hodejegeren.dev.intern.nav.no"))
                 .build();
     }
 }
