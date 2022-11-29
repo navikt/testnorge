b50c02f45d4b73d7a0b3c1daa9b5599470d3ff26
stigus <stian.gustavsson@nav.no>
2022-11-29 21:52:29 +0100

    Fix #deploy-batch-bestilling-service-dev

--- apps/batch-bestilling-service/src/test/java/no/nav/registre/testnorge/batchbestillingservice/ApplicationContextTest.java
+++ apps/batch-bestilling-service/src/test/java/no/nav/registre/testnorge/batchbestillingservice/ApplicationContextTest.java
@@ -1,10 +1,12 @@
-package no.nav.registre.testnorge.miljoerservice;
+package no.nav.registre.testnorge.batchbestillingservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 public class ApplicationContextTest {
 
