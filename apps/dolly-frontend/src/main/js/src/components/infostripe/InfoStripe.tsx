d4d5115bdd51944c2ed52105786a62514a55040d
dainius <dainiusfigoras@gmail.com>
2022-11-30 16:11:18 +0100

    utvide og skjule varsler

--- apps/dolly-frontend/src/main/js/src/components/infostripe/InfoStripe.tsx
+++ apps/dolly-frontend/src/main/js/src/components/infostripe/InfoStripe.tsx
@@ -3,7 +3,8 @@ import useSWR from 'swr'
 import { fetcher } from '~/api'
 
 import './InfoStripe.less'
-import {DollyInfoAlert} from "~/components/infostripe/DollyInfoAlert";
+import { DollyInfoAlert } from '~/components/infostripe/DollyInfoAlert'
+import Button from "~/components/ui/button/Button";
 
 const getInfoStripeUrl = `/dolly-backend/api/v1/info-stripe`
 
@@ -17,16 +18,33 @@ type InfoStripeType = {
 
 export const InfoStripe = () => {
     const expireDate = new Date(2023, 11, 29, 13, 26, 0)
-    const arenaAlert = {id: 1, text: 'Arena er ned', type: 'error', hide: null, expires: null}
+    const arenaAlert = {id: 1, text: 'Arena er nede', type: 'error', hide: null, expires: null}
     const dollyAlert = {id: 2, text: 'Team Dolly er på ferie, forvent lengre tid på svar', type: 'info', hide: null, expires: null}
 
     const [alerts, setAlerts] = useState([arenaAlert])
-    const [hidenAlerts, setHidenAlerts] = useState({})
+    const [showAll, setShowAll] = useState(false)
+    const [visibleAlerts, setVisibleAlerts] = useState([])
+    const [hiddenAlerts, setHiddenAlerts] = useState([])
 
     const hideAlert = (id) => {
-        hidenAlerts[id] = true
-        setHidenAlerts(hidenAlerts)
-        setAlerts([{expires: null, text: null, type: null, id: id, hide: true}])
+        hiddenAlerts.push(id)
+        setHiddenAlerts(hiddenAlerts)
+
+        const alert = alerts.find(alert => alert.id === id)
+        if (alert) {
+            alert.hide = true
+            setAlerts(alerts)
+
+            const newVisibleAlerts = alerts && alerts.filter(alert => {
+                console.log('is alert hiden for ', alert.id)
+                return !alert.hide
+            })
+            setVisibleAlerts(newVisibleAlerts)
+        }
+    }
+
+    const invertVisAlleMeldinger = () => {
+        setShowAll(!showAll)
     }
 
     const { data, error } = useSWR<InfoStripeType, Error>(getInfoStripeUrl, fetcher, {
@@ -40,26 +58,37 @@ export const InfoStripe = () => {
     }
 
     useEffect(() => {
-        setTimeout(() => setAlerts([arenaAlert]), 5000)
-        setTimeout(() => setAlerts([arenaAlert, dollyAlert]), 10000)
-        setTimeout(() => setAlerts([arenaAlert, dollyAlert]), 30000)
+        setTimeout(() => {setAlerts([arenaAlert]); setVisibleAlerts([arenaAlert]) }, 1000)
+        setTimeout(() => {setAlerts([arenaAlert, dollyAlert]); setVisibleAlerts([arenaAlert, dollyAlert]) }, 10000)
+        setTimeout(() => {setAlerts([arenaAlert, dollyAlert]); setVisibleAlerts([arenaAlert, dollyAlert]) }, 30000)
     }, [])
 
     if (!alerts) {
         return null
     }
 
-    const visibleAlerts = alerts && alerts.filter(alert => !alert.hide)
-
+    console.log('visible alert', visibleAlerts, hiddenAlerts)
     if (!visibleAlerts || !visibleAlerts.length) {
         return null
     }
 
+    const expandButtonText = !showAll ? `Vis alle meldinger ( ${visibleAlerts.length})`: 'Vis bare én meldinge'
+
     return (
         <div className="info-stripe">
-            {visibleAlerts && visibleAlerts.map(alert => {
+            {!showAll &&
+                <DollyInfoAlert id={visibleAlerts[0].id} type={visibleAlerts[0].type} text={visibleAlerts[0].text} onHide={hideAlert} key={visibleAlerts[0].id} />
+            }
+            {showAll && visibleAlerts.map(alert => {
                 return <DollyInfoAlert id={alert.id} type={alert.type} text={alert.text} onHide={hideAlert} key={alert.id} />
             })}
+            {visibleAlerts.length > 1 &&
+                <div>
+                    <Button onClick={invertVisAlleMeldinger} kind={showAll === false ? 'expand' : 'collapse'}>
+                        {expandButtonText}
+                    </Button>
+                </div>
+            }
         </div>
     )
 }
