82bbadfbd640f6f2046cb9f37dd3e42af9f10eb0
dainius <dainiusfigoras@gmail.com>
2022-12-01 11:11:14 +0100

    openApi konfigurasjon

--- apps/testnorge-tp/src/main/java/no/nav/registre/tp/provider/OrkestreringController.java
+++ apps/testnorge-tp/src/main/java/no/nav/registre/tp/provider/OrkestreringController.java
@@ -1,5 +1,6 @@
 package no.nav.registre.tp.provider;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.registre.tp.provider.request.OrkestreringRequest;
@@ -23,8 +24,8 @@ public class OrkestreringController {
 
     private final TpService tpService;
 
-//    @ApiOperation(value = "Dette endepunktet kan benyttes for å initialisere en database i et gitt miljø. Identer som ikke finnes i TJPEN databasen, men i avspillergruppen på det gitte miljøet vil bli "
-//            + "opprettet uten noen tilhørende ytelser.")
+    @Operation(description = "Dette endepunktet kan benyttes for å initialisere en database i et gitt miljø. Identer som ikke finnes i TJPEN databasen, men i avspillergruppen på det gitte miljøet vil bli "
+            + "opprettet uten noen tilhørende ytelser.")
     @PostMapping("/init")
     public ResponseEntity<Integer> initializeDatabase(
             @RequestBody OrkestreringRequest request
@@ -33,7 +34,7 @@ public class OrkestreringController {
         return ResponseEntity.ok(count);
     }
 
-    //@ApiOperation(value = "Dette endepunktet kan benyttes for å opprette gitte personer. De vil bli opprettet i TJPEN. Det er ikke noen verifikasjon av FNR mot TPS eller om det er et gyldig FNR.")
+    @Operation(description = "Dette endepunktet kan benyttes for å opprette gitte personer. De vil bli opprettet i TJPEN. Det er ikke noen verifikasjon av FNR mot TPS eller om det er et gyldig FNR.")
     @PostMapping("/opprettPersoner/{miljoe}")
     public ResponseEntity<List<String>> addPeople(
             @PathVariable String miljoe,
@@ -44,7 +45,7 @@ public class OrkestreringController {
         return ResponseEntity.ok(feilet);
     }
 
-    //@ApiOperation(value = "Dette endepunktet kan benyttes for å hente personer fra en gitt liste som finnes i TP.")
+    @Operation(description = "Dette endepunktet kan benyttes for å hente personer fra en gitt liste som finnes i TP.")
     @PostMapping("/hentPersonerITp/{miljoe}")
     public List<String> getPeopleInTp(
             @PathVariable String miljoe,
@@ -53,8 +54,8 @@ public class OrkestreringController {
         return tpService.filterTpOnFnrs(fnrs);
     }
 
-//    @ApiOperation(value = "Enkel implementasjon for å fjerne personer i en gitt liste fra TP. Personene fjernes kun hvis de ikke"
-//            + "har tilhørende forhold. Returnerer en liste med de personene som ble slettet.")
+    @Operation(description = "Enkel implementasjon for å fjerne personer i en gitt liste fra TP. Personene fjernes kun hvis de ikke"
+            + "har tilhørende forhold. Returnerer en liste med de personene som ble slettet.")
     @DeleteMapping("/fjernPersoner/{miljoe}")
     public List<String> removeIdentsFromTp(@PathVariable String miljoe, @RequestBody List<String> fnrs) {
         return tpService.removeFnrsFromTp(fnrs);
