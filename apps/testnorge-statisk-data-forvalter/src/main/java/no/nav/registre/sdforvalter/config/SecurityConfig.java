a11f517be76899e298bffa8dfee694f92001a3a4
dainius <dainiusfigoras@gmail.com>
2022-12-01 12:43:06 +0100

    oauth2ResourceServer for /api/**

--- apps/testnorge-statisk-data-forvalter/src/main/java/no/nav/registre/sdforvalter/config/SecurityConfig.java
+++ apps/testnorge-statisk-data-forvalter/src/main/java/no/nav/registre/sdforvalter/config/SecurityConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 /**
  * Remove this call with AzureAd config
@@ -16,13 +16,16 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http
-                .httpBasic()
-                .and()
-                .headers().frameOptions().disable()
+    protected void configure(HttpSecurity http) throws Exception {
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .csrf().disable()
-                .formLogin().disable();
+                .authorizeRequests()
+                .antMatchers("/api/**")
+                .fullyAuthenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
     }
 }
