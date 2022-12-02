e98320283c43b2f22c1f610a7bd265eed48cef26
dainius <dainiusfigoras@gmail.com>
2022-12-02 08:56:23 +0100

    infostripe api

--- apps/dolly-backend/src/main/java/no/nav/dolly/provider/api/InfostripeController.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/provider/api/InfostripeController.java
@@ -3,18 +3,27 @@ package no.nav.dolly.provider.api;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import ma.glasnost.orika.MapperFacade;
+import no.nav.dolly.domain.jpa.InfoStripe;
+import no.nav.dolly.domain.resultset.entity.infostripe.RsInfostripeMelding;
+import no.nav.dolly.exceptions.NotFoundException;
 import no.nav.dolly.repository.InformasjonsmeldingRepository;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashSet;
-import java.util.Set;
+import javax.transaction.Transactional;
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
@@ -22,30 +31,53 @@ import java.util.Set;
 @RequestMapping(value = "/api/v1/infostripe", produces = MediaType.APPLICATION_JSON_VALUE)
 public class InfostripeController {
 
-    private InformasjonsmeldingRepository informasjonsmeldingRepository;
+    private final InformasjonsmeldingRepository informasjonsmeldingRepository;
+    private final MapperFacade mapperFacade;
 
     @GetMapping()
     @Operation(description = "Hent alle gyldige informasjons meldinger")
-    public Set<Object> hentAlle() {
-        informasjonsmeldingRepository.findAll()
-        return new HashSet<>();
+    public Collection<RsInfostripeMelding> hentAlle() {
+        return informasjonsmeldingRepository.findGyldigMeldinger().stream()
+                .map(melding -> mapperFacade.map(melding, RsInfostripeMelding.class))
+                .collect(Collectors.toList());
     }
 
-    @PostMapping()
+    @PostMapping(produces = MediaType.TEXT_PLAIN_VALUE)
     @Operation(description = "Oppret ny informasjons melding")
-    public void oppretNyMelding() {
-
+    @ResponseStatus(HttpStatus.CREATED)
+    @Transactional
+    public String oppretNyMelding(@RequestBody RsInfostripeMelding melding) {
+        var infostripeMelding = mapperFacade.map(melding, InfoStripe.class);
+        informasjonsmeldingRepository.save(infostripeMelding);
+        return infostripeMelding.getId().toString();
     }
 
     @PutMapping("{id}")
     @Operation(description = "Oppdater informasjons melding")
-    public void oppdaterMeldig(@PathVariable("id") Long id) {
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Transactional
+    public void oppdaterMeldig(@PathVariable("id") Long id, @RequestBody RsInfostripeMelding melding) {
+        var infostripeMelding = informasjonsmeldingRepository.findById(id);
+        if (infostripeMelding.isEmpty()) {
+            throw new NotFoundException(id + " finnes ikke");
+        }
 
+        infostripeMelding.get().setType(melding.getType());
+        infostripeMelding.get().setMessage(melding.getMessage());
+        infostripeMelding.get().setStart(melding.getStart());
+        infostripeMelding.get().setExpires(melding.getExpires());
+        informasjonsmeldingRepository.save(infostripeMelding.get());
     }
 
     @DeleteMapping("{id}")
     @Operation(description = "Slett informasjons melding")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Transactional
     public void slettMelding(@PathVariable("id") Long id) {
-
+        try {
+            informasjonsmeldingRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException exception) {
+            throw new NotFoundException(id + " finnes ikke");
+        }
     }
 }
--- apps/dolly-backend/src/main/java/no/nav/dolly/repository/InformasjonsmeldingRepository.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/repository/InformasjonsmeldingRepository.java
@@ -1,12 +1,13 @@
 package no.nav.dolly.repository;
 
 import no.nav.dolly.domain.jpa.InfoStripe;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-import java.time.LocalDateTime;
+import java.util.Collection;
 
 public interface InformasjonsmeldingRepository extends CrudRepository<InfoStripe, Long> {
 
-    Iterable<InfoStripe> findAllByExpiresIsNullOrExpiresIsAfter(LocalDateTime expiresAfter);
-
+    @Query(value = "SELECT i FROM InfoStripe i WHERE (i.start IS NULL OR i.start <= CURRENT_TIMESTAMP) AND (i.expires IS NULL OR i.expires >= CURRENT_TIMESTAMP) ORDER BY i.id DESC")
+    Collection<InfoStripe> findGyldigMeldinger();
 }
