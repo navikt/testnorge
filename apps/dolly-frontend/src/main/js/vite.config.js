9727f7a3729c05d5d1f714f766a07febdc2b58bf
stigus <stian.gustavsson@nav.no>
2022-12-12 17:29:37 +0100

    Test #deploy-frontend-unstable

--- apps/dolly-frontend/src/main/js/vite.config.js
+++ apps/dolly-frontend/src/main/js/vite.config.js
@@ -3,7 +3,6 @@ import viteTsconfigPaths from 'vite-tsconfig-paths'
 import svgr from 'vite-plugin-svgr'
 import { resolve } from 'path'
 import EnvironmentPlugin from 'vite-plugin-environment'
-import proxyRoutes from './proxy-routes.json'
 import react from '@vitejs/plugin-react'
 import * as child from 'child_process'
 
@@ -15,7 +14,7 @@ const gitBranch = child.execSync('git branch --show-current').toString()
 export default defineConfig({
 	build: {
 		outDir: 'build',
-		loader: { '.js': 'jsx' },
+		assetsDir: '',
 	},
 	resolve: {
 		alias: {
