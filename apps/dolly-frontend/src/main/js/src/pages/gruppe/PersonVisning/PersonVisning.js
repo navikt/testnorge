46eefc0ce18c6bf9e7899467ccaec4dd0b1dd284
Aileen Hay <aileen.hay@nav.no>
2022-12-13 11:02:24 +0100

    Kode rydding

--- apps/dolly-frontend/src/main/js/src/pages/gruppe/PersonVisning/PersonVisning.js
+++ apps/dolly-frontend/src/main/js/src/pages/gruppe/PersonVisning/PersonVisning.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect } from 'react'
 import Button from '~/components/ui/button/Button'
 import { TidligereBestillinger } from './TidligereBestillinger/TidligereBestillinger'
 import { PersonMiljoeinfo } from './PersonMiljoeinfo/PersonMiljoeinfo'
