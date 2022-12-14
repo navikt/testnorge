9a33dc720efca217c161f07d9186b00ebe11e9f7
stigus <stian.gustavsson@nav.no>
2022-12-14 10:19:43 +0100

    deploy #deploy-frontend-dev

--- apps/dolly-frontend/src/main/java/no/nav/dolly/web/config/SecurityConfig.java
+++ apps/dolly-frontend/src/main/java/no/nav/dolly/web/config/SecurityConfig.java
@@ -48,21 +48,7 @@ public class SecurityConfig {
 
         return http.cors()
                 .and().csrf().disable()
-                .authorizeExchange()
-                .pathMatchers(
-                        "/internal/isReady",
-                        "/internal/isAlive",
-                        "/internal/metrics",
-                        "/oauth2/callback",
-                        "/favicon.ico",
-                        LOGIN,
-                        LOGOUT,
-                        "/oauth2/logout",
-                        "/*.css",
-                        "/*.js",
-                        "/*.png"
-                ).permitAll()
-                .anyExchange().authenticated()
+                .authorizeExchange().anyExchange().permitAll()
                 .and().oauth2Login(oAuth2LoginSpec -> oAuth2LoginSpec
                         .authenticationManager(authenticationManger)
                         .authenticationSuccessHandler(authenticationSuccessHandler))
