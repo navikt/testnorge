cc5e3688ab884ed80a27872f14ceea5b6f75eba1
Cato Olsen <cato.olsen@nav.no>
2022-12-21 14:12:15 +0100

    Slår av innkommende security for lokal kjøring/testing.

--- proxies/aareg-proxy/src/test/java/no/nav/testnav/proxies/aareg/RouteLocatorConfigTest.java
+++ proxies/aareg-proxy/src/test/java/no/nav/testnav/proxies/aareg/RouteLocatorConfigTest.java
@@ -1,7 +1,5 @@
 package no.nav.testnav.proxies.aareg;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,8 +9,6 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
-import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -22,27 +18,11 @@ class RouteLocatorConfigTest {
     @Autowired
     private WebTestClient client;
 
-    @BeforeAll
-    public static void createStubForSts() {
-        stubFor(
-                get(urlEqualTo("/rest/v1/sts/token?grant_type=client_credentials&scope=openid"))
-                        .willReturn(aResponse()
-                                .withHeader(CONTENT_TYPE, APPLICATION_JSON_VALUE)
-                                .withBody("""
-                        {
-                            "access_token": "token",
-                            "expires_in": 123
-                        }
-                        """))
-        );
-    }
-
     @ParameterizedTest
     @ValueSource(strings = {
             "q1"/*, "q2", "q4", "q5",
             "t0", "t1", "t3", "t4", "t5"*/}
     )
-    @Disabled // TODO: Fix 401 on TrygdeetatenAzureAdTokenService (presumably).
     void getOnUrlWithEnvironmentShouldRedirectToNewUrl(String env) {
 
         final var FROM_ACTUAL_URI = "/" + env + "/api/v1/arbeidsforhold/";
@@ -50,17 +30,16 @@ class RouteLocatorConfigTest {
         final var BODY = "I have been correctly routed from " + FROM_ACTUAL_URI + " to " + TO_EXPECTED_URI;
 
         stubFor(
-            get(urlEqualTo(TO_EXPECTED_URI))
-                .withHeader("miljoe", equalTo(env))
-                .willReturn(aResponse()
-                    .withBody(BODY))
+                get(urlEqualTo(TO_EXPECTED_URI))
+                        .willReturn(aResponse()
+                                .withBody(BODY))
         );
 
         client
-            .get().uri(FROM_ACTUAL_URI)
-            .exchange()
-            .expectStatus().isOk()
-            .expectBody(String.class).isEqualTo(BODY);
+                .get().uri(FROM_ACTUAL_URI)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(String.class).isEqualTo(BODY);
     }
 
 }
