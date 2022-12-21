20688ee76e59fd6ccae35400a81680b3a4865e8d
dainius <dainiusfigoras@gmail.com>
2022-12-21 15:03:58 +0100

    popp-testdata-proxy for hente token for popp-testdata som er i trygdetaten tenant #deploy-popp-testdata-proxy

---
+++ apps/dolly-backend/src/main/java/no/nav/dolly/bestilling/pensjonforvalter/command/GetPoppInntekterCommand.java
@@ -0,0 +1,48 @@
+package no.nav.dolly.bestilling.pensjonforvalter.command;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import no.nav.dolly.util.WebClientFilter;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
+
+import java.time.Duration;
+import java.util.concurrent.Callable;
+
+import static no.nav.dolly.domain.CommonKeysAndUtils.*;
+import static no.nav.dolly.util.CallIdUtil.generateCallId;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+@Slf4j
+@RequiredArgsConstructor
+public class GetPoppInntekterCommand implements Callable<Mono<JsonNode>> {
+
+    private static final String FNR_HEADER = "fnr";
+    private static final String MILJO_HEADER = "environment";
+    private static final String POPP_INNTEKT_URL = "/inntekt";
+
+    private final WebClient webClient;
+    private final String token;
+    private final String ident;
+    private final String miljoe;
+
+    public Mono<JsonNode> call() {
+        return webClient
+                .get()
+                .uri(uriBuilder -> uriBuilder
+                        .path(POPP_INNTEKT_URL)
+                        .build())
+                .header(AUTHORIZATION, "Bearer " + token)
+                .header(HEADER_NAV_CALL_ID, generateCallId())
+                .header(HEADER_NAV_CONSUMER_ID, CONSUMER)
+                .header(FNR_HEADER, ident)
+                .header(MILJO_HEADER, miljoe)
+                .retrieve()
+                .bodyToMono(JsonNode.class)
+                .doOnError(WebClientFilter::logErrorMessage)
+                .retryWhen(Retry.backoff(3, Duration.ofSeconds(5))
+                        .filter(WebClientFilter::is5xxException));
+    }
+}
