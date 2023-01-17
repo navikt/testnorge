1982815253d67590ed4a7b268a9bc6fe1c8592e5
stigus <stian.gustavsson@nav.no>
2023-01-17 16:34:46 +0100

    Testeteste #deploy-frontend-unstable

--- apps/dolly-frontend/src/main/java/no/nav/dolly/web/config/SecurityConfig.java
+++ apps/dolly-frontend/src/main/java/no/nav/dolly/web/config/SecurityConfig.java
@@ -1,26 +1,36 @@
 package no.nav.dolly.web.config;
 
+import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoders;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
+import java.util.Arrays;
+
 
 @Slf4j
 @Configuration
 @Profile({ "prod", "dev" })
 @EnableWebFluxSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+
+    private final Environment environment;
     @Value("${spring.security.oauth2.resourceserver.aad.issuer-uri}")
-    private String issuer;
+    private String aadIssuer;
+
+    @Value("${spring.security.oauth2.resourceserver.idporten.issuer-uri}")
+    private String idPortenIssuer;
 
     @SneakyThrows
     @Bean
@@ -37,6 +47,6 @@ public class SecurityConfig {
 
     @Bean
     public ReactiveJwtDecoder jwtDecoder() {
-        return ReactiveJwtDecoders.fromOidcIssuerLocation(issuer);
+        return ReactiveJwtDecoders.fromOidcIssuerLocation(Arrays.asList(environment.getActiveProfiles()).contains("idporten") ? idPortenIssuer : aadIssuer);
     }
 }
