f43bbe56d69d23cc92c094f3b4e17891ed638349
stigus <stian.gustavsson@nav.no>
2023-01-19 15:05:08 +0100

    Test #deploy-frontend-idporten

--- apps/dolly-frontend/src/main/java/no/nav/dolly/web/service/AccessService.java
+++ apps/dolly-frontend/src/main/java/no/nav/dolly/web/service/AccessService.java
@@ -10,20 +10,22 @@ import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 import java.util.Arrays;
+import java.util.List;
 
 
 @Service
 @RequiredArgsConstructor
 public class AccessService {
+    private static final List<String> SESSION_PROFILES = List.of("lokal", "idporten");
     private final TokenExchange tokenExchange;
 
     private final Environment environment;
 
-    private final no.nav.testnav.libs.reactivesessionsecurity.exchange.TokenExchange localClientExchange;
+    private final no.nav.testnav.libs.reactivesessionsecurity.exchange.TokenExchange sessionTokenExchange;
 
     public Mono<String> getAccessToken(ServerProperties serverProperties, ServerWebExchange exchange) {
-        return Arrays.asList(environment.getActiveProfiles()).contains("local")
-                ? localClientExchange
+        return Arrays.stream(environment.getActiveProfiles()).anyMatch(SESSION_PROFILES::contains)
+                ? sessionTokenExchange
                 .exchange(serverProperties, exchange)
                 .map(AccessToken::getTokenValue)
                 : tokenExchange
