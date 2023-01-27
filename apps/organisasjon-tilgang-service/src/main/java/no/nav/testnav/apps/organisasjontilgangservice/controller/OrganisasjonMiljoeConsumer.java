5c9bda1f3c08110c9d16c1545b9258f337f13bb6
kristenhaerum <kristen.herum@nav.no>
2023-01-27 09:50:36 +0100

    Lagt til NOT FOND exception #deploy-organisasjon-tilgang-service

--- apps/organisasjon-tilgang-service/src/main/java/no/nav/testnav/apps/organisasjontilgangservice/controller/OrganisasjonMiljoeConsumer.java
+++ apps/organisasjon-tilgang-service/src/main/java/no/nav/testnav/apps/organisasjontilgangservice/controller/OrganisasjonMiljoeConsumer.java
@@ -4,11 +4,18 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.testnav.apps.organisasjontilgangservice.database.entity.OrganisasjonTilgang;
 import no.nav.testnav.apps.organisasjontilgangservice.database.repository.OrganisasjonTilgangRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.webjars.NotFoundException;
 import reactor.core.publisher.Mono;
+
+import static org.apache.commons.lang3.BooleanUtils.isTrue;
 
 @Slf4j
 @RestController
@@ -21,6 +28,20 @@ public class OrganisasjonMiljoeConsumer {
     @GetMapping("/organisasjon/{orgnummer}")
     public Mono<OrganisasjonTilgang> getOrganisasjon(@RequestParam("orgnummer") String orgnummer) {
 
-        return organisasjonTilgangRepository.findByOrganisasjonNummer(orgnummer);
+        return organisasjonTilgangRepository.existsByOrganisasjonNummer(orgnummer)
+                .flatMap(miljoe -> isTrue(miljoe) ?
+                        organisasjonTilgangRepository.findByOrganisasjonNummer(orgnummer) :
+                        Mono.error(new NotFoundException(
+                                String.format("Organisasjonsnummer %s ble ikke funnet", orgnummer))));
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<String> handleNoSuchElementFoundException(
+            NotFoundException exception
+    ) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(exception.getMessage());
     }
 }
