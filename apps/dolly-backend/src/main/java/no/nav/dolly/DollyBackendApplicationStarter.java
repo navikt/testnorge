96fe22c2ba8f7ddfd669f3305c683ddf5ad38d06
Cato Olsen <cato.olsen@nav.no>
2023-02-06 15:35:45 +0100

    Filer som manglet i forrige commit... #deploy-backend-dev

--- apps/dolly-backend/src/main/java/no/nav/dolly/DollyBackendApplicationStarter.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/DollyBackendApplicationStarter.java
@@ -2,8 +2,13 @@ package no.nav.dolly;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 
 @SpringBootApplication
+// TODO: Clean up test code.
+@EnableReactiveMethodSecurity
+//@EnableWebFluxSecurity
+//
 public class DollyBackendApplicationStarter {
 
     public static void main(String[] args) {
