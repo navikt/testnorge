041f3ca267683c4ea762e723c23869f97a5e23dc
Cato Olsen <cato.olsen@nav.no>
2023-02-07 16:14:11 +0100

    Lagt på logging ved krysskobling. #deploy-backend-dev

--- apps/dolly-backend/src/main/java/no/nav/dolly/util/EnvironmentsCrossConnect.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/util/EnvironmentsCrossConnect.java
@@ -1,10 +1,12 @@
 package no.nav.dolly.util;
 
 import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.*;
 
 @UtilityClass
+@Slf4j
 public class EnvironmentsCrossConnect {
     
     public static Set<String> crossConnect(Set<String> environments, Type type) {
@@ -13,14 +15,17 @@ public class EnvironmentsCrossConnect {
             case Q4_TO_Q1 -> {
                 // Krysskobling av miljøer Q4 -> Q1 etter ønske fra pensjon
                 if (connected.contains("q4")) {
+                    log.info("Krysskobler miljøer Q4 -> Q1");
                     connected.add("q1");
                 }
                 yield connected;
             }
             case Q1_AND_Q2 -> {
                 if (connected.contains("q1")) {
+                    log.info("Krysskobler miljøer Q1 -> Q2");
                     connected.add("q2");
                 } else if (connected.contains("q2")) {
+                    log.info("Krysskobler miljøer Q2 -> Q1");
                     connected.add("q1");
                 }
                 yield connected;
