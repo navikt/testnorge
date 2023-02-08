650c885a25dbbc87c26c8ac6b51a575810ec3da2
stigus <stian.gustavsson@nav.no>
2023-02-08 09:06:23 +0100

    Idporten #deploy-frontend-idporten

--- apps/dolly-frontend/src/main/java/no/nav/dolly/web/config/IdportenSecurityConfig.java
+++ apps/dolly-frontend/src/main/java/no/nav/dolly/web/config/IdportenSecurityConfig.java
@@ -3,16 +3,14 @@ package no.nav.dolly.web.config;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import no.nav.testnav.libs.reactivesecurity.manager.JwtReactiveAuthenticationManager;
 import no.nav.testnav.libs.reactivesessionsecurity.config.OicdRedisSessionConfiguration;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
-import org.springframework.security.oauth2.jwt.ReactiveJwtDecoders;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 
@@ -24,9 +22,7 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 @RequiredArgsConstructor
 public class IdportenSecurityConfig {
 
-
-    @Value("${spring.security.oauth2.resourceserver.idporten.issuer-uri}")
-    private String idportenIssuer;
+    private final JwtReactiveAuthenticationManager jwtReactiveAuthenticationManager;
 
     @SneakyThrows
     @Bean
@@ -37,12 +33,8 @@ public class IdportenSecurityConfig {
                 .authorizeExchange()
                 .anyExchange()
                 .permitAll()
-                .and().oauth2ResourceServer().jwt(jwt -> jwtDecoder());
+                .and().oauth2ResourceServer()
+                .jwt(spec -> spec.authenticationManager(jwtReactiveAuthenticationManager));
         return http.build();
-    }
-
-    @Bean
-    public ReactiveJwtDecoder jwtDecoder() {
-        return ReactiveJwtDecoders.fromOidcIssuerLocation(idportenIssuer);
     }
 }
