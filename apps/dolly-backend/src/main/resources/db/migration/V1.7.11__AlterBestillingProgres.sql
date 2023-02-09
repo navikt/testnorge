810aadd1753a9ee90cb185a50342b31ffb08d928
kristenhaerum <kristen.herum@nav.no>
2023-02-09 10:47:06 +0100

    Oppdatert flyway #deploy-backend-dev

--- apps/dolly-backend/src/main/resources/db/migration/V1.7.11__AlterBestillingProgres.sql
+++ apps/dolly-backend/src/main/resources/db/migration/V1.7.11__AlterBestillingProgres.sql
@@ -5,5 +5,8 @@
 alter table bestilling_progress
     add column pdl_person_status varchar(500);
 
+alter table bestilling_progress
+    add column VERSJON smallint not null default 0;
+
 alter table bestilling
     alter column miljoer drop not null;
--- apps/dolly-backend/src/main/resources/db/migration/V1.7.12__AlterBestillingProgress.sql
+++
@@ -1,6 +0,0 @@
-------------------------------
--- A L T E R   T A B L E S  --
-------------------------------
-
-alter table bestilling_progress
-    add column VERSJON bigint not null default 0;
