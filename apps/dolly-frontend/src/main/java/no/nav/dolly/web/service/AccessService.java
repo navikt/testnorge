98ee3fcb99e607f276d283b02e40c30c8024ec98
stigus <stian.gustavsson@nav.no>
2023-02-09 11:34:14 +0100

    Idporten #deploy-frontend-idporten

--- apps/dolly-frontend/src/main/java/no/nav/dolly/web/service/AccessService.java
+++ apps/dolly-frontend/src/main/java/no/nav/dolly/web/service/AccessService.java
@@ -2,6 +2,7 @@ package no.nav.dolly.web.service;
 
 import lombok.RequiredArgsConstructor;
 import no.nav.testnav.libs.reactivesecurity.exchange.TokenExchange;
+import no.nav.testnav.libs.reactivesessionsecurity.exchange.TokenXExchange;
 import no.nav.testnav.libs.securitycore.domain.AccessToken;
 import no.nav.testnav.libs.securitycore.domain.ServerProperties;
 import org.springframework.core.env.Environment;
@@ -9,7 +10,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-import java.util.Arrays;
 import java.util.List;
 
 
@@ -19,17 +19,22 @@ public class AccessService {
     private static final List<String> SESSION_PROFILES = List.of("local");
     private final TokenExchange tokenExchange;
 
+    private final TokenXExchange tokenXExchange;
+
     private final Environment environment;
 
     private final no.nav.testnav.libs.reactivesessionsecurity.exchange.TokenExchange sessionTokenExchange;
 
     public Mono<String> getAccessToken(ServerProperties serverProperties, ServerWebExchange exchange) {
-        return Arrays.stream(environment.getActiveProfiles()).anyMatch(SESSION_PROFILES::contains)
-                ? sessionTokenExchange
-                .exchange(serverProperties, exchange)
-                .map(AccessToken::getTokenValue)
-                : tokenExchange
-                .exchange(serverProperties)
-                .map(AccessToken::getTokenValue);
+
+
+        return tokenXExchange.exchange(serverProperties, exchange).map(AccessToken::getTokenValue);
+//        return Arrays.stream(environment.getActiveProfiles()).anyMatch(SESSION_PROFILES::contains)
+//                ? sessionTokenExchange
+//                .exchange(serverProperties, exchange)
+//                .map(AccessToken::getTokenValue)
+//                : tokenExchange
+//                .exchange(serverProperties)
+//                .map(AccessToken::getTokenValue);
     }
 }
