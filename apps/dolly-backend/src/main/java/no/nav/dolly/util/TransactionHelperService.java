361fbbb8a4622b6e4a85508bcc9e889f41f60760
kristenhaerum <kristen.herum@nav.no>
2023-02-13 07:25:31 +0100

    Foreslår pessimistic locking

--- apps/dolly-backend/src/main/java/no/nav/dolly/util/TransactionHelperService.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/util/TransactionHelperService.java
@@ -8,6 +8,7 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.persistence.EntityManager;
+import javax.persistence.LockModeType;
 import java.util.function.BiConsumer;
 
 import static java.time.LocalDateTime.now;
@@ -34,7 +35,7 @@ public class TransactionHelperService {
     public BestillingProgress oppdaterProgress(BestillingProgress progress) {
 
         return transactionTemplate.execute(status -> {
-            var best = entityManager.find(Bestilling.class, progress.getBestilling().getId());
+            var best = entityManager.find(Bestilling.class, progress.getBestilling().getId(), LockModeType.PESSIMISTIC_FORCE_INCREMENT);
             entityManager.persist(progress);
             best.setSistOppdatert(now());
             entityManager.merge(best);
@@ -46,7 +47,7 @@ public class TransactionHelperService {
     public BestillingProgress persister(BestillingProgress bestillingProgress, BiConsumer<BestillingProgress, String> setter, String status) {
 
         return transactionTemplate.execute(status1 -> {
-            var progress = entityManager.find(BestillingProgress.class, bestillingProgress.getId());
+            var progress = entityManager.find(BestillingProgress.class, bestillingProgress.getId(), LockModeType.PESSIMISTIC_FORCE_INCREMENT);
 
             this.setField(progress, status, setter);
             entityManager.persist(progress);
@@ -59,7 +60,7 @@ public class TransactionHelperService {
     public Bestilling oppdaterBestillingFerdig(Bestilling bestilling) {
 
         return transactionTemplate.execute(status -> {
-            var best = entityManager.find(Bestilling.class, bestilling.getId());
+            var best = entityManager.find(Bestilling.class, bestilling.getId(), LockModeType.PESSIMISTIC_FORCE_INCREMENT);
             best.setSistOppdatert(now());
             best.setFerdig(true);
             best.setFeil(bestilling.getFeil());
