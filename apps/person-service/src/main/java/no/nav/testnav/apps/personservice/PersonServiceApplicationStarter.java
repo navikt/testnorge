959b48aeb538c821d3f48eb60ab142d84073151e
kristenhaerum <kristen.herum@nav.no>
2023-02-15 12:30:14 +0100

    Oppdatert config #deploy-dokarkiv-proxy

--- apps/person-service/src/main/java/no/nav/testnav/apps/personservice/PersonServiceApplicationStarter.java
+++ apps/person-service/src/main/java/no/nav/testnav/apps/personservice/PersonServiceApplicationStarter.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PersonServiceApplicationStarter {
-
     public static void main(String[] args) {
         SpringApplication.run(PersonServiceApplicationStarter.class, args);
     }
