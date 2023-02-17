e1a614ed06bd467e2bdd1e58ab0e6e7d3dc7f062
stigus <stian.gustavsson@nav.no>
2023-02-17 11:28:01 +0100

    Test oppdatering #deploy-oppsummeringsdokument-service-dev

--- apps/oppsummeringsdokument-service/src/main/java/no/nav/testnav/apps/oppsummeringsdokumentservice/OppsummeringsdokumentServiceApplicationStarter.java
+++ apps/oppsummeringsdokument-service/src/main/java/no/nav/testnav/apps/oppsummeringsdokumentservice/OppsummeringsdokumentServiceApplicationStarter.java
@@ -2,9 +2,10 @@ package no.nav.testnav.apps.oppsummeringsdokumentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { ElasticsearchDataAutoConfiguration.class })
 public class OppsummeringsdokumentServiceApplicationStarter {
 
     public static void main(String[] args) {
