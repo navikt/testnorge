e7dd99856483e0aec9280831ca55aa0f8c2b3163
kristenhaerum <kristen.herum@nav.no>
2023-02-20 10:04:02 +0100

    Rettet feil ved nyPerson #deploy_backend_dev

--- apps/dolly-backend/src/main/java/no/nav/dolly/bestilling/service/OppdaterPersonService.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/bestilling/service/OppdaterPersonService.java
@@ -81,7 +81,7 @@ public class OppdaterPersonService extends DollyBestillingService {
                                         .flatMap(pdlResponse -> sendOrdrePerson(progress, pdlResponse)) :
                                 Flux.just(testident.getIdent()))
                                 .filter(Objects::nonNull)
-                                .flatMap(ident -> opprettDollyPerson(testident.getIdent(), progress, bestilling.getBruker())
+                                .flatMap(ident -> opprettDollyPerson(ident, progress, bestilling.getBruker())
                                         .flatMap(dollyPerson -> (!dollyPerson.getIdent().equals(bestilling.getIdent()) ?
                                                 updateIdent(dollyPerson, progress) : Flux.just(ident))
                                                 .doOnNext(nyident -> counterCustomRegistry.invoke(request))
