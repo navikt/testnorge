b078d59847cf3c486a40ad921d4fc87e78d31821
kristenhaerum <kristen.herum@nav.no>
2023-02-23 14:01:22 +0100

    Lagt til proxy for arbeidsplassen-cv #deploy-arbeidsplassencv-proxy

---
+++ apps/dolly-backend/src/main/java/no/nav/testnav/proxies/arbeidsplassencvproxy/ArbeidsplassenCVProxyApplicationStarter.java
@@ -0,0 +1,56 @@
+package no.nav.testnav.proxies.arbeidsplassencvproxy;
+
+import no.nav.testnav.libs.reactivecore.config.CoreConfig;
+import no.nav.testnav.libs.reactiveproxy.config.DevConfig;
+import no.nav.testnav.libs.reactiveproxy.config.SecurityConfig;
+import no.nav.testnav.libs.reactiveproxy.filter.AddAuthenticationRequestGatewayFilterFactory;
+import no.nav.testnav.libs.reactivesecurity.exchange.azuread.TrygdeetatenAzureAdTokenService;
+import no.nav.testnav.libs.securitycore.domain.AccessToken;
+import no.nav.testnav.proxies.arbeidsplassencvproxy.config.ArbeidsplassenCVProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.Buildable;
+import org.springframework.cloud.gateway.route.builder.PredicateSpec;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+
+import java.util.function.Function;
+
+@Import({
+        CoreConfig.class,
+        DevConfig.class,
+        SecurityConfig.class
+})
+@SpringBootApplication
+public class ArbeidsplassenCVProxyApplicationStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(ArbeidsplassenCVProxyApplicationStarter.class, args);
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder,
+                                           TrygdeetatenAzureAdTokenService tokenService,
+                                           ArbeidsplassenCVProperties arbeidsplassenCVProperties) {
+
+        return builder.routes()
+                        .route(createRoute(arbeidsplassenCVProperties.getUrl(),
+                        AddAuthenticationRequestGatewayFilterFactory
+                                .bearerAuthenticationHeaderFilter(() -> tokenService.exchange(arbeidsplassenCVProperties)
+                                        .map(AccessToken::getTokenValue))))
+                .build();
+    }
+
+    private Function<PredicateSpec, Buildable<Route>> createRoute(String url, GatewayFilter filter) {
+        return spec -> spec
+                .path("**")
+                .filters(filterSpec -> filterSpec
+                        .rewritePath("(?<segment>.*)", "/pam-cv-api/${segment}")
+                        .setResponseHeader("Content-Type", "application/json; charset=UTF-8")
+                        .filter(filter)
+                ).uri(url);
+    }
+}
---
+++ apps/dolly-backend/src/main/java/no/nav/testnav/proxies/arbeidsplassencvproxy/config/ArbeidsplassenCVProperties.java
@@ -0,0 +1,10 @@
+package no.nav.testnav.proxies.arbeidsplassencvproxy.config;
+
+import no.nav.testnav.libs.securitycore.domain.ServerProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "consumer.arbeidsplassencv-proxy")
+public class ArbeidsplassenCVProperties extends ServerProperties {
+}
