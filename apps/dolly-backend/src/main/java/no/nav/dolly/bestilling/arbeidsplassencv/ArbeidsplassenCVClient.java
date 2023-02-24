a5dda96fa28d8b372782f0c2f484310c12b46a09
kristenhaerum <kristen.herum@nav.no>
2023-02-24 15:53:46 +0100

    Kommentert ut kode #deploy-backend-dev

--- apps/dolly-backend/src/main/java/no/nav/dolly/bestilling/arbeidsplassencv/ArbeidsplassenCVClient.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/bestilling/arbeidsplassencv/ArbeidsplassenCVClient.java
@@ -33,8 +33,8 @@ public class ArbeidsplassenCVClient implements ClientRegister {
     @Override
     public void release(List<String> identer) {
 
-        arbeidplassenCVConsumer.deleteCVer(identer)
-                .collectList()
-                .subscribe(response -> log.info("Slettet utført mot ArbeidsplassenCV"));
+//        arbeidplassenCVConsumer.deleteCVer(identer)
+//                .collectList()
+//                .subscribe(response -> log.info("Slettet utført mot ArbeidsplassenCV"));
     }
 }
