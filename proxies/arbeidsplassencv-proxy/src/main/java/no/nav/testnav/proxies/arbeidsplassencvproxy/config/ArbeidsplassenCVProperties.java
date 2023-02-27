10ae448c15be742783cbac074510da8d3eeb1eac
Kristen Hærum <kristen.herum@nav.no>
2023-02-27 10:13:51 +0100

    Feature/ny proxy for arbeidsplassen cv (#3056)
    * Lagt til proxy for arbeidsplassen-cv #deploy-arbeidsplassencv-proxy

--- proxies/arbeidsplassencv-proxy/src/main/java/no/nav/testnav/proxies/arbeidsplassencvproxy/config/ArbeidsplassenCVProperties.java
+++ proxies/arbeidsplassencv-proxy/src/main/java/no/nav/testnav/proxies/arbeidsplassencvproxy/config/ArbeidsplassenCVProperties.java
@@ -1,10 +1,10 @@
-package no.nav.dolly.config.credentials;
+package no.nav.testnav.proxies.arbeidsplassencvproxy.config;
 
 import no.nav.testnav.libs.securitycore.domain.ServerProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix = "consumers.testnav-popp-testdata-proxy")
-public class PoppTestdataProperties extends ServerProperties {
+@ConfigurationProperties(prefix = "consumer.arbeidsplassencv-proxy")
+public class ArbeidsplassenCVProperties extends ServerProperties {
 }
