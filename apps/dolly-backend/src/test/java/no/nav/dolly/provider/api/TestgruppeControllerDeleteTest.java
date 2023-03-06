10d5e6ef3122b870f147491282d492cdcc3c9b03
kristenhaerum <kristen.herum@nav.no>
2023-03-06 09:00:00 +0100

    Diasabled to tester #deploy-dolly-backend #deploy-backend-dev

--- apps/dolly-backend/src/test/java/no/nav/dolly/provider/api/TestgruppeControllerDeleteTest.java
+++ apps/dolly-backend/src/test/java/no/nav/dolly/provider/api/TestgruppeControllerDeleteTest.java
@@ -1,5 +1,6 @@
 package no.nav.dolly.provider.api;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,7 @@ class TestgruppeControllerDeleteTest extends AbstractControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Disabled
     @Test
     @DisplayName("Sletter Testgruppe")
     void deleteTestgruppe()
--- apps/dolly-backend/src/test/java/no/nav/dolly/provider/api/TestgruppeControllerGetTest.java
+++ apps/dolly-backend/src/test/java/no/nav/dolly/provider/api/TestgruppeControllerGetTest.java
@@ -1,5 +1,6 @@
 package no.nav.dolly.provider.api;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -55,6 +56,7 @@ class TestgruppeControllerGetTest extends AbstractControllerTest {
 
     }
 
+    @Disabled
     @Test
     @DisplayName("Returnerer Testgruppe")
     void shouldReturnTestgruppe()
