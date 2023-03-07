048dea2bdb5f1870b0f27fa7adc656474b942466
stigus <stian.gustavsson@nav.no>
2023-03-07 12:47:50 +0100

    Fix for tps-messaging og testing av Udi-Stub #deploy-udi-stub #deploy-messaging-service-dev #deploy-inntektsmelding-generator-service

--- apps/inntektsmelding-service/src/main/java/no/nav/registre/testnav/inntektsmeldingservice/repository/model/InntektsmeldingModel.java
+++ apps/inntektsmelding-service/src/main/java/no/nav/registre/testnav/inntektsmeldingservice/repository/model/InntektsmeldingModel.java
@@ -1,24 +1,26 @@
 package no.nav.registre.testnav.inntektsmeldingservice.repository.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.Hibernate;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import java.util.Date;
+import java.util.Objects;
 
 
 @Entity
@@ -28,12 +30,12 @@ import java.util.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
-@EqualsAndHashCode
 @ToString
 public class InntektsmeldingModel {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequence_generator")
+    @SequenceGenerator(name = "sequence_generator", sequenceName = "hibernate_sequence")
     @Column(name = "id")
     private Long id;
 
@@ -41,4 +43,17 @@ public class InntektsmeldingModel {
     @Column(name = "created_at", nullable = false, updatable = false)
     @CreatedDate
     private Date createdAt;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        InntektsmeldingModel that = (InntektsmeldingModel) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
