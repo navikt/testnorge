cc7edc7f75dca56f85dec388071526960eaa1f7c
stigus <stian.gustavsson@nav.no>
2023-03-07 11:28:39 +0100

    Fix for tps-messaging og testing av Udi-Stub #deploy-udi-stub #deploy-messaging-service-dev #deploy-inntektsmelding-generator-service

--- apps/inntektsmelding-generator-service/src/main/java/no/nav/registre/inntektsmeldinggeneratorservice/v20181211/dto/v1/KontaktinformasjonDTO.java
+++ apps/inntektsmelding-generator-service/src/main/java/no/nav/registre/inntektsmeldinggeneratorservice/v20181211/dto/v1/KontaktinformasjonDTO.java
@@ -1,12 +1,11 @@
 package no.nav.registre.inntektsmeldinggeneratorservice.v20181211.dto.v1;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Size;
 import lombok.NoArgsConstructor;
 import lombok.Value;
 import no.seres.xsd.nav.inntektsmelding_m._20181211.ObjectFactory;
 import no.seres.xsd.nav.inntektsmelding_m._20181211.XMLKontaktinformasjon;
-
-import javax.validation.constraints.Size;
 
 @Value
 @NoArgsConstructor(force = true)
