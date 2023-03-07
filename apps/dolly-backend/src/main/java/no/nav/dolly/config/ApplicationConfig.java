45511a6eb25859dc08b290a52b790b5881b4a9e9
Cato Olsen <olsen.cato@gmail.com>
2023-03-07 15:08:43 +0100

    Dependency på og @Import av reactive-core.

--- apps/dolly-backend/src/main/java/no/nav/dolly/config/ApplicationConfig.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/config/ApplicationConfig.java
@@ -1,6 +1,7 @@
 package no.nav.dolly.config;
 
 import jakarta.annotation.PostConstruct;
+import no.nav.testnav.libs.reactivecore.config.CoreConfig;
 import no.nav.testnav.libs.servletcore.config.ApplicationCoreConfig;
 import no.nav.testnav.libs.servletsecurity.config.SecureOAuth2ServerToServerConfiguration;
 import no.nav.testnav.libs.standalone.servletsecurity.config.InsecureJwtServerToServerConfiguration;
@@ -11,13 +12,17 @@ import org.springframework.security.core.context.SecurityContextHolder;
 
 @Configuration
 @EnableRetry
-@Import({ApplicationCoreConfig.class,
+@Import({
+        ApplicationCoreConfig.class,
         SecureOAuth2ServerToServerConfiguration.class,
-        InsecureJwtServerToServerConfiguration.class})
+        InsecureJwtServerToServerConfiguration.class,
+        CoreConfig.class
+})
 public class ApplicationConfig {
 
     @PostConstruct
     public void enableAuthCtxOnSpawnedThreads() {
         SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
     }
+
 }
