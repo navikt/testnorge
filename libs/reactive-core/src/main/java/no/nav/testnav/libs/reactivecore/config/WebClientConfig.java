4c244ca8544a302ea0b8d18924f378c6f78cbb75
Cato Olsen <olsen.cato@gmail.com>
2023-03-08 10:57:48 +0100

    Bean for WebClient.Builder tar nå hensyn til om det finnes en ObservationRegistry bean eller ikke.

--- libs/reactive-core/src/main/java/no/nav/testnav/libs/reactivecore/config/WebClientConfig.java
+++ libs/reactive-core/src/main/java/no/nav/testnav/libs/reactivecore/config/WebClientConfig.java
@@ -1,25 +1,48 @@
 package no.nav.testnav.libs.reactivecore.config;
 
 import io.micrometer.observation.ObservationRegistry;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.DefaultClientRequestObservationConvention;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
+@Slf4j
 public class WebClientConfig {
 
     @Bean
-    public WebClient.Builder webClientBuilder(ObservationRegistry observationRegistry) {
-        return WebClient
-                .builder()
-                .observationConvention(new DefaultClientRequestObservationConvention())
-                .observationRegistry(observationRegistry);
+    public WebClient.Builder webClientBuilder(ApplicationContext context) {
+
+        try {
+
+            var observationRegistry = context.getBean(ObservationRegistry.class);
+            log.info(
+                    "Using {} with observation registry {}",
+                    WebClient.Builder.class.getCanonicalName(),
+                    observationRegistry.getClass().getCanonicalName()
+            );
+            return WebClient
+                    .builder()
+                    .observationConvention(new DefaultClientRequestObservationConvention())
+                    .observationRegistry(observationRegistry);
+
+        } catch (NoSuchBeanDefinitionException e) {
+
+            log.info(
+                    "No {} found in context, using {} without any observation registry",
+                    ObservationRegistry.class.getCanonicalName(),
+                    WebClient.Builder.class.getCanonicalName()
+            );
+            return WebClient.builder();
+
+        }
     }
 
     @Bean
     public WebClient webClient(WebClient.Builder webClientBuilder) {
         return webClientBuilder.build();
     }
-
 }
