987c81a02fad745b7263a46d1b9552484ca2d02e
stigus <stian.gustavsson@nav.no>
2023-03-15 14:09:59 +0100

    Dolly assets update #deploy-oversikt-frontend

---
+++ navikt/dolly-assets/vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+import svgr from 'vite-plugin-svgr';
+import { resolve } from 'path';
+import react from '@vitejs/plugin-react';
+
+/** @type {import('vite').UserConfig} */
+
+export default defineConfig(({ mode }) => ({
+  base: '/',
+  build: {
+    outDir: 'build',
+    cssCodeSplit: false,
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+      '~': resolve(__dirname, './src'),
+    },
+  },
+  plugins: [react(), svgr(), viteTsconfigPaths(), splitVendorChunkPlugin()],
+}));
