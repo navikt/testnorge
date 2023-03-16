382636f42e37de5061f58fdb91e2d2051a6288f5
stigus <stian.gustavsson@nav.no>
2023-03-16 17:28:08 +0100

    Logger sendt sykemelding #deploy-sykemelding-api

--- apps/sykemelding-api/src/main/java/no/nav/registre/testnorge/sykemelding/consumer/SyfoConsumer.java
+++ apps/sykemelding-api/src/main/java/no/nav/registre/testnorge/sykemelding/consumer/SyfoConsumer.java
@@ -1,11 +1,10 @@
 package no.nav.registre.testnorge.sykemelding.consumer;
 
 import lombok.extern.slf4j.Slf4j;
+import no.nav.registre.testnorge.sykemelding.domain.Sykemelding;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
-
-import no.nav.registre.testnorge.sykemelding.domain.Sykemelding;
 
 @Slf4j
 @Component
@@ -20,7 +19,7 @@ public class SyfoConsumer {
 
     public void send(Sykemelding sykemelding) {
         String xml = sykemelding.toXml();
-        log.info("Legger sykemelding på kø med MsgId {}", sykemelding.getMsgId());
+        log.info("Legger sykemelding på kø med MsgId {}\n{}", sykemelding.getMsgId(), sykemelding);
         jmsTemplate.send(queueName, session -> session.createTextMessage(xml));
         log.trace(xml);
     }
