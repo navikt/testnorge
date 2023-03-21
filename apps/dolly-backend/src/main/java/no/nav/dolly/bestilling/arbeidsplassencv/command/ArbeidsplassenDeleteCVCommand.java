d85b404bb153da256b716123918789673754df51
kristenhaerum <kristen.herum@nav.no>
2023-03-21 11:30:39 +0100

    Fix for mal og arbplCV sletting #deploy-backend-dev

--- apps/dolly-backend/src/main/java/no/nav/dolly/bestilling/arbeidsplassencv/command/ArbeidsplassenDeleteCVCommand.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/bestilling/arbeidsplassencv/command/ArbeidsplassenDeleteCVCommand.java
@@ -1,13 +1,13 @@
 package no.nav.dolly.bestilling.arbeidsplassencv.command;
 
 import lombok.RequiredArgsConstructor;
-import no.nav.dolly.bestilling.arbeidsplassencv.dto.CVDeleteDTO;
+import no.nav.dolly.bestilling.arbeidsplassencv.dto.ArbeidsplassenCVStatusDTO;
 import no.nav.dolly.util.WebClientFilter;
-import no.nav.testnav.libs.dto.arbeidsplassencv.v1.ArbeidsplassenCVDTO;
 import no.nav.testnav.libs.securitycore.config.UserConstant;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.util.retry.Retry;
 
 import java.time.Duration;
@@ -16,18 +16,17 @@ import java.util.concurrent.Callable;
 import static no.nav.dolly.util.TokenXUtil.getUserJwt;
 
 @RequiredArgsConstructor
-public class ArbeidsplassenDeleteCVCommand implements Callable<Flux<ArbeidsplassenCVDTO>> {
+public class ArbeidsplassenDeleteCVCommand implements Callable<Mono<ArbeidsplassenCVStatusDTO>> {
 
-    private static final String ARBEIDSPLASSEN_CV_URL = "/rest/v2/cv";
+    private static final String ARBEIDSPLASSEN_CV_URL = "/rest/samtykke";
     private static final String FNR = "fnr";
 
     private final WebClient webClient;
     private final String ident;
-    private final CVDeleteDTO cvDelete;
     private final String token;
 
     @Override
-    public Flux<ArbeidsplassenCVDTO> call() {
+    public Mono<ArbeidsplassenCVStatusDTO> call() {
 
         return webClient.delete().uri(
                         uriBuilder -> uriBuilder
@@ -37,9 +36,11 @@ public class ArbeidsplassenDeleteCVCommand implements Callable<Flux<Arbeidsplass
                 .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
                 .header(UserConstant.USER_HEADER_JWT, getUserJwt())
                 .retrieve()
-                .bodyToFlux(ArbeidsplassenCVDTO.class)
+                .toBodilessEntity()
+                .map(status -> ArbeidsplassenCVStatusDTO.builder()
+                                .status(HttpStatus.valueOf(status.getStatusCode().value()))
+                                .build())
                 .doOnError(WebClientFilter::logErrorMessage)
-                .onErrorResume(throwable -> Flux.empty())
                 .retryWhen(Retry.backoff(3, Duration.ofSeconds(5))
                         .filter(WebClientFilter::is5xxException));
     }
