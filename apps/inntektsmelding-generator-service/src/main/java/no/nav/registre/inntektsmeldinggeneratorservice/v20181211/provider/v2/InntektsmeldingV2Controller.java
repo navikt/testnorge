ee57c1997bc07ebe3d634578df4c3b91f816172c
stigus <stian.gustavsson@nav.no>
2023-03-21 18:08:16 +0100

    Enda mer logging for å kunne sette riktig verdi på journalpost #deploy-frontend-dev #deploy-inntektsmelding-service #deploy-inntektsmelding-generator-service #deploy-backend-dev

--- apps/inntektsmelding-generator-service/src/main/java/no/nav/registre/inntektsmeldinggeneratorservice/v20181211/provider/v2/InntektsmeldingV2Controller.java
+++ apps/inntektsmelding-generator-service/src/main/java/no/nav/registre/inntektsmeldinggeneratorservice/v20181211/provider/v2/InntektsmeldingV2Controller.java
@@ -1,7 +1,10 @@
 package no.nav.registre.inntektsmeldinggeneratorservice.v20181211.provider.v2;
 
+import io.swagger.v3.core.util.Json;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import no.nav.registre.inntektsmeldinggeneratorservice.util.XmlConverter;
+import no.nav.registre.inntektsmeldinggeneratorservice.v20181211.dto.v1.InntektsmeldingDTO;
 import no.seres.xsd.nav.inntektsmelding_m._20181211.XMLInntektsmeldingM;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -13,9 +16,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.xml.bind.JAXBElement;
 
-import no.nav.registre.inntektsmeldinggeneratorservice.util.XmlConverter;
-import no.nav.registre.inntektsmeldinggeneratorservice.v20181211.dto.v1.InntektsmeldingDTO;
-
 @Slf4j
 @RestController
 @RequestMapping("/api/v2/inntektsmelding/2018/12/11")
@@ -24,6 +24,7 @@ public class InntektsmeldingV2Controller {
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_XML_VALUE)
     public ResponseEntity<String> create(@RequestBody InntektsmeldingDTO inntektsmeldingDTO) {
+        log.info("Mottok inntektsmeldingDTO: {}", Json.pretty(inntektsmeldingDTO));
         JAXBElement<XMLInntektsmeldingM> melding = inntektsmeldingDTO.toMelding();
         String xml = XmlConverter.toXml(melding, XMLInntektsmeldingM.class);
 
@@ -32,6 +33,7 @@ public class InntektsmeldingV2Controller {
                     .status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body("Validering av opprett xml feilet");
         }
+        log.info("Genererte XML for inntektsmelding: {}", Json.pretty(xml));
 
         return ResponseEntity.ok(xml);
     }
