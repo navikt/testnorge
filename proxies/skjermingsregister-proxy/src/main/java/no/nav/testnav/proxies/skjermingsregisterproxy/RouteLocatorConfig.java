dafe7f328c3d26b97cade6136a64bac7d1e03303
Cato Olsen <olsen.cato@gmail.com>
2023-03-24 11:47:41 +0100

    Erstattet STS med Azure.

--- proxies/skjermingsregister-proxy/src/main/java/no/nav/testnav/proxies/skjermingsregisterproxy/RouteLocatorConfig.java
+++ proxies/skjermingsregister-proxy/src/main/java/no/nav/testnav/proxies/skjermingsregisterproxy/RouteLocatorConfig.java
@@ -1,11 +1,12 @@
 package no.nav.testnav.proxies.skjermingsregisterproxy;
 
-import no.nav.testnav.libs.reactivecore.config.CoreConfig;
-import no.nav.testnav.libs.reactiveproxy.config.DevConfig;
 import no.nav.testnav.libs.reactiveproxy.config.SecurityConfig;
 import no.nav.testnav.libs.reactiveproxy.filter.AddAuthenticationRequestGatewayFilterFactory;
-import no.nav.testnav.libs.securitytokenservice.StsOidcTokenService;
-import org.springframework.beans.factory.annotation.Value;
+import no.nav.testnav.libs.reactivesecurity.config.SecureOAuth2ServerToServerConfiguration;
+import no.nav.testnav.libs.reactivesecurity.exchange.azuread.TrygdeetatenAzureAdTokenService;
+import no.nav.testnav.libs.securitycore.domain.AccessToken;
+import no.nav.testnav.libs.securitycore.domain.ServerProperties;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -13,35 +14,33 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Import({
-        CoreConfig.class,
-        DevConfig.class,
+        SecureOAuth2ServerToServerConfiguration.class,
         SecurityConfig.class
 })
 @Configuration
 public class RouteLocatorConfig {
 
     @Bean
-    public StsOidcTokenService stsOidcTokenService(
-            @Value("${sts.token.provider.url}") String url,
-            @Value("${sts.token.provider.username}") String username,
-            @Value("${sts.token.provider.password}") String password) {
-
-        return new StsOidcTokenService(url, username, password);
-    }
-
-    @Bean
-    public RouteLocator customRouteLocator(RouteLocatorBuilder builder, StsOidcTokenService stsOidcTokenService) {
-
-        var addAuthenticationHeaderFilter = AddAuthenticationRequestGatewayFilterFactory
-                .bearerAuthenticationHeaderFilter(stsOidcTokenService::getToken);
-
-        return builder.routes()
+    public RouteLocator customRouteLocator(
+            RouteLocatorBuilder builder,
+            TrygdeetatenAzureAdTokenService tokenService,
+            SkjermingsregisterProperties serverProperties
+    ) {
+        return builder
+                .routes()
                 .route(spec -> spec
                         .path("/**")
                         .filters(filterSpec -> filterSpec
-                                .filter(addAuthenticationHeaderFilter))
-                        .uri("https://skjermede-personer.dev.adeo.no/"))
+                                .filter(getAuthenticationFilter(tokenService, serverProperties)))
+                        .uri(serverProperties.getUrl()))
                 .build();
+    }
+
+    private GatewayFilter getAuthenticationFilter(TrygdeetatenAzureAdTokenService tokenService, ServerProperties serverProperties) {
+        return AddAuthenticationRequestGatewayFilterFactory
+                .bearerAuthenticationHeaderFilter(() -> tokenService
+                        .exchange(serverProperties)
+                        .map(AccessToken::getTokenValue));
     }
 
 }
