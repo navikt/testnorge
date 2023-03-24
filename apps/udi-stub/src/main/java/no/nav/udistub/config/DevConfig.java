c936313cbd318d267344dfbf8336fb901806487e
kristenhaerum <kristen.herum@nav.no>
2023-03-24 07:56:29 +0100

    Sonar trivia

--- apps/udi-stub/src/main/java/no/nav/udistub/config/DevConfig.java
+++ apps/udi-stub/src/main/java/no/nav/udistub/config/DevConfig.java
@@ -1,9 +1,6 @@
 package no.nav.udistub.config;
 
-import no.nav.testnav.libs.database.config.FlywayConfiguration;
-import no.nav.testnav.libs.database.config.VaultHikariConfiguration;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 import org.springframework.vault.annotation.VaultPropertySource;
 import org.springframework.vault.authentication.ClientAuthentication;
@@ -16,6 +13,8 @@ import org.springframework.vault.config.AbstractVaultConfiguration;
 @VaultPropertySource(value = "secret/dolly/lokal", ignoreSecretNotFound = false)
 public class DevConfig extends AbstractVaultConfiguration {
 
+    private static final String VAULT_PROPERTY = "spring.cloud.vault.token";
+
     @Override
     public VaultEndpoint vaultEndpoint() {
         return VaultEndpoint.create("vault.adeo.no", 443);
@@ -25,12 +24,12 @@ public class DevConfig extends AbstractVaultConfiguration {
     public ClientAuthentication clientAuthentication() {
 
         if (System.getenv().containsKey("VAULT_TOKEN")) {
-            System.setProperty("spring.cloud.vault.token", System.getenv("VAULT_TOKEN"));
+            System.setProperty(VAULT_PROPERTY, System.getenv("VAULT_TOKEN"));
         }
-        var token = System.getProperty("spring.cloud.vault.token");
+        var token = System.getProperty(VAULT_PROPERTY);
         if (token == null) {
-            throw new IllegalArgumentException("Påkreved property 'spring.cloud.vault.token' er ikke satt.");
+            throw new IllegalArgumentException(String.format("Påkreved property '%s' er ikke satt.", VAULT_PROPERTY));
         }
-        return new TokenAuthentication(System.getProperty("spring.cloud.vault.token"));
+        return new TokenAuthentication(System.getProperty(VAULT_PROPERTY));
     }
 }
