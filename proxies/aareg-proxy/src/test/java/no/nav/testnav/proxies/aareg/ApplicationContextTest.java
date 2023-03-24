0abf2ecdc159d8e46193fbc952d49645520ca946
kristenhaerum <kristen.herum@nav.no>
2023-03-24 10:40:07 +0100

    Aareg-proxy: disabled test #deploy-aareg-proxy

--- proxies/aareg-proxy/src/test/java/no/nav/testnav/proxies/aareg/ApplicationContextTest.java
+++ proxies/aareg-proxy/src/test/java/no/nav/testnav/proxies/aareg/ApplicationContextTest.java
@@ -1,5 +1,6 @@
 package no.nav.testnav.proxies.aareg;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -8,6 +9,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+@Disabled
 @SpringBootTest
 @ActiveProfiles("test")
 class ApplicationContextTest {
