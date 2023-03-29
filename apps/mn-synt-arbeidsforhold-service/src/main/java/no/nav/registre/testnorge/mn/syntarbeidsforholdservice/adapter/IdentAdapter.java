04a14609d430c809bf26d696fe19cb8a89a05674
Cato Olsen <olsen.cato@gmail.com>
2023-03-29 09:25:15 +0200

    Fjerner mn-synt-arbeidsforhold-service.

--- apps/mn-synt-arbeidsforhold-service/src/main/java/no/nav/registre/testnorge/mn/syntarbeidsforholdservice/adapter/IdentAdapter.java
+++
@@ -1,9 +0,0 @@
-package no.nav.registre.testnorge.mn.syntarbeidsforholdservice.adapter;
-
-import reactor.core.publisher.Flux;
-
-import java.util.Set;
-
-public interface IdentAdapter {
-    Flux<String> getIdenter(String miljo, int antall);
-}
