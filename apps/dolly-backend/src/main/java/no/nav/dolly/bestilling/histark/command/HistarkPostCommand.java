53015c077ad63769d42de16248261cfad28f5a17
stigus <stian.gustavsson@nav.no>
2023-03-29 15:19:17 +0200

    Tester mer endringer for histark

--- apps/dolly-backend/src/main/java/no/nav/dolly/bestilling/histark/command/HistarkPostCommand.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/bestilling/histark/command/HistarkPostCommand.java
@@ -4,12 +4,16 @@ import lombok.RequiredArgsConstructor;
 import no.nav.dolly.bestilling.histark.domain.HistarkRequest;
 import no.nav.dolly.bestilling.histark.domain.HistarkResponse;
 import no.nav.dolly.util.WebClientFilter;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.MultipartBodyBuilder;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.util.retry.Retry;
 
+import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.util.concurrent.Callable;
 
@@ -26,19 +30,25 @@ public class HistarkPostCommand implements Callable<Flux<HistarkResponse>> {
     @Override
     public Flux<HistarkResponse> call() {
 
-        return Flux.fromIterable(histarkRequest.getHistarkDokumenter()).flatMap(histarkDokument -> webClient.post()
-                .uri(builder ->
-                        builder.path("/api/saksmapper/import").build())
-                .header(AUTHORIZATION, "Bearer " + token)
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .bodyValue(histarkDokument)
-                .retrieve()
-                .bodyToMono(HistarkResponse.class)
-                .retryWhen(Retry.backoff(3, Duration.ofSeconds(5))
-                        .filter(WebClientFilter::is5xxException))
-                .doOnError(WebClientFilter::logErrorMessage)
-                .onErrorResume(error -> Mono.just(HistarkResponse.builder()
-                        .feilmelding(WebClientFilter.getMessage(error))
-                        .build())));
+        return Flux.fromIterable(histarkRequest.getHistarkDokumenter()).flatMap(histarkDokument -> {
+            var bodyBuilder = new MultipartBodyBuilder();
+            bodyBuilder.part("file", histarkDokument.getFile().getBytes(StandardCharsets.UTF_8));
+            bodyBuilder.part("metadata", histarkDokument.getMetadata().toString());
+            return webClient.post()
+                    .uri(builder ->
+                            builder.path("/api/saksmapper/import").build())
+                    .header(AUTHORIZATION, "Bearer " + token)
+                    .contentType(MediaType.MULTIPART_FORM_DATA)
+                    .header(HttpHeaders.CONTENT_TYPE, MediaType.MULTIPART_FORM_DATA_VALUE)
+                    .body(BodyInserters.fromMultipartData(bodyBuilder.build()))
+                    .retrieve()
+                    .bodyToMono(HistarkResponse.class)
+                    .retryWhen(Retry.backoff(3, Duration.ofSeconds(5))
+                            .filter(WebClientFilter::is5xxException))
+                    .doOnError(WebClientFilter::logErrorMessage)
+                    .onErrorResume(error -> Mono.just(HistarkResponse.builder()
+                            .feilmelding(WebClientFilter.getMessage(error))
+                            .build()));
+        });
     }
 }
