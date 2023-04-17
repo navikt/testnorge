deb29ceebaf9f50ccf9aa8abed9fb514d5c9fd55
kristenhaerum <kristen.herum@nav.no>
2023-04-17 12:21:47 +0200

    Service discovery inntektstub-proxy #deploy-inntektstub-proxy

--- proxies/inntektstub-proxy/src/main/java/no/nav/testnav/proxies/inntektstubproxy/InntektstubProxyApplicationStarter.java
+++ proxies/inntektstub-proxy/src/main/java/no/nav/testnav/proxies/inntektstubproxy/InntektstubProxyApplicationStarter.java
@@ -1,15 +1,14 @@
 package no.nav.testnav.proxies.inntektstubproxy;
 
+import no.nav.testnav.libs.reactivecore.config.CoreConfig;
+import no.nav.testnav.libs.reactiveproxy.config.DevConfig;
+import no.nav.testnav.libs.reactiveproxy.config.SecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
-
-import no.nav.testnav.libs.reactivecore.config.CoreConfig;
-import no.nav.testnav.libs.reactiveproxy.config.DevConfig;
-import no.nav.testnav.libs.reactiveproxy.config.SecurityConfig;
 
 @Import({
         CoreConfig.class,
@@ -25,7 +24,7 @@ public class InntektstubProxyApplicationStarter {
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route(spec -> spec.path("/**").uri("https://inntektstub.dev.adeo.no/"))
+                .route(spec -> spec.path("/**").uri("http://inntektstub.team-inntekt.svc.nais.local/"))
                 .build();
     }
 }
