274596bde1e589f810e357ae8af2d69bc2f16563
kristenhaerum <kristen.herum@nav.no>
2023-04-17 12:48:23 +0200

    Service discovery kontoregister-proxy II #deploy-kontoregister-person-proxy

--- proxies/kontoregister-person-proxy/src/main/java/no/nav/testnav/proxies/kontoregisterperson/StatusController.java
+++ proxies/kontoregister-person-proxy/src/main/java/no/nav/testnav/proxies/kontoregisterperson/StatusController.java
@@ -20,8 +20,8 @@ public class StatusController {
         var statusWebClient = WebClient.builder().build();
 
         var pensjonStatus = checkConsumerStatus(
-                "https://sokos-kontoregister-person.dev.intern.nav.no/internal/is_alive",
-                "https://sokos-kontoregister-person.dev.intern.nav.no/internal/is_ready",
+                "http://sokos-kontoregister-person.okonomi.svc.cluster.local/internal/is_alive",
+                "http://sokos-kontoregister-person.okonomi.svc.cluster.local/internal/is_ready",
                 statusWebClient);
         pensjonStatus.put("team", TEAM_OKONOMI);
 
