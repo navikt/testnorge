4dc8b3a000b099a2ae3fa68a0b13c7c2dba8b7c6
kristenhaerum <kristen.herum@nav.no>
2023-04-18 08:54:58 +0200

    Redeploy #deploy-brregstub-proxy

--- proxies/brregstub-proxy/src/main/java/no/nav/testnav/proxies/brregstubproxy/BrregstubProxyApplicationStarter.java
+++ proxies/brregstub-proxy/src/main/java/no/nav/testnav/proxies/brregstubproxy/BrregstubProxyApplicationStarter.java
@@ -27,5 +27,4 @@ public class BrregstubProxyApplicationStarter {
                 .route(spec -> spec.path("/**").uri("http://brreg-stub.dolly.svc.nais.local/"))
                 .build();
     }
-
 }
