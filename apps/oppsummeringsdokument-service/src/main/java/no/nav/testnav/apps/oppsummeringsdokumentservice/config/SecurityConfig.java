2cbe0a8b8f5016a2bd1cb91b29355a6364e151ae
stigus <stian.gustavsson@nav.no>
2023-04-24 10:25:11 +0200

    Oppsummeringsdokument upgrade og forsøk på fix #deploy-oppsummeringsdokument-service

--- apps/oppsummeringsdokument-service/src/main/java/no/nav/testnav/apps/oppsummeringsdokumentservice/config/SecurityConfig.java
+++ apps/oppsummeringsdokument-service/src/main/java/no/nav/testnav/apps/oppsummeringsdokumentservice/config/SecurityConfig.java
@@ -1,28 +1,41 @@
 package no.nav.testnav.apps.oppsummeringsdokumentservice.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
 
-@Order(1)
 @EnableWebSecurity
 @Configuration
 @Profile({ "prod", "dev" })
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.sessionManagement()
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and().csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/api/**")
-                .fullyAuthenticated()
-                .and().oauth2ResourceServer().jwt();
+                .authorizeHttpRequests()
+                .requestMatchers(
+                        "/internal/**",
+                        "/webjars/**",
+                        "/swagger-resources/**",
+                        "/v3/api-docs/**",
+                        "/swagger-ui/**",
+                        "/swagger",
+                        "/error",
+                        "/swagger-ui.html"
+                ).permitAll()
+                .requestMatchers("/api/**").fullyAuthenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+
+        return httpSecurity.build();
     }
 }
