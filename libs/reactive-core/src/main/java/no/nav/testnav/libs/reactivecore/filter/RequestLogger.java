cf21d0033cd1ecf75da1a61c60ddb3f145325d99
Cato Olsen <olsen.cato@gmail.com>
2023-04-26 10:22:25 +0200

    try-finally for å sikre at originalContextMap blir preservert.

--- libs/reactive-core/src/main/java/no/nav/testnav/libs/reactivecore/filter/RequestLogger.java
+++ libs/reactive-core/src/main/java/no/nav/testnav/libs/reactivecore/filter/RequestLogger.java
@@ -14,7 +14,6 @@ import reactor.core.publisher.Mono;
 
 import java.net.InetSocketAddress;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -43,33 +42,37 @@ public class RequestLogger implements WebFilter {
                 return;
             }
             var originalContextMap = MDC.getCopyOfContextMap();
-            var modifiedContextMap = Optional
-                    .of(MDC.getCopyOfContextMap())
-                    .orElse(new HashMap<>());
-            MDC.setContextMap(new HashMap<>());
-            var method = request.getMethod().name();
-            var uri = request.getPath().toString();
-            var queryParrams = request.getQueryParams().isEmpty() ? null : request.getQueryParams().toString();
-            var host = Optional
-                    .ofNullable(request.getHeaders().getHost())
-                    .map(InetSocketAddress::toString)
-                    .orElse("unknown");
-            var contentType = request.getHeaders().getContentType();
+            try {
 
-            modifiedContextMap.put("Transaction-Type", "request");
-            modifiedContextMap.put("Method", method);
-            if (contentType != null) {
-                modifiedContextMap.put(HttpHeaders.CONTENT_TYPE, contentType.getType());
+                var modifiedContextMap = Optional
+                        .of(MDC.getCopyOfContextMap())
+                        .orElse(new HashMap<>());
+                var method = request.getMethod().name();
+                var uri = request.getPath().toString();
+                var queryParrams = request.getQueryParams().isEmpty() ? null : request.getQueryParams().toString();
+                var host = Optional
+                        .ofNullable(request.getHeaders().getHost())
+                        .map(InetSocketAddress::toString)
+                        .orElse("unknown");
+                var contentType = request.getHeaders().getContentType();
+
+                modifiedContextMap.put("Transaction-Type", "request");
+                modifiedContextMap.put("Method", method);
+                if (contentType != null) {
+                    modifiedContextMap.put(HttpHeaders.CONTENT_TYPE, contentType.getType());
+                }
+                modifiedContextMap.put(HttpHeaders.HOST, host);
+                modifiedContextMap.put(HttpHeaders.ORIGIN, request.getHeaders().getOrigin());
+                modifiedContextMap.put(HttpHeaders.ACCEPT, request.getHeaders().getAccept().toString());
+                modifiedContextMap.put("Query-Params", queryParrams);
+                modifiedContextMap.put("URI", uri);
+
+                MDC.setContextMap(modifiedContextMap);
+                log.trace("[Request ] {} {}{}", method, host, uri);
+
+            } finally {
+                MDC.setContextMap(originalContextMap);
             }
-            modifiedContextMap.put(HttpHeaders.HOST, host);
-            modifiedContextMap.put(HttpHeaders.ORIGIN, request.getHeaders().getOrigin());
-            modifiedContextMap.put(HttpHeaders.ACCEPT, request.getHeaders().getAccept().toString());
-            modifiedContextMap.put("Query-Params", queryParrams);
-            modifiedContextMap.put("URI", uri);
-
-            MDC.setContextMap(modifiedContextMap);
-            log.trace("[Request ] {} {}{}", method, host, uri);
-            MDC.setContextMap(originalContextMap);
 
         };
     }
@@ -80,36 +83,44 @@ public class RequestLogger implements WebFilter {
             if (!log.isTraceEnabled()) {
                 return;
             }
-            Map<String, String> original = MDC.getCopyOfContextMap() != null ? MDC.getCopyOfContextMap() : new HashMap<>();
-            Map<String, String> contextMap = MDC.getCopyOfContextMap() != null ? MDC.getCopyOfContextMap() : new HashMap<>();
-            var method = request.getMethod().name();
-            var uri = request.getPath().toString();
-            var queryParrams = request.getQueryParams().isEmpty() ? null : request.getQueryParams().toString();
-            var host = Objects.requireNonNull(request.getHeaders().getHost()).toString();
+            var originalContextMap = MDC.getCopyOfContextMap();
+            try {
 
-            contextMap.put("Transaction-Type", "response");
-            contextMap.put("Method", method);
-            contextMap.put(HttpHeaders.CONTENT_TYPE, Objects.requireNonNull(response.getHeaders().getContentType()).getType());
-            contextMap.put(HttpHeaders.HOST, host);
-            contextMap.put(HttpHeaders.ORIGIN, response.getHeaders().getOrigin());
-            contextMap.put(HttpHeaders.ACCEPT, response.getHeaders().getAccept().toString());
-            contextMap.put("Query-Params", queryParrams);
-            if (!Objects.requireNonNull(response.getStatusCode()).is2xxSuccessful()) {
-                contextMap.put("Body", body == null ? "[empty]" : body);
+                var modifiedContextMap = Optional
+                        .ofNullable(MDC.getCopyOfContextMap())
+                        .orElse(new HashMap<>());
+                var method = request.getMethod().name();
+                var uri = request.getPath().toString();
+                var queryParrams = request.getQueryParams().isEmpty() ? null : request.getQueryParams().toString();
+                var host = Objects.requireNonNull(request.getHeaders().getHost()).toString();
+
+                modifiedContextMap.put("Transaction-Type", "response");
+                modifiedContextMap.put("Method", method);
+                modifiedContextMap.put(HttpHeaders.CONTENT_TYPE, Objects.requireNonNull(response.getHeaders().getContentType()).getType());
+                modifiedContextMap.put(HttpHeaders.HOST, host);
+                modifiedContextMap.put(HttpHeaders.ORIGIN, response.getHeaders().getOrigin());
+                modifiedContextMap.put(HttpHeaders.ACCEPT, response.getHeaders().getAccept().toString());
+                modifiedContextMap.put("Query-Params", queryParrams);
+                if (!Objects.requireNonNull(response.getStatusCode()).is2xxSuccessful()) {
+                    modifiedContextMap.put("Body", body == null ? "[empty]" : body);
+                }
+
+                var statusCode = response.getStatusCode();
+                modifiedContextMap.put(
+                        "Http-Status",
+                        Optional
+                                .ofNullable(statusCode)
+                                .map(Object::toString)
+                                .orElse("unknown"));
+                modifiedContextMap.put("URI", uri);
+
+                MDC.setContextMap(modifiedContextMap);
+                log.trace("[Response] {} {}{} HTTP:{}", method, host, uri, statusCode);
+
+            } finally {
+                MDC.setContextMap(originalContextMap);
             }
 
-            var statusCode = response.getStatusCode();
-            contextMap.put(
-                    "Http-Status",
-                    Optional
-                            .ofNullable(statusCode)
-                            .map(Object::toString)
-                            .orElse("unknown"));
-            contextMap.put("URI", uri);
-
-            MDC.setContextMap(contextMap);
-            log.trace("[Response] {} {}{} HTTP:{}", method, host, uri, statusCode);
-            MDC.setContextMap(original);
         };
     }
 }
