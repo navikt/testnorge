826ef122a4a34acafa0a1cd1c28ef8cfc6a57356
Cato Olsen <olsen.cato@gmail.com>
2023-05-05 16:19:23 +0200

    isAlive og isReady gir nå image-navn med link til siste commit hvis NAIS_APP_IMAGE er satt i pod.

--- libs/servlet-core/src/main/java/no/nav/testnav/libs/servletcore/provider/InternalController.java
+++ libs/servlet-core/src/main/java/no/nav/testnav/libs/servletcore/provider/InternalController.java
@@ -1,28 +1,46 @@
 package no.nav.testnav.libs.servletcore.provider;
 
 import io.swagger.v3.oas.annotations.Operation;
-import org.springframework.context.annotation.Profile;
-import org.springframework.http.HttpStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Profile({"prod","dev"})
 @RestController
 @RequestMapping("/internal")
+@RequiredArgsConstructor
 public class InternalController {
+
+    private final Environment env;
+    private String html;
 
     @GetMapping("/isAlive")
     @Operation(hidden = true)
-    public ResponseEntity<HttpStatus> isAlive() {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<String> isAlive() {
+        return ResponseEntity.ok(body());
     }
 
     @GetMapping("/isReady")
     @Operation(hidden = true)
-    public ResponseEntity<HttpStatus> isReady() {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<String> isReady() {
+        return ResponseEntity.ok(body());
+    }
+
+    private synchronized String body() {
+        if (html == null) {
+            html = "OK";
+            var naisAppImage = env.getProperty("NAIS_APP_IMAGE");
+            if (naisAppImage != null) {
+                var i = naisAppImage.lastIndexOf("-");
+                if (i > 0) {
+                    var hash = naisAppImage.substring(i + 1);
+                    html = "OK - image is <a href=https://github.com/navikt/testnorge/commit/%s>%s</a>".formatted(hash, naisAppImage);
+                }
+            }
+        }
+        return html;
     }
 
 }
