557fd6caf7b9060172f72be28e5129b1f77594a4
Cato Olsen <cato.olsen@nav.no>
2023-05-08 08:58:07 +0200

    feature/image_isalive_isready (#3153)
    isAlive og isReady gir nå image-navn med link til siste commit hvis NAIS_APP_IMAGE er satt i pod.

--- libs/reactive-core/src/main/java/no/nav/testnav/libs/reactivecore/router/InternalHandler.java
+++ libs/reactive-core/src/main/java/no/nav/testnav/libs/reactivecore/router/InternalHandler.java
@@ -1,5 +1,7 @@
 package no.nav.testnav.libs.reactivecore.router;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.ServerRequest;
@@ -7,11 +9,33 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
 @Component
+@SuppressWarnings("java:S1172")
+@RequiredArgsConstructor
 public class InternalHandler {
-    public Mono<ServerResponse> isAlive(ServerRequest request) {
-        return ServerResponse.ok().body(BodyInserters.fromValue("OK"));
+
+    private final Environment env;
+    private String html;
+
+    public Mono<ServerResponse> isAlive(ServerRequest ignored) {
+        return ServerResponse.ok().body(BodyInserters.fromValue(body()));
     }
-    public Mono<ServerResponse> isReady(ServerRequest request) {
-        return ServerResponse.ok().body(BodyInserters.fromValue("OK"));
+    public Mono<ServerResponse> isReady(ServerRequest ignored) {
+        return ServerResponse.ok().body(BodyInserters.fromValue(body()));
     }
+
+    private synchronized String body() {
+        if (html == null) {
+            html = "OK";
+            var naisAppImage = env.getProperty("NAIS_APP_IMAGE");
+            if (naisAppImage != null) {
+                var i = naisAppImage.lastIndexOf("-");
+                if (i > 0) {
+                    var hash = naisAppImage.substring(i + 1);
+                    html = "OK - image is <a href=https://github.com/navikt/testnorge/commit/%s>%s</a>".formatted(hash, naisAppImage);
+                }
+            }
+        }
+        return html;
+    }
+
 }
