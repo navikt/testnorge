c2e17f2fd62f8c75dd86cfdc57a822e6906f9de9
Kristen Hærum <kristen.herum@nav.no>
2023-05-11 11:26:22 +0200

    Cleanup/internal controller (#3158)
    Oppdatert InternalController

--- libs/servlet-core/src/main/java/no/nav/testnav/libs/servletcore/provider/InternalController.java
+++ libs/servlet-core/src/main/java/no/nav/testnav/libs/servletcore/provider/InternalController.java
@@ -2,13 +2,18 @@ package no.nav.testnav.libs.servletcore.provider;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.v3.oas.annotations.Operation;
+import lombok.Builder;
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import static java.util.Objects.nonNull;
 
 @RestController
 @RequestMapping(
@@ -18,41 +23,39 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class InternalController {
 
-    private final Environment env;
-    private JsonResponse json;
+    @Value("${NAIS_APP_IMAGE:null}")
+    private final String image;
 
     @GetMapping("/isAlive")
     @Operation(hidden = true)
-    public ResponseEntity<JsonResponse> isAlive() {
-        return ResponseEntity.ok(body());
+    public ResponseEntity<HttpStatus> isAlive() {
+        return ResponseEntity.ok().build();
     }
 
     @GetMapping("/isReady")
     @Operation(hidden = true)
-    public ResponseEntity<JsonResponse> isReady() {
-        return ResponseEntity.ok(body());
+    public ResponseEntity<HttpStatus> isReady() {
+        return ResponseEntity.ok().build();
     }
 
-    private synchronized JsonResponse body() {
-        if (json == null) {
-            json = new JsonResponse("OK", null, null);
-            var naisImage = env.getProperty("NAIS_APP_IMAGE");
-            if (naisImage != null) {
-                var i = naisImage.lastIndexOf("-");
-                if (i > 0) {
-                    json = new JsonResponse("OK", naisImage, "https://github.com/navikt/testnorge/commit/" + naisImage.substring(i + 1));
-                }
-            }
-        }
-        return json;
+    @GetMapping("/image")
+    @Operation(hidden = true)
+    @ResponseStatus(HttpStatus.OK)
+    public JsonResponse getVersion() {
+
+        return JsonResponse.builder()
+                .image(image)
+                .commit(nonNull(image) && image.lastIndexOf("-") > 0 ?
+                        "https://github.com/navikt/testnorge/commit/" +
+                                image.substring(image.lastIndexOf("-") + 1) : null)
+                .build();
     }
 
+    @Builder
     @JsonInclude(JsonInclude.Include.NON_NULL)
     record JsonResponse(
-            String status,
             String image,
             String commit
     ) {
     }
-
 }
