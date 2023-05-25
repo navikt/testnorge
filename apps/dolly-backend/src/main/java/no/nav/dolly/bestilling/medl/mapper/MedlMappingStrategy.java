abe813bd58c79020cd7e61f5c9b32be6b70c3c6b
stigus <stian.gustavsson@nav.no>
2023-05-25 11:13:22 +0200

    Medl mapping fix #deploy-test-dolly-backend

--- apps/dolly-backend/src/main/java/no/nav/dolly/bestilling/medl/mapper/MedlMappingStrategy.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/bestilling/medl/mapper/MedlMappingStrategy.java
@@ -8,6 +8,8 @@ import no.nav.dolly.domain.resultset.medl.RsMedl;
 import no.nav.dolly.mapper.MappingStrategy;
 import org.springframework.stereotype.Component;
 
+import static java.util.Objects.isNull;
+
 @Component
 public class MedlMappingStrategy implements MappingStrategy {
     @Override
@@ -18,12 +20,14 @@ public class MedlMappingStrategy implements MappingStrategy {
                     public void mapAtoB(RsMedl rsMedl, MedlData medlDataRequest, MappingContext context) {
 
                         medlDataRequest.setIdent((String) context.getProperty("ident"));
-                        medlDataRequest.setStudieinformasjon(MedlData.Studieinformasjon.builder()
-                                .delstudie(rsMedl.getStudieinformasjon().getDelstudie())
-                                .soeknadInnvilget(rsMedl.getStudieinformasjon().getSoeknadInnvilget())
-                                .studieland(rsMedl.getStudieinformasjon().getStudieland())
-                                .statsborgerland(rsMedl.getStudieinformasjon().getStudieland())
-                                .build());
+                        if (!isNull(rsMedl.getStudieinformasjon())) {
+                            medlDataRequest.setStudieinformasjon(MedlData.Studieinformasjon.builder()
+                                    .delstudie(rsMedl.getStudieinformasjon().getDelstudie())
+                                    .soeknadInnvilget(rsMedl.getStudieinformasjon().getSoeknadInnvilget())
+                                    .studieland(rsMedl.getStudieinformasjon().getStudieland())
+                                    .statsborgerland(rsMedl.getStudieinformasjon().getStudieland())
+                                    .build());
+                        }
                     }
                 })
                 .exclude("studieinformasjon")
