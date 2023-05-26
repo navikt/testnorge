9c65ab73156a8aba96873137208fe3c9c9484ec0
kristenhaerum <kristen.herum@nav.no>
2023-05-26 07:19:38 +0200

    Revert "Revert "Oppdatert feilmelding for Arena-forvalter""
    This reverts commit c405c29efaabc89aa013e85af7e0ec54d76cc272.

--- apps/dolly-backend/src/main/java/no/nav/dolly/errorhandling/ErrorStatusDecoder.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/errorhandling/ErrorStatusDecoder.java
@@ -119,7 +119,8 @@ public class ErrorStatusDecoder {
         if (json.contains("{")) {
             try {
                 Map<String, Object> status = objectMapper.readValue(json, Map.class);
-                if (status.containsKey(ERROR) && isNotBlank((String) status.get(ERROR))) {
+                if (status.containsKey(ERROR) && isNotBlank((String) status.get(ERROR)) &&
+                        !((String) status.get(ERROR)).toLowerCase().contains("bad request")) {
                     builder.append("error=").append(status.get(ERROR)).append("; ");
                 } else if (status.containsKey(MESSAGE) && isNotBlank((String) status.get(MESSAGE))) {
                     builder.append("message=").append(encodeStatus((String) status.get(MESSAGE))).append("; ");
