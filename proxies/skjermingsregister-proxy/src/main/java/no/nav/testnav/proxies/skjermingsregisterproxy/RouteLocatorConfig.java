05355e561c7731eaaba991cdc95ba655db2549e2
Cato Olsen <olsen.cato@gmail.com>
2023-06-01 16:40:58 +0200

    Forenklet auth config.

--- proxies/skjermingsregister-proxy/src/main/java/no/nav/testnav/proxies/skjermingsregisterproxy/RouteLocatorConfig.java
+++ proxies/skjermingsregister-proxy/src/main/java/no/nav/testnav/proxies/skjermingsregisterproxy/RouteLocatorConfig.java
@@ -1,11 +1,22 @@
 package no.nav.testnav.proxies.skjermingsregisterproxy;
 
+import no.nav.testnav.libs.reactiveproxy.config.SecurityConfig;
+import no.nav.testnav.libs.reactiveproxy.filter.AddAuthenticationRequestGatewayFilterFactory;
+import no.nav.testnav.libs.reactivesecurity.config.SecureOAuth2ServerToServerConfiguration;
+import no.nav.testnav.libs.reactivesecurity.exchange.azuread.TrygdeetatenAzureAdTokenService;
+import no.nav.testnav.libs.securitycore.domain.AccessToken;
+import no.nav.testnav.libs.securitycore.domain.ServerProperties;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
+@Import({
+        SecureOAuth2ServerToServerConfiguration.class,
+        SecurityConfig.class
+})
 @Configuration
 public class RouteLocatorConfig {
 
@@ -22,6 +33,14 @@ public class RouteLocatorConfig {
                         .filters(f -> f.filter(authenticationFilter))
                         .uri(serverProperties.getUrl()))
                 .build();
+    }
+
+    @Bean
+    GatewayFilter getAuthenticationFilter(TrygdeetatenAzureAdTokenService tokenService, ServerProperties serverProperties) {
+        return AddAuthenticationRequestGatewayFilterFactory
+                .bearerAuthenticationHeaderFilter(() -> tokenService
+                        .exchange(serverProperties)
+                        .map(AccessToken::getTokenValue));
     }
 
 }
