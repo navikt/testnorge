898ebab45b1d039b92d59a9fe1b872eb67d80bb4
Cato Olsen <olsen.cato@gmail.com>
2023-06-05 15:08:16 +0200

    Refaktorert test for å unngå InjectMocks og leser nå filer med UTF-8, ikke default charset.

--- apps/generer-navn-service/src/main/java/no/nav/registre/testnorge/generernavnservice/service/FileReader.java
+++ apps/generer-navn-service/src/main/java/no/nav/registre/testnorge/generernavnservice/service/FileReader.java
@@ -1,17 +1,20 @@
 package no.nav.registre.testnorge.generernavnservice.service;
 
 import lombok.NoArgsConstructor;
+import lombok.SneakyThrows;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
-@NoArgsConstructor
+@NoArgsConstructor(access = lombok.AccessLevel.PRIVATE)
 class FileReader {
+
     private static InputStreamReader readFromResources(String path) {
-        return new InputStreamReader(FileReader.class.getClassLoader().getResourceAsStream(path));
+        return new InputStreamReader(FileReader.class.getClassLoader().getResourceAsStream(path), StandardCharsets.UTF_8);
     }
 
     static List<String> readLinesFromResources(String path) {
