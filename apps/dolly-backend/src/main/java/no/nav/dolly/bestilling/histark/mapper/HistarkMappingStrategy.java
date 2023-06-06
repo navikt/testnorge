289907c4aea2da7ea323d9c116c86d84e83b4fa8
stigus <stian.gustavsson@nav.no>
2023-06-06 14:53:56 +0200

    Endret histark sjekksum kalkulering i samarbeid med Lars Jensen #deploy-test-dolly-backend

--- apps/dolly-backend/src/main/java/no/nav/dolly/bestilling/histark/mapper/HistarkMappingStrategy.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/bestilling/histark/mapper/HistarkMappingStrategy.java
@@ -7,9 +7,13 @@ import ma.glasnost.orika.MappingContext;
 import no.nav.dolly.bestilling.histark.domain.HistarkRequest;
 import no.nav.dolly.domain.resultset.histark.RsHistark;
 import no.nav.dolly.mapper.MappingStrategy;
+import org.apache.commons.codec.binary.Base64InputStream;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.stereotype.Component;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.time.format.DateTimeFormatter;
 
 import static io.micrometer.common.util.StringUtils.isBlank;
@@ -30,31 +34,45 @@ public class HistarkMappingStrategy implements MappingStrategy {
                     @Override
                     public void mapAtoB(RsHistark histark, HistarkRequest histarkRequest, MappingContext context) {
 
+
                         histark.getDokumenter().forEach(dokument -> {
+
 
                             String fysiskDokument = isBlank(dokument.getFysiskDokument()) ? PDF_VEDLEGG : dokument.getFysiskDokument();
 
-                            histarkRequest.getHistarkDokumenter().add(HistarkRequest.HistarkDokument.builder()
-                                    .file(fysiskDokument)
-                                    .metadata(HistarkRequest.HistarkDokument.HistarkMetadata.builder()
-                                            .antallSider(String.valueOf(dokument.getAntallSider()))
-                                            .brukerident(((String) context.getProperty(PERSON_IDENT)))
-                                            .enhetsnavn(dokument.getEnhetsnavn())
-                                            .enhetsnummer(dokument.getEnhetsnummer())
-                                            .filnavn(dokument.getTittel())
-                                            .skanner(dokument.getSkanner())
-                                            .skannested(dokument.getSkannested())
-                                            .klage("")
-                                            .sjekksum(DigestUtils.sha256Hex(fysiskDokument))
-                                            .skanningstidspunkt(dokument.getSkanningsTidspunkt().format(dateTimeFormatter))
-                                            .startAar(String.valueOf(dokument.getStartAar().getYear()))
-                                            .sluttAar(String.valueOf(dokument.getSluttAar().getYear()))
-                                            .temakoder(String.join(",", dokument.getTemakoder()))
-                                            .build())
-                                    .build());
+                            try {
+                                histarkRequest.getHistarkDokumenter().add(HistarkRequest.HistarkDokument.builder()
+                                        .file(fysiskDokument)
+                                        .metadata(HistarkRequest.HistarkDokument.HistarkMetadata.builder()
+                                                .antallSider(String.valueOf(dokument.getAntallSider()))
+                                                .brukerident(((String) context.getProperty(PERSON_IDENT)))
+                                                .enhetsnavn(dokument.getEnhetsnavn())
+                                                .enhetsnummer(dokument.getEnhetsnummer())
+                                                .filnavn(dokument.getTittel())
+                                                .skanner(dokument.getSkanner())
+                                                .skannested(dokument.getSkannested())
+                                                .klage("")
+                                                .sjekksum(calculateBinaryChecksum(fysiskDokument))
+                                                .skanningstidspunkt(dokument.getSkanningsTidspunkt().format(dateTimeFormatter))
+                                                .startAar(String.valueOf(dokument.getStartAar().getYear()))
+                                                .sluttAar(String.valueOf(dokument.getSluttAar().getYear()))
+                                                .temakoder(String.join(",", dokument.getTemakoder()))
+                                                .build())
+                                        .build());
+                            } catch (IOException e) {
+                                log.error("Klarte ikke å kalkulere sjekksum fra innsendt histark dokument");
+                                throw new RuntimeException(e);
+                            }
                         });
                     }
                 })
                 .register();
+    }
+
+    private static String calculateBinaryChecksum(String fysiskDokument) throws IOException {
+        InputStream byteArrayInput = new ByteArrayInputStream(fysiskDokument.getBytes());
+        InputStream base64Input = new Base64InputStream(byteArrayInput);
+
+        return DigestUtils.sha256Hex(base64Input);
     }
 }
