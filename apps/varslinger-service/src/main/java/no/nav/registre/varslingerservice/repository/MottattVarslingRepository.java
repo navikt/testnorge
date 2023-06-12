18b923d36c28e6fbc489c1159843860265f761b7
Cato Olsen <olsen.cato@gmail.com>
2023-06-12 16:57:48 +0200

    Fjerner ubrukt metode/import.

--- apps/varslinger-service/src/main/java/no/nav/registre/varslingerservice/repository/MottattVarslingRepository.java
+++ apps/varslinger-service/src/main/java/no/nav/registre/varslingerservice/repository/MottattVarslingRepository.java
@@ -1,14 +1,12 @@
 package no.nav.registre.varslingerservice.repository;
 
+import no.nav.registre.varslingerservice.repository.model.MottattVarslingModel;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
-
-import no.nav.registre.varslingerservice.repository.model.MottattVarslingModel;
 
 public interface MottattVarslingRepository extends CrudRepository<MottattVarslingModel, Long> {
 
     List<MottattVarslingModel> findAllByBrukerObjectId(String objectId);
 
-    void deleteAllByBrukerObjectId(String objectId);
 }
