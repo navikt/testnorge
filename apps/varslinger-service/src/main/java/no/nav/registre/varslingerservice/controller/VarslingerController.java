6090270891cedbcb47d8fe7c47d08dde63e18dea
Cato Olsen <olsen.cato@gmail.com>
2023-06-13 12:53:55 +0200

    Forenkler eksplisitte returverdier for metoder, med unntak av en header.

--- apps/varslinger-service/src/main/java/no/nav/registre/varslingerservice/controller/VarslingerController.java
+++ apps/varslinger-service/src/main/java/no/nav/registre/varslingerservice/controller/VarslingerController.java
@@ -4,8 +4,10 @@ import lombok.RequiredArgsConstructor;
 import no.nav.registre.varslingerservice.adapter.VarslingerAdapter;
 import no.nav.registre.varslingerservice.domain.Varsling;
 import no.nav.testnav.libs.dto.varslingerapi.v1.VarslingDTO;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.util.List;
@@ -19,20 +21,18 @@ public class VarslingerController {
     private final VarslingerAdapter varslingerAdapter;
 
     @GetMapping
-    public ResponseEntity<List<VarslingDTO>> getVarslinger() {
-        var list = varslingerAdapter
+    public List<VarslingDTO> getVarslinger() {
+        return varslingerAdapter
                 .getAll()
                 .stream()
                 .map(Varsling::toDTO)
                 .toList();
-        return ResponseEntity.ok(list);
     }
 
     @PutMapping
     public ResponseEntity<Object> oppdaterVarslinger(@RequestBody VarslingDTO dto) {
-        String varslingId = varslingerAdapter.save(new Varsling(dto));
-        var uri = ServletUriComponentsBuilder
-                .fromCurrentRequest()
+        var varslingId = varslingerAdapter.save(new Varsling(dto));
+        var uri = ServletUriComponentsBuilder.fromCurrentRequest()
                 .path("/{varslingId}")
                 .buildAndExpand(varslingId)
                 .toUri();
@@ -40,17 +40,16 @@ public class VarslingerController {
     }
 
     @DeleteMapping("/{varslingId}")
-    public ResponseEntity<Object> deleteVarslinger(@PathVariable("varslingId") String varslingId) {
+    public void deleteVarslinger(@PathVariable("varslingId") String varslingId) {
+        // TODO: Returnerer alltid 200, selv om varslingId ikke finnes. Burde det returneres 404?
         varslingerAdapter.delete(varslingId);
-        return ResponseEntity.ok().build();
     }
 
     @GetMapping("/{varslingId}")
-    public ResponseEntity<VarslingDTO> getVarslinger(@PathVariable("varslingId") String varslingId) {
-        return Optional
-                .ofNullable(varslingerAdapter.get(varslingId))
-                .map(varsling -> ResponseEntity.ok(varsling.toDTO()))
-                .orElse(ResponseEntity.notFound().build());
+    public VarslingDTO getVarslinger(@PathVariable("varslingId") String varslingId) {
+        return Optional.ofNullable(varslingerAdapter.get(varslingId))
+                .map(Varsling::toDTO)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
 }
