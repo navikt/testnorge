5aaa14859e75843c1993cd4c1838c71e10a4cc5a
Cato Olsen <cato.olsen@nav.no>
2023-06-13 14:34:22 +0200

    cleanup/warning_generics (#3199)
    Fjerner noen warnings på generics, lagt til tester, annen opprydding.

--- apps/varslinger-service/src/main/java/no/nav/registre/varslingerservice/controller/VarslingerController.java
+++ apps/varslinger-service/src/main/java/no/nav/registre/varslingerservice/controller/VarslingerController.java
@@ -1,22 +1,17 @@
 package no.nav.registre.varslingerservice.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
 import no.nav.registre.varslingerservice.adapter.VarslingerAdapter;
 import no.nav.registre.varslingerservice.domain.Varsling;
 import no.nav.testnav.libs.dto.varslingerapi.v1.VarslingDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/varslinger")
@@ -26,16 +21,18 @@ public class VarslingerController {
     private final VarslingerAdapter varslingerAdapter;
 
     @GetMapping
-    public ResponseEntity<List<VarslingDTO>> getVarslinger() {
-        var list = varslingerAdapter.getAll().stream().map(Varsling::toDTO).collect(Collectors.toList());
-        return ResponseEntity.ok(list);
+    public List<VarslingDTO> getVarslinger() {
+        return varslingerAdapter
+                .getAll()
+                .stream()
+                .map(Varsling::toDTO)
+                .toList();
     }
 
     @PutMapping
-    public ResponseEntity<?> oppdaterVarslinger(@RequestBody VarslingDTO dto) {
-        String varslingId = varslingerAdapter.save(new Varsling(dto));
-        var uri = ServletUriComponentsBuilder
-                .fromCurrentRequest()
+    public ResponseEntity<Object> oppdaterVarslinger(@RequestBody VarslingDTO dto) {
+        var varslingId = varslingerAdapter.save(new Varsling(dto));
+        var uri = ServletUriComponentsBuilder.fromCurrentRequest()
                 .path("/{varslingId}")
                 .buildAndExpand(varslingId)
                 .toUri();
@@ -43,17 +40,16 @@ public class VarslingerController {
     }
 
     @DeleteMapping("/{varslingId}")
-    public ResponseEntity<?> deleteVarslinger(@PathVariable("varslingId") String varslingId) {
+    public void deleteVarslinger(@PathVariable("varslingId") String varslingId) {
+        // TODO: Returnerer alltid 200, selv om varslingId ikke finnes. Burde det returneres 404?
         varslingerAdapter.delete(varslingId);
-        return ResponseEntity.ok().build();
     }
 
     @GetMapping("/{varslingId}")
-    public ResponseEntity<?> getVarslinger(@PathVariable("varslingId") String varslingId) {
-        var varsling = varslingerAdapter.get(varslingId);
-        if (varsling == null) {
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok(varsling.toDTO());
+    public VarslingDTO getVarslinger(@PathVariable("varslingId") String varslingId) {
+        return Optional.ofNullable(varslingerAdapter.get(varslingId))
+                .map(Varsling::toDTO)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
+
 }
