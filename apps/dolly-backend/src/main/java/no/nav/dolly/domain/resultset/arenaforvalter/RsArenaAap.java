2e1695e73a7a8b3ddf74d08b53e071e52bbd14b2
kristenhaerum <kristen.herum@nav.no>
2023-06-15 09:18:54 +0200

    Viser tomme felter for Arenadata

--- apps/dolly-backend/src/main/java/no/nav/dolly/domain/resultset/arenaforvalter/RsArenaAap.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/domain/resultset/arenaforvalter/RsArenaAap.java
@@ -1,6 +1,5 @@
 package no.nav.dolly.domain.resultset.arenaforvalter;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,7 +12,6 @@ import java.time.LocalDateTime;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public class RsArenaAap {
 
     @Schema(
