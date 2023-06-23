48f72b0af8020570462ac2769adefb316eeee164
kristenhaerum <kristen.herum@nav.no>
2023-06-23 09:25:35 +0200

    Oppdatert config for proxy #deploy-arena-proxy

--- proxies/arena-forvalteren-proxy/src/main/java/no/nav/testnav/proxies/arenaforvalterenproxy/ArenaForvalterenProxyApplicationStarter.java
+++ proxies/arena-forvalteren-proxy/src/main/java/no/nav/testnav/proxies/arenaforvalterenproxy/ArenaForvalterenProxyApplicationStarter.java
@@ -5,12 +5,17 @@ import no.nav.testnav.libs.reactiveproxy.config.DevConfig;
 import no.nav.testnav.libs.reactiveproxy.config.SecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.route.Route;
 import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.Buildable;
+import org.springframework.cloud.gateway.route.builder.PredicateSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 import java.util.Arrays;
+import java.util.function.Function;
 
 @Import({
         CoreConfig.class,
@@ -30,12 +35,20 @@ public class ArenaForvalterenProxyApplicationStarter {
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder, ArenaServerProperties serverProperties) {
 
         var routes = builder.routes()
-                .route(spec -> spec.path("/**").uri("http://arena-forvalteren.teamarenanais.svc.nais.local/"));
+                .route(spec -> spec.path("/api/**").uri("http://arena-forvalteren.teamarenanais.svc.nais.local/"));
 
         Arrays.stream(ARENA_MILJOER)
                 .forEach(env ->
-                        routes.route(spec -> spec.path("/**").uri(serverProperties.forEnvironment(env).getUrl())));
+                        routes.route(createRoute(env, serverProperties.forEnvironment(env).getUrl())));
 
         return routes.build();
+    }
+
+    private Function<PredicateSpec, Buildable<Route>> createRoute(String segment, String host) {
+        return spec -> spec
+                .path("/" + segment + "/**")
+                .filters(filterSpec -> filterSpec
+                        .rewritePath("/" + segment + "/(?<segment>.*)", "/${segment}")
+                ).uri(host);
     }
 }
--- proxies/arena-forvalteren-proxy/src/main/java/no/nav/testnav/proxies/arenaforvalterenproxy/ArenaServerProperties.java
+++ proxies/arena-forvalteren-proxy/src/main/java/no/nav/testnav/proxies/arenaforvalterenproxy/ArenaServerProperties.java
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix = "app.arena")
+@ConfigurationProperties(prefix = "app.arena.services")
 public class ArenaServerProperties extends ServerProperties {
 
     private static ArenaServerProperties copyOf(ArenaServerProperties original) {
