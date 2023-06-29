cac47cb7f3b37eb313c7fd54c43b21fc3a495dab
stigus <stian.gustavsson@nav.no>
2023-06-29 08:58:51 +0200

    Lagt til grafana telemetrics for dolly-frontend og noe mindre oppdatering #deploy-test-frontend

--- apps/dolly-frontend/src/main/js/src/RootComponent.tsx
+++ apps/dolly-frontend/src/main/js/src/RootComponent.tsx
@@ -7,6 +7,15 @@ import { history, store } from '@/Store'
 import { HistoryRouter as Router } from 'redux-first-history/rr6'
 import { SWRConfig } from 'swr'
 import { App } from '@/app/App'
+import nais from './nais.js'
+
+import { initializeFaro } from '@grafana/faro-web-sdk'
+
+!nais.telemetryCollectorURL?.includes('localhost') &&
+	initializeFaro({
+		url: nais.telemetryCollectorURL,
+		app: nais.app,
+	})
 
 export const RootComponent = () => (
 	<Provider store={store}>
---
+++ apps/dolly-frontend/src/main/js/src/nais.js
@@ -0,0 +1,7 @@
+export default {
+	telemetryCollectorURL: 'http://localhost:1234/collect',
+	app: {
+		name: 'myapplication',
+		version: 'dev',
+	},
+}
