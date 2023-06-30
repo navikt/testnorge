cafea3a407d5ab9000656ea1c0a553aa8d49201b
Cato Olsen <olsen.cato@gmail.com>
2023-06-30 12:37:22 +0200

    Enkel dokumentasjon for PersonController, med mindre cleanup.

--- apps/testnorge-statisk-data-forvalter/src/main/java/no/nav/registre/sdforvalter/provider/rs/PersonController.java
+++ apps/testnorge-statisk-data-forvalter/src/main/java/no/nav/registre/sdforvalter/provider/rs/PersonController.java
@@ -1,6 +1,7 @@
 package no.nav.registre.sdforvalter.provider.rs;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.registre.sdforvalter.domain.person.Person;
@@ -8,12 +9,9 @@ import no.nav.registre.sdforvalter.domain.person.PersonStatusMap;
 import no.nav.registre.sdforvalter.service.PersonService;
 import no.nav.testnav.libs.dto.organisasjonfastedataservice.v1.Gruppe;
 import no.nav.testnav.libs.dto.person.v1.PersonDTO;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -21,32 +19,42 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/person")
+@Tag(
+        name = "PersonController",
+        description = "Henter personer fra database."
+)
 public class PersonController {
 
     private final PersonService personService;
 
     @GetMapping
-    public ResponseEntity<List<Person>> getPersonByGruppe(
+    @Operation(
+            description = "Hent liste over personer fra tabell TPS_IDENTER basert på angitt gruppe."
+    )
+    public List<Person> getPersonByGruppe(
             @RequestParam(value = "gruppe") Gruppe gruppe
     ) {
-        return ResponseEntity.ok(personService.getPerson(gruppe.name()));
+        return personService.getPerson(gruppe.name());
     }
 
     @GetMapping("/pdl/status")
-    public ResponseEntity<PersonStatusMap> statusByGruppe(
+    @Operation(
+            description = "Hent liste over status per fødselsnummer basert på angitt gruppe. Personer hentes fra tabell TPS_IDENTER, status på disse fra tjeneste testnav-person-service."
+    )
+    public PersonStatusMap statusByGruppe(
             @RequestParam(value = "equal", required = false) Boolean equal,
             @RequestParam(value = "gruppe") Gruppe gruppe
     ) {
-        return ResponseEntity.ok(personService.getStatusMap(gruppe.name(), equal));
+        return personService.getStatusMap(gruppe.name(), equal);
     }
 
     @Operation(summary = "Hent person fra Team Dollys database (faste data)")
     @GetMapping("/{ident}")
-    public ResponseEntity<PersonDTO> getPersonByIdent(@PathVariable String ident) {
+    public PersonDTO getPersonByIdent(@PathVariable String ident) {
         var person = personService.getPersonByIdent(ident);
         if (person == null) {
-            return ResponseEntity.notFound().build();
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Fant ikke person med ident " + ident);
         }
-        return ResponseEntity.ok(person.toDTO());
+        return person.toDTO();
     }
 }
