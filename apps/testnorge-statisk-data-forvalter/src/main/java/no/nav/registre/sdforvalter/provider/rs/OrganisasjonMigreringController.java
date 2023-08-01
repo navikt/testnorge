400d1c21b0f1c390b87d2bcfb27aacf2369f3e38
Cato Olsen <cato.olsen@nav.no>
2023-08-01 11:34:10 +0200

    change/api-docs (#3230)
    Endrer på dokumentasjon for OpenAPI.

--- apps/testnorge-statisk-data-forvalter/src/main/java/no/nav/registre/sdforvalter/provider/rs/OrganisasjonMigreringController.java
+++ apps/testnorge-statisk-data-forvalter/src/main/java/no/nav/registre/sdforvalter/provider/rs/OrganisasjonMigreringController.java
@@ -1,28 +1,34 @@
 package no.nav.registre.sdforvalter.provider.rs;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import no.nav.registre.sdforvalter.adapter.EregAdapter;
+import no.nav.registre.sdforvalter.consumer.rs.organisasjon.OrganisasjonFasteDataConsumer;
 import no.nav.testnav.libs.dto.organisasjonfastedataservice.v1.Gruppe;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import no.nav.registre.sdforvalter.adapter.EregAdapter;
-import no.nav.registre.sdforvalter.consumer.rs.organisasjon.OrganisasjonFasteDataConsumer;
-
 @RestController
 @RequestMapping("/api/v1/organisasjon/migrering")
 @RequiredArgsConstructor
+@Tag(
+        name = "OrganisasjonMigreringController",
+        description = "Operasjoner knyttet til databasetabell EREG."
+)
 public class OrganisasjonMigreringController {
+
     private final EregAdapter eregAdapter;
     private final OrganisasjonFasteDataConsumer fasteDataConsumer;
 
     @PostMapping
-    public ResponseEntity<HttpStatus> opprett(@RequestParam(required = false) Gruppe gruppe) {
+    @Operation(
+            description = "Migrerer organisasjoner som er definert i databasetabell EREG til tjenesten testnav-organisasjon-faste-data-service."
+    )
+    public void opprett(@RequestParam(required = false) Gruppe gruppe) {
         var eregListe = gruppe == null ? eregAdapter.fetchAll() : eregAdapter.fetchBy(gruppe.name());
         fasteDataConsumer.opprett(eregListe);
-        return ResponseEntity.noContent().build();
     }
 }
--- apps/testnorge-statisk-data-forvalter/src/main/java/no/nav/registre/sdforvalter/provider/rs/PersonMigreringController.java
+++ apps/testnorge-statisk-data-forvalter/src/main/java/no/nav/registre/sdforvalter/provider/rs/PersonMigreringController.java
@@ -1,28 +1,33 @@
 package no.nav.registre.sdforvalter.provider.rs;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import no.nav.registre.sdforvalter.adapter.TpsIdenterAdapter;
+import no.nav.registre.sdforvalter.consumer.rs.person.PersonFasteDataConsumer;
 import no.nav.testnav.libs.dto.organisasjonfastedataservice.v1.Gruppe;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import no.nav.registre.sdforvalter.adapter.TpsIdenterAdapter;
-import no.nav.registre.sdforvalter.consumer.rs.person.PersonFasteDataConsumer;
-
 @RestController
 @RequestMapping("/api/v1/person/migrering")
 @RequiredArgsConstructor
+@Tag(
+        name = "PersonMigreringController",
+        description = "Oppretter personer i testnav-person-faste-data-service."
+)
 public class PersonMigreringController {
     private final TpsIdenterAdapter tpsIdenterAdapter;
     private final PersonFasteDataConsumer fasteDataConsumer;
 
     @PostMapping
-    public ResponseEntity<HttpStatus> opprett(@RequestParam(required = false) Gruppe gruppe) {
+    @Operation(
+            description = "Oppretter personer i tjenesten testnav-person-faste-data-service basert på identer fra tabell TPS_IDENTER for angitt gruppe."
+    )
+    public void opprett(@RequestParam(required = false) Gruppe gruppe) {
         var eregListe = gruppe == null ? tpsIdenterAdapter.fetchAll() : tpsIdenterAdapter.fetchBy(gruppe.name());
         fasteDataConsumer.opprett(eregListe);
-        return ResponseEntity.noContent().build();
     }
 }
