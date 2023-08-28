41f75d34cc783972a0f860625bba58991303ed78
Kristen Hærum <kristen.herum@nav.no>
2023-08-28 09:55:13 +0200

    Oppdatere annulering av samboerregister #deploy-test-dolly-backend (#3257)
    Oppdatert annullering av samboerregister, samt setting av datoTom

--- apps/dolly-backend/src/main/java/no/nav/dolly/bestilling/pensjonforvalter/mapper/PensjonSamboerMappingStrategy.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/bestilling/pensjonforvalter/mapper/PensjonSamboerMappingStrategy.java
@@ -4,13 +4,16 @@ import ma.glasnost.orika.CustomMapper;
 import ma.glasnost.orika.MapperFactory;
 import ma.glasnost.orika.MappingContext;
 import no.nav.dolly.bestilling.pensjonforvalter.domain.PensjonSamboerRequest;
+import no.nav.dolly.bestilling.pensjonforvalter.domain.PensjonSivilstandWrapper;
 import no.nav.dolly.mapper.MappingStrategy;
 import no.nav.testnav.libs.dto.pdlforvalter.v1.SivilstandDTO;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 
 import static java.util.Objects.nonNull;
 import static no.nav.dolly.domain.CommonKeysAndUtils.CONSUMER;
@@ -20,26 +23,45 @@ public class PensjonSamboerMappingStrategy implements MappingStrategy {
 
     @Override
     public void register(MapperFactory factory) {
-        factory.classMap(SivilstandDTO.class, List.class)
+        factory.classMap(PensjonSivilstandWrapper.class, List.class)
                 .customize(new CustomMapper<>() {
                     @Override
-                    public void mapAtoB(SivilstandDTO sivilstandDTO, List list, MappingContext context) {
+                    public void mapAtoB(PensjonSivilstandWrapper sivilstander, List list, MappingContext context) {
 
                         var ident = (String) context.getProperty("ident");
-                        var hovedperson = PensjonSamboerRequest.builder()
-                                .pidBruker(ident)
-                                .pidSamboer(sivilstandDTO.getRelatertVedSivilstand())
-                                .datoFom(toLocalDate(nonNull(sivilstandDTO.getSivilstandsdato()) ?
-                                        sivilstandDTO.getSivilstandsdato() :
-                                        sivilstandDTO.getBekreftelsesdato()))
-                                .registrertAv(CONSUMER)
-                                .build();
+                        sivilstander.getSivilstander().stream()
+                                .sorted(Comparator.comparing(SivilstandDTO::getId).reversed())
+                                .filter(SivilstandDTO::isSamboer)
+                                .findFirst()
+                                .ifPresent(sivilstandSamboer -> {
 
-                        var samboer = mapperFacade.map(hovedperson, PensjonSamboerRequest.class);
-                        samboer.setPidBruker(hovedperson.getPidSamboer());
-                        samboer.setPidSamboer(hovedperson.getPidBruker());
+                                    var hovedperson = PensjonSamboerRequest.builder()
+                                            .pidBruker(ident)
+                                            .pidSamboer(sivilstandSamboer.getRelatertVedSivilstand())
+                                            .datoFom(nonNull(sivilstandSamboer.getSivilstandsdato()) ?
+                                                    toLocalDate(sivilstandSamboer.getSivilstandsdato()) :
+                                                    LocalDate.now())
+                                            .datoTom(sivilstander.getSivilstander().stream()
+                                                    .sorted(Comparator.comparing(SivilstandDTO::getId).reversed())
+                                                    .filter(sivilstand -> sivilstand.isGift() &&
+                                                            sivilstander.getSivilstander().stream()
+                                                                    .anyMatch(sivilstand2 -> sivilstand2.isSamboer() &&
+                                                                            sivilstand2.getId() < sivilstand.getId()))
+                                                    .map(sivilstand -> toLocalDate(nonNull(sivilstand.getSivilstandsdato()) ?
+                                                            sivilstand.getSivilstandsdato() : sivilstand.getBekreftelsesdato()))
+                                                    .filter(Objects::nonNull)
+                                                    .map(dato -> dato.minusDays(1))
+                                                    .findFirst()
+                                                    .orElse(null))
+                                            .registrertAv(CONSUMER)
+                                            .build();
 
-                        list.addAll(List.of(hovedperson, samboer));
+                                    var samboer = mapperFacade.map(hovedperson, PensjonSamboerRequest.class);
+                                    samboer.setPidBruker(hovedperson.getPidSamboer());
+                                    samboer.setPidSamboer(hovedperson.getPidBruker());
+
+                                    list.addAll(List.of(hovedperson, samboer));
+                                });
                     }
                 })
                 .register();
