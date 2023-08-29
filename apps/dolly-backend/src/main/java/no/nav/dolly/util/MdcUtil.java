97a795ce10fad1b8a377737651b53d81cffad1e0
kristenhaerum <kristen.herum@nav.no>
2023-08-29 13:03:51 +0200

    Revert "Tester MDC logging"
    This reverts commit c71d87d1480a3c2a2360ea1460a52e5062b20990.

--- apps/dolly-backend/src/main/java/no/nav/dolly/util/MdcUtil.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/util/MdcUtil.java
@@ -5,5 +5,4 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class MdcUtil {
     public static final String MDC_KEY_BESTILLING = "bestillingId";
-    public static final String MDC_KEY_CORRELATION_ID = "correlationId";
 }
