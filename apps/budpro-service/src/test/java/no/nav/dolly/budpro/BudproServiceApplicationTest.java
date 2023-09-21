4593e710a5d1dc3c522c39001475fcdabe5858a1
Cato Olsen <olsen.cato@gmail.com>
2023-09-21 09:45:43 +0200

    Bruker samme versjon av plugin com.gradle.enterprise som ellers.

--- apps/budpro-service/src/test/java/no/nav/dolly/budpro/BudproServiceApplicationTest.java
+++ apps/budpro-service/src/test/java/no/nav/dolly/budpro/BudproServiceApplicationTest.java
@@ -3,11 +3,15 @@ package no.nav.dolly.budpro;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 @SpringBootTest
 class BudproServiceApplicationTest {
 
     @Test
     void contextLoads() {
+        assertThat(true)
+                .isTrue();
     }
 
 }
