43ec550d6f8a202a9527a433ad970c977c1c873f
stigus <stian.gustavsson@nav.no>
2023-09-21 17:21:23 +0200

    Endret litt på oppsett #deploy-sykemelding-api

--- apps/sykemelding-api/src/main/java/no/nav/registre/testnorge/sykemelding/config/AppConfig.java
+++ apps/sykemelding-api/src/main/java/no/nav/registre/testnorge/sykemelding/config/AppConfig.java
@@ -2,13 +2,14 @@ package no.nav.registre.testnorge.sykemelding.config;
 
 import no.nav.registre.testnorge.sykemelding.domain.ApplicationInfo;
 import no.nav.testnav.libs.servletcore.config.ApplicationCoreConfig;
+import no.nav.testnav.libs.servletsecurity.config.SecureOAuth2ServerToServerConfiguration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import(ApplicationCoreConfig.class)
+@Import({ ApplicationCoreConfig.class, SecureOAuth2ServerToServerConfiguration.class })
 public class AppConfig {
 
     @Bean
