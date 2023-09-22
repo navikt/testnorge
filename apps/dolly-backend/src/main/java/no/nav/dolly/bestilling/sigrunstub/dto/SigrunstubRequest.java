5fbdbfab87346324a6bce81e93db3dfa1c7a3e56
Kristen Hærum <kristen.herum@nav.no>
2023-09-22 10:50:07 +0200

    Feature/sigrunstub nytt api (#3283)
    * Sigrunstub nytt endepunkt (put) for lignet inntekt
    * Sigrunstub nytt endepunkt for pensjonsgivende inntekt
    * Refaktorering

---
+++ apps/dolly-backend/src/main/java/no/nav/dolly/bestilling/sigrunstub/dto/SigrunstubRequest.java
@@ -0,0 +1,17 @@
+package no.nav.dolly.bestilling.sigrunstub.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SigrunstubRequest {
+
+    private String inntektsaar;
+    private String personidentifikator;
+    private String testdataEier;
+}
