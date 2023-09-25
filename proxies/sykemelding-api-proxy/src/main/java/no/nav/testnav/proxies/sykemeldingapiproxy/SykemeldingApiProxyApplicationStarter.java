1294565a09f1fbc076657fc7b22e5fd3e940cb50
stigus <stian.gustavsson@nav.no>
2023-09-25 10:39:25 +0200

    Endret litt på SykemeldingApiProxyApplicationStarter oppsett #deploy-sykemelding-api #deploy-sykemelding-api-proxy

--- proxies/sykemelding-api-proxy/src/main/java/no/nav/testnav/proxies/sykemeldingapiproxy/SykemeldingApiProxyApplicationStarter.java
+++ proxies/sykemelding-api-proxy/src/main/java/no/nav/testnav/proxies/sykemeldingapiproxy/SykemeldingApiProxyApplicationStarter.java
@@ -4,53 +4,40 @@ import no.nav.testnav.libs.reactivecore.config.CoreConfig;
 import no.nav.testnav.libs.reactiveproxy.config.DevConfig;
 import no.nav.testnav.libs.reactiveproxy.config.SecurityConfig;
 import no.nav.testnav.libs.reactiveproxy.filter.AddAuthenticationRequestGatewayFilterFactory;
+import no.nav.testnav.libs.reactivesecurity.config.SecureOAuth2ServerToServerConfiguration;
 import no.nav.testnav.libs.reactivesecurity.exchange.azuread.NavAzureAdTokenService;
 import no.nav.testnav.libs.securitycore.domain.AccessToken;
 import no.nav.testnav.proxies.sykemeldingapiproxy.config.SykemeldingApiProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.filter.GatewayFilter;
-import org.springframework.cloud.gateway.route.Route;
 import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.Buildable;
-import org.springframework.cloud.gateway.route.builder.PredicateSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-import java.util.function.Function;
-
 @Import({
         CoreConfig.class,
         DevConfig.class,
-        SecurityConfig.class
+        SecurityConfig.class,
+        SecureOAuth2ServerToServerConfiguration.class
 })
 @SpringBootApplication
 public class SykemeldingApiProxyApplicationStarter {
     @Bean
-    public RouteLocator customRouteLocator(RouteLocatorBuilder builder,
-                                           NavAzureAdTokenService tokenService,
-                                           SykemeldingApiProperties sykemeldingApiProperties) {
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder, NavAzureAdTokenService tokenService, SykemeldingApiProperties properties) {
+        var addAuthenticationHeaderDevFilter = AddAuthenticationRequestGatewayFilterFactory
+                .bearerAuthenticationHeaderFilter(() -> tokenService.exchange(properties).map(AccessToken::getTokenValue));
 
-        return builder
-                .routes()
-                .route(createRoute("sykemelding", "http://testnorge-sykemelding-api.dolly.svc.nais.local",
-                        AddAuthenticationRequestGatewayFilterFactory
-                                .bearerAuthenticationHeaderFilter(() -> tokenService.exchange(sykemeldingApiProperties)
-                                        .map(AccessToken::getTokenValue))))
+        return builder.routes()
+                .route(spec -> spec
+                        .path("/**")
+                        .filters(filterSpec -> filterSpec.filter(addAuthenticationHeaderDevFilter))
+                        .uri(properties.getUrl())
+                )
                 .build();
     }
 
     public static void main(String[] args) {
         SpringApplication.run(SykemeldingApiProxyApplicationStarter.class, args);
-    }
-
-    private Function<PredicateSpec, Buildable<Route>> createRoute(String segment, String host, GatewayFilter filter) {
-        return spec -> spec
-                .path("/" + segment + "/**")
-                .filters(filterSpec -> filterSpec
-                        .rewritePath("/" + segment + "/(?<segment>.*)", "/${segment}")
-                        .filter(filter)
-                ).uri(host);
     }
 }
