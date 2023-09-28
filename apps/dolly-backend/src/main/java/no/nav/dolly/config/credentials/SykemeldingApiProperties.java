edf7066e1013900ca929e12b716fef3dd0a4ae09
stigus <stian.gustavsson@nav.no>
2023-09-28 13:37:05 +0200

    Fjernet sykemelding-api-proxy og refaktorert noe på sykemelding knytningene #deploy-sykemelding-api #deploy-synt-sykemelding-api #deploy-test-dolly-backend

--- apps/dolly-backend/src/main/java/no/nav/dolly/config/credentials/SykemeldingApiProperties.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/config/credentials/SykemeldingApiProperties.java
@@ -1,10 +1,11 @@
-package no.nav.testnav.proxies.sykemeldingapiproxy.config;
+package no.nav.dolly.config.credentials;
 
 import no.nav.testnav.libs.securitycore.domain.ServerProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
-@ConfigurationProperties(prefix = "consumers.sykemelding-api")
+@ConfigurationProperties(prefix = "consumers.testnorge-sykemelding-api")
 public class SykemeldingApiProperties extends ServerProperties {
 }
