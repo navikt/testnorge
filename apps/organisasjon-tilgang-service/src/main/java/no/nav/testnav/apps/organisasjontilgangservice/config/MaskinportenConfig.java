f7e88b067685d3d62b5618146155e2281d52fddb
kristenhaerum <kristen.herum@nav.no>
2023-09-29 08:08:32 +0200

    Oppdatert konfig V #deploy-organisasjon-tilgang-service

--- apps/organisasjon-tilgang-service/src/main/java/no/nav/testnav/apps/organisasjontilgangservice/config/MaskinportenConfig.java
+++ apps/organisasjon-tilgang-service/src/main/java/no/nav/testnav/apps/organisasjontilgangservice/config/MaskinportenConfig.java
@@ -1,41 +1,24 @@
 package no.nav.testnav.apps.organisasjontilgangservice.config;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
+@Getter
 @Configuration
+@NoArgsConstructor
 public class MaskinportenConfig {
 
-    private final String clientId;
-    private final String jwkPrivate;
-    private final String scope;
-    private final String wellKnownUrl;
+    @Value("${MASKINPORTEN_CLIENT_ID}")
+    private String clientId;
 
-    public MaskinportenConfig(
-            @Value("${MASKINPORTEN_CLIENT_ID}") String clientId,
-            @Value("${MASKINPORTEN_CLIENT_JWK}") String jwkPrivate,
-            @Value("${MASKINPORTEN_SCOPES}") String scope,
-            @Value("${MASKINPORTEN_WELL_KNOWN_URL}") String wellKnownUrl
-    ) {
-        this.clientId = clientId;
-        this.scope = scope;
-        this.jwkPrivate = jwkPrivate;
-        this.wellKnownUrl = wellKnownUrl;
-    }
+    @Value("${MASKINPORTEN_CLIENT_JWK}")
+    private String jwkPrivate;
 
-    public String getClientId() {
-        return clientId;
-    }
+    @Value("${MASKINPORTEN_SCOPES}")
+    private String scope;
 
-    public String getJwkPrivate() {
-        return jwkPrivate;
-    }
-
-    public String getScope() {
-        return scope;
-    }
-
-    public String getWellKnownUrl() {
-        return wellKnownUrl;
-    }
+    @Value("${MASKINPORTEN_WELL_KNOWN_URL}")
+    private String wellKnownUrl;
 }
