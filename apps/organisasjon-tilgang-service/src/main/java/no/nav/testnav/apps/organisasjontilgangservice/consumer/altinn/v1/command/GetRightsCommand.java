395c0b4c3bda4f942ce8d381f1522040c946bfee
kristenhaerum <kristen.herum@nav.no>
2023-10-02 12:26:09 +0200

    Prøve og feile #deploy-organisasjon-tilgang-service

--- apps/organisasjon-tilgang-service/src/main/java/no/nav/testnav/apps/organisasjontilgangservice/consumer/altinn/v1/command/GetRightsCommand.java
+++ apps/organisasjon-tilgang-service/src/main/java/no/nav/testnav/apps/organisasjontilgangservice/consumer/altinn/v1/command/GetRightsCommand.java
@@ -27,6 +27,7 @@ public class GetRightsCommand implements Callable<Flux<RightDTO>> {
                 .uri(builder -> builder.path("/api/serviceowner/Srr")
                         .queryParam("serviceCode", serviceCode)
                         .queryParam("serviceEditionCode", serviceEdition)
+                        .queryParam("reportee", "NAVIKT")
                         .build()
                 )
                 .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
