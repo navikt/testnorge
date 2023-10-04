894175ce404326ddc5b7242bad5b6933be24bdcc
kristenhaerum <kristen.herum@nav.no>
2023-10-04 10:39:52 +0200

    Oppdatering av tilganger #deploy-generer-navn-service

--- apps/generer-navn-service/src/main/java/no/nav/registre/testnorge/generernavnservice/GenererNavnServiceApplicationStarter.java
+++ apps/generer-navn-service/src/main/java/no/nav/registre/testnorge/generernavnservice/GenererNavnServiceApplicationStarter.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GenererNavnServiceApplicationStarter {
     public static void main(String[] args) {
+
         SpringApplication.run(GenererNavnServiceApplicationStarter.class, args);
     }
-
 }
