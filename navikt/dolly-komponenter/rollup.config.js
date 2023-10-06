5905586b5966bbd4f879bd52f8f4c63110595006
stigus <stian.gustavsson@nav.no>
2023-10-06 10:53:27 +0200

    Lagt til storybook og endret hvordan komponenter exportes for dolly komponenter

---
+++ navikt/dolly-komponenter/rollup.config.js
@@ -0,0 +1,46 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import dts from 'rollup-plugin-dts'; // To handle css files
+import postcss from 'rollup-plugin-postcss';
+
+import terser from '@rollup/plugin-terser';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import image from '@rollup/plugin-image';
+import typescript from '@rollup/plugin-typescript';
+
+import packageJson from './package.json' assert { type: 'json' };
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: './tsconfig.json' }),
+      postcss(),
+
+      terser(),
+      image(),
+    ],
+  },
+  {
+    input: 'dist/esm/types/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    plugins: [dts()],
+
+    external: [/\.css$/], // telling rollup anything that is .css aren't part of type exports
+  },
+];
---
+++ navikt/dolly-komponenter/src/header/Header.stories.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import { Story, Meta } from '@storybook/react';
+import Comp, { HeaderProps } from './Header';
+import Profil from '../profil/Profil';
+import HeaderLinkGroup from './HeaderLinkGroup';
+import HeaderLink from './HeaderLink';
+
+export default {
+  title: 'Header/Header',
+  component: Comp,
+} as Meta;
+
+const Template: Story<HeaderProps> = (args) => <Comp {...args} />;
+export const Header = Template.bind({});
+Header.args = {
+  title: 'Header text',
+};
+
+export const ProfileHeader = Template.bind({});
+ProfileHeader.args = {
+  title: 'Header with profile',
+  profile: <Profil visningsnavn="Jon Smith" />,
+};
+
+const TemplateLinkGroup: Story<HeaderProps> = (args) => (
+  <Comp {...args}>
+    <HeaderLinkGroup>
+      <HeaderLink href="#/link1" isActive={() => true}>
+        Link 1
+      </HeaderLink>
+      <HeaderLink href="#/link2" isActive={() => false}>
+        Link 2
+      </HeaderLink>
+      <HeaderLink href="#/link3" isActive={() => false}>
+        Link 3
+      </HeaderLink>
+    </HeaderLinkGroup>
+  </Comp>
+);
+
+export const LinksHeader = TemplateLinkGroup.bind({});
+LinksHeader.args = {
+  title: 'Header with links',
+};
+
+export const LinksWithProfileHeader = TemplateLinkGroup.bind({});
+LinksWithProfileHeader.args = {
+  title: 'Header with links and profile',
+  profile: <Profil visningsnavn="Jon Smith" />,
+};
