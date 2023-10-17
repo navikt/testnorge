5b4ec02e6c60d1d3f19422e833a35cee493d48cd
Cato Olsen <olsen.cato@gmail.com>
2023-10-17 14:30:57 +0200

    Endrer fra annotasjoner fra javax.persistence til jakarta.persistence.

--- apps/brreg-stub/src/main/java/no/nav/brregstub/database/domene/HentRolle.java
+++ apps/brreg-stub/src/main/java/no/nav/brregstub/database/domene/HentRolle.java
@@ -1,9 +1,9 @@
 package no.nav.brregstub.database.domene;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 @Entity
