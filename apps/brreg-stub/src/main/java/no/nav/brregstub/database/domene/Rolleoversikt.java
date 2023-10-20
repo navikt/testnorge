3ee01e9b48e4b56142513a4aed3294c4a6bb94d2
stigus <stian.gustavsson@nav.no>
2023-10-20 09:39:01 +0200

    Brreg-stub hibernate 6 migrering #deploy-brreg-stub

--- apps/brreg-stub/src/main/java/no/nav/brregstub/database/domene/Rolleoversikt.java
+++ apps/brreg-stub/src/main/java/no/nav/brregstub/database/domene/Rolleoversikt.java
@@ -3,7 +3,6 @@ package no.nav.brregstub.database.domene;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
@@ -17,13 +16,7 @@ import lombok.Setter;
 public class Rolleoversikt {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    @GeneratedValue(generator = "rolleOversiktIdGenerator")
-//    @GenericGenerator(name = "rolleOversiktIdGenerator", strategy = SEQUENCE_STYLE_GENERATOR, parameters = {
-//            @org.hibernate.annotations.Parameter(name = "sequence_name", value = "ROLLEOVERSIKT_SEQ"),
-//            @org.hibernate.annotations.Parameter(name = "initial_value", value = "1"),
-//            @org.hibernate.annotations.Parameter(name = "increment_size", value = "1")
-//    })
+    @GeneratedValue
     private Long id;
 
     @NotNull(message = "fnr must not be null")
--- apps/brreg-stub/src/main/java/no/nav/brregstub/jpa/HibernateConstants.java
+++
@@ -1,9 +0,0 @@
-package no.nav.brregstub.jpa;
-
-import lombok.experimental.UtilityClass;
-
-@UtilityClass
-public class HibernateConstants {
-
-    public static final String SEQUENCE_STYLE_GENERATOR = "org.hibernate.id.enhanced.SequenceStyleGenerator";
-}
