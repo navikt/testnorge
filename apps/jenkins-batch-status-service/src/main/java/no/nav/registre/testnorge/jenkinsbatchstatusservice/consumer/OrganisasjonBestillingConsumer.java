9398a6b067b2bb15e5801697102fb03e590b4455
Cato Olsen <olsen.cato@gmail.com>
2023-10-26 13:42:40 +0200

    Fjernet boilerplate fra jenkins-batch-status-service sin bruk av ServerProperties.

--- apps/jenkins-batch-status-service/src/main/java/no/nav/registre/testnorge/jenkinsbatchstatusservice/consumer/OrganisasjonBestillingConsumer.java
+++ apps/jenkins-batch-status-service/src/main/java/no/nav/registre/testnorge/jenkinsbatchstatusservice/consumer/OrganisasjonBestillingConsumer.java
@@ -4,7 +4,7 @@ import io.netty.channel.ChannelOption;
 import io.netty.handler.timeout.ReadTimeoutHandler;
 import io.netty.handler.timeout.WriteTimeoutHandler;
 import lombok.extern.slf4j.Slf4j;
-import no.nav.registre.testnorge.jenkinsbatchstatusservice.config.credentials.OrganisasjonBestillingServiceProperties;
+import no.nav.registre.testnorge.jenkinsbatchstatusservice.config.Consumers;
 import no.nav.registre.testnorge.jenkinsbatchstatusservice.consumer.command.SaveOrganisasjonBestillingCommand;
 import no.nav.registre.testnorge.jenkinsbatchstatusservice.consumer.command.UpdateOrganisasjonBestillingCommand;
 import no.nav.testnav.libs.dto.organiasjonbestilling.v1.OrderDTO;
@@ -23,30 +23,30 @@ public class OrganisasjonBestillingConsumer {
     private static final int TIMEOUT_SECONDS = 10;
     private final WebClient webClient;
     private final TokenExchange tokenExchange;
-    private final ServerProperties properties;
+    private final ServerProperties serverProperties;
 
     public OrganisasjonBestillingConsumer(
-            OrganisasjonBestillingServiceProperties properties,
+            Consumers consumers,
             TokenExchange tokenExchange) {
-
-        this.properties = properties;
+        serverProperties = consumers.getOrganisasjonBestillingService();
         this.tokenExchange = tokenExchange;
         this.webClient = WebClient
-            .builder()
-            .baseUrl(properties.getUrl())
-            .clientConnector(new ReactorClientHttpConnector(
-                HttpClient
-                    .create()
-                    .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, TIMEOUT_SECONDS * 1000)
-                    .doOnConnected(connection -> connection
-                        .addHandlerLast(new ReadTimeoutHandler(TIMEOUT_SECONDS))
-                        .addHandlerLast(new WriteTimeoutHandler(TIMEOUT_SECONDS)))
-            ))
-            .build();
+                .builder()
+                .baseUrl(serverProperties.getUrl())
+                .clientConnector(
+                        new ReactorClientHttpConnector(
+                                HttpClient
+                                        .create()
+                                        .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, TIMEOUT_SECONDS * 1000)
+                                        .doOnConnected(connection -> connection
+                                                .addHandlerLast(new ReadTimeoutHandler(TIMEOUT_SECONDS))
+                                                .addHandlerLast(new WriteTimeoutHandler(TIMEOUT_SECONDS)))
+                        ))
+                .build();
     }
 
     public Long save(String uuid) {
-        var accessToken = Objects.requireNonNull(tokenExchange.exchange(properties).block(), "Token var null.");
+        var accessToken = Objects.requireNonNull(tokenExchange.exchange(serverProperties).block(), "Token var null.");
         log.info("Registrerer jobb med uuid: {}.", uuid);
         var id = new SaveOrganisasjonBestillingCommand(webClient, accessToken.getTokenValue(), uuid).call();
         log.info("Jobb registert med id {} og uuid: {}.", uuid, id);
@@ -54,7 +54,7 @@ public class OrganisasjonBestillingConsumer {
     }
 
     public void update(String uuid, String miljo, Long jobId, Long id) {
-        var accessToken = Objects.requireNonNull(tokenExchange.exchange(properties).block(), "Token var null.");
+        var accessToken = Objects.requireNonNull(tokenExchange.exchange(serverProperties).block(), "Token var null.");
         log.info("Oppretter organisasjon bestilling for uuid {} med job id: {}", uuid, jobId);
         var dto = new OrderDTO(miljo, jobId);
         new UpdateOrganisasjonBestillingCommand(webClient, dto, accessToken.getTokenValue(), uuid, id).run();
