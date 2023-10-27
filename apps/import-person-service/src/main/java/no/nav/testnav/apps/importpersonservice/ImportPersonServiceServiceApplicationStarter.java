d79b4b5ab56f89e26dd46ff917634b14006e0272
kristenhaerum <kristen.herum@nav.no>
2023-10-27 07:39:30 +0200

    Oppdatert helsesjekk

--- apps/import-person-service/src/main/java/no/nav/testnav/apps/importpersonservice/ImportPersonServiceServiceApplicationStarter.java
+++ apps/import-person-service/src/main/java/no/nav/testnav/apps/importpersonservice/ImportPersonServiceServiceApplicationStarter.java
@@ -1,20 +1,18 @@
 package no.nav.testnav.apps.importpersonservice;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
-import org.springframework.context.annotation.Import;
-import org.springframework.web.reactive.config.EnableWebFlux;
-
 import no.nav.testnav.libs.reactivecore.config.CoreConfig;
 import no.nav.testnav.libs.reactivesecurity.config.SecureOAuth2ServerToServerConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @Import({
         CoreConfig.class,
         SecureOAuth2ServerToServerConfiguration.class
 })
 @EnableWebFlux
-@SpringBootApplication(exclude = {ElasticsearchDataAutoConfiguration.class})
+@SpringBootApplication
 public class ImportPersonServiceServiceApplicationStarter {
     public static void main(String[] args) {
         SpringApplication.run(ImportPersonServiceServiceApplicationStarter.class, args);
--- apps/tps-messaging-service/src/main/java/no/nav/testnav/apps/tpsmessagingservice/TpsMessagingServiceApplicationStarter.java
+++ apps/tps-messaging-service/src/main/java/no/nav/testnav/apps/tpsmessagingservice/TpsMessagingServiceApplicationStarter.java
@@ -2,9 +2,8 @@ package no.nav.testnav.apps.tpsmessagingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
 
-@SpringBootApplication(exclude = ElasticsearchDataAutoConfiguration.class)
+@SpringBootApplication
 public class TpsMessagingServiceApplicationStarter {
 
     public static void main(String[] args) {
