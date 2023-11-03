7d17749339bca49d72a47608e3a6bdcd50f54572
stigus <stian.gustavsson@nav.no>
2023-11-03 13:06:43 +0100

    Tester endring på SequenceGenerator i Brregstub #deploy-brreg-stub

--- apps/brreg-stub/src/main/java/no/nav/brregstub/database/domene/Rolleoversikt.java
+++ apps/brreg-stub/src/main/java/no/nav/brregstub/database/domene/Rolleoversikt.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
@@ -17,7 +18,8 @@ import lombok.Setter;
 public class Rolleoversikt {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "rolleoversikt_seq")
+    @SequenceGenerator(name = "rolleoversikt_seq", sequenceName = "ROLLEOVERSIKT_SEQ")
     private Long id;
 
     @NotNull(message = "fnr must not be null")
