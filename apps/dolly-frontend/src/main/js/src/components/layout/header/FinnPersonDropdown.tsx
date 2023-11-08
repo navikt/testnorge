5a57fd60672459de4979225398d6a3990910504d
Betsy Carina Traran <betsy.carina.traran@nav.no>
2023-11-08 14:59:48 +0100

    Navigering til soek

---
+++ apps/dolly-frontend/src/main/js/src/components/layout/header/FinnPersonDropdown.tsx
@@ -0,0 +1,47 @@
+import { Dropdown, DropdownContext } from '@navikt/ds-react-internal'
+import { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+import Icon from '@/components/ui/icon/Icon'
+
+const DropdownToggle = () => {
+	const context = useContext(DropdownContext)
+	const { isOpen } = context
+
+	return (
+		<Dropdown.Toggle
+			className={isOpen ? 'dropdown-toggle active' : 'dropdown-toggle'}
+			style={{ margin: '0 10px', padding: '20px 10px' }}
+		>
+			Finn person
+		</Dropdown.Toggle>
+	)
+}
+export const FinnPersonDropdown = () => {
+	const navigate = useNavigate()
+
+	return (
+		<div style={{ color: 'white', fontSize: '1.2em', margin: '0 10px' }}>
+			<Dropdown>
+				<DropdownToggle />
+				<Dropdown.Menu placement="bottom-end">
+					<Dropdown.Menu.List>
+						<Dropdown.Menu.List.Item
+							onClick={() => navigate('/dollysoek')}
+							style={{ color: '#212529' }}
+						>
+							<Icon kind="search" fontSize="1.5rem" />
+							Søk i Dolly
+						</Dropdown.Menu.List.Item>
+						<Dropdown.Menu.List.Item
+							onClick={() => navigate('/testnorge')}
+							style={{ color: '#212529' }}
+						>
+							<Icon kind="search" fontSize="1.5rem" />
+							Søk i Test-Norge
+						</Dropdown.Menu.List.Item>
+					</Dropdown.Menu.List>
+				</Dropdown.Menu>
+			</Dropdown>
+		</div>
+	)
+}
--- apps/dolly-frontend/src/main/js/src/components/layout/header/Header.tsx
+++ apps/dolly-frontend/src/main/js/src/components/layout/header/Header.tsx
@@ -8,6 +8,7 @@ import Loading from '@/components/ui/loading/Loading'
 import { BrukerDropdown } from '@/components/layout/header/BrukerDropdown'
 import { DokumentasjonDropdown } from '@/components/layout/header/DokumentasjonDropdown'
 import { CypressSelector } from '../../../../cypress/mocks/Selectors'
+import { FinnPersonDropdown } from '@/components/layout/header/FinnPersonDropdown'
 
 export default () => {
 	const { currentBruker, loading } = useCurrentBruker()
@@ -34,9 +35,10 @@ export default () => {
 				<NavLink data-cy={CypressSelector.BUTTON_HEADER_ORGANISASJONER} to="/organisasjoner">
 					Organisasjoner
 				</NavLink>
-				<NavLink data-cy={CypressSelector.BUTTON_HEADER_TESTNORGE} to="/testnorge">
-					Test-Norge
-				</NavLink>
+				<FinnPersonDropdown />
+				{/*<NavLink data-cy={CypressSelector.BUTTON_HEADER_TESTNORGE} to="/testnorge">*/}
+				{/*	Test-Norge*/}
+				{/*</NavLink>*/}
 				{!bankidBruker && (
 					<NavLink data-cy={CypressSelector.BUTTON_HEADER_ENDRINGSMELDING} to="/endringsmelding">
 						Endringsmelding
