66d8539457288867ca2f7704b392f1e3494f0d90
stigus <stian.gustavsson@nav.no>
2023-11-27 10:52:13 +0100

    Refaktorert kodeverk-proxy og sender nå med accesstoken etter forespørsel fra de #deploy-proxy"

--- proxies/kodeverk-proxy/src/main/java/no/nav/testnav/proxies/kodeverkproxy/KodeverkProxyApplicationStarter.java
+++ proxies/kodeverk-proxy/src/main/java/no/nav/testnav/proxies/kodeverkproxy/KodeverkProxyApplicationStarter.java
@@ -3,6 +3,10 @@ package no.nav.testnav.proxies.kodeverkproxy;
 import no.nav.testnav.libs.reactivecore.config.CoreConfig;
 import no.nav.testnav.libs.reactiveproxy.config.DevConfig;
 import no.nav.testnav.libs.reactiveproxy.config.SecurityConfig;
+import no.nav.testnav.libs.reactiveproxy.filter.AddAuthenticationRequestGatewayFilterFactory;
+import no.nav.testnav.libs.reactivesecurity.config.SecureOAuth2ServerToServerConfiguration;
+import no.nav.testnav.libs.reactivesecurity.exchange.azuread.TrygdeetatenAzureAdTokenService;
+import no.nav.testnav.libs.securitycore.domain.AccessToken;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
@@ -13,21 +17,33 @@ import org.springframework.context.annotation.Import;
 @Import({
         CoreConfig.class,
         DevConfig.class,
-        SecurityConfig.class
+        SecurityConfig.class,
+        SecureOAuth2ServerToServerConfiguration.class
 })
 @SpringBootApplication
 public class KodeverkProxyApplicationStarter {
-    public static void main(String[] args) {
-        SpringApplication.run(KodeverkProxyApplicationStarter.class, args);
-    }
-
     @Bean
-    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-        return builder.routes()
+    public RouteLocator customRouteLocator(
+            RouteLocatorBuilder builder,
+            TrygdeetatenAzureAdTokenService tokenService,
+            Consumers consumers
+    ) {
+        var addAuthenticationHeaderDevFilter = AddAuthenticationRequestGatewayFilterFactory
+                .bearerAuthenticationHeaderFilter(
+                        () -> tokenService
+                                .exchange(consumers.getKodeverk())
+                                .map(AccessToken::getTokenValue));
+        return builder
+                .routes()
                 .route(spec -> spec
                         .path("/**")
+                        .filters(filterSpec -> filterSpec.filter(addAuthenticationHeaderDevFilter))
                         .uri("http://kodeverk.org.svc.nais.local/")
                 )
                 .build();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodeverkProxyApplicationStarter.class, args);
     }
 }
