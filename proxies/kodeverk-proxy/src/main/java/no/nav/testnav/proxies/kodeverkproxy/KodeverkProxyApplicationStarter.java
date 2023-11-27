77e6ef6e40db6ad760ef8f0faf423692bb52107a
stigus <stian.gustavsson@nav.no>
2023-11-27 14:09:15 +0100

    Deploy med azure oppsett for nav tenant #deploy-proxy"

--- proxies/kodeverk-proxy/src/main/java/no/nav/testnav/proxies/kodeverkproxy/KodeverkProxyApplicationStarter.java
+++ proxies/kodeverk-proxy/src/main/java/no/nav/testnav/proxies/kodeverkproxy/KodeverkProxyApplicationStarter.java
@@ -5,7 +5,7 @@ import no.nav.testnav.libs.reactiveproxy.config.DevConfig;
 import no.nav.testnav.libs.reactiveproxy.config.SecurityConfig;
 import no.nav.testnav.libs.reactiveproxy.filter.AddAuthenticationRequestGatewayFilterFactory;
 import no.nav.testnav.libs.reactivesecurity.config.SecureOAuth2ServerToServerConfiguration;
-import no.nav.testnav.libs.reactivesecurity.exchange.azuread.NavAzureAdTokenService;
+import no.nav.testnav.libs.reactivesecurity.exchange.TokenExchange;
 import no.nav.testnav.libs.securitycore.domain.AccessToken;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -25,12 +25,12 @@ public class KodeverkProxyApplicationStarter {
     @Bean
     public RouteLocator customRouteLocator(
             RouteLocatorBuilder builder,
-            NavAzureAdTokenService tokenService,
+            TokenExchange tokenExchange,
             Consumers consumers
     ) {
         var addAuthenticationHeaderDevFilter = AddAuthenticationRequestGatewayFilterFactory
                 .bearerAuthenticationHeaderFilter(
-                        () -> tokenService
+                        () -> tokenExchange
                                 .exchange(consumers.getKodeverk())
                                 .map(AccessToken::getTokenValue));
         return builder
