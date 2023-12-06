7d7974061014ddb2e2f7e063efa83a82c02a6cc1
kristenhaerum <kristen.herum@nav.no>
2023-12-06 10:16:56 +0100

    Oppdatert validering

--- apps/dolly-frontend/src/main/js/src/components/fagsystem/alderspensjon/form/validation.tsx
+++ apps/dolly-frontend/src/main/js/src/components/fagsystem/alderspensjon/form/validation.tsx
@@ -1,39 +1,56 @@
-import {ifPresent, messages, requiredNumber} from '@/utils/YupValidations'
+import { ifPresent, messages, requiredNumber } from '@/utils/YupValidations'
 import * as Yup from 'yup'
-import {isAfter} from 'date-fns'
+import {isAfter, isBefore} from 'date-fns'
 
 export const validation = {
-    alderspensjon: ifPresent(
-        '$pensjonforvalter.alderspensjon',
-        Yup.object({
-            kravFremsattDato: Yup.date().nullable(),
-            iverksettelsesdato: Yup.date()
-                .when('soknad', {
-                    is: (soknad: Boolean) => soknad,
-                    then: () => Yup.date().test('is-month-after-now', 'Dato må være frem i tid.',
-                        (iverksettelsesdato) => {
-                            return iverksettelsesdato && isAfter(iverksettelsesdato, new Date())
-                        }),
-                    otherwise: () => Yup.date().test('is-month-after-kravfremsatt',
-                        'Dato må være etter krav fremsatt.',
-                        function validate(iverksettelsesdato) {
-                            const kravFremsattDato: Date =
-                                this.options.context?.pensjonforvalter?.alderspensjon?.kravFremsattDato
-                            return iverksettelsesdato && isAfter(iverksettelsesdato, new Date(kravFremsattDato))
-                        })
-                })
-                .nullable(),
-            saksbehandler: Yup.string().nullable(),
-            attesterer: Yup.string().nullable(),
-            uttaksgrad: requiredNumber.typeError(messages.required),
-            navEnhetId: Yup.string().nullable(),
-            relasjoner: Yup.array().of(
-                Yup.object({
-                    sumAvForvArbKapPenInntekt: Yup.number()
-                        .transform((i, j) => (j === '' ? null : i))
-                        .nullable(),
-                }),
-            ),
-        }),
-    ),
+	alderspensjon: ifPresent(
+		'$pensjonforvalter.alderspensjon',
+		Yup.object({
+			kravFremsattDato: Yup.date()
+				.test(
+					'is-before-iverksettelsesdato',
+					'Dato må være før iverksettelsesdato.',
+					function validate(kravFremsattDato) {
+						const iverksettelsesdato: Date =
+							this.options.context?.pensjonforvalter?.alderspensjon?.iverksettelsesdato
+						return kravFremsattDato && isBefore(kravFremsattDato, new Date(iverksettelsesdato))
+					},
+				)
+				.nullable(),
+			iverksettelsesdato: Yup.date()
+				.when('soknad', {
+					is: (soknad: Boolean) => soknad,
+					then: () =>
+						Yup.date().test(
+							'is-month-after-now',
+							'Dato må være frem i tid.',
+							(iverksettelsesdato) => {
+								return iverksettelsesdato && isAfter(iverksettelsesdato, new Date())
+							},
+						),
+					otherwise: () =>
+						Yup.date().test(
+							'is-month-after-kravfremsatt',
+							'Dato må være etter krav fremsatt.',
+							function validate(iverksettelsesdato) {
+								const kravFremsattDato: Date =
+									this.options.context?.pensjonforvalter?.alderspensjon?.kravFremsattDato
+								return iverksettelsesdato && isAfter(iverksettelsesdato, new Date(kravFremsattDato))
+							},
+						),
+				})
+				.nullable(),
+			saksbehandler: Yup.string().nullable(),
+			attesterer: Yup.string().nullable(),
+			uttaksgrad: requiredNumber.typeError(messages.required),
+			navEnhetId: Yup.string().nullable(),
+			relasjoner: Yup.array().of(
+				Yup.object({
+					sumAvForvArbKapPenInntekt: Yup.number()
+						.transform((i, j) => (j === '' ? null : i))
+						.nullable(),
+				}),
+			),
+		}),
+	),
 }
