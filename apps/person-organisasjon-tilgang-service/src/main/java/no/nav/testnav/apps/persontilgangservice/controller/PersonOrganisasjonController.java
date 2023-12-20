9437189039b3a5b82dce9f52771005dfcea9f7da
kristenhaerum <kristen.herum@nav.no>
2023-12-20 09:00:35 +0100

    Lagt til mer logging #deploy-person-organisasjon-tilgang-service

--- apps/person-organisasjon-tilgang-service/src/main/java/no/nav/testnav/apps/persontilgangservice/controller/PersonOrganisasjonController.java
+++ apps/person-organisasjon-tilgang-service/src/main/java/no/nav/testnav/apps/persontilgangservice/controller/PersonOrganisasjonController.java
@@ -1,6 +1,7 @@
 package no.nav.testnav.apps.persontilgangservice.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import no.nav.testnav.apps.persontilgangservice.controller.dto.OrganisasjonDTO;
 import no.nav.testnav.apps.persontilgangservice.domain.Access;
 import no.nav.testnav.apps.persontilgangservice.service.PersonOrganisasjonService;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/person/organisasjoner")
 @RequiredArgsConstructor
@@ -30,6 +32,8 @@ public class PersonOrganisasjonController {
         return personOrganisasjonService
                 .getAccess(organisasjonsnummer)
                 .map(Access::toDTO)
+                .doOnNext(response -> log.info("Hentet organisasjon fra Altinn: navn: {}, orgnr: {}, orgform: {}, gyldigTil: {}",
+                        response.navn(), response.organisasjonsnummer(), response.organisasjonsfrom(), response.gyldigTil()))
                 .map(ResponseEntity::ok)
                 .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()));
     }
