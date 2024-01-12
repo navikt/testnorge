442b739b72654359d2b2d3a057cf45e703e1e765
kristenhaerum <kristen.herum@nav.no>
2024-01-12 13:43:38 +0100

    Lagt på mere logging II #deploy-tenor-search-service

--- apps/tenor-search-service/src/main/java/no/nav/testnav/apps/tenorsearchservice/consumers/TenorClient.java
+++ apps/tenor-search-service/src/main/java/no/nav/testnav/apps/tenorsearchservice/consumers/TenorClient.java
@@ -1,15 +1,19 @@
 package no.nav.testnav.apps.tenorsearchservice.consumers;
 
+import lombok.extern.slf4j.Slf4j;
 import no.nav.testnav.apps.tenorsearchservice.config.Consumers;
 import no.nav.testnav.apps.tenorsearchservice.consumers.command.GetTenorTestdata;
 import no.nav.testnav.apps.tenorsearchservice.domain.TenorResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.DefaultUriBuilderFactory;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @Service
 public class TenorClient {
 
+    private static final String TENOR_QUERY_URL = "/api/testnorge/v2/soek/freg";
     private final WebClient webClient;
     private final MaskinportenClient maskinportenClient;
 
@@ -23,6 +27,16 @@ public class TenorClient {
     }
 
     public Mono<TenorResponse> getTestdata(String query) {
+
+        log.info("URI output: {}", new DefaultUriBuilderFactory().uriString(TENOR_QUERY_URL)
+                .queryParam("kql", query)
+                .queryParam("nokkelinformasjon", true)
+                .build());
+
+        log.info("URI output 2: {}", new DefaultUriBuilderFactory().uriString(TENOR_QUERY_URL)
+                .queryParam("kql", query)
+                .queryParam("nokkelinformasjon", true)
+                .build(false));
 
         return maskinportenClient.getAccessToken()
                 .flatMap(token -> new GetTenorTestdata(webClient, query, token.value()).call());
