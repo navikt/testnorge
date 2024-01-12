25f9230f6955bce64f392be677bcb0dea4732493
kristenhaerum <kristen.herum@nav.no>
2024-01-12 08:50:58 +0100

    Endret enkoding II #deploy-tenor-search-service

--- apps/tenor-search-service/src/main/java/no/nav/testnav/apps/tenorsearchservice/consumers/command/GetTenorTestdata.java
+++ apps/tenor-search-service/src/main/java/no/nav/testnav/apps/tenorsearchservice/consumers/command/GetTenorTestdata.java
@@ -2,6 +2,7 @@ package no.nav.testnav.apps.tenorsearchservice.consumers.command;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import no.nav.testnav.apps.tenorsearchservice.domain.TenorResponse;
 import no.nav.testnav.libs.reactivecore.utils.WebClientFilter;
 import org.springframework.http.HttpHeaders;
@@ -15,6 +16,7 @@ import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.util.concurrent.Callable;
 
+@Slf4j
 @RequiredArgsConstructor
 public class GetTenorTestdata implements Callable<Mono<TenorResponse>> {
 
@@ -27,9 +29,11 @@ public class GetTenorTestdata implements Callable<Mono<TenorResponse>> {
     @Override
     public Mono<TenorResponse> call() {
 
+        log.info("Query-parameter: {}", query);
+
         return webClient.get()
                 .uri(uriBuilder -> uriBuilder.path(TENOR_QUERY_URL)
-                        .queryParam("kql", URLEncoder.encode(query, StandardCharsets.US_ASCII))
+                        .queryParam("kql", query)
                         .queryParam("nokkelinformasjon", true)
                         .build())
                 .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
