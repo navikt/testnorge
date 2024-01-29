fa098c4f5dd849ef42ebbad0648bb22f18ab89a2
Cato Olsen <olsen.cato@gmail.com>
2024-01-29 14:09:01 +0100

    Lagt til midlertidig test som alltid skal feile, for å teste build step som trigger på feil i tester.

---
+++ apps/budpro-service/src/test/java/no/nav/dolly/budpro/IntentionallyFailingTest.java
@@ -0,0 +1,14 @@
+package no.nav.dolly.budpro;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+class IntentionallyFailingTest {
+
+    @Test
+    void shouldAlwaysFail() {
+        fail();
+    }
+
+}
