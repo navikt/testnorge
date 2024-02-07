252614b91bb902bc7d99512ae672310980c69d30
kristenhaerum <kristen.herum@nav.no>
2024-02-07 11:10:21 +0100

    Oppdatert inntektsmeldingRequest

--- libs/data-transfer-objects/src/main/java/no/nav/testnav/libs/dto/dokarkiv/v1/RsJoarkMetadata.java
+++ libs/data-transfer-objects/src/main/java/no/nav/testnav/libs/dto/dokarkiv/v1/RsJoarkMetadata.java
@@ -1,10 +1,14 @@
 package no.nav.testnav.libs.dto.dokarkiv.v1;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class RsJoarkMetadata {
 
     private String journalpostType;
