c6841e7780d5e7e0f141c2ccee3e48494056f27a
Kristen Hærum <kristen.herum@nav.no>
2024-02-15 13:03:10 +0100

    Orika trivia inntektsmelding dato mapping

--- libs/data-transfer-objects/src/main/java/no/nav/testnav/libs/dto/inntektsmeldinggeneratorservice/v1/rs/RsArbeidsgiver.java
+++ libs/data-transfer-objects/src/main/java/no/nav/testnav/libs/dto/inntektsmeldinggeneratorservice/v1/rs/RsArbeidsgiver.java
@@ -1,11 +1,15 @@
 package no.nav.testnav.libs.dto.inntektsmeldinggeneratorservice.v1.rs;
 
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class RsArbeidsgiver {
 
     @Size(min = 9, max = 9)
