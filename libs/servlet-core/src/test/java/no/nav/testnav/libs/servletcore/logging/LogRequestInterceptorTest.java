9b3a127834389a2698643d28143d706079c3c0ff
Cato Olsen <olsen.cato@gmail.com>
2024-02-22 10:26:37 +0100

    Logger alltid body ("" hvis ikke satt), for å unngå å måtte deploye endringer i dolly-backend under testing.

--- libs/servlet-core/src/test/java/no/nav/testnav/libs/servletcore/logging/LogRequestInterceptorTest.java
+++ libs/servlet-core/src/test/java/no/nav/testnav/libs/servletcore/logging/LogRequestInterceptorTest.java
@@ -24,25 +24,12 @@ class LogRequestInterceptorTest {
     }
 
     @Test
-    void shouldLogRequestWithoutBody() {
+    void shouldLogRequest() {
 
         var request = new MockHttpServletRequest();
         request.setRequestURI("/api/v1/inntektsmelding");
 
         assertThat(logRequestInterceptor.shouldLogRequest(request)).isTrue();
-        assertThat(logRequestInterceptor.shouldLogBodyOfRequest(request)).isFalse();
-
-    }
-
-    @Test
-    void shouldLogRequestWithBody() {
-
-        var request = new MockHttpServletRequest();
-        request.setRequestURI("/api/v1/inntektsmelding");
-        request.addHeader(LogRequestInterceptor.HEADER_SHOULD_LOG_BODY, "true");
-
-        assertThat(logRequestInterceptor.shouldLogRequest(request)).isTrue();
-        assertThat(logRequestInterceptor.shouldLogBodyOfRequest(request)).isTrue();
 
     }
 
@@ -52,7 +39,6 @@ class LogRequestInterceptorTest {
 
         var request = new MockHttpServletRequest();
         request.setRequestURI("/api/v1/inntektsmelding");
-        request.addHeader(LogRequestInterceptor.HEADER_SHOULD_LOG_BODY, "true");
 
         var body = logRequestInterceptor.getBody(request);
         assertThat(body).isEmpty();
@@ -65,7 +51,6 @@ class LogRequestInterceptorTest {
 
         var request = new MockHttpServletRequest();
         request.setRequestURI("/api/v1/inntektsmelding");
-        request.addHeader(LogRequestInterceptor.HEADER_SHOULD_LOG_BODY, "true");
         request.setContent("test".getBytes(StandardCharsets.UTF_8));
 
         var body = logRequestInterceptor.getBody(request);
