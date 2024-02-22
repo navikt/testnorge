cb2b5b7b78f79a6d141afc84f9a29fe38a240647
Cato Olsen <olsen.cato@gmail.com>
2024-02-22 11:12:51 +0100

    Lagt til custom logging filter.

---
+++ apps/inntektsmelding-service/src/main/java/no/nav/registre/testnav/inntektsmeldingservice/config/RequestLoggingFilterConfig.java
@@ -0,0 +1,22 @@
+package no.nav.registre.testnav.inntektsmeldingservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class RequestLoggingFilterConfig {
+
+    @Bean
+    public CommonsRequestLoggingFilter logFilter() {
+        var filter = new CommonsRequestLoggingFilter();
+        filter.setIncludeClientInfo(true);
+        filter.setIncludeHeaders(true);
+        filter.setIncludePayload(true);
+        filter.setIncludeQueryString(true);
+        filter.setMaxPayloadLength(10000);
+        filter.setAfterMessagePrefix("REQUEST: ");
+        return filter;
+    }
+
+}
