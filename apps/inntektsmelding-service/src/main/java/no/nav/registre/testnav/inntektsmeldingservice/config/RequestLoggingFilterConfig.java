f5e3a20a4a2b14906a1412b7440760adedda5e05
Cato Olsen <olsen.cato@gmail.com>
2024-02-23 15:01:32 +0100

    Reintrodusert (custom) log filter, POST only.

---
+++ apps/inntektsmelding-service/src/main/java/no/nav/registre/testnav/inntektsmeldingservice/config/RequestLoggingFilterConfig.java
@@ -0,0 +1,38 @@
+package no.nav.registre.testnav.inntektsmeldingservice.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.FilterChainProxy;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+@Slf4j
+class RequestLoggingFilterConfig {
+
+    @Bean
+    PostOnlyRequestLoggingFilter logFilter(FilterChainProxy filterChainProxy) {
+
+        log.info("Filter chain: {}", filterChainProxy.getFilterChains());
+
+        var filter = new PostOnlyRequestLoggingFilter();
+        filter.setIncludeClientInfo(true);
+        filter.setIncludeHeaders(true);
+        filter.setIncludePayload(true);
+        filter.setIncludeQueryString(true);
+        filter.setMaxPayloadLength(10000);
+        filter.setAfterMessagePrefix("REQUEST: ");
+        return filter;
+    }
+
+    private static class PostOnlyRequestLoggingFilter extends CommonsRequestLoggingFilter {
+
+        @Override
+        protected boolean shouldLog(HttpServletRequest request) {
+            return "POST".equalsIgnoreCase(request.getMethod());
+        }
+
+    }
+
+}
