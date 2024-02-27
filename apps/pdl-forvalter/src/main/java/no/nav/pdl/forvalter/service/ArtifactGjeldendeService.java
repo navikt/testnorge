3f0e2572bd799fdc75fd0b62e0cf4dd8e9d0ff25
kristenhaerum <kristen.herum@nav.no>
2024-02-27 08:51:14 +0100

    Oppdatert setting av metadata tidspunkter + fjernet parameter isTpsMaster

--- apps/pdl-forvalter/src/main/java/no/nav/pdl/forvalter/service/ArtifactGjeldendeService.java
+++ apps/pdl-forvalter/src/main/java/no/nav/pdl/forvalter/service/ArtifactGjeldendeService.java
@@ -1,17 +1,15 @@
 package no.nav.pdl.forvalter.service;
 
 import lombok.RequiredArgsConstructor;
+import no.nav.pdl.forvalter.database.model.DbPerson;
 import no.nav.pdl.forvalter.database.model.DbRelasjon;
 import no.nav.pdl.forvalter.database.repository.PersonRepository;
 import no.nav.testnav.libs.data.pdlforvalter.v1.AdresseDTO;
 import no.nav.testnav.libs.data.pdlforvalter.v1.DbVersjonDTO;
 import no.nav.testnav.libs.data.pdlforvalter.v1.FolkeregistermetadataDTO;
-import no.nav.testnav.libs.data.pdlforvalter.v1.PersonDTO;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Objects;
 
 import static java.util.Objects.isNull;
 import static no.nav.testnav.libs.data.pdlforvalter.v1.FolkeregisterPersonstatusDTO.FolkeregisterPersonstatus.BOSATT;
@@ -60,51 +58,53 @@ public class ArtifactGjeldendeService {
         }
     }
 
-    @Transactional
     public void setGjeldendeForRelasjon(String ident) {
 
+        setGjeldendeForIdent(ident);
+
         personRepository.findByIdent(ident)
-                .ifPresent(hovedperson -> {
-                    setGjeldendeForRelasjon(hovedperson.getPerson());
-                    hovedperson.getRelasjoner().stream()
-                            .map(DbRelasjon::getRelatertPerson)
-                            .filter(Objects::nonNull)
-                            .forEach(relasjon -> setGjeldendeForRelasjon(relasjon.getPerson()));
-                });
+                .ifPresent(hovedperson -> hovedperson.getRelasjoner().stream()
+                        .map(DbRelasjon::getRelatertPerson)
+                        .map(DbPerson::getIdent)
+                        .forEach(this::setGjeldendeForIdent));
     }
 
-    private void setGjeldendeForRelasjon(PersonDTO person) {
+    private void setGjeldendeForIdent(String ident) {
 
-        setSisteGjeldende(person.getFoedsel(), !person.isStatusIn(OPPHOERT));
-        setSisteGjeldende(person.getNavn(), !person.isStatusIn(OPPHOERT));
-        setSisteGjeldende(person.getKjoenn(), !person.isStatusIn(OPPHOERT));
-        setSisteGjeldende(person.getBostedsadresse(), !person.isStatusIn(OPPHOERT, IKKE_BOSATT, FORSVUNNET, DOED));
-        setAlleGjeldende(person.getKontaktadresse(), !person.isStatusIn(OPPHOERT, FORSVUNNET, DOED));
-        setAlleGjeldende(person.getOppholdsadresse(), !person.isStatusIn(OPPHOERT, FORSVUNNET, DOED));
-        setIngenNorskAdresse(person.getBostedsadresse(), person.isStatusIn(UTFLYTTET, MIDLERTIDIG, INAKTIV));
-        setIngenNorskAdresse(person.getOppholdsadresse(), person.isStatusIn(UTFLYTTET));
-        setIngenNorskAdresse(person.getKontaktadresse(), person.isStatusIn(UTFLYTTET));
-        setSisteGjeldende(person.getInnflytting(), !person.isStatusIn(OPPHOERT, UTFLYTTET));
-        setSisteGjeldende(person.getUtflytting(), !person.isStatusIn(OPPHOERT, BOSATT));
-        setSisteGjeldende(person.getDeltBosted(), !person.isStatusIn(OPPHOERT, DOED));
-        setSisteGjeldende(person.getForeldreansvar(), !person.isStatusIn(OPPHOERT));
-        setSisteGjeldende(person.getUtenlandskIdentifikasjonsnummer(), !person.isStatusIn(OPPHOERT));
-        setSisteGjeldende(person.getFalskIdentitet(), !person.isStatusIn(OPPHOERT));
-        setSisteGjeldende(person.getAdressebeskyttelse(), !person.isStatusIn(OPPHOERT));
-        setSisteGjeldende(person.getDoedsfall(), !person.isStatusIn(OPPHOERT));
-        setSisteGjeldende(person.getFolkeregisterPersonstatus(), true);
-        setSisteGjeldende(person.getTilrettelagtKommunikasjon(), !person.isStatusIn(OPPHOERT, DOED));
-        setAlleGjeldende(person.getStatsborgerskap(), !person.isStatusIn(OPPHOERT));
-        setAlleGjeldende(person.getForelderBarnRelasjon(), !person.isStatusIn(OPPHOERT));
-        setAlleGjeldende(person.getTelefonnummer(), !person.isStatusIn(OPPHOERT, DOED));
-        setSisteGjeldende(person.getOpphold(), !person.isStatusIn(OPPHOERT));
-        setSisteGjeldende(person.getSivilstand(), !person.isStatusIn(OPPHOERT));
-        setSisteGjeldende(person.getFullmakt(), !person.isStatusIn(OPPHOERT, DOED));
-        setSisteGjeldende(person.getVergemaal(), !person.isStatusIn(OPPHOERT));
-        setSisteGjeldende(person.getSikkerhetstiltak(), !person.isStatusIn(OPPHOERT, DOED));
-        setAlleGjeldende(person.getDoedfoedtBarn(), !person.isStatusIn(OPPHOERT));
-        setSisteGjeldende(person.getKontaktinformasjonForDoedsbo(), person.isStatusIn(DOED));
-        setAlleGjeldende(person.getNyident(), false);
+          personRepository.findByIdent(ident)
+                        .ifPresent(dbPerson -> {
 
+                            var person = dbPerson.getPerson();
+                            setSisteGjeldende(person.getFoedsel(), !person.isStatusIn(OPPHOERT));
+                            setSisteGjeldende(person.getNavn(), !person.isStatusIn(OPPHOERT));
+                            setSisteGjeldende(person.getKjoenn(), !person.isStatusIn(OPPHOERT));
+                            setSisteGjeldende(person.getBostedsadresse(), !person.isStatusIn(OPPHOERT, IKKE_BOSATT, FORSVUNNET, DOED));
+                            setAlleGjeldende(person.getKontaktadresse(), !person.isStatusIn(OPPHOERT, FORSVUNNET, DOED));
+                            setAlleGjeldende(person.getOppholdsadresse(), !person.isStatusIn(OPPHOERT, FORSVUNNET, DOED));
+                            setIngenNorskAdresse(person.getBostedsadresse(), person.isStatusIn(UTFLYTTET, MIDLERTIDIG, INAKTIV));
+                            setIngenNorskAdresse(person.getOppholdsadresse(), person.isStatusIn(UTFLYTTET));
+                            setIngenNorskAdresse(person.getKontaktadresse(), person.isStatusIn(UTFLYTTET));
+                            setSisteGjeldende(person.getInnflytting(), !person.isStatusIn(OPPHOERT, UTFLYTTET));
+                            setSisteGjeldende(person.getUtflytting(), !person.isStatusIn(OPPHOERT, BOSATT));
+                            setSisteGjeldende(person.getDeltBosted(), !person.isStatusIn(OPPHOERT, DOED));
+                            setSisteGjeldende(person.getForeldreansvar(), !person.isStatusIn(OPPHOERT));
+                            setSisteGjeldende(person.getUtenlandskIdentifikasjonsnummer(), !person.isStatusIn(OPPHOERT));
+                            setSisteGjeldende(person.getFalskIdentitet(), !person.isStatusIn(OPPHOERT));
+                            setSisteGjeldende(person.getAdressebeskyttelse(), !person.isStatusIn(OPPHOERT));
+                            setSisteGjeldende(person.getDoedsfall(), !person.isStatusIn(OPPHOERT));
+                            setSisteGjeldende(person.getFolkeregisterPersonstatus(), true);
+                            setSisteGjeldende(person.getTilrettelagtKommunikasjon(), !person.isStatusIn(OPPHOERT, DOED));
+                            setAlleGjeldende(person.getStatsborgerskap(), !person.isStatusIn(OPPHOERT));
+                            setAlleGjeldende(person.getForelderBarnRelasjon(), !person.isStatusIn(OPPHOERT));
+                            setAlleGjeldende(person.getTelefonnummer(), !person.isStatusIn(OPPHOERT, DOED));
+                            setSisteGjeldende(person.getOpphold(), !person.isStatusIn(OPPHOERT));
+                            setSisteGjeldende(person.getSivilstand(), !person.isStatusIn(OPPHOERT));
+                            setSisteGjeldende(person.getFullmakt(), !person.isStatusIn(OPPHOERT, DOED));
+                            setSisteGjeldende(person.getVergemaal(), !person.isStatusIn(OPPHOERT));
+                            setSisteGjeldende(person.getSikkerhetstiltak(), !person.isStatusIn(OPPHOERT, DOED));
+                            setAlleGjeldende(person.getDoedfoedtBarn(), !person.isStatusIn(OPPHOERT));
+                            setSisteGjeldende(person.getKontaktinformasjonForDoedsbo(), person.isStatusIn(DOED));
+                            setAlleGjeldende(person.getNyident(), false);
+                        });
     }
 }
