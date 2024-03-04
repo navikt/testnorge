32b46e593b96d3b96d196704008aa32b7f4d54b0
Betsy Carina Traran <betsy.carina.traran@nav.no>
2024-03-04 14:59:45 +0100

    Fix import med attributter

--- apps/dolly-frontend/src/main/js/src/pages/tenorSoek/resultatVisning/ImporterPerson.tsx
+++ apps/dolly-frontend/src/main/js/src/pages/tenorSoek/resultatVisning/ImporterPerson.tsx
@@ -3,10 +3,12 @@ import { useNaviger } from '@/utils/hooks/useNaviger'
 import { Button } from '@navikt/ds-react'
 import { CypressSelector } from '../../../../cypress/mocks/Selectors'
 import { ArrowRightIcon } from '@navikt/aksel-icons'
+import { usePdlPersonbolk } from '@/utils/hooks/usePdlPerson'
 
 export const ImporterPerson = ({ valgtPerson }) => {
 	const navigate = useNavigate()
 	const { result, loading } = useNaviger(valgtPerson)
+	const { pdlPersoner, loading: loadingPdl, error: errorPdl } = usePdlPersonbolk(valgtPerson)
 
 	// TODO vis loading?
 	if (result) {
@@ -14,10 +16,19 @@ export const ImporterPerson = ({ valgtPerson }) => {
 	}
 
 	const handleClick = (event) => {
-		event.stopPropagation()
+		// event.stopPropagation()
 		navigate(`/importer`, {
 			state: {
-				importPersoner: [valgtPerson],
+				importPersoner: [
+					{
+						ident: valgtPerson,
+						data: {
+							hentPerson: pdlPersoner?.hentPersonBolk?.find((p) => p.ident === valgtPerson)?.person,
+							hentIdenter: pdlPersoner?.hentIdenterBolk?.find((p) => p.ident === valgtPerson)
+								?.identer,
+						},
+					},
+				],
 				// mal: mal,
 				// gruppe: gruppe,
 			},
@@ -30,8 +41,9 @@ export const ImporterPerson = ({ valgtPerson }) => {
 			variant="tertiary"
 			size="xsmall"
 			icon={<ArrowRightIcon />}
-			loading={loading}
+			loading={loading || loadingPdl}
 			onClick={handleClick}
+			style={{ minWidth: '150px' }}
 		>
 			Importer til gruppe
 		</Button>
