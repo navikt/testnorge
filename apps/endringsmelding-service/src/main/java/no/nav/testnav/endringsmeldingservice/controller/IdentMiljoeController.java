c3336f6a7815410561841f8e41a53d1c8bee0eea
stigus <stian.gustavsson@nav.no>
2024-03-07 08:41:55 +0100

    Liten refaktor av endringsmelding-service #deploy-endringsmelding-frontend

--- apps/endringsmelding-service/src/main/java/no/nav/testnav/endringsmeldingservice/controller/IdentMiljoeController.java
+++ apps/endringsmelding-service/src/main/java/no/nav/testnav/endringsmeldingservice/controller/IdentMiljoeController.java
@@ -3,20 +3,20 @@ package no.nav.testnav.endringsmeldingservice.controller;
 import lombok.RequiredArgsConstructor;
 import no.nav.testnav.endringsmeldingservice.consumer.TpsMessagingConsumer;
 import no.nav.testnav.libs.data.tpsmessagingservice.v1.PersonMiljoeDTO;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
 @RestController
-@RequestMapping("/api/v1/identer/{ident}/miljoer")
+@RequestMapping("/api/v1/ident")
 @RequiredArgsConstructor
 public class IdentMiljoeController {
     private final TpsMessagingConsumer tpsMessagingConsumer;
 
-    @GetMapping
-    public Flux<PersonMiljoeDTO> getMiljoer(@PathVariable String ident) {
+    @PostMapping
+    public Flux<PersonMiljoeDTO> identFinnesIMiljoer(@RequestBody String ident) {
         return tpsMessagingConsumer.hentMiljoer(ident);
     }
 
