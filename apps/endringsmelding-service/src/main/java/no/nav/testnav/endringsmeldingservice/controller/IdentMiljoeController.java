c802229cc52496fd5e8379d1a4586155554e46d7
stigus <stian.gustavsson@nav.no>
2024-03-07 08:54:12 +0100

    Bruker Get med RequestParam #deploy-endringsmelding-service

--- apps/endringsmelding-service/src/main/java/no/nav/testnav/endringsmeldingservice/controller/IdentMiljoeController.java
+++ apps/endringsmelding-service/src/main/java/no/nav/testnav/endringsmeldingservice/controller/IdentMiljoeController.java
@@ -4,9 +4,9 @@ import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import no.nav.testnav.endringsmeldingservice.consumer.TpsMessagingConsumer;
 import no.nav.testnav.libs.data.tpsmessagingservice.v1.PersonMiljoeDTO;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
@@ -16,9 +16,9 @@ import reactor.core.publisher.Flux;
 public class IdentMiljoeController {
     private final TpsMessagingConsumer tpsMessagingConsumer;
 
-    @PostMapping("/miljoer")
+    @GetMapping("/miljoer")
     @Operation(description = "Sjekk om ident finnes i miljøer")
-    public Flux<PersonMiljoeDTO> identFinnesIMiljoer(@RequestBody String ident) {
+    public Flux<PersonMiljoeDTO> identFinnesIMiljoer(@RequestParam String ident) {
         return tpsMessagingConsumer.hentMiljoer(ident);
     }
 
