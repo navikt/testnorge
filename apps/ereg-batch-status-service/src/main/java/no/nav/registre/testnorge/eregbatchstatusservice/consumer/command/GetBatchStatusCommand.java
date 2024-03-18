0376c3f85c2e619c4c8c32fff373409fc0666653
stigus <stian.gustavsson@nav.no>
2024-03-18 14:41:46 +0100

    Mer logging og svarer med 404 dersom kall ikke kommer gjennom, slik at org-service ikke blir stuck i loop #deploy-ereg-batch-status-service

--- apps/ereg-batch-status-service/src/main/java/no/nav/registre/testnorge/eregbatchstatusservice/consumer/command/GetBatchStatusCommand.java
+++ apps/ereg-batch-status-service/src/main/java/no/nav/registre/testnorge/eregbatchstatusservice/consumer/command/GetBatchStatusCommand.java
@@ -3,7 +3,11 @@ package no.nav.registre.testnorge.eregbatchstatusservice.consumer.command;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.registre.testnorge.eregbatchstatusservice.util.WebClientFilter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.server.ResponseStatusException;
+import reactor.core.publisher.Mono;
 import reactor.util.retry.Retry;
 
 import java.time.Duration;
@@ -21,11 +25,19 @@ public class GetBatchStatusCommand implements Callable<Long> {
                 .get()
                 .uri(builder -> builder.path("/ereg/internal/batch/poll/{id}").build(id))
                 .retrieve()
+                .onStatus(HttpStatusCode::is4xxClientError, clientResponse -> {
+                    if (clientResponse.statusCode().equals(HttpStatus.UNAUTHORIZED)) {
+                        log.error("Unauthorized error occurred when calling EREG");
+                        return Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found"));
+                    }
+                    log.error("Client error occurred when calling EREG");
+                    return Mono.error(new ResponseStatusException(clientResponse.statusCode(), "Client error occurred"));
+                })
                 .bodyToMono(Long.class)
                 .retryWhen(Retry.backoff(3, Duration.ofSeconds(5))
                         .filter(WebClientFilter::is5xxException))
                 .block();
-        log.info("Fikk statuskode: " + response);
+        log.info("Mottok statuskode fra Batch-Ereg: " + response);
         return response;
     }
 }
