21171e91613a509a1e6a1747110105d17e6f5edf
kristenhaerum <kristen.herum@nav.no>
2024-03-18 15:59:58 +0100

    Oppdatert byggescript, JAXB trivia

--- apps/skattekort-service/src/main/java/no/nav/skattekortservice/config/AppConfig.java
+++ apps/skattekort-service/src/main/java/no/nav/skattekortservice/config/AppConfig.java
@@ -1,7 +1,5 @@
 package no.nav.skattekortservice.config;
 
-import no.nav.testnav.libs.database.config.FlywayConfiguration;
-import no.nav.testnav.libs.database.config.VaultHikariConfiguration;
 import no.nav.testnav.libs.servletcore.config.ApplicationCoreConfig;
 import no.nav.testnav.libs.servletsecurity.config.SecureOAuth2ServerToServerConfiguration;
 import org.springframework.context.annotation.Configuration;
@@ -9,8 +7,6 @@ import org.springframework.context.annotation.Import;
 
 @Configuration
 @Import({ ApplicationCoreConfig.class,
-        FlywayConfiguration.class,
-        VaultHikariConfiguration.class,
         SecureOAuth2ServerToServerConfiguration.class
 })
 public class AppConfig {
