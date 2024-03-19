633198d4c26dbad40d4da39770d11e3212b2817c
stigus <stian.gustavsson@nav.no>
2024-03-19 08:48:58 +0100

    * Skrevet om ereg-batch-status-service til reactive * Bytter fra nav til trygdeetaten tokenservice #deploy-ereg-batch-status-service

--- apps/ereg-batch-status-service/src/main/java/no/nav/registre/testnorge/eregbatchstatusservice/config/ApplicationConfig.java
+++ apps/ereg-batch-status-service/src/main/java/no/nav/registre/testnorge/eregbatchstatusservice/config/ApplicationConfig.java
@@ -1,14 +1,14 @@
 package no.nav.registre.testnorge.eregbatchstatusservice.config;
 
+import no.nav.testnav.libs.reactivecore.config.CoreConfig;
+import no.nav.testnav.libs.reactivesecurity.config.SecureOAuth2ServerToServerConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
-import no.nav.testnav.libs.servletcore.config.ApplicationCoreConfig;
-import no.nav.testnav.libs.servletsecurity.config.SecureOAuth2ServerToServerConfiguration;
 
 @Configuration
 @Import({
-        ApplicationCoreConfig.class,
+        CoreConfig.class,
         SecureOAuth2ServerToServerConfiguration.class
 })
 public class ApplicationConfig {
