7e334934fdc0fe6e266f7b34ac1f16b55ad2577d
kristenhaerum <kristen.herum@nav.no>
2024-04-02 09:22:29 +0200

    Refactor Kodeverk implementation to use testnav-kodeverk-service #deploy-test-dolly-backend
    Revised Kodeverk implementation to utilize testnav-kodeverk-service instead of kodeverk-api. This involved changes to consumers configuration, property files, and KodeverkConsumer. Corresponding Mapper and Controller methods were removed, and unused test cases were deleted. This change is aimed at simplifying the codebase and removing redundant dependencies.

--- apps/dolly-backend/src/main/java/no/nav/dolly/config/Consumers.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/config/Consumers.java
@@ -9,14 +9,6 @@ import org.springframework.context.annotation.Configuration;
 
 import static lombok.AccessLevel.PACKAGE;
 
-/**
- * Samler alle placeholders for ulike {@code consumers.*}-konfigurasjon her, dvs. subklasser av {@code ServerProperties}.
- * <br/><br/>
- * Husk at Spring Boot bruker <a href="https://docs.spring.io/spring-boot/docs/current/reference/html/features.html#features.external-config.typesafe-configuration-properties.relaxed-binding">relaxed binding</a>
- * mellom configuration properties og field names.
- *
- * @see ServerProperties
- */
 @Configuration
 @ConfigurationProperties(prefix = "consumers")
 @NoArgsConstructor(access = PACKAGE)
@@ -34,10 +26,11 @@ public class Consumers {
     private ServerProperties testnavInntektsmeldingService;
     private ServerProperties testnavInntektstubProxy;
     private ServerProperties testnavInstProxy;
-    private ServerProperties kodeverkApi;
+    private ServerProperties testnavKodeverkService;
     private ServerProperties testnavKontoregisterPersonProxy;
     private ServerProperties testnavKrrstubProxy;
     private ServerProperties testnavMedlProxy;
+    private ServerProperties testnavMiljoerService;
     private ServerProperties testnavNorg2Proxy;
     private ServerProperties testnavOrganisasjonForvalter;
     private ServerProperties testnavOrganisasjonService;
@@ -50,7 +43,5 @@ public class Consumers {
     private ServerProperties testnavSykemeldingApi;
     private ServerProperties testnavSyntSykemeldingApi;
     private ServerProperties testnavTpsMessagingService;
-    private ServerProperties testnavMiljoerService;
     private ServerProperties testnavUdistubProxy;
-
 }
