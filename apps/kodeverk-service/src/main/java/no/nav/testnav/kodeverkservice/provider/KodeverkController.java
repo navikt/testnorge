58f407221a817adcddb2843fd7199af252633e49
Kristen Hærum <kristen.herum@nav.no>
2024-04-05 14:46:07 +0200

    Feature/kodeverk kommuner2024 (#3459)
    Lagt til nytt kodeverk, Kommuner2024, og referert til denne fra klientene

--- apps/kodeverk-service/src/main/java/no/nav/testnav/kodeverkservice/provider/KodeverkController.java
+++ apps/kodeverk-service/src/main/java/no/nav/testnav/kodeverkservice/provider/KodeverkController.java
@@ -3,6 +3,7 @@ package no.nav.testnav.kodeverkservice.provider;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import no.nav.testnav.kodeverkservice.service.KodeverkService;
+import no.nav.testnav.kodeverkservice.utility.KommunerUtility;
 import no.nav.testnav.libs.dto.kodeverkservice.v1.KodeverkAdjustedDTO;
 import no.nav.testnav.libs.dto.kodeverkservice.v1.KodeverkDTO;
 import org.springframework.cache.annotation.Cacheable;
@@ -16,6 +17,8 @@ import reactor.core.publisher.Mono;
 
 import static no.nav.testnav.kodeverkservice.config.CacheConfig.CACHE_KODEVERK;
 import static no.nav.testnav.kodeverkservice.config.CacheConfig.CACHE_KODEVERK_2;
+import static no.nav.testnav.kodeverkservice.utility.KommunerUtility.KOMMUNER;
+import static no.nav.testnav.kodeverkservice.utility.KommunerUtility.KOMMUNER2024;
 
 @RestController
 @RequestMapping("/api/v1/kodeverk")
@@ -30,7 +33,12 @@ public class KodeverkController {
     @Operation(description = "Hent kodeverk, returnerer map")
     public Mono<KodeverkDTO> fetchKodeverk(@RequestParam String kodeverk) {
 
-        return kodeverkService.getKodeverkMap(kodeverk);
+        return !KOMMUNER2024.equals(kodeverk) ?
+
+                kodeverkService.getKodeverkMap(kodeverk) :
+
+                kodeverkService.getKodeverkMap(KOMMUNER)
+                        .map(KommunerUtility::filterKommuner2024);
     }
 
     @Cacheable(value = CACHE_KODEVERK_2, unless = "#result.koder?.size() == 0")
@@ -38,6 +46,11 @@ public class KodeverkController {
     @Operation(description = "Hent kodeverk etter kodeverkNavn")
     public Mono<KodeverkAdjustedDTO> getKodeverkByName(@PathVariable("kodeverkNavn") String kodeverkNavn) {
 
-        return kodeverkService.getKodeverkByName(kodeverkNavn);
+        return !KOMMUNER2024.equals(kodeverkNavn) ?
+
+                kodeverkService.getKodeverkByName(kodeverkNavn) :
+
+                kodeverkService.getKodeverkByName(KOMMUNER)
+                        .map(KommunerUtility::filterKommuner2024);
     }
 }
