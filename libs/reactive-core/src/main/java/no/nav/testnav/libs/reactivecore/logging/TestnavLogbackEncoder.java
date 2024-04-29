9099fc5b23d19dfbcb132744650fa162a5877efd
stigus <stian.gustavsson@nav.no>
2024-04-29 16:31:04 +0200

    Refaktorert Logback funksjonalitet #deploy-test-frontend

--- libs/reactive-core/src/main/java/no/nav/testnav/libs/reactivecore/logging/TestnavLogbackEncoder.java
+++ libs/reactive-core/src/main/java/no/nav/testnav/libs/reactivecore/logging/TestnavLogbackEncoder.java
@@ -1,48 +1,62 @@
 package no.nav.testnav.libs.reactivecore.logging;
 
-import ch.qos.logback.classic.PatternLayout;
 import ch.qos.logback.classic.spi.ILoggingEvent;
-import ch.qos.logback.core.pattern.PatternLayoutEncoderBase;
+import ch.qos.logback.classic.spi.LoggingEvent;
+import ch.qos.logback.classic.spi.ThrowableProxy;
+import com.fasterxml.jackson.core.JsonGenerator;
+import net.logstash.logback.composite.JsonProvider;
+import net.logstash.logback.composite.loggingevent.MessageJsonProvider;
+import net.logstash.logback.encoder.LoggingEventCompositeJsonEncoder;
 
+import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class TestnavLogbackEncoder extends PatternLayoutEncoderBase<ILoggingEvent> {
+public class TestnavLogbackEncoder extends LoggingEventCompositeJsonEncoder {
     private final Pattern pattern = Pattern.compile("(?<!\\d)\\d{11}(?!\\d)");
 
-    @Override
-    public void start() {
-        TestnavPatternLayout patternLayout = new TestnavPatternLayout();
-        patternLayout.setContext(context);
-        patternLayout.setPattern(getPattern() != null ? getPattern() : "%-5relative %-5level %logger{35} - %msg%ex%n");
-        patternLayout.start();
-        this.layout = patternLayout;
-        super.start();
+    public TestnavLogbackEncoder() {
+        JsonProvider<ILoggingEvent> provider = new CustomMessageJsonProvider();
+        provider.setContext(getContext());
+        provider.start();
+        getProviders().addProvider(provider);
     }
 
-    private class TestnavPatternLayout extends PatternLayout {
+    private class CustomMessageJsonProvider extends MessageJsonProvider {
         @Override
-        protected String writeLoopOnConverters(ILoggingEvent event) {
-            String logEvent = super.writeLoopOnConverters(event);
-            Matcher matcher = pattern.matcher(logEvent);
+        public void writeTo(JsonGenerator generator, ILoggingEvent event) throws IOException {
+            String originalMessage = event.getFormattedMessage();
+            Matcher matcher = pattern.matcher(originalMessage);
 
-            if (!matcher.find()) {
-                return logEvent;
-            }
+            if (matcher.find()) {
+                StringBuilder result = new StringBuilder();
+                matcher.reset();
 
-            matcher.reset();
-            StringBuilder result = new StringBuilder();
-
-            while (matcher.find()) {
-                String match = matcher.group();
-                if (match.charAt(2) == '0' || match.charAt(2) == '1') {
-                    String replacement = match.substring(0, 6) + "xxxxx";
-                    matcher.appendReplacement(result, replacement);
+                while (matcher.find()) {
+                    String match = matcher.group();
+                    if (match.charAt(2) == '0' || match.charAt(2) == '1') {
+                        String replacement = match.substring(0, 6) + "xxxxx";
+                        matcher.appendReplacement(result, replacement);
+                    }
                 }
-            }
-            matcher.appendTail(result);
+                matcher.appendTail(result);
 
-            return result.toString();
+                LoggingEvent modifiedEvent = new LoggingEvent();
+                modifiedEvent.setLoggerName(event.getLoggerName());
+                modifiedEvent.setLoggerContextRemoteView(event.getLoggerContextVO());
+                modifiedEvent.setThreadName(event.getThreadName());
+                modifiedEvent.setLevel(event.getLevel());
+                modifiedEvent.setMessage(result.toString());
+                modifiedEvent.setArgumentArray(event.getArgumentArray());
+                if (event.getThrowableProxy() != null) {
+                    modifiedEvent.setThrowableProxy((ThrowableProxy) event.getThrowableProxy());
+                }
+                modifiedEvent.setTimeStamp(event.getTimeStamp());
+
+                super.writeTo(generator, modifiedEvent);
+            } else {
+                super.writeTo(generator, event);
+            }
         }
     }
 }
