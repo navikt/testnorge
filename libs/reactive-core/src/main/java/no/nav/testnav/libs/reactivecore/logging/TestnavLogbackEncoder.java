a39107ae42824cc9b37fc2eede8cc6950b90b585
Stian Gustavsson <stian.gustavsson@nav.no>
2024-04-30 11:03:41 +0200

    Bugfix/testnav logging (#3479)
    * Refaktorert Logback funksjonalitet til å displaye level og stacktrace osv, i påvente av ny funksjonalitet for maskering av logger

---
+++ libs/reactive-core/src/main/java/no/nav/testnav/libs/reactivecore/logging/TestnavLogbackEncoder.java
@@ -0,0 +1,8 @@
+package no.nav.testnav.libs.reactivecore.logging;
+
+import lombok.extern.slf4j.Slf4j;
+import net.logstash.logback.encoder.LogstashEncoder;
+
+@Slf4j
+public class TestnavLogbackEncoder extends LogstashEncoder {
+}
---
+++ libs/servlet-core/src/main/java/no/nav/testnav/libs/servletcore/logging/TestnavLogbackEncoder.java
@@ -0,0 +1,8 @@
+package no.nav.testnav.libs.servletcore.logging;
+
+import lombok.extern.slf4j.Slf4j;
+import net.logstash.logback.encoder.LogstashEncoder;
+
+@Slf4j
+public class TestnavLogbackEncoder extends LogstashEncoder {
+}
