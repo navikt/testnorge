cb5a8efe2794a15808c85d71c40cce71f0cc3b81
Betsy Carina Traran <betsy.carina.traran@nav.no>
2024-05-02 14:41:22 +0200

    Tilpass historiske data

--- apps/dolly-frontend/src/main/js/src/components/ui/historikk/ArrayHistorikk.tsx
+++ apps/dolly-frontend/src/main/js/src/components/ui/historikk/ArrayHistorikk.tsx
@@ -19,39 +19,42 @@ export const ArrayHistorikk = ({
 	const MainRedigerbar = componentRedigerbar
 	const historikkHeader = header !== '' ? header + ' historikk' : 'Historikk'
 
+	const getComponent = (element, idx) => {
+		const pdlfElement = pdlfData?.find((item) => item.hendelseId === element.metadata.opplysningsId)
+		if (element.metadata.master === 'PDL' && pdlfElement) {
+			return (
+				<MainRedigerbar
+					idx={idx}
+					data={pdlfElement}
+					alleData={pdlfData}
+					tmpData={tmpData}
+					tmpPersoner={tmpPersoner}
+					ident={ident}
+					identtype={identtype}
+				/>
+			)
+		}
+		return <Main idx={idx} data={element} />
+	}
+
 	return (
 		<div className="array-historikk">
 			{data?.length > 0 && (
 				<ErrorBoundary>
 					<DollyFieldArray data={data} header={header} nested>
 						{(element, idx) => {
-							const pdlfElement = pdlfData?.find(
-								(item) => item.hendelseId === element.metadata.opplysningsId,
-							)
-							if (element.metadata.master === 'PDL' && pdlfElement) {
-								return (
-									<MainRedigerbar
-										idx={idx}
-										data={pdlfElement}
-										alleData={pdlfData}
-										tmpData={tmpData}
-										tmpPersoner={tmpPersoner}
-										ident={ident}
-										identtype={identtype}
-									/>
-								)
-							}
-							return <Main idx={idx} data={element} />
+							return getComponent(element, idx)
 						}}
 					</DollyFieldArray>
 				</ErrorBoundary>
 			)}
-			{/*TODO: Hva gjør vi med historiske data?*/}
 			{historiskData?.length > 0 && (
 				<Panel heading={historikkHeader}>
 					<ErrorBoundary>
 						<DollyFieldArray data={historiskData} nested>
-							{(element, idx) => <Main idx={idx} data={element} />}
+							{(element, idx) => {
+								return getComponent(element, idx)
+							}}
 						</DollyFieldArray>
 					</ErrorBoundary>
 				</Panel>
