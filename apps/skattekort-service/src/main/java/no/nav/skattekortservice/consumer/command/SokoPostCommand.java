9fceb6c197a0a5221b98b3f5bddf1035a05803c0
kristenhaerum <kristen.herum@nav.no>
2024-05-07 14:00:58 +0200

    Added authorization and body to SokoPostCommand
    Updated the SokoPostCommand to include the endpoint URL, authorization token in the header, and request body. The WebClient now posts to the defined URL with provided authorization and request body, and retrieves a Mono of type SokosResponse.

--- apps/skattekort-service/src/main/java/no/nav/skattekortservice/consumer/command/SokoPostCommand.java
+++ apps/skattekort-service/src/main/java/no/nav/skattekortservice/consumer/command/SokoPostCommand.java
@@ -3,13 +3,19 @@ package no.nav.skattekortservice.consumer.command;
 import lombok.RequiredArgsConstructor;
 import no.nav.skattekortservice.dto.SokosRequest;
 import no.nav.skattekortservice.dto.SokosResponse;
+import org.apache.hc.core5.http.HttpHeaders;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 import java.util.concurrent.Callable;
 
+import static org.apache.hc.core5.http.HttpHeaders.AUTHORIZATION;
+
 @RequiredArgsConstructor
 public class SokoPostCommand implements Callable<Mono<SokosResponse>> {
+
+    private static final String SOKOS_URL = "/skattekort/opprettskattekort";
 
     private final WebClient webClient;
     private final SokosRequest request;
@@ -20,6 +26,11 @@ public class SokoPostCommand implements Callable<Mono<SokosResponse>> {
 
         return webClient
                 .post()
-                .uri()
+                .uri(SOKOS_URL)
+                .header(AUTHORIZATION, "Bearer " + token)
+                .body(BodyInserters.fromValue(request))
+                .retrieve()
+                .bodyToMono(SokosResponse.class)
+
     }
 }
