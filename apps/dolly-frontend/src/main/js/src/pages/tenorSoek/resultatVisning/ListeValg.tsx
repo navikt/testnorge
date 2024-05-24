1890db97c71b8141ffff0bf93a71aec030f5cdbb
Betsy Carina Traran <betsy.carina.traran@nav.no>
2024-05-24 16:17:14 +0200

    Stop propagation paa checkbox label

--- apps/dolly-frontend/src/main/js/src/pages/tenorSoek/resultatVisning/ListeValg.tsx
+++ apps/dolly-frontend/src/main/js/src/pages/tenorSoek/resultatVisning/ListeValg.tsx
@@ -25,13 +25,8 @@ export const ListeValg = ({ person, markertePersoner, setMarkertePersoner }: Lis
 	return ibruk ? (
 		<NavigerTilPerson ident={id} />
 	) : (
-		<div style={{ margin: '-8px 0' }}>
-			<Checkbox
-				value={id}
-				size="small"
-				onChange={(val: any) => handleChangeCheckbox(val)}
-				onClick={(e) => e.stopPropagation()}
-			>
+		<div style={{ margin: '-8px 0', width: '100%' }} onClick={(e) => e.stopPropagation()}>
+			<Checkbox value={id} size="small" onChange={(val: any) => handleChangeCheckbox(val)}>
 				Importer person
 			</Checkbox>
 		</div>
