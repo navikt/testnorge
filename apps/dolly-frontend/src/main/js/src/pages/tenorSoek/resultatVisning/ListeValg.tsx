c66a26dcf0c9a99cf0d051269841283aee553657
Betsy Carina Traran <betsy.carina.traran@nav.no>
2024-05-27 15:46:18 +0200

    Fix sonar-issue

--- apps/dolly-frontend/src/main/js/src/pages/tenorSoek/resultatVisning/ListeValg.tsx
+++ apps/dolly-frontend/src/main/js/src/pages/tenorSoek/resultatVisning/ListeValg.tsx
@@ -25,7 +25,11 @@ export const ListeValg = ({ person, markertePersoner, setMarkertePersoner }: Lis
 	return ibruk ? (
 		<NavigerTilPerson ident={id} />
 	) : (
-		<div style={{ margin: '-8px 0' }} onClick={(e) => e.stopPropagation()}>
+		<div
+			style={{ margin: '-8px 0' }}
+			onClick={(e) => e.stopPropagation()}
+			onKeyDown={(e) => e.stopPropagation()}
+		>
 			<Checkbox value={id} size="small" onChange={(val: any) => handleChangeCheckbox(val)}>
 				Importer person
 			</Checkbox>
