2cf8f2403260683150098727eabc8418ce775ece
kristenhaerum <kristen.herum@nav.no>
2024-05-28 15:25:08 +0200

    Add OppdragRequest DTO class
    A new data transfer object (DTO) class called 'OppdragRequest' is introduced. This class is suited for the Oppdrag service and is equipped with Lombok annotations for data encapsulation, builder pattern support, no-args and all-args constructors.

---
+++ libs/data-transfer-objects/src/main/java/no/nav/testnav/libs/dto/oppdragservice/v1/OppdragRequest.java
@@ -0,0 +1,13 @@
+package no.nav.testnav.libs.dto.oppdragservice.v1;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OppdragRequest {
+}
