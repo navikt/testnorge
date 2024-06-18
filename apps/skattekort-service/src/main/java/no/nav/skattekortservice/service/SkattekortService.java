0ff046672fff3fd089dff5c55e6524f43031ffde
kristenhaerum <kristen.herum@nav.no>
2024-06-18 08:43:12 +0200

    Refactor SkattekortService to improve response handling #deploy-skattekort-service
    The SkattekortService code was updated to improve how Skattekort responses are processed. Previously, a SokosResponse object was used, but this was removed and replaced with more direct handling using Base64 decoding and Flux mapping. In addition, the unmarshal method was tweaked to directly return a List of Arbeidsgiver objects. Changes made aim to streamline code and increase performance.

--- apps/skattekort-service/src/main/java/no/nav/skattekortservice/service/SkattekortService.java
+++ apps/skattekort-service/src/main/java/no/nav/skattekortservice/service/SkattekortService.java
@@ -10,7 +10,6 @@ import no.nav.skattekortservice.consumer.SokosSkattekortConsumer;
 import no.nav.skattekortservice.dto.SkattekortRequest;
 import no.nav.skattekortservice.dto.SkattekortResponsIntermediate;
 import no.nav.skattekortservice.dto.SokosRequest;
-import no.nav.skattekortservice.dto.SokosResponse;
 import no.nav.skattekortservice.utility.SkattekortValidator;
 import no.nav.testnav.libs.dto.skattekortservice.v1.Arbeidsgiver;
 import no.nav.testnav.libs.dto.skattekortservice.v1.SkattekortRequestDTO;
@@ -23,6 +22,7 @@ import reactor.core.publisher.Mono;
 import java.io.StringWriter;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
+import java.util.List;
 
 @Slf4j
 @Service
@@ -63,13 +63,6 @@ public class SkattekortService {
                 .flatMap(skattekortConsumer::sendSkattekort);
     }
 
-//    public Mono<String> hentSkattekort(String ident, Integer inntektsaar) {
-//
-//        return skattekortConsumer.hentSkattekort(ident, inntektsaar)
-//                .doOnNext(response -> log.info("Hentet resultat fra Sokos {}", response))
-//                .map(data -> data.substring(data.indexOf("<?")));
-//    }
-
     @SneakyThrows
     private String marshallToXml(SkattekortRequest melding) {
 
@@ -90,39 +83,41 @@ public class SkattekortService {
     public Flux<SkattekortResponseDTO> hentSkattekort(String ident) {
 
 //        var xml = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><skattekortTilArbeidsgiver xmlns=\"urn:no:skatteetaten:fastsetting:formueinntekt:forskudd:skattekorttilarbeidsgiver:v3\"><arbeidsgiver><arbeidsgiveridentifikator><organisasjonsnummer>311734830</organisasjonsnummer></arbeidsgiveridentifikator><arbeidstaker><arbeidstakeridentifikator>19879098303</arbeidstakeridentifikator><resultatPaaForespoersel>skattekortopplysningerOK</resultatPaaForespoersel><skattekort><utstedtDato>2024-06-12</utstedtDato><skattekortidentifikator>121212</skattekortidentifikator><forskuddstrekk><trekkode>loennFraHovedarbeidsgiver</trekkode></forskuddstrekk></skattekort><tilleggsopplysning>oppholdPaaSvalbard</tilleggsopplysning><inntektsaar>2023</inntektsaar></arbeidstaker></arbeidsgiver></skattekortTilArbeidsgiver>";
+//        var base64encoded = "PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/Pjxza2F0dGVrb3J0VGlsQXJiZWlkc2dpdmVyIHhtbG5zPSJ1cm46bm86c2thdHRlZXRhdGVuOmZhc3RzZXR0aW5nOmZvcm11ZWlubnRla3Q6Zm9yc2t1ZGQ6c2thdHRla29ydHRpbGFyYmVpZHNnaXZlcjp2MyI+PGFyYmVpZHNnaXZlcj48YXJiZWlkc2dpdmVyaWRlbnRpZmlrYXRvcj48b3JnYW5pc2Fzam9uc251bW1lcj45MTA5NjI3Mjg8L29yZ2FuaXNhc2pvbnNudW1tZXI+PC9hcmJlaWRzZ2l2ZXJpZGVudGlmaWthdG9yPjxhcmJlaWRzdGFrZXI+PGFyYmVpZHN0YWtlcmlkZW50aWZpa2F0b3I+MDIxMTg0MDAxOTc8L2FyYmVpZHN0YWtlcmlkZW50aWZpa2F0b3I+PHJlc3VsdGF0UGFhRm9yZXNwb2Vyc2VsPnNrYXR0ZWtvcnRvcHBseXNuaW5nZXJPSzwvcmVzdWx0YXRQYWFGb3Jlc3BvZXJzZWw+PHNrYXR0ZWtvcnQ+PHV0c3RlZHREYXRvPjIwMjEtMDMtMDY8L3V0c3RlZHREYXRvPjxza2F0dGVrb3J0aWRlbnRpZmlrYXRvcj4xNTc5ODk8L3NrYXR0ZWtvcnRpZGVudGlmaWthdG9yPjxmb3Jza3VkZHN0cmVrayB4c2k6dHlwZT0iVHJla2t0YWJlbGwiIHhtbG5zOnhzaT0iaHR0cDovL3d3dy53My5vcmcvMjAwMS9YTUxTY2hlbWEtaW5zdGFuY2UiPjx0cmVra29kZT5pbnRyb2R1a3Nqb25zc3RvZW5hZDwvdHJla2tvZGU+PHRhYmVsbHR5cGU+dHJla2t0YWJlbGxGb3JQZW5zam9uPC90YWJlbGx0eXBlPjx0YWJlbGxudW1tZXI+NzEwMDwvdGFiZWxsbnVtbWVyPjxwcm9zZW50c2F0cz4yMjwvcHJvc2VudHNhdHM+PGFudGFsbE1hYW5lZGVyRm9yVHJla2s+MTE8L2FudGFsbE1hYW5lZGVyRm9yVHJla2s+PC9mb3Jza3VkZHN0cmVraz48Zm9yc2t1ZGRzdHJla2sgeHNpOnR5cGU9IlRyZWtrcHJvc2VudCIgeG1sbnM6eHNpPSJodHRwOi8vd3d3LnczLm9yZy8yMDAxL1hNTFNjaGVtYS1pbnN0YW5jZSI+PHRyZWtrb2RlPmxvZW5uRnJhSG92ZWRhcmJlaWRzZ2l2ZXI8L3RyZWtrb2RlPjxwcm9zZW50c2F0cz4zMjwvcHJvc2VudHNhdHM+PC9mb3Jza3VkZHN0cmVraz48Zm9yc2t1ZGRzdHJla2sgeHNpOnR5cGU9IlRyZWtrcHJvc2VudCIgeG1sbnM6eHNpPSJodHRwOi8vd3d3LnczLm9yZy8yMDAxL1hNTFNjaGVtYS1pbnN0YW5jZSI+PHRyZWtrb2RlPmxvZW5uRnJhQmlhcmJlaWRzZ2l2ZXI8L3RyZWtrb2RlPjxwcm9zZW50c2F0cz4zMjwvcHJvc2VudHNhdHM+PC9mb3Jza3VkZHN0cmVraz48L3NrYXR0ZWtvcnQ+PGlubnRla3RzYWFyPjIwMjE8L2lubnRla3RzYWFyPjwvYXJiZWlkc3Rha2VyPjwvYXJiZWlkc2dpdmVyPjwvc2thdHRla29ydFRpbEFyYmVpZHNnaXZlcj4=";
+//        var decodedXml = decodeRequest(base64encoded);
 //
-//        var unmarshalled = unmarshal(xml);
-//        var intermediate = mapperFacade.map(unmarshalled, SkattekortResponsIntermediate.class);
-//        var arbeidsgiver = mapperFacade.mapAsList(intermediate.getSkattekortTilArbeidsgiver().getArbeidsgiver(), Arbeidsgiver.class);
+//        var arbeidsgivere = unmarshal(decodedXml);
+//
 //        return Flux.just(SkattekortResponseDTO.builder()
 //                .ident(ident)
 //                .inntektsaar("2023")
-//                .arbeidsgiver(arbeidsgiver)
+//                .arbeidsgiver(arbeidsgivere)
 //                .skattekortXml(xml)
 //                .build());
 
         return skattekortConsumer.hentSkattekort(ident)
                 .doOnNext(response -> log.info("Hentet resultat fra Sokos {}", response))
-                .map(this::unwrapResponse);
+                .map(response -> Flux.just(decodeRequest(response.getSkattekort()))
+                        .map(this::unmarshal)
+                        .map(arbeidsgivere -> SkattekortResponseDTO.builder()
+                                .ident(response.getFnr())
+                                .inntektsaar(response.getInntektsar())
+                                .arbeidsgiver(arbeidsgivere)
+                                .skattekortXml(response.getSkattekort())
+                                .build()))
+                .flatMap(Flux::from);
     }
 
-    private SkattekortResponseDTO unwrapResponse(SokosResponse response) {
+    private String decodeRequest(String request) {
 
-        var unmarshalled = unmarshal(response.getSkattekort());
-        var intermediate = mapperFacade.map(unmarshalled, SkattekortResponsIntermediate.class);
-        var arbeidsgiver = mapperFacade.mapAsList(intermediate.getSkattekortTilArbeidsgiver().getArbeidsgiver(), Arbeidsgiver.class);
-        return SkattekortResponseDTO.builder()
-                .ident(response.getFnr())
-                .inntektsaar(response.getInntektsar())
-                .arbeidsgiver(arbeidsgiver)
-                .skattekortXml(response.getSkattekort())
-                .build();
+        return new String(Base64.getDecoder().decode(request), StandardCharsets.UTF_8);
     }
 
     @SneakyThrows
-    private SkattekortResponsIntermediate unmarshal(String xmlData) {
+    private List<Arbeidsgiver> unmarshal(String xmlData) {
 
         var jsonRoot = XML.toJSONObject(xmlData);
-        return objectMapper.readValue(jsonRoot.toString(), SkattekortResponsIntermediate.class);
+        var intermediate = objectMapper.readValue(jsonRoot.toString(), SkattekortResponsIntermediate.class);
+        return mapperFacade.mapAsList(intermediate.getSkattekortTilArbeidsgiver().getArbeidsgiver(), Arbeidsgiver.class);
     }
 }
