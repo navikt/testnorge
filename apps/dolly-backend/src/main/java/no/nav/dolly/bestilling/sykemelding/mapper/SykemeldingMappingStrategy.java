e2423398972d747dae12c2f2ba6b1ee923e2ded8
kristenhaerum <kristen.herum@nav.no>
2024-06-18 08:07:32 +0200

    Refactor birth data fetching in PdlPerson
    The previous handling of 'foedselsdato' in the PdlPerson class was replaced with a more streamlined approach. This refactoring includes changes in the fetching and setting of birth details across multiple methods and services to ensure consistency.

--- apps/dolly-backend/src/main/java/no/nav/dolly/bestilling/sykemelding/mapper/SykemeldingMappingStrategy.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/bestilling/sykemelding/mapper/SykemeldingMappingStrategy.java
@@ -103,7 +103,9 @@ public class SykemeldingMappingStrategy implements MappingStrategy {
                                 .findFirst().orElse(new PdlPerson.Navn()), pasient);
                         pasient.setAdresse(mapperFacade.map(person.getPerson().getBostedsadresse().stream()
                                 .findFirst().orElse(new BostedadresseDTO()), Adresse.class, context));
-                        pasient.setFoedselsdato(person.getPerson().getFoedselsdato().getFirst().getFoedselsdato());
+                        pasient.setFoedselsdato(person.getPerson().getFoedselsdato().stream()
+                                .map(PdlPerson.Foedselsdato::getFoedselsdato)
+                                .findFirst().orElse(null));
 
                         pasient.setTelefon(person.getPerson().getTelefonnummer().stream()
                                 .filter(telefonnummer -> telefonnummer.getPrioritet() == 1)
