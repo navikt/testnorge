ccb649e70ecbffdcea2808d8312730c1dc1197fa
stigus <stian.gustavsson@nav.no>
2024-06-20 08:37:06 +0200

    * Fix for identtype som ble vist feil i bestillingsdetaljer #deploy-test-frontend

--- apps/dolly-frontend/src/main/js/src/components/bestilling/sammendrag/kriterier/BestillingKriterieMapper.tsx
+++ apps/dolly-frontend/src/main/js/src/components/bestilling/sammendrag/kriterier/BestillingKriterieMapper.tsx
@@ -65,7 +65,7 @@ const mapBestillingsinformasjon = (
 		if (parseInt(firstIdent?.charAt(2)) < 4) {
 			return 'Standard'
 		}
-		return parseInt(firstIdent?.charAt(2)) > 5 ? 'NAV-syntetisk' : 'Test-Norge'
+		return parseInt(firstIdent?.charAt(2)) > 5 ? 'Test-Norge' : 'NAV-syntetisk'
 	}
 
 	if (bestillingsinformasjon) {
