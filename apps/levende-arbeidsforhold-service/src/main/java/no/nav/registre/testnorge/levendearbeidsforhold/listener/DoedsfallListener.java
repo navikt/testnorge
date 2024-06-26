85a0a53eb7847e1b61210a4d938cbb6f3ae397ee
Andrea Devold Fjeld <solveig.andrea.devold.fjeld@nav.no>
2024-06-26 14:00:59 +0200

    Slettet ting som var til generer navn service

--- apps/levende-arbeidsforhold-service/src/main/java/no/nav/registre/testnorge/levendearbeidsforhold/listener/DoedsfallListener.java
+++ apps/levende-arbeidsforhold-service/src/main/java/no/nav/registre/testnorge/levendearbeidsforhold/listener/DoedsfallListener.java
@@ -2,11 +2,18 @@ package no.nav.registre.testnorge.levendearbeidsforhold.listener;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import no.nav.testnav.libs.avro.organisasjon.v1.Opprettelsesdokument;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactoryFriend;
 import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import no.nav.registre.testnorge.levendearbeidsforhold.config.KafkaConfig;
 
 import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Profile("dev")
@@ -16,7 +23,15 @@ public class DoedsfallListener {
     private static final String doedsfallTopic = "pdl.leesah-v1";
 
     @KafkaListener(topics = doedsfallTopic)
-    public void getHendelser() {
+    public void getHendelser(List<ConsumerRecord<String, Object>> records) {
+        System.out.println(records.stream().collect().toList());
 
+        /*
+                records.stream().collect(Collectors.groupingBy(value -> value.value().getMetadata().getMiljo())).forEach((miljo, list) -> {
+            var organisasjoner = list.stream().map(value -> value.value().getOrganisasjon()).collect(Collectors.toList());
+            var ids = list.stream().map(ConsumerRecord::key).collect(Collectors.toSet());
+            save(organisasjoner, miljo, false, ids);
+        });
+         */
     }
 }
