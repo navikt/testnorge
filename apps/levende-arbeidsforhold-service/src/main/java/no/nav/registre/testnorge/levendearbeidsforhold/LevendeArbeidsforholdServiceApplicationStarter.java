0599f1c3a5e8545b63140f9a7dc3390a24fc167b
Andrea Devold Fjeld <solveig.andrea.devold.fjeld@nav.no>
2024-06-27 13:43:54 +0200

    Deploy #deploy-levende-arbeidsforhold-service

--- apps/levende-arbeidsforhold-service/src/main/java/no/nav/registre/testnorge/levendearbeidsforhold/LevendeArbeidsforholdServiceApplicationStarter.java
+++ apps/levende-arbeidsforhold-service/src/main/java/no/nav/registre/testnorge/levendearbeidsforhold/LevendeArbeidsforholdServiceApplicationStarter.java
@@ -2,11 +2,6 @@ package no.nav.registre.testnorge.levendearbeidsforhold;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
-
-import no.nav.registre.testnorge.levendearbeidsforhold.listener.DoedsfallListener;
 
 
 @SpringBootApplication
@@ -16,3 +11,4 @@ public class LevendeArbeidsforholdServiceApplicationStarter {
         SpringApplication.run(LevendeArbeidsforholdServiceApplicationStarter.class, args);
     }
 }
+
