150412bed0d0fe1728fe63ccb4656c104708506a
Andrea Devold Fjeld <solveig.andrea.devold.fjeld@nav.no>
2024-07-01 14:18:43 +0200

    Se om consumer fungerer nåt deployet #deploy-levende-arbeidsforhold-service

--- apps/levende-arbeidsforhold-service/src/main/java/no/nav/registre/testnorge/levendearbeidsforhold/service/ArbeidsforholdService.java
+++ apps/levende-arbeidsforhold-service/src/main/java/no/nav/registre/testnorge/levendearbeidsforhold/service/ArbeidsforholdService.java
@@ -1,2 +1,36 @@
 package no.nav.registre.testnorge.levendearbeidsforhold.service;
 
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import no.nav.registre.testnorge.levendearbeidsforhold.consumers.HentArbeidsforholdConsumer;
+import no.nav.testnav.libs.dto.ameldingservice.v1.ArbeidsforholdDTO;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Service for å sjekke arbeidsforhold.
+ * Henter arbeidsforhold fra Aareg og logger informasjonen.
+ * @see no.nav.registre.testnorge.levendearbeidsforhold.consumers.HentArbeidsforholdConsumer
+ */
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ArbeidsforholdService {
+    private final HentArbeidsforholdConsumer aaregArbeidsforholdConsumer;
+    @EventListener(ApplicationReadyEvent.class)
+    public void sjekkArbeidsforhold(String id) {
+        id = "30447515845";
+        List<ArbeidsforholdDTO> arbeidsforhold = aaregArbeidsforholdConsumer.getArbeidsforhold(id);
+        if (arbeidsforhold != null) {
+            log.info("Arbeidsforhold funnet: {}", arbeidsforhold);
+        } else {
+            log.warn("Fant ikke arbeidsforhold med id: {}", id);
+        }
+    }
+
+}
+//Les i appen ArbeidsforholdService
