ea3acaced31580a9c06445f100cf9cbede7ec34c
Andrea Devold Fjeld <solveig.andrea.devold.fjeld@nav.no>
2024-07-01 15:36:48 +0200

    Se om jeg får kjørt consumer #deploy

--- apps/levende-arbeidsforhold-service/src/main/java/no/nav/registre/testnorge/levendearbeidsforhold/service/Test.java
+++ apps/levende-arbeidsforhold-service/src/main/java/no/nav/registre/testnorge/levendearbeidsforhold/service/Test.java
@@ -2,9 +2,11 @@ package no.nav.registre.testnorge.levendearbeidsforhold.service;
 
 import no.nav.registre.testnorge.levendearbeidsforhold.config.Consumers;
 import no.nav.registre.testnorge.levendearbeidsforhold.consumers.HentArbeidsforholdConsumer;
+import no.nav.registre.testnorge.levendearbeidsforhold.consumers.command.HentArbeidsforholdCommand;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class Test {
@@ -12,8 +14,8 @@ public class Test {
 
     @EventListener(ApplicationReadyEvent.class)
     public void sjekkArbeidsforhold() {
-        ArbeidsforholdService arbeidsforholdService = new ArbeidsforholdService(new HentArbeidsforholdConsumer(
-                new Consumers().getAaregConsumer()
-        ));
+        WebClient webClient = WebClient.create();
+
+
     }
 }
