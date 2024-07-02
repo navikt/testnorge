eac246912637fe052779378b22a8f51b59544b80
Andrea Devold Fjeld <solveig.andrea.devold.fjeld@nav.no>
2024-07-02 10:27:22 +0200

    Forandret fra å logge token #deploy-levende-arbeidsforhold-service

--- apps/levende-arbeidsforhold-service/src/main/java/no/nav/registre/testnorge/levendearbeidsforhold/consumers/HentArbeidsforholdConsumer.java
+++ apps/levende-arbeidsforhold-service/src/main/java/no/nav/registre/testnorge/levendearbeidsforhold/consumers/HentArbeidsforholdConsumer.java
@@ -1,10 +1,13 @@
 package no.nav.registre.testnorge.levendearbeidsforhold.consumers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import no.nav.registre.testnorge.levendearbeidsforhold.consumers.command.HentArbeidsforholdCommand;
 import no.nav.testnav.libs.dto.ameldingservice.v1.ArbeidsforholdDTO;
 import no.nav.testnav.libs.securitycore.domain.ServerProperties;
+import no.nav.testnav.libs.securitycore.domain.Token;
 import no.nav.testnav.libs.servletsecurity.exchange.TokenExchange;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.codec.json.Jackson2JsonDecoder;
 import org.springframework.http.codec.json.Jackson2JsonEncoder;
@@ -18,31 +21,23 @@ import java.util.List;
 
 import static java.util.Objects.nonNull;
 
-/**
- * Consumer for å hente arbeidsforhold fra AAREG
- * WebClient for å hente data fra AAREG
- * TokenExchange for å hente token fra STS
- * ObjectMapper for å mappe json til objekter
- */
+@Slf4j
 @Component
 public class HentArbeidsforholdConsumer {
     private final WebClient webClient;
     private final ServerProperties serverProperties;
     private final TokenExchange tokenExchange;
 
-    /**
-     * Konstruktør
-     * @param consumers er ett objekt som inneholder serverProperties som finnes i application.yml
-     * @param tokenExchange er ett objekt som henter token fra STS
-     * @param objectMapper er ett objekt som mapper json til objekter
-     */
     public HentArbeidsforholdConsumer(
-            Consumers consumers,
+            Consumers consumers, //TokenExchange tokenExchange
             TokenExchange tokenExchange,
             ObjectMapper objectMapper) {
-        // Henter serverProperties fra consumers
-        serverProperties = consumers.getTestnavAaregProxy();
         this.tokenExchange = tokenExchange;
+        // Henter serverProperties fra consumers
+
+        serverProperties = consumers.getTestnavLevendeArbeidsforholdService();
+        log.info("ServerProperties: {}", serverProperties.toString());
+        //this.tokenExchange = tokenExchange;
         //Bygger en ExchangeStratefies for å mappe json til objekter
         ExchangeStrategies exchangeStrategies = ExchangeStrategies
                 .builder()
@@ -58,7 +53,7 @@ public class HentArbeidsforholdConsumer {
         this.webClient = WebClient
                 .builder()
                 .exchangeStrategies(exchangeStrategies)
-                .baseUrl("serverProperties.getUrl()") //For lokal kjøring bytt ut med https://aareg-services-q2.intern.nav.no
+                .baseUrl(serverProperties.getUrl()) //For lokal kjøring bytt ut med https://aareg-services-q2.intern.nav.no
                 .build();
         log.info("WebClient: {}", webClient.toString());
     }
@@ -68,10 +63,10 @@ public class HentArbeidsforholdConsumer {
      * @param ident FNR/DNR/aktør id til arbeidstakeren
      * @return Hvis arbeidsforhold finnes for identen vil en liste med ArbeidsforholdDTO bli returnert, ellers en tom liste
      */
-    public List<ArbeidsforholdDTO> getArbeidsforhold(String ident) {
-        var token = tokenExchange.exchange(serverProperties).block();
+    public List<ArbeidsforholdDTO> getArbeidsforhold(String ident, Token token) {
+        //var tokenX = tokenExchange.exchange(serverProperties).block();
         if(nonNull(token)) {
-            return new HentArbeidsforholdCommand(webClient, token.getTokenValue(), ident).call();
+            return new HentArbeidsforholdCommand(webClient, token.getAccessTokenValue(), ident).call();
         }
         return new ArrayList<>();
     }
--- apps/levende-arbeidsforhold-service/src/main/java/no/nav/registre/testnorge/levendearbeidsforhold/service/ArbeidsforholdService.java
+++ apps/levende-arbeidsforhold-service/src/main/java/no/nav/registre/testnorge/levendearbeidsforhold/service/ArbeidsforholdService.java
@@ -7,17 +7,25 @@ import lombok.extern.slf4j.Slf4j;
 import no.nav.registre.testnorge.levendearbeidsforhold.config.Consumers;
 import no.nav.registre.testnorge.levendearbeidsforhold.consumers.HentArbeidsforholdConsumer;
 import no.nav.testnav.libs.dto.ameldingservice.v1.ArbeidsforholdDTO;
+import no.nav.testnav.libs.securitycore.domain.AccessToken;
 import no.nav.testnav.libs.securitycore.domain.ResourceServerType;
 import no.nav.testnav.libs.securitycore.domain.ServerProperties;
+import no.nav.testnav.libs.securitycore.domain.Token;
 import no.nav.testnav.libs.servletsecurity.action.GetAuthenticatedResourceServerType;
 import no.nav.testnav.libs.servletsecurity.exchange.TokenExchange;
+import no.nav.testnav.libs.servletsecurity.exchange.TokenService;
+import no.nav.testnav.libs.servletsecurity.exchange.TokenXService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static no.nav.testnav.libs.securitycore.domain.ResourceServerType.TOKEN_X;
 
 /**
  * Service for å sjekke arbeidsforhold.
@@ -31,30 +39,19 @@ public class ArbeidsforholdService {
     private final HentArbeidsforholdConsumer hentArbeidsforholdConsumer;
     private final GetAuthenticatedResourceServerType getAuthenticatedResourceServerType;
     private final Consumers consumers;
-    private String issuerUri;
-    private String jwkSetUri;
+    @Autowired
+    Environment env;
     private String id;
-    private List<String> acceptedAudience;
 
 
     @EventListener(ApplicationReadyEvent.class)
     public void sjekkArbeidsforhold() {
-        WebClient webClient = WebClient.create();
-        ServerProperties serverProperties = ServerProperties.of("dev-gcp", "dolly", "aareg", "https://aareg-services-q2.intern.dev.nav.no");
 
-        TokenExchange tokenExchange = new TokenExchange(getAuthenticatedResourceServerType, new ArrayList<>(), new ObjectMapper());
-        Token token = Token.builder().accessTokenValue(env.getProperty("VAULT_TOKEN")).build();
-        log.info("Token: {}", token.getAccessTokenValue());
-        List<TokenService> tokenServices = new ArrayList<>();
-        HentArbeidsforholdConsumer arbeid = new HentArbeidsforholdConsumer(
-                consumers,
-                new TokenExchange(
-                        getAuthenticatedResourceServerType,
-                        tokenServices,
-                        new ObjectMapper()) ,
-                new ObjectMapper());//tokenExchange
+        WebClient webClient = WebClient.create();
+        ServerProperties serverProperties = consumers.getTestnavLevendeArbeidsforholdService();
+        log.info("ServerProperties: {}", serverProperties.toString());
         id = "30447515845";
-        List<ArbeidsforholdDTO> arbeidsforhold = hentArbeidsforholdConsumer.getArbeidsforhold(id, token);
+        List<ArbeidsforholdDTO> arbeidsforhold = hentArbeidsforholdConsumer.getArbeidsforhold(id);
         if (arbeidsforhold != null) {
             log.info("Arbeidsforhold funnet: {}", arbeidsforhold);
         } else {
