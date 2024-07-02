b38723afd207ea5615e43e646e786f9dc9aab210
Andrea Devold Fjeld <solveig.andrea.devold.fjeld@nav.no>
2024-07-02 15:16:14 +0200

    Prøvet å lage en EndreArbeidsforholdCommand

--- apps/levende-arbeidsforhold-service/src/main/java/no/nav/registre/testnorge/levendearbeidsforhold/consumers/command/EndreArbeidsforholdCommand.java
+++ apps/levende-arbeidsforhold-service/src/main/java/no/nav/registre/testnorge/levendearbeidsforhold/consumers/command/EndreArbeidsforholdCommand.java
@@ -1,35 +1,64 @@
 package no.nav.registre.testnorge.levendearbeidsforhold.consumers.command;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import no.nav.testnav.libs.dto.ameldingservice.v1.ArbeidsforholdDTO;
+import no.nav.testnav.libs.commands.utils.WebClientFilter;
+import no.nav.testnav.libs.dto.aareg.v1.Arbeidsforhold;
+import no.nav.testnav.libs.dto.syntrest.v1.ArbeidsforholdRequest;
+import no.nav.testnav.libs.dto.syntrest.v1.ArbeidsforholdResponse;
+import org.apache.kafka.common.protocol.types.Field;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
 
+import java.time.Duration;
+import java.util.Arrays;
 import java.util.List;
-import java.util.UUID;
+import java.util.concurrent.Callable;
+import java.util.stream.Collectors;
 
-import static java.lang.String.format;
-/*
 @Slf4j
 @RequiredArgsConstructor
-public class EndreArbeidsforholdCommand {
+public class EndreArbeidsforholdCommand implements Callable<List<Arbeidsforhold>> {
     private final WebClient webClient;
+    private final Arbeidsforhold requests;
     private final String token;
-    private final String ident;
-    private final String miljo = "q2";
-    private static final String NAV_PERSON_IDENT = "Nav-Personident";
-    private static final String CONSUMER = "Dolly";
-
-    private static String getNavCallId() {
-        return format("%s %s", CONSUMER, UUID.randomUUID());
-    }
-    /*
+    private final ObjectMapper objectMapper;
+    private final String navArbeidsforholdKilde = "Dolly-doedsfall-hendelse" ;
     @SneakyThrows
     @Override
-    public List<ArbeidsforholdDTO> update(){
-        return null;
+    public List<Arbeidsforhold> call() throws Exception {
+
+        var arbeidsforhold = webClient
+                .put()
+                .uri("/api/v1/arbeidsforhold/{navArbeidsforholdId}",
+                        requests.getNavArbeidsforholdId())
+                .body(requests, Arbeidsforhold.class)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+                .header("Nav-Arbeidsforhold-Id", navArbeidsforholdKilde)
+                .header("Nav-Arbeidsforhold-Periode", requests.getAnsettelsesperiode().toString())
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<Arbeidsforhold>() {
+                }).retryWhen(Retry.backoff(3, Duration.ofSeconds(5))
+                        .filter(WebClientFilter::is5xxException))
+                .doOnError(error -> {
+                    try {
+                        log.error("Feil ved oppdatering av arbeidsforhold med body: {}.", objectMapper.writeValueAsString(requests), error);
+                    } catch (JsonProcessingException e) {
+                        log.error("Feil ved convertering av body til string.", e);
+                    }
+                }).block();
+
+        return arbeidsforhold == null ? List.of() : List.of(arbeidsforhold);
     }
 }
 
- */
+
