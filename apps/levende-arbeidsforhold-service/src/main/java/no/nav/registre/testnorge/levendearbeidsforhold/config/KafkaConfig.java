5598b542c59302605ecf13f8bf3697cc58633a05
ulrikHesmyr <ulrik.hesmyr@nav.no>
2024-07-02 11:53:56 +0200

    La til en tilfeldig suffix for group-id sånn at vi ikke kun har 1 konsument

--- apps/levende-arbeidsforhold-service/src/main/java/no/nav/registre/testnorge/levendearbeidsforhold/config/KafkaConfig.java
+++ apps/levende-arbeidsforhold-service/src/main/java/no/nav/registre/testnorge/levendearbeidsforhold/config/KafkaConfig.java
@@ -39,6 +39,9 @@ public class KafkaConfig {
     }
 
     public ConsumerFactory<String, String> consumerFactory() {
+
+        String randomSuffixGroupID = String.valueOf((int)(Math.random() * 1000));
+
         InetSocketAddress inetSocketAddress = new InetSocketAddress(0);
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, System.getenv("KAFKA_BROKERS"));
@@ -56,7 +59,7 @@ public class KafkaConfig {
         props.put(AbstractKafkaSchemaSerDeConfig.USER_INFO_CONFIG, username + ":" + password);
         props.put(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, System.getenv("KAFKA_SCHEMA_REGISTRY"));
 
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId + randomSuffixGroupID);
         props.put(ConsumerConfig.CLIENT_ID_CONFIG, groupId + inetSocketAddress.getHostString());
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
