8f3ac44628993e01c294063aed21f0a6cf8c2d06
Andrea Devold Fjeld <solveig.andrea.devold.fjeld@nav.no>
2024-07-02 12:44:31 +0200

    Fått hjelp av Stian #deploy-levende-arbeidsforhold-service

--- apps/levende-arbeidsforhold-service/src/main/java/no/nav/registre/testnorge/levendearbeidsforhold/config/ApplicationConfig.java
+++ apps/levende-arbeidsforhold-service/src/main/java/no/nav/registre/testnorge/levendearbeidsforhold/config/ApplicationConfig.java
@@ -5,11 +5,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 import no.nav.testnav.libs.servletcore.config.ApplicationCoreConfig;
+import no.nav.testnav.libs.servletsecurity.config.SecureOAuth2ServerToServerConfiguration;
 
 @Configuration
 @Import({
         ApplicationCoreConfig.class,
         InsecureJwtServerToServerConfiguration.class
+        // SecureOAuth2ServerToServerConfiguration.class
 })
 public class ApplicationConfig {
 
