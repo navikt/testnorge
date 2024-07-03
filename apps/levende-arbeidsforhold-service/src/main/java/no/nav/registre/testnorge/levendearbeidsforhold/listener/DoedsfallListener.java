02b156433bce5152f449f199f8d3d737e374d65e
Martineem <martine.erdal.mansaker@nav.no>
2024-07-03 10:41:03 +0200

    Sender spørring med aktørid

--- apps/levende-arbeidsforhold-service/src/main/java/no/nav/registre/testnorge/levendearbeidsforhold/listener/DoedsfallListener.java
+++ apps/levende-arbeidsforhold-service/src/main/java/no/nav/registre/testnorge/levendearbeidsforhold/listener/DoedsfallListener.java
@@ -6,11 +6,8 @@ import no.nav.person.pdl.leesah.Personhendelse;
 import no.nav.registre.testnorge.levendearbeidsforhold.service.ArbeidsforholdService;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
-
 import java.util.List;
 
 
@@ -21,21 +18,36 @@ public class DoedsfallListener {
     private static final String doedsfallTopic = "pdl.leesah-v1";
     @Autowired
     private final ArbeidsforholdService arbeidsforholdsService;
-    @EventListener(ApplicationReadyEvent.class)
-    public void onApplicationEvent() {
-        log.info("Hello World");
-        String id = "18426839454";
-        arbeidsforholdsService.getArbeidsforhold(id);
+
+    public void getArbeidsforhold(String aktorID) {
+        arbeidsforholdsService.getArbeidsforhold(aktorID);
     }
-/*
+
     @KafkaListener(topics = doedsfallTopic)
     public void getHendelser(List<ConsumerRecord<String, Personhendelse>> records) {
         for (ConsumerRecord<String, Personhendelse> record: records){
-            log.info("Record key:" + record.key());
-            log.info("Record value:" + record.value().toString());
-        }
 
+            String aktorID = record.key().split("\u001A")[1];
+
+            //Validerer om vi skal fortsette eller ignorere hendelsen
+            Boolean riktigHendelse = validerHendelse(record.value().get(4).toString());
+
+            //Flyt for doedsfall-hendelser
+            if (riktigHendelse){
+                log.info("DØDSFALL. Hendelse: {} {}", record.key(), record.value().toString());
+                getArbeidsforhold(aktorID); //List<Arbeidsforhold> arbeidsforhold = arbeidsforholdsService.getArbeidsforhold(aktorID);
+                //siOppArbeidsforhold(arbeidsforhold);
+            }
+        }
     }
 
- */
+    /**
+     * Validerer om hendelsen er dødsfall
+     * @param personhendelse - Hendelse/opplysningstype, f.eks: FOLKEREGISTERIDENTIFIKATOR_V1, NAVN_V1, SIVILSTAND_V1, etc.
+     * @return true dersom det er dødsfall, false hvis ikke
+     */
+    private Boolean validerHendelse(String personhendelse) {
+        return personhendelse.equals("DOEDSFALL_V1");
+    }
 }
+
