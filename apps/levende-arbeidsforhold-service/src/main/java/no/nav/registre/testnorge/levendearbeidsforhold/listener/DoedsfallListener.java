074be8acce8ee6b1cb3a042056806b41627c08cb
ulrikHesmyr <ulrik.hesmyr@nav.no>
2024-07-05 08:28:27 +0200

    La til doxygen dokumentasjon for /listener mappen

--- apps/levende-arbeidsforhold-service/src/main/java/no/nav/registre/testnorge/levendearbeidsforhold/listener/DoedsfallListener.java
+++ apps/levende-arbeidsforhold-service/src/main/java/no/nav/registre/testnorge/levendearbeidsforhold/listener/DoedsfallListener.java
@@ -23,6 +23,11 @@ public class DoedsfallListener {
     private final ArbeidsforholdService arbeidsforholdService;
     private final String oensketHendelsestype = "DOEDSFALL_V1";
 
+    /**
+     * Lytter til og konsumerer hendelser fra Kafka hendelsesstrømmen på et gitt topic.
+     * Behandler også alle hendelser av en gitt hendelsestype.
+     * @param hendelser - Alle konsumerte hendelser fra hendelsesstrømmen
+     */
     @KafkaListener(topics = doedsfallTopic)
     public void getHendelser(List<ConsumerRecord<String, Personhendelse>> hendelser) {
         for (ConsumerRecord<String, Personhendelse> hendelse: hendelser){
@@ -37,9 +42,9 @@ public class DoedsfallListener {
     }
 
     /**
-     * Validerer om hendelsen er dødsfall
+     * Validerer om hendelsen er av ønsket hendelsestype
      * @param personhendelse - Hendelse/opplysningstype, f.eks: FOLKEREGISTERIDENTIFIKATOR_V1, NAVN_V1, SIVILSTAND_V1, etc.
-     * @return true dersom det er dødsfall, false hvis ikke
+     * @return true dersom det er av ønsket hendelsestype, false hvis ikke
      */
     private Boolean validerHendelse(String personhendelse) {
         return personhendelse.equals(oensketHendelsestype);
