dfffc20a7558af1a73424193a7f83de0494e7512
Andrea Devold Fjeld <solveig.andrea.devold.fjeld@nav.no>
2024-07-08 11:56:16 +0200

    Laget en ny app levende-arbeidsforhold-ansettelse

---
+++ apps/levende-arbeidsforhold-ansettelse/src/main/java/no/nav/registre/testnorge/levendearbeidsforhold/LevendeArbeidsforholdServiceApplicationStarter.java
@@ -0,0 +1,13 @@
+package no.nav.registre.testnorge.levendearbeidsforhold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class LevendeArbeidsforholdServiceApplicationStarter {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(LevendeArbeidsforholdServiceApplicationStarter.class, args);
+    }
+}
