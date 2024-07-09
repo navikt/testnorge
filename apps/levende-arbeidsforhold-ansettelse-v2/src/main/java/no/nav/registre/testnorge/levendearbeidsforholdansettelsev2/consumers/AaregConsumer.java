5fba5423522372493bea42d47ae2b6238fed9e0a
Andrea Devold Fjeld <solveig.andrea.devold.fjeld@nav.no>
2024-07-09 13:13:20 +0200

    Spring fungerer å kjøre

---
+++ apps/levende-arbeidsforhold-ansettelse-v2/src/main/java/no/nav/registre/testnorge/levendearbeidsforholdansettelsev2/consumers/AaregConsumer.java
@@ -0,0 +1,74 @@
+package no.nav.registre.testnorge.levendearbeidsforholdansettelsev2.consumers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import no.nav.registre.testnorge.levendearbeidsforholdansettelsev2.config.Consumers;
+import no.nav.registre.testnorge.levendearbeidsforholdansettelsev2.consumers.command.HentArbeidsforholdCommand;
+import no.nav.registre.testnorge.levendearbeidsforholdansettelsev2.consumers.command.EndreArbeidsforholdCommand;
+import no.nav.registre.testnorge.levendearbeidsforholdansettelsev2.domain.v1.Arbeidsforhold;
+import no.nav.testnav.libs.securitycore.domain.ServerProperties;
+import no.nav.testnav.libs.standalone.servletsecurity.exchange.TokenExchange;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.json.Jackson2JsonDecoder;
+import org.springframework.http.codec.json.Jackson2JsonEncoder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Objects.nonNull;
+
+@Slf4j
+@Component
+public class AaregConsumer {
+
+    private final WebClient webClient;
+    private final ServerProperties serverProperties;
+    private final TokenExchange tokenExchange;
+
+    public AaregConsumer(
+            Consumers consumers,
+            TokenExchange tokenExchange,
+            ObjectMapper objectMapper) {
+
+        this.serverProperties = consumers.getTestnavAaregProxy();
+        this.tokenExchange = tokenExchange;
+
+        ExchangeStrategies jacksonStrategy = ExchangeStrategies
+            .builder()
+            .codecs(
+                config -> {
+                    config
+                        .defaultCodecs()
+                        .jackson2JsonEncoder(new Jackson2JsonEncoder(objectMapper, MediaType.APPLICATION_JSON));
+                    config
+                        .defaultCodecs()
+                        .jackson2JsonDecoder(new Jackson2JsonDecoder(objectMapper, MediaType.APPLICATION_JSON));
+                })
+            .build();
+
+        this.webClient = WebClient
+            .builder()
+            .exchangeStrategies(jacksonStrategy)
+            .baseUrl(serverProperties.getUrl())
+            .build();
+    }
+
+    public List<Arbeidsforhold> hentArbeidsforhold(String ident) {
+        var token = tokenExchange.exchange(serverProperties).block();
+        if (nonNull(token)) {
+            return new HentArbeidsforholdCommand(webClient, token.getTokenValue(), ident).call();
+        }
+        return new ArrayList<>();
+    }
+
+    public void endreArbeidsforhold(Arbeidsforhold requests) {
+        var token = tokenExchange.exchange(serverProperties).block();
+        if (nonNull(token)) {
+            new EndreArbeidsforholdCommand(webClient, requests, token.getTokenValue()).call();
+        }
+    }
+}
+
---
+++ apps/levende-arbeidsforhold-ansettelse-v2/src/main/java/no/nav/registre/testnorge/levendearbeidsforholdansettelsev2/domain/v1/Persontype.java
@@ -0,0 +1,6 @@
+package no.nav.registre.testnorge.levendearbeidsforholdansettelsev2.domain.v1;
+
+public interface Persontype {
+
+    String getType();
+}
