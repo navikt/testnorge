744421f706ae8e4aab24858cedf8bb464a2e30d6
Andrea Devold Fjeld <solveig.andrea.devold.fjeld@nav.no>
2024-07-10 15:21:20 +0200

    Laget EREG Consumers som henter Organisasjon

---
+++ apps/levende-arbeidsforhold-ansettelse/src/main/java/no/nav/registre/testnorge/levendearbeidsforholdansettelse/service/HentOrganisasjonService.java
@@ -0,0 +1,21 @@
+package no.nav.registre.testnorge.levendearbeidsforholdansettelse.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import no.nav.registre.testnorge.levendearbeidsforholdansettelse.consumers.EregConsumer;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class HentOrganisasjonService {
+    private final EregConsumer eregConsumer;
+    @EventListener(ApplicationReadyEvent.class)
+    public void hentOrganisasjoner() {
+        System.out.println("Henter organisasjoner");
+        log.info("eregConsumer.getOrganisasjon(\"955937864\", \"q2\")");
+        eregConsumer.getOrganisasjon("955937864", "q2");
+    }
+}
