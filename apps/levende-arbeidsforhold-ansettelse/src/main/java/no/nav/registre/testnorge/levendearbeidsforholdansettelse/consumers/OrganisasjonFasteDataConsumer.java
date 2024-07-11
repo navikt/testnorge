d3afa0d418775f5ca5a28bfe2f58949db852921d
Andrea Devold Fjeld <solveig.andrea.devold.fjeld@nav.no>
2024-07-11 08:51:02 +0200

    Får hentet organisasjonsnr

--- apps/levende-arbeidsforhold-ansettelse/src/main/java/no/nav/registre/testnorge/levendearbeidsforholdansettelse/consumers/OrganisasjonFasteDataConsumer.java
+++ apps/levende-arbeidsforhold-ansettelse/src/main/java/no/nav/registre/testnorge/levendearbeidsforholdansettelse/consumers/OrganisasjonFasteDataConsumer.java
@@ -4,7 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import no.nav.registre.testnorge.levendearbeidsforholdansettelse.config.Consumers;
 import no.nav.registre.testnorge.levendearbeidsforholdansettelse.consumers.command.GetOrganisasjonerCommand;
 import no.nav.registre.testnorge.levendearbeidsforholdansettelse.domain.organisasjon.Organisasjon2;
-import no.nav.registre.testnorge.levendearbeidsforholdansettelse.domain.organisasjon.dto.OrganisasjonDTO;
+import no.nav.testnav.libs.dto.organisasjonfastedataservice.v1.OrganisasjonDTO;
 import no.nav.registre.testnorge.levendearbeidsforholdansettelse.domain.v1.Organisasjon;
 import no.nav.testnav.libs.securitycore.domain.ServerProperties;
 import no.nav.testnav.libs.standalone.servletsecurity.exchange.TokenExchange;
@@ -31,9 +31,9 @@ public class OrganisasjonFasteDataConsumer {
         this.tokenExchange = tokenExchange;
     }
 
-    public List<Organisasjon2> hentOrganisasjoner() {
+    public List<OrganisasjonDTO> hentOrganisasjoner() {
         var accessToken = tokenExchange.exchange(serverProperties).block();
-        List<Organisasjon2> dto = new GetOrganisasjonerCommand(webClient, accessToken.getTokenValue()).call();
+        List<OrganisasjonDTO> dto = new GetOrganisasjonerCommand(webClient, accessToken.getTokenValue()).call();
         return dto; //org -> new Organisasjon(org.getOrganisasjonsnummer())
     }
 }
