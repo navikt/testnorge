2bf331822b5fed4464c0c9cc55e786a161f53d3c
Martineem <martine.erdal.mansaker@nav.no>
2024-07-12 14:54:10 +0200

    Sletter ubrukte queries

--- apps/levende-arbeidsforhold-ansettelse/src/main/java/no/nav/registre/testnorge/levendearbeidsforholdansettelse/consumers/PdlConsumer.java
+++ apps/levende-arbeidsforhold-ansettelse/src/main/java/no/nav/registre/testnorge/levendearbeidsforholdansettelse/consumers/PdlConsumer.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
-import no.nav.registre.testnorge.levendearbeidsforholdansettelse.consumers.command.PdlBolkPersonCommand;
-import no.nav.registre.testnorge.levendearbeidsforholdansettelse.consumers.command.PdlPersonGetCommand;
 import no.nav.registre.testnorge.levendearbeidsforholdansettelse.consumers.command.SokPersonCommand;
 import no.nav.registre.testnorge.levendearbeidsforholdansettelse.domain.pdl.GraphqlVariables;
 import no.nav.registre.testnorge.levendearbeidsforholdansettelse.provider.PdlMiljoer;
@@ -25,7 +23,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.time.Duration;
-import java.util.List;
 import java.util.stream.Collectors;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
@@ -39,43 +36,34 @@ public class PdlConsumer {
     private final ServerProperties serverProperties;
     private final WebClient webClient;
 
-
-
     public PdlConsumer(
             TokenExchange tokenService,
             Consumers consumers,
             ObjectMapper objectMapper,
             WebClient.Builder webClientBuilder
     ) {
-
         serverProperties = consumers.getPdlProxy();
         this.tokenService = tokenService;
         webClient = webClientBuilder
-                .baseUrl(serverProperties.getUrl())
-                .exchangeStrategies(getJacksonStrategy(objectMapper))
-                .clientConnector(
-                        new ReactorClientHttpConnector(
-                                HttpClient.create(
-                                                ConnectionProvider
-                                                        .builder("custom")
-                                                        .maxConnections(10)
-                                                        .pendingAcquireMaxCount(5000)
-                                                        .pendingAcquireTimeout(Duration.ofMinutes(15))
-                                                        .build())
-                                        .responseTimeout(Duration.ofSeconds(5))))
-                .build();
-
-
+            .baseUrl(serverProperties.getUrl())
+            .exchangeStrategies(getJacksonStrategy(objectMapper))
+            .clientConnector(
+                new ReactorClientHttpConnector(
+                    HttpClient.create(
+                        ConnectionProvider
+                            .builder("custom")
+                            .maxConnections(10)
+                            .pendingAcquireMaxCount(5000)
+                            .pendingAcquireTimeout(Duration.ofMinutes(15))
+                            .build())
+            .responseTimeout(Duration.ofSeconds(5))))
+            .build();
     }
 
-    public Mono<JsonNode> getPdlPerson(String ident, PdlMiljoer pdlMiljoe) {
-
-        return tokenService.exchange(serverProperties)
-                .flatMap((AccessToken token) -> new PdlPersonGetCommand(webClient, ident, token.getTokenValue(), pdlMiljoe)
-                        .call());
-    }
-    public Mono<JsonNode> getSokPerson(GraphqlVariables.Paging paging, GraphqlVariables.Criteria criteria, PdlMiljoer pdlMiljoe) {
-
+    public Mono<JsonNode> getSokPerson(
+            GraphqlVariables.Paging paging,
+            GraphqlVariables.Criteria criteria,
+            PdlMiljoer pdlMiljoe) {
         return tokenService.exchange(serverProperties)
                 .flatMap((AccessToken token) -> new SokPersonCommand(webClient, paging, criteria, token.getTokenValue(), pdlMiljoe)
                         .call());
@@ -85,12 +73,9 @@ public class PdlConsumer {
         val resource = new ClassPathResource(pathResource);
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(resource.getInputStream(), UTF_8))) {
             return reader.lines().collect(Collectors.joining("\n"));
-
         } catch (IOException e) {
             log.error("Lesing av query ressurs {} feilet", pathResource, e);
             return null;
         }
     }
-
-
 }
