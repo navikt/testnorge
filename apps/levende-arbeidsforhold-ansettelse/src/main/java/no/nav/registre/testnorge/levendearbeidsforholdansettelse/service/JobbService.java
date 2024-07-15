032e83aec79aa34897356e4dab27fb656488d477
ulrikHesmyr <ulrik.hesmyr@nav.no>
2024-07-15 14:50:48 +0200

    Tilkoble database instans med app #deploy-levende-arbeidsforhold-ansettelse

--- apps/levende-arbeidsforhold-ansettelse/src/main/java/no/nav/registre/testnorge/levendearbeidsforholdansettelse/service/JobbService.java
+++ apps/levende-arbeidsforhold-ansettelse/src/main/java/no/nav/registre/testnorge/levendearbeidsforholdansettelse/service/JobbService.java
@@ -1,18 +1,30 @@
 package no.nav.registre.testnorge.levendearbeidsforholdansettelse.service;
 
+import lombok.extern.slf4j.Slf4j;
 import no.nav.registre.testnorge.levendearbeidsforholdansettelse.entity.Jobber;
 import no.nav.registre.testnorge.levendearbeidsforholdansettelse.repository.JobberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Service
 public class JobbService {
 
     @Autowired
     private JobberRepository jobberRepository;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void skrivUtJobber(){
+        log.info("Jobber:");
+        Jobber jobb1 = hentJobber("123").getFirst();
+        var ant = "" + jobb1.hentAntbedrifter();
+        log.info(ant);
+    }
 
     public List<Jobber> hentJobber(String id){
         var jobber = jobberRepository.findAllById(id);
