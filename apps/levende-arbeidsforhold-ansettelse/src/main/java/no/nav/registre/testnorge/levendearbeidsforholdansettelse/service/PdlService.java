829f4493bd77ba462b696cd73f789d53bb1ae589
Andrea Devold Fjeld <solveig.andrea.devold.fjeld@nav.no>
2024-07-16 14:45:25 +0200

    Kommentert ut i hentOrganisasjonService

--- apps/levende-arbeidsforhold-ansettelse/src/main/java/no/nav/registre/testnorge/levendearbeidsforholdansettelse/service/PdlService.java
+++ apps/levende-arbeidsforhold-ansettelse/src/main/java/no/nav/registre/testnorge/levendearbeidsforholdansettelse/service/PdlService.java
@@ -1,13 +1,19 @@
 package no.nav.registre.testnorge.levendearbeidsforholdansettelse.service;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.registre.testnorge.levendearbeidsforholdansettelse.consumers.PdlConsumer;
+import no.nav.registre.testnorge.levendearbeidsforholdansettelse.domain.pdl.GraphqlVariables;
+import no.nav.registre.testnorge.levendearbeidsforholdansettelse.domain.pdl.SokPersonVariables;
 import no.nav.registre.testnorge.levendearbeidsforholdansettelse.provider.PdlMiljoer;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Slf4j
 @Service
@@ -15,11 +21,25 @@ import org.springframework.stereotype.Service;
 public class PdlService {
     private final PdlConsumer pdlConsumer;
 
-    //@EventListener(ApplicationReadyEvent.class)
     public void getPerson(){
-        String ident = "12345678901";
         PdlMiljoer miljoer = PdlMiljoer.Q2;
-        JsonNode pdl = pdlConsumer.getPdlPerson(ident, miljoer).block();
-        log.info(pdl.toString());
+        SokPersonVariables sokPerson = SokPersonVariables
+                .builder()
+                .pageNumber(1)
+                .resultsPerPage(5)
+                .from("1955")
+                .to("2006")
+                .postnr("1???")
+                .build();
+
+        JsonNode node = pdlConsumer.getSokPerson(sokPerson.lagSokPersonPaging(),
+                        sokPerson.lagSokPersonCriteria(),
+                        PdlMiljoer.Q2)
+                .block();
+
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, Object> result = mapper.convertValue(node, new TypeReference<Map<String, Object>>(){});
+
+        log.info("{}", result);
     }
 }
