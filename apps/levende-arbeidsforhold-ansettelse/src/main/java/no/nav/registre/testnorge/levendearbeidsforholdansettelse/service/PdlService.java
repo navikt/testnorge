cc9e441ca72df2f5ed401c3b70bae9da409fb7ea
Andrea Devold Fjeld <solveig.andrea.devold.fjeld@nav.no>
2024-07-16 16:00:06 +0200

    Laget Ident objekt og filtrerte resultatet fra pdl til å bare returnere identer fra folgeregisteret

--- apps/levende-arbeidsforhold-ansettelse/src/main/java/no/nav/registre/testnorge/levendearbeidsforholdansettelse/service/PdlService.java
+++ apps/levende-arbeidsforhold-ansettelse/src/main/java/no/nav/registre/testnorge/levendearbeidsforholdansettelse/service/PdlService.java
@@ -1,19 +1,16 @@
 package no.nav.registre.testnorge.levendearbeidsforholdansettelse.service;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.registre.testnorge.levendearbeidsforholdansettelse.consumers.PdlConsumer;
-import no.nav.registre.testnorge.levendearbeidsforholdansettelse.domain.pdl.GraphqlVariables;
+import no.nav.registre.testnorge.levendearbeidsforholdansettelse.domain.pdl.Ident;
 import no.nav.registre.testnorge.levendearbeidsforholdansettelse.domain.pdl.SokPersonVariables;
 import no.nav.registre.testnorge.levendearbeidsforholdansettelse.provider.PdlMiljoer;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Service
@@ -21,7 +18,8 @@ import java.util.Map;
 public class PdlService {
     private final PdlConsumer pdlConsumer;
 
-    public void getPerson(){
+    //@EventListener(ApplicationReadyEvent.class)
+    public List<Ident> getPerson(){
         PdlMiljoer miljoer = PdlMiljoer.Q2;
         SokPersonVariables sokPerson = SokPersonVariables
                 .builder()
@@ -37,9 +35,21 @@ public class PdlService {
                         PdlMiljoer.Q2)
                 .block();
 
-        ObjectMapper mapper = new ObjectMapper();
-        Map<String, Object> result = mapper.convertValue(node, new TypeReference<Map<String, Object>>(){});
+        List<Ident> identer = new ArrayList<>();
 
-        log.info("{}", result);
+        node.get("data").get("sokPerson").findValues("identer").forEach(
+                hit -> {
+                    hit.forEach(
+                            ident -> {
+                                if(ident.get("gruppe").asText().equals("FOLKEREGISTERIDENT")) {
+                                    identer.add(new Ident(ident.get("ident").asText(), ident.get("gruppe").asText()));
+                                }
+                            }
+                    );
+                }
+        );
+
+        return identer;
+
     }
 }
