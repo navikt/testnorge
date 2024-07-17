e907495cabc19737c849f5aafa205493ef289b04
Martineem <martine.erdal.mansaker@nav.no>
2024-07-17 09:59:22 +0200

    Ny klasse

---
+++ apps/levende-arbeidsforhold-ansettelse/src/main/java/no/nav/registre/testnorge/levendearbeidsforholdansettelse/service/AnsettelseService.java
@@ -0,0 +1,12 @@
+package no.nav.registre.testnorge.levendearbeidsforholdansettelse.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AnsettelseService {
+
+}
