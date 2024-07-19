92908c527942d7a7b7a0c3cf28cefaa3e05578fe
Andrea Devold Fjeld <solveig.andrea.devold.fjeld@nav.no>
2024-07-19 14:58:33 +0200

    Lagt til inbound og outboud rule #deploy-testnav-levende-arbeidsforhold-ansettelse

--- apps/levende-arbeidsforhold-ansettelse/src/main/java/no/nav/registre/testnorge/levendearbeidsforholdansettelse/service/JobbService.java
+++ apps/levende-arbeidsforhold-ansettelse/src/main/java/no/nav/registre/testnorge/levendearbeidsforholdansettelse/service/JobbService.java
@@ -19,7 +19,6 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class JobbService {
 
-
     private final JobberRepository jobberRepository;
     private final VerdiRepository verdiRepository;
     public List<JobbParameterEntity> hentAlleParametere(){
