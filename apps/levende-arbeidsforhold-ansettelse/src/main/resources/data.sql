d1e7ee0e375705326cf33f516caccd48beadd7d4
ulrikHesmyr <ulrik.hesmyr@nav.no>
2024-07-22 15:52:02 +0200

    La til data-migrasjon av in-memory database

--- apps/levende-arbeidsforhold-ansettelse/src/main/resources/data.sql
+++ apps/levende-arbeidsforhold-ansettelse/src/main/resources/data.sql
@@ -1,10 +1,7 @@
-/*
+
 INSERT INTO jobb_parameter (NAVN, TEKST, VERDI) VALUES ('antallOrganisasjoner', 'Organisasjoner', '100'),
                                                                          ('antallPersoner', 'Personer', '20'),
                                                                          ('typeArbeidsforhold', 'Type Arbeidsforhold', 'ordinaertArbeidsforhold'),
                                                                          ('arbeidstidsOrdning', 'Arbeidstids Ordning', 'ikkeSkift'),
+                                                                         ('intervall', 'Intervall', '2'),
                                                                          ('stillingsprosent', 'Stillingsprosent', '100.0');
-
-
-
- */
