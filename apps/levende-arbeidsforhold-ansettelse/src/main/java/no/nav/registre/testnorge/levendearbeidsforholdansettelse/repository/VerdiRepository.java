6c2fd2c0e9f09609df29ae4391584ba7d4fae324
Andrea Devold Fjeld <solveig.andrea.devold.fjeld@nav.no>
2024-07-22 11:48:04 +0200

    Lagt til outbund rule på oversikt-frontend og fjernet eventlistener i test. #deploy-levende-arbeidsforhold-ansettelse

--- apps/levende-arbeidsforhold-ansettelse/src/main/java/no/nav/registre/testnorge/levendearbeidsforholdansettelse/repository/VerdiRepository.java
+++ apps/levende-arbeidsforhold-ansettelse/src/main/java/no/nav/registre/testnorge/levendearbeidsforholdansettelse/repository/VerdiRepository.java
@@ -2,7 +2,6 @@ package no.nav.registre.testnorge.levendearbeidsforholdansettelse.repository;
 
 import no.nav.registre.testnorge.levendearbeidsforholdansettelse.entity.VerdierEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
