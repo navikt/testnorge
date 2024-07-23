99752792f88e7c20a68e58f08a4de6576fadd305
Andrea Devold Fjeld <solveig.andrea.devold.fjeld@nav.no>
2024-07-23 17:50:09 +0200

    prøver med schema.sql og data.sql #deploy-levende-arbeidsforhold-ansettelse

--- apps/levende-arbeidsforhold-ansettelse/src/main/resources/db/migration/V1.0.0_CreateTables.sql
+++ apps/levende-arbeidsforhold-ansettelse/src/main/resources/db/migration/V1.0.0_CreateTables.sql
@@ -14,8 +14,8 @@ create table verdier (
     id INT SERIAL PRIMARY KEY ,
     verdi_navn varchar(255)  ,
     verdi_verdi varchar(255)  ,
-    foreign key (verdi_navn) references jobb_parameter(NAVN)
-)
+    foreign key (verdi_navn) references jobb_parameter(navn)
+);
 /*
 INSERT INTO JOBB_PARAMETER (NAVN, TEKST, VERDI) VALUES ('antallOrganisasjoner', 'Organisasjoner', '100'),
                                                                          ('antallPersoner', 'Personer', '20'),
