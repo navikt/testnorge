c9206c854d0f58718183494f2c1cac21f0aefe54
Martineem <martine.erdal.mansaker@nav.no>
2024-07-24 10:37:03 +0200

    Tester tilgang til databasen #deploy-levende-arbeidsforhold-ansettelse

--- apps/levende-arbeidsforhold-ansettelse/src/main/resources/db/migration/V1.0.0_CreateTables.sql
+++ apps/levende-arbeidsforhold-ansettelse/src/main/resources/db/migration/V1.0.0_CreateTables.sql
@@ -1,6 +1,17 @@
 -------------------------------
 -- C R E A T E   T A B L E S --
 -------------------------------
+DO
+$$
+BEGIN
+    -- TODO: Find a better way to do this
+    --grant all privileges on all tables in schema public to cloudsqliamuser;
+    grant all privileges on table jobb_parameter to cloudsqliamuser;
+    grant all privileges on table verdier to cloudsqliamuser;
+    grant all privileges on table jobb_parameter to "testnav-levende-arbeidsforhold-ansettelsev2";
+    grant all privileges on table verdier to "testnav-levende-arbeidsforhold-ansettelsev2";
+END
+$$;
 
 
 CREATE TABLE IF NOT EXISTS jobb_parameter (
