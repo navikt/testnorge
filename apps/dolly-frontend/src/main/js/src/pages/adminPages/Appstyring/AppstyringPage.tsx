7f47359c8a856a45142b9f9122e8fb59707e5b5e
Martineem <martine.erdal.mansaker@nav.no>
2024-07-25 09:27:56 +0200

    Fjerner ubrukt kode

--- apps/dolly-frontend/src/main/js/src/pages/adminPages/Appstyring/AppstyringPage.tsx
+++ apps/dolly-frontend/src/main/js/src/pages/adminPages/Appstyring/AppstyringPage.tsx
@@ -3,72 +3,15 @@ import {Alert} from '@navikt/ds-react'
 import {AppstyringTable} from '@/pages/adminPages/Appstyring/AppstyringTable'
 import {erDollyAdmin} from '@/utils/DollyAdmin'
 import React, {useEffect, useState} from "react";
-import Request from "@/service/services/Request";
-import {OrgInfoResponse} from "@/service/services/organisasjonservice/types";
 
 export default () => {
-	//if (!erDollyAdmin()) {
-	//	return <AdminAccessDenied/>
-	//}
+	if (!erDollyAdmin()) {
+		return <AdminAccessDenied/>
+	}
 
 	const [apiData , setApiData] = useState([]);
+
 	/*
-	const headers = {
-		Authorization: 'Bearer '
-	}
-	let data: { navn: string, verdi: string, verdier: { verdi: string, navn: string }[] }[] = [];
-
-	//let response = await fetch('http://localhost:8080/api', {headers});
-
-
-
-	*/
-	Request.get('/testnav-levende-arbeidsforhold-ansettelsev2/api').then(
-		(response: any) => {
-			if (response != null) {
-				console.log(response);
-			}
-		}
-	);
-
- /*
-	useEffect(() => {
-		fetch("/testnav-levende-arbeidsforhold-ansettelse/api", {
-			method: "GET"})
-			.then((response) => response.json())
-			.then(data => {
-			for (const parameter of data) {
-				let verdier: {verdi: string, navn: string}[] = [];
-				for (const verdi of data.verdier) {
-					verdier.push({verdi: verdi.verdi, navn: verdi.navn });
-				}
-				data.push({parameter: parameter.navn, verdi: parameter.verdi, verdier: verdier});
-			}
-		})
-	.then(json=>console.log(json));
-	});
-
-	 */
-
-	//const headers = { 'Authorization': 'Bearer ' };
-	/*
-	fetch('/testnav-levende-arbeidsforhold-ansettelse/api')
-
-		.then(res=>res.json())
-		.then((data) => {
-			for (const parameter of data) {
-				let verdier: {verdi: string, navn: string}[] = [];
-				for (const verdi of data.verdier) {
-					verdier.push({verdi: verdi.verdi, navn: verdi.navn });
-				}
-				data.push({parameter: parameter.navn, verdi: parameter.verdi, verdier: verdier});
-			}
-		})
-		.then(json=>console.log(json));
-
-	 */
-
-	//TODO: Implementer henting av data fra backend
 	const dataMock = [
 		{parameter: 'Parameter 1', verdi: 'verdi1'},
 		{parameter: 'Parameter 2', verdi: 'verdi2'},
@@ -81,8 +24,9 @@ export default () => {
 		{parameter: 'Parameter 9', verdi: 'verdi9'},
 		{parameter: 'Parameter 10', verdi: 'verdi10'},
 	]
-	useEffect(() => {
+	 */
 
+	useEffect(() => {
 		async function fetchData() {
 			const req = await fetch('/testnav-levende-arbeidsforhold-ansettelsev2/api');
 			const res = await req.json();
@@ -102,7 +46,7 @@ export default () => {
 			<Alert variant={'info'} style={{marginBottom: '15px'}}>
 				Denne siden er under utvikling.
 			</Alert>
-			<AppstyringTable data={dataMock}/>
+			<AppstyringTable data={apiData}/>
 		</>
 	)
 }
