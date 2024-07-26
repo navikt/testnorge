edc90a8bdb105b246c79a5219ba66deea1576ca3
ulrikHesmyr <ulrik.hesmyr@nav.no>
2024-07-26 16:14:44 +0200

    Legger til doxygen for commander funksjoner + wrapper API spørring i try-catch

--- apps/levende-arbeidsforhold-scheduler/src/main/java/no/nav/levendearbeidsforholdscheduler/consumer/command/AnsettelseCommand.java
+++ apps/levende-arbeidsforhold-scheduler/src/main/java/no/nav/levendearbeidsforholdscheduler/consumer/command/AnsettelseCommand.java
@@ -4,8 +4,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.levendearbeidsforholdscheduler.config.Consumers;
 import no.nav.testnav.libs.servletsecurity.exchange.TokenExchange;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -17,27 +15,30 @@ public class AnsettelseCommand {
     private final Consumers consumers;
     private final TokenExchange tokenExchange;
 
-    @EventListener(ApplicationReadyEvent.class)
+    /**
+     * Funksjon som sender http spørring til levende-arbeidsforhold-ansettelse appen for å aktivere en ansettelse-jobb
+     */
     public void aktiverAnsettelseService(){
 
-        log.info("Kjørrrr");
+        try {
+            var accessToken = hentToken();
 
-        var accessToken = hentToken();
-
-        WebClient webClient = WebClient.builder().baseUrl("https://testnav-levende-arbeidsforhold-ansettelse-v2.intern.dev.nav.no/api/ansettelse-jobb").build();
-        String response = webClient.get()
-                .header("Authorization", "Bearer " + accessToken)
-                .retrieve().bodyToMono(String.class).block();
-        if(response != null){
-            log.info("Response: {}", response);
+            WebClient webClient = WebClient.builder().baseUrl("https://testnav-levende-arbeidsforhold-ansettelse-v2.intern.dev.nav.no/api/ansettelse-jobb").build();
+            String response = webClient.get()
+                    .header("Authorization", "Bearer " + accessToken)
+                    .retrieve().bodyToMono(String.class).block();
+            if(response != null){
+                log.info("Response: {}", response);
+            }
+        } catch (Exception e) {
+            log.warn("Kunne ikke aktivere ansettelse service pga spørring feilet");
         }
-//        try {
-//
-//        } catch (Exception e){
-//            log.info("Feilet å sende spørring til levende-arbeidsforhold-ansettelse");
-//        }
     }
 
+    /**
+     * Henter ut accesstoken for å autorisere mot levende-arbeidsforhold-ansettelse appen sitt API
+     * @return access-tokenen dersom forespørselen var vellykket
+     */
     public String hentToken(){
 
         var serverProperties = consumers.getTestnavLevendeArbeidsforholdAnsettelsev2();
