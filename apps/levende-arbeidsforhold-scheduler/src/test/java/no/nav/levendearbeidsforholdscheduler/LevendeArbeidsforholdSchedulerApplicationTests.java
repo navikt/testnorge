13bb6ed07a008bc3e51088526ebbfe963b49166e
ulrikHesmyr <ulrik.hesmyr@nav.no>
2024-07-26 16:00:05 +0200

    Ny scheduler app gradle prosjekt

---
+++ apps/levende-arbeidsforhold-scheduler/src/test/java/no/nav/levendearbeidsforholdscheduler/LevendeArbeidsforholdSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package no.nav.levendearbeidsforholdscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LevendeArbeidsforholdSchedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
