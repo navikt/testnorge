6ac662b23eaddb3ae74cb27d353f0025a068da78
Andrea Devold Fjeld <solveig.andrea.devold.fjeld@nav.no>
2024-07-29 11:44:32 +0200

    Startet å lage graphql spørring

--- apps/levende-arbeidsforhold-ansettelse/src/main/java/no/nav/registre/testnorge/levendearbeidsforholdansettelse/domain/pdl/SokPersonVariables.java
+++ apps/levende-arbeidsforhold-ansettelse/src/main/java/no/nav/registre/testnorge/levendearbeidsforholdansettelse/domain/pdl/SokPersonVariables.java
@@ -51,4 +51,33 @@ public class SokPersonVariables {
                 .resultsPerPage(resultsPerPage)
                 .build();
     }
+
+    public GraphqlVariables.Criteria lagSokPersonCriteriaTenor(){
+        Map<String, String> searchRuleFoedselsaar = new java.util.HashMap<>();
+        searchRuleFoedselsaar.put("from", from);
+        searchRuleFoedselsaar.put("to", to );
+
+        GraphqlVariables.Filter filterBostedPostnr = GraphqlVariables.Filter.builder()
+                .fieldName("person.bostedsadresse.vegadresse.postnummer")
+                .searchRule(Map.of("wildcard", postnr))
+                .build();
+
+        GraphqlVariables.Filter filterOppholdPostnr = GraphqlVariables.Filter.builder()
+                .fieldName("person.oppholdsadresse.vegadresse.postnummer")
+                .searchRule(Map.of("wildcard", postnr))
+                .build();
+
+        GraphqlVariables.Filter filterFoedselsaar = GraphqlVariables.Filter.builder()
+                .fieldName("person.foedselsdato.foedselsaar")
+                .searchRule(searchRuleFoedselsaar)
+                .build();
+
+        GraphqlVariables.Filter filterTenorPerson = GraphqlVariables.Filter.builder()
+                .build();
+
+        Map<String, List<GraphqlVariables.Filter>> or = Map.of("or", List.of(filterBostedPostnr, filterOppholdPostnr));
+        List<Object> and = List.of(or, filterFoedselsaar);
+
+        return null;
+    }
 }
