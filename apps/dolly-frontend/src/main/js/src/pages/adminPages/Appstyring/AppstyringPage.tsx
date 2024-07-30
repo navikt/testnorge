12bdf577aa5ff646469d10a355d6a36347ab9784
Martineem <martine.erdal.mansaker@nav.no>
2024-07-30 10:31:48 +0200

    Setter opp kobling til scheduler-appen i frontend og fetcher status

--- apps/dolly-frontend/src/main/js/src/pages/adminPages/Appstyring/AppstyringPage.tsx
+++ apps/dolly-frontend/src/main/js/src/pages/adminPages/Appstyring/AppstyringPage.tsx
@@ -4,6 +4,7 @@ import {AppstyringTable} from '@/pages/adminPages/Appstyring/AppstyringTable'
 import {erDollyAdmin} from '@/utils/DollyAdmin'
 import React, {useEffect, useState} from "react";
 import {FetchData} from "@/pages/adminPages/Appstyring/util/Typer";
+import { Box, VStack } from "@navikt/ds-react";
 
 export default () => {
 	if (!erDollyAdmin()) {
@@ -13,9 +14,22 @@ export default () => {
 	const [apiData , setApiData] = useState<Array<FetchData>>([]);
 	let optionsData: FetchData[] = [];
 
+	const statusBox = () => {
+		return (
+			<VStack gap="4">
+				<Box padding="4" background="surface-alt-3-subtle">
+
+				</Box>
+				<Box padding="4" background="surface-success-subtle">
+
+				</Box>
+			</VStack>
+		);
+	};
+
 	useEffect(() => {
 		async function fetchData() {
-			const data = await fetch('/testnav-levende-arbeidsforhold-ansettelsev2/api')
+			await fetch('/testnav-levende-arbeidsforhold-ansettelsev2/api')
 				.then(res => res.json())
 				.then(res => {
 					res.map((r: FetchData) => optionsData.push(r))
@@ -25,6 +39,18 @@ export default () => {
 		}
 		fetchData();
 	}, []);
+
+	useEffect(() => {
+		async function fetchStatus() {
+			const data = await fetch('/testnav-levende-arbeidsforhold-scheduler/scheduler/status')
+				.then(res => res.json())
+				.catch(err => console.error(err));
+			console.log(data);
+		}
+		fetchStatus();
+	}, []);
+
+
 	return (
 		<>
 			<h1>App-styring</h1>
