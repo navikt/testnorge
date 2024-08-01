97b5c5de053e5fcee751f8fb234bf4ff17bbe96d
ulrikHesmyr <ulrik.hesmyr@nav.no>
2024-08-01 15:19:07 +0200

    Legger til sjekk for om deaktivering av scheduler jobben var vellykket

--- apps/dolly-frontend/src/main/js/src/pages/adminPages/Appstyring/AppstyringPage.tsx
+++ apps/dolly-frontend/src/main/js/src/pages/adminPages/Appstyring/AppstyringPage.tsx
@@ -16,16 +16,23 @@ export default () => {
 	const [henterStatus, setHenterStatus] = useState(false);
 
 	let optionsData: FetchData[] = [];
+	
+	const SCHEDULER_DOMENE : string = "/testnav-levende-arbeidsforhold-scheduler";
+	const ANSETTELSE_DOMENE : string = "/testnav-levende-arbeidsforhold-ansettelse";
 
+	/**
+	 * Funksjon som sender spørring til scheduler appen sitt API for å aktivere jobb-scheduler
+	 */
 	async function aktiverScheduler(){
 		setHenterStatus(true);
 
-		let intervall = apiData.find(d => d.navn == "intervall")?.verdi;
+		let intervall = apiData.find(d => d.navn == "intervall");
 		if(intervall == undefined){
 			alert("Finner ikke intervall");
+			setHenterStatus(false);
 			return;
 		}
-		await fetch(`/testnav-levende-arbeidsforhold-scheduler/scheduler?intervall=${intervall}`).then(res => {
+		await fetch(`${SCHEDULER_DOMENE}/scheduler?intervall=${intervall.verdi}`).then(res => {
 			if (res.ok) {
 				setTimeout(()=>{
 					fetchStatusScheduler();
@@ -35,23 +42,39 @@ export default () => {
 		});
 	}
 
-
+	/**
+	 * Funksjon som sender spørring til scheduler appen sitt API for å deaktivere jobb-scheduler
+	 */
 	async function deaktiverScheduler(){
-		setHenterStatus(true);
-		await fetch('/testnav-levende-arbeidsforhold-scheduler/scheduler/stopp').then(res => {
-			if (res.ok) {
-				//Sjekk om body er true?
-				setTimeout(()=>{
-					fetchStatusScheduler();
-					setHenterStatus(false);
-				}, 200)
-			}
-		});
+
+		if (window.confirm("Vil du deaktivere jobben? Du kan aktivere ny jobb igjen når som helst.")){
+			setHenterStatus(true);
+			await fetch(`${SCHEDULER_DOMENE}/scheduler/stopp`).then(async res => {
+				if (res.ok) {
+					let data = await res.json();
+					if (data && !data.status){
+						alert("Deaktivering av scheduler feilet, prøv igjen.");
+						setHenterStatus(false);
+						return;
+					}
+
+					setTimeout(()=>{
+						fetchStatusScheduler();
+						setHenterStatus(false);
+					}, 200)
+				}
+			});
+		}
+
 
 	}
 
+	/**
+	 * Funksjon som sender spørring til scheduler appen sitt API for å hente ut status på om scheduleren er aktiv
+	 * eller ikke
+	 */
 	async function fetchStatusScheduler() {
-		const data = await fetch('/testnav-levende-arbeidsforhold-scheduler/scheduler/status')
+		const data = await fetch(`${SCHEDULER_DOMENE}/scheduler/status`)
 			.then(res => res.json())
 			.catch(err => console.error(err));
 		setStatusData(data);
@@ -59,7 +82,7 @@ export default () => {
 
 	useEffect(() => {
 		async function fetchData() {
-			await fetch('/testnav-levende-arbeidsforhold-ansettelse/api')
+			await fetch(`${ANSETTELSE_DOMENE}/api`)
 				.then(res => res.json())
 				.then(res => {
 					res.map((r: FetchData) => optionsData.push(r))
@@ -82,7 +105,7 @@ export default () => {
 				Denne siden er under utvikling.
 			</Alert>
 			<StatusBox nesteKjoring={statusData.nesteKjoring} status={statusData.status}/>
-			{!statusData.status ? <Button loading={henterStatus} onClick={aktiverScheduler}>Aktiver</Button> : <Button loading={henterStatus} onClick={deaktiverScheduler}>Deaktiver</Button> }
+			{!statusData.status ? <Button loading={henterStatus} onClick={aktiverScheduler}>Aktiver</Button> : <Button loading={henterStatus} onClick={deaktiverScheduler} variant={"danger"}>Deaktiver</Button> }
 			<AppstyringTable data={apiData} setData={setApiData}/>
 		</>
 	)
