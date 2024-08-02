069e00c56d79afdfb24844fe16e2ebc6b0d6764e
Martineem <martine.erdal.mansaker@nav.no>
2024-08-02 13:04:18 +0200

    Fjerner bruk av ereg

--- apps/levende-arbeidsforhold-ansettelse/src/main/java/no/nav/registre/testnorge/levendearbeidsforholdansettelse/domain/organisasjon/Adresse.java
+++ apps/levende-arbeidsforhold-ansettelse/src/main/java/no/nav/registre/testnorge/levendearbeidsforholdansettelse/domain/organisasjon/Adresse.java
@@ -25,16 +25,6 @@ public class Adresse {
         this.poststed = dto.getPoststed();
     }
 
-    public Adresse(no.nav.testnav.libs.dto.organisasjon.v1.AdresseDTO dto) {
-        this.kommunenummer = dto.getKommunenummer();
-        this.adresselinje1 = dto.getAdresselinje1();
-        this.adresselinje2 = dto.getAdresselinje2();
-        this.adresselinje3 = dto.getAdresselinje3();
-        this.landkode = dto.getLandkode();
-        this.postnummer = dto.getPostnummer();
-        this.poststed = dto.getPoststed();
-    }
-
     public no.nav.testnav.libs.dto.organisasjon.v1.AdresseDTO toDTO() {
         return no.nav.testnav.libs.dto.organisasjon.v1.AdresseDTO.builder()
                 .kommunenummer(kommunenummer)
@@ -43,22 +33,6 @@ public class Adresse {
                 .adresselinje3(adresselinje3)
                 .landkode(landkode)
                 .postnummer(postnummer)
-                .poststed(poststed)
-                .build();
-    }
-
-
-    public no.nav.testnav.libs.dto.eregmapper.v1.AdresseDTO toAdresseDTO() {
-        return no.nav.testnav.libs.dto.eregmapper.v1.AdresseDTO
-                .builder()
-                .adresser(Arrays.asList(
-                        adresselinje1,
-                        adresselinje2,
-                        adresselinje3
-                ))
-                .kommunenr(kommunenummer)
-                .landkode(landkode)
-                .postnr(postnummer)
                 .poststed(poststed)
                 .build();
     }
