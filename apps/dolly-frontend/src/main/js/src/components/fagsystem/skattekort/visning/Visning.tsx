3e293e6dada82d427e011068dd9fc6a0569af8dd
Betsy Carina Traran <betsy.carina.traran@nav.no>
2024-08-12 12:09:53 +0200

    Vis xml av skattekort

--- apps/dolly-frontend/src/main/js/src/components/fagsystem/skattekort/visning/Visning.tsx
+++ apps/dolly-frontend/src/main/js/src/components/fagsystem/skattekort/visning/Visning.tsx
@@ -4,10 +4,13 @@ import SubOverskrift from '@/components/ui/subOverskrift/SubOverskrift'
 import { Alert } from '@navikt/ds-react'
 import { ErrorBoundary } from '@/components/ui/appError/ErrorBoundary'
 import { TitleValue } from '@/components/ui/titleValue/TitleValue'
-import { arrayToString, formatDate } from '@/utils/DataFormatter'
+import { arrayToString, formatDate, formatXml } from '@/utils/DataFormatter'
 import { DollyFieldArray } from '@/components/ui/form/fieldArray/DollyFieldArray'
 import { useSkattekortKodeverk } from '@/utils/hooks/useSkattekort'
 import { ForskuddstrekkVisning } from '@/components/fagsystem/skattekort/visning/ForskuddstrekkVisning'
+import Button from '@/components/ui/button/Button'
+import useBoolean from '@/utils/hooks/useBoolean'
+import { CodeView } from '@/components/codeView'
 
 type SkattekortVisning = {
 	liste?: Array<SkattekortData>
@@ -48,7 +51,9 @@ export const SkattekortVisning = ({ liste, loading }: SkattekortVisning) => {
 			) : (
 				<ErrorBoundary>
 					<DollyFieldArray header="" data={liste} expandable={liste.length > 5} nested>
-						{(skattekort: SkattekortData) => {
+						{(skattekort: SkattekortData, idx: number) => {
+							const [viserXml, vis, skjul] = useBoolean(false)
+
 							const arbeidsgiver = skattekort?.arbeidsgiver?.[0]
 							const arbeidstaker = arbeidsgiver?.arbeidstaker?.[0]
 							const trekkListe = arbeidstaker?.skattekort?.forskuddstrekk
@@ -59,38 +64,57 @@ export const SkattekortVisning = ({ liste, loading }: SkattekortVisning) => {
 								},
 							)
 
+							const xmlFormatted = formatXml(skattekort?.skattekortXml, '  ')
+
 							return (
-								<div className="person-visning_content">
-									<TitleValue
-										title="Resultat på forespørsel"
-										value={showKodeverkLabel(
-											'RESULTATSTATUS',
-											arbeidstaker?.resultatPaaForespoersel,
-										)}
-									/>
-									<TitleValue title="Inntektsår" value={arbeidstaker?.inntektsaar} />
-									<TitleValue
-										title="Utstedt dato"
-										value={formatDate(arbeidstaker?.skattekort?.utstedtDato)}
-									/>
-									<TitleValue
-										title="Skattekortidentifikator"
-										value={arbeidstaker?.skattekort?.skattekortidentifikator}
-									/>
-									<TitleValue
-										title="Tilleggsopplysning"
-										value={arrayToString(tilleggsopplysningFormatted)}
-									/>
-									<TitleValue
-										title="Arbeidsgiver (org.nr.)"
-										value={arbeidsgiver?.arbeidsgiveridentifikator?.organisasjonsnummer}
-									/>
-									<TitleValue
-										title="Arbeidsgiver (ident)"
-										value={arbeidsgiver?.arbeidsgiveridentifikator?.personidentifikator}
-									/>
-									<ForskuddstrekkVisning trekkliste={trekkListe} />
-								</div>
+								<React.Fragment key={idx}>
+									<div className="person-visning_content">
+										<TitleValue
+											title="Resultat på forespørsel"
+											value={showKodeverkLabel(
+												'RESULTATSTATUS',
+												arbeidstaker?.resultatPaaForespoersel,
+											)}
+										/>
+										<TitleValue title="Inntektsår" value={arbeidstaker?.inntektsaar} />
+										<TitleValue
+											title="Utstedt dato"
+											value={formatDate(arbeidstaker?.skattekort?.utstedtDato)}
+										/>
+										<TitleValue
+											title="Skattekortidentifikator"
+											value={arbeidstaker?.skattekort?.skattekortidentifikator}
+										/>
+										<TitleValue
+											title="Tilleggsopplysning"
+											value={arrayToString(tilleggsopplysningFormatted)}
+										/>
+										<TitleValue
+											title="Arbeidsgiver (org.nr.)"
+											value={arbeidsgiver?.arbeidsgiveridentifikator?.organisasjonsnummer}
+										/>
+										<TitleValue
+											title="Arbeidsgiver (ident)"
+											value={arbeidsgiver?.arbeidsgiveridentifikator?.personidentifikator}
+										/>
+										<ForskuddstrekkVisning trekkliste={trekkListe} />
+									</div>
+									<Button
+										onClick={viserXml ? skjul : vis}
+										kind={viserXml ? 'chevron-up' : 'chevron-down'}
+										style={{ position: 'initial', paddingTop: '0' }}
+									>
+										{(viserXml ? 'SKJUL ' : 'VIS ') + 'SKATTEKORT-XML'}
+									</Button>
+									{viserXml &&
+										(skattekort?.skattekortXml ? (
+											<CodeView language="xml" code={xmlFormatted} wrapLongLines />
+										) : (
+											<Alert variant="error" size="small" inline>
+												Kunne ikke vise skattekort-xml
+											</Alert>
+										))}
+								</React.Fragment>
 							)
 						}}
 					</DollyFieldArray>
--- apps/dolly-frontend/src/main/js/src/utils/DataFormatter.tsx
+++ apps/dolly-frontend/src/main/js/src/utils/DataFormatter.tsx
@@ -243,3 +243,14 @@ export const getYearRangeOptions = (start, stop) => {
 	}
 	return years.reverse()
 }
+
+export const formatXml = (xml: string, tab = '\t') => {
+	let formatted = ''
+	let indent = ''
+	xml.split(/>\s*</).forEach(function (node) {
+		if (node.match(/^\/\w/)) indent = indent.substring(tab.length)
+		formatted += indent + '<' + node + '>\r\n'
+		if (node.match(/^<?\w[^>]*[^\/]$/)) indent += tab
+	})
+	return formatted.substring(1, formatted.length - 3)
+}
