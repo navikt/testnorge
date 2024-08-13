4879b9233b28e8454bf02eff9ec7c013101b2af9
kristenhaerum <kristen.herum@nav.no>
2024-08-13 09:56:36 +0200

    Remove Pensjonsavtaler form and visning components #deploy-test-frontend
    Deleted `Pensjonsavtaler.tsx` and `PensjonsavtaleVisning.tsx` components due to refactoring and removal of deprecated functionality. Updated the `initalValues.tsx` file to streamline the initial payment period data structure.

--- apps/dolly-frontend/src/main/js/src/components/fagsystem/pensjonsavtale/initalValues.tsx
+++ apps/dolly-frontend/src/main/js/src/components/fagsystem/pensjonsavtale/initalValues.tsx
@@ -1,23 +1,15 @@
+export const initialUtbetalingsperiode = {
+	startAlderAar: 62,
+	startAlderMaaned: 1,
+	sluttAlderAar: 72,
+	sluttAlderMaaned: 12,
+	aarligUtbetaling: 30000,
+}
+
 export const initialPensjonsavtale = [
 	{
 		produktBetegnelse: 'Test av pensjonsavtale',
 		avtaleKategori: 'PRIVAT_TJENESTEPENSJON',
-		utbetalingsperioder: [
-			{
-				startAlderAar: 62,
-				startAlderMaaned: 1,
-				sluttAlderAar: 72,
-				sluttAlderMaaned: 12,
-				aarligUtbetaling: 30000,
-			},
-		],
+		utbetalingsperioder: [initialUtbetalingsperiode],
 	},
 ]
-
-export const initialUtbetalingsperiode = {
-	startAlderAar: 67,
-	startAlderMaaned: 1,
-	sluttAlderAar: undefined,
-	sluttAlderMaaned: undefined,
-	aarligUtbetaling: 10000,
-}
