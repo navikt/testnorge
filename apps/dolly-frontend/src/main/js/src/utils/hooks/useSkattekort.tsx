3a02b6111bebb3000b39581115dd1473a1b2cb96
Betsy Carina Traran <betsy.carina.traran@nav.no>
2024-08-14 10:25:44 +0200

    Hent skattekort fra skattekort-service

--- apps/dolly-frontend/src/main/js/src/utils/hooks/useSkattekort.tsx
+++ apps/dolly-frontend/src/main/js/src/utils/hooks/useSkattekort.tsx
@@ -1,8 +1,37 @@
 import useSWR from 'swr'
 import { fetcher } from '@/api'
 
+const baseUrl = '/testnav-skattekort-service/api/v1'
+
+const getSkattekortUrl = (ident: string) => {
+	return `${baseUrl}/skattekort?ident=${ident}`
+}
+
 const getKodeverkUrl = (kodeverkstype: string) => {
-	return `/testnav-skattekort-service/api/v1/kodeverk?kodeverkstype=${kodeverkstype}`
+	return `${baseUrl}/kodeverk?kodeverkstype=${kodeverkstype}`
+}
+
+export const useSkattekort = (ident: string, harSkattekortBestilling: boolean) => {
+	if (!ident) {
+		return {
+			loading: false,
+			error: 'Ident mangler!',
+		}
+	}
+
+	if (!harSkattekortBestilling) {
+		return {
+			loading: false,
+		}
+	}
+
+	const { data, isLoading, error } = useSWR<any, Error>(getSkattekortUrl(ident), fetcher)
+
+	return {
+		skattekort: data,
+		loading: isLoading,
+		error: error,
+	}
 }
 
 export const useSkattekortKodeverk = (kodeverkstype: string) => {
