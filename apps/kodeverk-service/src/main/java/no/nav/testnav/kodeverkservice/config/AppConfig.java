4d37a631fea265d6fd47049b4de2554f95110da0
stigus <stian.gustavsson@nav.no>
2024-08-23 14:15:27 +0200

    * Endret kodeverk-service til å støtte token fra idporten og route denne korrekt videre til kodeverk-api #deploy-kodeverk-service

--- apps/kodeverk-service/src/main/java/no/nav/testnav/kodeverkservice/config/AppConfig.java
+++ apps/kodeverk-service/src/main/java/no/nav/testnav/kodeverkservice/config/AppConfig.java
@@ -1,14 +1,14 @@
 package no.nav.testnav.kodeverkservice.config;
 
-import no.nav.testnav.libs.reactivecore.config.CoreConfig;
-import no.nav.testnav.libs.reactivesecurity.config.SecureOAuth2ServerToServerConfiguration;
+import no.nav.testnav.libs.servletcore.config.ApplicationCoreConfig;
+import no.nav.testnav.libs.standalone.servletsecurity.config.InsecureJwtServerToServerConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
 @Import({
-        CoreConfig.class,
-        SecureOAuth2ServerToServerConfiguration.class
+        ApplicationCoreConfig.class,
+        InsecureJwtServerToServerConfiguration.class
 })
 public class AppConfig {
 }
