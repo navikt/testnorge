0005d812877811dab091fe5522d993c714c8059d
Stian Gustavsson <stian.gustavsson@nav.no>
2024-08-27 09:27:59 +0200

    * Oppdatert spring til versjon 3.2.2 og backend rammeverk til nyere versjoner (#3591)
    * Oppdatert spring til versjon 3.2.2 og backend rammeverk til nyere versjoner

--- apps/dolly-backend/src/main/java/no/nav/dolly/elastic/BestillingElasticRepository.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/elastic/BestillingElasticRepository.java
@@ -2,9 +2,10 @@ package no.nav.dolly.elastic;
 
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface BestillingElasticRepository extends ElasticsearchRepository<ElasticBestilling, Long> {
 
-    List<ElasticBestilling> getAllByIdenterIn(List<String> ident);
+    List<ElasticBestilling> getAllByIdenterIn(Collection<String> identer);
 }
