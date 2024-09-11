1ddb0c1e128eea8d6759f58551eb99c1ff520bc4
kristenhaerum <kristen.herum@nav.no>
2024-09-11 15:53:20 +0200

    Add 'isFunnet' method to OrganisasjonDTO
    Introduce 'isFunnet' method to check if the juridiskEnhet is found. This includes defining a constant NOT_FOUND and utilizing @JsonIgnore for the method. This enhancement is crucial for distinguishing valid organizational entities.

--- libs/data-transfer-objects/src/main/java/no/nav/testnav/libs/dto/organisasjon/v1/OrganisasjonDTO.java
+++ libs/data-transfer-objects/src/main/java/no/nav/testnav/libs/dto/organisasjon/v1/OrganisasjonDTO.java
@@ -1,5 +1,6 @@
 package no.nav.testnav.libs.dto.organisasjon.v1;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -8,12 +9,16 @@ import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+import static java.util.Objects.nonNull;
+
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(force = true)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class OrganisasjonDTO {
+
+    public static final String NOT_FOUND = "Ikke funnet";
 
     private final String error;
     private final String orgnummer;
@@ -24,5 +29,10 @@ public class OrganisasjonDTO {
     private final AdresseDTO forretningsadresser;
     private final String redigertnavn;
     private final List<String> driverVirksomheter;
-}
 
+    @JsonIgnore
+    public boolean isFunnet() {
+
+        return nonNull(juridiskEnhet) && !NOT_FOUND.equals(juridiskEnhet);
+    }
+}
