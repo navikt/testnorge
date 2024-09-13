4cd2a951051c88d525d1a13369b7d961b6158db7
kristenhaerum <kristen.herum@nav.no>
2024-09-13 09:17:44 +0200

    Deploy #deploy-ereg-batch-status-service #deploy-organisasjon-bestilling-service

--- apps/ereg-batch-status-service/src/main/java/no/nav/registre/testnorge/eregbatchstatusservice/EregbatchStatusServiceApplicationStarter.java
+++ apps/ereg-batch-status-service/src/main/java/no/nav/registre/testnorge/eregbatchstatusservice/EregbatchStatusServiceApplicationStarter.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EregbatchStatusServiceApplicationStarter {
+
     public static void main(String[] args) {
         SpringApplication.run(EregbatchStatusServiceApplicationStarter.class, args);
     }
