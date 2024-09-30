97c0ea0bcc7def5a61cc4ed54aaa09819cfedd53
stigus <stian.gustavsson@nav.no>
2024-09-30 14:33:22 +0200

    * Pdl fullmakt proxy lagt på audience #deploy-proxy-fullmakt

--- proxies/fullmakt-proxy/src/main/java/no/nav/testnav/proxies/fullmaktproxy/consumer/command/FakedingsGetCommand.java
+++ proxies/fullmakt-proxy/src/main/java/no/nav/testnav/proxies/fullmaktproxy/consumer/command/FakedingsGetCommand.java
@@ -1,12 +1,14 @@
 package no.nav.testnav.proxies.fullmaktproxy.consumer.command;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 import java.util.concurrent.Callable;
 
 @RequiredArgsConstructor
+@Slf4j
 public class FakedingsGetCommand implements Callable<Mono<String>> {
 
     private static final String FAKEDINGS_URL = "/fake/idporten";
@@ -19,9 +21,11 @@ public class FakedingsGetCommand implements Callable<Mono<String>> {
         return webClient.get()
                 .uri(uriBuilder -> uriBuilder.path(FAKEDINGS_URL)
                         .queryParam("pid", ident)
+                        .queryParam("aud", "dev-fss:pdl:pdl-fullmakt")
                         .queryParam("acr", "Level4")
                         .build())
                 .retrieve()
-                .bodyToMono(String.class);
+                .bodyToMono(String.class)
+                .doOnError(throwable -> log.error("Feil ved henting av fakedings token i fullmakt-proxy", throwable));
     }
 }
