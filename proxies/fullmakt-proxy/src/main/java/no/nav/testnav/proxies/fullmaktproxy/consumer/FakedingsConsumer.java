5c8d74913f4dbe7fb2728414c81acf4c7bad5590
stigus <stian.gustavsson@nav.no>
2024-10-18 09:53:10 +0200

    * Pdl fullmakt proxy kommentar endring og deploy #deploy-proxy-fullmakt

--- proxies/fullmakt-proxy/src/main/java/no/nav/testnav/proxies/fullmaktproxy/consumer/FakedingsConsumer.java
+++ proxies/fullmakt-proxy/src/main/java/no/nav/testnav/proxies/fullmaktproxy/consumer/FakedingsConsumer.java
@@ -1,8 +1,6 @@
 package no.nav.testnav.proxies.fullmaktproxy.consumer;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import no.nav.testnav.proxies.fullmaktproxy.consumer.command.FakedingsGetCommand;
-import no.nav.testnav.proxies.fullmaktproxy.util.JacksonExchangeStrategyUtil;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -13,11 +11,10 @@ public class FakedingsConsumer {
     private static final String FAKE_TOKENDINGS_URL = "https://fakedings.intern.dev.nav.no";
     private final WebClient webClient;
 
-    public FakedingsConsumer(ObjectMapper objectMapper) {
+    public FakedingsConsumer() {
 
         this.webClient = WebClient.builder()
                 .baseUrl(FAKE_TOKENDINGS_URL)
-                .exchangeStrategies(JacksonExchangeStrategyUtil.getJacksonStrategy(objectMapper))
                 .build();
     }
 
