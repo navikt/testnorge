b70c4e08a030380639fb80c97365e93d4a09a72e
stigus <stian.gustavsson@nav.no>
2024-10-18 10:22:15 +0200

    * Pdl fullmakt proxy kommentar endring og deploy #deploy-proxy-fullmakt

--- proxies/fullmakt-proxy/src/main/java/no/nav/testnav/proxies/fullmaktproxy/config/LocalConfig.java
+++ proxies/fullmakt-proxy/src/main/java/no/nav/testnav/proxies/fullmaktproxy/config/LocalConfig.java
@@ -1,6 +1,5 @@
 package no.nav.testnav.proxies.fullmaktproxy.config;
 
-import no.nav.testnav.libs.database.config.VaultHikariConfiguration;
 import no.nav.testnav.libs.vault.AbstractLocalVaultConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -9,7 +8,6 @@ import org.springframework.context.annotation.Profile;
 @Configuration
 @Profile("local")
 @Import({
-        VaultHikariConfiguration.class
 })
 public class LocalConfig extends AbstractLocalVaultConfiguration {
 } 
