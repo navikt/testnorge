636975b755544ef35975b9215c8cbd26cd109ef5
Kristen.Herum <krharum@gmail.com>
2024-10-21 14:41:55 +0200

    Add protocol support for R2DBC configuration #deploy-levende-arbeidsforhold-ansettelse
    Introduced protocol field in R2DBC configuration to enhance flexibility. Adjusted configurations to conditionally include host and protocol options if they are present. Updated application configuration files to use the new protocol setting.

--- apps/levende-arbeidsforhold-ansettelse/src/main/java/no/nav/testnav/levendearbeidsforholdansettelse/config/ApplicationConfig.java
+++ apps/levende-arbeidsforhold-ansettelse/src/main/java/no/nav/testnav/levendearbeidsforholdansettelse/config/ApplicationConfig.java
@@ -3,6 +3,7 @@ package no.nav.testnav.levendearbeidsforholdansettelse.config;
 import io.r2dbc.spi.ConnectionFactories;
 import io.r2dbc.spi.ConnectionFactory;
 import io.r2dbc.spi.ConnectionFactoryOptions;
+import lombok.extern.slf4j.Slf4j;
 import no.nav.testnav.libs.reactivecore.config.CoreConfig;
 import no.nav.testnav.libs.reactivesecurity.config.SecureOAuth2ServerToServerConfiguration;
 import no.nav.testnav.libs.standalone.servletsecurity.config.InsecureJwtServerToServerConfiguration;
@@ -20,8 +21,11 @@ import static io.r2dbc.spi.ConnectionFactoryOptions.DRIVER;
 import static io.r2dbc.spi.ConnectionFactoryOptions.HOST;
 import static io.r2dbc.spi.ConnectionFactoryOptions.PASSWORD;
 import static io.r2dbc.spi.ConnectionFactoryOptions.PORT;
+import static io.r2dbc.spi.ConnectionFactoryOptions.PROTOCOL;
 import static io.r2dbc.spi.ConnectionFactoryOptions.USER;
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
 
+@Slf4j
 @Configuration
 @Import({
         CoreConfig.class,
@@ -37,8 +41,11 @@ public class ApplicationConfig {
     @Value("${config.r2dbc.driver}")
     private String driver;
 
-    @Value("${config.r2dbc.host}")
+    @Value("${config.r2dbc.host:null}")
     private String host;
+
+    @Value("${config.r2dbc.protocol:null}")
+    private String protocol;
 
     @Value("${config.r2dbc.port}")
     private int port;
@@ -56,14 +63,21 @@ public class ApplicationConfig {
     @Bean
     ConnectionFactory connectionFactory() {
 
-        return ConnectionFactories.get(ConnectionFactoryOptions.builder()
+        var factory = ConnectionFactoryOptions.builder()
                 .option(DRIVER, driver)
-                .option(HOST, host)
                 .option(PORT, port)
                 .option(USER, username)
                 .option(PASSWORD, password)
-                .option(DATABASE, database)
-                .build());
+                .option(DATABASE, database);
+
+        if (isNotBlank(host)) {
+            factory.option(HOST, host);
+        }
+        if (isNotBlank(protocol)) {
+            factory.option(PROTOCOL, protocol);
+        }
+
+        log.info("ConnectionFactory: {}", factory.build());
+        return ConnectionFactories.get(factory.build());
     }
 }
-
