cbd01e499fc77739a6859ce34f19dd4a7e498dfa
stigus <stian.gustavsson@nav.no>
2024-10-25 09:59:58 +0200

    * Mindre endringer for Histark Post oppsett #deploy-test-dolly-backend

--- apps/dolly-backend/src/main/java/no/nav/dolly/bestilling/histark/command/HistarkPostCommand.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/bestilling/histark/command/HistarkPostCommand.java
@@ -33,32 +33,33 @@ public class HistarkPostCommand implements Callable<Flux<HistarkResponse>> {
     @Override
     public Flux<HistarkResponse> call() {
 
-        return Flux.fromIterable(histarkRequest.getHistarkDokumenter()).flatMap(histarkDokument -> {
-            var bodyBuilder = new MultipartBodyBuilder();
-            bodyBuilder.part("file", histarkDokument.getFile().getBytes(StandardCharsets.UTF_8));
-            bodyBuilder.part("metadata", histarkDokument.getMetadata().toString());
-            return webClient.post()
-                    .uri(builder ->
-                            builder.path("/api/saksmapper/import").build())
-                    .header(AUTHORIZATION, "Bearer " + token)
-                    .contentType(MediaType.MULTIPART_FORM_DATA)
-                    .header(HttpHeaders.CONTENT_TYPE, MediaType.MULTIPART_FORM_DATA_VALUE)
-                    .body(BodyInserters.fromMultipartData(bodyBuilder.build()))
-                    .retrieve()
-                    .bodyToMono(Object.class)
-                    .doOnNext(response -> log.info("Histark response: {}", response))
-                    .map(response -> {
-                        log.info("Histark response: {}", response);
-                        return HistarkResponse.builder()
-                                .histarkId(Json.pretty(response))
-                                .build();
-                    })
-                    .retryWhen(Retry.backoff(3, Duration.ofSeconds(5))
-                            .filter(WebClientFilter::is5xxException))
-                    .doOnError(WebClientFilter::logErrorMessage)
-                    .onErrorResume(error -> Mono.just(HistarkResponse.builder()
-                            .feilmelding(WebClientFilter.getMessage(error))
-                            .build()));
-        });
+        return Flux.fromIterable(histarkRequest.getHistarkDokumenter())
+                .flatMap(histarkDokument -> {
+                    var bodyBuilder = new MultipartBodyBuilder();
+                    bodyBuilder.part("file", histarkDokument.getFile().getBytes(StandardCharsets.UTF_8));
+                    bodyBuilder.part("metadata", histarkDokument.getMetadata().toString());
+                    return webClient.post()
+                            .uri(builder ->
+                                    builder.path("/api/saksmapper/import").build())
+                            .header(AUTHORIZATION, "Bearer " + token)
+                            .contentType(MediaType.MULTIPART_FORM_DATA)
+                            .header(HttpHeaders.CONTENT_TYPE, MediaType.MULTIPART_FORM_DATA_VALUE)
+                            .body(BodyInserters.fromMultipartData(bodyBuilder.build()))
+                            .retrieve()
+                            .bodyToMono(String.class)
+                            .doOnNext(response -> log.info("Histark response: {}", response))
+                            .map(response -> {
+                                log.info("Histark response: {}", response);
+                                return HistarkResponse.builder()
+                                        .histarkId(Json.pretty(response))
+                                        .build();
+                            })
+                            .retryWhen(Retry.backoff(3, Duration.ofSeconds(5))
+                                    .filter(WebClientFilter::is5xxException))
+                            .doOnError(WebClientFilter::logErrorMessage)
+                            .onErrorResume(error -> Mono.just(HistarkResponse.builder()
+                                    .feilmelding(WebClientFilter.getMessage(error))
+                                    .build()));
+                });
     }
 }
