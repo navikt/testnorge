c7a614b0f8afb800c1d9757634946b84ebbac300
stigus <stian.gustavsson@nav.no>
2024-10-25 15:14:24 +0200

    * Øker buffer size for Backend og Histark Proxy #deploy-test-dolly-backend #deploy-proxy-histark

--- apps/dolly-backend/src/main/java/no/nav/dolly/bestilling/histark/command/HistarkPostCommand.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/bestilling/histark/command/HistarkPostCommand.java
@@ -7,6 +7,7 @@ import no.nav.dolly.bestilling.histark.domain.HistarkResponse;
 import no.nav.testnav.libs.reactivecore.utils.WebClientFilter;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.MultipartBodyBuilder;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -36,9 +37,8 @@ public class HistarkPostCommand implements Callable<Flux<HistarkResponse>> {
                 .flatMap(histarkDokument -> {
                     var bodyBuilder = new MultipartBodyBuilder();
                     bodyBuilder.part("file", histarkDokument.getFile().getBytes(StandardCharsets.UTF_8));
-                    bodyBuilder.part("metadata", histarkDokument.getMetadata().toString());
+                    bodyBuilder.part("metadata", histarkDokument.getMetadata());
                     var body = bodyBuilder.build();
-                    log.info("Sending histark body: {}", body);
                     return webClient.post()
                             .uri(builder ->
                                     builder.path("/api/saksmapper/import").build())
@@ -46,18 +46,16 @@ public class HistarkPostCommand implements Callable<Flux<HistarkResponse>> {
                             .contentType(MediaType.MULTIPART_FORM_DATA)
                             .header(HttpHeaders.CONTENT_TYPE, MediaType.MULTIPART_FORM_DATA_VALUE)
                             .body(BodyInserters.fromMultipartData(body))
-                            .retrieve()
-                            .bodyToMono(String.class)
-                            .doOnNext(response -> log.info("Histark post response: {}", response))
-                            .map(response -> HistarkResponse.builder()
-                                    .histarkId(response)
-                                    .build())
+                            .exchangeToMono(clientResponse -> {
+                                log.info("Status fra histark: {}", clientResponse.statusCode());
+                                return clientResponse.toEntity(HistarkResponse.class);
+                            })
+                            .mapNotNull(ResponseEntity::getBody)
+                            .doOnNext(response -> log.info("Response fraa histark: {}", response))
                             .retryWhen(Retry.backoff(3, Duration.ofSeconds(5))
                                     .filter(WebClientFilter::is5xxException))
                             .doOnError(WebClientFilter::logErrorMessage)
-                            .onErrorResume(error -> Mono.just(HistarkResponse.builder()
-                                    .feilmelding(WebClientFilter.getMessage(error))
-                                    .build()));
+                            .onErrorResume(throwable -> Mono.empty());
                 });
     }
 }
