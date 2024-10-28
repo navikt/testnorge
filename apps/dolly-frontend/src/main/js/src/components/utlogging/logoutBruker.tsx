0576e59d2ab59e94b5f956d7e3a6e6384972e165
Stian Gustavsson <stian.gustavsson@nav.no>
2024-10-28 17:57:23 +0100

    * Logger ut bruker ved feil under henting av bruker/org i dolly-idporten (#3662)

--- apps/dolly-frontend/src/main/js/src/components/utlogging/logoutBruker.tsx
+++ apps/dolly-frontend/src/main/js/src/components/utlogging/logoutBruker.tsx
@@ -2,21 +2,7 @@ const logoutBruker = (feilmelding?: string) => {
 	const runningLocalOrIdporten =
 		window.location.hostname.includes('localhost') || window.location.hostname.includes('idporten')
 
-	const extractFeilmelding = (stackTrace: string) => {
-		if (stackTrace?.includes('miljoer')) {
-			return 'miljoe_error'
-		} else if (stackTrace?.includes('current')) {
-			return 'azure_error'
-		} else if (stackTrace?.includes('person_org')) {
-			return 'person_org_error'
-		} else {
-			return 'unknown_error'
-		}
-	}
-
-	window.location.href = runningLocalOrIdporten
-		? '/logout' + (feilmelding ? '?state=' + extractFeilmelding(feilmelding) : '')
-		: '/oauth2/logout'
+	window.location.href = runningLocalOrIdporten ? '/logout' : '/oauth2/logout'
 	console.error(feilmelding)
 }
 export default logoutBruker
