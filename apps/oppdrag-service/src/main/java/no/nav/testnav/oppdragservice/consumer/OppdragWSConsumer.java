4bea7061ce652cab963f5937d0a63287f4202ae3
Kristen.Herum <krharum@gmail.com>
2024-11-07 08:35:09 +0100

    Handle SendInnOppdragFeilUnderBehandling exception #deploy-oppdrag-service
    Add a try-catch block to specifically catch and log details of SendInnOppdragFeilUnderBehandling exceptions. This improves error logging by providing more context about the error cause and source, while still logging general exceptions.

--- apps/oppdrag-service/src/main/java/no/nav/testnav/oppdragservice/consumer/OppdragWSConsumer.java
+++ apps/oppdrag-service/src/main/java/no/nav/testnav/oppdragservice/consumer/OppdragWSConsumer.java
@@ -3,6 +3,7 @@ package no.nav.testnav.oppdragservice.consumer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.testnav.oppdragservice.config.ServerProperties;
+import no.nav.testnav.oppdragservice.wsdl.SendInnOppdragFeilUnderBehandling;
 import no.nav.testnav.oppdragservice.wsdl.SendInnOppdragRequest;
 import no.nav.testnav.oppdragservice.wsdl.SendInnOppdragResponse;
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
@@ -21,7 +22,23 @@ public class OppdragWSConsumer extends WebServiceGatewaySupport {
         var url = "%s:%s%s".formatted(props.getHost(),
                 props.getPorts().get(miljoe), OPPDRAG_URL);
 
-        return (SendInnOppdragResponse) getWebServiceTemplate()
-                .marshalSendAndReceive(url, melding, new SoapActionCallback(""));
+        try {
+            return (SendInnOppdragResponse) getWebServiceTemplate()
+                    .marshalSendAndReceive(url, melding, new SoapActionCallback(""));
+
+        } catch (Exception e) {
+
+            if (e instanceof SendInnOppdragFeilUnderBehandling sendInnOppdragFeilUnderBehandling) {
+                log.error("SendInnOppdragFeilUnderBehandling message: {}, rootCause: {}, errorSource; {}",
+                        sendInnOppdragFeilUnderBehandling.getMessage(),
+                        sendInnOppdragFeilUnderBehandling.getFaultInfo().getRootCause(),
+                sendInnOppdragFeilUnderBehandling.getFaultInfo().getErrorSource(), e);
+
+            } else {
+                log.error(e.getMessage(), e);
+            }
+
+            throw e;
+        }
     }
 }
