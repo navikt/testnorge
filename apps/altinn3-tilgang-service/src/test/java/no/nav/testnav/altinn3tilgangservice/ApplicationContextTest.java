90e7cb9856313ce41828fc8c781f45b2efcecfe1
Kristen.Herum <krharum@gmail.com>
2024-11-15 08:00:54 +0100

    Add SecretManagerServiceClient mock to ApplicationContextTest
    This change removes the `@Disabled` annotation and includes a mock for `SecretManagerServiceClient` in `ApplicationContextTest`. This addition ensures the application context loads correctly with the secret manager dependency mocked.

--- apps/altinn3-tilgang-service/src/test/java/no/nav/testnav/altinn3tilgangservice/ApplicationContextTest.java
+++ apps/altinn3-tilgang-service/src/test/java/no/nav/testnav/altinn3tilgangservice/ApplicationContextTest.java
@@ -1,19 +1,21 @@
 package no.nav.testnav.altinn3tilgangservice;
 
-import org.junit.jupiter.api.Disabled;
+import com.google.cloud.secretmanager.v1.SecretManagerServiceClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.test.context.ActiveProfiles;
 
-@Disabled
 @SpringBootTest
 @ActiveProfiles("test")
 class ApplicationContextTest {
 
     @MockBean
     public ReactiveJwtDecoder jwtDecoder;
+
+    @MockBean
+    public SecretManagerServiceClient secretManagerClient;
 
     @Test
     @SuppressWarnings("java:S2699")
