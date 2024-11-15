fa9de3c68acc2cff8f33d0a1b233b2f5be57c961
Kristen.Herum <krharum@gmail.com>
2024-11-15 07:34:17 +0100

    Remove DeleteStatus DTO and update configuration #deploy-altinn3-tilgang-service
    Deleted the DeleteStatus.java file due to its redundancy in the codebase. Modified the configuration file to exclude a deprecated host and re-enabled ApplicationContext tests. These changes result in a cleaner and more efficient codebase.

--- apps/altinn3-tilgang-service/src/test/java/no/nav/testnav/altinn3tilgangservice/ApplicationContextTest.java
+++ apps/altinn3-tilgang-service/src/test/java/no/nav/testnav/altinn3tilgangservice/ApplicationContextTest.java
@@ -1,13 +1,11 @@
 package no.nav.testnav.altinn3tilgangservice;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.test.context.ActiveProfiles;
 
-@Disabled
 @SpringBootTest
 @ActiveProfiles("test")
 class ApplicationContextTest {
