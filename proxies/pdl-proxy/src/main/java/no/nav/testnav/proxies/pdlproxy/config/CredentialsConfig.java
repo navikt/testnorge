ca0a4962a2daff5ccb8a33d71ab80593e1aea202
Cato Olsen <olsen.cato@gmail.com>
2024-11-15 13:13:29 +0100

    - Testing Kubernetes secrets.

--- proxies/pdl-proxy/src/main/java/no/nav/testnav/proxies/pdlproxy/config/CredentialsConfig.java
+++ proxies/pdl-proxy/src/main/java/no/nav/testnav/proxies/pdlproxy/config/CredentialsConfig.java
@@ -1,27 +1,45 @@
 package no.nav.testnav.proxies.pdlproxy.config;
 
+import lombok.extern.slf4j.Slf4j;
 import no.nav.testnav.proxies.pdlproxy.CredentialsHolder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Slf4j
 public class CredentialsConfig {
 
-    @Value("${hendelse.lager.api.key}")
+    @Value("${app.hendelse.lager.api.key}")
     private String hendelselagerApiKey;
 
-    @Value("${person.aktor.admin.api}")
+    @Value("${app.person.aktor.admin.api}")
     private String aktoerAdminApiKey;
 
-    @Value("${elastic.username}")
+    @Value("${app.elastic.username}")
     private String elasticUsername;
 
-    @Value("${elastic.password}")
+    @Value("${app.elastic.password}")
     private String elasticPassword;
+
+    @Value("${hendelse.lager.api.key}")
+    private String oldHendelselagerApiKey;
+
+    @Value("${person.aktor.admin.api}")
+    private String oldAktoerAdminApiKey;
+
+    @Value("${elastic.username}")
+    private String oldElasticUsername;
+
+    @Value("${elastic.password}")
+    private String oldElasticPassword;
 
     @Bean
     CredentialsHolder credentialsHolder() {
+        log.info("Checking aktoerAdminApiKey: {}", (aktoerAdminApiKey.equals(oldAktoerAdminApiKey)));
+        log.info("Checking hendelselagerApiKey: {}", (hendelselagerApiKey.equals(oldHendelselagerApiKey)));
+        log.info("Checking elasticUsername: {}", (elasticUsername.equals(oldElasticUsername)));
+        log.info("Checking elasticPassword: {}", (elasticPassword.equals(oldElasticPassword)));
         return new CredentialsHolder(hendelselagerApiKey, aktoerAdminApiKey, elasticUsername, elasticPassword);
     }
 
