7280021f1c142a8ffcc64f5e936e76936a1ebadf
Cato Olsen <olsen.cato@gmail.com>
2024-11-19 16:06:00 +0100

    - Moves from Vault to Secret Manager for local runs.

--- proxies/pdl-proxy/src/main/java/no/nav/testnav/proxies/pdlproxy/config/CredentialsConfig.java
+++ proxies/pdl-proxy/src/main/java/no/nav/testnav/proxies/pdlproxy/config/CredentialsConfig.java
@@ -1,13 +1,11 @@
 package no.nav.testnav.proxies.pdlproxy.config;
 
-import lombok.extern.slf4j.Slf4j;
 import no.nav.testnav.proxies.pdlproxy.CredentialsHolder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@Slf4j
 public class CredentialsConfig {
 
     @Value("${app.hendelse.lager.api.key}")
@@ -22,24 +20,8 @@ public class CredentialsConfig {
     @Value("${app.elastic.password}")
     private String elasticPassword;
 
-    @Value("${hendelse.lager.api.key:null}")
-    private String oldHendelselagerApiKey;
-
-    @Value("${person.aktor.admin.api:null}")
-    private String oldAktoerAdminApiKey;
-
-    @Value("${elastic.username:null}")
-    private String oldElasticUsername;
-
-    @Value("${elastic.password:null}")
-    private String oldElasticPassword;
-
     @Bean
     CredentialsHolder credentialsHolder() {
-        log.info("Checking aktoerAdminApiKey: {}", (aktoerAdminApiKey.equals(oldAktoerAdminApiKey)));
-        log.info("Checking hendelselagerApiKey: {}", (hendelselagerApiKey.equals(oldHendelselagerApiKey)));
-        log.info("Checking elasticUsername: {}", (elasticUsername.equals(oldElasticUsername)));
-        log.info("Checking elasticPassword: {}", (elasticPassword.equals(oldElasticPassword)));
         return new CredentialsHolder(hendelselagerApiKey, aktoerAdminApiKey, elasticUsername, elasticPassword);
     }
 
