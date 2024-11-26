8670b1ec9152416d1c9604a1b4b60481a6f283f6
Cato Olsen <olsen.cato@gmail.com>
2024-11-26 17:02:26 +0100

    - Fixes tests.

--- apps/levende-arbeidsforhold-scheduler/src/test/java/no/nav/testnav/levendearbeidsforholdscheduler/LevendeArbeidsforholdSchedulerApplicationTests.java
+++ apps/levende-arbeidsforhold-scheduler/src/test/java/no/nav/testnav/levendearbeidsforholdscheduler/LevendeArbeidsforholdSchedulerApplicationTests.java
@@ -2,12 +2,17 @@ package no.nav.testnav.levendearbeidsforholdscheduler;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
+@Profile("test")
 class LevendeArbeidsforholdSchedulerApplicationTests {
 
     @Test
     void contextLoads() {
+        assertThat(true).isTrue();
     }
 
 }
