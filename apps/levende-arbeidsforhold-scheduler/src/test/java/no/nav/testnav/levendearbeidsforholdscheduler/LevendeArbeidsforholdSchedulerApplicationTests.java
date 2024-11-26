117119f5521cdf4a432f6eb1208ac82854d0af87
Cato Olsen <olsen.cato@gmail.com>
2024-11-26 17:12:43 +0100

    - Fix typo in annotation.

--- apps/levende-arbeidsforhold-scheduler/src/test/java/no/nav/testnav/levendearbeidsforholdscheduler/LevendeArbeidsforholdSchedulerApplicationTests.java
+++ apps/levende-arbeidsforhold-scheduler/src/test/java/no/nav/testnav/levendearbeidsforholdscheduler/LevendeArbeidsforholdSchedulerApplicationTests.java
@@ -2,12 +2,12 @@ package no.nav.testnav.levendearbeidsforholdscheduler;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
-@Profile("test")
+@ActiveProfiles("test")
 class LevendeArbeidsforholdSchedulerApplicationTests {
 
     @Test
