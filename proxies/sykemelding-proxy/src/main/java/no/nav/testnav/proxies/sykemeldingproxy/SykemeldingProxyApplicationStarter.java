9a7a497bda6ed2120b057cc400b298fb471ab42b
stigus <stian.gustavsson@nav.no>
2024-11-26 10:14:09 +0100

    - Lagt til oppsett for sykemelding-proxy #deploy-proxy-sykemelding

---
+++ proxies/sykemelding-proxy/src/main/java/no/nav/testnav/proxies/sykemeldingproxy/SykemeldingProxyApplicationStarter.java
@@ -0,0 +1,16 @@
+package no.nav.testnav.proxies.sykemeldingproxy;
+
+import no.nav.testnav.libs.reactivecore.config.CoreConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+@SpringBootApplication
+@Import(CoreConfig.class)
+public class SykemeldingProxyApplicationStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SykemeldingProxyApplicationStarter.class, args);
+    }
+
+}
