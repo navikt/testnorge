62bb3e3080598f8bff120b41ade0344f92ada5c5
Kristen.Herum <krharum@gmail.com>
2024-11-29 12:26:14 +0100

    Add logging to SyfosmreglerPostValidateCommand #deploy-test-sykemelding-api
    Enhanced the SyfosmreglerPostValidateCommand with logging capability using SLF4J, allowing more detailed insights into the data being sent for validation. This update includes utilizing the pretty print feature of Json to format the logged sykemelding data, ensuring clarity for debugging purposes.

--- apps/sykemelding-api/src/main/java/no/nav/registre/testnorge/sykemelding/consumer/command/SyfosmreglerPostValidateCommand.java
+++ apps/sykemelding-api/src/main/java/no/nav/registre/testnorge/sykemelding/consumer/command/SyfosmreglerPostValidateCommand.java
@@ -1,6 +1,8 @@
 package no.nav.registre.testnorge.sykemelding.consumer.command;
 
+import io.swagger.v3.core.util.Json;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import no.nav.registre.testnorge.sykemelding.dto.ReceivedSykemeldingDTO;
 import no.nav.testnav.libs.dto.sykemelding.v1.ValidationResultDTO;
 import no.nav.testnav.libs.reactivecore.utils.WebClientFilter;
@@ -10,10 +12,11 @@ import reactor.core.publisher.Mono;
 
 import java.util.concurrent.Callable;
 
+@Slf4j
 @RequiredArgsConstructor
 public class SyfosmreglerPostValidateCommand implements Callable<Mono<ValidationResultDTO>> {
 
-    private static final String SYFOSMREGLER_VALIDATE_URL =  "/v1/rules/validate";
+    private static final String SYFOSMREGLER_VALIDATE_URL = "/v1/rules/validate";
 
     private final WebClient webClient;
     private final ReceivedSykemeldingDTO receivedSykemelding;
@@ -21,6 +24,8 @@ public class SyfosmreglerPostValidateCommand implements Callable<Mono<Validation
 
     @Override
     public Mono<ValidationResultDTO> call() {
+
+        log.info("Sender til syfosmregler {}", Json.pretty(receivedSykemelding));
 
         return webClient.post()
                 .uri(uriBuilder -> uriBuilder.path(SYFOSMREGLER_VALIDATE_URL).build())
@@ -30,8 +35,8 @@ public class SyfosmreglerPostValidateCommand implements Callable<Mono<Validation
                 .bodyToMono(ValidationResultDTO.class)
                 .doOnError(WebClientFilter::logErrorMessage)
                 .onErrorResume(error -> Mono.just(ValidationResultDTO.builder()
-                                .httpStatus(WebClientFilter.getStatus(error))
-                                .message(WebClientFilter.getMessage(error))
+                        .httpStatus(WebClientFilter.getStatus(error))
+                        .message(WebClientFilter.getMessage(error))
                         .build()));
     }
 }
