7a52bec4639d3bad7ba8e2ef3a705658b5694b2f
stigus <stian.gustavsson@nav.no>
2024-12-05 15:55:15 +0100

    - Sykemelding frontend mindre endringer #deploy-test-frontend

--- apps/dolly-frontend/src/main/java/no/nav/dolly/web/DollyFrontendApplicationStarter.java
+++ apps/dolly-frontend/src/main/java/no/nav/dolly/web/DollyFrontendApplicationStarter.java
@@ -65,6 +65,7 @@ public class DollyFrontendApplicationStarter {
                 .route(createRoute(consumers.getTestnavOrganisasjonTilgangService(), "testnav-organisasjon-tilgang-service"))
                 .route(createRoute(consumers.getTestnavTpsMessagingService(), "testnav-tps-messaging-service"))
                 .route(createRoute(consumers.getTestnorgeProfilApi(), "testnorge-profil-api"))
+                .route(createRoute(consumers.getTestnavSykemeldingApi(), "testnav-sykemelding-api"))
                 .route(createRoute(consumers.getTestnavBrukerService(), "testnav-bruker-service"))
                 .route(createRoute(consumers.getTestnavMiljoerService()))
                 .route(createRoute(consumers.getDollyBackend(), "dolly-backend"))
--- apps/dolly-frontend/src/main/js/src/utils/hooks/useSykemelding.tsx
+++ apps/dolly-frontend/src/main/js/src/utils/hooks/useSykemelding.tsx
@@ -1,9 +1,8 @@
 import useSWR from 'swr'
 import { sykemeldingFetcher } from '@/api'
 
-const baseUrl = '/testnav-sykemelding-api'
 const getValideringUrl = (shouldValidate: boolean) =>
-	shouldValidate ? `${baseUrl}/api/v1/sykemeldinger/validate` : null
+	shouldValidate ? '/testnav-sykemelding-api/api/v1/sykemeldinger/validate' : null
 
 export const useSykemeldingValidering = (shouldValidate: boolean, values: any) => {
 	const { data, isLoading, error } = useSWR<any, Error>(
