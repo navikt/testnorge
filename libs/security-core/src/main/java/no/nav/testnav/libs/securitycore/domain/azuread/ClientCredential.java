99f099d7d21845ead51a170f0d73dfdfc20a5a86
Cato Olsen <olsen.cato@gmail.com>
2024-12-11 12:58:18 +0100

    - Overriding client credentials in Spring profile "test". - Client credentials are now manually asserted to be non-null.

--- libs/security-core/src/main/java/no/nav/testnav/libs/securitycore/domain/azuread/ClientCredential.java
+++ libs/security-core/src/main/java/no/nav/testnav/libs/securitycore/domain/azuread/ClientCredential.java
@@ -1,11 +1,23 @@
 package no.nav.testnav.libs.securitycore.domain.azuread;
 
-import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import org.springframework.util.Assert;
 
-@Data
+@EqualsAndHashCode
+@Getter
 public class ClientCredential {
+
     private final String clientId;
     private final String clientSecret;
+
+    public ClientCredential(String clientId, String clientSecret) {
+        Assert.notNull(clientId, "AZURE_NAV_APP_CLIENT_ID must be set");
+        Assert.notNull(clientSecret, "AZURE_NAV_APP_CLIENT_SECRET must be set");
+
+        this.clientId = clientId;
+        this.clientSecret = clientSecret;
+    }
 
     @Override
     public final String toString() {
