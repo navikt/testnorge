7aaef7d9a34808ffb603bbdb09762b335ed92c87
Cato Olsen <olsen.cato@gmail.com>
2024-12-11 13:06:42 +0100

    - Also overriding client credentials in other subclasses of ClientCredential.

--- libs/reactive-security/src/main/java/no/nav/testnav/libs/reactivesecurity/domain/AzureTrygdeetatenClientCredential.java
+++ libs/reactive-security/src/main/java/no/nav/testnav/libs/reactivesecurity/domain/AzureTrygdeetatenClientCredential.java
@@ -1,5 +1,6 @@
 package no.nav.testnav.libs.reactivesecurity.domain;
 
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,22 +9,19 @@ import java.util.Objects;
 import no.nav.testnav.libs.securitycore.domain.azuread.ClientCredential;
 
 @Configuration
+@Getter
 public class AzureTrygdeetatenClientCredential extends ClientCredential {
+
     private final String tokenEndpoint;
 
     public AzureTrygdeetatenClientCredential(
             @Value("${AZURE_TRYGDEETATEN_OPENID_CONFIG_TOKEN_ENDPOINT:#{null}}") String tokenEndpoint,
-            @Value("${AZURE_TRYGDEETATEN_APP_CLIENT_ID:#{null}}") String clientId,
-            @Value("${AZURE_TRYGDEETATEN_APP_CLIENT_SECRET:#{null}}") String clientSecret
+            @Value("#{systemProperties['spring.profiles.active'] == 'test' ? 'test-client-id' : '${AZURE_APP_CLIENT_ID:#{null}}'}") String clientId,
+            @Value("#{systemProperties['spring.profiles.active'] == 'test' ? 'test-client-secret' : '${AZURE_APP_CLIENT_SECRET:#{null}}'}") String clientSecret
     ) {
         super(clientId, clientSecret);
         this.tokenEndpoint = tokenEndpoint;
     }
-
-    public String getTokenEndpoint() {
-        return tokenEndpoint;
-    }
-
 
     @Override
     public boolean equals(Object o) {
@@ -38,4 +36,5 @@ public class AzureTrygdeetatenClientCredential extends ClientCredential {
     public int hashCode() {
         return Objects.hash(super.hashCode(), tokenEndpoint);
     }
+
 }
--- libs/security-core/src/main/java/no/nav/testnav/libs/securitycore/domain/azuread/AzureNavClientCredential.java
+++ libs/security-core/src/main/java/no/nav/testnav/libs/securitycore/domain/azuread/AzureNavClientCredential.java
@@ -10,7 +10,7 @@ public class AzureNavClientCredential extends ClientCredential {
     TODO: A better solution, for another day:
         1. No longer import AzureNavClientCredential - generify this, or use factories. It cannot be a @Configuration for its own @Bean.
         2. Create two beans, one for @Profile("test"), one on @ConditionalOnMissingBean.
-        3. Check if we really need subclasses for this, or if ClientCredential will suffice.
+        3. Check if we really need subclasses for this, or if ClientCredential will suffice. Check all other extends ClientCredential
      */
     public AzureNavClientCredential(
             @Value("#{systemProperties['spring.profiles.active'] == 'test' ? 'test-client-id' : '${AZURE_APP_CLIENT_ID:#{null}}'}") String clientId,
