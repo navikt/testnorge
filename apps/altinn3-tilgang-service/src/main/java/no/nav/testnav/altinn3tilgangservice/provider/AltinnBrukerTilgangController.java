6a42801c89a1b13c37f8892fcf1891493f96e72b
Kristen.Herum <krharum@gmail.com>
2024-12-16 12:13:27 +0100

    Refactor imports in Altinn3TilgangService and Controller #deploy-altinn3-tilgang-service-prod #deploy-altinn3-tilgang-service
    Reorganized and corrected import statements for better code clarity and functioning. Added missing `RequestBody` annotation in the Controller and included the required DTO in the Service layer. This ensures all dependencies are correctly referenced and improves maintainability.

--- apps/altinn3-tilgang-service/src/main/java/no/nav/testnav/altinn3tilgangservice/provider/AltinnBrukerTilgangController.java
+++ apps/altinn3-tilgang-service/src/main/java/no/nav/testnav/altinn3tilgangservice/provider/AltinnBrukerTilgangController.java
@@ -1,12 +1,12 @@
 package no.nav.testnav.altinn3tilgangservice.provider;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import lombok.RequiredArgsConstructor;
 import no.nav.testnav.altinn3tilgangservice.domain.PersonRequest;
 import no.nav.testnav.altinn3tilgangservice.service.AltinnBrukerTilgangService;
 import no.nav.testnav.libs.dto.altinn3.v1.OrganisasjonDTO;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
