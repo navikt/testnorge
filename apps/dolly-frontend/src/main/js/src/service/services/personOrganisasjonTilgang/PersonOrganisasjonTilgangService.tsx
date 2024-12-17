757146a9e835fb0deefd28f40e328737b53683d0
Kristen.Herum <krharum@gmail.com>
2024-12-17 09:10:26 +0100

    Refactor API call in PersonOrganisasjonTilgangService #deploy-unstable-frontend
    Replaced generic API fetch with custom Request service for better consistency and error handling. Added logout functionality in case of errors when fetching user access data. Improved logging for debugging purposes.

--- apps/dolly-frontend/src/main/js/src/service/services/personOrganisasjonTilgang/PersonOrganisasjonTilgangService.tsx
+++ apps/dolly-frontend/src/main/js/src/service/services/personOrganisasjonTilgang/PersonOrganisasjonTilgangService.tsx
@@ -1,7 +1,18 @@
-import Api from "@/api";
+import Request from '@/service/services/Request'
+import logoutBruker from '@/components/utlogging/logoutBruker'
+
+const personOrgTilgangServiceUrl = '/altinn/organisasjoner'
 
 export default {
 	getOrganisasjoner() {
-		return Api.fetch('/altinn/organisasjoner', {method: 'GET'})
+		return Request.get(`${personOrgTilgangServiceUrl}`)
+			.catch((error) => {
+				console.warn('Noe gikk galt ved henting av tilganger for bruker')
+				console.error(error)
+				logoutBruker('person_org_error')
+			})
+			.then((response) => {
+				return response
+			})
 	}
 }
