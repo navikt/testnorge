d7b1a9cd1ff71f4a7e7eb163b4804c13ebd9cc34
Kristen.Herum <krharum@gmail.com>
2024-12-17 12:40:59 +0100

    Refactor token attribute retrieval in reactive security. #deploy-idporten-frontend
    Replaced direct Jwt token access with a context-based approach to support both Jwt and OAuth2 authentication tokens. Introduced a new `Oauth2Resolver` utility for secure handling of tokens and expiration checks. This improves flexibility and enhances token handling consistency.

--- libs/reactive-security/src/main/java/no/nav/testnav/libs/reactivesecurity/action/GetAuthenticatedUserId.java
+++ libs/reactive-security/src/main/java/no/nav/testnav/libs/reactivesecurity/action/GetAuthenticatedUserId.java
@@ -1,6 +1,11 @@
 package no.nav.testnav.libs.reactivesecurity.action;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
@@ -8,7 +13,7 @@ import java.util.concurrent.Callable;
 
 @Component
 @RequiredArgsConstructor
-public class GetAuthenticatedUserId extends JwtResolver implements Callable<Mono<String>> {
+public class GetAuthenticatedUserId implements Callable<Mono<String>> {
 
     private final GetAuthenticatedResourceServerType getAuthenticatedResourceServerType;
 
@@ -23,6 +28,23 @@ public class GetAuthenticatedUserId extends JwtResolver implements Callable<Mono
     }
 
     private Mono<String> getTokenAttribute(String attribute) {
-        return getJwtAuthenticationToken().map(value -> value.getTokenAttributes().get(attribute).toString());
+
+        return ReactiveSecurityContextHolder
+                .getContext()
+                .map(SecurityContext::getAuthentication)
+                .map(context -> getTokenAttribute(context, attribute));
+    }
+
+    private String getTokenAttribute(Authentication context, String attribute) {
+
+        if (context instanceof JwtAuthenticationToken jwtAuthenticationToken) {
+            return jwtAuthenticationToken.getTokenAttributes().get(attribute).toString();
+
+        } else if (context instanceof OAuth2AuthenticationToken oauth2AuthenticationToken) {
+            return oauth2AuthenticationToken.getPrincipal().getAttributes().get(attribute).toString();
+
+        } else {
+            return null;
+        }
     }
 }
