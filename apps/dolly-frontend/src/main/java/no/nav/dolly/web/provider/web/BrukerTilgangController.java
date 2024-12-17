d1cf9ff4f6d7eb4830e4542f32f3d6e6b744df07
Kristen.Herum <krharum@gmail.com>
2024-12-17 07:37:11 +0100

    Add logging for ServerWebExchange attributes in controller deploy #deploy-unstable-frontend
    Introduced logging to capture all attributes in ServerWebExchange within the `getOrganisasjoner` method of `BrukerTilgangController`. This helps in debugging and provides better traceability of request context data.

--- apps/dolly-frontend/src/main/java/no/nav/dolly/web/provider/web/BrukerTilgangController.java
+++ apps/dolly-frontend/src/main/java/no/nav/dolly/web/provider/web/BrukerTilgangController.java
@@ -1,6 +1,7 @@
 package no.nav.dolly.web.provider.web;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import no.nav.dolly.web.consumers.Altinn3PersonOrganisasjonTilgangConsumer;
 import no.nav.testnav.libs.dto.altinn3.v1.OrganisasjonDTO;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Flux;
 
+@Slf4j
 @RestController
 @RequestMapping("/altinn")
 @RequiredArgsConstructor
@@ -18,6 +20,9 @@ public class BrukerTilgangController {
 
     @GetMapping("/organisasjoner")
     public Flux<OrganisasjonDTO> getOrganisasjoner(ServerWebExchange exchange) {
+
+        exchange.getAttributes()
+                .forEach((key, value) -> log.info("Atributt {}: {}", key, value));
 
         return altinn3PersonOrganisasjonTilgangConsumer.getOrganisasjoner(exchange);
     }
