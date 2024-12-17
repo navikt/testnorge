34d718a4834e444df0b5322972285d68287dd7bd
Cato Olsen <olsen.cato@gmail.com>
2024-12-17 09:55:00 +0100

    Revert "change/opensearch_config_cleanup (#3701)"
    This reverts commit d7c6b12498fe6a7d0b91805f81a8178d8017ede0.

--- libs/security-core/src/main/java/no/nav/testnav/libs/securitycore/domain/azuread/ClientCredential.java
+++ libs/security-core/src/main/java/no/nav/testnav/libs/securitycore/domain/azuread/ClientCredential.java
@@ -1,24 +1,11 @@
 package no.nav.testnav.libs.securitycore.domain.azuread;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import org.springframework.util.Assert;
+import lombok.Data;
 
-@EqualsAndHashCode
-@Getter
+@Data
 public class ClientCredential {
-
-    private static final String MISSING_CONFIG_MESSAGE = "AZURE_NAV_APP_CLIENT_ID and AZURE_NAV_APP_CLIENT_SECRET must be set";
-
     private final String clientId;
     private final String clientSecret;
-
-    public ClientCredential(String clientId, String clientSecret) {
-        Assert.noNullElements(new String[]{clientId, clientSecret}, MISSING_CONFIG_MESSAGE);
-
-        this.clientId = clientId;
-        this.clientSecret = clientSecret;
-    }
 
     @Override
     public final String toString() {
