cb3200e25d02fd4ff3ae11270dbdaf679a47224a
Cato Olsen <olsen.cato@gmail.com>
2024-12-18 01:41:56 +0100

    Added conditionals to beans in order to avoid autoconfiguration when the necessary requirements are not set.

--- libs/security-core/src/main/java/no/nav/testnav/libs/securitycore/domain/azuread/ClientCredentialAutoConfiguration.java
+++ libs/security-core/src/main/java/no/nav/testnav/libs/securitycore/domain/azuread/ClientCredentialAutoConfiguration.java
@@ -3,6 +3,7 @@ package no.nav.testnav.libs.securitycore.domain.azuread;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
@@ -11,9 +12,9 @@ import org.springframework.util.Assert;
 @AutoConfiguration
 public class ClientCredentialAutoConfiguration {
 
-    private static final String AZURE_MISSING = "AAD_ISSUER_URI, AZURE_APP_CLIENT_ID and AZURE_APP_CLIENT_SECRET must be set";
-    private static final String TRYGDEETATEN_MISSING = "AZURE_TRYGDEETATEN_OPENID_CONFIG_TOKEN_ENDPOINT, AZURE_TRYGDEETATEN_APP_CLIENT_ID and AZURE_TRYGDEETATEN_APP_CLIENT_SECRET must be set";
-    private static final String NAV_MISSING = "AZURE_NAV_OPENID_CONFIG_TOKEN_ENDPOINT, AZURE_NAV_APP_CLIENT_ID and AZURE_NAV_APP_CLIENT_SECRET must be set";
+    private static final String AZURE_MISSING = "AZURE_APP_CLIENT_ID and AZURE_APP_CLIENT_SECRET must be set";
+    private static final String TRYGDEETATEN_MISSING = "AZURE_TRYGDEETATEN_APP_CLIENT_ID and AZURE_TRYGDEETATEN_APP_CLIENT_SECRET must be set";
+    private static final String NAV_MISSING = "AZURE_NAV_APP_CLIENT_ID and AZURE_NAV_APP_CLIENT_SECRET must be set";
 
     private static final String TEST_TOKEN_ENDPOINT = "test-token-endpoint";
     private static final String TEST_CLIENT_ID = "test-client-id";
@@ -27,13 +28,13 @@ public class ClientCredentialAutoConfiguration {
     }
 
     @Bean("azureClientCredential")
+    @ConditionalOnProperty("AAD_ISSUER_URI")
     @ConditionalOnMissingBean(AzureClientCredential.class)
     AzureClientCredential azureClientCredential(
             @Value("${AAD_ISSUER_URI:#{null}}") String azureTokenEndpoint, // TODO: Not currently used, AAD_ISSUER_URI is hardcoded elsewhere; should be refactored to use AZURE_OPENID_CONFIG_TOKEN_ENDPOINT instead.
             @Value("${AZURE_APP_CLIENT_ID:#{null}}") String azureClientId,
             @Value("${AZURE_APP_CLIENT_SECRET:#{null}}") String azureClientSecret
     ) {
-        Assert.hasLength(azureTokenEndpoint, AZURE_MISSING);
         Assert.hasLength(azureClientId, AZURE_MISSING);
         Assert.hasLength(azureClientSecret, AZURE_MISSING);
         return new AzureClientCredential(azureTokenEndpoint, azureClientId, azureClientSecret);
@@ -47,13 +48,13 @@ public class ClientCredentialAutoConfiguration {
     }
 
     @Bean("azureTrygdeetatenClientCredential")
+    @ConditionalOnProperty("AZURE_TRYGDEETATEN_OPENID_CONFIG_TOKEN_ENDPOINT")
     @ConditionalOnMissingBean(AzureTrygdeetatenClientCredential.class)
     AzureTrygdeetatenClientCredential azureTrygdeetatenClientCredential(
             @Value("${AZURE_TRYGDEETATEN_OPENID_CONFIG_TOKEN_ENDPOINT:#{null}}") String azureTrygdeetatenTokenEndpoint,
             @Value("${AZURE_TRYGDEETATEN_APP_CLIENT_ID:#{null}}") String azureTrygdeetatenClientId,
             @Value("${AZURE_TRYGDEETATEN_APP_CLIENT_SECRET:#{null}}") String azureTrygdeetatenClientSecret
     ) {
-        Assert.hasLength(azureTrygdeetatenTokenEndpoint, TRYGDEETATEN_MISSING);
         Assert.hasLength(azureTrygdeetatenClientId, TRYGDEETATEN_MISSING);
         Assert.hasLength(azureTrygdeetatenClientSecret, TRYGDEETATEN_MISSING);
         return new AzureTrygdeetatenClientCredential(azureTrygdeetatenTokenEndpoint, azureTrygdeetatenClientId, azureTrygdeetatenClientSecret);
@@ -67,13 +68,13 @@ public class ClientCredentialAutoConfiguration {
     }
 
     @Bean("azureNavClientCredential")
+    @ConditionalOnProperty("AZURE_NAV_OPENID_CONFIG_TOKEN_ENDPOINT")
     @ConditionalOnMissingBean(AzureNavClientCredential.class)
     AzureNavClientCredential azureNavClientCredential(
             @Value("${AZURE_NAV_OPENID_CONFIG_TOKEN_ENDPOINT:#{null}}") String azureNavTokenEndpoint,
             @Value("${AZURE_NAV_APP_CLIENT_ID:#{null}}") String azureNavClientId,
             @Value("${AZURE_NAV_APP_CLIENT_SECRET:#{null}}") String azureNavClientSecret
     ) {
-        Assert.hasLength(azureNavTokenEndpoint, NAV_MISSING);
         Assert.hasLength(azureNavClientId, NAV_MISSING);
         Assert.hasLength(azureNavClientSecret, NAV_MISSING);
         return new AzureNavClientCredential(azureNavTokenEndpoint, azureNavClientId, azureNavClientSecret);
