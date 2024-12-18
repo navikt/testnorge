90f9bfeaa57c10e7cb6c2cbd41ddd8b644b29c29
Kristen.Herum <krharum@gmail.com>
2024-12-18 08:20:13 +0100

    Refactor security token handling and update logging configs. #deploy-idporten-frontend
    Simplified and unified authentication token handling across reactive security classes. Adjusted logging configurations for better stack trace management. Enabled Playwright job in GitHub workflow.

--- libs/reactive-security/src/main/java/no/nav/testnav/libs/reactivesecurity/action/GetAuthenticatedUserId.java
+++ libs/reactive-security/src/main/java/no/nav/testnav/libs/reactivesecurity/action/GetAuthenticatedUserId.java
@@ -2,9 +2,6 @@ package no.nav.testnav.libs.reactivesecurity.action;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.ReactiveSecurityContextHolder;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Component;
@@ -15,7 +12,7 @@ import java.util.concurrent.Callable;
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class GetAuthenticatedUserId implements Callable<Mono<String>> {
+public class GetAuthenticatedUserId extends JwtResolver implements Callable<Mono<String>> {
 
     private final GetAuthenticatedResourceServerType getAuthenticatedResourceServerType;
 
@@ -31,24 +28,20 @@ public class GetAuthenticatedUserId implements Callable<Mono<String>> {
 
     private Mono<String> getTokenAttribute(String attribute) {
 
-        return ReactiveSecurityContextHolder
-                .getContext()
-                .map(SecurityContext::getAuthentication)
-                .map(authentication -> getTokenAttribute(authentication, attribute));
-    }
+        return getJwtAuthenticationToken()
+                .map(authentication -> {
 
-    private String getTokenAttribute(Authentication authentication, String attribute) {
+                    log.info("GetAuthenticatedUserId context.authentication {}, {}", authentication.getClass().getPackageName(), authentication);
+                    return switch (authentication) {
 
-        log.info("GetAuthenticatedUserId context.authentication {}, {}", authentication.getClass().getPackageName(), authentication);
-        if (authentication instanceof JwtAuthenticationToken jwtAuthenticationToken) {
-            return jwtAuthenticationToken.getTokenAttributes().get(attribute).toString();
+                        case JwtAuthenticationToken jwtAuthenticationToken ->
+                                jwtAuthenticationToken.getTokenAttributes().get(attribute).toString();
 
-        } else if (authentication instanceof OAuth2AuthenticationToken oauth2AuthenticationToken) {
-            log.info("oauth2AuthenticationToken {}", oauth2AuthenticationToken.getPrincipal());
-            return oauth2AuthenticationToken.getPrincipal().getAttributes().get(attribute).toString();
+                        case OAuth2AuthenticationToken oauth2AuthenticationToken ->
+                                oauth2AuthenticationToken.getPrincipal().getAttributes().get(attribute).toString();
 
-        } else {
-            return "";
-        }
+                        default -> "";
+                    };
+                });
     }
 }
