a10f9f0f6d04061cb0cdd67c8221a00a39fe11fd
Cato Olsen <olsen.cato@gmail.com>
2024-12-20 13:13:19 +0100

    Fixed test by mocking ReactiveClientRegistrationRepository.

--- apps/endringsmelding-frontend/src/test/java/no/nav/testnav/apps/endringsmeldingfrontend/ApplicationContextTest.java
+++ apps/endringsmelding-frontend/src/test/java/no/nav/testnav/apps/endringsmeldingfrontend/ApplicationContextTest.java
@@ -3,7 +3,7 @@ package no.nav.testnav.apps.endringsmeldingfrontend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -14,11 +14,12 @@ class ApplicationContextTest {
 
     @MockBean
     @SuppressWarnings("unused")
-    private JwtDecoder jwtDecoder;
+    private ReactiveClientRegistrationRepository reactiveClientRegistrationRepository;
 
     @Test
     @SuppressWarnings("java:S2699")
     void load_app_context() {
         assertThat(true).isTrue();
     }
+
 }
