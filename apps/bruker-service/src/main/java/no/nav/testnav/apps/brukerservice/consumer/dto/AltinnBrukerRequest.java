12bd36346cf12bf7b8d17a391acf6f88db24d08a
Kristen.Herum <krharum@gmail.com>
2024-12-20 11:00:18 +0100

    ``` Refactor to use Altinn3 API for user access management #deploy-bruker-service
    Replaced Person-Organisasjon-Tilgang API with Altinn3 API across the application. Deprecated `OrganisasjonDTO` and updated classes to utilize `AltinnBrukerRequest` and Altinn3 DTOs. Adjusted configuration and consumer logic to align with the new API structure for improved functionality.
    ```

---
+++ apps/bruker-service/src/main/java/no/nav/testnav/apps/brukerservice/consumer/dto/AltinnBrukerRequest.java
@@ -0,0 +1,11 @@
+package no.nav.testnav.apps.brukerservice.consumer.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AltinnBrukerRequest {
+
+    private String ident;
+}
