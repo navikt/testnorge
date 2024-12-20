1128e922ca57269ae5e796bcc08585dfe673e5b4
Cato Olsen <olsen.cato@gmail.com>
2024-12-20 11:46:42 +0100

    Test cleanup.

--- apps/endringsmelding-frontend/src/test/java/no/nav/testnav/apps/endringsmeldingfrontend/ApplicationContextTest.java
+++ apps/endringsmelding-frontend/src/test/java/no/nav/testnav/apps/endringsmeldingfrontend/ApplicationContextTest.java
@@ -6,15 +6,19 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.test.context.ActiveProfiles;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 @SpringBootTest
 @ActiveProfiles("test")
 class ApplicationContextTest {
 
     @MockBean
-    public JwtDecoder jwtDecoder;
+    @SuppressWarnings("unused")
+    private JwtDecoder jwtDecoder;
 
     @Test
     @SuppressWarnings("java:S2699")
     void load_app_context() {
+        assertThat(true).isTrue();
     }
 }
