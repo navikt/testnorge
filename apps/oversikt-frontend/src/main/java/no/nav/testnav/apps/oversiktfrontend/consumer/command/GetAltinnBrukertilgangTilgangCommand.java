705b34db3566c1ed407b2921ab77b65048d52e86
Kristen.Herum <krharum@gmail.com>
2024-12-20 15:24:26 +0100

    Refactor Altinn access integration and update service dependencies
    Replaces PersonOrganisasjonTilgang functionality with Altinn3Tilgang integration, including adjustments to DTOs, commands, service logic, and configuration. Renamed classes and endpoints to reflect the updated functionality. Added dependency on shared data-transfer-objects library for streamlined DTO management.

--- apps/oversikt-frontend/src/main/java/no/nav/testnav/apps/oversiktfrontend/consumer/command/GetAltinnBrukertilgangTilgangCommand.java
+++ apps/oversikt-frontend/src/main/java/no/nav/testnav/apps/oversiktfrontend/consumer/command/GetAltinnBrukertilgangTilgangCommand.java
@@ -1,7 +1,8 @@
 package no.nav.testnav.apps.oversiktfrontend.consumer.command;
 
 import lombok.RequiredArgsConstructor;
-import no.nav.testnav.apps.oversiktfrontend.consumer.dto.OrganisasjonDTO;
+import no.nav.testnav.apps.oversiktfrontend.consumer.dto.AltinnBrukerRequest;
+import no.nav.testnav.libs.dto.altinn3.v1.OrganisasjonDTO;
 import no.nav.testnav.libs.reactivecore.utils.WebClientFilter;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -12,16 +13,18 @@ import java.time.Duration;
 import java.util.concurrent.Callable;
 
 @RequiredArgsConstructor
-public class GetPersonOrganisasjonerTilgangCommand implements Callable<Flux<OrganisasjonDTO>> {
+public class GetAltinnBrukertilgangTilgangCommand implements Callable<Flux<OrganisasjonDTO>> {
     private final WebClient webClient;
+    private final String ident;
     private final String token;
 
     @Override
-    public Flux<OrganisasjonDTO> call() {
+    public Flux<no.nav.testnav.libs.dto.altinn3.v1.OrganisasjonDTO> call() {
         return webClient
-                .get()
-                .uri("/api/v1/person/organisasjoner")
+                .post()
+                .uri("/api/v1/brukertilgang")
                 .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+                .bodyValue(new AltinnBrukerRequest(ident))
                 .retrieve()
                 .bodyToFlux(OrganisasjonDTO.class)
                 .retryWhen(Retry.backoff(3, Duration.ofSeconds(5))
