3ba3e8bcbc7a0589293fe50398e7b48191c13822
Kristen.Herum <krharum@gmail.com>
2025-01-06 14:20:07 +0100

    Remove unused imports from SecureOAuth2ServerToServerConfiguration
    Cleaned up unnecessary imports, including `GetUserInfo` and `JwtDecoder`, to improve code readability and maintainability. This change reduces clutter without affecting functionality.

--- libs/reactive-security/src/main/java/no/nav/testnav/libs/reactivesecurity/config/SecureOAuth2ServerToServerConfiguration.java
+++ libs/reactive-security/src/main/java/no/nav/testnav/libs/reactivesecurity/config/SecureOAuth2ServerToServerConfiguration.java
@@ -3,7 +3,6 @@ package no.nav.testnav.libs.reactivesecurity.config;
 import no.nav.testnav.libs.reactivesecurity.action.GetAuthenticatedResourceServerType;
 import no.nav.testnav.libs.reactivesecurity.action.GetAuthenticatedToken;
 import no.nav.testnav.libs.reactivesecurity.action.GetAuthenticatedUserId;
-import no.nav.testnav.libs.reactivesecurity.action.GetUserInfo;
 import no.nav.testnav.libs.reactivesecurity.exchange.TokenExchange;
 import no.nav.testnav.libs.reactivesecurity.exchange.tokenx.TokenXService;
 import no.nav.testnav.libs.reactivesecurity.manager.JwtReactiveAuthenticationManager;
@@ -17,7 +16,6 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
 
 import java.util.List;
 
@@ -31,8 +29,7 @@ import java.util.List;
         GetAuthenticatedUserId.class,
         GetAuthenticatedResourceServerType.class,
         GetAuthenticatedToken.class,
-        TokenXProperties.class,
-        GetUserInfo.class
+        TokenXProperties.class
 })
 public class SecureOAuth2ServerToServerConfiguration {
 
