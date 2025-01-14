f24ad20ff0c0c0fe20c628b98825d29a6dffbc28
Cato Olsen <olsen.cato@gmail.com>
2025-01-14 16:24:13 +0100

    Removed mocked beans no longer needed because of autoconfiguration.

--- apps/altinn3-tilgang-service/src/test/java/no/nav/testnav/altinn3tilgangservice/ApplicationContextTest.java
+++ apps/altinn3-tilgang-service/src/test/java/no/nav/testnav/altinn3tilgangservice/ApplicationContextTest.java
@@ -1,10 +1,7 @@
 package no.nav.testnav.altinn3tilgangservice;
 
-import com.google.cloud.secretmanager.v1.SecretManagerServiceClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -12,14 +9,6 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 @SpringBootTest
 @ActiveProfiles("test")
 class ApplicationContextTest {
-
-    @MockitoBean
-    @SuppressWarnings("unused")
-    public ReactiveJwtDecoder jwtDecoder;
-
-    @MockitoBean
-    @SuppressWarnings("unused")
-    public SecretManagerServiceClient secretManagerClient;
 
     @Test
     void load_app_context() {
--- apps/endringsmelding-service/src/test/java/no/nav/testnav/endringsmeldingservice/ApplicationContextTest.java
+++ apps/endringsmelding-service/src/test/java/no/nav/testnav/endringsmeldingservice/ApplicationContextTest.java
@@ -2,19 +2,17 @@ package no.nav.testnav.endringsmeldingservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class ApplicationContextTest {
 
-    @MockitoBean
-    public ReactiveJwtDecoder jwtDecoder;
-
     @Test
-    @SuppressWarnings("java:S2699")
     void load_app_context() {
+        assertThat(true).isTrue();
     }
+
 }
