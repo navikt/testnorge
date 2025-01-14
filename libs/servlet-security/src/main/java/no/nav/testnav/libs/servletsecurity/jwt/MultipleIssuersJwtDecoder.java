63a2d8e5c0da3b70f72e9a302a33686dcc3ab972
Cato Olsen <olsen.cato@gmail.com>
2025-01-14 15:43:41 +0100

    Autoconfiguration for JwtDecoder in servlet-security.

--- libs/servlet-security/src/main/java/no/nav/testnav/libs/servletsecurity/jwt/MultipleIssuersJwtDecoder.java
+++ libs/servlet-security/src/main/java/no/nav/testnav/libs/servletsecurity/jwt/MultipleIssuersJwtDecoder.java
@@ -1,30 +1,24 @@
-package no.nav.testnav.libs.servletsecurity.decoder;
+package no.nav.testnav.libs.servletsecurity.jwt;
 
 import com.nimbusds.jwt.JWTParser;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.testnav.libs.servletsecurity.properties.ResourceServerProperties;
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
 import org.springframework.security.oauth2.core.OAuth2Error;
 import org.springframework.security.oauth2.core.OAuth2TokenValidator;
 import org.springframework.security.oauth2.core.OAuth2TokenValidatorResult;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtDecoders;
-import org.springframework.security.oauth2.jwt.JwtException;
-import org.springframework.security.oauth2.jwt.JwtValidationException;
-import org.springframework.security.oauth2.jwt.JwtValidators;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.*;
 
+import java.text.ParseException;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 @Slf4j
-public class MultipleIssuersJwtDecoder implements JwtDecoder {
+class MultipleIssuersJwtDecoder implements JwtDecoder {
     private final Map<String, NimbusJwtDecoder> decoderMap;
 
-    public MultipleIssuersJwtDecoder(List<ResourceServerProperties> properties) {
+    MultipleIssuersJwtDecoder(List<ResourceServerProperties> properties) {
         this.decoderMap = properties.stream().collect(Collectors.toMap(
                 ResourceServerProperties::getIssuerUri,
                 props -> {
@@ -35,15 +29,21 @@ public class MultipleIssuersJwtDecoder implements JwtDecoder {
         ));
     }
 
-    @SneakyThrows
     @Override
     public Jwt decode(String token) throws JwtException {
-        var jwt = JWTParser.parse(token);
-
         try {
-            return decoderMap.get(jwt.getJWTClaimsSet().getIssuer()).decode(token);
+            var issuer = JWTParser
+                    .parse(token)
+                    .getJWTClaimsSet()
+                    .getIssuer();
+            return decoderMap
+                    .get(issuer)
+                    .decode(token);
+        } catch (ParseException e) {
+            log.error("Feil ved parsing av token", e);
+            throw new JwtException("Feil ved parsing av token", e);
         } catch (JwtValidationException e) {
-            log.error("Jwt valideringsfeil", e);
+            log.error("Feil ved validering av token", e);
             throw e;
         } catch (Exception e) {
             log.error("Ukjent feil", e);
@@ -65,4 +65,5 @@ public class MultipleIssuersJwtDecoder implements JwtDecoder {
     private OAuth2Error createError(String msg) {
         return new OAuth2Error("invalid_token", msg, null);
     }
+
 }
