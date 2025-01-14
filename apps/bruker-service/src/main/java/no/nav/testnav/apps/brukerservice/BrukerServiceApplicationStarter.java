cc7770d53c3177306e298f002ec9c4bac2086a1a
stigus <stian.gustavsson@nav.no>
2025-01-14 09:23:11 +0100

    Deployer diverse apper etter spring oppdatering for å se at ting fungerer som de skal #deploy-test-bruker-service #deploy-test-pdl-forvalter #deploy-test-frontend

--- apps/bruker-service/src/main/java/no/nav/testnav/apps/brukerservice/BrukerServiceApplicationStarter.java
+++ apps/bruker-service/src/main/java/no/nav/testnav/apps/brukerservice/BrukerServiceApplicationStarter.java
@@ -1,12 +1,11 @@
 package no.nav.testnav.apps.brukerservice;
 
+import no.nav.testnav.libs.reactivecore.config.CoreConfig;
+import no.nav.testnav.libs.reactivesecurity.config.SecureOAuth2ServerToServerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.reactive.config.EnableWebFlux;
-
-import no.nav.testnav.libs.reactivecore.config.CoreConfig;
-import no.nav.testnav.libs.reactivesecurity.config.SecureOAuth2ServerToServerConfiguration;
 
 
 @Import({
@@ -21,3 +20,4 @@ public class BrukerServiceApplicationStarter {
         SpringApplication.run(BrukerServiceApplicationStarter.class, args);
     }
 }
+ 
