b0c6bc4d401e23bd12e2a838f677d3a30eab22cf
Cato Olsen <olsen.cato@gmail.com>
2025-01-16 15:07:17 +0100

    Added a noop ReactiveClientRegistrationRepository used in test profile to avoid mocking.

--- libs/security-core/src/main/java/no/nav/testnav/libs/securitycore/oauth2/client/userinfo/NoopReactiveOauth2AutoConfiguration.java
+++ libs/security-core/src/main/java/no/nav/testnav/libs/securitycore/oauth2/client/userinfo/NoopReactiveOauth2AutoConfiguration.java
@@ -2,7 +2,9 @@ package no.nav.testnav.libs.securitycore.oauth2.client.userinfo;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcReactiveOAuth2UserService;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.ReactiveOAuth2UserService;
 import org.springframework.security.oauth2.core.user.OAuth2User;
@@ -18,6 +20,19 @@ public class NoopReactiveOauth2AutoConfiguration {
     @Bean
     public OidcReactiveOAuth2UserService oidcReactiveOAuth2UserService() {
         return new NoopOidcReactiveOAuth2UserService();
+    }
+
+    /**
+     * When testing, just use an empty repository so we don't trigger the full behaviour of
+     * {@code InMemoryReactiveClientRegistrationRepository} - which is not needed for tests.
+     * @return An instance of {@code NoopReactiveClientRegistrationRepository}.
+     * @see org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository
+     * @see no.nav.testnav.libs.securitycore.oauth2.client.userinfo.NoopReactiveClientRegistrationRepository
+     */
+    @Bean
+    @Profile("test")
+    ReactiveClientRegistrationRepository reactiveClientRegistrationRepositoryForTesting() {
+        return new NoopReactiveClientRegistrationRepository();
     }
 
 }
