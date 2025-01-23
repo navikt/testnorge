92c4aa19f0b4bba78b692598a881ddbdaadb96a8
Betsy Carina Traran <betsy.carina.traran@nav.no>
2025-01-23 11:29:33 +0100

    Fjern synt-sykemelding fra form

--- apps/dolly-frontend/src/main/js/src/components/fagsystem/sykdom/form/Form.tsx
+++ apps/dolly-frontend/src/main/js/src/components/fagsystem/sykdom/form/Form.tsx
@@ -1,11 +1,10 @@
 import { Vis } from '@/components/bestillingsveileder/VisAttributt'
 import Panel from '@/components/ui/panel/Panel'
 import { erForsteEllerTest, panelError } from '@/components/ui/form/formUtils'
-import { Sykemelding } from './partials/Sykemelding'
 import { validation } from './validation'
 import React from 'react'
-import StyledAlert from '@/components/ui/alert/StyledAlert'
 import { useFormContext } from 'react-hook-form'
+import { DetaljertSykemelding } from '@/components/fagsystem/sykdom/form/partials/DetaljertSykemelding'
 
 export const sykdomAttributt = 'sykemelding'
 
@@ -19,16 +18,8 @@ export const SykdomForm = () => {
 				hasErrors={panelError(sykdomAttributt)}
 				iconType="sykdom"
 				startOpen={erForsteEllerTest(formMethods.getValues(), [sykdomAttributt])}
-				informasjonstekst="Om du velger å generere en sykemelding automatisk, vil du få en syntetisk sykemelding hvor alle verdier blir satt for deg."
 			>
-				{formMethods.getValues().sykemelding != null &&
-					formMethods.getValues().sykemelding.hasOwnProperty('syntSykemelding') && (
-						<StyledAlert variant={'info'} size={'small'}>
-							Syntetisk sykemelding behandler en stor mengde data for å opprette realistiske
-							sykemeldinger og kan derfor medføre litt lenger bestillingstid.
-						</StyledAlert>
-					)}
-				<Sykemelding formMethods={formMethods} />
+				<DetaljertSykemelding formMethods={formMethods} />
 			</Panel>
 		</Vis>
 	)
---
+++ apps/dolly-frontend/src/main/js/src/components/fagsystem/sykdom/form/initialValues.tsx
@@ -0,0 +1,65 @@
+import { Diagnose } from '@/components/fagsystem/sykdom/SykemeldingTypes'
+import { addDays } from 'date-fns'
+import { getRandomValue } from '@/components/fagsystem/utils'
+import { SelectOptionsDiagnoser } from '@/components/fagsystem/sykdom/form/partials/SelectOptionsDiagnoser'
+
+const randomDiagnose = getRandomValue(SelectOptionsDiagnoser())
+
+export const initialValuesDetaljertSykemelding = {
+	detaljertSykemelding: {
+		arbeidsgiver: {
+			navn: '',
+			stillingsprosent: 100,
+			yrkesbetegnelse: '',
+		},
+		biDiagnoser: [] as Array<Diagnose>,
+		detaljer: {
+			arbeidsforEtterEndtPeriode: false,
+			beskrivHensynArbeidsplassen: '',
+			tiltakArbeidsplass: '',
+			tiltakNav: '',
+		},
+		hovedDiagnose: {
+			diagnose: randomDiagnose?.diagnoseNavn || '',
+			diagnosekode: randomDiagnose?.value || '',
+			system: randomDiagnose ? '2.16.578.1.12.4.1.1.7170' : '',
+		},
+		helsepersonell: {
+			etternavn: '',
+			fornavn: '',
+			hprId: '',
+			ident: '',
+			mellomnavn: '',
+			samhandlerType: '',
+		},
+		manglendeTilretteleggingPaaArbeidsplassen: false,
+		mottaker: {
+			navn: '',
+			orgNr: null,
+			adresse: {
+				by: '',
+				gate: '',
+				land: '',
+				postnummer: '',
+			},
+		},
+		perioder: [
+			{
+				aktivitet: {
+					aktivitet: null as unknown as string,
+					behandlingsdager: 0,
+					grad: 0,
+					reisetilskudd: false,
+				},
+				fom: addDays(new Date(), -7),
+				tom: addDays(new Date(), -1),
+			},
+		],
+		kontaktMedPasient: {
+			kontaktDato: null,
+			begrunnelseIkkeKontakt: '',
+		},
+		startDato: new Date(),
+		umiddelbarBistand: false,
+	},
+}
