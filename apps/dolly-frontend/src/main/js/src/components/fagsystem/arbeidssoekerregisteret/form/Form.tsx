b64b3fe7bea6b3c1ace1356fecab28c3b032b0f2
Betsy Carina Traran <betsy.carina.traran@nav.no>
2025-02-06 14:53:44 +0100

    Oppsett for arbeidssoekerregisteret-proxy

--- apps/dolly-frontend/src/main/js/src/components/fagsystem/arbeidssoekerregisteret/form/Form.tsx
+++ apps/dolly-frontend/src/main/js/src/components/fagsystem/arbeidssoekerregisteret/form/Form.tsx
@@ -3,11 +3,16 @@ import { useFormContext } from 'react-hook-form'
 import Panel from '@/components/ui/panel/Panel'
 import { erForsteEllerTest, panelError } from '@/components/ui/form/formUtils'
 import { FormSelect } from '@/components/ui/form/inputs/select/Select'
+import { useArbeidssoekerTyper } from '@/utils/hooks/useArbeidssoekerregisteret'
 
 export const arbeidssoekerregisteretAttributt = 'arbeidssoekerregisteret'
 
 export const ArbeidssoekerregisteretForm = () => {
 	const formMethods = useFormContext()
+
+	const { data: brukertypeOptions, loading: loadingBrukertype } =
+		useArbeidssoekerTyper('BRUKERTYPE')
+
 	return (
 		<Vis attributt={arbeidssoekerregisteretAttributt}>
 			<Panel
@@ -17,7 +22,12 @@ export const ArbeidssoekerregisteretForm = () => {
 				startOpen={erForsteEllerTest(formMethods.getValues(), [arbeidssoekerregisteretAttributt])}
 			>
 				<div className="flexbox--flex-wrap">
-					<FormSelect name="arbeidssoekerregisteret.utfoertAv" label="Utført av" options={[]} />
+					<FormSelect
+						name="arbeidssoekerregisteret.utfoertAv"
+						label="Utført av"
+						options={brukertypeOptions}
+						isLoading={loadingBrukertype}
+					/>
 				</div>
 			</Panel>
 		</Vis>
