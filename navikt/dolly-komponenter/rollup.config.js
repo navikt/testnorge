3b994323f893bec353c134ea8a067ce0eb169b8b
stigus <stian.gustavsson@nav.no>
2025-02-11 08:40:09 +0100

    Oppdatert dolly komponenter og refaktorert til å bruke nye pakker #publish-libs

--- navikt/dolly-komponenter/rollup.config.js
+++ navikt/dolly-komponenter/rollup.config.js
@@ -1,43 +1,34 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import dts from 'rollup-plugin-dts';
 import terser from '@rollup/plugin-terser';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import image from '@rollup/plugin-image';
 import typescript from '@rollup/plugin-typescript';
 import css from 'rollup-plugin-import-css';
+import {createRequire} from 'module';
 
-import packageJson from './package.json' assert { type: 'json' };
+const require = createRequire(import.meta.url);
+const packageJson = require('./package.json', {assert: {type: 'json'}});
 
 export default [
-  {
-    input: 'src/index.ts',
-    output: [
-      {
-        file: packageJson.main,
-        format: 'cjs',
-        sourcemap: true,
-      },
-      {
-        file: packageJson.module,
-        format: 'esm',
-        sourcemap: true,
-      },
-    ],
-    plugins: [
-      peerDepsExternal(),
-      resolve(),
-      commonjs(),
-      css(),
-      typescript({ tsconfig: './tsconfig.json' }),
-      terser(),
-      image(),
-    ],
-    external: ['react', 'react-dom', 'styled-components'],
-  },
-  {
-    input: 'src/index.ts',
-    output: [{ file: 'dist/types.d.ts', format: 'es' }],
-    plugins: [dts.default()],
-  },
+    {
+        input: 'src/index.ts',
+        output: [
+            {
+                file: packageJson.module,
+                format: 'esm',
+                sourcemap: true,
+            },
+        ],
+        plugins: [
+            peerDepsExternal(),
+            resolve(),
+            commonjs(),
+            css(),
+            typescript({tsconfig: './tsconfig.json', jsx: 'react'}),
+            terser(),
+            image(),
+        ],
+        external: ['react', 'react-dom'],
+    },
 ];
--- navikt/dolly-komponenter/src/timepicker/TimePicker.tsx
+++ navikt/dolly-komponenter/src/timepicker/TimePicker.tsx
@@ -1,73 +1,73 @@
 import React from 'react';
-import ReactDatepicker, { registerLocale } from 'react-datepicker';
+import ReactDatepicker, {registerLocale} from 'react-datepicker';
 // @ts-ignore
-import { addYears, subYears } from 'date-fns';
+import {addYears, subYears} from 'date-fns';
 // @ts-ignore
-import { nb } from 'date-fns/locale';
+import {nb} from 'date-fns/locale';
 import 'react-datepicker/dist/react-datepicker.css';
 import styled from 'styled-components';
-import { InputFormItem } from '../form';
+import {InputFormItem} from '../form';
 
 registerLocale('nb', nb);
 
 type Props = {
-  label?: string;
-  name?: string;
-  value: Date;
-  placeholder?: string;
-  onChange: (value: Date[] | null) => void;
-  onBlur?: () => void;
-  disabled?: boolean;
-  excludeDates?: Date[];
-  minDate?: Date;
-  maxDate?: Date;
-  className?: string;
+    label?: string;
+    name?: string;
+    value: Date;
+    placeholder?: string;
+    onChange: (value: Date[] | null) => void;
+    onBlur?: () => void;
+    disabled?: boolean;
+    excludeDates?: Date[];
+    minDate?: Date;
+    maxDate?: Date;
+    className?: string;
 };
 
 const StyledInput = styled(InputFormItem)`
-  margin-top: 5px;
-  width: 100%;
+    margin-top: 5px;
+    width: 100%;
 `;
 
 const StyledDatePicker = styled(ReactDatepicker)`
-  && {
-    padding-right: unset;
-  }
+    && {
+        padding-right: unset;
+    }
 `;
 
 const TimePicker = ({
-  label,
-  name,
-  value,
-  placeholder = 'Ikke spesifisert',
-  onChange,
-  onBlur,
-  disabled = false,
-  excludeDates,
-  minDate,
-  maxDate,
-  className,
-}: Props) => (
-  <StyledDatePicker
-    locale="nb"
-    className={className}
-    dateFormat="dd.MM.yyyy HH:mm"
-    placeholderText={placeholder}
-    selected={(value && new Date(value)) || null}
-    onChange={onChange}
-    showTimeInput
-    timeInputLabel={'Tid'}
-    minDate={minDate || subYears(new Date(), 100)}
-    maxDate={maxDate || addYears(new Date(), 5)}
-    dropdownMode="select"
-    disabled={disabled}
-    onBlur={onBlur}
-    name={name}
-    id={name}
-    autoComplete="off"
-    customInput={<StyledInput label={label} />}
-    excludeDates={excludeDates}
-  />
+                        label,
+                        name,
+                        value,
+                        placeholder = 'Ikke spesifisert',
+                        onChange,
+                        onBlur,
+                        disabled = false,
+                        excludeDates,
+                        minDate,
+                        maxDate,
+                        className,
+                    }: Props) => (
+    <StyledDatePicker
+        locale="nb"
+        className={className}
+        dateFormat="dd.MM.yyyy HH:mm"
+        placeholderText={placeholder}
+        selected={value instanceof Date ? value : null}
+        onChange={onChange}
+        showTimeInput
+        timeInputLabel={'Tid'}
+        minDate={minDate || subYears(new Date(), 100)}
+        maxDate={maxDate || addYears(new Date(), 5)}
+        dropdownMode="select"
+        disabled={disabled}
+        onBlur={onBlur}
+        name={name}
+        id={name}
+        autoComplete="off"
+        customInput={<StyledInput label={label}/>}
+        excludeDates={excludeDates}
+    />
 );
 
 export default TimePicker;
