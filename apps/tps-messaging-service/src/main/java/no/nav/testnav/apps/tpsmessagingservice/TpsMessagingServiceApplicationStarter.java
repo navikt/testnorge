d87b6f0ff04ea65e90a0057d613e53f460ed9758
Cato Olsen <olsen.cato@gmail.com>
2025-02-13 16:26:45 +0100

    Fixes import of NaisEnvironmentApplicationContextInitializer.

--- apps/tps-messaging-service/src/main/java/no/nav/testnav/apps/tpsmessagingservice/TpsMessagingServiceApplicationStarter.java
+++ apps/tps-messaging-service/src/main/java/no/nav/testnav/apps/tpsmessagingservice/TpsMessagingServiceApplicationStarter.java
@@ -1,6 +1,6 @@
 package no.nav.testnav.apps.tpsmessagingservice;
 
-import no.nav.testnav.libs.servletcore.config.NaisEnvironmentApplicationContextInitializer;
+import no.nav.dolly.libs.nais.NaisEnvironmentApplicationContextInitializer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
