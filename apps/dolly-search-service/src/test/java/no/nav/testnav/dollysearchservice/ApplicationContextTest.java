75438da31ee6bda454b829471772557ef88d5b28
Kristen Hærum <krharum@gmail.com>
2025-02-18 09:10:37 +0100

    Refactor: Update application configuration and test setup; remove unused properties and switch to MockitoBean for better test isolation #deploy-dolly-search-service

--- apps/dolly-search-service/src/test/java/no/nav/testnav/dollysearchservice/ApplicationContextTest.java
+++ apps/dolly-search-service/src/test/java/no/nav/testnav/dollysearchservice/ApplicationContextTest.java
@@ -2,15 +2,15 @@ package no.nav.testnav.dollysearchservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class ApplicationContextTest {
 
-    @MockBean
+    @MockitoBean
     public JwtDecoder jwtDecoder;
 
     @Test
