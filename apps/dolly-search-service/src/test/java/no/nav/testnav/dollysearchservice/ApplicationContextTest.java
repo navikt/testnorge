ac50668aa8e651b1677cff8594145040a023b464
Kristen Hærum <krharum@gmail.com>
2025-02-18 09:22:17 +0100

    Disable test #deploy-dolly-search-service

--- apps/dolly-search-service/src/test/java/no/nav/testnav/dollysearchservice/ApplicationContextTest.java
+++ apps/dolly-search-service/src/test/java/no/nav/testnav/dollysearchservice/ApplicationContextTest.java
@@ -1,11 +1,13 @@
 package no.nav.testnav.dollysearchservice;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
+@Disabled
 @SpringBootTest
 @ActiveProfiles("test")
 class ApplicationContextTest {
