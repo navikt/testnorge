4927e2a3d7ef1b43a08709789bf244bbda313781
Cato Olsen <olsen.cato@gmail.com>
2025-02-20 16:00:51 +0100

    Reinstated WireMockInitializer, with cleanup.

--- apps/bruker-service/src/test/java/no/nav/testnav/apps/brukerservice/ApplicationContextTest.java
+++ apps/bruker-service/src/test/java/no/nav/testnav/apps/brukerservice/ApplicationContextTest.java
@@ -1,8 +1,12 @@
 package no.nav.testnav.apps.brukerservice;
 
+import no.nav.dolly.libs.nais.NaisEnvironmentApplicationContextInitializer;
 import no.nav.dolly.libs.test.DollySpringBootTest;
 import no.nav.dolly.libs.test.DollyApplicationContextTest;
 
-@DollySpringBootTest
+@DollySpringBootTest(initializers = {
+        NaisEnvironmentApplicationContextInitializer.class,
+        WireMockInitializer.class
+})
 class ApplicationContextTest extends DollyApplicationContextTest {
 }
