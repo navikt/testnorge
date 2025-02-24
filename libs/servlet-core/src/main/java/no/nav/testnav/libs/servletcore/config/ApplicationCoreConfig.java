e76065417d682f23f065ae831db5e771c07d5679
Cato Olsen <olsen.cato@gmail.com>
2025-02-24 14:30:02 +0100

    Removes obsolete InternalController for isAlive/isReady endpoints.

--- libs/servlet-core/src/main/java/no/nav/testnav/libs/servletcore/config/ApplicationCoreConfig.java
+++ libs/servlet-core/src/main/java/no/nav/testnav/libs/servletcore/config/ApplicationCoreConfig.java
@@ -1,13 +1,11 @@
 package no.nav.testnav.libs.servletcore.config;
 
 import no.nav.testnav.libs.servletcore.health.HealthToMeterAutoConfig;
-import no.nav.testnav.libs.servletcore.provider.InternalController;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
 @Import({
-        InternalController.class,
         ApplicationProperties.class,
         LoggingWebConfig.class,
         HealthToMeterAutoConfig.class
