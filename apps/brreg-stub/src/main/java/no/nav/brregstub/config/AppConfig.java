5adcfacbf95d83222b2d88a9cdd0429ab980a2e7
Cato Olsen <cato.olsen@nav.no>
2025-02-26 10:20:17 +0100

    cleanup/vault_autoconfigured (#3766)
    * Normalizing import statements.
    
    * Refactored Vault code as autoconfigured beans.
    
    * Removed unnecessary @Import statements related to Vault autoconfiguration.
    
    * Fixed dependencies.
    
    * Now getting datasource URL from Spring DataSource config, not from Vault Database config.
    
    * Removed explicit imports to avoid polluting application context (which is autoconfigured in the actual apps, but not understood by IntelliJ).
    
    * Removed hanging comment.
    
    * Reenabled DataSourceProperties, VaultDatabaseProperties.

--- apps/brreg-stub/src/main/java/no/nav/brregstub/config/AppConfig.java
+++ apps/brreg-stub/src/main/java/no/nav/brregstub/config/AppConfig.java
@@ -6,16 +6,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import no.nav.testnav.libs.servletcore.config.ApplicationCoreConfig;
-import no.nav.testnav.libs.database.config.FlywayConfiguration;
-import no.nav.testnav.libs.database.config.VaultHikariConfiguration;
 
 @Configuration
 @EnableJpaAuditing
 @EnableJpaRepositories(basePackages = "no.nav.brregstub.database.repository")
 @Import({
-        ApplicationCoreConfig.class,
-        VaultHikariConfiguration.class,
-        FlywayConfiguration.class,
+        ApplicationCoreConfig.class
 })
 public class AppConfig {
 }
