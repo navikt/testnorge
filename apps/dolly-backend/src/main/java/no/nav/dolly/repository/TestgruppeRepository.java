d719838c1c6b66431b14d881f582b861a6290569
Kristen Hærum <krharum@gmail.com>
2025-02-27 15:24:52 +0100

    Update query in TestgruppeRepository to use native SQL syntax and correct column names #deploy-test-dolly-backend #deploy-dolly-backend

--- apps/dolly-backend/src/main/java/no/nav/dolly/repository/TestgruppeRepository.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/repository/TestgruppeRepository.java
@@ -33,8 +33,8 @@ public interface TestgruppeRepository extends PagingAndSortingRepository<Testgru
     @Query(value = "delete from Testgruppe tg where tg.id = :testgruppeId")
     int deleteAllById(@Param("testgruppeId") Long id);
 
-    @Query(value = "select tg from Testgruppe tg " +
-            "join Bruker b on tg.opprettetAv = b " +
-            "and b.id in (:brukere)")
+    @Query(value = "select * from gruppe tg " +
+            "join bruker b on tg.opprettet_av = b.id " +
+            "and b.bruker_Id in  (:brukere)", nativeQuery = true)
     Page<Testgruppe> findAllByOpprettetAv_BrukerIdIn(@Param("brukere") List<String> brukere, PageRequest id);
 }
