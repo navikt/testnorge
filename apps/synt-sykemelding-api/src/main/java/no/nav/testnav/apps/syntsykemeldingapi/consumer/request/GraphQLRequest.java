6e11e716aaf33c73e67435cd4dbb41ea553e716b
Kristen Hærum <krharum@gmail.com>
2025-03-03 07:49:48 +0100

    Remove unused services and add new data models for dolly search service

--- apps/synt-sykemelding-api/src/main/java/no/nav/testnav/apps/syntsykemeldingapi/consumer/request/GraphQLRequest.java
+++
@@ -1,9 +0,0 @@
-package no.nav.testnav.apps.syntsykemeldingapi.consumer.request;
-
-import lombok.Builder;
-
-import java.util.Map;
-
-@Builder
-public record GraphQLRequest(String query, Map<String, Object> variables) {
-}
