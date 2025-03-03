16de3754c754f761c4a91acb58d2b95e4c39d419
Cato Olsen <olsen.cato@gmail.com>
2025-03-03 11:13:18 +0100

    - brregstub-reverse-proxy didn't define any health probes - fixed by importing common dolly-application.yml. - Tests now extend DollyApplicationContextTest (e.g. runs actual health endpoint tests).

--- proxies/brregstub-reverse-proxy/src/test/java/no/nav/testnav/proxies/brregstubreverseproxy/ApplicationContextTest.java
+++ proxies/brregstub-reverse-proxy/src/test/java/no/nav/testnav/proxies/brregstubreverseproxy/ApplicationContextTest.java
@@ -1,16 +1,8 @@
 package no.nav.testnav.proxies.brregstubreverseproxy;
 
+import no.nav.dolly.libs.test.DollyApplicationContextTest;
 import no.nav.dolly.libs.test.DollySpringBootTest;
-import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @DollySpringBootTest
-class ApplicationContextTest {
-
-    @Test
-    void load_app_context() {
-        assertThat(true).isTrue();
-    }
-
+class ApplicationContextTest extends DollyApplicationContextTest {
 }
