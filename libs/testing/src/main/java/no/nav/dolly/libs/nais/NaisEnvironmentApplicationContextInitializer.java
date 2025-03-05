e8add99ef12e8f03b4ee8f0dd64925a6c416c9d3
Cato Olsen <olsen.cato@gmail.com>
2025-03-05 16:05:16 +0100

    Enables all actuator endpoints for local profile.

--- libs/testing/src/main/java/no/nav/dolly/libs/nais/NaisEnvironmentApplicationContextInitializer.java
+++ libs/testing/src/main/java/no/nav/dolly/libs/nais/NaisEnvironmentApplicationContextInitializer.java
@@ -33,6 +33,9 @@ public class NaisEnvironmentApplicationContextInitializer implements Application
 
         log.info("Configuring environment for local profile using Secret Manager");
 
+        // Enable all actuator endpoints.
+        properties.putIfAbsent("management.endpoints.web.exposure.include", "*");
+
         // Emulating NAIS provided environment variables.
         properties.putIfAbsent("ALTINN_URL", "${sm\\://altinn-url}"); // Used by altinn3-tilgang-service only.
         properties.putIfAbsent("AZURE_APP_CLIENT_ID", "${sm\\://azure-app-client-id}");
