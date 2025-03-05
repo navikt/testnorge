69b7fd6e51b1359e9759f48488282c9869b6617c
Cato Olsen <olsen.cato@gmail.com>
2025-03-05 10:14:41 +0100

    Finalized SelfCheckCommand.

--- apps/budpro-service/src/main/java/no/nav/dolly/budpro/test/SelfCheckCommand.java
+++ apps/budpro-service/src/main/java/no/nav/dolly/budpro/test/SelfCheckCommand.java
@@ -1,6 +1,5 @@
 package no.nav.dolly.budpro.test;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.testnav.libs.reactivecore.web.WebClientError;
 import no.nav.testnav.libs.reactivecore.web.WebClientHeaders;
@@ -11,27 +10,38 @@ import reactor.core.publisher.Mono;
 
 import java.util.concurrent.Callable;
 
-@RequiredArgsConstructor
 @Slf4j
 class SelfCheckCommand implements Callable<Mono<DummyDTO>> {
 
     private final WebClient webClient;
     private final String token;
 
+    public SelfCheckCommand(WebClient webClient, String token) {
+        this.webClient = webClient
+                .mutate()
+                .baseUrl("http://localhost:8080")
+                .build();
+        this.token = token;
+    }
+
     @Override
     public Mono<DummyDTO> call() {
         var webClientErrorHandler = new WebClientError.Handler();
         return webClient
                 .get()
-                .uri(uriBuilder -> uriBuilder.path("/failure/get")
-                        .queryParam("httpStatus", "401")
-                        .queryParam("delayInMillis", 0)
+                .uri(uriBuilder -> uriBuilder
+                        .host("localhost")
+                        .port(8080)
+                        .path("/failure/get")
+                        .queryParam("httpStatus", "402")
+                        .queryParam("delayInMillis", 100000)
                         .build())
                 .headers(WebClientHeaders.bearer(token))
                 .retrieve()
                 .onStatus(HttpStatusCode::isError, webClientErrorHandler::log)
                 .bodyToMono(DummyDTO.class)
-                .retryWhen(WebClientError.is(HttpClientErrorException.class::isInstance, 1, 0));
+                .retryWhen(WebClientError.is(HttpClientErrorException.class::isInstance, 1, 0))
+                .doOnError(webClientErrorHandler::log);
 
     }
 
