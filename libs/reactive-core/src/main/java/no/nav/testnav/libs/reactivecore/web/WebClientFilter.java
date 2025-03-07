6df5b55a3113b2d80d3e7faf780d13963f46c4c5
Cato Olsen <olsen.cato@gmail.com>
2025-03-07 14:46:58 +0100

    Reverted to original implementation.

--- libs/reactive-core/src/main/java/no/nav/testnav/libs/reactivecore/web/WebClientFilter.java
+++ libs/reactive-core/src/main/java/no/nav/testnav/libs/reactivecore/web/WebClientFilter.java
@@ -13,14 +13,12 @@ import java.net.SocketException;
 import java.nio.charset.StandardCharsets;
 import java.util.concurrent.TimeoutException;
 
-@UtilityClass
 @Slf4j
+@UtilityClass
 public class WebClientFilter {
 
-    private static final String TIMEOUT = "Mottaker svarer ikke, eller har for lang svartid.";
-    private static final String UNREACHABLE = "Forbindelsen er ustabil og mottaker kunne ikke nås.";
-
     public static boolean is5xxException(Throwable throwable) {
+
         return throwable instanceof WebClientResponseException responseException &&
                 responseException.getStatusCode().is5xxServerError() ||
                 throwable instanceof WebClientRequestException requestException &&
@@ -28,52 +26,56 @@ public class WebClientFilter {
     }
 
     public static String getMessage(Throwable throwable) {
-        switch (throwable) {
 
-            case WebClientResponseException e -> {
-                return e.getResponseBodyAsString(StandardCharsets.UTF_8);
+        if (throwable instanceof WebClientResponseException responseException) {
+            return responseException.getResponseBodyAsString(StandardCharsets.UTF_8);
+
+        } else if (throwable instanceof WebClientRequestException requestException) {
+
+            if (requestException.getCause() instanceof ConnectTimeoutException ||
+                    requestException.getCause() instanceof ReadTimeoutException ||
+                    requestException.getCause() instanceof WriteTimeoutException) {
+
+                return "Mottaker svarer ikke, eller har for lang svartid.";
+
+            } else if (requestException.getCause() instanceof SocketException) {
+
+                return "Forbindelsen er ustabil og mottaker kunne ikke nås.";
+
+            } else {
+
+                return requestException.getCause().toString();
             }
 
-            case WebClientRequestException e -> {
-                switch (e.getCause()) {
-                    case ConnectTimeoutException ignored -> {
-                        return TIMEOUT;
-                    }
-                    case ReadTimeoutException ignored -> {
-                        return TIMEOUT;
-                    }
-                    case WriteTimeoutException ignored -> {
-                        return TIMEOUT;
-                    }
-                    case SocketException ignored -> {
-                        return UNREACHABLE;
-                    }
-                    default -> {
-                        return e.getCause().toString();
-                    }
-                }
-            }
+        } else if (throwable instanceof TimeoutException) {
 
-            default -> {
-                return throwable.getMessage();
-            }
+            return "Mottaker svarer ikke, eller har for lang svartid.";
 
+        } else {
+            return throwable.getMessage();
         }
     }
 
     public static HttpStatus getStatus(Throwable throwable) {
-        return switch (throwable) {
-            case WebClientResponseException e -> HttpStatus.valueOf(e.getStatusCode().value());
-            case TimeoutException ignored -> HttpStatus.REQUEST_TIMEOUT;
-            default -> HttpStatus.INTERNAL_SERVER_ERROR;
-        };
+
+        if (throwable instanceof WebClientResponseException webClientResponseException) {
+            return HttpStatus.valueOf(webClientResponseException.getStatusCode().value());
+
+        } else if (throwable instanceof TimeoutException) {
+            return HttpStatus.REQUEST_TIMEOUT;
+
+        } else {
+            return HttpStatus.INTERNAL_SERVER_ERROR;
+        }
     }
 
     public static void logErrorMessage(Throwable throwable) {
-        if ((throwable instanceof WebClientResponseException e)) {
-            log.error("CATO1: {} {}", throwable.getMessage(), e.getResponseBodyAsString(), throwable);
+
+        if ((throwable instanceof WebClientResponseException webClientResponseException)) {
+            log.error("%s, %s".formatted(throwable.getMessage(),
+                    webClientResponseException.getResponseBodyAsString()), throwable);
         } else {
-            log.error("CATO2: {}", throwable.getMessage(), throwable);
+            log.error(throwable.getMessage(), throwable);
         }
     }
 }
