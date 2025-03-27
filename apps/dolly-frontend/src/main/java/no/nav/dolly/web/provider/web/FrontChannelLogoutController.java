6e20b47509302716317183fed967ae7fe9e1fa28
Cato Olsen <olsen.cato@gmail.com>
2025-03-27 11:39:00 +0100

    Removed an NPE warning.

--- apps/dolly-frontend/src/main/java/no/nav/dolly/web/provider/web/FrontChannelLogoutController.java
+++ apps/dolly-frontend/src/main/java/no/nav/dolly/web/provider/web/FrontChannelLogoutController.java
@@ -3,6 +3,7 @@ package no.nav.dolly.web.provider.web;
 import io.valkey.Jedis;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextImpl;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +17,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/oauth2/logout")
@@ -28,28 +30,37 @@ public class FrontChannelLogoutController {
 
     @GetMapping()
     public Mono<Void> logout(@RequestParam String sid) {
+
         var sessionIds = getAllSessionIds();
         var manager = (DefaultWebSessionManager) this.webSessionManager;
         var sessions = Flux.concat(sessionIds
                 .stream()
                 .map(sessionId -> manager.getSessionStore().retrieveSession(sessionId))
                 .toList());
-
         return sessions
                 .filter(session -> session.getAttribute("SPRING_SECURITY_CONTEXT") != null)
-                .filter(session -> {
-                    var securityContext = (SecurityContextImpl) session.getAttribute("SPRING_SECURITY_CONTEXT");
-                    if (securityContext.getAuthentication().getPrincipal() instanceof DefaultOidcUser user) {
-                        var claim = user.getIdToken().getClaim("sid");
-                        return claim != null && claim.equals(sid);
-                    }
-                    return false;
-                })
+                .filter(session -> Optional
+                        .ofNullable(session.getAttribute("SPRING_SECURITY_CONTEXT"))
+                        .map(SecurityContextImpl.class::cast)
+                        .map(SecurityContextImpl::getAuthentication)
+                        .map(Authentication::getPrincipal)
+                        .filter(DefaultOidcUser.class::isInstance)
+                        .map(DefaultOidcUser.class::cast)
+                        .map(DefaultOidcUser::getIdToken)
+                        .map(token -> token.<String>getClaim("sid"))
+                        .map(claim -> claim.equals(sid))
+                        .orElse(false))
                 .flatMap(WebSession::invalidate)
                 .then();
+
     }
 
     private List<String> getAllSessionIds() {
-        return this.jedis.keys("*").stream().map(session -> session.split(":")[session.split(":").length - 1]).toList();
+        return jedis
+                .keys("*")
+                .stream()
+                .map(session -> session.split(":")[session.split(":").length - 1])
+                .toList();
     }
+
 }
