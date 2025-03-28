a5462a7dd0859f8bfef225694d4cc98e6712162f
Cato Olsen <olsen.cato@gmail.com>
2025-03-28 13:44:52 +0100

    Changes to Valkey config read from NAIS.

--- apps/dolly-frontend/src/main/java/no/nav/dolly/web/config/JedisConfig.java
+++ apps/dolly-frontend/src/main/java/no/nav/dolly/web/config/JedisConfig.java
@@ -1,8 +1,8 @@
 package no.nav.dolly.web.config;
 
 import io.valkey.DefaultJedisClientConfig;
+import io.valkey.DefaultRedisCredentials;
 import io.valkey.Jedis;
-import io.valkey.JedisClientConfig;
 import io.valkey.util.JedisURIHelper;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.testnav.libs.reactivesessionsecurity.config.OidcRedisSessionConfiguration;
@@ -13,6 +13,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
 import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * Usage in {@code FrontChannelLogoutController}.
@@ -25,18 +26,30 @@ class JedisConfig {
 
     @Bean
     Jedis jedis(
-            @Value("${spring.data.redis.host}") String host,
-            @Value("${spring.data.redis.port}") Integer port,
+            @Value("${spring.data.redis.url}") String url,
             @Value("${spring.data.redis.username}") String username,
-            @Value("${spring.data.redis.password") String password
+            @Value("${spring.data.redis.password") CharSequence password
     ) {
-        log.info("Connecting to Jedis on {}:{} with username {}", host, port, username);
+        log.info("Connecting to Jedis on {}", url);
+        var uri = validate(url);
         var config = DefaultJedisClientConfig
                 .builder()
-                .user(username)
-                .password(password)
+                .credentials(new DefaultRedisCredentials(username, password))
                 .build();
-        return new Jedis(host, port, config);
+        return new Jedis(uri, config);
+    }
+
+    private static URI validate(String url)
+            throws IllegalArgumentException {
+        try {
+            var uri = new URI(url);
+            if (!JedisURIHelper.isValid(uri)) {
+                throw new URISyntaxException(url, "Jedis does not accept URI %s".formatted(url));
+            }
+            return uri;
+        } catch (URISyntaxException e) {
+            throw new IllegalArgumentException(e);
+        }
     }
 
 }
