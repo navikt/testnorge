fab86bf1fd7a73b8e4f3a4615e228344fc53c155
Cato Olsen <olsen.cato@gmail.com>
2025-03-28 13:18:29 +0100

    Added config for Jedis username/password.

--- apps/dolly-frontend/src/main/java/no/nav/dolly/web/config/JedisConfig.java
+++ apps/dolly-frontend/src/main/java/no/nav/dolly/web/config/JedisConfig.java
@@ -1,6 +1,10 @@
 package no.nav.dolly.web.config;
 
+import io.valkey.DefaultJedisClientConfig;
 import io.valkey.Jedis;
+import io.valkey.JedisClientConfig;
+import io.valkey.util.JedisURIHelper;
+import lombok.extern.slf4j.Slf4j;
 import no.nav.testnav.libs.reactivesessionsecurity.config.OidcRedisSessionConfiguration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -8,17 +12,31 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
+import java.net.URI;
+
+/**
+ * Usage in {@code FrontChannelLogoutController}.
+ */
 @Configuration
 @Profile("idporten")
 @Import(OidcRedisSessionConfiguration.class)
+@Slf4j
 class JedisConfig {
 
     @Bean
     Jedis jedis(
             @Value("${spring.data.redis.host}") String host,
-            @Value("${spring.data.redis.port}") Integer port
+            @Value("${spring.data.redis.port}") Integer port,
+            @Value("${spring.data.redis.username}") String username,
+            @Value("${spring.data.redis.password") String password
     ) {
-        return new Jedis(host, port);
+        log.info("Connecting to Jedis on {}:{} with username {}", host, port, username);
+        var config = DefaultJedisClientConfig
+                .builder()
+                .user(username)
+                .password(password)
+                .build();
+        return new Jedis(host, port, config);
     }
 
 }
