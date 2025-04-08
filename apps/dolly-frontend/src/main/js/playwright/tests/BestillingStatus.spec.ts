c9cc6e1e7ec1b6a89781287370abc7a7b5375311
stigus <stian.gustavsson@nav.no>
2025-04-08 08:43:57 +0200

    - Mer tydelig beskrivelse og refaktor av Playwright tester #deploy-test-frontend

--- apps/dolly-frontend/src/main/js/playwright/tests/BestillingStatus.spec.ts
+++ apps/dolly-frontend/src/main/js/playwright/tests/BestillingStatus.spec.ts
@@ -3,18 +3,16 @@ import { test } from '#/globalSetup'
 import { TestComponentSelectors } from '#/mocks/Selectors'
 import { testnorgeMalBestillinger } from '#/mocks/BasicMocks'
 
-test.describe('Dolly Bestillingsstatus testing', () => {
-	test('passes', async ({ page, context }) => {
-		await page.goto('/gruppe')
-		const hentGruppeBestilling = new RegExp(/dolly-backend\/api\/v1\/bestilling\/gruppe\/1/)
-		await page.route(hentGruppeBestilling, (route) => {
-			route.fulfill({ body: JSON.stringify(testnorgeMalBestillinger) })
-		})
-
-		await page.locator('div').getByText('Testytest', { exact: true }).click()
-
-		await page.getByTestId(TestComponentSelectors.TOGGLE_VISNING_BESTILLINGER).click()
-
-		await page.locator(':nth-child(2) > .dot-body-row > .dot-body-row-columns').click()
+test('Dolly Bestillingsstatus testing', async ({ page }) => {
+	await page.goto('/gruppe')
+	const hentGruppeBestilling = new RegExp(/dolly-backend\/api\/v1\/bestilling\/gruppe\/1/)
+	await page.route(hentGruppeBestilling, (route) => {
+		route.fulfill({ body: JSON.stringify(testnorgeMalBestillinger) })
 	})
+
+	await page.locator('div').getByText('Testytest', { exact: true }).click()
+
+	await page.getByTestId(TestComponentSelectors.TOGGLE_VISNING_BESTILLINGER).click()
+
+	await page.locator(':nth-child(2) > .dot-body-row > .dot-body-row-columns').click()
 })
--- apps/dolly-frontend/src/main/js/playwright/tests/Endringsmelding.spec.ts
+++ apps/dolly-frontend/src/main/js/playwright/tests/Endringsmelding.spec.ts
@@ -1,10 +1,8 @@
 import { expect, test } from '#/globalSetup'
 import { TestComponentSelectors } from '#/mocks/Selectors'
 
-test.describe('Navigering til endringsmelding', () => {
-	test('passes', async ({ page }) => {
-		await page.goto('gruppe')
-		await page.getByTestId(TestComponentSelectors.BUTTON_HEADER_ENDRINGSMELDING).click()
-		await expect(page).toHaveURL(/\/endringsmelding/)
-	})
+test('Navigering til endringsmelding', async ({ page }) => {
+	await page.goto('gruppe')
+	await page.getByTestId(TestComponentSelectors.BUTTON_HEADER_ENDRINGSMELDING).click()
+	await expect(page).toHaveURL(/\/endringsmelding/)
 })
