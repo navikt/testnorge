cfbd08fc62a72dd7d61345fb7a537a2e7754dc6e
Cato Olsen <olsen.cato@gmail.com>
2025-04-09 12:14:04 +0200

    Removed static token preload.

--- apps/budpro-service/src/main/java/no/nav/dolly/budpro/navn/GeneratedNameService.java
+++ apps/budpro-service/src/main/java/no/nav/dolly/budpro/navn/GeneratedNameService.java
@@ -1,8 +1,6 @@
 package no.nav.dolly.budpro.navn;
 
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import no.nav.dolly.libs.texas.Texas;
 import org.springframework.stereotype.Service;
 
@@ -10,23 +8,9 @@ import java.util.Arrays;
 
 @Service
 @RequiredArgsConstructor
-@Slf4j
 public class GeneratedNameService {
 
     private final Texas texas;
-
-    @PostConstruct
-    void postConstruct() {
-        log.info("Getting token...");
-        var token = texas
-                .getToken("generer-navn-service")
-                .block();
-        if (token != null) {
-            log.info("Got a token with TTL of {} seconds", token.expires_in());
-        } else {
-            log.error("Got a null token");
-        }
-    }
 
     public String[] getNames(Long seed, int number) {
         var names = new GenererNavnCommand(texas, seed, number).call();
