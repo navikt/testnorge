21d983a5bf7eda6027e8701209e87fcab59fe4b8
Kristen Hærum <krharum@gmail.com>
2025-04-09 13:43:26 +0200

    Feature/update apioversikt.yml and ApiOversiktService to enhance API documentation and response structure

--- apps/api-oversikt-service/src/main/java/no/nav/testnav/apps/apioversiktservice/provider/ApiOversiktController.java
+++ apps/api-oversikt-service/src/main/java/no/nav/testnav/apps/apioversiktservice/provider/ApiOversiktController.java
@@ -1,12 +1,11 @@
 package no.nav.testnav.apps.apioversiktservice.provider;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import lombok.RequiredArgsConstructor;
 import no.nav.testnav.apps.apioversiktservice.service.ApiOversiktService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 @RequestMapping("/api/v1/apioversikt")
 @RestController
@@ -16,7 +15,7 @@ public class ApiOversiktController {
     private final ApiOversiktService apiOversiktService;
 
     @GetMapping
-    public String getApiOversikt() {
+    public JsonNode getApiOversikt() {
 
         return apiOversiktService.getDokumeter();
     }
--- apps/api-oversikt-service/src/main/java/no/nav/testnav/apps/apioversiktservice/service/ApiOversiktService.java
+++ apps/api-oversikt-service/src/main/java/no/nav/testnav/apps/apioversiktservice/service/ApiOversiktService.java
@@ -1,8 +1,12 @@
 package no.nav.testnav.apps.apioversiktservice.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
+import org.yaml.snakeyaml.Yaml;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -13,19 +17,24 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class ApiOversiktService {
 
-    private static final String pathResource = "data/apioversikt.yml";
+    private static final String PATH_RESOURCE = "data/apioversikt.yml";
+    private final ObjectMapper objectMapper;
 
-    public String getDokumeter() {
+    public JsonNode getDokumeter() {
 
-        var resource = new ClassPathResource(pathResource);
+        var resource = new ClassPathResource(PATH_RESOURCE);
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(resource.getInputStream(), UTF_8))) {
-            var contents = reader.lines().collect(Collectors.joining("\n"));
+            var yaml = reader.lines().collect(Collectors.joining("\n"));
 
-            return contents;
+            var maps = new Yaml().load(yaml);
+            var jsonString = objectMapper.writeValueAsString(maps);
+            return objectMapper.readTree(jsonString);
+
         } catch (IOException e) {
-            log.error("Lesing av query ressurs {} feilet", pathResource, e);
+            log.error("Lesing av query ressurs {} feilet", PATH_RESOURCE, e);
             return null;
         }
     }
