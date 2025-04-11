66d4f6a0da2641f5bff98e32077220702521df81
Cato Olsen <olsen.cato@gmail.com>
2025-04-11 11:10:19 +0200

    Added JavaDoc.

--- libs/servlet-security/src/main/java/no/nav/dolly/libs/security/config/DollyHttpSecurity.java
+++ libs/servlet-security/src/main/java/no/nav/dolly/libs/security/config/DollyHttpSecurity.java
@@ -5,9 +5,17 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 
+/**
+ * Utility class for configuring Spring Security for a servlet web application.
+ */
 @UtilityClass
 public class DollyHttpSecurity {
 
+    /**
+     * <p>Allow access to certain common endpoints ({@code /error}, {@code /internal}, {@code /swagger} etc.) without authentication, protecting all others.</p>
+     * <p>Customize further as needed after calling this method, if necessary.</p>
+     * @return A customizer for use as {@code .authorizeHttpRequests(DollyHttpSecurity.withDefaultHttpRequests())}.
+     */
     public static Customizer<AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry> withDefaultHttpRequests() {
         return registry -> registry
                 .requestMatchers(
