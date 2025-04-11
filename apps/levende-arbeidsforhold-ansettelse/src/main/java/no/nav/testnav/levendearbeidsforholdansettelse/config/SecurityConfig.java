c0d0900eb0fd16a6caa26596bb5802deac7edb3b
Cato Olsen <olsen.cato@gmail.com>
2025-04-11 11:12:44 +0200

    Replaced .authorizeExchange with use of DollyServerHttpSecurity; note the use of further customization.

--- apps/levende-arbeidsforhold-ansettelse/src/main/java/no/nav/testnav/levendearbeidsforholdansettelse/config/SecurityConfig.java
+++ apps/levende-arbeidsforhold-ansettelse/src/main/java/no/nav/testnav/levendearbeidsforholdansettelse/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package no.nav.testnav.levendearbeidsforholdansettelse.config;
 
 import lombok.RequiredArgsConstructor;
+import no.nav.dolly.libs.security.config.DollyServerHttpSecurity;
 import no.nav.testnav.libs.reactivesecurity.manager.JwtReactiveAuthenticationManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,28 +14,26 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
 @RequiredArgsConstructor
-public class SecurityConfig {
+class SecurityConfig {
 
     private final JwtReactiveAuthenticationManager jwtReactiveAuthenticationManager;
 
     @Bean
-    @SuppressWarnings("java:S4502")
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity httpSecurity) {
+    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity httpSecurity) {
         return httpSecurity
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .authorizeExchange(authorizeConfig -> authorizeConfig.pathMatchers(
-                                "/internal/**",
-                                "/webjars/**",
-                                "/swagger-resources/**",
-                                "/v3/api-docs/**",
-                                "/swagger-ui/**",
-                                "/swagger",
-                                "/error",
-                                "/swagger-ui.html",
-                                "/h2/**",
-                                "/member/**")
-                .permitAll().anyExchange().authenticated())
+                .authorizeExchange(spec -> {
+                    DollyServerHttpSecurity.withDefaultHttpRequests().customize(spec);
+                    spec
+                            .pathMatchers(
+                                    "/h2/**",
+                                    "/member/**")
+                            .permitAll()
+                            .anyExchange()
+                            .authenticated();
+                })
                 .oauth2ResourceServer(oauth2RSConfig -> oauth2RSConfig.jwt(jwtSpec -> jwtSpec.authenticationManager(jwtReactiveAuthenticationManager)))
                 .build();
     }
+
 }
