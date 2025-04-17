28a74e1f3f9f5def917faea601d13f4efcd44724
Cato Olsen <olsen.cato@gmail.com>
2025-04-17 15:50:48 +0200

    Changed Lettuce confic in OidcRedisSessionConfiguration to specifically use SSL.

--- apps/dolly-frontend/src/main/java/no/nav/dolly/web/config/JedisConfig.java
+++ apps/dolly-frontend/src/main/java/no/nav/dolly/web/config/JedisConfig.java
@@ -6,6 +6,7 @@ import io.valkey.Jedis;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.testnav.libs.reactivesessionsecurity.config.OidcRedisSessionConfiguration;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -24,16 +25,18 @@ class JedisConfig {
 
     @Bean
     Jedis jedis(
-            @Value("${spring.data.redus.url}") URI uri,
+            @Value("${spring.data.redis.host}") String host,
+            @Value("${spring.data.redis.port}") Integer port,
             @Value("${spring.data.redis.username}") String username,
             @Value("${spring.data.redis.password}") CharSequence password
     ) {
-        log.info("Connecting to Jedis on {}", uri);
+        log.info("Connecting to Jedis on {}:{} using SSL", host, port);
         var config = DefaultJedisClientConfig
                 .builder()
                 .credentials(new DefaultRedisCredentials(username, password))
+                .ssl(true)
                 .build();
-        return new Jedis(uri, config);
+        return new Jedis(host, port, config);
     }
 
 }
