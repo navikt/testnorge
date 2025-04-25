5b99db48f9ba151ea8df5408ca1a881dd7552785
Kristen Hærum <krharum@gmail.com>
2025-04-25 10:36:23 +0200

    Feature: Refactor RequestLoggingFilter to use WebFilter and conditionally log traceId

--- apps/dolly-backend/src/main/java/no/nav/dolly/config/RequestLoggingFilter.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/config/RequestLoggingFilter.java
@@ -1,31 +1,23 @@
 package no.nav.dolly.config;
 
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
 import org.slf4j.MDC;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
 
-import java.io.IOException;
 import java.util.UUID;
 
-@Component
+@Configuration
 public class RequestLoggingFilter implements WebFilter {
-  
-  @Override
-  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-    String traceId = UUID.randomUUID().toString();
-    MDC.put("traceId", traceId);
-    chain.doFilter(request, response);
-  }
 
   @Override
-  public Mono<Void> filter(ServletRequest request, ServletResponse response, FilterChain chain) {
-    String traceId = UUID.randomUUID().toString();
-    MDC.put("traceId", traceId);
-    chain.doFilter(request, response);
+  public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+    if (!exchange.getRequest().getPath().toString().contains("/api")) {
+      String traceId = UUID.randomUUID().toString();
+      MDC.put("traceId", traceId);
+    }
+    return chain.filter(exchange);
   }
 }
