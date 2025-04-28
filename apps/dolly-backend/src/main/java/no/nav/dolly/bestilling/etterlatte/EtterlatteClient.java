556de58ada190c763d8425dc64df6596c30abd8e
Kristen Hærum <krharum@gmail.com>
2025-04-28 15:37:50 +0200

    Feature: Refactor EtterlatteClient and add FoedselsdatoUtility for age verification #deploy-test-dolly-backend

--- apps/dolly-backend/src/main/java/no/nav/dolly/bestilling/etterlatte/EtterlatteClient.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/bestilling/etterlatte/EtterlatteClient.java
@@ -14,13 +14,22 @@ import no.nav.dolly.domain.jpa.BestillingProgress;
 import no.nav.dolly.domain.resultset.RsDollyUtvidetBestilling;
 import no.nav.dolly.domain.resultset.dolly.DollyPerson;
 import no.nav.dolly.mapper.MappingContextUtils;
+import no.nav.dolly.util.FoedselsdatoUtility;
 import no.nav.dolly.util.TransactionHelperService;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+import static org.apache.commons.lang3.BooleanUtils.isTrue;
 
 @Slf4j
 @Service
@@ -49,9 +58,9 @@ public class EtterlatteClient implements ClientRegister {
                             })
                     )
                     .collectList()
-                            .map(statusList -> statusList.stream()
-                                    .filter(status -> !status.is2xxSuccessful())
-                                    .findFirst().orElse(HttpStatus.OK))
+                    .map(statusList -> statusList.stream()
+                            .filter(status -> !status.is2xxSuccessful())
+                            .findFirst().orElse(HttpStatus.OK))
                     .map(status -> futurePersist(progress, status)));
         }
         return Flux.empty();
@@ -66,7 +75,7 @@ public class EtterlatteClient implements ClientRegister {
     private ClientFuture futurePersist(BestillingProgress progress, HttpStatus httpStatus) {
 
         var status = httpStatus.is2xxSuccessful() ? "OK" :
-                "Feil= %d %s".formatted(httpStatus.value(),httpStatus.getReasonPhrase());
+                "Feil= %d %s".formatted(httpStatus.value(), httpStatus.getReasonPhrase());
 
         return () -> {
             transactionHelperService.persister(progress, BestillingProgress::setEtterlatteStatus, status);
@@ -80,24 +89,74 @@ public class EtterlatteClient implements ClientRegister {
                 .map(PdlPersonBolk::getData)
                 .map(PdlPersonBolk.Data::getHentPersonBolk)
                 .flatMap(Flux::fromIterable)
-                .map(personBolk -> VedtakRequestDTO.builder()
-                        .barn(personBolk.getPerson().getForelderBarnRelasjon().stream()
-                                .filter(PdlPerson.ForelderBarnRelasjon::isBarn)
-                                .map(PdlPerson.ForelderBarnRelasjon::getRelatertPersonsIdent)
-                                .toList())
-                        .avdoed(!personBolk.getPerson().getDoedsfall().isEmpty() ? personBolk.getIdent() :
-                                getRelatertVedSivilstand(personBolk.getPerson()))
-                        .gjenlevende(personBolk.getPerson().getDoedsfall().isEmpty() ? personBolk.getIdent() :
-                                getRelatertVedSivilstand(personBolk.getPerson()))
-                        .build())
-                .reduce(new VedtakRequestDTO(), (a, b) -> b);
+                .flatMap(personBolk -> personServiceConsumer.getPdlPersoner(
+                        Stream.of(Stream.of(ident),
+                                        personBolk.getPerson().getForelderBarnRelasjon().stream()
+                                                .map(PdlPerson.ForelderBarnRelasjon::getRelatertPersonsIdent)
+                                                .filter(Objects::nonNull),
+                                        personBolk.getPerson().getSivilstand().stream()
+                                                .filter(sivilstand -> sivilstand.isGift() || sivilstand.isGjenlevende())
+                                                .map(PdlPerson.Sivilstand::getRelatertVedSivilstand)
+                                                .filter(Objects::nonNull)
+                                )
+                                .flatMap(Function.identity())
+                                .distinct()
+                                .toList()))
+                .map(PdlPersonBolk::getData)
+                .map(PdlPersonBolk.Data::getHentPersonBolk)
+                .flatMap(Flux::fromIterable)
+                .map(personBolk -> Map.of(personBolk.getIdent(), personBolk))
+                .reduce(new HashMap<String, PdlPersonBolk.PersonBolk>(), (map, element) -> {
+                    map.putAll(element);
+                    return map;
+                })
+                .flatMap(personer -> {
+                    if (isTrue(FoedselsdatoUtility.isMyndig(personer.get(ident)))) {
+                        return Mono.just(VedtakRequestDTO.builder()
+                                        .barn(personer.get(ident).getPerson().getForelderBarnRelasjon().stream()
+                                                .filter(PdlPerson.ForelderBarnRelasjon::isBarn)
+                                                .map(PdlPerson.ForelderBarnRelasjon::getRelatertPersonsIdent)
+                                                .distinct()
+                                                .toList())
+                                        .avdoed(getForelderFraForelder(personer, ident, bolk -> !bolk.getPerson().getDoedsfall().isEmpty()))
+                                        .gjenlevende(getForelderFraForelder(personer, ident, bolk -> bolk.getPerson().getDoedsfall().isEmpty()))
+                                        .build());
+                    } else {
+                        return Mono.just(VedtakRequestDTO.builder()
+                                .barn(List.of(personer.get(ident).getIdent()))
+                                .gjenlevende(getForlderfraBarn(personer, ident,
+                                        bolk -> bolk.getPerson().getDoedsfall().isEmpty()))
+                                .avdoed(getForlderfraBarn(personer, ident, bolk -> !bolk.getPerson().getDoedsfall().isEmpty()))
+                                .build());
+                    }
+                });
     }
 
-    private static String getRelatertVedSivilstand(PdlPerson.Person person) {
+    private static String getForlderfraBarn(HashMap<String, PdlPersonBolk.PersonBolk> personer, String ident, Gjenlevende gjenlevende) {
+        return personer.get(ident).getPerson().getForelderBarnRelasjon().stream()
+                .filter(PdlPerson.ForelderBarnRelasjon::isForelder)
+                .map(PdlPerson.ForelderBarnRelasjon::getRelatertPersonsIdent)
+                .map(personer::get)
+                .filter(gjenlevende::apply)
+                .map(PdlPersonBolk.PersonBolk::getIdent)
+                .findFirst().orElse(null);
+    }
 
-        return person.getSivilstand().stream()
-                .filter(sivilstand -> sivilstand.isGift() || sivilstand.isGjenlevende())
-                .map(PdlPerson.Sivilstand::getRelatertVedSivilstand)
-                .findAny().orElse(null);
+
+    private static String getForelderFraForelder(HashMap<String, PdlPersonBolk.PersonBolk> personer, String ident, Gjenlevende gjenlevende) {
+
+        return personer.get(ident).getPerson().getForelderBarnRelasjon().stream()
+                .filter(PdlPerson.ForelderBarnRelasjon::isBarn)
+                .map(PdlPerson.ForelderBarnRelasjon::getRelatertPersonsIdent)
+                .map(personer::get)
+                .map(PdlPersonBolk.PersonBolk::getPerson)
+                .map(PdlPerson.Person::getForelderBarnRelasjon)
+                .flatMap(Collection::stream)
+                .filter(PdlPerson.ForelderBarnRelasjon::isForelder)
+                .map(PdlPerson.ForelderBarnRelasjon::getRelatertPersonsIdent)
+                .map(personer::get)
+                .filter(gjenlevende::apply)
+                .map(PdlPersonBolk.PersonBolk::getIdent)
+                .findFirst().orElse(null);
     }
 }
