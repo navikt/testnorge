f742a999674d07d178f4ad88ec71981745349f41
stigus <stian.gustavsson@nav.no>
2025-05-07 13:43:21 +0200

    - Fix locking functionality for groups and update related components - Add tests for the GruppeHeader functionality - Refactoring and updates

--- apps/dolly-frontend/src/main/js/src/utils/hooks/useDokumenter.tsx
+++ apps/dolly-frontend/src/main/js/src/utils/hooks/useDokumenter.tsx
@@ -13,3 +13,46 @@ export const useDokumenterFraMal = (malId: string) => {
 		error: error,
 	}
 }
+
+export const useJoarkPDF = (journalpostId?: number, dokumentInfoId?: number, miljo?: string) => {
+	const { data, isLoading, error } = useSWR<Blob, Error>(
+		journalpostId && dokumentInfoId && miljo
+			? `/testnav-joark-dokument-service/api/v2/journalpost/${journalpostId}/dokumenter/${dokumentInfoId}/pdf`
+			: null,
+		fetcher,
+		{
+			revalidateOnFocus: false,
+			headers: {
+				miljo,
+				Accept: 'application/pdf',
+				'Content-Type': 'application/pdf',
+			},
+		},
+	)
+
+	return {
+		pdf: data,
+		loading: isLoading,
+		error,
+	}
+}
+
+export const useHistarkPDF = (dokumentInfoId?: number) => {
+	const { data, isLoading, error } = useSWR<Blob, Error>(
+		dokumentInfoId ? `/testnav-histark-proxy/api/saksmapper/${dokumentInfoId}/pdf` : null,
+		fetcher,
+		{
+			revalidateOnFocus: false,
+			headers: {
+				Accept: 'application/pdf',
+				'Content-Type': 'application/pdf',
+			},
+		},
+	)
+
+	return {
+		pdf: data,
+		loading: isLoading,
+		error,
+	}
+}
