7f4ccec57d501990dc86ff716933319ad9c180ae
Kristen Hærum <krharum@gmail.com>
2025-05-07 09:45:08 +0200

    Refactor ApplicationContextTest: enable test by removing @Disabled annotation #deploy-proxy-aareg

--- proxies/aareg-proxy/src/test/java/no/nav/testnav/proxies/aareg/ApplicationContextTest.java
+++ proxies/aareg-proxy/src/test/java/no/nav/testnav/proxies/aareg/ApplicationContextTest.java
@@ -1,10 +1,8 @@
 package no.nav.testnav.proxies.aareg;
 
-import no.nav.dolly.libs.test.DollySpringBootTest;
 import no.nav.dolly.libs.test.DollyApplicationContextTest;
-import org.junit.jupiter.api.Disabled;
+import no.nav.dolly.libs.test.DollySpringBootTest;
 
-@Disabled
 @DollySpringBootTest
 class ApplicationContextTest extends DollyApplicationContextTest {
 }
