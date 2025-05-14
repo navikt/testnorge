1d4a3d4411e8351ead70576ca0dba74cf11edf6e
Cato Olsen <olsen.cato@gmail.com>
2025-05-14 12:22:53 +0200

    Removes support for secure logs.

--- apps/budpro-service/src/main/java/no/nav/dolly/budpro/ansettelsestype/AnsettelsestypeService.java
+++ apps/budpro-service/src/main/java/no/nav/dolly/budpro/ansettelsestype/AnsettelsestypeService.java
@@ -3,7 +3,6 @@ package no.nav.dolly.budpro.ansettelsestype;
 import jakarta.annotation.PostConstruct;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import no.nav.dolly.libs.logging.Markers;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
@@ -30,7 +29,6 @@ public class AnsettelsestypeService {
             }
         }
         log.info("Read {} lines into memory", data.size());
-        log.info(Markers.SECURE, "Test");
     }
 
     List<String> getAll() {
