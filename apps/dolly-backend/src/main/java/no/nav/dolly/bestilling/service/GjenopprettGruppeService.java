252f2d6ce79508e725665aad3846617eaa38aa9d
Kristen Hærum <kristen.herum@nav.no>
2025-05-19 11:51:54 +0200

    Feature/diverse teknisk gjeld (#3863)
    * Feature: Update FullmaktClient to use FULLMAKT constant in status message
    
    * Feature: Remove unused MDC logging and related imports from service classes
    
    * Feature: Refactor RequestLoggingFilter to use WebFilter and conditionally log traceId
    
    * Feature: Update logging configuration to include traceId and refactor RequestLoggingFilter to use WebFilter
    
    * Feature: Simplify response handling in EtterlatteClient by consolidating status mapping
    
    * Feature: Refactor sendTagsPaaGruppe method for improved readability and efficiency
    
    * Feature: Update MalBestillingController and MalBestillingService to return Mono for reactive support
    
    * Feature: Enhance logging for arbeidsforhold operations with pretty JSON formatting #deploy-test-dolly-backend

--- apps/dolly-backend/src/main/java/no/nav/dolly/bestilling/service/GjenopprettGruppeService.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/bestilling/service/GjenopprettGruppeService.java
@@ -16,22 +16,17 @@ import no.nav.dolly.metrics.CounterCustomRegistry;
 import no.nav.dolly.repository.IdentRepository.GruppeBestillingIdent;
 import no.nav.dolly.service.BestillingService;
 import no.nav.dolly.service.IdentService;
-import no.nav.dolly.util.ThreadLocalContextLifter;
 import no.nav.dolly.util.TransactionHelperService;
 import no.nav.testnav.libs.reactivecore.web.WebClientError;
-import org.slf4j.MDC;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Hooks;
-import reactor.core.publisher.Operators;
 
 import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 
 import static java.util.Objects.nonNull;
-import static no.nav.dolly.util.MdcUtil.MDC_KEY_BESTILLING;
 
 @Slf4j
 @Service
@@ -71,8 +66,6 @@ public class GjenopprettGruppeService extends DollyBestillingService {
     public void executeAsync(Bestilling bestilling) {
 
         log.info("Bestilling med id=#{} og type={} er startet ...", bestilling.getId(), getBestillingType(bestilling));
-        MDC.put(MDC_KEY_BESTILLING, bestilling.getId().toString());
-        Hooks.onEachOperator(Operators.lift(new ThreadLocalContextLifter<>()));
 
         var bestKriterier = getDollyBestillingRequest(bestilling);
         if (nonNull(bestKriterier)) {
