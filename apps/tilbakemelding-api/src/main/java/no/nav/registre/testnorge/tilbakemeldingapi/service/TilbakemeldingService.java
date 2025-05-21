47f119d6c856184d15cdb5face144a84f4e8055a
stigus <stian.gustavsson@nav.no>
2025-05-21 10:56:18 +0200

    Add logging for non-anonymous feedback submissions
    Introduced logging to capture user details when feedback is submitted by non-anonymous users. This change enhances traceability and provides better insights into feedback origins. Also annotated the service with @Slf4j for logging support.
    #deploy-tilbakemelding-api

--- apps/tilbakemelding-api/src/main/java/no/nav/registre/testnorge/tilbakemeldingapi/service/TilbakemeldingService.java
+++ apps/tilbakemelding-api/src/main/java/no/nav/registre/testnorge/tilbakemeldingapi/service/TilbakemeldingService.java
@@ -1,11 +1,14 @@
 package no.nav.registre.testnorge.tilbakemeldingapi.service;
 
+import io.swagger.v3.core.util.Json;
+import lombok.extern.slf4j.Slf4j;
 import no.nav.registre.testnorge.tilbakemeldingapi.consumer.ProfilApiConsumer;
 import no.nav.registre.testnorge.tilbakemeldingapi.domain.Tilbakemelding;
 import no.nav.testnav.libs.slack.consumer.SlackConsumer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class TilbakemeldingService {
     private final SlackConsumer slackConsumer;
@@ -23,6 +26,9 @@ public class TilbakemeldingService {
     }
 
     public void publish(Tilbakemelding tilbakemelding) {
+        if (Boolean.FALSE.equals(tilbakemelding.getIsAnonym())) {
+            log.info("Bruker {} har sendt tilbakemelding", Json.pretty(profilApiConsumer.getBruker()));
+        }
         String visningsNavn = Boolean.TRUE.equals(tilbakemelding.getIsAnonym())
                 ? "Anonym"
                 : profilApiConsumer.getBruker().getVisningsNavn();
