f6d8b5ca84aa4d3381c576dbd45942470628bfcb
Cato Olsen <olsen.cato@gmail.com>
2025-05-23 11:23:27 +0200

    Adds proper Spring Security context authentication after success.

--- proxies/texas-proxy/src/main/java/no/nav/dolly/proxy/texas/DollyAuthorizationHeaderFilter.java
+++ proxies/texas-proxy/src/main/java/no/nav/dolly/proxy/texas/DollyAuthorizationHeaderFilter.java
@@ -4,6 +4,9 @@ import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
@@ -11,6 +14,7 @@ import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 @RequiredArgsConstructor
 class DollyAuthorizationHeaderFilter implements WebFilter {
@@ -44,14 +48,14 @@ class DollyAuthorizationHeaderFilter implements WebFilter {
         if (header == null || header.isEmpty()) {
             var response = exchange.getResponse();
             response.setStatusCode(HttpStatus.UNAUTHORIZED);
-            response.getHeaders().add(HttpHeaders.WWW_AUTHENTICATE, "Bearer error=\"missing_token\", error_description=\"You need a special Dolly token\"");
+            response.getHeaders().add(HttpHeaders.WWW_AUTHENTICATE, "Bearer error=\"missing_token\", error_description=\"You need a special Dolly token denoted by 'Dolly ' (not 'Bearer ').\"");
             return response.setComplete();
         }
 
         if (!header.startsWith(PREFIX)) {
             var response = exchange.getResponse();
             response.setStatusCode(HttpStatus.UNAUTHORIZED);
-            response.getHeaders().add(HttpHeaders.WWW_AUTHENTICATE, "Bearer error=\"invalid_token\", error_description=\"This is not a Dolly token\"");
+            response.getHeaders().add(HttpHeaders.WWW_AUTHENTICATE, "Bearer error=\"invalid_token\", error_description=\"This is not a Dolly token denoted by 'Dolly ' (not 'Bearer ').\"");
             return response.setComplete();
         }
 
@@ -60,11 +64,15 @@ class DollyAuthorizationHeaderFilter implements WebFilter {
         if (!sharedSecret.equals(providedSecret)) {
             var response = exchange.getResponse();
             response.setStatusCode(HttpStatus.UNAUTHORIZED);
-            response.getHeaders().add(HttpHeaders.WWW_AUTHENTICATE, "Bearer error=\"invalid_token\", error_description=\"Invalid Dolly secret\"");
+            response.getHeaders().add(HttpHeaders.WWW_AUTHENTICATE, "Bearer error=\"invalid_token\", error_description=\"Invalid Dolly token\"");
             return response.setComplete();
         }
 
-        return chain.filter(exchange);
+        var authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_DOLLY_SERVICE"));
+        var authentication = new UsernamePasswordAuthenticationToken(providedSecret, null, authorities);
+        return chain
+                .filter(exchange)
+                .contextWrite(ReactiveSecurityContextHolder.withAuthentication(authentication));
 
     }
 }
