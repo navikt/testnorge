41ef51121d6807c3b5b76c99bfeb6dd84dd16713
Betsy Carina Traran <betsy.carina.traran@nav.no>
2025-05-27 17:15:39 +0200

    Utkast bruker dropdown

--- apps/dolly-frontend/src/main/js/src/components/layout/header/BrukerDropdown.tsx
+++ apps/dolly-frontend/src/main/js/src/components/layout/header/BrukerDropdown.tsx
@@ -8,6 +8,27 @@ import { TestComponentSelectors } from '#/mocks/Selectors'
 import { ActionMenuWrapper, DropdownStyledIcon, DropdownStyledLink } from './ActionMenuWrapper'
 import { PreloadableActionMenuItem } from '@/utils/PreloadableActionMenuItem'
 
+const myTeamsMock = [
+	{
+		name: 'Team Dolly',
+		id: 'team-dolly',
+	},
+	{
+		name: 'Team Black Sheep',
+		id: 'team-black-sheep',
+	},
+]
+
+const currentMock = {
+	brukerId: '952ab92e-926f-4ac4-93d7-f2d552025caf',
+	brukernavn: 'Traran, Betsy Carina',
+	brukertype: 'AZURE',
+	epost: 'Betsy.Carina.Traran@nav.no',
+	grupper: ['2d7f1c0d-5784-4f81-8bb2-8f3a79f8f949', '9c7efec1-1599-4216-a67e-6fd53a6a951c'],
+	representerer: null,
+	// representerer: 'team-dolly',
+}
+
 export const BrukerDropdown = () => {
 	const { brukerProfil } = useBrukerProfil()
 	const { brukerBilde } = useBrukerProfilBilde()
@@ -31,14 +52,52 @@ export const BrukerDropdown = () => {
 					</ActionMenu.Trigger>
 				}
 			>
-				<PreloadableActionMenuItem
-					route="/minside"
-					style={{ color: '#212529' }}
-					dataTestId={TestComponentSelectors.BUTTON_PROFIL_MINSIDE}
-				>
-					<DropdownStyledIcon kind="person" fontSize="1.5rem" />
-					<DropdownStyledLink href="/minside">Min side</DropdownStyledLink>
-				</PreloadableActionMenuItem>
+				{/*TODO: Blir det dobbelt opp med group naar ActionMenuWrapper wrapper alt i en group?*/}
+				<ActionMenu.Group label="Representasjon">
+					<ActionMenu.Item
+						onClick={() => {}}
+						style={{
+							color: '#212529',
+							backgroundColor: !currentMock?.representerer ? '#99C3FF' : null,
+						}}
+					>
+						<DropdownStyledIcon kind="person" fontSize="1.5rem" />
+						<DropdownStyledLink href="">
+							{brukerProfil?.visningsNavn + (!currentMock?.representerer ? ' (valgt)' : '')}
+						</DropdownStyledLink>
+					</ActionMenu.Item>
+					{myTeamsMock?.map((team) => (
+						<ActionMenu.Item
+							onClick={() => {}}
+							key={team.id}
+							style={{
+								color: '#212529',
+								backgroundColor: currentMock?.representerer === team.id ? '#99C3FF' : null,
+							}}
+						>
+							<DropdownStyledIcon kind="group" fontSize="1.5rem" />
+							<DropdownStyledLink href="">
+								{team.name + (currentMock?.representerer === team.id ? ' (valgt)' : '')}
+							</DropdownStyledLink>
+						</ActionMenu.Item>
+					))}
+				</ActionMenu.Group>
+				<ActionMenu.Divider />
+				<ActionMenu.Group label="Administrasjon">
+					<PreloadableActionMenuItem
+						route="/minside"
+						style={{ color: '#212529' }}
+						dataTestId={TestComponentSelectors.BUTTON_PROFIL_MINSIDE}
+					>
+						<DropdownStyledIcon kind="person" fontSize="1.5rem" />
+						<DropdownStyledLink href="/minside">Min side</DropdownStyledLink>
+					</PreloadableActionMenuItem>
+					<ActionMenu.Item onClick={() => {}} style={{ color: '#212529' }}>
+						<DropdownStyledIcon kind="group" fontSize="1.5rem" />
+						<DropdownStyledLink href="/team">Mine team</DropdownStyledLink>
+					</ActionMenu.Item>
+				</ActionMenu.Group>
+				<ActionMenu.Divider />
 				<ActionMenu.Item onClick={() => logoutBruker()} style={{ color: '#212529' }}>
 					<DropdownStyledIcon kind="logout" fontSize="1.5rem" />
 					<DropdownStyledLink href="#">Logg ut</DropdownStyledLink>
