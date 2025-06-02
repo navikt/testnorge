4692f2571f32e02f8ae8c312c2b3b70b946c19c7
Kristen Hærum <krharum@gmail.com>
2025-06-02 11:53:29 +0200

    Revert "Fix: Add kodeverknavn to KodeverkBetydningerResponse and implement filtering logic for specific entries"
    This reverts commit 62bfeb798e907ff1e12b702266027667528ce559.

--- apps/kodeverk-service/src/main/java/no/nav/testnav/kodeverkservice/consumer/KodeverkSelectorService.java
+++ apps/kodeverk-service/src/main/java/no/nav/testnav/kodeverkservice/consumer/KodeverkSelectorService.java
@@ -3,7 +3,6 @@ package no.nav.testnav.kodeverkservice.consumer;
 import lombok.RequiredArgsConstructor;
 import no.nav.testnav.kodeverkservice.service.KodeverkService;
 import no.nav.testnav.kodeverkservice.utility.TemaHistarkUtility;
-import no.nav.testnav.kodeverkservice.utility.VergemaalFylkesembeterUtility;
 import no.nav.testnav.kodeverkservice.utility.YrkesklassifiseringUtility;
 import no.nav.testnav.libs.dto.kodeverkservice.v1.KodeverkAdjustedDTO;
 import no.nav.testnav.libs.dto.kodeverkservice.v1.KodeverkDTO;
@@ -11,7 +10,6 @@ import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 import static no.nav.testnav.kodeverkservice.utility.CommonKeysAndUtils.TEMAHISTARK;
-import static no.nav.testnav.kodeverkservice.utility.CommonKeysAndUtils.VERGEMAAL_FYLKESEMBETER;
 import static no.nav.testnav.kodeverkservice.utility.CommonKeysAndUtils.YRKESKLASSIFISERING;
 
 @Service
@@ -25,7 +23,6 @@ public class KodeverkSelectorService {
         return switch (kodeverk) {
             case TEMAHISTARK -> Mono.just(TemaHistarkUtility.getKodeverk());
             case YRKESKLASSIFISERING -> Mono.just(YrkesklassifiseringUtility.getKodeverk());
-            case VERGEMAAL_FYLKESEMBETER -> Mono.just(VergemaalFylkesembeterUtility.getKodeverk());
             default -> kodeverkService.getKodeverkMap(kodeverk);
         };
     }
@@ -35,7 +32,6 @@ public class KodeverkSelectorService {
         return switch (kodeverkNavn) {
             case TEMAHISTARK -> Mono.just(TemaHistarkUtility.getKodeverkAdjusted());
             case YRKESKLASSIFISERING -> Mono.just(YrkesklassifiseringUtility.getKodeverkAdjusted());
-            case VERGEMAAL_FYLKESEMBETER -> Mono.just(VergemaalFylkesembeterUtility.getKodeverkAdjusted());
             default -> kodeverkService.getKodeverkByName(kodeverkNavn);
         };
     }
