de7d7f6c612d899b63e595a1085b4b4791bc9bb9
Kristen Hærum <krharum@gmail.com>
2025-08-08 10:53:07 +0200

    Refactor BestillingProgressService to enhance reactive programming support by consolidating fetch methods and improving exception handling in tests for better clarity and consistency.

--- apps/dolly-backend/src/main/java/no/nav/dolly/service/BestillingProgressService.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/service/BestillingProgressService.java
@@ -5,7 +5,6 @@ import no.nav.dolly.domain.jpa.BestillingProgress;
 import no.nav.dolly.exceptions.NotFoundException;
 import no.nav.dolly.repository.BestillingProgressRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -15,26 +14,10 @@ public class BestillingProgressService {
 
     private final BestillingProgressRepository bestillingProgressRepository;
 
-    public Mono<BestillingProgress> save(BestillingProgress progress) {
-
-        return bestillingProgressRepository.save(progress);
-    }
-
-    public Flux<BestillingProgress> fetchBestillingProgressByBestillingsIdFromDB(Long bestillingsId) {
+    public Flux<BestillingProgress> fetchBestillingProgressByBestillingId(Long bestillingsId) {
 
         return bestillingProgressRepository.findByBestillingId(bestillingsId)
                 .switchIfEmpty(Mono.error(
                         new NotFoundException("Kunne ikke finne bestillingsprogress med bestillingId=" + bestillingsId)));
-    }
-
-    public Flux<BestillingProgress> fetchBestillingProgressByBestillingId(Long bestillingsId) {
-
-        return bestillingProgressRepository.findByBestillingId(bestillingsId);
-    }
-
-    @Transactional
-    public Mono<BestillingProgress> swapIdent(String oldIdent, String newIdent) {
-
-        return bestillingProgressRepository.swapIdent(oldIdent, newIdent);
     }
 }
