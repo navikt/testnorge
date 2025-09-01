5499cc23324d50d21a675f304fc2a2f498a4aa51
Kristen Hærum <krharum@gmail.com>
2025-09-01 14:46:38 +0200

    Feature: Add logging for JwtAuthenticationToken attributes in GetUserInfo #deploy-dolly-backend

--- libs/reactive-security/src/main/java/no/nav/testnav/libs/reactivesecurity/action/GetUserInfo.java
+++ libs/reactive-security/src/main/java/no/nav/testnav/libs/reactivesecurity/action/GetUserInfo.java
@@ -26,6 +26,7 @@ public class GetUserInfo extends JwtResolver implements Callable<Mono<UserInfoEx
                     if (authentication instanceof JwtAuthenticationToken jwtAuthenticationToken) {
 
                         var attrib = jwtAuthenticationToken.getTokenAttributes();
+                        log.info("GetUserInfo jwtAuthenticationToken {}", jwtAuthenticationToken.getTokenAttributes());
                         return new UserInfoExtended(
                                 (String) attrib.get("oid"),
                                 "889640782",
