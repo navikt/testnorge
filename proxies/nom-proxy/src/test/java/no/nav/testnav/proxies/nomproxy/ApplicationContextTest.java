fd2d3f31707240d9e7bef2a31b1b6f1ea8ae3cac
Kristen Hærum <krharum@gmail.com>
2025-09-12 13:59:49 +0200

    fix: correct consumer name from 'noma-api' to 'nom-api' in configuration files #deploy-proxy-nom

--- proxies/nom-proxy/src/test/java/no/nav/testnav/proxies/nomproxy/ApplicationContextTest.java
+++ proxies/nom-proxy/src/test/java/no/nav/testnav/proxies/nomproxy/ApplicationContextTest.java
@@ -2,9 +2,7 @@ package no.nav.testnav.proxies.nomproxy;
 
 import no.nav.dolly.libs.test.DollyApplicationContextTest;
 import no.nav.dolly.libs.test.DollySpringBootTest;
-import org.junit.jupiter.api.Disabled;
 
-@Disabled
 @DollySpringBootTest
 public class ApplicationContextTest extends DollyApplicationContextTest {
 }
