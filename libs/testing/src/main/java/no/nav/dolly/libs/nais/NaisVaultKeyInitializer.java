76d81cdf008b0d9401a888eb39b8776a4db2aed7
Cato Olsen <olsen.cato@gmail.com>
2025-09-12 11:50:08 +0200

    Attempting to set spring.cloud.vault.token directly.

--- libs/testing/src/main/java/no/nav/dolly/libs/nais/NaisVaultKeyInitializer.java
+++ libs/testing/src/main/java/no/nav/dolly/libs/nais/NaisVaultKeyInitializer.java
@@ -9,6 +9,7 @@ import java.nio.file.Paths;
 public class NaisVaultKeyInitializer {
 
     private static final String KEY = "SPRING_CLOUD_VAULT_TOKEN";
+    private static final String PROPERTY = "spring.cloud.vault.token";
     private static final String PATH = "/var/run/secrets/nais.io/vault/vault_token";
 
     public static void run() {
@@ -18,7 +19,9 @@ public class NaisVaultKeyInitializer {
             if (Files.exists(path)) {
                 var value = Files.readString(path).trim();
                 System.setProperty(KEY, value);
-                log.info("System property {} set from file {}", KEY, PATH);
+                System.setProperty(PROPERTY, value);
+                var maskedValue = value.charAt(0) + "*".repeat(value.length() - 2) + value.charAt(value.length() - 1);
+                log.info("System property {} set from file {} with value {}", KEY, PATH, maskedValue);
             } else {
                 log.warn("File not found at {}; hopefully you're running locally", PATH);
             }
