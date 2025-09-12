6cb5c1588a14fd0f6f4efd3682b326131cb3cd5b
Cato Olsen <olsen.cato@gmail.com>
2025-09-12 10:20:18 +0200

    Attempts to move Vault token initialization outside ApplicationContextInitializer.

--- apps/brreg-stub/src/main/java/no/nav/brregstub/BrregApplicationLauncher.java
+++ apps/brreg-stub/src/main/java/no/nav/brregstub/BrregApplicationLauncher.java
@@ -1,17 +1,16 @@
 package no.nav.brregstub;
 
 import no.nav.dolly.libs.nais.NaisEnvironmentApplicationContextInitializer;
-import no.nav.dolly.libs.nais.NaisFileIntoSystemPropertyInitializer;
+import no.nav.dolly.libs.nais.NaisVaultKeyInitializer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class BrregApplicationLauncher {
     public static void main(String[] args) {
+        NaisVaultKeyInitializer.run();
         new SpringApplicationBuilder(BrregApplicationLauncher.class)
-                .initializers(
-                        new NaisEnvironmentApplicationContextInitializer(),
-                        new NaisFileIntoSystemPropertyInitializer("SPRING_CLOUD_VAULT_TOKEN", "/var/run/secrets/nais.io/vault/vault_token"))
+                .initializers(new NaisEnvironmentApplicationContextInitializer())
                 .run(args);
     }
 }
--- apps/udi-stub/src/main/java/no/nav/udistub/UdiStubApplicationStarter.java
+++ apps/udi-stub/src/main/java/no/nav/udistub/UdiStubApplicationStarter.java
@@ -1,17 +1,16 @@
 package no.nav.udistub;
 
 import no.nav.dolly.libs.nais.NaisEnvironmentApplicationContextInitializer;
-import no.nav.dolly.libs.nais.NaisFileIntoSystemPropertyInitializer;
+import no.nav.dolly.libs.nais.NaisVaultKeyInitializer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class UdiStubApplicationStarter {
     public static void main(String[] args) {
+        NaisVaultKeyInitializer.run();
         new SpringApplicationBuilder(UdiStubApplicationStarter.class)
-                .initializers(
-                        new NaisEnvironmentApplicationContextInitializer(),
-                        new NaisFileIntoSystemPropertyInitializer("SPRING_CLOUD_VAULT_TOKEN", "/var/run/secrets/nais.io/vault/vault_token"))
+                .initializers(new NaisEnvironmentApplicationContextInitializer())
                 .run(args);
     }
 }
