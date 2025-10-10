a36c6ed889c13b6c2633802f70443e208a91a4c1
Betsy Carina Traran <betsy.carina.traran@nav.no>
2025-10-10 15:54:15 +0200

    Paginering paa orgoversikt

--- apps/dolly-frontend/src/main/js/src/pages/adminPages/Orgtilgang/OrgOversikt.tsx
+++ apps/dolly-frontend/src/main/js/src/pages/adminPages/Orgtilgang/OrgOversikt.tsx
@@ -1,8 +1,7 @@
-import { Alert, Box, Table } from '@navikt/ds-react'
+import { Alert, Box, Pagination, Table, VStack } from '@navikt/ds-react'
 import { ErrorBoundary } from '@/components/ui/appError/ErrorBoundary'
-import { formatDate } from '@/utils/DataFormatter'
 import Loading from '@/components/ui/loading/Loading'
-import React from 'react'
+import React, { useState } from 'react'
 import { DeleteOrganisasjon } from '@/pages/adminPages/Orgtilgang/DeleteOrganisasjon'
 import { OrgtilgangTypes } from '@/pages/adminPages/Orgtilgang/OrgtilgangForm'
 import { RedigerOrganisasjon } from '@/pages/adminPages/Orgtilgang/RedigerOrganisasjon'
@@ -15,6 +14,12 @@ type OversiktTypes = {
 }
 
 export const OrgOversikt = ({ organisasjonTilgang, loading, error, mutate }: OversiktTypes) => {
+	const [page, setPage] = useState(1)
+	const rowsPerPage = 10
+
+	let sortData = organisasjonTilgang
+	sortData = sortData?.slice((page - 1) * rowsPerPage, page * rowsPerPage)
+
 	return (
 		<Box background="surface-default" padding="4">
 			<h2 style={{ marginTop: '5px' }}>Organisasjoner som har tilgang til Dolly</h2>
@@ -28,24 +33,26 @@ export const OrgOversikt = ({ organisasjonTilgang, loading, error, mutate }: Ove
 			)}
 			{organisasjonTilgang?.length > 0 && (
 				<ErrorBoundary>
-					<Table>
-						<Table.Header>
-							<Table.HeaderCell scope="col">Org.nr.</Table.HeaderCell>
-							<Table.HeaderCell scope="col">Navn</Table.HeaderCell>
-							<Table.HeaderCell scope="col">Form</Table.HeaderCell>
-							<Table.HeaderCell scope="col">Miljø</Table.HeaderCell>
-							<Table.HeaderCell scope="col">Endre org.</Table.HeaderCell>
-						</Table.Header>
-						<Table.Body>
-							{organisasjonTilgang?.map(
-								({ organisasjonsnummer, navn, organisasjonsform, miljoe, gyldigTil }, idx) => {
+					<VStack gap="space-16">
+						<Table>
+							<Table.Header>
+								<Table.Row>
+									<Table.HeaderCell scope="col">Org.nr.</Table.HeaderCell>
+									<Table.HeaderCell scope="col">Navn</Table.HeaderCell>
+									<Table.HeaderCell scope="col">Form</Table.HeaderCell>
+									<Table.HeaderCell scope="col">Miljø</Table.HeaderCell>
+									<Table.HeaderCell scope="col">Endre org.</Table.HeaderCell>
+								</Table.Row>
+							</Table.Header>
+							<Table.Body>
+								{sortData?.map(({ organisasjonsnummer, navn, organisasjonsform, miljoe }, idx) => {
 									return (
 										<Table.Row key={organisasjonsnummer + idx}>
 											<Table.HeaderCell scope="row">{organisasjonsnummer}</Table.HeaderCell>
-											<Table.DataCell>{navn}</Table.DataCell>
-											<Table.DataCell>{organisasjonsform}</Table.DataCell>
-											<Table.DataCell>{miljoe}</Table.DataCell>
-											<Table.DataCell>
+											<Table.DataCell width="55%">{navn}</Table.DataCell>
+											<Table.DataCell width="10%">{organisasjonsform}</Table.DataCell>
+											<Table.DataCell width="10%">{miljoe}</Table.DataCell>
+											<Table.DataCell width="12%">
 												<DeleteOrganisasjon
 													orgNr={organisasjonsnummer}
 													navn={navn}
@@ -59,10 +66,16 @@ export const OrgOversikt = ({ organisasjonTilgang, loading, error, mutate }: Ove
 											</Table.DataCell>
 										</Table.Row>
 									)
-								},
-							)}
-						</Table.Body>
-					</Table>
+								})}
+							</Table.Body>
+						</Table>
+						<Pagination
+							page={page}
+							onPageChange={setPage}
+							count={Math.ceil(organisasjonTilgang?.length / rowsPerPage)}
+							size="small"
+						/>
+					</VStack>
 				</ErrorBoundary>
 			)}
 		</Box>
