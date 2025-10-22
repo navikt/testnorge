841f16399fde7c4311537aebf0470d1a6f9f1433
Cato Olsen <olsen.cato@gmail.com>
2025-10-22 16:28:54 +0200

    Replaces usages of testnav-sigrunstub-proxy with testnav-dolly-proxy.

--- apps/dolly-backend/src/main/java/no/nav/dolly/bestilling/sigrunstub/command/SigrunstubSummertSkattgrunnlagDeleteCommand.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/bestilling/sigrunstub/command/SigrunstubSummertSkattgrunnlagDeleteCommand.java
@@ -21,7 +21,7 @@ import static no.nav.dolly.domain.CommonKeysAndUtils.HEADER_NAV_CONSUMER_ID;
 @Slf4j
 public class SigrunstubSummertSkattgrunnlagDeleteCommand implements Callable<Mono<SigrunstubResponse>> {
 
-    private static final String SIGRUNSTUB_DELETE_URL = "/api/v2/summertskattegrunnlag";
+    private static final String SIGRUNSTUB_DELETE_URL = "/sigrunstub/api/v2/summertskattegrunnlag";
 
     private final WebClient webClient;
     private final String ident;
