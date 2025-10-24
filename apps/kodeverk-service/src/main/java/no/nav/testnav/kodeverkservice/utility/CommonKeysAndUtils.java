83b3ebcf95ff001c9a13d9f5245cf1808ecd3ada
Kristen Hærum <kristen.herum@nav.no>
2025-10-24 08:48:17 +0200

    Oppdatert kodeverk med historiske kommuner (#3994)

--- apps/kodeverk-service/src/main/java/no/nav/testnav/kodeverkservice/utility/CommonKeysAndUtils.java
+++ apps/kodeverk-service/src/main/java/no/nav/testnav/kodeverkservice/utility/CommonKeysAndUtils.java
@@ -13,7 +13,7 @@ public final class CommonKeysAndUtils {
 
     public static final String TEMAHISTARK = "TemaHistark";
     public static final String YRKESKLASSIFISERING = "Yrkesklassifisering";
-    public static final String VERGEMAAL_FYLKESEMBETER = "Vergemål_Fylkesmannsembeter";
+    public static final String KOMMUNER_MED_HISTORISKE = "KommunerMedHistoriske";
 
     public static final LocalDate GYLDIG_FRA = LocalDate.of(1900, 1, 1);
     public static final LocalDate GYLDIG_TIL = LocalDate.of(9999, 12, 31);
