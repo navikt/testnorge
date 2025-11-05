310800e4aec7713cb1cee11f00b4ca34ab85efc4
Cato Olsen <olsen.cato@gmail.com>
2025-11-05 10:46:30 +0100

    Removes apparently unused HTTP proxy support that relied on Netty APIs.

--- apps/profil-api/src/main/java/no/nav/registre/testnorge/profil/consumer/AzureAdProfileConsumer.java
+++ apps/profil-api/src/main/java/no/nav/registre/testnorge/profil/consumer/AzureAdProfileConsumer.java
@@ -6,15 +6,11 @@ import no.nav.registre.testnorge.profil.consumer.command.GetProfileImageCommand;
 import no.nav.registre.testnorge.profil.domain.Profil;
 import no.nav.registre.testnorge.profil.service.AzureAdTokenService;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
-import reactor.netty.http.client.HttpClient;
-import reactor.netty.transport.ProxyProvider;
 
-import java.net.URI;
 import java.util.Optional;
 
 @Slf4j
@@ -27,34 +23,21 @@ public class AzureAdProfileConsumer {
     private final String url;
 
     public AzureAdProfileConsumer(
-            @Value("${HTTP_PROXY:#{null}}") String proxyHost,
             @Value("${api.azuread.url}") String url,
             AzureAdTokenService azureAdTokenService,
             WebClient webClient
     ) {
         this.url = url;
         this.azureAdTokenService = azureAdTokenService;
-        var builder = webClient
+        this.webClient = webClient
                 .mutate()
                 .baseUrl(url)
                 .exchangeStrategies(ExchangeStrategies.builder()
                         .codecs(configurer -> configurer
                                 .defaultCodecs()
                                 .maxInMemorySize(16 * 1024 * 1024))
-                        .build());
-        if (proxyHost != null) {
-            log.trace("Setter opp proxy host {} for Azure Ad", proxyHost);
-            var uri = URI.create(proxyHost);
-            builder.clientConnector(new ReactorClientHttpConnector(
-                    HttpClient
-                            .create()
-                            .proxy(proxy -> proxy
-                                    .type(ProxyProvider.Proxy.HTTP)
-                                    .host(uri.getHost())
-                                    .port(uri.getPort()))
-            ));
-        }
-        this.webClient = builder.build();
+                        .build())
+                .build();
     }
 
     public Mono<Profil> getProfil() {
