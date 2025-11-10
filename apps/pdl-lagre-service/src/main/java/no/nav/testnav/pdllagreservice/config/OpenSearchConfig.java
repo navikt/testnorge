5dfa572141c584eb9eaed7c2d0e69272f8447e59
Kristen Hærum <krharum@gmail.com>
2025-11-10 08:17:42 +0100

    Oppdatert konfigurasjon #deploy-pdl-lagre-service

--- apps/pdl-lagre-service/src/main/java/no/nav/testnav/pdllagreservice/config/OpenSearchConfig.java
+++ apps/pdl-lagre-service/src/main/java/no/nav/testnav/pdllagreservice/config/OpenSearchConfig.java
@@ -14,17 +14,13 @@ import org.opensearch.client.transport.httpclient5.ApacheHttpClient5TransportBui
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 import java.net.URISyntaxException;
 
 @Slf4j
 @Configuration
-@Profile("local")
 @RequiredArgsConstructor
-@EnableElasticsearchRepositories("no.nav.dolly.elastic")
-public class OpenSearchLocalConfig {
+public class OpenSearchConfig {
 
     @Bean
     public CredentialsProvider credentialsProvider(@Value("${OPEN_SEARCH_HOST}") String url,
