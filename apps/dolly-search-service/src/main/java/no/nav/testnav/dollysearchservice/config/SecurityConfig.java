cca505465d442e45fe1346c3329d5ec940b01bb1
Cato Olsen <olsen.cato@gmail.com>
2025-11-21 11:11:25 +0100

    Removes unused import.

--- apps/dolly-search-service/src/main/java/no/nav/testnav/dollysearchservice/config/SecurityConfig.java
+++ apps/dolly-search-service/src/main/java/no/nav/testnav/dollysearchservice/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import no.nav.dolly.libs.security.config.DollyServerHttpSecurity;
 import no.nav.testnav.libs.reactivesecurity.manager.JwtReactiveAuthenticationManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
