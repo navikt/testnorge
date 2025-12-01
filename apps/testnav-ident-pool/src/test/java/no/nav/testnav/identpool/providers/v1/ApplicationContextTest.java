91e9aa14a2d474517486d1e715f45a8a620a1c47
Kristen Hærum <krharum@gmail.com>
2025-12-01 12:07:26 +0100

    Fix response timeout configuration in PdlDataConsumer #deploy-test-dolly-backend

---
+++ apps/testnav-ident-pool/src/test/java/no/nav/testnav/identpool/providers/v1/ApplicationContextTest.java
@@ -0,0 +1,17 @@
+package no.nav.testnav.identpool.providers.v1;
+
+import no.nav.dolly.libs.test.DollySpringBootTest;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+@Disabled
+@DollySpringBootTest
+class ApplicationContextTest extends AbstractTestcontainer {
+
+    @Test
+    @SuppressWarnings("java:S2699")
+    void contextLoads() {
+        // This test will pass if the application context loads successfully.
+        // No assertions are needed here, as the test will fail if there are any issues with the context.
+    }
+}
