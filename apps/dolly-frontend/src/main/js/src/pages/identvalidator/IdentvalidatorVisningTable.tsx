23d751c9e7068cb02d8541b26448c85f2d14e597
Betsy Carina Traran <betsy.carina.traran@nav.no>
2025-12-03 11:53:08 +0100

    Identvalidator sortering paa erIProd

--- apps/dolly-frontend/src/main/js/src/pages/identvalidator/IdentvalidatorVisningTable.tsx
+++ apps/dolly-frontend/src/main/js/src/pages/identvalidator/IdentvalidatorVisningTable.tsx
@@ -3,33 +3,69 @@ import { oversettBoolean } from '@/utils/DataFormatter'
 import { IdentvalidatorVisning } from '@/pages/identvalidator/IdentvalidatorVisning'
 import { useState } from 'react'
 
+function comparator<T>(a: T, b: T, orderBy: keyof T): number {
+	if (b[orderBy] == null || b[orderBy] < a[orderBy]) {
+		return -1
+	}
+	if (b[orderBy] > a[orderBy]) {
+		return 1
+	}
+	return 0
+}
+
 export const IdentvalidatorVisningTable = ({ identListe }) => {
 	const [page, setPage] = useState(1)
 	const rowsPerPage = 10
+
+	const [sort, setSort] = useState()
 
 	if (!identListe || identListe.length === 0) {
 		return null
 	}
 
-	let sortData = identListe
-	sortData = sortData.slice((page - 1) * rowsPerPage, page * rowsPerPage)
+	const handleSort = (sortKey) => {
+		setSort(
+			sort && sortKey === sort.orderBy && sort.direction === 'descending'
+				? undefined
+				: {
+						orderBy: sortKey,
+						direction:
+							sort && sortKey === sort.orderBy && sort.direction === 'ascending'
+								? 'descending'
+								: 'ascending',
+					},
+		)
+	}
+
+	let sortedData = identListe.slice().sort((a, b) => {
+		if (sort) {
+			return sort.direction === 'descending'
+				? comparator(b, a, sort.orderBy)
+				: comparator(a, b, sort.orderBy)
+		}
+		return 1
+	})
+
+	sortedData = sortedData.slice((page - 1) * rowsPerPage, page * rowsPerPage)
 
 	return (
 		<VStack gap="space-16">
-			<Table>
+			<Table sort={sort} onSortChange={(sortKey) => handleSort(sortKey)}>
 				<Table.Header>
 					<Table.Row>
 						<Table.HeaderCell />
-						<Table.HeaderCell>Ident</Table.HeaderCell>
-						<Table.HeaderCell>Er gyldig</Table.HeaderCell>
-						<Table.HeaderCell>Er i prod</Table.HeaderCell>
-						<Table.HeaderCell>Er ny ident (2032)</Table.HeaderCell>
-						<Table.HeaderCell>Er syntetisk</Table.HeaderCell>
-						<Table.HeaderCell>Er Testnorge-ident</Table.HeaderCell>
+						<Table.HeaderCell scope="col">Ident</Table.HeaderCell>
+						<Table.HeaderCell scope="col">Er gyldig</Table.HeaderCell>
+						<Table.ColumnHeader sortKey="erIProd" sortable>
+							Er i prod
+						</Table.ColumnHeader>
+						<Table.HeaderCell scope="col">Er ny ident (2032)</Table.HeaderCell>
+						<Table.HeaderCell scope="col">Er syntetisk</Table.HeaderCell>
+						<Table.HeaderCell scope="col">Er Testnorge-ident</Table.HeaderCell>
 					</Table.Row>
 				</Table.Header>
 				<Table.Body>
-					{sortData?.map((identData, index) => {
+					{sortedData?.map((identData, index) => {
 						return (
 							<Table.ExpandableRow
 								key={index + identData.ident}
