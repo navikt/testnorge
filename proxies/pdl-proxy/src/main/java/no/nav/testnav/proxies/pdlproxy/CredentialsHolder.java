4d3b9c8c0c562c7f5eac75bf475aa9fa5fbf34c0
Cato Olsen <olsen.cato@gmail.com>
2025-12-03 15:09:46 +0100

    Removes pdl-proxy.

--- proxies/pdl-proxy/src/main/java/no/nav/testnav/proxies/pdlproxy/CredentialsHolder.java
+++
@@ -1,7 +0,0 @@
-package no.nav.testnav.proxies.pdlproxy;
-
-public record CredentialsHolder(String hendelselagerApiKey,
-                                String aktoerAdminApiKey,
-                                String elasticUsername,
-                                String elasticPassword) {
-}
