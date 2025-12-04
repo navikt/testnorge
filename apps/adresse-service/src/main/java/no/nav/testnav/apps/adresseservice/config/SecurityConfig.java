d0cfa682a11cca24f6723f7d18c8dcabc3d377b1
stigus <stian.gustavsson@nav.no>
2025-12-04 10:29:32 +0100

    - Migrate to reactive security configuration in SecurityConfig.java - Add support for Azure AD resource server properties - Introduce a JWT decoder for non-test profiles #deploy-adresse-service

--- apps/adresse-service/src/main/java/no/nav/testnav/apps/adresseservice/config/SecurityConfig.java
+++ apps/adresse-service/src/main/java/no/nav/testnav/apps/adresseservice/config/SecurityConfig.java
@@ -1,17 +1,23 @@
 package no.nav.testnav.apps.adresseservice.config;
 
+import lombok.RequiredArgsConstructor;
 import no.nav.dolly.libs.security.config.DollyServerHttpSecurity;
+import no.nav.testnav.libs.reactivesecurity.properties.AzureAdResourceServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoders;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import reactor.core.publisher.Mono;
 
 @Configuration
+@RequiredArgsConstructor
 class SecurityConfig {
+
+    private final AzureAdResourceServerProperties azureAdProperties;
 
     @Bean
     SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity httpSecurity) {
@@ -20,6 +26,12 @@ class SecurityConfig {
                 .authorizeExchange(DollyServerHttpSecurity.withDefaultHttpRequests())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
+    }
+
+    @Bean
+    @Profile("!test")
+    ReactiveJwtDecoder jwtDecoder() {
+        return ReactiveJwtDecoders.fromOidcIssuerLocation(azureAdProperties.getIssuerUri());
     }
 
     @Bean
