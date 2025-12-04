fe90d3144a0f5f1335acf1ec98287ad39b0f6e74
Betsy Carina Traran <betsy.carina.traran@nav.no>
2025-12-04 10:44:29 +0100

    Identvalidator div styling-fix

--- apps/dolly-frontend/src/main/js/src/pages/identvalidator/IdentvalidatorVisningTable.tsx
+++ apps/dolly-frontend/src/main/js/src/pages/identvalidator/IdentvalidatorVisningTable.tsx
@@ -1,4 +1,4 @@
-import { Alert, HStack, Pagination, Table, VStack } from '@navikt/ds-react'
+import { Alert, Box, HStack, Pagination, Table, VStack } from '@navikt/ds-react'
 import { oversettBoolean } from '@/utils/DataFormatter'
 import { getIcon, IdentvalidatorVisning } from '@/pages/identvalidator/IdentvalidatorVisning'
 import { useState } from 'react'
@@ -63,64 +63,69 @@ export const IdentvalidatorVisningTable = ({ identListe }) => {
 	sortedData = sortedData.slice((page - 1) * rowsPerPage, page * rowsPerPage)
 
 	return (
-		<VStack gap="space-16">
-			<Table sort={sort} onSortChange={(sortKey) => handleSort(sortKey)}>
-				<Table.Header>
-					<Table.Row>
-						<Table.HeaderCell />
-						<Table.HeaderCell scope="col">Ident</Table.HeaderCell>
-						<Table.ColumnHeader sortKey="erGyldig" sortable>
-							Er gyldig
-						</Table.ColumnHeader>
-						<Table.ColumnHeader sortKey="erIProd" sortable>
-							Er i prod
-						</Table.ColumnHeader>
-						<Table.ColumnHeader sortKey="erSyntetisk" sortable>
-							Er syntetisk
-						</Table.ColumnHeader>
-						<Table.ColumnHeader sortKey="erTestnorgeIdent" sortable>
-							Er Tenor-ident
-						</Table.ColumnHeader>
-						<Table.ColumnHeader sortKey="erPersonnummer2032" sortable>
-							Er ny ident (2032)
-						</Table.ColumnHeader>
-					</Table.Row>
-				</Table.Header>
-				<Table.Body>
-					{sortedData?.map((identData, index) => {
-						return (
-							<Table.ExpandableRow
-								key={index + identData.ident}
-								content={<IdentvalidatorVisning data={identData} />}
-								defaultOpen={identListe.length === 1}
-							>
-								<Table.HeaderCell scope="row">{identData.ident}</Table.HeaderCell>
-								<Table.DataCell>
-									{IconItem(identData.erGyldig, getIcon(identData.erGyldig, true))}
-								</Table.DataCell>
-								<Table.DataCell>
-									{IconItem(identData.erIProd, identData.erIProd ? 'warning' : 'none')}
-								</Table.DataCell>
-								<Table.DataCell>
-									{IconItem(identData.erSyntetisk, getIcon(identData.erSyntetisk))}
-								</Table.DataCell>
-								<Table.DataCell>
-									{IconItem(identData.erTestnorgeIdent, getIcon(identData.erTestnorgeIdent))}
-								</Table.DataCell>
-								<Table.DataCell>
-									{IconItem(identData.erPersonnummer2032, getIcon(identData.erPersonnummer2032))}
-								</Table.DataCell>
-							</Table.ExpandableRow>
-						)
-					})}
-				</Table.Body>
-			</Table>
-			<Pagination
-				page={page}
-				onPageChange={setPage}
-				count={Math.ceil(identListe?.length / rowsPerPage)}
-				size="small"
-			/>
-		</VStack>
+		<Box background={'surface-default'} padding="6">
+			<VStack gap="space-16">
+				<Table sort={sort} onSortChange={(sortKey) => handleSort(sortKey)}>
+					<Table.Header>
+						<Table.Row>
+							<Table.HeaderCell />
+							<Table.HeaderCell scope="col">Ident</Table.HeaderCell>
+							<Table.ColumnHeader sortKey="erGyldig" sortable>
+								Gyldig
+							</Table.ColumnHeader>
+							<Table.ColumnHeader sortKey="erIProd" sortable>
+								I prod
+							</Table.ColumnHeader>
+							<Table.ColumnHeader sortKey="erSyntetisk" sortable>
+								Syntetisk
+							</Table.ColumnHeader>
+							<Table.ColumnHeader sortKey="erTestnorgeIdent" sortable>
+								Tenor-ident
+							</Table.ColumnHeader>
+							<Table.ColumnHeader sortKey="erPersonnummer2032" sortable>
+								Ny ident (2032)
+							</Table.ColumnHeader>
+						</Table.Row>
+					</Table.Header>
+					<Table.Body>
+						{sortedData?.map((identData, index) => {
+							return (
+								<Table.ExpandableRow
+									key={index + identData.ident}
+									content={<IdentvalidatorVisning data={identData} />}
+									defaultOpen={identListe.length === 1}
+									expandOnRowClick
+								>
+									<Table.HeaderCell scope="row" width="18%">
+										{identData.ident}
+									</Table.HeaderCell>
+									<Table.DataCell>
+										{IconItem(identData.erGyldig, getIcon(identData.erGyldig, true))}
+									</Table.DataCell>
+									<Table.DataCell>
+										{IconItem(identData.erIProd, identData.erIProd ? 'warning' : 'none')}
+									</Table.DataCell>
+									<Table.DataCell>
+										{IconItem(identData.erSyntetisk, getIcon(identData.erSyntetisk))}
+									</Table.DataCell>
+									<Table.DataCell>
+										{IconItem(identData.erTestnorgeIdent, getIcon(identData.erTestnorgeIdent))}
+									</Table.DataCell>
+									<Table.DataCell>
+										{IconItem(identData.erPersonnummer2032, getIcon(identData.erPersonnummer2032))}
+									</Table.DataCell>
+								</Table.ExpandableRow>
+							)
+						})}
+					</Table.Body>
+				</Table>
+				<Pagination
+					page={page}
+					onPageChange={setPage}
+					count={Math.ceil(identListe?.length / rowsPerPage)}
+					size="small"
+				/>
+			</VStack>
+		</Box>
 	)
 }
