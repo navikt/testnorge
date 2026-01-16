86282b755bce0469b64d226c637b56bd47a9afd1
stigus <stian.gustavsson@nav.no>
2026-01-16 14:39:15 +0100

    - Deploy vergemaal changes #deploy-test-dolly-backend #deploy-test-pdl-forvalter

--- libs/data-transfer-objects/src/main/java/no/nav/testnav/libs/dto/pdlforvalter/v1/TjenesteomraadeDTO.java
+++ libs/data-transfer-objects/src/main/java/no/nav/testnav/libs/dto/pdlforvalter/v1/TjenesteomraadeDTO.java
@@ -1,9 +1,13 @@
 package no.nav.testnav.libs.dto.pdlforvalter.v1;
 
+import com.fasterxml.jackson.annotation.JsonSetter;
+import com.fasterxml.jackson.annotation.Nulls;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import no.nav.testnav.libs.dto.pdlforvalter.v1.deserializer.StringOrListDeserializer;
 
 import java.util.List;
 
@@ -13,6 +17,8 @@ import java.util.List;
 @AllArgsConstructor
 public class TjenesteomraadeDTO {
 
+    @JsonDeserialize(using = StringOrListDeserializer.class)
+    @JsonSetter(nulls = Nulls.AS_EMPTY)
     private List<String> tjenesteoppgave;
     private String tjenestevirksomhet;
 }
