e5a53b934664f68f8c4ad12ba6e987cec5a7ab86
Cato Olsen <olsen.cato@gmail.com>
2026-01-19 10:21:49 +0100

    Revert "Removes pdl-proxy."
    This reverts commit 4d3b9c8c0c562c7f5eac75bf475aa9fa5fbf34c0.

---
+++ proxies/pdl-proxy/src/test/java/no/nav/testnav/proxies/pdlproxy/ApplicationContextTest.java
@@ -0,0 +1,8 @@
+package no.nav.testnav.proxies.pdlproxy;
+
+import no.nav.dolly.libs.test.DollySpringBootTest;
+import no.nav.dolly.libs.test.DollyApplicationContextTest;
+
+@DollySpringBootTest
+class ApplicationContextTest extends DollyApplicationContextTest {
+}
