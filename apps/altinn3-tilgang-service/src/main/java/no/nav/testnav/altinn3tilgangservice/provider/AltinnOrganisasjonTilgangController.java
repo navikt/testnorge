2f17c6349b74a4e892322c44c232aa5769a62acd
Kristen Hærum <krharum@gmail.com>
2026-01-19 13:07:27 +0100

    Legg til metode for å hente Maskinporten-token for Altinn-tilgang #deploy-altinn3-tilgang-service

--- apps/altinn3-tilgang-service/src/main/java/no/nav/testnav/altinn3tilgangservice/provider/AltinnOrganisasjonTilgangController.java
+++ apps/altinn3-tilgang-service/src/main/java/no/nav/testnav/altinn3tilgangservice/provider/AltinnOrganisasjonTilgangController.java
@@ -3,6 +3,7 @@ package no.nav.testnav.altinn3tilgangservice.provider;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import no.nav.testnav.altinn3tilgangservice.consumer.maskinporten.MaskinportenConsumer;
 import no.nav.testnav.altinn3tilgangservice.domain.OrganisasjonResponse;
 import no.nav.testnav.altinn3tilgangservice.service.AltinnOrganisasjonTilgangService;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -24,12 +25,13 @@ import java.util.Comparator;
 public class AltinnOrganisasjonTilgangController {
 
     private final AltinnOrganisasjonTilgangService altinnTilgangService;
+    private final MaskinportenConsumer maskinportenConsumer;
 
     @GetMapping
     @Operation(description = "Henter alle organisasjoner med Altinn-tilgang")
     public Flux<OrganisasjonResponse> getAll() {
 
-        return altinnTilgangService.getAll()
+        return  altinnTilgangService.getAll()
                 .sort(Comparator.comparing(OrganisasjonResponse::getNavn));
     }
 
@@ -47,5 +49,12 @@ public class AltinnOrganisasjonTilgangController {
     public Flux<OrganisasjonResponse> delete(@PathVariable String organisasjonsnummer) {
 
         return altinnTilgangService.delete(organisasjonsnummer);
+    }
+
+    @GetMapping("/token")
+    @Operation(description = "Henter Maskinporten-token for Altinn-tilgang")
+    public Mono<String> getToken() {
+
+        return maskinportenConsumer.getAccessToken();
     }
 }
