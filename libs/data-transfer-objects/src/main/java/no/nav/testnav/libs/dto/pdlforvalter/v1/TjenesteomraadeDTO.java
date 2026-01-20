e4ba37a6fa03842dfce9da687896d0a0da495185
stigus <stian.gustavsson@nav.no>
2026-01-20 13:14:11 +0100

    - Remove unused StringOrListDeserializer from TjenesteomraadeDTO - Clean up imports in TjenesteomraadeDTO

--- libs/data-transfer-objects/src/main/java/no/nav/testnav/libs/dto/pdlforvalter/v1/TjenesteomraadeDTO.java
+++ libs/data-transfer-objects/src/main/java/no/nav/testnav/libs/dto/pdlforvalter/v1/TjenesteomraadeDTO.java
@@ -2,12 +2,10 @@ package no.nav.testnav.libs.dto.pdlforvalter.v1;
 
 import com.fasterxml.jackson.annotation.JsonSetter;
 import com.fasterxml.jackson.annotation.Nulls;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import no.nav.testnav.libs.dto.pdlforvalter.v1.deserializer.StringOrListDeserializer;
 
 import java.io.Serializable;
 import java.util.List;
@@ -18,7 +16,6 @@ import java.util.List;
 @AllArgsConstructor
 public class TjenesteomraadeDTO implements Serializable {
 
-    @JsonDeserialize(using = StringOrListDeserializer.class)
     @JsonSetter(nulls = Nulls.AS_EMPTY)
     private List<String> tjenesteoppgave;
     private String tjenestevirksomhet;
