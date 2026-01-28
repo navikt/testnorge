189186747db4acffff814638ea6265d47a9317c3
Betsy Carina Traran <betsy.carina.traran@nav.no>
2026-01-28 11:19:26 +0100

    Noe forenkling av bestillingsvisning

--- apps/dolly-frontend/src/main/js/src/components/bestilling/sammendrag/BestillingSammendrag.tsx
+++ apps/dolly-frontend/src/main/js/src/components/bestilling/sammendrag/BestillingSammendrag.tsx
@@ -1,28 +1,34 @@
 import MiljoeStatus from './miljoeStatus/MiljoeStatus'
 import React, { Suspense } from 'react'
 import Loading from '@/components/ui/loading/Loading'
-import { BestillingKriterier } from '@/components/bestilling/sammendrag/BestillingKriterier'
 import * as _ from 'lodash-es'
-import { BestillingKriterierOrganisasjon } from '@/components/bestilling/sammendrag/BestillingKriterierOrganisasjon'
+import SubOverskrift from '@/components/ui/subOverskrift/SubOverskrift'
+import { Bestillingsinformasjon } from '@/components/bestilling/sammendrag/visning/Bestillingsinformasjon'
+import { BestillingsdataOrganisasjon } from '@/components/bestilling/sammendrag/BestillingsdataOrganisasjon'
+import { Bestillingsdata } from '@/components/bestilling/sammendrag/Bestillingsdata'
 
-export default function BestillingSammendrag({
+export const BestillingSammendrag = ({
 	bestilling,
 	closeModal,
 }: {
 	bestilling: any
 	closeModal?: () => void
-}) {
+}) => {
 	const erOrganisasjon = _.has(bestilling, 'organisasjonNummer')
 
 	return (
 		<>
 			<MiljoeStatus bestilling={bestilling} closeModal={closeModal} />
 			<Suspense fallback={<Loading label={'Laster bestillingskriterier ...'} />}>
-				{erOrganisasjon ? (
-					<BestillingKriterierOrganisasjon bestilling={bestilling} />
-				) : (
-					<BestillingKriterier bestilling={bestilling} />
-				)}
+				<div className="bestilling-data">
+					<SubOverskrift label="Bestillingskriterier" />
+					<Bestillingsinformasjon bestillingsinfo={bestilling} />
+					{erOrganisasjon ? (
+						<BestillingsdataOrganisasjon bestilling={bestilling.bestilling} />
+					) : (
+						<Bestillingsdata bestilling={bestilling.bestilling} erGruppevisning />
+					)}
+				</div>
 			</Suspense>
 		</>
 	)
