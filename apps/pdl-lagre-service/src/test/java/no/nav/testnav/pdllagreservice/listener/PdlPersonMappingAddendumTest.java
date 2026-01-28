9b88910449da48af374c41de1c4df8dde7f25536
Kristen Hærum <kristen.herum@nav.no>
2026-01-28 14:48:34 +0100

    Feature/angi historikk og dypde for Dolly soek (#4110)

---
+++ apps/pdl-lagre-service/src/test/java/no/nav/testnav/pdllagreservice/listener/PdlPersonMappingAddendumTest.java
@@ -0,0 +1,58 @@
+package no.nav.testnav.pdllagreservice.listener;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.val;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static java.util.Objects.requireNonNull;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasEntry;
+import static org.hamcrest.Matchers.is;
+
+@ExtendWith(MockitoExtension.class)
+class PdlPersonMappingAddendumTest {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    void shouldAppendSizeAttribute_OK() throws Exception {
+
+        val dokument = objectMapper.readValue(getJsonPerson(), new TypeReference<HashMap<String, Object>>() {
+        });
+
+        val result = PdlPersonMappingAddendum.appendSizeAttribute(dokument);
+        val persondataljer = (HashMap<String, Object>) result.get("hentPerson");
+
+        assertThat(((List<Map<String, Object>>) persondataljer.get("bostedsadresse")).getFirst(),
+                hasEntry(is("size"), is(3)));
+        assertThat(((List<Map<String, Object>>) persondataljer.get("sivilstand")).getFirst(),
+                hasEntry(is("size"), is(1)));
+        assertThat(((List<Map<String, Object>>) persondataljer.get("statsborgerskap")).getFirst(),
+                hasEntry(is("size"), is(1)));
+        assertThat(((List<Map<String, Object>>) persondataljer.get("navn")).getFirst(),
+                hasEntry(is("size"), is(1)));
+        assertThat(((List<Map<String, Object>>) persondataljer.get("kjoenn")).getFirst(),
+                hasEntry(is("size"), is(1)));
+        assertThat(((List<Map<String, Object>>) persondataljer.get("foedested")).getFirst(),
+                hasEntry(is("size"), is(1)));
+        assertThat(((List<Map<String, Object>>) persondataljer.get("foedselsdato")).getFirst(),
+                hasEntry(is("size"), is(1)));
+        assertThat(((List<Map<String, Object>>) persondataljer.get("folkeregisteridentifikator")).getFirst(),
+                hasEntry(is("size"), is(1)));
+        assertThat(((List<Map<String, Object>>) persondataljer.get("folkeregisterpersonstatus")).getFirst(),
+                hasEntry(is("size"), is(1)));
+    }
+
+    private File getJsonPerson() {
+
+        return new File(requireNonNull(getClass().getResource("/testdata/person.json")).getFile());
+    }
+}
