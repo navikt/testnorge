64cf9545ac04bd81d45f31b837b45c7dd9253fc9
Kristen Hærum <krharum@gmail.com>
2026-01-28 16:01:06 +0100

    Implement address protection and death status checks in CV creation process

--- apps/dolly-backend/src/main/java/no/nav/dolly/bestilling/arbeidsplassencv/ArbeidsplassenCVClient.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/bestilling/arbeidsplassencv/ArbeidsplassenCVClient.java
@@ -5,6 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import ma.glasnost.orika.MapperFacade;
 import no.nav.dolly.bestilling.ClientRegister;
 import no.nav.dolly.bestilling.arbeidsplassencv.dto.PAMCVDTO;
+import no.nav.dolly.bestilling.personservice.PersonServiceConsumer;
+import no.nav.dolly.domain.PdlPersonBolk;
 import no.nav.dolly.domain.jpa.BestillingProgress;
 import no.nav.dolly.domain.resultset.RsDollyUtvidetBestilling;
 import no.nav.dolly.domain.resultset.dolly.DollyPerson;
@@ -12,8 +14,10 @@ import no.nav.dolly.errorhandling.ErrorStatusDecoder;
 import no.nav.dolly.service.TransactionHelperService;
 import no.nav.dolly.util.CallIdUtil;
 import no.nav.testnav.libs.dto.arbeidsplassencv.v1.ArbeidsplassenCVDTO;
+import no.nav.testnav.libs.dto.pdlforvalter.v1.AdressebeskyttelseDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.util.retry.Retry;
 
@@ -21,7 +25,9 @@ import java.util.List;
 import java.util.function.Consumer;
 
 import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
 import static no.nav.dolly.errorhandling.ErrorStatusDecoder.getInfoVenter;
+import static org.apache.commons.lang3.BooleanUtils.isTrue;
 
 @Slf4j
 @Service
@@ -32,6 +38,7 @@ public class ArbeidsplassenCVClient implements ClientRegister {
     private final MapperFacade mapperFacade;
     private final TransactionHelperService transactionHelperService;
     private final ErrorStatusDecoder errorStatusDecoder;
+    private final PersonServiceConsumer personServiceConsumer;
 
     @Override
     public Mono<BestillingProgress> gjenopprett(RsDollyUtvidetBestilling bestilling, DollyPerson dollyPerson, BestillingProgress progress, boolean isOpprettEndre) {
@@ -40,27 +47,49 @@ public class ArbeidsplassenCVClient implements ClientRegister {
             return Mono.empty();
         }
 
-        return oppdaterStatus(progress, getInfoVenter("Arbeidsplassen"))
-                .then(Mono.just(bestilling.getArbeidsplassenCV()))
-                .flatMap(arbeidsplassenCV -> {
-                    var oppdatertOrdre = mapperFacade.map(arbeidsplassenCV, ArbeidsplassenCVDTO.class);
-                    bestilling.setArbeidsplassenCV(oppdatertOrdre);
-                    return transactionHelperService.persister(progress.getBestillingId(), bestilling)
-                            .then(Mono.just(arbeidsplassenCV));
-                })
-                .flatMap(oppdatertOrdre -> Mono.just(CallIdUtil.generateCallId())
-                        .flatMap(uuid -> arbeidsplassenCVConsumer.opprettPerson(dollyPerson.getIdent(), uuid)
-                                .flatMap(response ->
-                                        arbeidsplassenCVConsumer.godtaHjemmel(dollyPerson.getIdent(), uuid)
-                                                .then(Mono.just(mapperFacade.map(oppdatertOrdre, PAMCVDTO.class))
-                                                        .flatMap(request -> arbeidsplassenCVConsumer.oppdaterCV(dollyPerson.getIdent(),
-                                                                request, uuid, logRetries(progress)))))
+        return personServiceConsumer.getPdlPersoner(List.of(dollyPerson.getIdent()))
+                .filter(pdlPersonBolk -> nonNull(pdlPersonBolk.getData()))
+                .map(PdlPersonBolk::getData)
+                .map(PdlPersonBolk.Data::getHentPersonBolk)
+                .flatMap(Flux::fromIterable)
+                .filter(personBolk -> nonNull(personBolk.getPerson()))
+                .map(PdlPersonBolk.PersonBolk::getPerson)
+                .flatMap(person -> Mono.just(person.getAdressebeskyttelse().stream()
+                                .anyMatch(ab -> ab.getGradering() ==
+                                        AdressebeskyttelseDTO.AdresseBeskyttelse.STRENGT_FORTROLIG))
+                        .zipWith(Mono.just(person.getDoedsfall().stream()
+                                .anyMatch(doed -> nonNull(doed.getDoedsdato())))))
+                .collectList()
+                .flatMap(harAdressebeskyttelseEllerDoed -> {
+                    if (isTrue(harAdressebeskyttelseEllerDoed.getFirst().getT1())) {
+                        return oppdaterStatus(progress, "Feil: CV kan ikke opprettes - person har adressebeskyttelse STRENGT FORTROLIG");
+                    } else if (isTrue(harAdressebeskyttelseEllerDoed.getFirst().getT2())) {
+                        return oppdaterStatus(progress, "Feil: CV kan ikke opprettes - person er død");
+                    } else {
 
-                                .map(status -> status.getStatus().is2xxSuccessful() ? "OK" :
-                                        errorStatusDecoder.getErrorText(HttpStatus.valueOf(status.getStatus().value()),
-                                                status.getFeilmelding()))
+                        return oppdaterStatus(progress, getInfoVenter("Arbeidsplassen"))
+                                .then(Mono.just(bestilling.getArbeidsplassenCV()))
+                                .flatMap(arbeidsplassenCV -> {
+                                    var oppdatertOrdre = mapperFacade.map(arbeidsplassenCV, ArbeidsplassenCVDTO.class);
+                                    bestilling.setArbeidsplassenCV(oppdatertOrdre);
+                                    return transactionHelperService.persister(progress.getBestillingId(), bestilling)
+                                            .then(Mono.just(arbeidsplassenCV));
+                                })
+                                .flatMap(oppdatertOrdre -> Mono.just(CallIdUtil.generateCallId())
+                                        .flatMap(uuid -> arbeidsplassenCVConsumer.opprettPerson(dollyPerson.getIdent(), uuid)
+                                                .flatMap(response ->
+                                                        arbeidsplassenCVConsumer.godtaHjemmel(dollyPerson.getIdent(), uuid)
+                                                                .then(Mono.just(mapperFacade.map(oppdatertOrdre, PAMCVDTO.class))
+                                                                        .flatMap(request -> arbeidsplassenCVConsumer.oppdaterCV(dollyPerson.getIdent(),
+                                                                                request, uuid, logRetries(progress)))))
 
-                                .flatMap(resultat -> oppdaterStatus(progress, resultat))));
+                                                .map(status -> status.getStatus().is2xxSuccessful() ? "OK" :
+                                                        errorStatusDecoder.getErrorText(HttpStatus.valueOf(status.getStatus().value()),
+                                                                status.getFeilmelding()))
+
+                                                .flatMap(resultat -> oppdaterStatus(progress, resultat))));
+                    }
+                });
     }
 
     private Consumer<Retry.RetrySignal> logRetries(BestillingProgress progress) {
