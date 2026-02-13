9fee01cd191730859b0a7262351c7e9ce9379cb5
stigus <stian.gustavsson@nav.no>
2026-02-13 17:08:01 +0100

    - Fix Org-faste-data og org-forvalter hibernate issue #deploy-test-dolly-backend #deploy-organisasjon-bestilling-service #deploy-organisasjon-faste-data-service #deploy-organisasjon-forvalter #deploy-organisasjon-mottak-service #deploy-organisasjon-service

--- apps/organisasjon-forvalter/src/main/java/no/nav/organisasjonforvalter/jpa/repository/OrganisasjonRepository.java
+++ apps/organisasjon-forvalter/src/main/java/no/nav/organisasjonforvalter/jpa/repository/OrganisasjonRepository.java
@@ -1,6 +1,7 @@
 package no.nav.organisasjonforvalter.jpa.repository;
 
 import no.nav.organisasjonforvalter.jpa.entity.Organisasjon;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -10,13 +11,16 @@ import java.util.Optional;
 
 public interface OrganisasjonRepository extends CrudRepository<Organisasjon, Long> {
 
+    @EntityGraph(attributePaths = {"adresser", "underenheter"})
     List<Organisasjon> findAllByOrganisasjonsnummerIn(List<String> orgnumre);
 
+    @EntityGraph(attributePaths = {"adresser", "underenheter"})
     @Query(value = "select o from Organisasjon as o " +
             "where o.brukerId = :brukerId " +
             "and o.enhetstype in ('BEDR', 'AAFY')")
     List<Organisasjon> findDriftsenheterByBrukerId(@Param(value = "brukerId") String brukerid);
 
+    @EntityGraph(attributePaths = {"adresser", "underenheter"})
     @Query(value = "select o from Organisasjon as o " +
             "where o.brukerId = :brukerId ")
     List<Organisasjon> findOrganisasjonerByBrukerId(@Param(value = "brukerId") String brukerid);
