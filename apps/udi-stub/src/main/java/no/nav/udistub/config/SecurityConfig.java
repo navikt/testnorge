331ea45893bdc5bb2d939cc620c232d04d262023
stigus <stian.gustavsson@nav.no>
2026-02-18 08:49:57 +0100

    - Migrate from servlet-based to reactive architecture by removing servlet-core dependencies - Update configuration files to reflect the removal of servlet-related imports - Implement JWT decoder for OAuth2 resource server #deploy-test-dolly-backend #deploy-udi-stub

--- apps/udi-stub/src/main/java/no/nav/udistub/config/SecurityConfig.java
+++ apps/udi-stub/src/main/java/no/nav/udistub/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package no.nav.udistub.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -8,8 +10,20 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2Error;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidatorResult;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.jwt.JwtValidators;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.util.List;
+
+@Slf4j
 @EnableWebSecurity
 @Configuration
 @Profile({ "prod", "local" })
@@ -26,5 +40,26 @@ public class SecurityConfig {
                 .oauth2ResourceServer(oauth2RSConfig -> oauth2RSConfig.jwt(Customizer.withDefaults()));
 
         return httpSecurity.build();
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder(
+            @Value("${spring.security.oauth2.resourceserver.aad.issuer-uri}") String issuerUri,
+            @Value("${spring.security.oauth2.resourceserver.aad.accepted-audience}") List<String> acceptedAudience
+    ) {
+        NimbusJwtDecoder jwtDecoder = JwtDecoders.fromIssuerLocation(issuerUri);
+
+        OAuth2TokenValidator<Jwt> issuerValidator = JwtValidators.createDefaultWithIssuer(issuerUri);
+        OAuth2TokenValidator<Jwt> audienceValidator = token ->
+                token.getAudience().stream().anyMatch(acceptedAudience::contains)
+                        ? OAuth2TokenValidatorResult.success()
+                        : OAuth2TokenValidatorResult.failure(new OAuth2Error(
+                        "invalid_token",
+                        "Fant ikke påkrevd audience %s i tokenet.".formatted(acceptedAudience),
+                        null
+                ));
+
+        jwtDecoder.setJwtValidator(new DelegatingOAuth2TokenValidator<>(issuerValidator, audienceValidator));
+        return jwtDecoder;
     }
 }
