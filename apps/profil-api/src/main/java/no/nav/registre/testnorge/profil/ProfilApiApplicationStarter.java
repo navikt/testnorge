fae75cd7f37db61b84f2957e909a791ec5934f44
stigus <stian.gustavsson@nav.no>
2026-02-20 13:52:00 +0100

    - Add ReactiveRequestContext to application context in ProfilApiApplicationStarter - Update AutoConfiguration imports to include ReactiveRequestContext

--- apps/profil-api/src/main/java/no/nav/registre/testnorge/profil/ProfilApiApplicationStarter.java
+++ apps/profil-api/src/main/java/no/nav/registre/testnorge/profil/ProfilApiApplicationStarter.java
@@ -1,6 +1,7 @@
 package no.nav.registre.testnorge.profil;
 
 import no.nav.dolly.libs.nais.NaisEnvironmentApplicationContextInitializer;
+import no.nav.dolly.libs.security.config.ReactiveRequestContext;
 import no.nav.testnav.libs.reactivecore.config.CoreConfig;
 import no.nav.testnav.libs.reactivesecurity.config.SecureOAuth2ServerToServerConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,7 +11,8 @@ import org.springframework.context.annotation.Import;
 @SpringBootApplication
 @Import({
         CoreConfig.class,
-        SecureOAuth2ServerToServerConfiguration.class
+        SecureOAuth2ServerToServerConfiguration.class,
+        ReactiveRequestContext.class
 })
 public class ProfilApiApplicationStarter {
     public static void main(String[] args) {
