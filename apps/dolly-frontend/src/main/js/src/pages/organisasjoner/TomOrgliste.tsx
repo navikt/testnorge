171a99de741931f179a3fa0c3dec6a63764105cc
Betsy Carina Traran <betsy.carina.traran@nav.no>
2026-02-25 16:30:22 +0100

    Fiks oppretting av standard organisasjon

--- apps/dolly-frontend/src/main/js/src/pages/organisasjoner/TomOrgliste.tsx
+++ apps/dolly-frontend/src/main/js/src/pages/organisasjoner/TomOrgliste.tsx
@@ -3,16 +3,20 @@ import NavButton from '@/components/ui/button/NavButton/NavButton'
 
 type Props = {
 	startBestilling: (bestillingstype: string) => void
+	values: any
 	bestillingstype: string
 }
 
-export default ({ startBestilling, bestillingstype }: Props) => (
+export default ({ startBestilling, values, bestillingstype }: Props) => (
 	<ContentContainer>
 		<p>
 			Du har for øyeblikket ingen organisasjoner. Trykk på knappen under for å opprette en
 			organisasjon med standard oppsett.
 		</p>
-		<NavButton onClick={() => startBestilling(bestillingstype)} style={{ marginTop: '10px' }}>
+		<NavButton
+			onClick={() => startBestilling(values, bestillingstype)}
+			style={{ marginTop: '10px' }}
+		>
 			Opprett standard organisasjon
 		</NavButton>
 	</ContentContainer>
