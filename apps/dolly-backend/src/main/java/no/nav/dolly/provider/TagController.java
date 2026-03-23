455b6444db2fccf82aca1aa7001b5010ccc10e90
Kristen Hærum <krharum@gmail.com>
2026-03-23 08:57:03 +0100

    Oppdatert TagsService for sletting av ubrukte tags #deploy-dolly-backend

--- apps/dolly-backend/src/main/java/no/nav/dolly/provider/TagController.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/provider/TagController.java
@@ -4,24 +4,12 @@ import com.fasterxml.jackson.databind.JsonNode;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import lombok.val;
-import no.nav.dolly.bestilling.personservice.PersonServiceConsumer;
 import no.nav.dolly.bestilling.tagshendelseslager.TagsHendelseslagerConsumer;
 import no.nav.dolly.bestilling.tagshendelseslager.dto.TagsOpprettingResponse;
-import no.nav.dolly.domain.PdlPerson;
-import no.nav.dolly.domain.PdlPersonBolk;
-import no.nav.dolly.domain.jpa.Testgruppe;
-import no.nav.dolly.domain.jpa.Testident;
 import no.nav.dolly.domain.resultset.Tags;
 import no.nav.dolly.domain.resultset.Tags.TagBeskrivelse;
-import no.nav.dolly.exceptions.NotFoundException;
-import no.nav.dolly.repository.IdentRepository;
-import no.nav.dolly.repository.TestgruppeRepository;
-import no.nav.testnav.libs.dto.pdlforvalter.v1.ForeldreansvarDTO;
-import no.nav.testnav.libs.dto.pdlforvalter.v1.FullmaktDTO;
-import no.nav.testnav.libs.dto.pdlforvalter.v1.KontaktinformasjonForDoedsboDTO;
+import no.nav.dolly.service.TagsService;
 import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -34,11 +22,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
-import java.util.Objects;
-import java.util.concurrent.atomic.AtomicReference;
-import java.util.stream.Collectors;
 
-import static java.util.Objects.nonNull;
 import static no.nav.dolly.config.CachingConfig.CACHE_GRUPPE;
 
 @Slf4j
@@ -48,10 +32,8 @@ import static no.nav.dolly.config.CachingConfig.CACHE_GRUPPE;
 @RequestMapping(value = "/api/v1/tags", produces = MediaType.APPLICATION_JSON_VALUE)
 public class TagController {
 
-    private final TestgruppeRepository testgruppeRepository;
     private final TagsHendelseslagerConsumer tagsHendelseslagerConsumer;
-    private final PersonServiceConsumer personServiceConsumer;
-    private final IdentRepository identRepository;
+    private final TagsService tagsService;
 
     @GetMapping()
     @Transactional
@@ -78,52 +60,6 @@ public class TagController {
     public Mono<TagsOpprettingResponse> sendTagsPaaGruppe(@RequestBody List<String> tags,
                                                           @PathVariable("gruppeId") Long gruppeId) {
 
-        val oppdatertGruppe = new AtomicReference<Testgruppe>();
-
-        return testgruppeRepository.findById(gruppeId)
-                .switchIfEmpty(Mono.error(new NotFoundException(String.format("Fant ikke gruppe på id: %s", gruppeId))))
-                .flatMapMany(gruppe -> Flux.fromArray(Tags.values())
-                        .filter(tag -> !Objects.equals(Tags.DOLLY, tag))
-                        .map(Tags::name)
-                        .filter(tags::contains)
-                        .collect(Collectors.joining(","))
-                        .doOnNext(gruppe::setTags)
-                        .thenReturn(gruppe))
-                .flatMap(testgruppeRepository::save)
-                .doOnNext(oppdatertGruppe::set)
-                .flatMap(gruppe -> identRepository.findByGruppeId(gruppeId, Pageable.unpaged())
-                        .map(Testident::getIdent)
-                        .collectList()
-                        .flatMapMany(identer -> personServiceConsumer.getPdlPersoner(identer)
-                                .filter(pdlBolk -> nonNull(pdlBolk) && nonNull(pdlBolk.getData()))
-                                .map(PdlPersonBolk::getData)
-                                .map(PdlPersonBolk.Data::getHentPersonBolk)
-                                .flatMap(Flux::fromIterable)
-                                .filter(personBolk -> nonNull(personBolk.getPerson()))
-                                .flatMap(person -> Flux.merge(Mono.just(person.getIdent()),
-                                                Flux.fromIterable(person.getPerson().getSivilstand())
-                                                        .filter(sivilstand -> nonNull(sivilstand.getRelatertVedSivilstand()))
-                                                        .map(PdlPerson.Sivilstand::getRelatertVedSivilstand),
-                                                Flux.fromIterable(person.getPerson().getForelderBarnRelasjon())
-                                                        .filter(relasjon -> nonNull(relasjon.getRelatertPersonsIdent()))
-                                                        .map(PdlPerson.ForelderBarnRelasjon::getRelatertPersonsIdent),
-                                                Flux.fromIterable(person.getPerson().getForeldreansvar())
-                                                        .filter(foreldreansvar -> nonNull(foreldreansvar.getAnsvarlig()))
-                                                        .map(ForeldreansvarDTO::getAnsvarlig),
-                                                Flux.fromIterable(person.getPerson().getFullmakt())
-                                                        .filter(fullmakt -> nonNull(fullmakt.getMotpartsPersonident()))
-                                                        .map(FullmaktDTO::getMotpartsPersonident),
-                                                Flux.fromIterable(person.getPerson().getVergemaalEllerFremtidsfullmakt())
-                                                        .map(PdlPerson.Vergemaal::getVergeEllerFullmektig)
-                                                        .filter(verge -> nonNull(verge.getMotpartsPersonident()))
-                                                        .map(PdlPerson.VergeEllerFullmektig::getMotpartsPersonident),
-                                                Flux.fromIterable(person.getPerson().getKontaktinformasjonForDoedsbo())
-                                                        .filter(kontakt -> nonNull(kontakt.getPersonSomKontakt()))
-                                                        .map(KontaktinformasjonForDoedsboDTO::getPersonSomKontakt)
-                                                        .filter(kontakt -> nonNull(kontakt.getIdentifikasjonsnummer()))
-                                                        .map(KontaktinformasjonForDoedsboDTO.KontaktpersonDTO::getIdentifikasjonsnummer))
-                                        .distinct())))
-                .collectList()
-                .flatMap(bolkPersoner -> tagsHendelseslagerConsumer.createTags(bolkPersoner, oppdatertGruppe.get().getTags()));
+        return tagsService.sendTags(gruppeId, tags);
     }
 }
