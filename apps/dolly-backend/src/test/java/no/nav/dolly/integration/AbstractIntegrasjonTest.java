97f6c91c65c625a02f9bc59d12b95af7eda54888
Cato Olsen <cato.olsen@nav.no>
2026-03-23 13:00:39 +0100

    bugfix/dolly_backend_tests (#4168)
    Fixes TeamIntegrationTest DB cleanup.

--- apps/dolly-backend/src/test/java/no/nav/dolly/integration/AbstractIntegrasjonTest.java
+++ apps/dolly-backend/src/test/java/no/nav/dolly/integration/AbstractIntegrasjonTest.java
@@ -5,9 +5,16 @@ import no.nav.dolly.domain.jpa.Bruker;
 import no.nav.dolly.domain.jpa.Team;
 import no.nav.dolly.domain.jpa.TeamBruker;
 import no.nav.dolly.libs.test.DollySpringBootTest;
+import no.nav.dolly.repository.BestillingKontrollRepository;
+import no.nav.dolly.repository.BestillingProgressRepository;
+import no.nav.dolly.repository.BestillingRepository;
+import no.nav.dolly.repository.BrukerFavoritterRepository;
 import no.nav.dolly.repository.BrukerRepository;
+import no.nav.dolly.repository.IdentRepository;
 import no.nav.dolly.repository.TeamBrukerRepository;
 import no.nav.dolly.repository.TeamRepository;
+import no.nav.dolly.repository.TestgruppeRepository;
+import no.nav.dolly.repository.TransaksjonMappingRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,11 +39,39 @@ public abstract class AbstractIntegrasjonTest {
     @Autowired
     private TeamBrukerRepository teamBrukerRepository;
 
+    @Autowired
+    private IdentRepository identRepository;
+
+    @Autowired
+    private BrukerFavoritterRepository brukerFavoritterRepository;
+
+    @Autowired
+    private TestgruppeRepository testgruppeRepository;
+
+    @Autowired
+    private TransaksjonMappingRepository transaksjonMappingRepository;
+
+    @Autowired
+    private BestillingProgressRepository bestillingProgressRepository;
+
+    @Autowired
+    private BestillingKontrollRepository bestillingKontrollRepository;
+
+    @Autowired
+    private BestillingRepository bestillingRepository;
+
     @BeforeEach
     void cleanup() {
 
         teamBrukerRepository.deleteAll().block();
         teamRepository.deleteAll().block();
+        identRepository.deleteAll().block();
+        brukerFavoritterRepository.deleteAll().block();
+        transaksjonMappingRepository.deleteAll().block();
+        bestillingProgressRepository.deleteAll().block();
+        bestillingKontrollRepository.deleteAll().block();
+        bestillingRepository.deleteAll().block();
+        testgruppeRepository.deleteAll().block();
         brukerRepository.deleteAll().block();
     }
 
