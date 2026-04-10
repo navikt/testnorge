b57f49b9a729833dfdd5ec248b299de38f65044b
Cato Olsen <olsen.cato@gmail.com>
2026-04-10 14:12:11 +0200

    Fixes Sonar plugin.

--- plugins/java/src/main/java/no/nav/dolly/plugins/DollySonarPlugin.java
+++ plugins/java/src/main/java/no/nav/dolly/plugins/DollySonarPlugin.java
@@ -2,6 +2,7 @@ package no.nav.dolly.plugins;
 
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.sonarqube.gradle.SonarExtension;
 
 public class DollySonarPlugin implements Plugin<Project> {
 
@@ -12,17 +13,18 @@ public class DollySonarPlugin implements Plugin<Project> {
         plugins.apply("jacoco");
         plugins.apply("org.sonarqube");
 
-        project.afterEvaluate(p -> {
-            var sonarqubeExt = p.getExtensions().findByName("sonarqube");
-            if (sonarqubeExt != null) {
-                p.getExtensions().getByName("sonarqube");
-            }
+        var sonar = project.getExtensions().getByType(SonarExtension.class);
+        sonar.properties(props -> {
+            props.property("sonar.dynamicAnalysis", "reuseReports");
+            props.property("sonar.host.url", "https://sonarcloud.io");
+            props.property("sonar.java.coveragePlugin", "jacoco");
+            props.property("sonar.language", "java");
+            props.property("sonar.organization", "navikt");
+            props.property("sonar.project.monorepo.enabled", true);
+            props.property("sonar.sourceEncoding", "UTF-8");
+            props.property("sonar.token", System.getenv("SONAR_TOKEN"));
         });
 
     }
 
 }
-
-
-
-
