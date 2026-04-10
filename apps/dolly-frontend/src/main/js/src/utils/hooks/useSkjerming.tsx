876fb4fc07a8e7cc2352161cd0e3322f24e4b6e0
stigus <stian.gustavsson@nav.no>
2026-04-10 11:36:01 +0200

    - Update useSkjerming to construct the fetch URL directly with query parameters. - Modify the fetcher function call to use a consistent parameter structure. #deploy-test-dolly-frontend

--- apps/dolly-frontend/src/main/js/src/utils/hooks/useSkjerming.tsx
+++ apps/dolly-frontend/src/main/js/src/utils/hooks/useSkjerming.tsx
@@ -6,8 +6,8 @@ const skjermingUrl = '/testnav-dolly-proxy/skjermingsregister/api/v1/skjerming/d
 
 export const useSkjerming = (ident: string) => {
 	const { data, isLoading, error, mutate } = useSWR<any, Error>(
-		ident?.length > 0 ? [skjermingUrl, { personident: ident }] : null,
-		([url, params]) => fetcher(url, params),
+		ident?.length > 0 ? `${skjermingUrl}?personident=${ident}` : null,
+		() => fetcher(skjermingUrl, { personident: ident }),
 	)
 
 	return {
