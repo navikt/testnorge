9960f0882f9e15ab2a7c5d8665efe48bead67867
Kristen Hærum <krharum@gmail.com>
2026-05-06 07:18:50 +0200

    Fix: correct spelling of Kelvin AAP in ElasticTyper enum #deploy-dolly-search-service

--- libs/data-transfer-objects/src/main/java/no/nav/testnav/libs/dto/dollysearchservice/v1/ElasticTyper.java
+++ libs/data-transfer-objects/src/main/java/no/nav/testnav/libs/dto/dollysearchservice/v1/ElasticTyper.java
@@ -26,7 +26,7 @@ public enum ElasticTyper {
     INNTK("Inntektskomponenten/stub (INNTK)", false),
     INNTKMELD("Inntektsmelding (ALTINN/JOARK)", true),
     INST("Institusjonsopphold (INST2)", true),
-    KELVIN_AAP("Kvinne AAP ytelse", false),
+    KELVIN_AAP("Kelvin AAP ytelse", false),
     KRRSTUB("Kontakt- og reservasjonsregister-stub", false),
     MEDL("Medlemskap (MEDL)", false),
     NOM("Nav-ansatt (NOM)", false),
