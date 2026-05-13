9cb14fcbfd814343a305ca477b0b930a97e01fba
Kristen Hærum <krharum@gmail.com>
2026-05-13 15:02:09 +0200

    Fix: Enhance validation logic for ForelderBarnRelasjon and Opphold in respective service tests

--- apps/pdl-forvalter/src/test/java/no/nav/pdl/forvalter/service/OppholdServiceTest.java
+++ apps/pdl-forvalter/src/test/java/no/nav/pdl/forvalter/service/OppholdServiceTest.java
@@ -1,6 +1,7 @@
 package no.nav.pdl.forvalter.service;
 
 import no.nav.pdl.forvalter.database.model.DbPerson;
+import no.nav.testnav.libs.dto.pdlforvalter.v1.DbVersjonDTO;
 import no.nav.testnav.libs.dto.pdlforvalter.v1.OppholdDTO;
 import no.nav.testnav.libs.dto.pdlforvalter.v1.PersonDTO;
 import org.junit.jupiter.api.Test;
@@ -18,6 +19,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
 
 @ExtendWith(MockitoExtension.class)
 class OppholdServiceTest {
@@ -148,6 +150,159 @@ class OppholdServiceTest {
                     assertThat(target.getPerson().getOpphold().get(1).getOppholdTil(),
                             is(equalTo(LocalDate.of(2020, 2, 3).atStartOfDay())));
                 })
+                .verifyComplete();
+    }
+
+    @Test
+    void shouldValidateSuccessfullyWhenRequestIsValid() {
+
+        var request = OppholdDTO.builder()
+                .oppholdFra(LocalDate.of(2020, 1, 1).atStartOfDay())
+                .oppholdTil(LocalDate.of(2020, 6, 1).atStartOfDay())
+                .type(MIDLERTIDIG)
+                .isNew(true)
+                .build();
+
+        StepVerifier.create(oppholdService.validate(request))
+                .verifyComplete();
+    }
+
+    @Test
+    void shouldThrowWhenOppholdFraEqualsOppholdTil() {
+
+        var request = OppholdDTO.builder()
+                .oppholdFra(LocalDate.of(2020, 1, 1).atStartOfDay())
+                .oppholdTil(LocalDate.of(2020, 1, 1).atStartOfDay())
+                .type(MIDLERTIDIG)
+                .isNew(true)
+                .build();
+
+        StepVerifier.create(oppholdService.validate(request))
+                .verifyErrorSatisfies(throwable ->
+                        assertThat(throwable.getMessage(), containsString("Ugyldig datointervall")));
+    }
+
+    @Test
+    void shouldValidateSuccessfullyWhenOnlyOppholdFraIsProvided() {
+
+        var request = OppholdDTO.builder()
+                .oppholdFra(LocalDate.of(2020, 1, 1).atStartOfDay())
+                .type(MIDLERTIDIG)
+                .isNew(true)
+                .build();
+
+        StepVerifier.create(oppholdService.validate(request))
+                .verifyComplete();
+    }
+
+    @Test
+    void shouldConvertSuccessfullyWhenOppholdListIsEmpty() {
+
+        var request = DbPerson.builder()
+                .person(PersonDTO.builder()
+                        .ident(IDENT)
+                        .opphold(List.of())
+                        .build())
+                .build();
+
+        StepVerifier.create(oppholdService.convert(request))
+                .assertNext(target -> assertThat(target.getPerson().getOpphold().isEmpty(), is(true)))
+                .verifyComplete();
+    }
+
+    @Test
+    void shouldSkipItemsNotMarkedAsNew() {
+
+        var request = DbPerson.builder()
+                .person(PersonDTO.builder()
+                        .ident(IDENT)
+                        .opphold(List.of(OppholdDTO.builder()
+                                .oppholdFra(LocalDate.of(2020, 1, 1).atStartOfDay())
+                                .type(MIDLERTIDIG)
+                                .isNew(false)
+                                .build()))
+                        .build())
+                .build();
+
+        StepVerifier.create(oppholdService.convert(request))
+                .assertNext(target -> {
+                    var opphold = target.getPerson().getOpphold().getFirst();
+                    assertThat(opphold.getKilde(), is(nullValue()));
+                    assertThat(opphold.getMaster(), is(nullValue()));
+                })
+                .verifyComplete();
+    }
+
+    @Test
+    void shouldSetKildeAndMasterOnNewItems() {
+
+        var request = DbPerson.builder()
+                .person(PersonDTO.builder()
+                        .ident(IDENT)
+                        .opphold(List.of(OppholdDTO.builder()
+                                .oppholdFra(LocalDate.of(2020, 1, 1).atStartOfDay())
+                                .type(MIDLERTIDIG)
+                                .isNew(true)
+                                .build()))
+                        .build())
+                .build();
+
+        StepVerifier.create(oppholdService.convert(request))
+                .assertNext(target -> {
+                    var opphold = target.getPerson().getOpphold().getFirst();
+                    assertThat(opphold.getKilde(), is(equalTo("Dolly")));
+                    assertThat(opphold.getMaster(), is(equalTo(DbVersjonDTO.Master.PDL)));
+                })
+                .verifyComplete();
+    }
+
+    @Test
+    void shouldSkipOverlapCheckWhenCurrentOppholdFraIsNull() {
+
+        var request = DbPerson.builder()
+                .person(PersonDTO.builder()
+                        .ident(IDENT)
+                        .opphold(List.of(
+                                OppholdDTO.builder()
+                                        .type(OPPLYSNING_MANGLER)
+                                        .isNew(true)
+                                        .build(),
+                                OppholdDTO.builder()
+                                        .oppholdFra(LocalDate.of(2020, 1, 1).atStartOfDay())
+                                        .type(MIDLERTIDIG)
+                                        .isNew(true)
+                                        .build()))
+                        .build())
+                .build();
+
+        StepVerifier.create(oppholdService.convert(request))
+                .assertNext(target -> assertThat(target.getPerson().getOpphold().size(), is(equalTo(2))))
+                .verifyComplete();
+    }
+
+    @Test
+    void shouldAcceptNonOverlappingEntries() {
+
+        var request = DbPerson.builder()
+                .person(PersonDTO.builder()
+                        .ident(IDENT)
+                        .opphold(List.of(
+                                OppholdDTO.builder()
+                                        .oppholdFra(LocalDate.of(2021, 1, 1).atStartOfDay())
+                                        .type(OPPLYSNING_MANGLER)
+                                        .isNew(true)
+                                        .build(),
+                                OppholdDTO.builder()
+                                        .oppholdFra(LocalDate.of(2020, 1, 1).atStartOfDay())
+                                        .oppholdTil(LocalDate.of(2020, 6, 1).atStartOfDay())
+                                        .type(MIDLERTIDIG)
+                                        .isNew(true)
+                                        .build()))
+                        .build())
+                .build();
+
+        StepVerifier.create(oppholdService.convert(request))
+                .assertNext(target -> assertThat(target.getPerson().getOpphold().size(), is(equalTo(2))))
                 .verifyComplete();
     }
 }
