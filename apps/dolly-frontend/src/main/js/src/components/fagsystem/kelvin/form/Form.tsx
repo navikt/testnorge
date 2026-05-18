995f4c4bbd4c6d699672b8cb9750ca564750052b
Betsy Carina Traran <betsy.carina.traran@nav.no>
2026-05-18 11:41:08 +0200

    Tilpass navn paa kelvin aap-ytelse

--- apps/dolly-frontend/src/main/js/src/components/fagsystem/kelvin/form/Form.tsx
+++ apps/dolly-frontend/src/main/js/src/components/fagsystem/kelvin/form/Form.tsx
@@ -44,7 +44,7 @@ export const KelvinAapForm = () => {
 	return (
 		<Vis attributt={kelvinAapPath}>
 			<Panel
-				heading="AAP-ytelse"
+				heading="Kelvin AAP-ytelse"
 				hasErrors={usePanelError(kelvinAapPath)}
 				iconType="arena"
 				startOpen={erForsteEllerTest(formMethods.getValues(), [kelvinAapPath])}
