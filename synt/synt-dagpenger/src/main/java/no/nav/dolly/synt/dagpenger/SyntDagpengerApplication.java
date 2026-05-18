f306467d1943c2aaddffbc681b1b3160bc3eed33
Cato Olsen <olsen.cato@gmail.com>
2026-05-18 14:53:37 +0200

    Refactored app config, and moved prod config into new file.

--- synt/synt-dagpenger/src/main/java/no/nav/dolly/synt/dagpenger/SyntDagpengerApplication.java
+++ synt/synt-dagpenger/src/main/java/no/nav/dolly/synt/dagpenger/SyntDagpengerApplication.java
@@ -1,8 +1,14 @@
 package no.nav.dolly.synt.dagpenger;
 
+import lombok.Getter;
+import lombok.Setter;
 import no.nav.dolly.libs.nais.NaisEnvironmentApplicationContextInitializer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @SpringBootApplication
 public class SyntDagpengerApplication {
@@ -11,6 +17,17 @@ public class SyntDagpengerApplication {
         new SpringApplicationBuilder(SyntDagpengerApplication.class)
                 .initializers(new NaisEnvironmentApplicationContextInitializer())
                 .run(args);
+    }
+
+    @Component
+    @ConfigurationProperties(prefix = "app.config")
+    @Getter
+    @Setter
+    public static class Config {
+
+        private String bucket;
+        private List<String> models;
+
     }
 
 }
