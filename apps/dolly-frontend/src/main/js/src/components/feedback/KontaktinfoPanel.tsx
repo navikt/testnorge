ce51d2873cb15d1713a085fd5c2ce29ff642202b
stigus <stian.gustavsson@nav.no>
2026-05-26 11:30:00 +0200

    - Add Kontaktinfo and KontaktinfoPanel components with tests - Update KontaktinfoPanel to conditionally render Slack link based on user type - Update package dependencies to latest versions #deploy-test-dolly-frontend

--- apps/dolly-frontend/src/main/js/src/components/feedback/KontaktinfoPanel.tsx
+++ apps/dolly-frontend/src/main/js/src/components/feedback/KontaktinfoPanel.tsx
@@ -1,92 +1,91 @@
-import { Button, VStack } from '@navikt/ds-react'
+import {Button, VStack} from '@navikt/ds-react'
 import styled from 'styled-components'
-import { ChatElipsisIcon, ChevronDownIcon, EnvelopeClosedIcon } from '@navikt/aksel-icons'
+import {ChatElipsisIcon, ChevronDownIcon, EnvelopeClosedIcon} from '@navikt/aksel-icons'
 import '@/styles/variables.less'
 import Icon from '@/components/ui/icon/Icon'
-import { TestComponentSelectors } from '#/mocks/Selectors'
-import { useCurrentBruker } from '@/utils/hooks/useBruker'
+import {TestComponentSelectors} from '#/mocks/Selectors'
+import {useCurrentBruker} from '@/utils/hooks/useBruker'
 
 const Panel = styled.div`
-	background-color: var(--ax-bg-accent-strong);
-	color: white;
-	padding: 5px 20px 25px 20px;
-	border-radius: 8px;
-	width: 300px;
+    background-color: var(--ax-bg-accent-strong);
+    color: white;
+    padding: 5px 20px 25px 20px;
+    border-radius: 8px;
+    width: 300px;
 
-	p {
-		margin-bottom: 25px;
-		line-height: normal;
-	}
+    p {
+        margin-bottom: 25px;
+        line-height: normal;
+    }
 `
 
 const Header = styled.div`
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
-	margin-bottom: -18px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: -18px;
 
-	h2 {
-		font-size: 1.8em;
-	}
+    h2 {
+        font-size: 1.8em;
+    }
 `
 
 const StyledButton = styled(Button)`
-	background-color: var(--ax-bg-default);
-	color: var(--ax-bg-accent-strong);
-	width: 100%;
+    background-color: var(--ax-bg-default);
+    color: var(--ax-bg-accent-strong);
+    width: 100%;
 
-	&:hover {
-		background-color: var(--ax-bg-accent-moderate-hover);
-	}
+    &:hover {
+        background-color: var(--ax-bg-accent-moderate-hover);
+    }
 `
 
-export const KontaktinfoPanel = ({ setOpenState, openKontaktskjema }: any) => {
-	const { currentBruker } = useCurrentBruker()
-	const bankIdBruker = currentBruker?.brukertype === 'BANKID'
+export const KontaktinfoPanel = ({setOpenState, openKontaktskjema}: any) => {
+    const {currentBruker} = useCurrentBruker()
+    const bankIdBruker = currentBruker?.brukertype === 'BANKID'
 
-	const closePanel = () => setOpenState(false)
+    const closePanel = () => setOpenState(false)
 
-	return (
-		<Panel>
-			<Header>
-				<h2>Kontakt oss</h2>
-				<Button size="small" onClick={closePanel} icon={<ChevronDownIcon title="Lukk" />} />
-			</Header>
-			<p>
-				{`Team Dolly er tilgjengelige på ${bankIdBruker ? '' : 'Slack (#dolly), '}e-post (dolly@nav.no), og via kontaktskjema.`}
-			</p>
-			<VStack gap="space-12">
-				{!bankIdBruker && (
-					<a href={'https://nav-it.slack.com/archives/CA3P9NGA2'} target="_blank">
-						<StyledButton
-							iconPosition="left"
-							icon={<Icon kind="slack" fill="#2176d4" />}
-							onClick={closePanel}
-						>
-							Gå til Slack-kanal
-						</StyledButton>
-					</a>
-				)}
-				<a href={'mailto:dolly@nav.no'}>
-					<StyledButton
-						iconPosition="left"
-						icon={<EnvelopeClosedIcon aria-hidden />}
-						style={{ width: '100%' }}
-						onClick={closePanel}
-					>
-						Send e-post
-					</StyledButton>
-				</a>
-				<StyledButton
-					data-testid={TestComponentSelectors.BUTTON_OPEN_KONTAKTSKJEMA}
-					iconPosition="left"
-					icon={<ChatElipsisIcon aria-hidden />}
-					style={{ width: '100%' }}
-					onClick={openKontaktskjema}
-				>
-					Åpne kontaktskjema
-				</StyledButton>
-			</VStack>
-		</Panel>
-	)
+    return (
+        <Panel>
+            <Header>
+                <h2>Kontakt oss</h2>
+                <Button size="small" onClick={closePanel} icon={<ChevronDownIcon title="Lukk"/>}/>
+            </Header>
+            <p>
+                {`Team Dolly er tilgjengelige på Slack (${bankIdBruker ? "#ext-dolly-syntetiske-data" : "#dolly"}), e-post (dolly@nav.no), og via kontaktskjema.`}
+            </p>
+            <VStack gap="space-12">
+                <a href={bankIdBruker ? 'https://offentlig-paas-no.slack.com/archives/C0B2BAXKJKV' : 'https://nav-it.slack.com/archives/CA3P9NGA2'}
+                   target="_blank">
+                    <StyledButton
+                        iconPosition="left"
+                        icon={<Icon kind="slack" fill="#2176d4"/>}
+                        onClick={closePanel}
+                    >
+                        Gå til Slack-kanal
+                    </StyledButton>
+                </a>
+                <a href={'mailto:dolly@nav.no'}>
+                    <StyledButton
+                        iconPosition="left"
+                        icon={<EnvelopeClosedIcon aria-hidden/>}
+                        style={{width: '100%'}}
+                        onClick={closePanel}
+                    >
+                        Send e-post
+                    </StyledButton>
+                </a>
+                <StyledButton
+                    data-testid={TestComponentSelectors.BUTTON_OPEN_KONTAKTSKJEMA}
+                    iconPosition="left"
+                    icon={<ChatElipsisIcon aria-hidden/>}
+                    style={{width: '100%'}}
+                    onClick={openKontaktskjema}
+                >
+                    Åpne kontaktskjema
+                </StyledButton>
+            </VStack>
+        </Panel>
+    )
 }
