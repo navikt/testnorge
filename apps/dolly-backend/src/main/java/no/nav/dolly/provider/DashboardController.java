bdfd321ef1ddd8e1b2a86836be4ecbfa3f282687
Kristen Hærum <krharum@gmail.com>
2026-06-05 07:23:46 +0200

    feature: Update operation descriptions in DashboardController for clarity and specificity #deploy-test-dolly-backend #deploy-dolly-backend

--- apps/dolly-backend/src/main/java/no/nav/dolly/provider/DashboardController.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/provider/DashboardController.java
@@ -20,28 +20,28 @@ public class DashboardController {
     private final DashboardService dashboardService;
 
     @GetMapping(value = "/personer")
-    @Operation(description = "Henter status for personer levert")
+    @Operation(description = "Henter status for personer levert (opprettet/importert og/eller gjenopprettet).")
     public Flux<DashboardPersonerDTO> getDashboardPersoner() {
 
         return dashboardService.getPersonerStatus();
     }
 
     @GetMapping(value = "/teams")
-    @Operation(description = "Henter status for team fra teamkatalogen, og antall unike personer som har bestilt")
+    @Operation(description = "Henter status per team i hht Teamkatalogen og antall unike personer som har bestilt. Gjelder AZURE-brukere.")
     public Flux<DashboardTeamsDTO> getDashboardTeams() {
 
         return dashboardService.getTeamsStatus();
     }
 
     @GetMapping(value = "/organisasjoner")
-    @Operation(description = "Henter status for organisasjoner fra Altinn, og antall unike personer som har bestilt")
+    @Operation(description = "Henter status per organisasjon med antall unike personer som har bestilt. Gjelder BANKID-brukere.")
     public Flux<DashboardOrganisasjonerDTO> getDashboardOrganisasjoner() {
 
         return dashboardService.getOrganisasjonerStatus();
     }
 
     @GetMapping(value = "/dollyteams")
-    @Operation(description = "Henter status for bruk av Dolly-teams, og antall unike personer som har bestilt")
+    @Operation(description = "Henter status for bruk av Dolly-teams, og antall medlemmer per team. Gjelder TEAM-brukere.")
     public Flux<DashboardDollyTeamsDTO> getDashboardDollyTeams() {
 
         return dashboardService.getDollyTeamsStatus();
