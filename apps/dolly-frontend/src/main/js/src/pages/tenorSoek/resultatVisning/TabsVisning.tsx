19f0e41dacc679800008af903270430d2096bc86
Inger.Liepelt <inger.liepelt@nav.no>
2026-06-22 10:08:17 +0200

    Refactor TabsVisning to lazy load PrettyCode component for improved performance

--- apps/dolly-frontend/src/main/js/src/pages/tenorSoek/resultatVisning/TabsVisning.tsx
+++ apps/dolly-frontend/src/main/js/src/pages/tenorSoek/resultatVisning/TabsVisning.tsx
@@ -4,6 +4,8 @@ import styled from 'styled-components'
 import { FileCodeIcon, KeyVerticalIcon } from '@navikt/aksel-icons'
 import Loading from '@/components/ui/loading/Loading'
 
+const PrettyCode = lazy(() => import('@/components/codeView/PrettyCode'))
+
 const TabsVisningFormatter = styled.div`
 	width: 100%;
 
@@ -21,8 +23,6 @@ const TabsVisningFormatter = styled.div`
 `
 
 export const TabsVisning = ({ children, kildedata }: any) => {
-	const PrettyCode = lazy(() => import('@/components/codeView/PrettyCode'))
-
 	if (!kildedata) {
 		return <div className="person-visning_content">{children}</div>
 	}
