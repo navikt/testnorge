5d192c18ad00d4d0fd81b2dd482e3117a0cd57bd
Kristen Hærum <krharum@gmail.com>
2026-06-29 09:10:21 +0200

    Reapply "Fix: status for Inntektstub"
    This reverts commit 3e6b5ec7762c58c75b1fd5ceaaf2ad8461595589.

--- apps/dolly-backend/src/main/java/no/nav/dolly/mapper/BestillingInntektstubStatusMapper.java
+++ apps/dolly-backend/src/main/java/no/nav/dolly/mapper/BestillingInntektstubStatusMapper.java
@@ -5,40 +5,36 @@ import lombok.NoArgsConstructor;
 import no.nav.dolly.domain.jpa.BestillingProgress;
 import no.nav.dolly.domain.resultset.RsStatusRapport;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import static java.util.Collections.emptyList;
 import static java.util.Collections.singletonList;
 import static no.nav.dolly.domain.resultset.SystemTyper.INNTK;
 import static no.nav.dolly.mapper.StatusMiljoeIdentForholdUtility.decodeMsg;
-import static org.apache.commons.lang3.StringUtils.isNotBlank;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class BestillingInntektstubStatusMapper {
 
     public static List<RsStatusRapport> buildInntektstubStatusMap(List<BestillingProgress> progressList) {
 
-        Map<String, List<String>> statusMap = new HashMap<>();
+        Map<String, Set<String>> statusMap = new HashMap<>();
 
-        progressList.forEach(progress -> {
-            if (isNotBlank(progress.getInntektstubStatus()) && isNotBlank(progress.getIdent())) {
-                if (statusMap.containsKey(progress.getInntektstubStatus())) {
-                    statusMap.get(progress.getInntektstubStatus()).add(progress.getIdent());
-                } else {
-                    statusMap.put(progress.getInntektstubStatus(), new ArrayList<>(List.of(progress.getIdent())));
-                }
-            }
-        });
+        progressList.forEach(progress ->
+                Arrays.stream(progress.getInntektstubStatus().split(","))
+                        .filter(melding -> statusMap.keySet().stream().noneMatch(status -> status.contains(melding)))
+                        .forEach(melding -> statusMap.computeIfAbsent(melding, _ -> new HashSet<>()).add(progress.getIdent())));
 
         return statusMap.isEmpty() ? emptyList() :
                 singletonList(RsStatusRapport.builder().id(INNTK).navn(INNTK.getBeskrivelse())
                         .statuser(statusMap.entrySet().stream()
                                 .map(entry -> RsStatusRapport.Status.builder()
                                         .melding(decodeMsg(entry.getKey()))
-                                        .identer(entry.getValue())
+                                        .identer(entry.getValue().stream().toList())
                                         .build())
                                 .toList())
                         .build());
