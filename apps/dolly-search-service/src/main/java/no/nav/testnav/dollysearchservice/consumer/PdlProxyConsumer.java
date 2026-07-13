a00e6be4359e1c18b57c62831f5b8c0851c44487
Cato Olsen <cato.olsen@nav.no>
2026-07-13 11:15:57 +0200

    cleanup/dolly-search-service-jsonmapper (#4235)
    Fixes deserialization issues.

--- apps/dolly-search-service/src/main/java/no/nav/testnav/dollysearchservice/consumer/PdlProxyConsumer.java
+++ apps/dolly-search-service/src/main/java/no/nav/testnav/dollysearchservice/consumer/PdlProxyConsumer.java
@@ -7,15 +7,16 @@ import no.nav.testnav.dollysearchservice.consumer.command.TagsPostCommand;
 import no.nav.testnav.dollysearchservice.dto.TagsOpprettingResponse;
 import no.nav.testnav.libs.reactivesecurity.exchange.TokenExchange;
 import no.nav.testnav.libs.securitycore.domain.ServerProperties;
+import org.springframework.http.codec.json.JacksonJsonDecoder;
+import org.springframework.http.codec.json.JacksonJsonEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
-import tools.jackson.databind.ObjectMapper;
+import tools.jackson.databind.json.JsonMapper;
 
 import java.util.List;
 import java.util.Map;
-
-import static no.nav.testnav.dollysearchservice.consumer.utils.JacksonExchangeStrategyUtil.getJacksonStrategy;
 
 @Slf4j
 @Service
@@ -28,22 +29,34 @@ public class PdlProxyConsumer {
     public PdlProxyConsumer(
             TokenExchange tokenExchange,
             Consumers consumers,
-            ObjectMapper objectMapper,
+            JsonMapper jsonMapper,
             WebClient webClient
     ) {
         serverProperties = consumers.getTestnavPdlProxy();
         this.webClient = webClient
                 .mutate()
                 .baseUrl(serverProperties.getUrl())
-                .exchangeStrategies(getJacksonStrategy(objectMapper))
+                .exchangeStrategies(getJacksonStrategy(jsonMapper))
                 .build();
         this.tokenExchange = tokenExchange;
+    }
+
+    private static ExchangeStrategies getJacksonStrategy(JsonMapper jsonMapper) {
+        return ExchangeStrategies
+                .builder()
+                .codecs(
+                        config -> {
+                            config.defaultCodecs().maxInMemorySize(32 * 1024 * 1024);
+                            config.defaultCodecs().jacksonJsonDecoder(new JacksonJsonDecoder(jsonMapper));
+                            config.defaultCodecs().jacksonJsonEncoder(new JacksonJsonEncoder(jsonMapper));
+                        })
+                .build();
     }
 
     public Mono<Map<String, List<String>>> getTags(List<String> identer) {
 
         return tokenExchange.exchange(serverProperties)
-                .flatMap(token-> new TagsGetCommand(webClient, identer, token.getTokenValue()).call())
+                .flatMap(token -> new TagsGetCommand(webClient, identer, token.getTokenValue()).call())
                 .doOnNext(response -> log.info("Hentet tags for {} personer", response.size()));
     }
 
