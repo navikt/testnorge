3b955973305858e9f340d741b0e5d1f600f81277
Kristen Hærum <kristen.herum@nav.no>
2026-08-12 08:35:22 +0200

    Bugfix/json mapper monorepo konflikter (#4296)

--- apps/api-oversikt-service/src/main/java/no/nav/testnav/apps/apioversiktservice/service/ApiOversiktService.java
+++ apps/api-oversikt-service/src/main/java/no/nav/testnav/apps/apioversiktservice/service/ApiOversiktService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 import org.yaml.snakeyaml.Yaml;
 import reactor.core.publisher.Mono;
 import tools.jackson.databind.JsonNode;
-import tools.jackson.databind.ObjectMapper;
+import tools.jackson.databind.json.JsonMapper;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -21,7 +21,7 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 public class ApiOversiktService {
 
     private static final String PATH_RESOURCE = "data/apioversikt.yml";
-    private final ObjectMapper objectMapper;
+    private final JsonMapper jsonMapper;
 
     public Mono<JsonNode> getDokumeter() {
 
@@ -31,8 +31,8 @@ public class ApiOversiktService {
                 var yaml = reader.lines().collect(Collectors.joining("\n"));
 
                 var maps = new Yaml().load(yaml);
-                var jsonString = objectMapper.writeValueAsString(maps);
-                return Mono.just(objectMapper.readTree(jsonString));
+                var jsonString = jsonMapper.writeValueAsString(maps);
+                return Mono.just(jsonMapper.readTree(jsonString));
             }
         } catch (Exception e) {
             log.error("Lesing av query ressurs {} feilet", PATH_RESOURCE, e);
