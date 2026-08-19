7059e40da288061b838e073d8f05d27eac2d929c
Betsy Carina Traran <betsy.carina.traran@nav.no>
2026-08-19 13:45:03 +0200

    Oppfoelgingsvedtak14a form

---
+++ apps/dolly-frontend/src/main/js/src/components/fagsystem/oppfoelgingsvedtak14a/form/Form.tsx
@@ -0,0 +1,64 @@
+import { useFormContext } from 'react-hook-form'
+import { Vis } from '@/components/bestillingsveileder/VisAttributt'
+import { oppfoelgingsvedtak14aPath } from '@/components/fagsystem/oppfoelgingsvedtak14a/initialValues'
+import Panel from '@/components/ui/panel/Panel'
+import { erForsteEllerTest, usePanelError } from '@/components/ui/form/formUtils'
+import { SelectOptionsManager as Options } from '@/service/SelectOptions'
+import { FormSelect } from '@/components/ui/form/inputs/select/Select'
+import * as React from 'react'
+import { FormDatepicker } from '@/components/ui/form/inputs/datepicker/Datepicker'
+import { useAlleNavEnheter } from '@/utils/hooks/useNorg2'
+import { FormTextInput } from '@/components/ui/form/inputs/textInput/TextInput'
+
+export const Oppfoelgingsvedtak14aForm = () => {
+	const formMethods = useFormContext()
+	const { alleNavEnheter, loading: loadingEnheter } = useAlleNavEnheter()
+
+	return (
+		<Vis attributt={oppfoelgingsvedtak14aPath}>
+			<Panel
+				heading="Oppfølgingsvedtak § 14 a"
+				hasErrors={usePanelError(oppfoelgingsvedtak14aPath)}
+				iconType="arena"
+				startOpen={erForsteEllerTest(formMethods.getValues(), [oppfoelgingsvedtak14aPath])}
+			>
+				<div className={'flexbox--flex-wrap'}>
+					<FormSelect
+						name={`${oppfoelgingsvedtak14aPath}.innsatsgruppe`}
+						label="Innsatsgruppe"
+						options={Options('innsatsgruppe')}
+						size="xlarge"
+						isClearable={false}
+					/>
+					<FormSelect
+						name={`${oppfoelgingsvedtak14aPath}.hovedmal`}
+						label="Hovedmål"
+						options={Options('hovedmal')}
+						isClearable={false}
+					/>
+					<FormDatepicker
+						name={`${oppfoelgingsvedtak14aPath}.vedtakFattet`}
+						label="Vedtak fattet"
+						minDate={new Date()}
+					/>
+					<FormSelect
+						name={`${oppfoelgingsvedtak14aPath}.oppfolgingsEnhet`}
+						label="Oppfølgingsenhet"
+						options={alleNavEnheter}
+						isLoading={loadingEnheter}
+						size="xlarge"
+					/>
+					<FormTextInput
+						name={`${oppfoelgingsvedtak14aPath}.begrunnelse`}
+						label="Begrunnelse"
+						size="xlarge"
+					/>
+					<FormTextInput
+						name={`${oppfoelgingsvedtak14aPath}.veilederIdent`}
+						label="Veileder ident"
+					/>
+				</div>
+			</Panel>
+		</Vis>
+	)
+}
