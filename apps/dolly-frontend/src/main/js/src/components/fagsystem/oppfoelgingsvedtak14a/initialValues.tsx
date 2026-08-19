a4c4f431c496e6ee458b93615849f41d4c214f99
Betsy Carina Traran <betsy.carina.traran@nav.no>
2026-08-19 11:46:58 +0200

    Oppfoelgingsvedtak14a steg 2 panel

---
+++ apps/dolly-frontend/src/main/js/src/components/fagsystem/oppfoelgingsvedtak14a/initialValues.tsx
@@ -0,0 +1,10 @@
+export const oppfoelgingsvedtak14aPath = 'oppfoelgingsvedtak14a'
+
+export const initialOppfoelgingsvedtak14a = {
+	innsatsgruppe: 'GODE_MULIGHETER',
+	hovedmal: 'SKAFFE_ARBEID',
+	vedtakFattet: new Date(),
+	oppfolgingsEnhet: null,
+	begrunnelse: null,
+	veilederIdent: null,
+}
