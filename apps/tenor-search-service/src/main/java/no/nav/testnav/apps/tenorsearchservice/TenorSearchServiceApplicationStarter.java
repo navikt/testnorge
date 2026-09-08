f1ef321827645df105692c580a37529434c8eeb8
stigus <stian.gustavsson@nav.no>
2026-09-08 13:36:48 +0200

    Add NaisPkcs8ConversionInitializer and update database configuration
    - Include NaisPkcs8ConversionInitializer in application startup
    - Update database configuration to include SSL properties
    
    #deploy-tenor-search-service

--- apps/tenor-search-service/src/main/java/no/nav/testnav/apps/tenorsearchservice/TenorSearchServiceApplicationStarter.java
+++ apps/tenor-search-service/src/main/java/no/nav/testnav/apps/tenorsearchservice/TenorSearchServiceApplicationStarter.java
@@ -1,6 +1,7 @@
 package no.nav.testnav.apps.tenorsearchservice;
 
 import no.nav.dolly.libs.nais.NaisEnvironmentApplicationContextInitializer;
+import no.nav.dolly.libs.nais.NaisPkcs8ConversionInitializer;
 import no.nav.testnav.libs.reactivecore.config.CoreConfig;
 import no.nav.testnav.libs.reactivesecurity.config.SecureOAuth2ServerToServerConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,7 +19,9 @@ public class TenorSearchServiceApplicationStarter {
 
     static void main(String[] args) {
         new SpringApplicationBuilder(TenorSearchServiceApplicationStarter.class)
-                .initializers(new NaisEnvironmentApplicationContextInitializer())
+                .initializers(
+                        new NaisEnvironmentApplicationContextInitializer(),
+                        new NaisPkcs8ConversionInitializer())
                 .run(args);
     }
 }
