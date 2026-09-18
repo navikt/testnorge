cb92ea810451107deb6111ddaf28b0d96ecd6b75
Kristen Hærum <krharum@gmail.com>
2026-09-18 15:53:25 +0200

    Fix: update springCloud version to 2025.1.3 and improve method invocation handling in MergeService

--- plugins/java/src/main/java/no/nav/dolly/plugins/DollyVersionsPlugin.java
+++ plugins/java/src/main/java/no/nav/dolly/plugins/DollyVersionsPlugin.java
@@ -17,7 +17,7 @@ public class DollyVersionsPlugin implements Plugin<Project> {
 
         public String springBoot = "4.1.1";
         public String springSession = "4.0.2";
-        public String springCloud = "2025.1.1";
+        public String springCloud = "2025.1.3";
 
         public String gcpSecretManager = "8.0.2";
         public String gcpStorage = "2.44.0";
